eed6a23bae48071f87dec56de94ba52170299a7f
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-06-13 11:54:22 -0300

    Update MINERI_g17_transformer.py

--- MINERI/MINERI_g17_transformer.py
+++ MINERI/MINERI_g17_transformer.py
@@ -4,15 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'destino': 'serie', 'concepto': 'subsubserie', 'porcentaje_total': 'valor'})
+rename_columns(destino='categoria', concepto='subcategoria', porcentaje_total='valor')
 )
 #  PIPELINE_END
 
@@ -32,18 +32,18 @@ rename_cols(map={'destino': 'serie', 'concepto': 'subsubserie', 'porcentaje_tota
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'destino': 'serie', 'concepto': 'subsubserie', 'porcentaje_total': 'valor'})
+#  rename_columns(destino='categoria', concepto='subcategoria', porcentaje_total='valor')
 #  RangeIndex: 16 entries, 0 to 15
 #  Data columns (total 3 columns):
-#   #   Column       Non-Null Count  Dtype  
-#  ---  ------       --------------  -----  
-#   0   serie        16 non-null     object 
-#   1   subsubserie  16 non-null     object 
-#   2   valor        16 non-null     float64
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   categoria     16 non-null     object 
+#   1   subcategoria  16 non-null     object 
+#   2   valor         16 non-null     float64
 #  
-#  |    | serie   | subsubserie                                                    |   valor |
-#  |---:|:--------|:---------------------------------------------------------------|--------:|
-#  |  0 | local   | consumo intermedio nacional (neto de importaciones indirectas) |    29.8 |
+#  |    | categoria   | subcategoria                                                   |   valor |
+#  |---:|:------------|:---------------------------------------------------------------|--------:|
+#  |  0 | local       | consumo intermedio nacional (neto de importaciones indirectas) |    29.8 |
 #  
 #  ------------------------------
 #  
