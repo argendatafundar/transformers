431b8d41a26d8f031337d9defe44729fd3930b30
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-06-13 11:54:30 -0300

    Update MINERI_g14_transformer.py

--- MINERI/MINERI_g14_transformer.py
+++ MINERI/MINERI_g14_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
-    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-wide_to_long(primary_keys=['rama_actividad', 'categoria_ocupacional'], value_name='valor', var_name='indicador')
+rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indicador', 'porcentaje_sobre_total_rama': 'valor'})
 )
 #  PIPELINE_END
 
@@ -31,19 +32,18 @@ wide_to_long(primary_keys=['rama_actividad', 'categoria_ocupacional'], value_nam
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['rama_actividad', 'categoria_ocupacional'], value_name='valor', var_name='indicador')
+#  rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indicador', 'porcentaje_sobre_total_rama': 'valor'})
 #  RangeIndex: 69 entries, 0 to 68
-#  Data columns (total 4 columns):
-#   #   Column                 Non-Null Count  Dtype  
-#  ---  ------                 --------------  -----  
-#   0   rama_actividad         69 non-null     object 
-#   1   categoria_ocupacional  69 non-null     object 
-#   2   indicador              69 non-null     object 
-#   3   valor                  69 non-null     float64
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  69 non-null     object 
+#   1   indicador  69 non-null     object 
+#   2   valor      69 non-null     float64
 #  
-#  |    | rama_actividad   | categoria_ocupacional   | indicador                   |   valor |
-#  |---:|:-----------------|:------------------------|:----------------------------|--------:|
-#  |  0 | Construcción     | asalariados_registrados | porcentaje_sobre_total_rama |   15.91 |
+#  |    | categoria    | indicador               |   valor |
+#  |---:|:-------------|:------------------------|--------:|
+#  |  0 | Construcción | asalariados_registrados |   15.91 |
 #  
 #  ------------------------------
 #  
