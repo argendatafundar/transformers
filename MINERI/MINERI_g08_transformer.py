8301a8c6606e40b69fb7b1458a1bd54b75f7a182
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-06-13 11:54:34 -0300

    Update MINERI_g08_transformer.py

--- MINERI/MINERI_g08_transformer.py
+++ MINERI/MINERI_g08_transformer.py
@@ -4,21 +4,34 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def datetime_to_year(df: DataFrame, col: str):
-    df[col] = pd.to_datetime(df[col]).dt.year
-    return df
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def media_doce_meses_indicador(df: DataFrame, indicador_col = 'indicador', anio_col = 'anio', value_col = 'valor'):
+    from pandas import to_datetime
+
+    result = {'anio': [], 'indicador': [], 'valor': []}
+    df[anio_col] = to_datetime(df[anio_col])
+    df[value_col] = df[value_col].astype(float)
+    for indicador, group in df.groupby(indicador_col):
+        group = group.drop(columns = indicador_col)
+
+        for yyyy, anio in  group.groupby(group.anio.dt.year):
+            result['indicador'].append(indicador)
+            result['anio'].append(yyyy)
+            result['valor'].append(anio.valor.mean())
+
+    result = DataFrame(result)
+    return result
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-datetime_to_year(col='fecha'),
-	rename_cols(map={'variable': 'serie'})
+rename_cols(map={'variable': 'indicador', 'fecha': 'anio'}),
+	media_doce_meses_indicador(indicador_col='indicador', anio_col='anio', value_col='valor')
 )
 #  PIPELINE_END
 
@@ -26,45 +39,45 @@ datetime_to_year(col='fecha'),
 #  start()
 #  RangeIndex: 4736 entries, 0 to 4735
 #  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype  
-#  ---  ------    --------------  -----  
-#   0   fecha     4736 non-null   object 
-#   1   variable  4736 non-null   object 
-#   2   valor     4736 non-null   float64
+#   #   Column     Non-Null Count  Dtype         
+#  ---  ------     --------------  -----         
+#   0   anio       4736 non-null   datetime64[ns]
+#   1   indicador  4736 non-null   object        
+#   2   valor      4736 non-null   float64       
 #  
-#  |    | fecha      | variable   |   valor |
-#  |---:|:-----------|:-----------|--------:|
-#  |  0 | 1960-01-01 | oro        |   35.27 |
-#  
-#  ------------------------------
-#  
-#  datetime_to_year(col='fecha')
-#  RangeIndex: 4736 entries, 0 to 4735
-#  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype  
-#  ---  ------    --------------  -----  
-#   0   fecha     4736 non-null   int32  
-#   1   variable  4736 non-null   object 
-#   2   valor     4736 non-null   float64
-#  
-#  |    |   fecha | variable   |   valor |
-#  |---:|--------:|:-----------|--------:|
-#  |  0 |    1960 | oro        |   35.27 |
+#  |    | anio                | indicador   |   valor |
+#  |---:|:--------------------|:------------|--------:|
+#  |  0 | 1960-01-01 00:00:00 | oro         |   35.27 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'variable': 'serie'})
+#  rename_cols(map={'variable': 'indicador', 'fecha': 'anio'})
 #  RangeIndex: 4736 entries, 0 to 4735
 #  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   fecha   4736 non-null   int32  
-#   1   serie   4736 non-null   object 
-#   2   valor   4736 non-null   float64
+#   #   Column     Non-Null Count  Dtype         
+#  ---  ------     --------------  -----         
+#   0   anio       4736 non-null   datetime64[ns]
+#   1   indicador  4736 non-null   object        
+#   2   valor      4736 non-null   float64       
 #  
-#  |    |   fecha | serie   |   valor |
-#  |---:|--------:|:--------|--------:|
-#  |  0 |    1960 | oro     |   35.27 |
+#  |    | anio                | indicador   |   valor |
+#  |---:|:--------------------|:------------|--------:|
+#  |  0 | 1960-01-01 00:00:00 | oro         |   35.27 |
+#  
+#  ------------------------------
+#  
+#  media_doce_meses_indicador(indicador_col='indicador', anio_col='anio', value_col='valor')
+#  RangeIndex: 402 entries, 0 to 401
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       402 non-null    int64  
+#   1   indicador  402 non-null    object 
+#   2   valor      402 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1960 | cobre       | 678.756 |
 #  
 #  ------------------------------
 #  
