89f7091231d7ad7ab4cc9fd782eef1ee19fad5eb
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 16:03:01 -0300

    Update SEBACO_g15_transformer.py

--- SEBACO/SEBACO_g15_transformer.py
+++ SEBACO/SEBACO_g15_transformer.py
@@ -4,21 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sector': 'indicador'}),
-	rename_cols(map={'prop_mujeres': 'valor'})
+rename_columns(sector='categoria', prop_mujeres='valor')
 )
 #  PIPELINE_END
 
@@ -38,31 +32,16 @@ rename_cols(map={'sector': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sector': 'indicador'})
-#  RangeIndex: 54 entries, 0 to 53
-#  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          54 non-null     int64  
-#   1   indicador     54 non-null     object 
-#   2   prop_mujeres  54 non-null     float64
-#  
-#  |    |   anio | indicador         |   prop_mujeres |
-#  |---:|-------:|:------------------|---------------:|
-#  |  0 |   1996 | Promedio economia |       0.267976 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'prop_mujeres': 'valor'})
+#  rename_columns(sector='categoria', prop_mujeres='valor')
 #  RangeIndex: 54 entries, 0 to 53
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       54 non-null     int64  
-#   1   indicador  54 non-null     object 
+#   1   categoria  54 non-null     object 
 #   2   valor      54 non-null     float64
 #  
-#  |    |   anio | indicador         |    valor |
+#  |    |   anio | categoria         |    valor |
 #  |---:|-------:|:------------------|---------:|
 #  |  0 |   1996 | Promedio economia | 0.267976 |
 #  
