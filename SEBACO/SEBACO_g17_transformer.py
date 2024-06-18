07bfce483847e163d787c6d0c54fa73fdff79b2e
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 16:04:27 -0300

    Update SEBACO_g17_transformer.py

--- SEBACO/SEBACO_g17_transformer.py
+++ SEBACO/SEBACO_g17_transformer.py
@@ -4,21 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sector': 'indicador'}),
-	rename_cols(map={'prop_teletrabajo': 'valor'})
+rename_columns(sector='categoria', prop_teletrabajo='valor')
 )
 #  PIPELINE_END
 
@@ -38,31 +32,16 @@ rename_cols(map={'sector': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sector': 'indicador'})
-#  RangeIndex: 21 entries, 0 to 20
-#  Data columns (total 3 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   anio              21 non-null     int64  
-#   1   indicador         21 non-null     object 
-#   2   prop_teletrabajo  21 non-null     float64
-#  
-#  |    |   anio | indicador   |   prop_teletrabajo |
-#  |---:|-------:|:------------|-------------------:|
-#  |  0 |   2016 | SBC         |           0.113657 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'prop_teletrabajo': 'valor'})
+#  rename_columns(sector='categoria', prop_teletrabajo='valor')
 #  RangeIndex: 21 entries, 0 to 20
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       21 non-null     int64  
-#   1   indicador  21 non-null     object 
+#   1   categoria  21 non-null     object 
 #   2   valor      21 non-null     float64
 #  
-#  |    |   anio | indicador   |    valor |
+#  |    |   anio | categoria   |    valor |
 #  |---:|-------:|:------------|---------:|
 #  |  0 |   2016 | SBC         | 0.113657 |
 #  
