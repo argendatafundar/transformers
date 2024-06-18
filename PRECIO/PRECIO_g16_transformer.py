4a6ac23ab605a905494ad43ce3d50259b8c740a6
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 11:40:34 -0300

    Update PRECIO_g16_transformer.py

--- PRECIO/PRECIO_g16_transformer.py
+++ PRECIO/PRECIO_g16_transformer.py
@@ -4,26 +4,26 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'tasa_inflacion': 'valor'})
+rename_columns(tasa_inflacion='valor')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 88 entries, 0 to 87
+#  RangeIndex: 89 entries, 0 to 88
 #  Data columns (total 2 columns):
 #   #   Column          Non-Null Count  Dtype  
 #  ---  ------          --------------  -----  
-#   0   anio            88 non-null     int64  
-#   1   tasa_inflacion  88 non-null     float64
+#   0   anio            89 non-null     int64  
+#   1   tasa_inflacion  89 non-null     float64
 #  
 #  |    |   anio |   tasa_inflacion |
 #  |---:|-------:|-----------------:|
@@ -31,13 +31,13 @@ rename_cols(map={'tasa_inflacion': 'valor'})
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'tasa_inflacion': 'valor'})
-#  RangeIndex: 88 entries, 0 to 87
+#  rename_columns(tasa_inflacion='valor')
+#  RangeIndex: 89 entries, 0 to 88
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    88 non-null     int64  
-#   1   valor   88 non-null     float64
+#   0   anio    89 non-null     int64  
+#   1   valor   89 non-null     float64
 #  
 #  |    |   anio |   valor |
 #  |---:|-------:|--------:|
