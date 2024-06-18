a4a27fc95a5209a461640095fd2a4b9cc830581b
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 16:05:41 -0300

    Update SEBACO_g20_transformer.py

--- SEBACO/SEBACO_g20_transformer.py
+++ SEBACO/SEBACO_g20_transformer.py
@@ -4,21 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'descripcion': 'indicador'}),
-	rename_cols(map={'prop_sobre_sbc_expo': 'valor'})
+rename_columns(descripcion='indicador', prop_sobre_sbc_expo='valor')
 )
 #  PIPELINE_END
 
@@ -38,22 +32,7 @@ rename_cols(map={'descripcion': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'descripcion': 'indicador'})
-#  RangeIndex: 102 entries, 0 to 101
-#  Data columns (total 3 columns):
-#   #   Column               Non-Null Count  Dtype  
-#  ---  ------               --------------  -----  
-#   0   anio                 102 non-null    int64  
-#   1   indicador            102 non-null    object 
-#   2   prop_sobre_sbc_expo  102 non-null    float64
-#  
-#  |    |   anio | indicador             |   prop_sobre_sbc_expo |
-#  |---:|-------:|:----------------------|----------------------:|
-#  |  0 |   2006 | Propiedad intelectual |             0.0319314 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'prop_sobre_sbc_expo': 'valor'})
+#  rename_columns(descripcion='indicador', prop_sobre_sbc_expo='valor')
 #  RangeIndex: 102 entries, 0 to 101
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
