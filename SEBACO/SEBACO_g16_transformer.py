fdc90aa65d5837b94b6d9ebf10ff2664aff5fbf4
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 16:03:47 -0300

    Update SEBACO_g16_transformer.py

--- SEBACO/SEBACO_g16_transformer.py
+++ SEBACO/SEBACO_g16_transformer.py
@@ -4,27 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'periodo': 'anio'}),
-	rename_cols(map={'sector': 'indicador'}),
-	rename_cols(map={'brecha': 'valor'})
+rename_columns(sector='categoria', brecha='valor', periodo='anio')
 )
 #  PIPELINE_END
 
@@ -40,52 +28,22 @@ rename_cols(map={'periodo': 'anio'}),
 #  
 #  |    |   periodo |   brecha | sector            |
 #  |---:|----------:|---------:|:------------------|
-#  |  0 |      1996 | 0.237825 | Promedio economía |
+#  |  0 |      1996 | 0.237825 | Promedio economÃ­a |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'periodo': 'anio'})
-#  RangeIndex: 54 entries, 0 to 53
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   anio    54 non-null     int64  
-#   1   brecha  54 non-null     float64
-#   2   sector  54 non-null     object 
-#  
-#  |    |   anio |   brecha | sector            |
-#  |---:|-------:|---------:|:------------------|
-#  |  0 |   1996 | 0.237825 | Promedio economía |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'sector': 'indicador'})
-#  RangeIndex: 54 entries, 0 to 53
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       54 non-null     int64  
-#   1   brecha     54 non-null     float64
-#   2   indicador  54 non-null     object 
-#  
-#  |    |   anio |   brecha | indicador         |
-#  |---:|-------:|---------:|:------------------|
-#  |  0 |   1996 | 0.237825 | Promedio economía |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'brecha': 'valor'})
+#  rename_columns(sector='categoria', brecha='valor', periodo='anio')
 #  RangeIndex: 54 entries, 0 to 53
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       54 non-null     int64  
 #   1   valor      54 non-null     float64
-#   2   indicador  54 non-null     object 
+#   2   categoria  54 non-null     object 
 #  
-#  |    |   anio |    valor | indicador         |
+#  |    |   anio |    valor | categoria         |
 #  |---:|-------:|---------:|:------------------|
-#  |  0 |   1996 | 0.237825 | Promedio economía |
+#  |  0 |   1996 | 0.237825 | Promedio economÃ­a |
 #  
 #  ------------------------------
 #  
