5801b4ee78466d11ddebf27473cf487addadf1e9
jgjuara <jg.juara@gmail.com>
2024-06-18 17:31:57 -0300

    Add AGROPE_g13_transformer.py

---
+++ AGROPE/AGROPE_g13_transformer.py
@@ -0,0 +1,186 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+replace_value(col='iso3', curr_value='F15', new_value='BLX'),
+	replace_value(col='iso3', curr_value='F228', new_value='SVU'),
+	replace_value(col='iso3', curr_value='F248', new_value='SER'),
+	replace_value(col='iso3', curr_value='F51', new_value='CSK'),
+	rename_cols(map={'iso3': 'geocodigo', 'rindes_trigo_ma5': 'valor'}),
+	drop_col(col='iso3_desc_fundar', axis=1),
+	drop_col(col='rindes', axis=1)
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   iso3              6876 non-null   object 
+#   1   iso3_desc_fundar  6876 non-null   object 
+#   2   anio              6876 non-null   int64  
+#   3   rindes            6876 non-null   float64
+#   4   rindes_trigo_ma5  6876 non-null   object 
+#  
+#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_trigo_ma5   |
+#  |---:|:-------|:-------------------|-------:|---------:|:-------------------|
+#  |  0 | AFG    | Afganistán         |   1961 |    1.022 | NA                 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='iso3', curr_value='F15', new_value='BLX')
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   iso3              6876 non-null   object 
+#   1   iso3_desc_fundar  6876 non-null   object 
+#   2   anio              6876 non-null   int64  
+#   3   rindes            6876 non-null   float64
+#   4   rindes_trigo_ma5  6876 non-null   object 
+#  
+#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_trigo_ma5   |
+#  |---:|:-------|:-------------------|-------:|---------:|:-------------------|
+#  |  0 | AFG    | Afganistán         |   1961 |    1.022 | NA                 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='iso3', curr_value='F228', new_value='SVU')
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   iso3              6876 non-null   object 
+#   1   iso3_desc_fundar  6876 non-null   object 
+#   2   anio              6876 non-null   int64  
+#   3   rindes            6876 non-null   float64
+#   4   rindes_trigo_ma5  6876 non-null   object 
+#  
+#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_trigo_ma5   |
+#  |---:|:-------|:-------------------|-------:|---------:|:-------------------|
+#  |  0 | AFG    | Afganistán         |   1961 |    1.022 | NA                 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='iso3', curr_value='F248', new_value='SER')
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   iso3              6876 non-null   object 
+#   1   iso3_desc_fundar  6876 non-null   object 
+#   2   anio              6876 non-null   int64  
+#   3   rindes            6876 non-null   float64
+#   4   rindes_trigo_ma5  6876 non-null   object 
+#  
+#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_trigo_ma5   |
+#  |---:|:-------|:-------------------|-------:|---------:|:-------------------|
+#  |  0 | AFG    | Afganistán         |   1961 |    1.022 | NA                 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='iso3', curr_value='F51', new_value='CSK')
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   iso3              6876 non-null   object 
+#   1   iso3_desc_fundar  6876 non-null   object 
+#   2   anio              6876 non-null   int64  
+#   3   rindes            6876 non-null   float64
+#   4   rindes_trigo_ma5  6876 non-null   object 
+#  
+#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_trigo_ma5   |
+#  |---:|:-------|:-------------------|-------:|---------:|:-------------------|
+#  |  0 | AFG    | Afganistán         |   1961 |    1.022 | NA                 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'iso3': 'geocodigo', 'rindes_trigo_ma5': 'valor'})
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   geocodigo         6876 non-null   object 
+#   1   iso3_desc_fundar  6876 non-null   object 
+#   2   anio              6876 non-null   int64  
+#   3   rindes            6876 non-null   float64
+#   4   valor             6876 non-null   object 
+#  
+#  |    | geocodigo   | iso3_desc_fundar   |   anio |   rindes | valor   |
+#  |---:|:------------|:-------------------|-------:|---------:|:--------|
+#  |  0 | AFG         | Afganistán         |   1961 |    1.022 | NA      |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='iso3_desc_fundar', axis=1)
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  6876 non-null   object 
+#   1   anio       6876 non-null   int64  
+#   2   rindes     6876 non-null   float64
+#   3   valor      6876 non-null   object 
+#  
+#  |    | geocodigo   |   anio |   rindes | valor   |
+#  |---:|:------------|-------:|---------:|:--------|
+#  |  0 | AFG         |   1961 |    1.022 | NA      |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='rindes', axis=1)
+#  RangeIndex: 6876 entries, 0 to 6875
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype 
+#  ---  ------     --------------  ----- 
+#   0   geocodigo  6876 non-null   object
+#   1   anio       6876 non-null   int64 
+#   2   valor      6876 non-null   object
+#  
+#  |    | geocodigo   |   anio | valor   |
+#  |---:|:------------|-------:|:--------|
+#  |  0 | AFG         |   1961 | NA      |
+#  
+#  ------------------------------
+#  
