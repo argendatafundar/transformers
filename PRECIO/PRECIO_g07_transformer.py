55be2c24c3a11b2802f633c39a83c8439332cd91
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 11:33:06 -0300

    Update PRECIO_g07_transformer.py

--- PRECIO/PRECIO_g07_transformer.py
+++ PRECIO/PRECIO_g07_transformer.py
@@ -4,67 +4,46 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'pais_o_grupo_de_paises': 'indicador'}),
-	rename_cols(map={'mediana_paises_inflacion': 'valor'})
+rename_columns(pais_o_grupo_de_paises='grupo', mediana_paises_inflacion='valor')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 62 entries, 0 to 61
+#  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
 #   #   Column                    Non-Null Count  Dtype  
 #  ---  ------                    --------------  -----  
-#   0   pais_o_grupo_de_paises    62 non-null     object 
-#   1   anio                      62 non-null     int64  
-#   2   mediana_paises_inflacion  62 non-null     float64
+#   0   anio                      64 non-null     int64  
+#   1   mediana_paises_inflacion  64 non-null     float64
+#   2   pais_o_grupo_de_paises    64 non-null     object 
 #  
-#  |    | pais_o_grupo_de_paises   |   anio |   mediana_paises_inflacion |
-#  |---:|:-------------------------|-------:|---------------------------:|
-#  |  0 | Argentina                |   1992 |                      17.55 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'pais_o_grupo_de_paises': 'indicador'})
-#  RangeIndex: 62 entries, 0 to 61
-#  Data columns (total 3 columns):
-#   #   Column                    Non-Null Count  Dtype  
-#  ---  ------                    --------------  -----  
-#   0   indicador                 62 non-null     object 
-#   1   anio                      62 non-null     int64  
-#   2   mediana_paises_inflacion  62 non-null     float64
-#  
-#  |    | indicador   |   anio |   mediana_paises_inflacion |
-#  |---:|:------------|-------:|---------------------------:|
-#  |  0 | Argentina   |   1992 |                      17.55 |
+#  |    |   anio |   mediana_paises_inflacion | pais_o_grupo_de_paises   |
+#  |---:|-------:|---------------------------:|:-------------------------|
+#  |  0 |   1992 |                    17.5459 | Argentina                |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'mediana_paises_inflacion': 'valor'})
-#  RangeIndex: 62 entries, 0 to 61
+#  rename_columns(pais_o_grupo_de_paises='grupo', mediana_paises_inflacion='valor')
+#  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   indicador  62 non-null     object 
-#   1   anio       62 non-null     int64  
-#   2   valor      62 non-null     float64
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    64 non-null     int64  
+#   1   valor   64 non-null     float64
+#   2   grupo   64 non-null     object 
 #  
-#  |    | indicador   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | Argentina   |   1992 |   17.55 |
+#  |    |   anio |   valor | grupo     |
+#  |---:|-------:|--------:|:----------|
+#  |  0 |   1992 | 17.5459 | Argentina |
 #  
 #  ------------------------------
 #  
