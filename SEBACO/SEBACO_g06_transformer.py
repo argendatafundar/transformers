7d6efae052e3bbac1676c97f2b2eefb4d3ac6281
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 14:59:55 -0300

    Update SEBACO_g06_transformer.py

--- SEBACO/SEBACO_g06_transformer.py
+++ SEBACO/SEBACO_g06_transformer.py
@@ -4,21 +4,26 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'provincia': 'indicador'}),
-	rename_cols(map={'prop': 'valor'})
+query(condition='anio == anio.max()'),
+	drop_col(col='anio', axis=1),
+	rename_columns(prop='valor', provincia='categoria')
 )
 #  PIPELINE_END
 
@@ -38,33 +43,46 @@ rename_cols(map={'provincia': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'provincia': 'indicador'})
-#  RangeIndex: 675 entries, 0 to 674
+#  query(condition='anio == anio.max()')
+#  Index: 25 entries, 26 to 674
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  675 non-null    object 
-#   1   anio       675 non-null    int64  
-#   2   prop       675 non-null    float64
+#   0   provincia  25 non-null     object 
+#   1   anio       25 non-null     int64  
+#   2   prop       25 non-null     float64
 #  
-#  |    | indicador   |   anio |     prop |
+#  |    | provincia   |   anio |     prop |
 #  |---:|:------------|-------:|---------:|
-#  |  0 | CABA        |   1996 | 0.410583 |
+#  | 26 | CABA        |   2022 | 0.581325 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'prop': 'valor'})
-#  RangeIndex: 675 entries, 0 to 674
-#  Data columns (total 3 columns):
+#  drop_col(col='anio', axis=1)
+#  Index: 25 entries, 26 to 674
+#  Data columns (total 2 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  675 non-null    object 
-#   1   anio       675 non-null    int64  
-#   2   valor      675 non-null    float64
+#   0   provincia  25 non-null     object 
+#   1   prop       25 non-null     float64
 #  
-#  |    | indicador   |   anio |    valor |
-#  |---:|:------------|-------:|---------:|
-#  |  0 | CABA        |   1996 | 0.410583 |
+#  |    | provincia   |     prop |
+#  |---:|:------------|---------:|
+#  | 26 | CABA        | 0.581325 |
+#  
+#  ------------------------------
+#  
+#  rename_columns(prop='valor', provincia='categoria')
+#  Index: 25 entries, 26 to 674
+#  Data columns (total 2 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  25 non-null     object 
+#   1   valor      25 non-null     float64
+#  
+#  |    | categoria   |    valor |
+#  |---:|:------------|---------:|
+#  | 26 | CABA        | 0.581325 |
 #  
 #  ------------------------------
 #  
