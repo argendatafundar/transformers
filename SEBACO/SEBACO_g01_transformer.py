52a11c6b97eba5a5bbce730189b47681331d279f
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 13:54:44 -0300

    Update SEBACO_g01_transformer.py

--- SEBACO/SEBACO_g01_transformer.py
+++ SEBACO/SEBACO_g01_transformer.py
@@ -4,21 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sector': 'indicador'}),
-	rename_cols(map={'share_ventas': 'valor'})
+rename_columns(sector='grupo', share_ventas='valor')
 )
 #  PIPELINE_END
 
@@ -38,33 +32,18 @@ rename_cols(map={'sector': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sector': 'indicador'})
+#  rename_columns(sector='grupo', share_ventas='valor')
 #  RangeIndex: 8 entries, 0 to 7
 #  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          8 non-null      int64  
-#   1   indicador     8 non-null      object 
-#   2   share_ventas  8 non-null      float64
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    8 non-null      int64  
+#   1   grupo   8 non-null      object 
+#   2   valor   8 non-null      float64
 #  
-#  |    |   anio | indicador   |   share_ventas |
-#  |---:|-------:|:------------|---------------:|
-#  |  0 |   2014 | SBC         |      0.0259628 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'share_ventas': 'valor'})
-#  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       8 non-null      int64  
-#   1   indicador  8 non-null      object 
-#   2   valor      8 non-null      float64
-#  
-#  |    |   anio | indicador   |     valor |
-#  |---:|-------:|:------------|----------:|
-#  |  0 |   2014 | SBC         | 0.0259628 |
+#  |    |   anio | grupo   |     valor |
+#  |---:|-------:|:--------|----------:|
+#  |  0 |   2014 | SBC     | 0.0259628 |
 #  
 #  ------------------------------
 #  
