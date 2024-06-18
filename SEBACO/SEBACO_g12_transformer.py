237cf774cc9802339bf03737345b481d0a60239b
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 15:30:28 -0300

    Update SEBACO_g12_transformer.py

--- SEBACO/SEBACO_g12_transformer.py
+++ SEBACO/SEBACO_g12_transformer.py
@@ -4,21 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sector_sbc': 'indicador'}),
-	rename_cols(map={'salario_promedio': 'valor'})
+rename_columns(sector_sbc='categoria', salario_promedio='valor')
 )
 #  PIPELINE_END
 
@@ -34,37 +28,22 @@ rename_cols(map={'sector_sbc': 'indicador'}),
 #  
 #  |    |   anio | sector_sbc                 |   salario_promedio |
 #  |---:|-------:|:---------------------------|-------------------:|
-#  |  0 |   1996 | Investigación y desarrollo |             540329 |
+#  |  0 |   1996 | InvestigaciÃ³n y desarrollo |             540329 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sector_sbc': 'indicador'})
-#  RangeIndex: 216 entries, 0 to 215
-#  Data columns (total 3 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   anio              216 non-null    int64  
-#   1   indicador         216 non-null    object 
-#   2   salario_promedio  216 non-null    float64
-#  
-#  |    |   anio | indicador                  |   salario_promedio |
-#  |---:|-------:|:---------------------------|-------------------:|
-#  |  0 |   1996 | Investigación y desarrollo |             540329 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'salario_promedio': 'valor'})
+#  rename_columns(sector_sbc='categoria', salario_promedio='valor')
 #  RangeIndex: 216 entries, 0 to 215
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       216 non-null    int64  
-#   1   indicador  216 non-null    object 
+#   1   categoria  216 non-null    object 
 #   2   valor      216 non-null    float64
 #  
-#  |    |   anio | indicador                  |   valor |
+#  |    |   anio | categoria                  |   valor |
 #  |---:|-------:|:---------------------------|--------:|
-#  |  0 |   1996 | Investigación y desarrollo |  540329 |
+#  |  0 |   1996 | InvestigaciÃ³n y desarrollo |  540329 |
 #  
 #  ------------------------------
 #  
