1bcce21bbcab643e8a31ae5d716e213740fd9600
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 15:33:52 -0300

    Update SEBACO_g14_transformer.py

--- SEBACO/SEBACO_g14_transformer.py
+++ SEBACO/SEBACO_g14_transformer.py
@@ -4,21 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sector': 'indicador'}),
-	rename_cols(map={'prop_mujeres': 'valor'})
+rename_columns(nivel='categoria', sector='indicador', prop_mujeres='valor')
 )
 #  PIPELINE_END
 
@@ -38,22 +32,7 @@ rename_cols(map={'sector': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sector': 'indicador'})
-#  RangeIndex: 34 entries, 0 to 33
-#  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          34 non-null     int64  
-#   1   indicador     34 non-null     object 
-#   2   prop_mujeres  34 non-null     float64
-#  
-#  |    |   anio | indicador   |   prop_mujeres |
-#  |---:|-------:|:------------|---------------:|
-#  |  0 |   2007 | SBC         |       0.384661 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'prop_mujeres': 'valor'})
+#  rename_columns(nivel='categoria', sector='indicador', prop_mujeres='valor')
 #  RangeIndex: 34 entries, 0 to 33
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
