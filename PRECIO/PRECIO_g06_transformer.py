4432b2d8c9dae69ad293c35009c5d6f4c92cf673
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 11:32:11 -0300

    Update PRECIO_g06_transformer.py

--- PRECIO/PRECIO_g06_transformer.py
+++ PRECIO/PRECIO_g06_transformer.py
@@ -4,67 +4,46 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_columns(df: DataFrame, **kwargs):
+    df = df.rename(columns=kwargs)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'grupo_de_paises': 'indicador'}),
-	rename_cols(map={'mediana_paises_inflacion': 'valor'})
+rename_columns(grupo_de_paises='grupo', mediana_paises_inflacion='valor')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 86 entries, 0 to 85
+#  RangeIndex: 88 entries, 0 to 87
 #  Data columns (total 3 columns):
 #   #   Column                    Non-Null Count  Dtype  
 #  ---  ------                    --------------  -----  
-#   0   grupo_de_paises           86 non-null     object 
-#   1   anio                      86 non-null     int64  
-#   2   mediana_paises_inflacion  86 non-null     float64
+#   0   grupo_de_paises           88 non-null     object 
+#   1   anio                      88 non-null     int64  
+#   2   mediana_paises_inflacion  88 non-null     float64
 #  
 #  |    | grupo_de_paises         |   anio |   mediana_paises_inflacion |
 #  |---:|:------------------------|-------:|---------------------------:|
-#  |  0 | América Latina y Caribe |   1980 |                     21.206 |
+#  |  0 | AmÃ©rica Latina y Caribe |   1980 |                     21.206 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'grupo_de_paises': 'indicador'})
-#  RangeIndex: 86 entries, 0 to 85
+#  rename_columns(grupo_de_paises='grupo', mediana_paises_inflacion='valor')
+#  RangeIndex: 88 entries, 0 to 87
 #  Data columns (total 3 columns):
-#   #   Column                    Non-Null Count  Dtype  
-#  ---  ------                    --------------  -----  
-#   0   indicador                 86 non-null     object 
-#   1   anio                      86 non-null     int64  
-#   2   mediana_paises_inflacion  86 non-null     float64
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   grupo   88 non-null     object 
+#   1   anio    88 non-null     int64  
+#   2   valor   88 non-null     float64
 #  
-#  |    | indicador               |   anio |   mediana_paises_inflacion |
-#  |---:|:------------------------|-------:|---------------------------:|
-#  |  0 | América Latina y Caribe |   1980 |                     21.206 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'mediana_paises_inflacion': 'valor'})
-#  RangeIndex: 86 entries, 0 to 85
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   indicador  86 non-null     object 
-#   1   anio       86 non-null     int64  
-#   2   valor      86 non-null     float64
-#  
-#  |    | indicador               |   anio |   valor |
+#  |    | grupo                   |   anio |   valor |
 #  |---:|:------------------------|-------:|--------:|
-#  |  0 | América Latina y Caribe |   1980 |  21.206 |
+#  |  0 | AmÃ©rica Latina y Caribe |   1980 |  21.206 |
 #  
 #  ------------------------------
 #  
