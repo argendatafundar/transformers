d893302841cbc74958eeae1f07c231f8ab5f6eaf
Juan Gabriel Juara <60323775+jgjuara@users.noreply.github.com>
2024-06-18 14:53:30 -0300

    Update SEBACO_g01_transformer.py

--- SEBACO/SEBACO_g01_transformer.py
+++ SEBACO/SEBACO_g01_transformer.py
@@ -4,6 +4,10 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
 def rename_columns(df: DataFrame, **kwargs):
     df = df.rename(columns=kwargs)
     return df
@@ -12,7 +16,8 @@ def rename_columns(df: DataFrame, **kwargs):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(sector='grupo', share_ventas='valor')
+drop_col(col='sector', axis=1),
+	rename_columns(share_ventas='valor')
 )
 #  PIPELINE_END
 
@@ -32,18 +37,31 @@ rename_columns(sector='grupo', share_ventas='valor')
 #  
 #  ------------------------------
 #  
-#  rename_columns(sector='grupo', share_ventas='valor')
+#  drop_col(col='sector', axis=1)
 #  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
+#  Data columns (total 2 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   anio          8 non-null      int64  
+#   1   share_ventas  8 non-null      float64
+#  
+#  |    |   anio |   share_ventas |
+#  |---:|-------:|---------------:|
+#  |  0 |   2014 |      0.0259628 |
+#  
+#  ------------------------------
+#  
+#  rename_columns(share_ventas='valor')
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
 #   0   anio    8 non-null      int64  
-#   1   grupo   8 non-null      object 
-#   2   valor   8 non-null      float64
+#   1   valor   8 non-null      float64
 #  
-#  |    |   anio | grupo   |     valor |
-#  |---:|-------:|:--------|----------:|
-#  |  0 |   2014 | SBC     | 0.0259628 |
+#  |    |   anio |     valor |
+#  |---:|-------:|----------:|
+#  |  0 |   2014 | 0.0259628 |
 #  
 #  ------------------------------
 #  
