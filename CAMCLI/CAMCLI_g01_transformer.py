e93d0d74c25f623d1f9ae1f7af4fca35fbf71ad8
joangq <jgquiroga@dc.uba.ar>
2024-06-19 14:12:51 -0300

    Update CAMCLI_g01_transformer.py

--- CAMCLI/CAMCLI_g01_transformer.py
+++ CAMCLI/CAMCLI_g01_transformer.py
@@ -4,6 +4,10 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -12,7 +16,8 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geoselector', 'continente_fundar': 'subgeoselector', 'iso3_desc_fundar': 'subsubgeoselector', 'valor_en_porcent': 'valor'})
+drop_col(col='iso3', axis=1),
+	rename_cols(map={'continente_fundar': 'categoria', 'iso3_desc_fundar': 'subcategoria', 'valor_en_porcent': 'valor'})
 )
 #  PIPELINE_END
 
@@ -30,24 +35,39 @@ rename_cols(map={'iso3': 'geoselector', 'continente_fundar': 'subgeoselector', '
 #  
 #  |    | iso3   | continente_fundar   | iso3_desc_fundar   |   anio |   valor_en_porcent |
 #  |---:|:-------|:--------------------|:-------------------|-------:|-------------------:|
-#  |  0 | AFG    | Asia                | Afganist·n         |   2021 |        0.000319854 |
+#  |  0 | AFG    | Asia                | Afganist√°n         |   2021 |        0.000319854 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geoselector', 'continente_fundar': 'subgeoselector', 'iso3_desc_fundar': 'subsubgeoselector', 'valor_en_porcent': 'valor'})
+#  drop_col(col='iso3', axis=1)
 #  RangeIndex: 219 entries, 0 to 218
-#  Data columns (total 5 columns):
+#  Data columns (total 4 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   geoselector        219 non-null    object 
-#   1   subgeoselector     219 non-null    object 
-#   2   subsubgeoselector  219 non-null    object 
-#   3   anio               219 non-null    int64  
-#   4   valor              219 non-null    float64
+#   0   continente_fundar  219 non-null    object 
+#   1   iso3_desc_fundar   219 non-null    object 
+#   2   anio               219 non-null    int64  
+#   3   valor_en_porcent   219 non-null    float64
 #  
-#  |    | geoselector   | subgeoselector   | subsubgeoselector   |   anio |       valor |
-#  |---:|:--------------|:-----------------|:--------------------|-------:|------------:|
-#  |  0 | AFG           | Asia             | Afganist·n          |   2021 | 0.000319854 |
+#  |    | continente_fundar   | iso3_desc_fundar   |   anio |   valor_en_porcent |
+#  |---:|:--------------------|:-------------------|-------:|-------------------:|
+#  |  0 | Asia                | Afganist√°n         |   2021 |        0.000319854 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'continente_fundar': 'categoria', 'iso3_desc_fundar': 'subcategoria', 'valor_en_porcent': 'valor'})
+#  RangeIndex: 219 entries, 0 to 218
+#  Data columns (total 4 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   categoria     219 non-null    object 
+#   1   subcategoria  219 non-null    object 
+#   2   anio          219 non-null    int64  
+#   3   valor         219 non-null    float64
+#  
+#  |    | categoria   | subcategoria   |   anio |       valor |
+#  |---:|:------------|:---------------|-------:|------------:|
+#  |  0 | Asia        | Afganist√°n     |   2021 | 0.000319854 |
 #  
 #  ------------------------------
 #  
