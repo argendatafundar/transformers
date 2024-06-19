a935438a5d05ae993101ee8941e7d09ea9249015
jgjuara <jg.juara@gmail.com>
2024-06-19 11:22:41 -0300

    Update TRANEN_g01_transformer.py

--- TRANEN/TRANEN_g01_transformer.py
+++ TRANEN/TRANEN_g01_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -12,7 +17,8 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'tipo_energia': 'indicador', 'valor_en_twh': 'valor'})
+query(condition='tipo_energia != "Total"'),
+	rename_cols(map={'tipo_energia': 'indicador', 'valor_en_twh': 'valor'})
 )
 #  PIPELINE_END
 
@@ -33,15 +39,31 @@ rename_cols(map={'tipo_energia': 'indicador', 'valor_en_twh': 'valor'})
 #  
 #  ------------------------------
 #  
+#  query(condition='tipo_energia != "Total"')
+#  Index: 750 entries, 0 to 749
+#  Data columns (total 4 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   anio          750 non-null    int64  
+#   1   tipo_energia  750 non-null    object 
+#   2   valor_en_twh  750 non-null    float64
+#   3   porcentaje    750 non-null    float64
+#  
+#  |    |   anio | tipo_energia     |   valor_en_twh |   porcentaje |
+#  |---:|-------:|:-----------------|---------------:|-------------:|
+#  |  0 |   1800 | Otras renovables |              0 |            0 |
+#  
+#  ------------------------------
+#  
 #  rename_cols(map={'tipo_energia': 'indicador', 'valor_en_twh': 'valor'})
-#  RangeIndex: 825 entries, 0 to 824
+#  Index: 750 entries, 0 to 749
 #  Data columns (total 4 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
-#   0   anio        825 non-null    int64  
-#   1   indicador   825 non-null    object 
-#   2   valor       825 non-null    float64
-#   3   porcentaje  825 non-null    float64
+#   0   anio        750 non-null    int64  
+#   1   indicador   750 non-null    object 
+#   2   valor       750 non-null    float64
+#   3   porcentaje  750 non-null    float64
 #  
 #  |    |   anio | indicador        |   valor |   porcentaje |
 #  |---:|-------:|:-----------------|--------:|-------------:|
