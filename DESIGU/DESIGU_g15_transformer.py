1cf1daec28d63e9e28406b7e9980cd01f00b0646
jgjuara <jg.juara@gmail.com>
2024-06-19 15:14:47 -0300

    Update DESIGU_g15_transformer.py

--- DESIGU/DESIGU_g15_transformer.py
+++ DESIGU/DESIGU_g15_transformer.py
@@ -9,16 +9,22 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'ano': 'anio', 'variable': 'indicador'}),
-	query(condition="indicador == 'participacion'")
+rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
+	replace_value(col='categoria', curr_value='linea', new_value='Promedio'),
+	replace_value(col='categoria', curr_value='participacion', new_value='Participación')
 )
 #  PIPELINE_END
 
@@ -38,33 +44,48 @@ rename_cols(map={'ano': 'anio', 'variable': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
+#  rename_cols(map={'ano': 'anio', 'variable': 'categoria'})
 #  RangeIndex: 172 entries, 0 to 171
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       172 non-null    int64  
-#   1   indicador  172 non-null    object 
+#   1   categoria  172 non-null    object 
 #   2   valor      172 non-null    float64
 #  
-#  |    |   anio | indicador     |   valor |
+#  |    |   anio | categoria     |   valor |
 #  |---:|-------:|:--------------|--------:|
 #  |  0 |   1935 | participacion |    35.5 |
 #  
 #  ------------------------------
 #  
-#  query(condition="indicador == 'participacion'")
-#  Index: 86 entries, 0 to 170
+#  replace_value(col='categoria', curr_value='linea', new_value='Promedio')
+#  RangeIndex: 172 entries, 0 to 171
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       86 non-null     int64  
-#   1   indicador  86 non-null     object 
-#   2   valor      86 non-null     float64
+#   0   anio       172 non-null    int64  
+#   1   categoria  172 non-null    object 
+#   2   valor      172 non-null    float64
 #  
-#  |    |   anio | indicador     |   valor |
+#  |    |   anio | categoria     |   valor |
 #  |---:|-------:|:--------------|--------:|
 #  |  0 |   1935 | participacion |    35.5 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='participacion', new_value='Participación')
+#  RangeIndex: 172 entries, 0 to 171
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       172 non-null    int64  
+#   1   categoria  172 non-null    object 
+#   2   valor      172 non-null    float64
+#  
+#  |    |   anio | categoria     |   valor |
+#  |---:|-------:|:--------------|--------:|
+#  |  0 |   1935 | Participación |    35.5 |
 #  
 #  ------------------------------
 #  
