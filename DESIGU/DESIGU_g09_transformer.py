7ce1a0843e752a9c533e296fb381fdc217ca0491
jgjuara <jg.juara@gmail.com>
2024-06-19 15:10:54 -0300

    Update DESIGU_g09_transformer.py

--- DESIGU/DESIGU_g09_transformer.py
+++ DESIGU/DESIGU_g09_transformer.py
@@ -7,12 +7,24 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
+rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
+	replace_value(col='categoria', curr_value='giniila', new_value='Gini del ingreso laboral'),
+	replace_value(col='categoria', curr_value='giniipcf', new_value='Gini del ingreso per cápita')
 )
 #  PIPELINE_END
 
@@ -32,18 +44,48 @@ rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
+#  rename_cols(map={'ano': 'anio', 'variable': 'categoria'})
 #  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       64 non-null     int64  
-#   1   indicador  64 non-null     object 
+#   1   categoria  64 non-null     object 
 #   2   valor      64 non-null     float64
 #  
-#  |    |   anio | indicador   |   valor |
+#  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
 #  |  0 |   1992 | giniipcf    |   44.78 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='giniila', new_value='Gini del ingreso laboral')
+#  RangeIndex: 64 entries, 0 to 63
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       64 non-null     int64  
+#   1   categoria  64 non-null     object 
+#   2   valor      64 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | giniipcf    |   44.78 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='giniipcf', new_value='Gini del ingreso per cápita')
+#  RangeIndex: 64 entries, 0 to 63
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       64 non-null     int64  
+#   1   categoria  64 non-null     object 
+#   2   valor      64 non-null     float64
+#  
+#  |    |   anio | categoria                   |   valor |
+#  |---:|-------:|:----------------------------|--------:|
+#  |  0 |   1992 | Gini del ingreso per cápita |   44.78 |
 #  
 #  ------------------------------
 #  
