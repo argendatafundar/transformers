8fb8f6ea14f1bfbf74dc90acb5d377cde23ff4b0
joangq <jgquiroga@dc.uba.ar>
2024-06-19 15:50:58 -0300

    Add COMEXT_g11_transformer.py

---
+++ COMEXT/COMEXT_g11_transformer.py
@@ -0,0 +1,139 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+query(condition='iso3 == "ARG"'),
+	drop_col(col='iso3', axis=1),
+	drop_col(col='location_name_short_en', axis=1),
+	drop_col(col='sitc_2_1_cod', axis=1),
+	rename_cols(map={'year': 'anio', 'sitc_product_name_es': 'indicador', 'import_value_pc': 'valor'})
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 122560 entries, 0 to 122559
+#  Data columns (total 6 columns):
+#   #   Column                  Non-Null Count   Dtype  
+#  ---  ------                  --------------   -----  
+#   0   year                    122560 non-null  int64  
+#   1   iso3                    122560 non-null  object 
+#   2   location_name_short_en  122560 non-null  object 
+#   3   sitc_2_1_cod            122560 non-null  int64  
+#   4   sitc_product_name_es    122560 non-null  object 
+#   5   import_value_pc         122360 non-null  float64
+#  
+#  |    |   year | iso3   | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|-------:|:-------|:-------------------------|---------------:|:-----------------------|------------------:|
+#  |  0 |   1962 | AFG    | Afghanistan              |              0 | Productos alimenticios |           7.95848 |
+#  
+#  ------------------------------
+#  
+#  query(condition='iso3 == "ARG"')
+#  Index: 600 entries, 50 to 120309
+#  Data columns (total 6 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   year                    600 non-null    int64  
+#   1   iso3                    600 non-null    object 
+#   2   location_name_short_en  600 non-null    object 
+#   3   sitc_2_1_cod            600 non-null    int64  
+#   4   sitc_product_name_es    600 non-null    object 
+#   5   import_value_pc         600 non-null    float64
+#  
+#  |    |   year | iso3   | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|-------:|:-------|:-------------------------|---------------:|:-----------------------|------------------:|
+#  | 50 |   1962 | ARG    | Argentina                |              0 | Productos alimenticios |           2.94598 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='iso3', axis=1)
+#  Index: 600 entries, 50 to 120309
+#  Data columns (total 5 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   year                    600 non-null    int64  
+#   1   location_name_short_en  600 non-null    object 
+#   2   sitc_2_1_cod            600 non-null    int64  
+#   3   sitc_product_name_es    600 non-null    object 
+#   4   import_value_pc         600 non-null    float64
+#  
+#  |    |   year | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|-------:|:-------------------------|---------------:|:-----------------------|------------------:|
+#  | 50 |   1962 | Argentina                |              0 | Productos alimenticios |           2.94598 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='location_name_short_en', axis=1)
+#  Index: 600 entries, 50 to 120309
+#  Data columns (total 4 columns):
+#   #   Column                Non-Null Count  Dtype  
+#  ---  ------                --------------  -----  
+#   0   year                  600 non-null    int64  
+#   1   sitc_2_1_cod          600 non-null    int64  
+#   2   sitc_product_name_es  600 non-null    object 
+#   3   import_value_pc       600 non-null    float64
+#  
+#  |    |   year |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|-------:|---------------:|:-----------------------|------------------:|
+#  | 50 |   1962 |              0 | Productos alimenticios |           2.94598 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='sitc_2_1_cod', axis=1)
+#  Index: 600 entries, 50 to 120309
+#  Data columns (total 3 columns):
+#   #   Column                Non-Null Count  Dtype  
+#  ---  ------                --------------  -----  
+#   0   year                  600 non-null    int64  
+#   1   sitc_product_name_es  600 non-null    object 
+#   2   import_value_pc       600 non-null    float64
+#  
+#  |    |   year | sitc_product_name_es   |   import_value_pc |
+#  |---:|-------:|:-----------------------|------------------:|
+#  | 50 |   1962 | Productos alimenticios |           2.94598 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'year': 'anio', 'sitc_product_name_es': 'indicador', 'import_value_pc': 'valor'})
+#  Index: 600 entries, 50 to 120309
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       600 non-null    int64  
+#   1   indicador  600 non-null    object 
+#   2   valor      600 non-null    float64
+#  
+#  |    |   anio | indicador              |   valor |
+#  |---:|-------:|:-----------------------|--------:|
+#  | 50 |   1962 | Productos alimenticios | 2.94598 |
+#  
+#  ------------------------------
+#  
