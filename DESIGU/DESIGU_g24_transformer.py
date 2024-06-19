1cb750008beec19a9fe7ec92bfeb5ed01f725fd7
jgjuara <jg.juara@gmail.com>
2024-06-19 16:22:44 -0300

    Update DESIGU_g24_transformer.py

--- DESIGU/DESIGU_g24_transformer.py
+++ DESIGU/DESIGU_g24_transformer.py
@@ -7,12 +7,30 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
+rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
+	replace_value(col='categoria', curr_value='agua', new_value='Agua'),
+	replace_value(col='categoria', curr_value='sanidad', new_value='Sanidad'),
+	replace_value(col='categoria', curr_value='cloacas', new_value='Cloacas')
 )
 #  PIPELINE_END
 
@@ -32,18 +50,63 @@ rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'ano': 'anio', 'variable': 'indicador'})
+#  rename_cols(map={'ano': 'anio', 'variable': 'categoria'})
 #  RangeIndex: 90 entries, 0 to 89
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   anio       90 non-null     int64  
-#   1   indicador  90 non-null     object 
+#   1   categoria  90 non-null     object 
 #   2   valor      90 non-null     float64
 #  
-#  |    |   anio | indicador   |   valor |
+#  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
 #  |  0 |   1992 | agua        |   10.07 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='agua', new_value='Agua')
+#  RangeIndex: 90 entries, 0 to 89
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       90 non-null     int64  
+#   1   categoria  90 non-null     object 
+#   2   valor      90 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Agua        |   10.07 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='sanidad', new_value='Sanidad')
+#  RangeIndex: 90 entries, 0 to 89
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       90 non-null     int64  
+#   1   categoria  90 non-null     object 
+#   2   valor      90 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Agua        |   10.07 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='cloacas', new_value='Cloacas')
+#  RangeIndex: 90 entries, 0 to 89
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       90 non-null     int64  
+#   1   categoria  90 non-null     object 
+#   2   valor      90 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Agua        |   10.07 |
 #  
 #  ------------------------------
 #  
