dff6afe43f83c2885bbb5850103adb4745ee8fd6
jgjuara <jg.juara@gmail.com>
2024-06-24 15:28:54 -0300

    Update DESHUM_g09_transformer.py

--- DESHUM/DESHUM_g09_transformer.py
+++ DESHUM/DESHUM_g09_transformer.py
@@ -24,13 +24,8 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     return df
 
 @transformer.convert
-def df_merge(df: DataFrame, df2: DataFrame,left:str, right:str, how:str='left'):
+def df_merge_geonomenclador(df: DataFrame, df2: DataFrame = geonomenclador, left:str = 'iso3', right:str = 'geocodigo', how:str='left'):
     df =  df.merge(df2, left_on = left, right_on = right, how = how)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
     return df
 
 @transformer.convert
@@ -38,12 +33,17 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
 def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
     return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
 
 @transformer.convert
-def apply_method(df: DataFrame, col:str, method):
-    df[col] = df[col].apply(method)
+def to_upper(df: DataFrame, col:str):
+    df[col] = df[col].str.upper()
     return df
 #  DEFINITIONS_END
 
@@ -54,24 +54,37 @@ query(condition="anio == anio.max() & iso3.isin(['NOR', 'ISL', 'DNK', 'USA', 'HU
 	replace_value(col='iso3', curr_value='ZZK.WORLD', new_value='WLD'),
 	replace_value(col='iso3', curr_value='ZZH.LAC', new_value='DESHUM_ZZH.LAC'),
 	replace_value(col='iso3', curr_value='ZZJ.SSA', new_value='DESHUM_ZZJ.SSA'),
-	df_merge(df2=           geocodigo                     name_long
-0                ABW                         Aruba
-1                AFE  África Oriental y Meridional
-2                AFG                    Afganistán
-3                AFW   África Occidental y Central
-4                AGO                        Angola
-...              ...                           ...
-1002  DESHUM_ZZF.EAP       Este de Asia y Pacífico
-1003  DESHUM_ZZG.ECA         Europa y Asia Central
-1004  DESHUM_ZZH.LAC    América Latina y el Caribe
-1005   DESHUM_ZZI.SA               Asia meridional
-1006  DESHUM_ZZJ.SSA           África Subsahariana
+	df_merge_geonomenclador(df2=           geocodigo                     name_long  \
+0                ABW                         Aruba   
+1                AFE  África Oriental y Meridional   
+2                AFG                    Afganistán   
+3                AFW   África Occidental y Central   
+4                AGO                        Angola   
+...              ...                           ...   
+1002  DESHUM_ZZF.EAP       Este de Asia y Pacífico   
+1003  DESHUM_ZZG.ECA         Europa y Asia Central   
+1004  DESHUM_ZZH.LAC    América Latina y el Caribe   
+1005   DESHUM_ZZI.SA               Asia meridional   
+1006  DESHUM_ZZJ.SSA           África Subsahariana   
 
-[1007 rows x 2 columns], left='iso3', right='geocodigo', how='left'),
+                        name_short iso_2  
+0                            Aruba    AW  
+1     África Oriental y Meridional   NaN  
+2                       Afganistán    AF  
+3      África Occidental y Central   NaN  
+4                           Angola    AO  
+...                            ...   ...  
+1002       Este de Asia y Pacífico   NaN  
+1003         Europa y Asia Central   NaN  
+1004    América Latina y el Caribe   NaN  
+1005               Asia meridional   NaN  
+1006           África Subsahariana   NaN  
+
+[1007 rows x 4 columns], left='iso3', right='geocodigo', how='left'),
+	drop_col(col=['geocodigo', 'iso3', 'country', 'name_short', 'iso_2', 'anio'], axis=1),
 	rename_cols(map={'name_long': 'categoria'}),
-	drop_col(col=['anio', 'country', 'iso3'], axis=1),
 	wide_to_long(primary_keys=['categoria'], value_name='valor', var_name='indicador'),
-	apply_method(col='indicador', method=<function <lambda> at 0x000001B18FF8AAC0>)
+	to_upper(col='indicador')
 )
 #  PIPELINE_END
 
@@ -161,81 +174,91 @@ query(condition="anio == anio.max() & iso3.isin(['NOR', 'ISL', 'DNK', 'USA', 'HU
 #  
 #  ------------------------------
 #  
-#  df_merge(df2=           geocodigo                     name_long
-#  0                ABW                         Aruba
-#  1                AFE  África Oriental y Meridional
-#  2                AFG                    Afganistán
-#  3                AFW   África Occidental y Central
-#  4                AGO                        Angola
-#  ...              ...                           ...
-#  1002  DESHUM_ZZF.EAP       Este de Asia y Pacífico
-#  1003  DESHUM_ZZG.ECA         Europa y Asia Central
-#  1004  DESHUM_ZZH.LAC    América Latina y el Caribe
-#  1005   DESHUM_ZZI.SA               Asia meridional
-#  1006  DESHUM_ZZJ.SSA           África Subsahariana
+#  df_merge_geonomenclador(df2=           geocodigo                     name_long  \
+#  0                ABW                         Aruba   
+#  1                AFE  África Oriental y Meridional   
+#  2                AFG                    Afganistán   
+#  3                AFW   África Occidental y Central   
+#  4                AGO                        Angola   
+#  ...              ...                           ...   
+#  1002  DESHUM_ZZF.EAP       Este de Asia y Pacífico   
+#  1003  DESHUM_ZZG.ECA         Europa y Asia Central   
+#  1004  DESHUM_ZZH.LAC    América Latina y el Caribe   
+#  1005   DESHUM_ZZI.SA               Asia meridional   
+#  1006  DESHUM_ZZJ.SSA           África Subsahariana   
 #  
-#  [1007 rows x 2 columns], left='iso3', right='geocodigo', how='left')
+#                          name_short iso_2  
+#  0                            Aruba    AW  
+#  1     África Oriental y Meridional   NaN  
+#  2                       Afganistán    AF  
+#  3      África Occidental y Central   NaN  
+#  4                           Angola    AO  
+#  ...                            ...   ...  
+#  1002       Este de Asia y Pacífico   NaN  
+#  1003         Europa y Asia Central   NaN  
+#  1004    América Latina y el Caribe   NaN  
+#  1005               Asia meridional   NaN  
+#  1006           África Subsahariana   NaN  
+#  
+#  [1007 rows x 4 columns], left='iso3', right='geocodigo', how='left')
 #  RangeIndex: 15 entries, 0 to 14
-#  Data columns (total 7 columns):
+#  Data columns (total 9 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   iso3        15 non-null     object 
+#   1   anio        15 non-null     int64  
+#   2   country     15 non-null     object 
+#   3   idh         15 non-null     float64
+#   4   idh_d       15 non-null     float64
+#   5   geocodigo   15 non-null     object 
+#   6   name_long   15 non-null     object 
+#   7   name_short  15 non-null     object 
+#   8   iso_2       12 non-null     object 
+#  
+#  |    | iso3   |   anio | country   |   idh |   idh_d | geocodigo   | name_long   | name_short   | iso_2   |
+#  |---:|:-------|-------:|:----------|------:|--------:|:------------|:------------|:-------------|:--------|
+#  |  0 | ARG    |   2022 | Argentina | 0.849 |   0.747 | ARG         | Argentina   | Argentina    | AR      |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['geocodigo', 'iso3', 'country', 'name_short', 'iso_2', 'anio'], axis=1)
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   iso3       15 non-null     object 
-#   1   anio       15 non-null     int64  
-#   2   country    15 non-null     object 
-#   3   idh        15 non-null     float64
-#   4   idh_d      15 non-null     float64
-#   5   geocodigo  15 non-null     object 
-#   6   name_long  15 non-null     object 
+#   0   idh        15 non-null     float64
+#   1   idh_d      15 non-null     float64
+#   2   name_long  15 non-null     object 
 #  
-#  |    | iso3   |   anio | country   |   idh |   idh_d | geocodigo   | name_long   |
-#  |---:|:-------|-------:|:----------|------:|--------:|:------------|:------------|
-#  |  0 | ARG    |   2022 | Argentina | 0.849 |   0.747 | ARG         | Argentina   |
+#  |    |   idh |   idh_d | name_long   |
+#  |---:|------:|--------:|:------------|
+#  |  0 | 0.849 |   0.747 | Argentina   |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'name_long': 'categoria'})
 #  RangeIndex: 15 entries, 0 to 14
-#  Data columns (total 7 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   iso3       15 non-null     object 
-#   1   anio       15 non-null     int64  
-#   2   country    15 non-null     object 
-#   3   idh        15 non-null     float64
-#   4   idh_d      15 non-null     float64
-#   5   geocodigo  15 non-null     object 
-#   6   categoria  15 non-null     object 
-#  
-#  |    | iso3   |   anio | country   |   idh |   idh_d | geocodigo   | categoria   |
-#  |---:|:-------|-------:|:----------|------:|--------:|:------------|:------------|
-#  |  0 | ARG    |   2022 | Argentina | 0.849 |   0.747 | ARG         | Argentina   |
-#  
-#  ------------------------------
-#  
-#  drop_col(col=['anio', 'country', 'iso3'], axis=1)
-#  RangeIndex: 15 entries, 0 to 14
-#  Data columns (total 4 columns):
+#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   idh        15 non-null     float64
 #   1   idh_d      15 non-null     float64
-#   2   geocodigo  15 non-null     object 
-#   3   categoria  15 non-null     object 
+#   2   categoria  15 non-null     object 
 #  
-#  |    |   idh |   idh_d | geocodigo   | categoria   |
-#  |---:|------:|--------:|:------------|:------------|
-#  |  0 | 0.849 |   0.747 | ARG         | Argentina   |
+#  |    |   idh |   idh_d | categoria   |
+#  |---:|------:|--------:|:------------|
+#  |  0 | 0.849 |   0.747 | Argentina   |
 #  
 #  ------------------------------
 #  
 #  wide_to_long(primary_keys=['categoria'], value_name='valor', var_name='indicador')
-#  RangeIndex: 45 entries, 0 to 44
+#  RangeIndex: 30 entries, 0 to 29
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   categoria  45 non-null     object
-#   1   indicador  45 non-null     object
-#   2   valor      45 non-null     object
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  30 non-null     object 
+#   1   indicador  30 non-null     object 
+#   2   valor      30 non-null     float64
 #  
 #  |    | categoria   | indicador   |   valor |
 #  |---:|:------------|:------------|--------:|
@@ -243,14 +266,14 @@ query(condition="anio == anio.max() & iso3.isin(['NOR', 'ISL', 'DNK', 'USA', 'HU
 #  
 #  ------------------------------
 #  
-#  apply_method(col='indicador', method=<function <lambda> at 0x000001B18FF8AAC0>)
-#  RangeIndex: 45 entries, 0 to 44
+#  to_upper(col='indicador')
+#  RangeIndex: 30 entries, 0 to 29
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   categoria  45 non-null     object
-#   1   indicador  45 non-null     object
-#   2   valor      45 non-null     object
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  30 non-null     object 
+#   1   indicador  30 non-null     object 
+#   2   valor      30 non-null     float64
 #  
 #  |    | categoria   | indicador   |   valor |
 #  |---:|:------------|:------------|--------:|
