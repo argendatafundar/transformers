6c695b244daaecd3e7f8b2bad0d26b84bd353686
jgjuara <jg.juara@gmail.com>
2024-06-24 15:33:15 -0300

    Update DESHUM_g12_transformer.py

--- DESHUM/DESHUM_g12_transformer.py
+++ DESHUM/DESHUM_g12_transformer.py
@@ -24,18 +24,18 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     return df
 
 @transformer.convert
-def df_merge(df: DataFrame, df2: DataFrame,left:str, right:str, how:str='left'):
-    df =  df.merge(df2, left_on = left, right_on = right, how = how)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def df_merge_geonomenclador(df: DataFrame, left:str = 'iso3', right:str = 'geocodigo', how:str='left'):
+    df =  df.merge(geonomenclador, left_on = left, right_on = right, how = how)
     return df
 
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
@@ -45,22 +45,9 @@ query(condition='anio == anio.max()'),
 	replace_value(col='iso3', curr_value='ZZK.WORLD', new_value='WLD'),
 	replace_value(col='iso3', curr_value='ZZH.LAC', new_value='DESHUM_ZZH.LAC'),
 	replace_value(col='iso3', curr_value='ZZJ.SSA', new_value='DESHUM_ZZJ.SSA'),
-	df_merge(df2=           geocodigo                     name_long
-0                ABW                         Aruba
-1                AFE  África Oriental y Meridional
-2                AFG                    Afganistán
-3                AFW   África Occidental y Central
-4                AGO                        Angola
-...              ...                           ...
-1002  DESHUM_ZZF.EAP       Este de Asia y Pacífico
-1003  DESHUM_ZZG.ECA         Europa y Asia Central
-1004  DESHUM_ZZH.LAC    América Latina y el Caribe
-1005   DESHUM_ZZI.SA               Asia meridional
-1006  DESHUM_ZZJ.SSA           África Subsahariana
-
-[1007 rows x 2 columns], left='iso3', right='geocodigo', how='left'),
-	rename_cols(map={'name_long': 'categoria', 'sexo': 'indicador', 'idh': 'valor'}),
-	drop_col(col=['iso3', 'country', 'geocodigo'], axis=1)
+	df_merge_geonomenclador(left='iso3', right='geocodigo', how='left'),
+	drop_col(col=['geocodigo', 'iso3', 'country', 'name_short', 'iso_2', 'anio'], axis=1),
+	rename_cols(map={'name_long': 'categoria', 'sexo': 'indicador', 'idh': 'valor'})
 )
 #  PIPELINE_END
 
@@ -150,70 +137,54 @@ query(condition='anio == anio.max()'),
 #  
 #  ------------------------------
 #  
-#  df_merge(df2=           geocodigo                     name_long
-#  0                ABW                         Aruba
-#  1                AFE  África Oriental y Meridional
-#  2                AFG                    Afganistán
-#  3                AFW   África Occidental y Central
-#  4                AGO                        Angola
-#  ...              ...                           ...
-#  1002  DESHUM_ZZF.EAP       Este de Asia y Pacífico
-#  1003  DESHUM_ZZG.ECA         Europa y Asia Central
-#  1004  DESHUM_ZZH.LAC    América Latina y el Caribe
-#  1005   DESHUM_ZZI.SA               Asia meridional
-#  1006  DESHUM_ZZJ.SSA           África Subsahariana
-#  
-#  [1007 rows x 2 columns], left='iso3', right='geocodigo', how='left')
+#  df_merge_geonomenclador(left='iso3', right='geocodigo', how='left')
 #  RangeIndex: 412 entries, 0 to 411
-#  Data columns (total 7 columns):
+#  Data columns (total 9 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   iso3        412 non-null    object 
+#   1   country     412 non-null    object 
+#   2   anio        412 non-null    int64  
+#   3   sexo        412 non-null    object 
+#   4   idh         386 non-null    float64
+#   5   geocodigo   396 non-null    object 
+#   6   name_long   396 non-null    object 
+#   7   name_short  396 non-null    object 
+#   8   iso_2       388 non-null    object 
+#  
+#  |    | iso3   | country     |   anio | sexo    |      idh | geocodigo   | name_long   | name_short   | iso_2   |
+#  |---:|:-------|:------------|-------:|:--------|---------:|:------------|:------------|:-------------|:--------|
+#  |  0 | AFG    | Afghanistan |   2022 | Varones | 0.534145 | AFG         | Afganistán  | Afganistán   | AF      |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['geocodigo', 'iso3', 'country', 'name_short', 'iso_2', 'anio'], axis=1)
+#  RangeIndex: 412 entries, 0 to 411
+#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   iso3       412 non-null    object 
-#   1   country    412 non-null    object 
-#   2   anio       412 non-null    int64  
-#   3   sexo       412 non-null    object 
-#   4   idh        386 non-null    float64
-#   5   geocodigo  396 non-null    object 
-#   6   name_long  396 non-null    object 
+#   0   sexo       412 non-null    object 
+#   1   idh        386 non-null    float64
+#   2   name_long  396 non-null    object 
 #  
-#  |    | iso3   | country     |   anio | sexo    |      idh | geocodigo   | name_long   |
-#  |---:|:-------|:------------|-------:|:--------|---------:|:------------|:------------|
-#  |  0 | AFG    | Afghanistan |   2022 | Varones | 0.534145 | AFG         | Afganistán  |
+#  |    | sexo    |      idh | name_long   |
+#  |---:|:--------|---------:|:------------|
+#  |  0 | Varones | 0.534145 | Afganistán  |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'name_long': 'categoria', 'sexo': 'indicador', 'idh': 'valor'})
 #  RangeIndex: 412 entries, 0 to 411
-#  Data columns (total 7 columns):
+#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   iso3       412 non-null    object 
-#   1   country    412 non-null    object 
-#   2   anio       412 non-null    int64  
-#   3   indicador  412 non-null    object 
-#   4   valor      386 non-null    float64
-#   5   geocodigo  396 non-null    object 
-#   6   categoria  396 non-null    object 
+#   0   indicador  412 non-null    object 
+#   1   valor      386 non-null    float64
+#   2   categoria  396 non-null    object 
 #  
-#  |    | iso3   | country     |   anio | indicador   |    valor | geocodigo   | categoria   |
-#  |---:|:-------|:------------|-------:|:------------|---------:|:------------|:------------|
-#  |  0 | AFG    | Afghanistan |   2022 | Varones     | 0.534145 | AFG         | Afganistán  |
-#  
-#  ------------------------------
-#  
-#  drop_col(col=['iso3', 'country', 'geocodigo'], axis=1)
-#  RangeIndex: 412 entries, 0 to 411
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       412 non-null    int64  
-#   1   indicador  412 non-null    object 
-#   2   valor      386 non-null    float64
-#   3   categoria  396 non-null    object 
-#  
-#  |    |   anio | indicador   |    valor | categoria   |
-#  |---:|-------:|:------------|---------:|:------------|
-#  |  0 |   2022 | Varones     | 0.534145 | Afganistán  |
+#  |    | indicador   |    valor | categoria   |
+#  |---:|:------------|---------:|:------------|
+#  |  0 | Varones     | 0.534145 | Afganistán  |
 #  
 #  ------------------------------
 #  
