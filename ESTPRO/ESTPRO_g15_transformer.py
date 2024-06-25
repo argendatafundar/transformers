7fb27760ea05716aa8fd786ced88d7eb9afe4907
jgjuara <jg.juara@gmail.com>
2024-06-25 16:12:19 -0300

    Add ESTPRO_g15_transformer.py

---
+++ ESTPRO/ESTPRO_g15_transformer.py
@@ -0,0 +1,112 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+query(condition='anio == 2019 & productividad_tipo == "Productividad por ocupado"'),
+	rename_cols(map={'iso3': 'geocodigo'}),
+	drop_col(col=['productividad_tipo', 'anio'], axis=1),
+	mutiplicar_por_escalar(col='valor', k=100)
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 25620 entries, 0 to 25619
+#  Data columns (total 4 columns):
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   iso3                25620 non-null  object 
+#   1   anio                25620 non-null  int64  
+#   2   productividad_tipo  25620 non-null  object 
+#   3   valor               13021 non-null  float64
+#  
+#  |    | iso3   |   anio | productividad_tipo     |   valor |
+#  |---:|:-------|-------:|:-----------------------|--------:|
+#  |  0 | ABW    |   1950 | Productividad por hora |     nan |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio == 2019 & productividad_tipo == "Productividad por ocupado"')
+#  Index: 183 entries, 139 to 25619
+#  Data columns (total 4 columns):
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   iso3                183 non-null    object 
+#   1   anio                183 non-null    int64  
+#   2   productividad_tipo  183 non-null    object 
+#   3   valor               177 non-null    float64
+#  
+#  |     | iso3   |   anio | productividad_tipo        |   valor |
+#  |----:|:-------|-------:|:--------------------------|--------:|
+#  | 139 | ABW    |   2019 | Productividad por ocupado | 64468.5 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'iso3': 'geocodigo'})
+#  Index: 183 entries, 139 to 25619
+#  Data columns (total 4 columns):
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   geocodigo           183 non-null    object 
+#   1   anio                183 non-null    int64  
+#   2   productividad_tipo  183 non-null    object 
+#   3   valor               177 non-null    float64
+#  
+#  |     | geocodigo   |   anio | productividad_tipo        |   valor |
+#  |----:|:------------|-------:|:--------------------------|--------:|
+#  | 139 | ABW         |   2019 | Productividad por ocupado | 64468.5 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['productividad_tipo', 'anio'], axis=1)
+#  Index: 183 entries, 139 to 25619
+#  Data columns (total 2 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  183 non-null    object 
+#   1   valor      177 non-null    float64
+#  
+#  |     | geocodigo   |       valor |
+#  |----:|:------------|------------:|
+#  | 139 | ABW         | 6.44685e+06 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  Index: 183 entries, 139 to 25619
+#  Data columns (total 2 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  183 non-null    object 
+#   1   valor      177 non-null    float64
+#  
+#  |     | geocodigo   |       valor |
+#  |----:|:------------|------------:|
+#  | 139 | ABW         | 6.44685e+06 |
+#  
+#  ------------------------------
+#  
