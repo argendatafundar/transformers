642cdbb74587c4f4e11f7d02406b7cc2d29154ce
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-06-26 10:15:57 -0300

    Update MERTRA_g15_transformer.py

--- MERTRA/MERTRA_g15_transformer.py
+++ MERTRA/MERTRA_g15_transformer.py
@@ -15,7 +15,8 @@ def round_to(df, col, n):
 
     def round_to(n):
         return lambda x: round_to_n_digits(x,n)
-    
+
+    df = df.copy()
     df[col] = df[col].map(round_to(n))
 
     return df
@@ -24,13 +25,24 @@ def round_to(df, col, n):
 def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 round_to(col='valor', n=2),
-	mutiplicar_por_escalar(col='valor', k=100)
+	mutiplicar_por_escalar(col='valor', k=100),
+	drop_col(col='region_wvs_code', axis=1),
+	rename_cols(map={'region_wvs': 'categoria', 'nivel_acuerdo': 'indicador'})
 )
 #  PIPELINE_END
 
@@ -80,6 +92,36 @@ round_to(col='valor', n=2),
 #  |    |   region_wvs_code | region_wvs                     | nivel_acuerdo   |   valor |
 #  |---:|------------------:|:-------------------------------|:----------------|--------:|
 #  |  0 |                 1 | Centro y Sur (exc. CABA y PBA) | De acuerdo      |      13 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='region_wvs_code', axis=1)
+#  RangeIndex: 12 entries, 0 to 11
+#  Data columns (total 3 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   region_wvs     12 non-null     object 
+#   1   nivel_acuerdo  12 non-null     object 
+#   2   valor          12 non-null     float64
+#  
+#  |    | region_wvs                     | nivel_acuerdo   |   valor |
+#  |---:|:-------------------------------|:----------------|--------:|
+#  |  0 | Centro y Sur (exc. CABA y PBA) | De acuerdo      |      13 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'region_wvs': 'categoria', 'nivel_acuerdo': 'indicador'})
+#  RangeIndex: 12 entries, 0 to 11
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  12 non-null     object 
+#   1   indicador  12 non-null     object 
+#   2   valor      12 non-null     float64
+#  
+#  |    | categoria                      | indicador   |   valor |
+#  |---:|:-------------------------------|:------------|--------:|
+#  |  0 | Centro y Sur (exc. CABA y PBA) | De acuerdo  |      13 |
 #  
 #  ------------------------------
 #  
