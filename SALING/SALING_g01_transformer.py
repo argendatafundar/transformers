426c93eaac549723a1f7d027acf4f0966a1030dc
jgjuara <jg.juara@gmail.com>
2024-06-26 19:28:52 -0300

    Add SALING_g01_transformer.py

---
+++ SALING/SALING_g01_transformer.py
@@ -0,0 +1,89 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def concatenar_columnas(df:DataFrame, cols:list, nueva_col:str, separtor:str = "-"):
+    df[nueva_col] = df[cols].astype(str).agg(separtor.join, axis=1)
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'),
+	drop_col(col=['year', 'semestre'], axis=1),
+	rename_cols(map={'ipcf_index': 'valor'})
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 40 entries, 0 to 39
+#  Data columns (total 3 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   year        40 non-null     int64  
+#   1   semestre    40 non-null     int64  
+#   2   ipcf_index  38 non-null     float64
+#  
+#  |    |   year |   semestre |   ipcf_index |
+#  |---:|-------:|-----------:|-------------:|
+#  |  0 |   2003 |          2 |          100 |
+#  
+#  ------------------------------
+#  
+#  concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-')
+#  RangeIndex: 40 entries, 0 to 39
+#  Data columns (total 4 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   year        40 non-null     int64  
+#   1   semestre    40 non-null     int64  
+#   2   ipcf_index  38 non-null     float64
+#   3   aniosem     40 non-null     object 
+#  
+#  |    |   year |   semestre |   ipcf_index | aniosem   |
+#  |---:|-------:|-----------:|-------------:|:----------|
+#  |  0 |   2003 |          2 |          100 | 2003-2    |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['year', 'semestre'], axis=1)
+#  RangeIndex: 40 entries, 0 to 39
+#  Data columns (total 2 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   ipcf_index  38 non-null     float64
+#   1   aniosem     40 non-null     object 
+#  
+#  |    |   ipcf_index | aniosem   |
+#  |---:|-------------:|:----------|
+#  |  0 |          100 | 2003-2    |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'ipcf_index': 'valor'})
+#  RangeIndex: 40 entries, 0 to 39
+#  Data columns (total 2 columns):
+#   #   Column   Non-Null Count  Dtype  
+#  ---  ------   --------------  -----  
+#   0   valor    38 non-null     float64
+#   1   aniosem  40 non-null     object 
+#  
+#  |    |   valor | aniosem   |
+#  |---:|--------:|:----------|
+#  |  0 |     100 | 2003-2    |
+#  
+#  ------------------------------
+#  
