bc9d2ede002ac8590ff3986762df5e508e53d2c0
jgjuara <jg.juara@gmail.com>
2024-06-26 23:56:02 -0300

    Update SALING_g23_transformer.py

--- SALING/SALING_g23_transformer.py
+++ SALING/SALING_g23_transformer.py
@@ -7,12 +7,30 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={' ano': 'anio', 'variable': 'indicador'})
+rename_cols(map={'variable': 'categoria', 'ano': 'anio'}),
+	replace_value(col='categoria', curr_value='(15-24)', new_value='15 a 24 años'),
+	replace_value(col='categoria', curr_value='(25-64)', new_value='25 a 64 años'),
+	replace_value(col='categoria', curr_value='(65-)', new_value='65 años y más')
 )
 #  PIPELINE_END
 
@@ -22,26 +40,71 @@ rename_cols(map={' ano': 'anio', 'variable': 'indicador'})
 #  Data columns (total 3 columns):
 #   #   Column    Non-Null Count  Dtype 
 #  ---  ------    --------------  ----- 
-#   0    ano      160 non-null    int64 
+#   0   ano       160 non-null    int64 
 #   1   variable  160 non-null    object
 #   2   valor     160 non-null    int64 
 #  
-#  |    |    ano | variable   |   valor |
-#  |---:|-------:|:-----------|--------:|
-#  |  0 |   1992 | Mujeres    |   11178 |
+#  |    |   ano | variable   |   valor |
+#  |---:|------:|:-----------|--------:|
+#  |  0 |  1992 | Mujeres    |   11178 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={' ano': 'anio', 'variable': 'indicador'})
+#  rename_cols(map={'variable': 'categoria', 'ano': 'anio'})
 #  RangeIndex: 160 entries, 0 to 159
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype 
 #  ---  ------     --------------  ----- 
 #   0   anio       160 non-null    int64 
-#   1   indicador  160 non-null    object
+#   1   categoria  160 non-null    object
 #   2   valor      160 non-null    int64 
 #  
-#  |    |   anio | indicador   |   valor |
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Mujeres     |   11178 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='(15-24)', new_value='15 a 24 años')
+#  RangeIndex: 160 entries, 0 to 159
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype 
+#  ---  ------     --------------  ----- 
+#   0   anio       160 non-null    int64 
+#   1   categoria  160 non-null    object
+#   2   valor      160 non-null    int64 
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Mujeres     |   11178 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='(25-64)', new_value='25 a 64 años')
+#  RangeIndex: 160 entries, 0 to 159
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype 
+#  ---  ------     --------------  ----- 
+#   0   anio       160 non-null    int64 
+#   1   categoria  160 non-null    object
+#   2   valor      160 non-null    int64 
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Mujeres     |   11178 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='(65-)', new_value='65 años y más')
+#  RangeIndex: 160 entries, 0 to 159
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype 
+#  ---  ------     --------------  ----- 
+#   0   anio       160 non-null    int64 
+#   1   categoria  160 non-null    object
+#   2   valor      160 non-null    int64 
+#  
+#  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
 #  |  0 |   1992 | Mujeres     |   11178 |
 #  
