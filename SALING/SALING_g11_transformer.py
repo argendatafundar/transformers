b3ca2ae953cc7a964c478a24b25ec292a07aab48
jgjuara <jg.juara@gmail.com>
2024-06-26 23:42:32 -0300

    Update SALING_g11_transformer.py

--- SALING/SALING_g11_transformer.py
+++ SALING/SALING_g11_transformer.py
@@ -22,70 +22,70 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'variable': 'indicador', '            ano': 'anio'}),
-	replace_value(col='indicador', curr_value='ingresohorrario', new_value='Ingreso laboral por hora'),
-	replace_value(col='indicador', curr_value='ingresomensual', new_value='Ingreso laboral por mes')
+rename_cols(map={'variable': 'categoria', 'ano': 'anio'}),
+	replace_value(col='categoria', curr_value='ingresohorrario', new_value='Ingreso horario'),
+	replace_value(col='categoria', curr_value='ingresomensual', new_value='Ingreso mensual')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 12 entries, 0 to 11
+#  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0               ano  12 non-null     object 
-#   1   variable         10 non-null     object 
-#   2   valor            10 non-null     float64
+#   #   Column    Non-Null Count  Dtype  
+#  ---  ------    --------------  -----  
+#   0   ano       64 non-null     int64  
+#   1   variable  64 non-null     object 
+#   2   valor     64 non-null     float64
 #  
-#  |    |             ano   | variable        |   valor |
-#  |---:|:------------------|:----------------|--------:|
-#  |  0 | 0     1992        | ingresohorrario |     100 |
+#  |    |   ano | variable        |   valor |
+#  |---:|------:|:----------------|--------:|
+#  |  0 |  1992 | ingresohorrario |     100 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'variable': 'indicador', '            ano': 'anio'})
-#  RangeIndex: 12 entries, 0 to 11
+#  rename_cols(map={'variable': 'categoria', 'ano': 'anio'})
+#  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       12 non-null     object 
-#   1   indicador  10 non-null     object 
-#   2   valor      10 non-null     float64
+#   0   anio       64 non-null     int64  
+#   1   categoria  64 non-null     object 
+#   2   valor      64 non-null     float64
 #  
-#  |    | anio       | indicador       |   valor |
-#  |---:|:-----------|:----------------|--------:|
-#  |  0 | 0     1992 | ingresohorrario |     100 |
+#  |    |   anio | categoria       |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1992 | ingresohorrario |     100 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='indicador', curr_value='ingresohorrario', new_value='Ingreso laboral por hora')
-#  RangeIndex: 12 entries, 0 to 11
+#  replace_value(col='categoria', curr_value='ingresohorrario', new_value='Ingreso horario')
+#  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       12 non-null     object 
-#   1   indicador  10 non-null     object 
-#   2   valor      10 non-null     float64
+#   0   anio       64 non-null     int64  
+#   1   categoria  64 non-null     object 
+#   2   valor      64 non-null     float64
 #  
-#  |    | anio       | indicador                |   valor |
-#  |---:|:-----------|:-------------------------|--------:|
-#  |  0 | 0     1992 | Ingreso laboral por hora |     100 |
+#  |    |   anio | categoria       |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1992 | Ingreso horario |     100 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='indicador', curr_value='ingresomensual', new_value='Ingreso laboral por mes')
-#  RangeIndex: 12 entries, 0 to 11
+#  replace_value(col='categoria', curr_value='ingresomensual', new_value='Ingreso mensual')
+#  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       12 non-null     object 
-#   1   indicador  10 non-null     object 
-#   2   valor      10 non-null     float64
+#   0   anio       64 non-null     int64  
+#   1   categoria  64 non-null     object 
+#   2   valor      64 non-null     float64
 #  
-#  |    |   anio | indicador                |   valor |
-#  |---:|-------:|:-------------------------|--------:|
-#  |  0 |   1992 | Ingreso laboral por hora |     100 |
+#  |    |   anio | categoria       |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1992 | Ingreso horario |     100 |
 #  
 #  ------------------------------
 #  
