f4430640fca83de277ecbe776dfbcff9f6fcd29a
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-01 15:38:01 -0300

    Update COMEXT_g04_transformer.py

--- COMEXT/COMEXT_g04_transformer.py
+++ COMEXT/COMEXT_g04_transformer.py
@@ -20,6 +20,13 @@ def drop_col(df: DataFrame, col, axis=1):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -28,7 +35,8 @@ pipeline = chain(
 query(condition="iso3 == 'ARG'"),
 	drop_col(col='iso3', axis=1),
 	drop_col(col='countryname', axis=1),
-	rename_cols(map={'exportsofgoodsandservicesofgdp': 'valor'})
+	rename_cols(map={'exportsofgoodsandservicesofgdp': 'valor'}),
+	sort_values(how='ascending', by=['anio'])
 )
 #  PIPELINE_END
 
@@ -43,14 +51,14 @@ query(condition="iso3 == 'ARG'"),
 #   2   countryname                     16960 non-null  object 
 #   3   exportsofgoodsandservicesofgdp  10756 non-null  float64
 #  
-#  |    |   anio | iso3   | countryname                 |   exportsofgoodsandservicesofgdp |
-#  |---:|-------:|:-------|:----------------------------|---------------------------------:|
-#  |  0 |   2023 | AFE    | Africa Eastern and Southern |                              nan |
+#  |    |   anio | iso3   | countryname   |   exportsofgoodsandservicesofgdp |
+#  |---:|-------:|:-------|:--------------|---------------------------------:|
+#  |  0 |   2023 | ABW    | Aruba         |                              nan |
 #  
 #  ------------------------------
 #  
 #  query(condition="iso3 == 'ARG'")
-#  Index: 64 entries, 3520 to 3583
+#  Index: 64 entries, 576 to 639
 #  Data columns (total 4 columns):
 #   #   Column                          Non-Null Count  Dtype  
 #  ---  ------                          --------------  -----  
@@ -59,14 +67,14 @@ query(condition="iso3 == 'ARG'"),
 #   2   countryname                     64 non-null     object 
 #   3   exportsofgoodsandservicesofgdp  63 non-null     float64
 #  
-#  |      |   anio | iso3   | countryname   |   exportsofgoodsandservicesofgdp |
-#  |-----:|-------:|:-------|:--------------|---------------------------------:|
-#  | 3520 |   2023 | ARG    | Argentina     |                              nan |
+#  |     |   anio | iso3   | countryname   |   exportsofgoodsandservicesofgdp |
+#  |----:|-------:|:-------|:--------------|---------------------------------:|
+#  | 576 |   2023 | ARG    | Argentina     |                              nan |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='iso3', axis=1)
-#  Index: 64 entries, 3520 to 3583
+#  Index: 64 entries, 576 to 639
 #  Data columns (total 3 columns):
 #   #   Column                          Non-Null Count  Dtype  
 #  ---  ------                          --------------  -----  
@@ -74,37 +82,51 @@ query(condition="iso3 == 'ARG'"),
 #   1   countryname                     64 non-null     object 
 #   2   exportsofgoodsandservicesofgdp  63 non-null     float64
 #  
-#  |      |   anio | countryname   |   exportsofgoodsandservicesofgdp |
-#  |-----:|-------:|:--------------|---------------------------------:|
-#  | 3520 |   2023 | Argentina     |                              nan |
+#  |     |   anio | countryname   |   exportsofgoodsandservicesofgdp |
+#  |----:|-------:|:--------------|---------------------------------:|
+#  | 576 |   2023 | Argentina     |                              nan |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='countryname', axis=1)
-#  Index: 64 entries, 3520 to 3583
+#  Index: 64 entries, 576 to 639
 #  Data columns (total 2 columns):
 #   #   Column                          Non-Null Count  Dtype  
 #  ---  ------                          --------------  -----  
 #   0   anio                            64 non-null     int64  
 #   1   exportsofgoodsandservicesofgdp  63 non-null     float64
 #  
-#  |      |   anio |   exportsofgoodsandservicesofgdp |
-#  |-----:|-------:|---------------------------------:|
-#  | 3520 |   2023 |                              nan |
+#  |     |   anio |   exportsofgoodsandservicesofgdp |
+#  |----:|-------:|---------------------------------:|
+#  | 576 |   2023 |                              nan |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'exportsofgoodsandservicesofgdp': 'valor'})
-#  Index: 64 entries, 3520 to 3583
+#  Index: 64 entries, 576 to 639
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
 #   0   anio    64 non-null     int64  
 #   1   valor   63 non-null     float64
 #  
-#  |      |   anio |   valor |
-#  |-----:|-------:|--------:|
-#  | 3520 |   2023 |     nan |
+#  |     |   anio |   valor |
+#  |----:|-------:|--------:|
+#  | 576 |   2023 |     nan |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 64 entries, 0 to 63
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    64 non-null     int64  
+#   1   valor   63 non-null     float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   1960 | 7.60405 |
 #  
 #  ------------------------------
 #  
