b805d76c842cf0903c67dcaf23d0243bf4e36a77
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-01 18:43:57 -0300

    Update COMEXT_g07_transformer.py

--- COMEXT/COMEXT_g07_transformer.py
+++ COMEXT/COMEXT_g07_transformer.py
@@ -18,6 +18,11 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
+def drop_na(df: DataFrame, col:str):
+    df = df.dropna(subset= col, axis=0)
+    return df
+
+@transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
@@ -31,6 +36,7 @@ pipeline = chain(
 rename_cols(map={'iso3': 'geocodigo'}),
 	drop_col(col='countryname', axis=1),
 	rename_cols(map={'exportunitvalueindex_2000': 'valor'}),
+	drop_na(col='valor'),
 	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
@@ -98,18 +104,33 @@ rename_cols(map={'iso3': 'geocodigo'}),
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'geocodigo'])
-#  RangeIndex: 16960 entries, 0 to 16959
+#  drop_na(col='valor')
+#  Index: 6291 entries, 2 to 16959
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       16960 non-null  int64  
-#   1   geocodigo  16960 non-null  object 
+#   0   anio       6291 non-null   int64  
+#   1   geocodigo  6291 non-null   object 
 #   2   valor      6291 non-null   float64
 #  
 #  |    |   anio | geocodigo   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   1960 | ABW         |     nan |
+#  |  2 |   2021 | ABW         |  250.29 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  RangeIndex: 6291 entries, 0 to 6290
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       6291 non-null   int64  
+#   1   geocodigo  6291 non-null   object 
+#   2   valor      6291 non-null   float64
+#  
+#  |    |   anio | geocodigo   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1980 | ARE         |  111.73 |
 #  
 #  ------------------------------
 #  
