727cac8e9c0c2b7778ae8676eff5aa68f0ee4f59
Juan Manuel Dias <juamadias@gmail.com>
2024-07-02 15:31:08 -0300

    Update CAMCLI_g03_transformer.py

--- CAMCLI/CAMCLI_g03_transformer.py
+++ CAMCLI/CAMCLI_g03_transformer.py
@@ -4,66 +4,116 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def col_from_map(df, map: dict, source: str, target: str):
-    df[target] = df[source].map(map)
-    return df
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def join_geonomencladores(df: DataFrame, geonomenclador: DataFrame):
+    geonomenclador = geonomenclador[['geocodigo', 'iso_2']].rename(columns={'iso_2': 'iso'})
+    return df.merge(geonomenclador, on='geocodigo', how='left')
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-col_from_map(map={'ABW': 'AW', 'AFG': 'AF', 'AGO': 'AO', 'AIA': 'AI', 'ALA': 'AX', 'ALB': 'AL', 'AND': 'AD', 'ARE': 'AE', 'ARG': 'AR', 'ARM': 'AM', 'ASM': 'AS', 'ATA': 'AQ', 'ATF': 'TF', 'ATG': 'AG', 'AUS': 'AU', 'AUT': 'AT', 'AZE': 'AZ', 'BDI': 'BI', 'BEL': 'BE', 'BEN': 'BJ', 'BES': 'BQ', 'BFA': 'BF', 'BGD': 'BD', 'BGR': 'BG', 'BHR': 'BH', 'BHS': 'BS', 'BIH': 'BA', 'BLM': 'BL', 'BLR': 'BY', 'BLZ': 'BZ', 'BMU': 'BM', 'BOL': 'BO', 'BRA': 'BR', 'BRB': 'BB', 'BRN': 'BN', 'BTN': 'BT', 'BVT': 'BV', 'BWA': 'BW', 'CAF': 'CF', 'CAN': 'CA', 'CCK': 'CC', 'CHE': 'CH', 'CHL': 'CL', 'CHN': 'CN', 'CIV': 'CI', 'CMR': 'CM', 'COD': 'CD', 'COG': 'CG', 'COK': 'CK', 'COL': 'CO', 'COM': 'KM', 'CPV': 'CV', 'CRI': 'CR', 'CUB': 'CU', 'CUW': 'CW', 'CXR': 'CX', 'CYM': 'KY', 'CYP': 'CY', 'CZE': 'CZ', 'DEU': 'DE', 'DJI': 'DJ', 'DMA': 'DM', 'DNK': 'DK', 'DOM': 'DO', 'DZA': 'DZ', 'ECU': 'EC', 'EGY': 'EG', 'ERI': 'ER', 'ESH': 'EH', 'ESP': 'ES', 'EST': 'EE', 'ETH': 'ET', 'FIN': 'FI', 'FJI': 'FJ', 'FLK': 'FK', 'FRA': 'FR', 'FRO': 'FO', 'FSM': 'FM', 'GAB': 'GA', 'GBR': 'GB', 'GEO': 'GE', 'GGY': 'GG', 'GHA': 'GH', 'GIB': 'GI', 'GIN': 'GN', 'GLP': 'GP', 'GMB': 'GM', 'GNB': 'GW', 'GNQ': 'GQ', 'GRC': 'GR', 'GRD': 'GD', 'GRL': 'GL', 'GTM': 'GT', 'GUF': 'GF', 'GUM': 'GU', 'GUY': 'GY', 'HKG': 'HK', 'HMD': 'HM', 'HND': 'HN', 'HRV': 'HR', 'HTI': 'HT', 'HUN': 'HU', 'IDN': 'ID', 'IMN': 'IM', 'IND': 'IN', 'IOT': 'IO', 'IRL': 'IE', 'IRN': 'IR', 'IRQ': 'IQ', 'ISL': 'IS', 'ISR': 'IL', 'ITA': 'IT', 'JAM': 'JM', 'JEY': 'JE', 'JOR': 'JO', 'JPN': 'JP', 'KAZ': 'KZ', 'KEN': 'KE', 'KGZ': 'KG', 'KHM': 'KH', 'KIR': 'KI', 'KNA': 'KN', 'KOR': 'KR', 'KWT': 'KW', 'LAO': 'LA', 'LBN': 'LB', 'LBR': 'LR', 'LBY': 'LY', 'LCA': 'LC', 'LIE': 'LI', 'LKA': 'LK', 'LSO': 'LS', 'LTU': 'LT', 'LUX': 'LU', 'LVA': 'LV', 'MAC': 'MO', 'MAF': 'MF', 'MAR': 'MA', 'MCO': 'MC', 'MDA': 'MD', 'MDG': 'MG', 'MDV': 'MV', 'MEX': 'MX', 'MHL': 'MH', 'MKD': 'MK', 'MLI': 'ML', 'MLT': 'MT', 'MMR': 'MM', 'MNE': 'ME', 'MNG': 'MN', 'MNP': 'MP', 'MOZ': 'MZ', 'MRT': 'MR', 'MSR': 'MS', 'MTQ': 'MQ', 'MUS': 'MU', 'MWI': 'MW', 'MYS': 'MY', 'MYT': 'YT', 'NCL': 'NC', 'NER': 'NE', 'NFK': 'NF', 'NGA': 'NG', 'NIC': 'NI', 'NIU': 'NU', 'NLD': 'NL', 'NOR': 'NO', 'NPL': 'NP', 'NRU': 'NR', 'NZL': 'NZ', 'OMN': 'OM', 'PAK': 'PK', 'PAN': 'PA', 'PCN': 'PN', 'PER': 'PE', 'PHL': 'PH', 'PLW': 'PW', 'PNG': 'PG', 'POL': 'PL', 'PRI': 'PR', 'PRK': 'KP', 'PRT': 'PT', 'PRY': 'PY', 'PSE': 'PS', 'PYF': 'PF', 'QAT': 'QA', 'REU': 'RE', 'ROU': 'RO', 'RUS': 'RU', 'RWA': 'RW', 'SAU': 'SA', 'SDN': 'SD', 'SEN': 'SN', 'SGP': 'SG', 'SGS': 'GS', 'SHN': 'SH', 'SJM': 'SJ', 'SLB': 'SB', 'SLE': 'SL', 'SLV': 'SV', 'SMR': 'SM', 'SOM': 'SO', 'SPM': 'PM', 'SRB': 'RS', 'SSD': 'SS', 'STP': 'ST', 'SUR': 'SR', 'SVK': 'SK', 'SVN': 'SI', 'SWE': 'SE', 'SWZ': 'SZ', 'SXM': 'SX', 'SYC': 'SC', 'SYR': 'SY', 'TCA': 'TC', 'TCD': 'TD', 'TGO': 'TG', 'THA': 'TH', 'TJK': 'TJ', 'TKL': 'TK', 'TKM': 'TM', 'TLS': 'TL', 'TON': 'TO', 'TTO': 'TT', 'TUN': 'TN', 'TUR': 'TR', 'TUV': 'TV', 'TWN': 'TW', 'TZA': 'TZ', 'UGA': 'UG', 'UKR': 'UA', 'UMI': 'UM', 'URY': 'UY', 'USA': 'US', 'UZB': 'UZ', 'VAT': 'VA', 'VCT': 'VC', 'VEN': 'VE', 'VGB': 'VG', 'VIR': 'VI', 'VNM': 'VN', 'VUT': 'VU', 'WLF': 'WF', 'WSM': 'WS', 'YEM': 'YE', 'ZAF': 'ZA', 'ZMB': 'ZM', 'ZWE': 'ZW'}, source='iso3', target='iso2'),
-	rename_cols(map={'iso3': 'geocodigo', 'iso2': 'iso', 'valor_en_ton': 'valor'})
+rename_cols(map={'iso3': 'geocodigo', 'valor_en_ton': 'valor'}),
+	join_geonomencladores(geonomenclador=            geocodigo                                  name_long  \
+0                 ABW                                      Aruba   
+1                 AFE               África Oriental y Meridional   
+2                 AFG                                 Afganistán   
+3                 AFW                África Occidental y Central   
+4                 AGO                                     Angola   
+...               ...                                        ...   
+1015  DESHUM_AHDI.SAS                 Asia del Sur (AHDI) - PNUD   
+1016  DESHUM_AHDI.SSA          África Subsahariana (AHDI) - PNUD   
+1017  DESHUM_AHDI.WEU            Europa Occidental (AHDI) - PNUD   
+1018  DESHUM_AHDI.WLD                        Mundo (AHDI) - PNUD   
+1019  DESHUM_AHDI.WOF  Ramificaciones de Occidente (AHDI) - PNUD   
+
+                                     name_short iso_2  
+0                                         Aruba    AW  
+1                  África Oriental y Meridional   NaN  
+2                                    Afganistán    AF  
+3                   África Occidental y Central   NaN  
+4                                        Angola    AO  
+...                                         ...   ...  
+1015                 Asia del Sur (AHDI) - PNUD   NaN  
+1016          África Subsahariana (AHDI) - PNUD   NaN  
+1017            Europa Occidental (AHDI) - PNUD   NaN  
+1018                        Mundo (AHDI) - PNUD   NaN  
+1019  Ramificaciones de Occidente (AHDI) - PNUD   NaN  
+
+[1020 rows x 4 columns])
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 23288 entries, 0 to 23287
-#  Data columns (total 4 columns):
+#  RangeIndex: 23046 entries, 0 to 23045
+#  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   iso3          23288 non-null  object 
-#   1   anio          23288 non-null  int64  
-#   2   valor_en_ton  23288 non-null  float64
-#   3   iso2          23015 non-null  object 
+#   0   iso3          23046 non-null  object 
+#   1   anio          23046 non-null  int64  
+#   2   valor_en_ton  23046 non-null  float64
 #  
-#  |    | iso3   |   anio |   valor_en_ton | iso2   |
-#  |---:|:-------|-------:|---------------:|:-------|
-#  |  0 | AFG    |   1949 |     0.00199215 | AF     |
+#  |    | iso3   |   anio |   valor_en_ton |
+#  |---:|:-------|-------:|---------------:|
+#  |  0 | AFG    |   1949 |     0.00199215 |
 #  
 #  ------------------------------
 #  
-#  col_from_map(map={'ABW': 'AW', 'AFG': 'AF', 'AGO': 'AO', 'AIA': 'AI', 'ALA': 'AX', 'ALB': 'AL', 'AND': 'AD', 'ARE': 'AE', 'ARG': 'AR', 'ARM': 'AM', 'ASM': 'AS', 'ATA': 'AQ', 'ATF': 'TF', 'ATG': 'AG', 'AUS': 'AU', 'AUT': 'AT', 'AZE': 'AZ', 'BDI': 'BI', 'BEL': 'BE', 'BEN': 'BJ', 'BES': 'BQ', 'BFA': 'BF', 'BGD': 'BD', 'BGR': 'BG', 'BHR': 'BH', 'BHS': 'BS', 'BIH': 'BA', 'BLM': 'BL', 'BLR': 'BY', 'BLZ': 'BZ', 'BMU': 'BM', 'BOL': 'BO', 'BRA': 'BR', 'BRB': 'BB', 'BRN': 'BN', 'BTN': 'BT', 'BVT': 'BV', 'BWA': 'BW', 'CAF': 'CF', 'CAN': 'CA', 'CCK': 'CC', 'CHE': 'CH', 'CHL': 'CL', 'CHN': 'CN', 'CIV': 'CI', 'CMR': 'CM', 'COD': 'CD', 'COG': 'CG', 'COK': 'CK', 'COL': 'CO', 'COM': 'KM', 'CPV': 'CV', 'CRI': 'CR', 'CUB': 'CU', 'CUW': 'CW', 'CXR': 'CX', 'CYM': 'KY', 'CYP': 'CY', 'CZE': 'CZ', 'DEU': 'DE', 'DJI': 'DJ', 'DMA': 'DM', 'DNK': 'DK', 'DOM': 'DO', 'DZA': 'DZ', 'ECU': 'EC', 'EGY': 'EG', 'ERI': 'ER', 'ESH': 'EH', 'ESP': 'ES', 'EST': 'EE', 'ETH': 'ET', 'FIN': 'FI', 'FJI': 'FJ', 'FLK': 'FK', 'FRA': 'FR', 'FRO': 'FO', 'FSM': 'FM', 'GAB': 'GA', 'GBR': 'GB', 'GEO': 'GE', 'GGY': 'GG', 'GHA': 'GH', 'GIB': 'GI', 'GIN': 'GN', 'GLP': 'GP', 'GMB': 'GM', 'GNB': 'GW', 'GNQ': 'GQ', 'GRC': 'GR', 'GRD': 'GD', 'GRL': 'GL', 'GTM': 'GT', 'GUF': 'GF', 'GUM': 'GU', 'GUY': 'GY', 'HKG': 'HK', 'HMD': 'HM', 'HND': 'HN', 'HRV': 'HR', 'HTI': 'HT', 'HUN': 'HU', 'IDN': 'ID', 'IMN': 'IM', 'IND': 'IN', 'IOT': 'IO', 'IRL': 'IE', 'IRN': 'IR', 'IRQ': 'IQ', 'ISL': 'IS', 'ISR': 'IL', 'ITA': 'IT', 'JAM': 'JM', 'JEY': 'JE', 'JOR': 'JO', 'JPN': 'JP', 'KAZ': 'KZ', 'KEN': 'KE', 'KGZ': 'KG', 'KHM': 'KH', 'KIR': 'KI', 'KNA': 'KN', 'KOR': 'KR', 'KWT': 'KW', 'LAO': 'LA', 'LBN': 'LB', 'LBR': 'LR', 'LBY': 'LY', 'LCA': 'LC', 'LIE': 'LI', 'LKA': 'LK', 'LSO': 'LS', 'LTU': 'LT', 'LUX': 'LU', 'LVA': 'LV', 'MAC': 'MO', 'MAF': 'MF', 'MAR': 'MA', 'MCO': 'MC', 'MDA': 'MD', 'MDG': 'MG', 'MDV': 'MV', 'MEX': 'MX', 'MHL': 'MH', 'MKD': 'MK', 'MLI': 'ML', 'MLT': 'MT', 'MMR': 'MM', 'MNE': 'ME', 'MNG': 'MN', 'MNP': 'MP', 'MOZ': 'MZ', 'MRT': 'MR', 'MSR': 'MS', 'MTQ': 'MQ', 'MUS': 'MU', 'MWI': 'MW', 'MYS': 'MY', 'MYT': 'YT', 'NCL': 'NC', 'NER': 'NE', 'NFK': 'NF', 'NGA': 'NG', 'NIC': 'NI', 'NIU': 'NU', 'NLD': 'NL', 'NOR': 'NO', 'NPL': 'NP', 'NRU': 'NR', 'NZL': 'NZ', 'OMN': 'OM', 'PAK': 'PK', 'PAN': 'PA', 'PCN': 'PN', 'PER': 'PE', 'PHL': 'PH', 'PLW': 'PW', 'PNG': 'PG', 'POL': 'PL', 'PRI': 'PR', 'PRK': 'KP', 'PRT': 'PT', 'PRY': 'PY', 'PSE': 'PS', 'PYF': 'PF', 'QAT': 'QA', 'REU': 'RE', 'ROU': 'RO', 'RUS': 'RU', 'RWA': 'RW', 'SAU': 'SA', 'SDN': 'SD', 'SEN': 'SN', 'SGP': 'SG', 'SGS': 'GS', 'SHN': 'SH', 'SJM': 'SJ', 'SLB': 'SB', 'SLE': 'SL', 'SLV': 'SV', 'SMR': 'SM', 'SOM': 'SO', 'SPM': 'PM', 'SRB': 'RS', 'SSD': 'SS', 'STP': 'ST', 'SUR': 'SR', 'SVK': 'SK', 'SVN': 'SI', 'SWE': 'SE', 'SWZ': 'SZ', 'SXM': 'SX', 'SYC': 'SC', 'SYR': 'SY', 'TCA': 'TC', 'TCD': 'TD', 'TGO': 'TG', 'THA': 'TH', 'TJK': 'TJ', 'TKL': 'TK', 'TKM': 'TM', 'TLS': 'TL', 'TON': 'TO', 'TTO': 'TT', 'TUN': 'TN', 'TUR': 'TR', 'TUV': 'TV', 'TWN': 'TW', 'TZA': 'TZ', 'UGA': 'UG', 'UKR': 'UA', 'UMI': 'UM', 'URY': 'UY', 'USA': 'US', 'UZB': 'UZ', 'VAT': 'VA', 'VCT': 'VC', 'VEN': 'VE', 'VGB': 'VG', 'VIR': 'VI', 'VNM': 'VN', 'VUT': 'VU', 'WLF': 'WF', 'WSM': 'WS', 'YEM': 'YE', 'ZAF': 'ZA', 'ZMB': 'ZM', 'ZWE': 'ZW'}, source='iso3', target='iso2')
-#  RangeIndex: 23288 entries, 0 to 23287
-#  Data columns (total 4 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   iso3          23288 non-null  object 
-#   1   anio          23288 non-null  int64  
-#   2   valor_en_ton  23288 non-null  float64
-#   3   iso2          23015 non-null  object 
+#  rename_cols(map={'iso3': 'geocodigo', 'valor_en_ton': 'valor'})
+#  RangeIndex: 23046 entries, 0 to 23045
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  23046 non-null  object 
+#   1   anio       23046 non-null  int64  
+#   2   valor      23046 non-null  float64
 #  
-#  |    | iso3   |   anio |   valor_en_ton | iso2   |
-#  |---:|:-------|-------:|---------------:|:-------|
-#  |  0 | AFG    |   1949 |     0.00199215 | AF     |
+#  |    | geocodigo   |   anio |      valor |
+#  |---:|:------------|-------:|-----------:|
+#  |  0 | AFG         |   1949 | 0.00199215 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'iso2': 'iso', 'valor_en_ton': 'valor'})
-#  RangeIndex: 23288 entries, 0 to 23287
+#  join_geonomencladores(geonomenclador=            geocodigo                                  name_long  \
+#  0                 ABW                                      Aruba   
+#  1                 AFE               África Oriental y Meridional   
+#  2                 AFG                                 Afganistán   
+#  3                 AFW                África Occidental y Central   
+#  4                 AGO                                     Angola   
+#  ...               ...                                        ...   
+#  1015  DESHUM_AHDI.SAS                 Asia del Sur (AHDI) - PNUD   
+#  1016  DESHUM_AHDI.SSA          África Subsahariana (AHDI) - PNUD   
+#  1017  DESHUM_AHDI.WEU            Europa Occidental (AHDI) - PNUD   
+#  1018  DESHUM_AHDI.WLD                        Mundo (AHDI) - PNUD   
+#  1019  DESHUM_AHDI.WOF  Ramificaciones de Occidente (AHDI) - PNUD   
+#  
+#                                       name_short iso_2  
+#  0                                         Aruba    AW  
+#  1                  África Oriental y Meridional   NaN  
+#  2                                    Afganistán    AF  
+#  3                   África Occidental y Central   NaN  
+#  4                                        Angola    AO  
+#  ...                                         ...   ...  
+#  1015                 Asia del Sur (AHDI) - PNUD   NaN  
+#  1016          África Subsahariana (AHDI) - PNUD   NaN  
+#  1017            Europa Occidental (AHDI) - PNUD   NaN  
+#  1018                        Mundo (AHDI) - PNUD   NaN  
+#  1019  Ramificaciones de Occidente (AHDI) - PNUD   NaN  
+#  
+#  [1020 rows x 4 columns])
+#  RangeIndex: 23046 entries, 0 to 23045
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  23288 non-null  object 
-#   1   anio       23288 non-null  int64  
-#   2   valor      23288 non-null  float64
-#   3   iso        23015 non-null  object 
+#   0   geocodigo  23046 non-null  object 
+#   1   anio       23046 non-null  int64  
+#   2   valor      23046 non-null  float64
+#   3   iso        22766 non-null  object 
 #  
 #  |    | geocodigo   |   anio |      valor | iso   |
 #  |---:|:------------|-------:|-----------:|:------|
