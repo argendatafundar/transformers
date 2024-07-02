268ffff78b82cc022fcb9cb09bca9f14d9b5a667
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-02 15:49:37 -0300

    Add SALING_g24_transformer.py

---
+++ SALING/SALING_g24_transformer.py
@@ -0,0 +1,47 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+rename_cols(map={'salario_real_base1970': 'valor'})
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 89 entries, 0 to 88
+#  Data columns (total 2 columns):
+#   #   Column                 Non-Null Count  Dtype  
+#  ---  ------                 --------------  -----  
+#   0   anio                   89 non-null     int64  
+#   1   salario_real_base1970  89 non-null     float64
+#  
+#  |    |   anio |   salario_real_base1970 |
+#  |---:|-------:|------------------------:|
+#  |  0 |   1935 |                 50.9449 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'salario_real_base1970': 'valor'})
+#  RangeIndex: 89 entries, 0 to 88
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    89 non-null     int64  
+#   1   valor   89 non-null     float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   1935 | 50.9449 |
+#  
+#  ------------------------------
+#  
