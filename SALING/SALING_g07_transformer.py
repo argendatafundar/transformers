85dcdecf389e5d678ca08019d834b9b09bb2841e
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-02 15:09:17 -0300

    Update SALING_g07_transformer.py

--- SALING/SALING_g07_transformer.py
+++ SALING/SALING_g07_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'fuente': 'indicador', 'decil': 'categoria', 'proporcion': 'valor'})
+rename_cols(map={'fuente': 'indicador', 'decil': 'categoria', 'proporcion': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -45,9 +51,26 @@ rename_cols(map={'fuente': 'indicador', 'decil': 'categoria', 'proporcion': 'val
 #   3   indicador  50 non-null     object 
 #   4   valor      50 non-null     float64
 #  
-#  |    |   year |   semestre | categoria   | indicador       |    valor |
-#  |---:|-------:|-----------:|:------------|:----------------|---------:|
-#  |  0 |   2023 |          1 | Decil 1     | Ingreso laboral | 0.618709 |
+#  |    |   year |   semestre | categoria   | indicador       |   valor |
+#  |---:|-------:|-----------:|:------------|:----------------|--------:|
+#  |  0 |   2023 |          1 | Decil 1     | Ingreso laboral | 61.8709 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 50 entries, 0 to 49
+#  Data columns (total 5 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   year       50 non-null     int64  
+#   1   semestre   50 non-null     int64  
+#   2   categoria  50 non-null     object 
+#   3   indicador  50 non-null     object 
+#   4   valor      50 non-null     float64
+#  
+#  |    |   year |   semestre | categoria   | indicador       |   valor |
+#  |---:|-------:|-----------:|:------------|:----------------|--------:|
+#  |  0 |   2023 |          1 | Decil 1     | Ingreso laboral | 61.8709 |
 #  
 #  ------------------------------
 #  
