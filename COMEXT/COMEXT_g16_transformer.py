863c74e391470196ed6b774ddf9b1239643ce89b
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-01 21:52:31 -0300

    Update COMEXT_g16_transformer.py

--- COMEXT/COMEXT_g16_transformer.py
+++ COMEXT/COMEXT_g16_transformer.py
@@ -24,6 +24,13 @@ def drop_col(df: DataFrame, col, axis=1):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -33,7 +40,8 @@ query(condition='iso3 == "ARG"'),
 	drop_col(col='iso3', axis=1),
 	drop_col(col='location_name_short_en', axis=1),
 	drop_col(col='partner_region_id', axis=1),
-	rename_cols(map={'year': 'anio', 'partner_region_name_short_es': 'indicador', 'export_value_pc': 'valor'})
+	rename_cols(map={'year': 'anio', 'partner_region_name_short_es': 'indicador', 'export_value_pc': 'valor'}),
+	sort_values(how='ascending', by=['anio'])
 )
 #  PIPELINE_END
 
@@ -52,7 +60,7 @@ query(condition='iso3 == "ARG"'),
 #  
 #  |    |   year | iso3   | location_name_short_en   |   partner_region_id | partner_region_name_short_es   |   export_value_pc |
 #  |---:|-------:|:-------|:-------------------------|--------------------:|:-------------------------------|------------------:|
-#  |  0 |   2005 | ABW    | Aruba                    |                 356 | África                         |        0.00608911 |
+#  |  0 |   2017 | ABW    | Aruba                    |                 356 | Ámerica del Norte              |           25.6312 |
 #  
 #  ------------------------------
 #  
@@ -70,7 +78,7 @@ query(condition='iso3 == "ARG"'),
 #  
 #  |    |   year | iso3   | location_name_short_en   |   partner_region_id | partner_region_name_short_es   |   export_value_pc |
 #  |---:|-------:|:-------|:-------------------------|--------------------:|:-------------------------------|------------------:|
-#  |  8 |   2009 | ARG    | Argentina                |                 357 | África                         |           5.48451 |
+#  |  8 |   1996 | ARG    | Argentina                |                 357 | Otros                          |          0.225482 |
 #  
 #  ------------------------------
 #  
@@ -87,7 +95,7 @@ query(condition='iso3 == "ARG"'),
 #  
 #  |    |   year | location_name_short_en   |   partner_region_id | partner_region_name_short_es   |   export_value_pc |
 #  |---:|-------:|:-------------------------|--------------------:|:-------------------------------|------------------:|
-#  |  8 |   2009 | Argentina                |                 357 | África                         |           5.48451 |
+#  |  8 |   1996 | Argentina                |                 357 | Otros                          |          0.225482 |
 #  
 #  ------------------------------
 #  
@@ -103,7 +111,7 @@ query(condition='iso3 == "ARG"'),
 #  
 #  |    |   year |   partner_region_id | partner_region_name_short_es   |   export_value_pc |
 #  |---:|-------:|--------------------:|:-------------------------------|------------------:|
-#  |  8 |   2009 |                 357 | África                         |           5.48451 |
+#  |  8 |   1996 |                 357 | Otros                          |          0.225482 |
 #  
 #  ------------------------------
 #  
@@ -118,7 +126,7 @@ query(condition='iso3 == "ARG"'),
 #  
 #  |    |   year | partner_region_name_short_es   |   export_value_pc |
 #  |---:|-------:|:-------------------------------|------------------:|
-#  |  8 |   2009 | África                         |           5.48451 |
+#  |  8 |   1996 | Otros                          |          0.225482 |
 #  
 #  ------------------------------
 #  
@@ -131,9 +139,24 @@ query(condition='iso3 == "ARG"'),
 #   1   indicador  415 non-null    object 
 #   2   valor      415 non-null    float64
 #  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  8 |   2009 | África      | 5.48451 |
+#  |    |   anio | indicador   |    valor |
+#  |---:|-------:|:------------|---------:|
+#  |  8 |   1996 | Otros       | 0.225482 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 415 entries, 0 to 414
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       415 non-null    int64  
+#   1   indicador  415 non-null    object 
+#   2   valor      415 non-null    float64
+#  
+#  |    |   anio | indicador   |    valor |
+#  |---:|-------:|:------------|---------:|
+#  |  0 |   1962 | Otros       | 0.040331 |
 #  
 #  ------------------------------
 #  
