9ccafd337df1168e313a9ad3c2664a49daa81374
jgjuara <jg.juara@gmail.com>
2024-07-02 16:32:40 -0300

    Update TRANEN_g13_transformer.py

--- TRANEN/TRANEN_g13_transformer.py
+++ TRANEN/TRANEN_g13_transformer.py
@@ -44,6 +44,11 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     return df
 
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def drop_na(df:DataFrame, cols:list):
     return df.dropna(subset=cols)
 
@@ -69,6 +74,7 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 	replace_value(col='indicador', curr_value='Carbon', new_value='Carbón'),
 	replace_value(col='indicador', curr_value='Petroleo', new_value='Petróleo'),
 	replace_value(col='indicador', curr_value='Eolica', new_value='Eólica'),
+	query(condition='indicador != "Total"'),
 	drop_na(cols=['valor']),
 	drop_col(col=['valor_en_twh'], axis=1),
 	sort_values(how='ascending', by=['anio', 'geocodigo', 'indicador'])
@@ -229,16 +235,33 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  
 #  ------------------------------
 #  
-#  drop_na(cols=['valor'])
-#  RangeIndex: 81900 entries, 0 to 81899
+#  query(condition='indicador != "Total"')
+#  Index: 73710 entries, 0 to 73709
 #  Data columns (total 5 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   anio          81900 non-null  int64  
-#   1   geocodigo     81900 non-null  object 
-#   2   indicador     81900 non-null  object 
-#   3   valor_en_twh  57843 non-null  float64
-#   4   valor         81900 non-null  float64
+#   0   anio          73710 non-null  int64  
+#   1   geocodigo     73710 non-null  object 
+#   2   indicador     73710 non-null  object 
+#   3   valor_en_twh  49653 non-null  float64
+#   4   valor         73710 non-null  float64
+#  
+#  |    |   anio | geocodigo   | indicador        |   valor_en_twh |   valor |
+#  |---:|-------:|:------------|:-----------------|---------------:|--------:|
+#  |  0 |   1985 | GBR         | Otras renovables |              0 |       0 |
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
+#  Index: 73710 entries, 0 to 73709
+#  Data columns (total 5 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   anio          73710 non-null  int64  
+#   1   geocodigo     73710 non-null  object 
+#   2   indicador     73710 non-null  object 
+#   3   valor_en_twh  49653 non-null  float64
+#   4   valor         73710 non-null  float64
 #  
 #  |    |   anio | geocodigo   | indicador        |   valor_en_twh |   valor |
 #  |---:|-------:|:------------|:-----------------|---------------:|--------:|
@@ -247,14 +270,14 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  ------------------------------
 #  
 #  drop_col(col=['valor_en_twh'], axis=1)
-#  RangeIndex: 81900 entries, 0 to 81899
+#  Index: 73710 entries, 0 to 73709
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       81900 non-null  int64  
-#   1   geocodigo  81900 non-null  object 
-#   2   indicador  81900 non-null  object 
-#   3   valor      81900 non-null  float64
+#   0   anio       73710 non-null  int64  
+#   1   geocodigo  73710 non-null  object 
+#   2   indicador  73710 non-null  object 
+#   3   valor      73710 non-null  float64
 #  
 #  |    |   anio | geocodigo   | indicador        |   valor |
 #  |---:|-------:|:------------|:-----------------|--------:|
@@ -263,14 +286,14 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio', 'geocodigo', 'indicador'])
-#  Index: 81900 entries, 15132 to 81899
+#  Index: 73710 entries, 15132 to 20045
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       81900 non-null  int64  
-#   1   geocodigo  81900 non-null  object 
-#   2   indicador  81900 non-null  object 
-#   3   valor      81900 non-null  float64
+#   0   anio       73710 non-null  int64  
+#   1   geocodigo  73710 non-null  object 
+#   2   indicador  73710 non-null  object 
+#   3   valor      73710 non-null  float64
 #  
 #  |       |   anio | geocodigo   | indicador   |   valor |
 #  |------:|-------:|:------------|:------------|--------:|
