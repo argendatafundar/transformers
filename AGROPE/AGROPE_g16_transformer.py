6a6f5e561af9a5cd71a489aac87d57c74ebd6f60
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-02 16:39:32 -0300

    Update AGROPE_g16_transformer.py

--- AGROPE/AGROPE_g16_transformer.py
+++ AGROPE/AGROPE_g16_transformer.py
@@ -31,6 +31,13 @@ def rename_cols(df: DataFrame, map):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -41,7 +48,8 @@ replace_value(col='iso3', curr_value='F15', new_value='BLX'),
 	replace_value(col='iso3', curr_value='F248', new_value='SER'),
 	replace_value(col='iso3', curr_value='F51', new_value='CSK'),
 	rename_cols(map={'iso3': 'geocodigo'}),
-	drop_col(col='iso3_desc_fundar', axis=1)
+	drop_col(col='iso3_desc_fundar', axis=1),
+	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
 
@@ -143,6 +151,21 @@ replace_value(col='iso3', curr_value='F15', new_value='BLX'),
 #  ------------------------------
 #  
 #  drop_col(col='iso3_desc_fundar', axis=1)
+#  RangeIndex: 9551 entries, 0 to 9550
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  9551 non-null   object 
+#   1   anio       9551 non-null   int64  
+#   2   valor      9551 non-null   float64
+#  
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AFG         |   1961 |  700000 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo'])
 #  RangeIndex: 9551 entries, 0 to 9550
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
