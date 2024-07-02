f4c039e7bab780e612c3719608e313b11627f77d
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-02 16:25:59 -0300

    Update AGROPE_g03_transformer.py

--- AGROPE/AGROPE_g03_transformer.py
+++ AGROPE/AGROPE_g03_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'cuenta': 'indicador', 'participacion_pbi': 'valor'})
+rename_cols(map={'cuenta': 'indicador', 'participacion_pbi': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -43,7 +49,22 @@ rename_cols(map={'cuenta': 'indicador', 'participacion_pbi': 'valor'})
 #  
 #  |    |   anio | indicador   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2004 | Agricultura |    0.05 |
+#  |  0 |   2004 | Agricultura |       5 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 76 entries, 0 to 75
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       76 non-null     int64  
+#   1   indicador  76 non-null     object 
+#   2   valor      76 non-null     float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   2004 | Agricultura |       5 |
 #  
 #  ------------------------------
 #  
