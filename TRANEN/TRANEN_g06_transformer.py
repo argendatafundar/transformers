5f04c69d9fc93816351d68a118f6b35c4bf0775a
jgjuara <jg.juara@gmail.com>
2024-07-02 15:13:13 -0300

    Update TRANEN_g06_transformer.py

--- TRANEN/TRANEN_g06_transformer.py
+++ TRANEN/TRANEN_g06_transformer.py
@@ -17,6 +17,16 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def drop_na(df:DataFrame, cols:list):
+    return df.dropna(subset=cols)
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    return df.sort_values(by=by, ascending=how == 'ascending')
 #  DEFINITIONS_END
 
 
@@ -24,7 +34,9 @@ def rename_cols(df: DataFrame, map):
 pipeline = chain(
 replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 	replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
-	rename_cols(map={'valor_en_twh': 'valor', 'iso3': 'geocodigo'})
+	rename_cols(map={'valor_en_twh': 'valor', 'iso3': 'geocodigo'}),
+	drop_na(cols=['valor']),
+	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
 
@@ -36,11 +48,11 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  ---  ------        --------------  -----  
 #   0   anio          12744 non-null  int64  
 #   1   iso3          12744 non-null  object 
-#   2   valor_en_twh  7423 non-null   float64
+#   2   valor_en_twh  7438 non-null   float64
 #  
 #  |    |   anio | iso3   |   valor_en_twh |
 #  |---:|-------:|:-------|---------------:|
-#  |  0 |   2000 | AFG    |           0.31 |
+#  |  0 |   1965 | GBR    |          4.612 |
 #  
 #  ------------------------------
 #  
@@ -51,11 +63,11 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  ---  ------        --------------  -----  
 #   0   anio          12744 non-null  int64  
 #   1   iso3          12744 non-null  object 
-#   2   valor_en_twh  7423 non-null   float64
+#   2   valor_en_twh  7438 non-null   float64
 #  
 #  |    |   anio | iso3   |   valor_en_twh |
 #  |---:|-------:|:-------|---------------:|
-#  |  0 |   2000 | AFG    |           0.31 |
+#  |  0 |   1965 | GBR    |          4.612 |
 #  
 #  ------------------------------
 #  
@@ -66,11 +78,11 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  ---  ------        --------------  -----  
 #   0   anio          12744 non-null  int64  
 #   1   iso3          12744 non-null  object 
-#   2   valor_en_twh  7423 non-null   float64
+#   2   valor_en_twh  7438 non-null   float64
 #  
 #  |    |   anio | iso3   |   valor_en_twh |
 #  |---:|-------:|:-------|---------------:|
-#  |  0 |   2000 | AFG    |           0.31 |
+#  |  0 |   1965 | GBR    |          4.612 |
 #  
 #  ------------------------------
 #  
@@ -81,11 +93,41 @@ replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
 #  ---  ------     --------------  -----  
 #   0   anio       12744 non-null  int64  
 #   1   geocodigo  12744 non-null  object 
-#   2   valor      7423 non-null   float64
+#   2   valor      7438 non-null   float64
 #  
 #  |    |   anio | geocodigo   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2000 | AFG         |    0.31 |
+#  |  0 |   1965 | GBR         |   4.612 |
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
+#  Index: 7438 entries, 0 to 12742
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       7438 non-null   int64  
+#   1   geocodigo  7438 non-null   object 
+#   2   valor      7438 non-null   float64
+#  
+#  |    |   anio | geocodigo   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1965 | GBR         |   4.612 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  Index: 7438 entries, 1298 to 1474
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       7438 non-null   int64  
+#   1   geocodigo  7438 non-null   object 
+#   2   valor      7438 non-null   float64
+#  
+#  |      |   anio | geocodigo   |   valor |
+#  |-----:|-------:|:------------|--------:|
+#  | 1298 |   1965 | ARE         |       0 |
 #  
 #  ------------------------------
 #  
