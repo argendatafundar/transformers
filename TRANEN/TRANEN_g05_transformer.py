1b6f1b2a9c95ae7e318780fc8dd32b5811a5d2bf
jgjuara <jg.juara@gmail.com>
2024-07-02 15:09:43 -0300

    Update TRANEN_g05_transformer.py

--- TRANEN/TRANEN_g05_transformer.py
+++ TRANEN/TRANEN_g05_transformer.py
@@ -36,6 +36,16 @@ def rename_cols(df: DataFrame, map):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def drop_na(df:DataFrame, cols:list):
+    return df.dropna(subset=cols)
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    return df.sort_values(by=by, ascending=how == 'ascending')
 #  DEFINITIONS_END
 
 
@@ -47,7 +57,9 @@ query(condition='tipo_energia != "Total"'),
 	replace_value(col='fuente_energia', curr_value='Petroleo', new_value='Petróleo'),
 	replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 	rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'}),
-	drop_col(col=['valor_en_twh', 'tipo_energia'], axis=1)
+	drop_col(col=['valor_en_twh', 'tipo_energia'], axis=1),
+	drop_na(cols=['valor']),
+	sort_values(how='ascending', by=['anio', 'geocodigo', 'indicador'])
 )
 #  PIPELINE_END
 
@@ -179,6 +191,38 @@ query(condition='tipo_energia != "Total"'),
 #  ------------------------------
 #  
 #  drop_col(col=['valor_en_twh', 'tipo_energia'], axis=1)
+#  Index: 46433 entries, 0 to 52863
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  46433 non-null  object 
+#   1   anio       46433 non-null  int64  
+#   2   indicador  46433 non-null  object 
+#   3   valor      46433 non-null  float64
+#  
+#  |    | geocodigo   |   anio | indicador       |   valor |
+#  |---:|:------------|-------:|:----------------|--------:|
+#  |  0 | AGO         |   1965 | Biocombustibles |       0 |
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
+#  Index: 46433 entries, 0 to 52863
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  46433 non-null  object 
+#   1   anio       46433 non-null  int64  
+#   2   indicador  46433 non-null  object 
+#   3   valor      46433 non-null  float64
+#  
+#  |    | geocodigo   |   anio | indicador       |   valor |
+#  |---:|:------------|-------:|:----------------|--------:|
+#  |  0 | AGO         |   1965 | Biocombustibles |       0 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo', 'indicador'])
 #  Index: 46433 entries, 0 to 52863
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
