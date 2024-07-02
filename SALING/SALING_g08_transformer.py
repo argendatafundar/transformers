371740e49035978a4bd285e84b007efd3eacb5c8
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-02 15:11:02 -0300

    Update SALING_g08_transformer.py

--- SALING/SALING_g08_transformer.py
+++ SALING/SALING_g08_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'fuente': 'indicador', 'edad_jefe': 'categoria', 'proporcion': 'valor'})
+rename_cols(map={'fuente': 'indicador', 'edad_jefe': 'categoria', 'proporcion': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -45,9 +51,26 @@ rename_cols(map={'fuente': 'indicador', 'edad_jefe': 'categoria', 'proporcion': 
 #   3   indicador  30 non-null     object 
 #   4   valor      30 non-null     float64
 #  
-#  |    |   year |   semestre | categoria   | indicador       |    valor |
-#  |---:|-------:|-----------:|:------------|:----------------|---------:|
-#  |  0 |   2023 |          1 | 24 o menos  | Ingreso laboral | 0.714832 |
+#  |    |   year |   semestre | categoria   | indicador       |   valor |
+#  |---:|-------:|-----------:|:------------|:----------------|--------:|
+#  |  0 |   2023 |          1 | 24 o menos  | Ingreso laboral | 71.4832 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 30 entries, 0 to 29
+#  Data columns (total 5 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   year       30 non-null     int64  
+#   1   semestre   30 non-null     int64  
+#   2   categoria  30 non-null     object 
+#   3   indicador  30 non-null     object 
+#   4   valor      30 non-null     float64
+#  
+#  |    |   year |   semestre | categoria   | indicador       |   valor |
+#  |---:|-------:|-----------:|:------------|:----------------|--------:|
+#  |  0 |   2023 |          1 | 24 o menos  | Ingreso laboral | 71.4832 |
 #  
 #  ------------------------------
 #  
