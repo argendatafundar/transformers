478e7440182ea702c5600fecc5a2c873e6ef943b
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-02 13:53:31 -0300

    Update MERTRA_g07_transformer.py

--- MERTRA/MERTRA_g07_transformer.py
+++ MERTRA/MERTRA_g07_transformer.py
@@ -7,12 +7,26 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'puestos_per_capita': 'valor'})
+rename_cols(map={'iso3': 'geocodigo', 'puestos_per_capita': 'valor'}),
+	drop_na(col='valor'),
+	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
 
@@ -44,6 +58,36 @@ rename_cols(map={'iso3': 'geocodigo', 'puestos_per_capita': 'valor'})
 #  |    | geocodigo   |   anio |   valor |
 #  |---:|:------------|-------:|--------:|
 #  |  0 | ABW         |   1950 |     nan |
+#  
+#  ------------------------------
+#  
+#  drop_na(col='valor')
+#  Index: 9529 entries, 41 to 12809
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  9529 non-null   object 
+#   1   anio       9529 non-null   int64  
+#   2   valor      9529 non-null   float64
+#  
+#  |    | geocodigo   |   anio |    valor |
+#  |---:|:------------|-------:|---------:|
+#  | 41 | ABW         |   1991 | 0.451859 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  RangeIndex: 9529 entries, 0 to 9528
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  9529 non-null   object 
+#   1   anio       9529 non-null   int64  
+#   2   valor      9529 non-null   float64
+#  
+#  |    | geocodigo   |   anio |    valor |
+#  |---:|:------------|-------:|---------:|
+#  |  0 | ARG         |   1950 | 0.386666 |
 #  
 #  ------------------------------
 #  
