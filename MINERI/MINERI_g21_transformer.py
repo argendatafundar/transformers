ac20db8d5dc4358ade9d93902242045a88ab9305
joangq <jgquiroga@dc.uba.ar>
2024-07-03 12:45:13 -0300

    Update MINERI_g21_transformer.py

--- MINERI/MINERI_g21_transformer.py
+++ MINERI/MINERI_g21_transformer.py
@@ -7,12 +7,72 @@ from data_transformers import chain, transformer
 def rename_columns(df: DataFrame, **kwargs):
     df = df.rename(columns=kwargs)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(tipo_energia='categoria', mineral_critico='indicador', mineral_utilizado='valor')
+rename_columns(tipo_energia='categoria', mineral_critico='indicador', mineral_utilizado='valor'),
+	replace_value(col='indicador', curr_value='cobre', new_value='Cobre'),
+	replace_value(col='indicador', curr_value='niquel', new_value='Níquel'),
+	replace_value(col='indicador', curr_value='manganeso', new_value='Manganeso'),
+	replace_value(col='indicador', curr_value='cobalto', new_value='Cobalto'),
+	replace_value(col='indicador', curr_value='cromo', new_value='Cromo'),
+	replace_value(col='indicador', curr_value='molibdeno', new_value='Molibdeno'),
+	replace_value(col='indicador', curr_value='cinc', new_value='Cinc'),
+	replace_value(col='indicador', curr_value='tierras_raras', new_value='Tierras raras'),
+	replace_value(col='indicador', curr_value='silicio', new_value='Silicio'),
+	replace_value(col='indicador', curr_value='otros', new_value='Otros')
 )
 #  PIPELINE_END
 
@@ -44,6 +104,156 @@ rename_columns(tipo_energia='categoria', mineral_critico='indicador', mineral_ut
 #  |    | categoria       | indicador   |   valor |
 #  |---:|:----------------|:------------|--------:|
 #  |  0 | eolica_offshore | cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='cobre', new_value='Cobre')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='niquel', new_value='Níquel')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='manganeso', new_value='Manganeso')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='cobalto', new_value='Cobalto')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='cromo', new_value='Cromo')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='molibdeno', new_value='Molibdeno')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='cinc', new_value='Cinc')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='tierras_raras', new_value='Tierras raras')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='silicio', new_value='Silicio')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='otros', new_value='Otros')
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  60 non-null     object 
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
+#  
+#  |    | categoria       | indicador   |   valor |
+#  |---:|:----------------|:------------|--------:|
+#  |  0 | eolica_offshore | Cobre       |    8000 |
 #  
 #  ------------------------------
 #  
