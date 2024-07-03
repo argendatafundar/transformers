d6ff77bc9b1f6d121b203b02beb9a24dbd6f8237
jgjuara <jg.juara@gmail.com>
2024-07-03 17:27:16 -0300

    Update SEBACO_g07_transformer.py

--- SEBACO/SEBACO_g07_transformer.py
+++ SEBACO/SEBACO_g07_transformer.py
@@ -42,6 +42,18 @@ def rename_cols(df: DataFrame, map):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -54,7 +66,9 @@ replace_value(col='provincia', curr_value='CABA', new_value='AR-C'),
 	replace_value(col='provincia', curr_value='Resto de Buenos Aires', new_value='SEBACO_AR-B-RESTO'),
 	replace_value(col='provincia', curr_value='Santa Fe', new_value='AR-S'),
 	rename_cols(map={'provincia': 'geocodigo'}),
-	rename_cols(map={'prop': 'valor'})
+	rename_cols(map={'prop': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100),
+	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
 
@@ -188,9 +202,39 @@ replace_value(col='provincia', curr_value='CABA', new_value='AR-C'),
 #   1   anio       162 non-null    int64  
 #   2   valor      162 non-null    float64
 #  
-#  |    | geocodigo   |   anio |    valor |
-#  |---:|:------------|-------:|---------:|
-#  |  0 | AR-C        |   1996 | 0.410583 |
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AR-C        |   1996 | 41.0583 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 162 entries, 0 to 161
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  162 non-null    object 
+#   1   anio       162 non-null    int64  
+#   2   valor      162 non-null    float64
+#  
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AR-C        |   1996 | 41.0583 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  RangeIndex: 162 entries, 0 to 161
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  162 non-null    object 
+#   1   anio       162 non-null    int64  
+#   2   valor      162 non-null    float64
+#  
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AR-C        |   1996 | 41.0583 |
 #  
 #  ------------------------------
 #  
