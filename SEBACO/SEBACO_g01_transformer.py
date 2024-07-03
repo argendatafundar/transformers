5bcab266324f6b2cc113a6c52a6218334ca655d4
jgjuara <jg.juara@gmail.com>
2024-07-03 15:48:47 -0300

    Update SEBACO_g01_transformer.py

--- SEBACO/SEBACO_g01_transformer.py
+++ SEBACO/SEBACO_g01_transformer.py
@@ -8,16 +8,30 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 drop_col(col='sector', axis=1),
-	rename_columns(share_ventas='valor')
+	rename_cols(map={'share_ventas': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100),
+	sort_values(how='ascending', by=['anio'])
 )
 #  PIPELINE_END
 
@@ -51,7 +65,7 @@ drop_col(col='sector', axis=1),
 #  
 #  ------------------------------
 #  
-#  rename_columns(share_ventas='valor')
+#  rename_cols(map={'share_ventas': 'valor'})
 #  RangeIndex: 8 entries, 0 to 7
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
@@ -59,9 +73,37 @@ drop_col(col='sector', axis=1),
 #   0   anio    8 non-null      int64  
 #   1   valor   8 non-null      float64
 #  
-#  |    |   anio |     valor |
-#  |---:|-------:|----------:|
-#  |  0 |   2014 | 0.0259628 |
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   2014 | 2.59628 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    8 non-null      int64  
+#   1   valor   8 non-null      float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   2014 | 2.59628 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    8 non-null      int64  
+#   1   valor   8 non-null      float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   2014 | 2.59628 |
 #  
 #  ------------------------------
 #  
