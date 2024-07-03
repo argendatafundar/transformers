9f4e5e7b3b0beb864986bf45c8de835e35a20b83
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-02 22:40:09 -0300

    Update PRECIO_g13_transformer.py

--- PRECIO/PRECIO_g13_transformer.py
+++ PRECIO/PRECIO_g13_transformer.py
@@ -17,6 +17,11 @@ def rename_cols(df: DataFrame, map):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
@@ -24,68 +29,84 @@ def rename_cols(df: DataFrame, map):
 pipeline = chain(
 rename_cols(map={'periodo': 'indicador'}),
 	rename_cols(map={'rubro': 'indicador'}),
-	rename_cols(map={'porcentaje': 'valor'})
+	rename_cols(map={'porcentaje': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 36 entries, 0 to 35
+#  RangeIndex: 39 entries, 0 to 38
 #  Data columns (total 3 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
-#   0   rubro       36 non-null     object 
-#   1   periodo     36 non-null     object 
-#   2   porcentaje  36 non-null     float64
+#   0   rubro       39 non-null     object 
+#   1   periodo     39 non-null     object 
+#   2   porcentaje  39 non-null     float64
 #  
 #  |    | rubro                              | periodo   |   porcentaje |
 #  |---:|:-----------------------------------|:----------|-------------:|
-#  |  0 | Alimentos y bebidas no alcoh髄icas | 1996-1997 |        0.288 |
+#  |  0 | Alimentos y bebidas no alcoh贸licas | 1996-1997 |        0.288 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'periodo': 'indicador'})
-#  RangeIndex: 36 entries, 0 to 35
+#  RangeIndex: 39 entries, 0 to 38
 #  Data columns (total 3 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
-#   0   rubro       36 non-null     object 
-#   1   indicador   36 non-null     object 
-#   2   porcentaje  36 non-null     float64
+#   0   rubro       39 non-null     object 
+#   1   indicador   39 non-null     object 
+#   2   porcentaje  39 non-null     float64
 #  
 #  |    | rubro                              | indicador   |   porcentaje |
 #  |---:|:-----------------------------------|:------------|-------------:|
-#  |  0 | Alimentos y bebidas no alcoh髄icas | 1996-1997   |        0.288 |
+#  |  0 | Alimentos y bebidas no alcoh贸licas | 1996-1997   |        0.288 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'rubro': 'indicador'})
-#  RangeIndex: 36 entries, 0 to 35
+#  RangeIndex: 39 entries, 0 to 38
 #  Data columns (total 3 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
-#   0   indicador   36 non-null     object 
-#   1   indicador   36 non-null     object 
-#   2   porcentaje  36 non-null     float64
+#   0   indicador   39 non-null     object 
+#   1   indicador   39 non-null     object 
+#   2   porcentaje  39 non-null     float64
 #  
 #  |    | indicador                          | indicador   |   porcentaje |
 #  |---:|:-----------------------------------|:------------|-------------:|
-#  |  0 | Alimentos y bebidas no alcoh髄icas | 1996-1997   |        0.288 |
+#  |  0 | Alimentos y bebidas no alcoh贸licas | 1996-1997   |        0.288 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'porcentaje': 'valor'})
-#  RangeIndex: 36 entries, 0 to 35
+#  RangeIndex: 39 entries, 0 to 38
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  36 non-null     object 
-#   1   indicador  36 non-null     object 
-#   2   valor      36 non-null     float64
+#   0   indicador  39 non-null     object 
+#   1   indicador  39 non-null     object 
+#   2   valor      39 non-null     float64
 #  
 #  |    | indicador                          | indicador   |   valor |
 #  |---:|:-----------------------------------|:------------|--------:|
-#  |  0 | Alimentos y bebidas no alcoh髄icas | 1996-1997   |   0.288 |
+#  |  0 | Alimentos y bebidas no alcoh贸licas | 1996-1997   |    28.8 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 39 entries, 0 to 38
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   indicador  39 non-null     object 
+#   1   indicador  39 non-null     object 
+#   2   valor      39 non-null     float64
+#  
+#  |    | indicador                          | indicador   |   valor |
+#  |---:|:-----------------------------------|:------------|--------:|
+#  |  0 | Alimentos y bebidas no alcoh贸licas | 1996-1997   |    28.8 |
 #  
 #  ------------------------------
 #  
