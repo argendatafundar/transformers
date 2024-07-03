e4a20e8a1bf824f715a4d678687ed04a0469a6ec
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-02 22:50:07 -0300

    Update PRECIO_g14_transformer.py

--- PRECIO/PRECIO_g14_transformer.py
+++ PRECIO/PRECIO_g14_transformer.py
@@ -4,15 +4,27 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(rubro='categoria', precio_relativo='valor')
+rename_cols(map={'rubro': 'categoria'}),
+	rename_cols(map={'precio_relativo': 'valor'}),
+	query(condition='nivel == 1')
 )
 #  PIPELINE_END
 
@@ -34,7 +46,24 @@ rename_columns(rubro='categoria', precio_relativo='valor')
 #  
 #  ------------------------------
 #  
-#  rename_columns(rubro='categoria', precio_relativo='valor')
+#  rename_cols(map={'rubro': 'categoria'})
+#  RangeIndex: 748 entries, 0 to 747
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             748 non-null    int64  
+#   1   codigo           748 non-null    object 
+#   2   nivel            748 non-null    int64  
+#   3   categoria        748 non-null    object 
+#   4   precio_relativo  748 non-null    float64
+#  
+#  |    |   anio |   codigo |   nivel | categoria                          |   precio_relativo |
+#  |---:|-------:|---------:|--------:|:-----------------------------------|------------------:|
+#  |  0 |   2013 |       01 |       1 | Alimentos y bebidas no alcohólicas |               100 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'precio_relativo': 'valor'})
 #  RangeIndex: 748 entries, 0 to 747
 #  Data columns (total 5 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -44,6 +73,23 @@ rename_columns(rubro='categoria', precio_relativo='valor')
 #   2   nivel      748 non-null    int64  
 #   3   categoria  748 non-null    object 
 #   4   valor      748 non-null    float64
+#  
+#  |    |   anio |   codigo |   nivel | categoria                          |   valor |
+#  |---:|-------:|---------:|--------:|:-----------------------------------|--------:|
+#  |  0 |   2013 |       01 |       1 | Alimentos y bebidas no alcohólicas |     100 |
+#  
+#  ------------------------------
+#  
+#  query(condition='nivel == 1')
+#  Index: 143 entries, 0 to 744
+#  Data columns (total 5 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       143 non-null    int64  
+#   1   codigo     143 non-null    object 
+#   2   nivel      143 non-null    int64  
+#   3   categoria  143 non-null    object 
+#   4   valor      143 non-null    float64
 #  
 #  |    |   anio |   codigo |   nivel | categoria                          |   valor |
 #  |---:|-------:|---------:|--------:|:-----------------------------------|--------:|
