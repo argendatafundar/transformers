04594e7e08fbfb42a4d1ad29f8a2a3e8cdea804b
joangq <jgquiroga@dc.uba.ar>
2024-07-03 14:07:42 -0300

    Update PRECIO_g15_transformer.py

--- PRECIO/PRECIO_g15_transformer.py
+++ PRECIO/PRECIO_g15_transformer.py
@@ -14,13 +14,18 @@ def sort_values(df: DataFrame, how: str, by: list):
         raise ValueError('how must be either "ascending" or "descending"')
     
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def drop_na(df:DataFrame, cols:list):
+    return df.dropna(subset=cols)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_columns(rubro='categoria', precio_relativo='valor'),
-	sort_values(how='ascending', by=['anio', 'categoria'])
+	sort_values(how='ascending', by=['anio', 'categoria']),
+	drop_na(cols=['valor'])
 )
 #  PIPELINE_END
 
@@ -56,6 +61,21 @@ rename_columns(rubro='categoria', precio_relativo='valor'),
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio', 'categoria'])
+#  RangeIndex: 778 entries, 0 to 777
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       778 non-null    int64  
+#   1   categoria  778 non-null    object 
+#   2   valor      778 non-null    float64
+#  
+#  |    |   anio | categoria           |   valor |
+#  |---:|-------:|:--------------------|--------:|
+#  |  0 |   1947 | Alimentos y bebidas | 92.3782 |
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
 #  RangeIndex: 778 entries, 0 to 777
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
