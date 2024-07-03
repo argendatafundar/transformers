11e19319e3c1ad8cd9cd7135afc0a556c2b77653
Juan Manuel Dias <juamadias@gmail.com>
2024-07-03 16:59:28 -0300

    Update INFDES_g14_transformer.py

--- INFDES/INFDES_g14_transformer.py
+++ INFDES/INFDES_g14_transformer.py
@@ -9,6 +9,10 @@ def query(df: DataFrame, condition: str):
     return df
 
 @transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -18,6 +22,7 @@ def rename_cols(df: DataFrame, map):
 #  PIPELINE_START
 pipeline = chain(
 query(condition='anio == 2023'),
+	drop_col(col='anio', axis=1),
 	rename_cols(map={'rango_edad': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
 )
 #  PIPELINE_END
@@ -57,20 +62,35 @@ query(condition='anio == 2023'),
 #  
 #  ------------------------------
 #  
+#  drop_col(col='anio', axis=1)
+#  Index: 8 entries, 56 to 63
+#  Data columns (total 4 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   rango_edad         8 non-null      int64  
+#   1   rango_edad_desc    8 non-null      object 
+#   2   sexo               8 non-null      object 
+#   3   tasa_desocupacion  8 non-null      float64
+#  
+#  |    |   rango_edad | rango_edad_desc   | sexo    |   tasa_desocupacion |
+#  |---:|-------------:|:------------------|:--------|--------------------:|
+#  | 56 |            1 | Hasta 30          | Mujeres |            0.124757 |
+#  
+#  ------------------------------
+#  
 #  rename_cols(map={'rango_edad': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
 #  Index: 8 entries, 56 to 63
-#  Data columns (total 5 columns):
+#  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             8 non-null      int64  
-#   1   categoria        8 non-null      int64  
-#   2   rango_edad_desc  8 non-null      object 
-#   3   indicador        8 non-null      object 
-#   4   valor            8 non-null      float64
+#   0   categoria        8 non-null      int64  
+#   1   rango_edad_desc  8 non-null      object 
+#   2   indicador        8 non-null      object 
+#   3   valor            8 non-null      float64
 #  
-#  |    |   anio |   categoria | rango_edad_desc   | indicador   |    valor |
-#  |---:|-------:|------------:|:------------------|:------------|---------:|
-#  | 56 |   2023 |           1 | Hasta 30          | Mujeres     | 0.124757 |
+#  |    |   categoria | rango_edad_desc   | indicador   |    valor |
+#  |---:|------------:|:------------------|:------------|---------:|
+#  | 56 |           1 | Hasta 30          | Mujeres     | 0.124757 |
 #  
 #  ------------------------------
 #  
