9dde11417c56cce86104ec478973d7e5da1d2b7d
jgjuara <jg.juara@gmail.com>
2024-07-03 17:22:40 -0300

    Update SEBACO_g08_transformer.py

--- SEBACO/SEBACO_g08_transformer.py
+++ SEBACO/SEBACO_g08_transformer.py
@@ -12,13 +12,27 @@ def rename_cols(df: DataFrame, map):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'sector': 'indicador'}),
-	rename_cols(map={'prop': 'valor'})
+	rename_cols(map={'prop': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100),
+	sort_values(how='ascending', by=['anio', 'indicador'])
 )
 #  PIPELINE_END
 
@@ -62,9 +76,39 @@ rename_cols(map={'sector': 'indicador'}),
 #   1   anio       24 non-null     int64  
 #   2   valor      24 non-null     float64
 #  
-#  |    | indicador                                                                                   |   anio |     valor |
-#  |---:|:--------------------------------------------------------------------------------------------|-------:|----------:|
-#  |  0 | Otros servicios (empresariales, relacionados con la salud humana y animal y comunicaciones) |   2015 | 0.0279136 |
+#  |    | indicador                                                                                   |   anio |   valor |
+#  |---:|:--------------------------------------------------------------------------------------------|-------:|--------:|
+#  |  0 | Otros servicios (empresariales, relacionados con la salud humana y animal y comunicaciones) |   2015 | 2.79136 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 24 entries, 0 to 23
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   indicador  24 non-null     object 
+#   1   anio       24 non-null     int64  
+#   2   valor      24 non-null     float64
+#  
+#  |    | indicador                                                                                   |   anio |   valor |
+#  |---:|:--------------------------------------------------------------------------------------------|-------:|--------:|
+#  |  0 | Otros servicios (empresariales, relacionados con la salud humana y animal y comunicaciones) |   2015 | 2.79136 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'indicador'])
+#  RangeIndex: 24 entries, 0 to 23
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   indicador  24 non-null     object 
+#   1   anio       24 non-null     int64  
+#   2   valor      24 non-null     float64
+#  
+#  |    | indicador                                                                                   |   anio |   valor |
+#  |---:|:--------------------------------------------------------------------------------------------|-------:|--------:|
+#  |  0 | Otros servicios (empresariales, relacionados con la salud humana y animal y comunicaciones) |   2015 | 2.79136 |
 #  
 #  ------------------------------
 #  
