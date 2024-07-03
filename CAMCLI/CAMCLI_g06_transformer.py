f9bb6a8a574b5414ac47b1bbf5be889c371ef655
Juan Manuel Dias <juamadias@gmail.com>
2024-07-03 11:00:05 -0300

    Update CAMCLI_g06_transformer.py

--- CAMCLI/CAMCLI_g06_transformer.py
+++ CAMCLI/CAMCLI_g06_transformer.py
@@ -11,13 +11,19 @@ def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indi
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', var_name='indicador'),
-	rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2'})
+	rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2'}),
+	query(condition="indicador == 'valor_en_porcent'")
 )
 #  PIPELINE_END
 
@@ -33,9 +39,9 @@ wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', v
 #   3   valor_en_mtco2e   15 non-null     float64
 #   4   valor_en_porcent  15 non-null     float64
 #  
-#  |    | sector   |   anio | subsector                |   valor_en_mtco2e |   valor_en_porcent |
-#  |---:|:---------|-------:|:-------------------------|------------------:|-------------------:|
-#  |  0 | Energía  |   2018 | Industrias de la energía |            58.627 |            16.0231 |
+#  |    | sector   |   anio | subsector                        |   valor_en_mtco2e |   valor_en_porcent |
+#  |---:|:---------|-------:|:---------------------------------|------------------:|-------------------:|
+#  |  0 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa |           4.52477 |            1.23665 |
 #  
 #  ------------------------------
 #  
@@ -50,9 +56,9 @@ wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', v
 #   3   indicador  30 non-null     object 
 #   4   valor      30 non-null     float64
 #  
-#  |    | sector   |   anio | subsector                | indicador       |   valor |
-#  |---:|:---------|-------:|:-------------------------|:----------------|--------:|
-#  |  0 | Energía  |   2018 | Industrias de la energía | valor_en_mtco2e |  58.627 |
+#  |    | sector   |   anio | subsector                        | indicador       |   valor |
+#  |---:|:---------|-------:|:---------------------------------|:----------------|--------:|
+#  |  0 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa | valor_en_mtco2e | 4.52477 |
 #  
 #  ------------------------------
 #  
@@ -67,9 +73,26 @@ wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', v
 #   3   indicador  30 non-null     object 
 #   4   valor      30 non-null     float64
 #  
-#  |    | nivel1   |   anio | nivel2                   | indicador       |   valor |
-#  |---:|:---------|-------:|:-------------------------|:----------------|--------:|
-#  |  0 | Energía  |   2018 | Industrias de la energía | valor_en_mtco2e |  58.627 |
+#  |    | nivel1   |   anio | nivel2                           | indicador       |   valor |
+#  |---:|:---------|-------:|:---------------------------------|:----------------|--------:|
+#  |  0 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa | valor_en_mtco2e | 4.52477 |
+#  
+#  ------------------------------
+#  
+#  query(condition="indicador == 'valor_en_porcent'")
+#  Index: 15 entries, 15 to 29
+#  Data columns (total 5 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   nivel1     15 non-null     object 
+#   1   anio       15 non-null     int64  
+#   2   nivel2     15 non-null     object 
+#   3   indicador  15 non-null     object 
+#   4   valor      15 non-null     float64
+#  
+#  |    | nivel1   |   anio | nivel2                           | indicador        |   valor |
+#  |---:|:---------|-------:|:---------------------------------|:-----------------|--------:|
+#  | 15 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa | valor_en_porcent | 1.23665 |
 #  
 #  ------------------------------
 #  
