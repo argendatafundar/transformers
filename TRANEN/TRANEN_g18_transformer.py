8b3b85fe983032f13b17d1e8247ecf5bbb076a29
jgjuara <jg.juara@gmail.com>
2024-07-03 15:59:40 -0300

    Update TRANEN_g18_transformer.py

--- TRANEN/TRANEN_g18_transformer.py
+++ TRANEN/TRANEN_g18_transformer.py
@@ -17,6 +17,16 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def drop_na(df:DataFrame, cols:list):
+    return df.dropna(subset=cols)
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    return df.sort_values(by=by, ascending=how == 'ascending')
 #  DEFINITIONS_END
 
 
@@ -24,7 +34,9 @@ def rename_cols(df: DataFrame, map):
 pipeline = chain(
 replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 	replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
-	rename_cols(map={'valor_en_gco2_por_kwh': 'valor', 'iso3': 'geocodigo'})
+	rename_cols(map={'valor_en_gco2_por_kwh': 'valor', 'iso3': 'geocodigo'}),
+	drop_na(cols=['valor']),
+	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
 
@@ -40,7 +52,7 @@ replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 #  
 #  |    |   anio | iso3   |   valor_en_gco2_por_kwh |
 #  |---:|-------:|:-------|------------------------:|
-#  |  0 |   2000 | AFG    |                     250 |
+#  |  0 |   1990 | GBR    |                  705.02 |
 #  
 #  ------------------------------
 #  
@@ -55,7 +67,7 @@ replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 #  
 #  |    |   anio | iso3   |   valor_en_gco2_por_kwh |
 #  |---:|-------:|:-------|------------------------:|
-#  |  0 |   2000 | AFG    |                     250 |
+#  |  0 |   1990 | GBR    |                  705.02 |
 #  
 #  ------------------------------
 #  
@@ -70,7 +82,7 @@ replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 #  
 #  |    |   anio | iso3   |   valor_en_gco2_por_kwh |
 #  |---:|-------:|:-------|------------------------:|
-#  |  0 |   2000 | AFG    |                     250 |
+#  |  0 |   1990 | GBR    |                  705.02 |
 #  
 #  ------------------------------
 #  
@@ -85,7 +97,37 @@ replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 #  
 #  |    |   anio | geocodigo   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2000 | AFG         |     250 |
+#  |  0 |   1990 | GBR         |  705.02 |
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
+#  Index: 5342 entries, 0 to 7342
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       5342 non-null   int64  
+#   1   geocodigo  5342 non-null   object 
+#   2   valor      5342 non-null   float64
+#  
+#  |    |   anio | geocodigo   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1990 | GBR         |  705.02 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  Index: 5342 entries, 340 to 2855
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       5342 non-null   int64  
+#   1   geocodigo  5342 non-null   object 
+#   2   valor      5342 non-null   float64
+#  
+#  |     |   anio | geocodigo   |   valor |
+#  |----:|-------:|:------------|--------:|
+#  | 340 |   1990 | AUT         | 249.848 |
 #  
 #  ------------------------------
 #  
