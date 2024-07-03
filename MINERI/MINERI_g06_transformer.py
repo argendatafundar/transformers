5cdfef5977193c1fd61a2fa8c004e1a52ecf2e2b
joangq <jgquiroga@dc.uba.ar>
2024-07-03 12:10:44 -0300

    Update MINERI_g06_transformer.py

--- MINERI/MINERI_g06_transformer.py
+++ MINERI/MINERI_g06_transformer.py
@@ -4,10 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
-    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -21,8 +17,7 @@ def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-wide_to_long(primary_keys=['anio', 'grupo_nuevo'], value_name='valor', var_name='indicador'),
-	rename_cols(map={'grupo_nuevo': 'serie'}),
+rename_cols(map={'serie': 'indicador', 'impo_grupo': 'valor'}),
 	mutiplicar_por_escalar(col='valor', k=1e-06)
 )
 #  PIPELINE_END
@@ -43,51 +38,33 @@ wide_to_long(primary_keys=['anio', 'grupo_nuevo'], value_name='valor', var_name=
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['anio', 'grupo_nuevo'], value_name='valor', var_name='indicador')
+#  rename_cols(map={'serie': 'indicador', 'impo_grupo': 'valor'})
 #  RangeIndex: 145 entries, 0 to 144
-#  Data columns (total 4 columns):
+#  Data columns (total 3 columns):
 #   #   Column       Non-Null Count  Dtype  
 #  ---  ------       --------------  -----  
-#   0   anio         145 non-null    int64  
-#   1   grupo_nuevo  145 non-null    object 
-#   2   indicador    145 non-null    object 
-#   3   valor        145 non-null    float64
+#   0   grupo_nuevo  145 non-null    object 
+#   1   anio         145 non-null    int64  
+#   2   valor        145 non-null    float64
 #  
-#  |    |   anio | grupo_nuevo   | indicador   |       valor |
-#  |---:|-------:|:--------------|:------------|------------:|
-#  |  0 |   1994 | aluminio      | impo_grupo  | 8.39544e+06 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'grupo_nuevo': 'serie'})
-#  RangeIndex: 145 entries, 0 to 144
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       145 non-null    int64  
-#   1   serie      145 non-null    object 
-#   2   indicador  145 non-null    object 
-#   3   valor      145 non-null    float64
-#  
-#  |    |   anio | serie    | indicador   |   valor |
-#  |---:|-------:|:---------|:------------|--------:|
-#  |  0 |   1994 | aluminio | impo_grupo  | 8.39544 |
+#  |    | grupo_nuevo   |   anio |   valor |
+#  |---:|:--------------|-------:|--------:|
+#  |  0 | aluminio      |   1994 | 8.39544 |
 #  
 #  ------------------------------
 #  
 #  mutiplicar_por_escalar(col='valor', k=1e-06)
 #  RangeIndex: 145 entries, 0 to 144
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       145 non-null    int64  
-#   1   serie      145 non-null    object 
-#   2   indicador  145 non-null    object 
-#   3   valor      145 non-null    float64
+#  Data columns (total 3 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   grupo_nuevo  145 non-null    object 
+#   1   anio         145 non-null    int64  
+#   2   valor        145 non-null    float64
 #  
-#  |    |   anio | serie    | indicador   |   valor |
-#  |---:|-------:|:---------|:------------|--------:|
-#  |  0 |   1994 | aluminio | impo_grupo  | 8.39544 |
+#  |    | grupo_nuevo   |   anio |   valor |
+#  |---:|:--------------|-------:|--------:|
+#  |  0 | aluminio      |   1994 | 8.39544 |
 #  
 #  ------------------------------
 #  
