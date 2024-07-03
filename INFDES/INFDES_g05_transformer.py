9f6392cb9aeac5feb27ea2b5f1b96b1843d21c07
Juan Manuel Dias <juamadias@gmail.com>
2024-07-03 15:53:01 -0300

    Update INFDES_g05_transformer.py

--- INFDES/INFDES_g05_transformer.py
+++ INFDES/INFDES_g05_transformer.py
@@ -8,6 +8,10 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -17,57 +21,72 @@ def rename_cols(df: DataFrame, map):
 #  PIPELINE_START
 pipeline = chain(
 drop_col(col='pais', axis=1),
-	rename_cols(map={'iso3': 'geocodigo', 'serie': 'indicador'})
+	drop_col(col='serie', axis=1),
+	rename_cols(map={'iso3': 'geocodigo'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 330 entries, 0 to 329
+#  RangeIndex: 286 entries, 0 to 285
 #  Data columns (total 5 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   iso3    330 non-null    object 
-#   1   pais    330 non-null    object 
-#   2   anio    330 non-null    int64  
-#   3   serie   330 non-null    object 
-#   4   valor   330 non-null    float64
+#   0   iso3    286 non-null    object 
+#   1   pais    286 non-null    object 
+#   2   anio    286 non-null    int64  
+#   3   valor   286 non-null    float64
+#   4   serie   286 non-null    object 
 #  
-#  |    | iso3   | pais      |   anio | serie           |    valor |
-#  |---:|:-------|:----------|-------:|:----------------|---------:|
-#  |  0 | ARG    | Argentina |   2021 | Serie empalmada | 0.327077 |
+#  |    | iso3   | pais      |   anio |    valor | serie           |
+#  |---:|:-------|:----------|-------:|---------:|:----------------|
+#  |  0 | ARG    | Argentina |   1986 | 0.268756 | Serie empalmada |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='pais', axis=1)
-#  RangeIndex: 330 entries, 0 to 329
+#  RangeIndex: 286 entries, 0 to 285
 #  Data columns (total 4 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   iso3    330 non-null    object 
-#   1   anio    330 non-null    int64  
-#   2   serie   330 non-null    object 
-#   3   valor   330 non-null    float64
+#   0   iso3    286 non-null    object 
+#   1   anio    286 non-null    int64  
+#   2   valor   286 non-null    float64
+#   3   serie   286 non-null    object 
 #  
-#  |    | iso3   |   anio | serie           |    valor |
-#  |---:|:-------|-------:|:----------------|---------:|
-#  |  0 | ARG    |   2021 | Serie empalmada | 0.327077 |
+#  |    | iso3   |   anio |    valor | serie           |
+#  |---:|:-------|-------:|---------:|:----------------|
+#  |  0 | ARG    |   1986 | 0.268756 | Serie empalmada |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'serie': 'indicador'})
-#  RangeIndex: 330 entries, 0 to 329
-#  Data columns (total 4 columns):
+#  drop_col(col='serie', axis=1)
+#  RangeIndex: 286 entries, 0 to 285
+#  Data columns (total 3 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   iso3    286 non-null    object 
+#   1   anio    286 non-null    int64  
+#   2   valor   286 non-null    float64
+#  
+#  |    | iso3   |   anio |    valor |
+#  |---:|:-------|-------:|---------:|
+#  |  0 | ARG    |   1986 | 0.268756 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'iso3': 'geocodigo'})
+#  RangeIndex: 286 entries, 0 to 285
+#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  330 non-null    object 
-#   1   anio       330 non-null    int64  
-#   2   indicador  330 non-null    object 
-#   3   valor      330 non-null    float64
+#   0   geocodigo  286 non-null    object 
+#   1   anio       286 non-null    int64  
+#   2   valor      286 non-null    float64
 #  
-#  |    | geocodigo   |   anio | indicador       |    valor |
-#  |---:|:------------|-------:|:----------------|---------:|
-#  |  0 | ARG         |   2021 | Serie empalmada | 0.327077 |
+#  |    | geocodigo   |   anio |    valor |
+#  |---:|:------------|-------:|---------:|
+#  |  0 | ARG         |   1986 | 0.268756 |
 #  
 #  ------------------------------
 #  
