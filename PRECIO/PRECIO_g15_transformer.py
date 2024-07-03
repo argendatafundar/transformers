ce1236499707d362d90af61ee676520793500aaa
joangq <jgquiroga@dc.uba.ar>
2024-07-03 14:03:00 -0300

    Update PRECIO_g15_transformer.py

--- PRECIO/PRECIO_g15_transformer.py
+++ PRECIO/PRECIO_g15_transformer.py
@@ -7,12 +7,20 @@ from data_transformers import chain, transformer
 def rename_columns(df: DataFrame, **kwargs):
     df = df.rename(columns=kwargs)
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(rubro='categoria', precio_relativo='valor')
+rename_columns(rubro='categoria', precio_relativo='valor'),
+	sort_values(how='ascending', by=['anio', 'categoria'])
 )
 #  PIPELINE_END
 
@@ -44,6 +52,21 @@ rename_columns(rubro='categoria', precio_relativo='valor')
 #  |    |   anio | categoria           |   valor |
 #  |---:|-------:|:--------------------|--------:|
 #  |  0 |   2006 | Alimentos y bebidas | 97.3951 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'categoria'])
+#  RangeIndex: 778 entries, 0 to 777
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       778 non-null    int64  
+#   1   categoria  778 non-null    object 
+#   2   valor      778 non-null    float64
+#  
+#  |    |   anio | categoria           |   valor |
+#  |---:|-------:|:--------------------|--------:|
+#  |  0 |   1947 | Alimentos y bebidas | 92.3782 |
 #  
 #  ------------------------------
 #  
