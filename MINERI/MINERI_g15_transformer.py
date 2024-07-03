79709531b8d49afeb4ab1e511b035d48ce3f69c3
joangq <jgquiroga@dc.uba.ar>
2024-07-03 12:34:00 -0300

    Update MINERI_g15_transformer.py

--- MINERI/MINERI_g15_transformer.py
+++ MINERI/MINERI_g15_transformer.py
@@ -4,10 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
-    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
-
-@transformer.convert
 def fecha_to_trimestre(df, col, trimestre_map, input_format_pattern='(.*)_([0-9]{4})', extractor=['trimester', 'year'], trimestre_format='{year}-{trimester}-1'):
     from re import findall
 
@@ -22,13 +18,18 @@ def fecha_to_trimestre(df, col, trimestre_map, input_format_pattern='(.*)_([0-9]
     df[col] = df[col].map(get_trimestre)
 
     return df
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-wide_to_long(primary_keys=['fecha'], value_name='valor', var_name='indicador'),
-	fecha_to_trimestre(col='fecha', trimestre_map={'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'VI': 6}, input_format_pattern='(.*)_([0-9]{4})', extractor=['trimester', 'year'], trimestre_format='{year}-{trimester}-1')
+fecha_to_trimestre(col='fecha', trimestre_map={'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'VI': 4}, input_format_pattern='(.*)_([0-9]{4})', extractor=['trimester', 'year'], trimestre_format='{year}-{trimester}'),
+	rename_cols(map={'fecha': 'aniotrim', 'tasa_feminizacion': 'valor'})
 )
 #  PIPELINE_END
 
@@ -47,33 +48,31 @@ wide_to_long(primary_keys=['fecha'], value_name='valor', var_name='indicador'),
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['fecha'], value_name='valor', var_name='indicador')
+#  fecha_to_trimestre(col='fecha', trimestre_map={'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'VI': 4}, input_format_pattern='(.*)_([0-9]{4})', extractor=['trimester', 'year'], trimestre_format='{year}-{trimester}')
 #  RangeIndex: 107 entries, 0 to 106
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   fecha      107 non-null    object 
-#   1   indicador  107 non-null    object 
-#   2   valor      107 non-null    float64
+#  Data columns (total 2 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   fecha              107 non-null    object 
+#   1   tasa_feminizacion  107 non-null    float64
 #  
-#  |    | fecha    | indicador         |   valor |
-#  |---:|:---------|:------------------|--------:|
-#  |  0 | 1996-1-1 | tasa_feminizacion | 6.62948 |
+#  |    | fecha   |   tasa_feminizacion |
+#  |---:|:--------|--------------------:|
+#  |  0 | 1996-1  |             6.62948 |
 #  
 #  ------------------------------
 #  
-#  fecha_to_trimestre(col='fecha', trimestre_map={'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'VI': 6}, input_format_pattern='(.*)_([0-9]{4})', extractor=['trimester', 'year'], trimestre_format='{year}-{trimester}-1')
+#  rename_cols(map={'fecha': 'aniotrim', 'tasa_feminizacion': 'valor'})
 #  RangeIndex: 107 entries, 0 to 106
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   fecha      107 non-null    object 
-#   1   indicador  107 non-null    object 
-#   2   valor      107 non-null    float64
+#  Data columns (total 2 columns):
+#   #   Column    Non-Null Count  Dtype  
+#  ---  ------    --------------  -----  
+#   0   aniotrim  107 non-null    object 
+#   1   valor     107 non-null    float64
 #  
-#  |    | fecha    | indicador         |   valor |
-#  |---:|:---------|:------------------|--------:|
-#  |  0 | 1996-1-1 | tasa_feminizacion | 6.62948 |
+#  |    | aniotrim   |   valor |
+#  |---:|:-----------|--------:|
+#  |  0 | 1996-1     | 6.62948 |
 #  
 #  ------------------------------
 #  
