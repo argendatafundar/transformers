71b2fe4416f233c86f7cd735ef1119b52137836d
jgjuara <jg.juara@gmail.com>
2024-07-04 13:13:04 -0300

    Update SEBACO_g16_transformer.py

--- SEBACO/SEBACO_g16_transformer.py
+++ SEBACO/SEBACO_g16_transformer.py
@@ -4,15 +4,35 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(sector='categoria', brecha='valor', periodo='anio')
+rename_cols(map={'periodo': 'anio', 'sector': 'categoria', 'brecha': 'valor'}),
+	sort_values(how='ascending', by=['anio', 'categoria']),
+	mutiplicar_por_escalar(col='valor', k=100),
+	replace_value(col='categoria', curr_value='Promedio economia', new_value='Promedio economía')
 )
 #  PIPELINE_END
 
@@ -32,7 +52,7 @@ rename_columns(sector='categoria', brecha='valor', periodo='anio')
 #  
 #  ------------------------------
 #  
-#  rename_columns(sector='categoria', brecha='valor', periodo='anio')
+#  rename_cols(map={'periodo': 'anio', 'sector': 'categoria', 'brecha': 'valor'})
 #  RangeIndex: 54 entries, 0 to 53
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -44,6 +64,51 @@ rename_columns(sector='categoria', brecha='valor', periodo='anio')
 #  |    |   anio |    valor | categoria         |
 #  |---:|-------:|---------:|:------------------|
 #  |  0 |   1996 | 0.237825 | Promedio economía |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'categoria'])
+#  RangeIndex: 54 entries, 0 to 53
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       54 non-null     int64  
+#   1   valor      54 non-null     float64
+#   2   categoria  54 non-null     object 
+#  
+#  |    |   anio |   valor | categoria         |
+#  |---:|-------:|--------:|:------------------|
+#  |  0 |   1996 | 23.7825 | Promedio economía |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 54 entries, 0 to 53
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       54 non-null     int64  
+#   1   valor      54 non-null     float64
+#   2   categoria  54 non-null     object 
+#  
+#  |    |   anio |   valor | categoria         |
+#  |---:|-------:|--------:|:------------------|
+#  |  0 |   1996 | 23.7825 | Promedio economía |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='Promedio economia', new_value='Promedio economía')
+#  RangeIndex: 54 entries, 0 to 53
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       54 non-null     int64  
+#   1   valor      54 non-null     float64
+#   2   categoria  54 non-null     object 
+#  
+#  |    |   anio |   valor | categoria         |
+#  |---:|-------:|--------:|:------------------|
+#  |  0 |   1996 | 23.7825 | Promedio economía |
 #  
 #  ------------------------------
 #  
