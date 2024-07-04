094ad059b09418d2db7ad73b679bf9962f45dd06
jgjuara <jg.juara@gmail.com>
2024-07-04 11:22:23 -0300

    Update SEBACO_g09_transformer.py

--- SEBACO/SEBACO_g09_transformer.py
+++ SEBACO/SEBACO_g09_transformer.py
@@ -12,13 +12,27 @@ def rename_cols(df: DataFrame, map):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'rama': 'indicador'}),
-	rename_cols(map={'prop_rama': 'valor'})
+	rename_cols(map={'prop_rama': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100),
+	sort_values(how='ascending', by=['anio', 'indicador'])
 )
 #  PIPELINE_END
 
@@ -34,7 +48,7 @@ rename_cols(map={'rama': 'indicador'}),
 #  
 #  |    | rama                       |   anio |   prop_rama |
 #  |---:|:---------------------------|-------:|------------:|
-#  |  0 | Investigaci髇 y desarrollo |   1996 |   0.0181982 |
+#  |  0 | Investigaci贸n y desarrollo |   1996 |   0.0181982 |
 #  
 #  ------------------------------
 #  
@@ -49,7 +63,7 @@ rename_cols(map={'rama': 'indicador'}),
 #  
 #  |    | indicador                  |   anio |   prop_rama |
 #  |---:|:---------------------------|-------:|------------:|
-#  |  0 | Investigaci髇 y desarrollo |   1996 |   0.0181982 |
+#  |  0 | Investigaci贸n y desarrollo |   1996 |   0.0181982 |
 #  
 #  ------------------------------
 #  
@@ -62,9 +76,39 @@ rename_cols(map={'rama': 'indicador'}),
 #   1   anio       162 non-null    int64  
 #   2   valor      162 non-null    float64
 #  
-#  |    | indicador                  |   anio |     valor |
-#  |---:|:---------------------------|-------:|----------:|
-#  |  0 | Investigaci髇 y desarrollo |   1996 | 0.0181982 |
+#  |    | indicador                  |   anio |   valor |
+#  |---:|:---------------------------|-------:|--------:|
+#  |  0 | Investigaci贸n y desarrollo |   1996 | 1.81982 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 162 entries, 0 to 161
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   indicador  162 non-null    object 
+#   1   anio       162 non-null    int64  
+#   2   valor      162 non-null    float64
+#  
+#  |    | indicador                  |   anio |   valor |
+#  |---:|:---------------------------|-------:|--------:|
+#  |  0 | Investigaci贸n y desarrollo |   1996 | 1.81982 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'indicador'])
+#  RangeIndex: 162 entries, 0 to 161
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   indicador  162 non-null    object 
+#   1   anio       162 non-null    int64  
+#   2   valor      162 non-null    float64
+#  
+#  |    | indicador                  |   anio |   valor |
+#  |---:|:---------------------------|-------:|--------:|
+#  |  0 | Investigaci贸n y desarrollo |   1996 | 1.81982 |
 #  
 #  ------------------------------
 #  
