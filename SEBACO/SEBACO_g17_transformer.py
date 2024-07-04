b4a3021394043ae6f0c7bf4dffae0b758a1ce4cc
jgjuara <jg.juara@gmail.com>
2024-07-04 13:14:44 -0300

    Update SEBACO_g17_transformer.py

--- SEBACO/SEBACO_g17_transformer.py
+++ SEBACO/SEBACO_g17_transformer.py
@@ -4,15 +4,29 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(sector='categoria', prop_teletrabajo='valor')
+rename_cols(map={'sector': 'categoria', 'prop_teletrabajo': 'valor'}),
+	sort_values(how='ascending', by=['anio', 'categoria']),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -32,7 +46,7 @@ rename_columns(sector='categoria', prop_teletrabajo='valor')
 #  
 #  ------------------------------
 #  
-#  rename_columns(sector='categoria', prop_teletrabajo='valor')
+#  rename_cols(map={'sector': 'categoria', 'prop_teletrabajo': 'valor'})
 #  RangeIndex: 21 entries, 0 to 20
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -44,6 +58,36 @@ rename_columns(sector='categoria', prop_teletrabajo='valor')
 #  |    |   anio | categoria   |    valor |
 #  |---:|-------:|:------------|---------:|
 #  |  0 |   2016 | SBC         | 0.113657 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'categoria'])
+#  RangeIndex: 21 entries, 0 to 20
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       21 non-null     int64  
+#   1   categoria  21 non-null     object 
+#   2   valor      21 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   2016 | SBC         | 11.3657 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 21 entries, 0 to 20
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       21 non-null     int64  
+#   1   categoria  21 non-null     object 
+#   2   valor      21 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   2016 | SBC         | 11.3657 |
 #  
 #  ------------------------------
 #  
