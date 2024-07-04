82820689e046b73a22da60c817e68df880b4f6e8
jgjuara <jg.juara@gmail.com>
2024-07-04 13:18:23 -0300

    Update SEBACO_g20_transformer.py

--- SEBACO/SEBACO_g20_transformer.py
+++ SEBACO/SEBACO_g20_transformer.py
@@ -4,15 +4,29 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(descripcion='indicador', prop_sobre_sbc_expo='valor')
+rename_cols(map={'descripcion': 'indicador', 'prop_sobre_sbc_expo': 'valor'}),
+	sort_values(how='ascending', by=['anio', 'indicador']),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -32,7 +46,7 @@ rename_columns(descripcion='indicador', prop_sobre_sbc_expo='valor')
 #  
 #  ------------------------------
 #  
-#  rename_columns(descripcion='indicador', prop_sobre_sbc_expo='valor')
+#  rename_cols(map={'descripcion': 'indicador', 'prop_sobre_sbc_expo': 'valor'})
 #  RangeIndex: 102 entries, 0 to 101
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -44,6 +58,36 @@ rename_columns(descripcion='indicador', prop_sobre_sbc_expo='valor')
 #  |    |   anio | indicador             |     valor |
 #  |---:|-------:|:----------------------|----------:|
 #  |  0 |   2006 | Propiedad intelectual | 0.0319314 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'indicador'])
+#  RangeIndex: 102 entries, 0 to 101
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       102 non-null    int64  
+#   1   indicador  102 non-null    object 
+#   2   valor      102 non-null    float64
+#  
+#  |    |   anio | indicador                  |   valor |
+#  |---:|-------:|:---------------------------|--------:|
+#  |  0 |   2006 | Investigación y desarrollo | 4.89056 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 102 entries, 0 to 101
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       102 non-null    int64  
+#   1   indicador  102 non-null    object 
+#   2   valor      102 non-null    float64
+#  
+#  |    |   anio | indicador                  |   valor |
+#  |---:|-------:|:---------------------------|--------:|
+#  |  0 |   2006 | Investigación y desarrollo | 4.89056 |
 #  
 #  ------------------------------
 #  
