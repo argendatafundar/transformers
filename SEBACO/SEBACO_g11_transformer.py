d46874cd48aae7e5d3dfb7954cfe1b60407b87ac
jgjuara <jg.juara@gmail.com>
2024-07-04 11:52:37 -0300

    Update SEBACO_g11_transformer.py

--- SEBACO/SEBACO_g11_transformer.py
+++ SEBACO/SEBACO_g11_transformer.py
@@ -4,15 +4,69 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def resub(df: DataFrame, col: str, pattern: str, replace: str):
+    # replace regex pattern in column
+    
+    df[col] = df[col].str.replace(pattern, replace)
+    return df
+
+@transformer.convert
+def resub(df: DataFrame, col: str, pattern: str, replace: str):
+    # replace regex pattern in column
+    
+    df[col] = df[col].str.replace(pattern, replace)
+    return df
+
+@transformer.convert
+def resub(df: DataFrame, col: str, pattern: str, replace: str):
+    # replace regex pattern in column
+    
+    df[col] = df[col].str.replace(pattern, replace)
+    return df
+
+@transformer.convert
+def resub(df: DataFrame, col: str, pattern: str, replace: str):
+    # replace regex pattern in column
+    
+    df[col] = df[col].str.replace(pattern, replace)
+    return df
+
+@transformer.convert
+def resub(df: DataFrame, col: str, pattern: str, replace: str):
+    # replace regex pattern in column
+    
+    df[col] = df[col].str.replace(pattern, replace)
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(sector='categoria', estado_ocupacional='indicador', prop_ocupados='valor')
+rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop_ocupados': 'valor'}),
+	mutiplicar_por_escalar(col='valor', k=100),
+	resub(col='categoria', pattern='a.', replace=''),
+	resub(col='categoria', pattern='b.', replace=''),
+	resub(col='categoria', pattern='c.', replace=''),
+	resub(col='categoria', pattern='d.', replace=''),
+	resub(col='categoria', pattern='e.', replace=''),
+	sort_values(how='ascending', by=['categoria', 'indicador'])
 )
 #  PIPELINE_END
 
@@ -32,7 +86,7 @@ rename_columns(sector='categoria', estado_ocupacional='indicador', prop_ocupados
 #  
 #  ------------------------------
 #  
-#  rename_columns(sector='categoria', estado_ocupacional='indicador', prop_ocupados='valor')
+#  rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop_ocupados': 'valor'})
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -41,9 +95,114 @@ rename_columns(sector='categoria', estado_ocupacional='indicador', prop_ocupados
 #   1   indicador  15 non-null     object 
 #   2   valor      15 non-null     float64
 #  
-#  |    | categoria                                            | indicador                |    valor |
-#  |---:|:-----------------------------------------------------|:-------------------------|---------:|
-#  |  0 | a. Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 0.146243 |
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  resub(col='categoria', pattern='a.', replace='')
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  resub(col='categoria', pattern='b.', replace='')
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  resub(col='categoria', pattern='c.', replace='')
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  resub(col='categoria', pattern='d.', replace='')
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  resub(col='categoria', pattern='e.', replace='')
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['categoria', 'indicador'])
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  15 non-null     object 
+#   1   indicador  15 non-null     object 
+#   2   valor      15 non-null     float64
+#  
+#  |    | categoria                                         | indicador                |   valor |
+#  |---:|:--------------------------------------------------|:-------------------------|--------:|
+#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
 #  
 #  ------------------------------
 #  
