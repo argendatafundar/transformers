6df380bd076f5a70c46acfa96d633c6467cbcca5
jgjuara <jg.juara@gmail.com>
2024-07-05 15:54:42 -0300

    Update INFDES_g14_transformer.py

--- INFDES/INFDES_g14_transformer.py
+++ INFDES/INFDES_g14_transformer.py
@@ -9,6 +9,13 @@ def query(df: DataFrame, condition: str):
     return df
 
 @transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
@@ -21,9 +28,10 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='anio == 2023'),
-	drop_col(col='anio', axis=1),
-	rename_cols(map={'rango_edad': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
+query(condition='anio == anio.max()'),
+	sort_values(how='ascending', by=['rango_edad', 'sexo']),
+	drop_col(col=['anio', 'rango_edad'], axis=1),
+	rename_cols(map={'rango_edad_desc': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
 )
 #  PIPELINE_END
 
@@ -45,7 +53,7 @@ query(condition='anio == 2023'),
 #  
 #  ------------------------------
 #  
-#  query(condition='anio == 2023')
+#  query(condition='anio == anio.max()')
 #  Index: 8 entries, 56 to 63
 #  Data columns (total 5 columns):
 #   #   Column             Non-Null Count  Dtype  
@@ -62,35 +70,50 @@ query(condition='anio == 2023'),
 #  
 #  ------------------------------
 #  
-#  drop_col(col='anio', axis=1)
-#  Index: 8 entries, 56 to 63
-#  Data columns (total 4 columns):
+#  sort_values(how='ascending', by=['rango_edad', 'sexo'])
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 5 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   rango_edad         8 non-null      int64  
-#   1   rango_edad_desc    8 non-null      object 
-#   2   sexo               8 non-null      object 
-#   3   tasa_desocupacion  8 non-null      float64
+#   0   anio               8 non-null      int64  
+#   1   rango_edad         8 non-null      int64  
+#   2   rango_edad_desc    8 non-null      object 
+#   3   sexo               8 non-null      object 
+#   4   tasa_desocupacion  8 non-null      float64
 #  
-#  |    |   rango_edad | rango_edad_desc   | sexo    |   tasa_desocupacion |
-#  |---:|-------------:|:------------------|:--------|--------------------:|
-#  | 56 |            1 | Hasta 30          | Mujeres |            0.124757 |
+#  |    |   anio |   rango_edad | rango_edad_desc   | sexo    |   tasa_desocupacion |
+#  |---:|-------:|-------------:|:------------------|:--------|--------------------:|
+#  |  0 |   2023 |            1 | Hasta 30          | Mujeres |            0.124757 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'rango_edad': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
-#  Index: 8 entries, 56 to 63
-#  Data columns (total 4 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   categoria        8 non-null      int64  
-#   1   rango_edad_desc  8 non-null      object 
-#   2   indicador        8 non-null      object 
-#   3   valor            8 non-null      float64
+#  drop_col(col=['anio', 'rango_edad'], axis=1)
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 3 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   rango_edad_desc    8 non-null      object 
+#   1   sexo               8 non-null      object 
+#   2   tasa_desocupacion  8 non-null      float64
 #  
-#  |    |   categoria | rango_edad_desc   | indicador   |    valor |
-#  |---:|------------:|:------------------|:------------|---------:|
-#  | 56 |           1 | Hasta 30          | Mujeres     | 0.124757 |
+#  |    | rango_edad_desc   | sexo    |   tasa_desocupacion |
+#  |---:|:------------------|:--------|--------------------:|
+#  |  0 | Hasta 30          | Mujeres |            0.124757 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'rango_edad_desc': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  8 non-null      object 
+#   1   indicador  8 non-null      object 
+#   2   valor      8 non-null      float64
+#  
+#  |    | categoria   | indicador   |    valor |
+#  |---:|:------------|:------------|---------:|
+#  |  0 | Hasta 30    | Mujeres     | 0.124757 |
 #  
 #  ------------------------------
 #  
