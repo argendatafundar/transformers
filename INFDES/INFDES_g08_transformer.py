97268455e25c54e0d9062acc66771d388b0d77e3
jgjuara <jg.juara@gmail.com>
2024-07-05 15:21:56 -0300

    Update INFDES_g08_transformer.py

--- INFDES/INFDES_g08_transformer.py
+++ INFDES/INFDES_g08_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'apertura_sexo': 'indicador'})
+rename_cols(map={'apertura_sexo': 'indicador'}),
+	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -41,9 +47,24 @@ rename_cols(map={'apertura_sexo': 'indicador'})
 #   1   indicador  105 non-null    object 
 #   2   valor      105 non-null    float64
 #  
-#  |    |   anio | indicador   |    valor |
-#  |---:|-------:|:------------|---------:|
-#  |  0 |   1986 | Mujer       | 0.299352 |
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1986 | Mujer       | 29.9352 |
+#  
+#  ------------------------------
+#  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 105 entries, 0 to 104
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       105 non-null    int64  
+#   1   indicador  105 non-null    object 
+#   2   valor      105 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1986 | Mujer       | 29.9352 |
 #  
 #  ------------------------------
 #  
