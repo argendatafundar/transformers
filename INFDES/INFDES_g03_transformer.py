80a10c26106a660daa8e5e5a4c784bd8700b4cf2
jgjuara <jg.juara@gmail.com>
2024-07-05 14:45:44 -0300

    Update INFDES_g03_transformer.py

--- INFDES/INFDES_g03_transformer.py
+++ INFDES/INFDES_g03_transformer.py
@@ -4,13 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def latest_year(df, by='anio'):
-    latest_year = df[by].max()
-    df = df.query(f'{by} == {latest_year}')
-    df = df.drop(columns = by)
-    return df
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -26,16 +19,21 @@ def drop_col(df: DataFrame, col, axis=1):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-latest_year(by='anio'),
-	rename_cols(map={'iso3': 'geocodigo', 'cat_ocup_detalle': 'indicador'}),
+rename_cols(map={'iso3': 'geocodigo', 'cat_ocup_detalle': 'indicador'}),
 	drop_col(col='formal_def_productiva', axis=1),
 	drop_col(col='cat_ocup_cod', axis=1),
-	drop_col(col='pais', axis=1)
+	drop_col(col='pais', axis=1),
+	query(condition='anio == anio.max() & indicador != "Total formal"')
 )
 #  PIPELINE_END
 
@@ -59,87 +57,89 @@ latest_year(by='anio'),
 #  
 #  ------------------------------
 #  
-#  latest_year(by='anio')
-#  Index: 80 entries, 0 to 119
-#  Data columns (total 6 columns):
-#   #   Column                 Non-Null Count  Dtype  
-#  ---  ------                 --------------  -----  
-#   0   iso3                   80 non-null     object 
-#   1   pais                   80 non-null     object 
-#   2   formal_def_productiva  80 non-null     object 
-#   3   cat_ocup_cod           70 non-null     float64
-#   4   cat_ocup_detalle       80 non-null     object 
-#   5   valor                  80 non-null     float64
-#  
-#  |    | iso3   | pais      | formal_def_productiva   |   cat_ocup_cod | cat_ocup_detalle   |   valor |
-#  |---:|:-------|:----------|:------------------------|---------------:|:-------------------|--------:|
-#  |  0 | ARG    | Argentina | Formal                  |              1 | Empleadores        |     3.7 |
-#  
-#  ------------------------------
-#  
 #  rename_cols(map={'iso3': 'geocodigo', 'cat_ocup_detalle': 'indicador'})
-#  Index: 80 entries, 0 to 119
-#  Data columns (total 6 columns):
+#  RangeIndex: 120 entries, 0 to 119
+#  Data columns (total 7 columns):
 #   #   Column                 Non-Null Count  Dtype  
 #  ---  ------                 --------------  -----  
-#   0   geocodigo              80 non-null     object 
-#   1   pais                   80 non-null     object 
-#   2   formal_def_productiva  80 non-null     object 
-#   3   cat_ocup_cod           70 non-null     float64
-#   4   indicador              80 non-null     object 
-#   5   valor                  80 non-null     float64
+#   0   geocodigo              120 non-null    object 
+#   1   pais                   120 non-null    object 
+#   2   anio                   120 non-null    int64  
+#   3   formal_def_productiva  120 non-null    object 
+#   4   cat_ocup_cod           105 non-null    float64
+#   5   indicador              120 non-null    object 
+#   6   valor                  120 non-null    float64
 #  
-#  |    | geocodigo   | pais      | formal_def_productiva   |   cat_ocup_cod | indicador   |   valor |
-#  |---:|:------------|:----------|:------------------------|---------------:|:------------|--------:|
-#  |  0 | ARG         | Argentina | Formal                  |              1 | Empleadores |     3.7 |
+#  |    | geocodigo   | pais      |   anio | formal_def_productiva   |   cat_ocup_cod | indicador   |   valor |
+#  |---:|:------------|:----------|-------:|:------------------------|---------------:|:------------|--------:|
+#  |  0 | ARG         | Argentina |   2022 | Formal                  |              1 | Empleadores |     3.7 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='formal_def_productiva', axis=1)
-#  Index: 80 entries, 0 to 119
-#  Data columns (total 5 columns):
+#  RangeIndex: 120 entries, 0 to 119
+#  Data columns (total 6 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   geocodigo     80 non-null     object 
-#   1   pais          80 non-null     object 
-#   2   cat_ocup_cod  70 non-null     float64
-#   3   indicador     80 non-null     object 
-#   4   valor         80 non-null     float64
+#   0   geocodigo     120 non-null    object 
+#   1   pais          120 non-null    object 
+#   2   anio          120 non-null    int64  
+#   3   cat_ocup_cod  105 non-null    float64
+#   4   indicador     120 non-null    object 
+#   5   valor         120 non-null    float64
 #  
-#  |    | geocodigo   | pais      |   cat_ocup_cod | indicador   |   valor |
-#  |---:|:------------|:----------|---------------:|:------------|--------:|
-#  |  0 | ARG         | Argentina |              1 | Empleadores |     3.7 |
+#  |    | geocodigo   | pais      |   anio |   cat_ocup_cod | indicador   |   valor |
+#  |---:|:------------|:----------|-------:|---------------:|:------------|--------:|
+#  |  0 | ARG         | Argentina |   2022 |              1 | Empleadores |     3.7 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='cat_ocup_cod', axis=1)
-#  Index: 80 entries, 0 to 119
-#  Data columns (total 4 columns):
+#  RangeIndex: 120 entries, 0 to 119
+#  Data columns (total 5 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  80 non-null     object 
-#   1   pais       80 non-null     object 
-#   2   indicador  80 non-null     object 
-#   3   valor      80 non-null     float64
+#   0   geocodigo  120 non-null    object 
+#   1   pais       120 non-null    object 
+#   2   anio       120 non-null    int64  
+#   3   indicador  120 non-null    object 
+#   4   valor      120 non-null    float64
 #  
-#  |    | geocodigo   | pais      | indicador   |   valor |
-#  |---:|:------------|:----------|:------------|--------:|
-#  |  0 | ARG         | Argentina | Empleadores |     3.7 |
+#  |    | geocodigo   | pais      |   anio | indicador   |   valor |
+#  |---:|:------------|:----------|-------:|:------------|--------:|
+#  |  0 | ARG         | Argentina |   2022 | Empleadores |     3.7 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='pais', axis=1)
-#  Index: 80 entries, 0 to 119
-#  Data columns (total 3 columns):
+#  RangeIndex: 120 entries, 0 to 119
+#  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  80 non-null     object 
-#   1   indicador  80 non-null     object 
-#   2   valor      80 non-null     float64
+#   0   geocodigo  120 non-null    object 
+#   1   anio       120 non-null    int64  
+#   2   indicador  120 non-null    object 
+#   3   valor      120 non-null    float64
 #  
-#  |    | geocodigo   | indicador   |   valor |
-#  |---:|:------------|:------------|--------:|
-#  |  0 | ARG         | Empleadores |     3.7 |
+#  |    | geocodigo   |   anio | indicador   |   valor |
+#  |---:|:------------|-------:|:------------|--------:|
+#  |  0 | ARG         |   2022 | Empleadores |     3.7 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio == anio.max() & indicador != "Total formal"')
+#  Index: 70 entries, 0 to 104
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  70 non-null     object 
+#   1   anio       70 non-null     int64  
+#   2   indicador  70 non-null     object 
+#   3   valor      70 non-null     float64
+#  
+#  |    | geocodigo   |   anio | indicador   |   valor |
+#  |---:|:------------|-------:|:------------|--------:|
+#  |  0 | ARG         |   2022 | Empleadores |     3.7 |
 #  
 #  ------------------------------
 #  
