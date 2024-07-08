7876c3e2bd25025bc299dedfad65725d5a1c0db6
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-08 13:07:39 -0300

    Update PRECIO_g04_transformer.py

--- PRECIO/PRECIO_g04_transformer.py
+++ PRECIO/PRECIO_g04_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_columns(df: DataFrame, **kwargs):
     df = df.rename(columns=kwargs)
     return df
+
+@transformer.convert
+def inputar(df, row):
+    from pandas import concat, DataFrame
+    return concat([df, DataFrame([row], columns=df.columns)])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(iso3='geocodigo', inflacion_prom_07_22='valor')
+rename_columns(iso3='geocodigo', inflacion_prom_07_22='valor'),
+	inputar(row=['ZWE', 87.09157143])
 )
 #  PIPELINE_END
 
@@ -38,6 +44,20 @@ rename_columns(iso3='geocodigo', inflacion_prom_07_22='valor')
 #  ---  ------     --------------  -----  
 #   0   geocodigo  132 non-null    object 
 #   1   valor      132 non-null    float64
+#  
+#  |    | geocodigo   |   valor |
+#  |---:|:------------|--------:|
+#  |  0 | AGO         | 16.9548 |
+#  
+#  ------------------------------
+#  
+#  inputar(row=['ZWE', 87.09157143])
+#  Index: 133 entries, 0 to 0
+#  Data columns (total 2 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  133 non-null    object 
+#   1   valor      133 non-null    float64
 #  
 #  |    | geocodigo   |   valor |
 #  |---:|:------------|--------:|
