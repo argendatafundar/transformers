f359b9d650f62ac1a82e5226fed472237185476d
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-08 15:35:34 -0300

    Update COMEXT_g01_transformer.py

--- COMEXT/COMEXT_g01_transformer.py
+++ COMEXT/COMEXT_g01_transformer.py
@@ -4,8 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def case_when_na(df: DataFrame, col_na:str, col_replace:str, col_name:str = None ): 
+    nan_rows = df[col_na].isna()
+    
+    new_col = col_name
+    if col_name is None: 
+        new_col = col_na
+
+    df[new_col] = df[col_na]    
+    df.loc[nan_rows, new_col] = df.loc[nan_rows, col_replace]
     return df
 
 @transformer.convert
@@ -16,49 +23,51 @@ def drop_col(df: DataFrame, col, axis=1):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'cantidades_exportacion_ferreres': 'valor'}),
-	drop_col(col='cantidades_exportacion_indec', axis=1)
+case_when_na(col_na='cantidades_exportacion_ferreres', col_replace='cantidades_exportacion_indec', col_name='valor'),
+	drop_col(col=['cantidades_exportacion_ferreres', 'cantidades_exportacion_indec'], axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 214 entries, 0 to 213
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column                           Non-Null Count  Dtype  
 #  ---  ------                           --------------  -----  
 #   0   anio                             214 non-null    int64  
 #   1   cantidades_exportacion_ferreres  209 non-null    float64
 #   2   cantidades_exportacion_indec     38 non-null     float64
+#   3   valor                            214 non-null    float64
 #  
-#  |    |   anio |   cantidades_exportacion_ferreres |   cantidades_exportacion_indec |
-#  |---:|-------:|----------------------------------:|-------------------------------:|
-#  |  0 |   1810 |                         0.0193818 |                            nan |
+#  |    |   anio |   cantidades_exportacion_ferreres |   cantidades_exportacion_indec |     valor |
+#  |---:|-------:|----------------------------------:|-------------------------------:|----------:|
+#  |  0 |   1810 |                         0.0193818 |                            nan | 0.0193818 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'cantidades_exportacion_ferreres': 'valor'})
+#  case_when_na(col_na='cantidades_exportacion_ferreres', col_replace='cantidades_exportacion_indec', col_name='valor')
 #  RangeIndex: 214 entries, 0 to 213
-#  Data columns (total 3 columns):
-#   #   Column                        Non-Null Count  Dtype  
-#  ---  ------                        --------------  -----  
-#   0   anio                          214 non-null    int64  
-#   1   valor                         209 non-null    float64
-#   2   cantidades_exportacion_indec  38 non-null     float64
+#  Data columns (total 4 columns):
+#   #   Column                           Non-Null Count  Dtype  
+#  ---  ------                           --------------  -----  
+#   0   anio                             214 non-null    int64  
+#   1   cantidades_exportacion_ferreres  209 non-null    float64
+#   2   cantidades_exportacion_indec     38 non-null     float64
+#   3   valor                            214 non-null    float64
 #  
-#  |    |   anio |     valor |   cantidades_exportacion_indec |
-#  |---:|-------:|----------:|-------------------------------:|
-#  |  0 |   1810 | 0.0193818 |                            nan |
+#  |    |   anio |   cantidades_exportacion_ferreres |   cantidades_exportacion_indec |     valor |
+#  |---:|-------:|----------------------------------:|-------------------------------:|----------:|
+#  |  0 |   1810 |                         0.0193818 |                            nan | 0.0193818 |
 #  
 #  ------------------------------
 #  
-#  drop_col(col='cantidades_exportacion_indec', axis=1)
+#  drop_col(col=['cantidades_exportacion_ferreres', 'cantidades_exportacion_indec'], axis=1)
 #  RangeIndex: 214 entries, 0 to 213
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
 #   0   anio    214 non-null    int64  
-#   1   valor   209 non-null    float64
+#   1   valor   214 non-null    float64
 #  
 #  |    |   anio |     valor |
 #  |---:|-------:|----------:|
