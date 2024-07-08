bb35d11287075c7c8f206cfe1a770e481317eb0d
Joan Gonzalez <jgquiroga@dc.uba.ar>
2024-07-08 17:03:10 -0300

    Update PRECIO_g14_transformer.py

--- PRECIO/PRECIO_g14_transformer.py
+++ PRECIO/PRECIO_g14_transformer.py
@@ -12,19 +12,13 @@ def rename_cols(df: DataFrame, map):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'rubro': 'indicador'}),
-	rename_cols(map={'precio_relativo': 'valor'}),
-	query(condition='nivel == 1')
+	rename_cols(map={'precio_relativo': 'valor'})
 )
 #  PIPELINE_END
 
@@ -73,23 +67,6 @@ rename_cols(map={'rubro': 'indicador'}),
 #   2   nivel      748 non-null    int64  
 #   3   indicador  748 non-null    object 
 #   4   valor      748 non-null    float64
-#  
-#  |    |   anio |   codigo |   nivel | indicador                          |   valor |
-#  |---:|-------:|---------:|--------:|:-----------------------------------|--------:|
-#  |  0 |   2013 |       01 |       1 | Alimentos y bebidas no alcohólicas |     100 |
-#  
-#  ------------------------------
-#  
-#  query(condition='nivel == 1')
-#  Index: 143 entries, 0 to 744
-#  Data columns (total 5 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       143 non-null    int64  
-#   1   codigo     143 non-null    object 
-#   2   nivel      143 non-null    int64  
-#   3   indicador  143 non-null    object 
-#   4   valor      143 non-null    float64
 #  
 #  |    |   anio |   codigo |   nivel | indicador                          |   valor |
 #  |---:|-------:|---------:|--------:|:-----------------------------------|--------:|
