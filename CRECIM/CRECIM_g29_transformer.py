a28a3f3d0b3c3f8cd61e40cdeea8109a2d2bf3d5
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-11 12:35:19 -0300

    Update CRECIM_g29_transformer.py

--- CRECIM/CRECIM_g29_transformer.py
+++ CRECIM/CRECIM_g29_transformer.py
@@ -4,6 +4,16 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
@@ -21,8 +31,24 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def drop_na(df:DataFrame, cols:list):
-    return df.dropna(subset=cols)
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 
 @transformer.convert
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
@@ -55,15 +81,20 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='continente_fundar', axis=1),
+replace_value(col='iso3', curr_value='WRL_MPD', new_value='WLD'),
+	query(condition="iso3.isin( ['USA','GBR','ARG','AUS','WRL_MDP','WEU_MPD'])"),
+	drop_col(col='continente_fundar', axis=1),
 	drop_col(col='es_agregacion', axis=1),
 	drop_col(col='pais_nombre', axis=1),
-	rename_cols(map={'iso3': 'geocodigo', 'pib_per_capita': 'valor'}),
-	drop_na(cols=['valor']),
-	replace_value(col='geocodigo', curr_value='WRL_MPD', new_value='WLD'),
-	replace_value(col='geocodigo', curr_value='YUG', new_value='SER'),
-	replace_value(col='geocodigo', curr_value='SUN', new_value='SVU'),
-	sort_values(how='ascending', by=['anio', 'geocodigo']),
+	rename_cols(map={'iso3': 'categoria', 'pib_per_capita': 'valor'}),
+	drop_na(col=['valor']),
+	replace_value(col='categoria', curr_value='WRL_MPD', new_value='Mundo'),
+	replace_value(col='categoria', curr_value='ARG', new_value='Argentina'),
+	replace_value(col='categoria', curr_value='AUS', new_value='Australia'),
+	replace_value(col='categoria', curr_value='GBR', new_value='Reino Unido'),
+	replace_value(col='categoria', curr_value='USA', new_value='Estados Unidos'),
+	replace_value(col='categoria', curr_value='WEU_MPD', new_value='Europa Occidental'),
+	sort_values(how='ascending', by=['anio', 'categoria']),
 	query(condition='anio >= 1900')
 )
 #  PIPELINE_END
@@ -87,156 +118,237 @@ drop_col(col='continente_fundar', axis=1),
 #  
 #  ------------------------------
 #  
-#  drop_col(col='continente_fundar', axis=1)
+#  replace_value(col='iso3', curr_value='WRL_MPD', new_value='WLD')
 #  RangeIndex: 21618 entries, 0 to 21617
+#  Data columns (total 6 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   iso3               21618 non-null  object 
+#   1   pais_nombre        21618 non-null  object 
+#   2   continente_fundar  21366 non-null  object 
+#   3   es_agregacion      21618 non-null  int64  
+#   4   anio               21618 non-null  int64  
+#   5   pib_per_capita     21586 non-null  float64
+#  
+#  |    | iso3   | pais_nombre   | continente_fundar   |   es_agregacion |   anio |   pib_per_capita |
+#  |---:|:-------|:--------------|:--------------------|----------------:|-------:|-----------------:|
+#  |  0 | AFG    | Afganistán    | Asia                |               0 |   1950 |             1156 |
+#  
+#  ------------------------------
+#  
+#  query(condition="iso3.isin( ['USA','GBR','ARG','AUS','WRL_MDP','WEU_MPD'])")
+#  Index: 1382 entries, 296 to 20893
+#  Data columns (total 6 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   iso3               1382 non-null   object 
+#   1   pais_nombre        1382 non-null   object 
+#   2   continente_fundar  1354 non-null   object 
+#   3   es_agregacion      1382 non-null   int64  
+#   4   anio               1382 non-null   int64  
+#   5   pib_per_capita     1382 non-null   float64
+#  
+#  |     | iso3   | pais_nombre   | continente_fundar   |   es_agregacion |   anio |   pib_per_capita |
+#  |----:|:-------|:--------------|:--------------------|----------------:|-------:|-----------------:|
+#  | 296 | ARG    | Argentina     | América del Sur     |               0 |   1800 |             1484 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='continente_fundar', axis=1)
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 5 columns):
 #   #   Column          Non-Null Count  Dtype  
 #  ---  ------          --------------  -----  
-#   0   iso3            21618 non-null  object 
-#   1   pais_nombre     21618 non-null  object 
-#   2   es_agregacion   21618 non-null  int64  
-#   3   anio            21618 non-null  int64  
-#   4   pib_per_capita  21586 non-null  float64
+#   0   iso3            1382 non-null   object 
+#   1   pais_nombre     1382 non-null   object 
+#   2   es_agregacion   1382 non-null   int64  
+#   3   anio            1382 non-null   int64  
+#   4   pib_per_capita  1382 non-null   float64
 #  
-#  |    | iso3   | pais_nombre   |   es_agregacion |   anio |   pib_per_capita |
-#  |---:|:-------|:--------------|----------------:|-------:|-----------------:|
-#  |  0 | AFG    | Afganistán    |               0 |   1950 |             1156 |
+#  |     | iso3   | pais_nombre   |   es_agregacion |   anio |   pib_per_capita |
+#  |----:|:-------|:--------------|----------------:|-------:|-----------------:|
+#  | 296 | ARG    | Argentina     |               0 |   1800 |             1484 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='es_agregacion', axis=1)
-#  RangeIndex: 21618 entries, 0 to 21617
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 4 columns):
 #   #   Column          Non-Null Count  Dtype  
 #  ---  ------          --------------  -----  
-#   0   iso3            21618 non-null  object 
-#   1   pais_nombre     21618 non-null  object 
-#   2   anio            21618 non-null  int64  
-#   3   pib_per_capita  21586 non-null  float64
+#   0   iso3            1382 non-null   object 
+#   1   pais_nombre     1382 non-null   object 
+#   2   anio            1382 non-null   int64  
+#   3   pib_per_capita  1382 non-null   float64
 #  
-#  |    | iso3   | pais_nombre   |   anio |   pib_per_capita |
-#  |---:|:-------|:--------------|-------:|-----------------:|
-#  |  0 | AFG    | Afganistán    |   1950 |             1156 |
+#  |     | iso3   | pais_nombre   |   anio |   pib_per_capita |
+#  |----:|:-------|:--------------|-------:|-----------------:|
+#  | 296 | ARG    | Argentina     |   1800 |             1484 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='pais_nombre', axis=1)
-#  RangeIndex: 21618 entries, 0 to 21617
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 3 columns):
 #   #   Column          Non-Null Count  Dtype  
 #  ---  ------          --------------  -----  
-#   0   iso3            21618 non-null  object 
-#   1   anio            21618 non-null  int64  
-#   2   pib_per_capita  21586 non-null  float64
+#   0   iso3            1382 non-null   object 
+#   1   anio            1382 non-null   int64  
+#   2   pib_per_capita  1382 non-null   float64
 #  
-#  |    | iso3   |   anio |   pib_per_capita |
-#  |---:|:-------|-------:|-----------------:|
-#  |  0 | AFG    |   1950 |             1156 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'pib_per_capita': 'valor'})
-#  RangeIndex: 21618 entries, 0 to 21617
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  21618 non-null  object 
-#   1   anio       21618 non-null  int64  
-#   2   valor      21586 non-null  float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFG         |   1950 |    1156 |
+#  |     | iso3   |   anio |   pib_per_capita |
+#  |----:|:-------|-------:|-----------------:|
+#  | 296 | ARG    |   1800 |             1484 |
 #  
 #  ------------------------------
 #  
-#  drop_na(cols=['valor'])
-#  Index: 21586 entries, 0 to 21617
+#  rename_cols(map={'iso3': 'categoria', 'pib_per_capita': 'valor'})
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  21586 non-null  object 
-#   1   anio       21586 non-null  int64  
-#   2   valor      21586 non-null  float64
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFG         |   1950 |    1156 |
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | ARG         |   1800 |    1484 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='WRL_MPD', new_value='WLD')
-#  Index: 21586 entries, 0 to 21617
+#  drop_na(col=['valor'])
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  21586 non-null  object 
-#   1   anio       21586 non-null  int64  
-#   2   valor      21586 non-null  float64
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFG         |   1950 |    1156 |
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | ARG         |   1800 |    1484 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='YUG', new_value='SER')
-#  Index: 21586 entries, 0 to 21617
+#  replace_value(col='categoria', curr_value='WRL_MPD', new_value='Mundo')
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  21586 non-null  object 
-#   1   anio       21586 non-null  int64  
-#   2   valor      21586 non-null  float64
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFG         |   1950 |    1156 |
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | ARG         |   1800 |    1484 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='SUN', new_value='SVU')
-#  Index: 21586 entries, 0 to 21617
+#  replace_value(col='categoria', curr_value='ARG', new_value='Argentina')
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  21586 non-null  object 
-#   1   anio       21586 non-null  int64  
-#   2   valor      21586 non-null  float64
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFG         |   1950 |    1156 |
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | Argentina   |   1800 |    1484 |
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'geocodigo'])
-#  RangeIndex: 21586 entries, 0 to 21585
+#  replace_value(col='categoria', curr_value='AUS', new_value='Australia')
+#  Index: 1382 entries, 296 to 20893
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  21586 non-null  object 
-#   1   anio       21586 non-null  int64  
-#   2   valor      21586 non-null  float64
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | Argentina   |   1800 |    1484 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='GBR', new_value='Reino Unido')
+#  Index: 1382 entries, 296 to 20893
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
+#  
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | Argentina   |   1800 |    1484 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='USA', new_value='Estados Unidos')
+#  Index: 1382 entries, 296 to 20893
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
+#  
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | Argentina   |   1800 |    1484 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='WEU_MPD', new_value='Europa Occidental')
+#  Index: 1382 entries, 296 to 20893
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
+#  
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 296 | Argentina   |   1800 |    1484 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'categoria'])
+#  RangeIndex: 1382 entries, 0 to 1381
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  1382 non-null   object 
+#   1   anio       1382 non-null   int64  
+#   2   valor      1382 non-null   float64
+#  
+#  |    | categoria   |   anio |   valor |
 #  |---:|:------------|-------:|--------:|
-#  |  0 | BEL         |      1 |     956 |
+#  |  0 | Reino Unido |   1000 |    1151 |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio >= 1900')
-#  Index: 14886 entries, 6700 to 21585
+#  Index: 512 entries, 870 to 1381
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  14886 non-null  object 
-#   1   anio       14886 non-null  int64  
-#   2   valor      14886 non-null  float64
+#   0   categoria  512 non-null    object 
+#   1   anio       512 non-null    int64  
+#   2   valor      512 non-null    float64
 #  
-#  |      | geocodigo   |   anio |   valor |
-#  |-----:|:------------|-------:|--------:|
-#  | 6700 | ALB         |   1900 |    1092 |
+#  |     | categoria   |   anio |   valor |
+#  |----:|:------------|-------:|--------:|
+#  | 870 | Argentina   |   1900 |    4583 |
 #  
 #  ------------------------------
 #  
