24734a81b2a863cf249e7806585ffbea06e1e67c
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-11 16:35:53 -0300

    Update AGROPE_g16_transformer.py

--- AGROPE/AGROPE_g16_transformer.py
+++ AGROPE/AGROPE_g16_transformer.py
@@ -38,6 +38,16 @@ def sort_values(df: DataFrame, how: str, by: list):
         raise ValueError('how must be either "ascending" or "descending"')
     
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
@@ -49,7 +59,9 @@ replace_value(col='iso3', curr_value='F15', new_value='BLX'),
 	replace_value(col='iso3', curr_value='F51', new_value='CSK'),
 	rename_cols(map={'iso3': 'geocodigo'}),
 	drop_col(col='iso3_desc_fundar', axis=1),
-	sort_values(how='ascending', by=['anio', 'geocodigo'])
+	sort_values(how='ascending', by=['anio', 'geocodigo']),
+	query(condition="geocodigo != 'F351'"),
+	multiplicar_por_escalar(col='valor', k=1e-06)
 )
 #  PIPELINE_END
 
@@ -177,6 +189,36 @@ replace_value(col='iso3', curr_value='F15', new_value='BLX'),
 #  |    | geocodigo   |   anio |   valor |
 #  |---:|:------------|-------:|--------:|
 #  |  0 | AFG         |   1961 |  700000 |
+#  
+#  ------------------------------
+#  
+#  query(condition="geocodigo != 'F351'")
+#  Index: 9489 entries, 0 to 9550
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  9489 non-null   object 
+#   1   anio       9489 non-null   int64  
+#   2   valor      9489 non-null   float64
+#  
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AFG         |   1961 |     0.7 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='valor', k=1e-06)
+#  Index: 9489 entries, 0 to 9550
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  9489 non-null   object 
+#   1   anio       9489 non-null   int64  
+#   2   valor      9489 non-null   float64
+#  
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AFG         |   1961 |     0.7 |
 #  
 #  ------------------------------
 #  
