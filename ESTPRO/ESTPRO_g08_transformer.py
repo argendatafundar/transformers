9fea6b183da4a523d9e961470174e1c54d30f563
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-11 15:30:59 -0300

    Update ESTPRO_g08_transformer.py

--- ESTPRO/ESTPRO_g08_transformer.py
+++ ESTPRO/ESTPRO_g08_transformer.py
@@ -4,11 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -26,9 +21,8 @@ def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='iso3  == "ARG"'),
-	rename_cols(map={'sector_desc': 'indicador', 'share_empleo': 'valor'}),
-	drop_col(col=['iso3', 'gran_sector', 'sector_codigo', 'empleo_miles'], axis=1),
+rename_cols(map={'sector_desc': 'indicador', 'share_empleo': 'valor'}),
+	drop_col(col=['gran_sector', 'sector_codigo', 'empleo_miles'], axis=1),
 	mutiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
@@ -53,37 +47,18 @@ query(condition='iso3  == "ARG"'),
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3  == "ARG"')
-#  Index: 828 entries, 0 to 827
-#  Data columns (total 7 columns):
-#   #   Column         Non-Null Count  Dtype  
-#  ---  ------         --------------  -----  
-#   0   iso3           828 non-null    object 
-#   1   anio           828 non-null    int64  
-#   2   gran_sector    828 non-null    object 
-#   3   sector_codigo  828 non-null    object 
-#   4   sector_desc    828 non-null    object 
-#   5   empleo_miles   828 non-null    float64
-#   6   share_empleo   828 non-null    float64
-#  
-#  |    | iso3   |   anio | gran_sector   | sector_codigo   | sector_desc   |   empleo_miles |   share_empleo |
-#  |---:|:-------|-------:|:--------------|:----------------|:--------------|---------------:|---------------:|
-#  |  0 | ARG    |   1950 | Bienes        | A               | Agro y pesca  |        1676.85 |       0.258262 |
-#  
-#  ------------------------------
-#  
 #  rename_cols(map={'sector_desc': 'indicador', 'share_empleo': 'valor'})
-#  Index: 828 entries, 0 to 827
+#  RangeIndex: 42228 entries, 0 to 42227
 #  Data columns (total 7 columns):
 #   #   Column         Non-Null Count  Dtype  
 #  ---  ------         --------------  -----  
-#   0   iso3           828 non-null    object 
-#   1   anio           828 non-null    int64  
-#   2   gran_sector    828 non-null    object 
-#   3   sector_codigo  828 non-null    object 
-#   4   indicador      828 non-null    object 
-#   5   empleo_miles   828 non-null    float64
-#   6   valor          828 non-null    float64
+#   0   iso3           42228 non-null  object 
+#   1   anio           42228 non-null  int64  
+#   2   gran_sector    42228 non-null  object 
+#   3   sector_codigo  42228 non-null  object 
+#   4   indicador      42228 non-null  object 
+#   5   empleo_miles   30958 non-null  float64
+#   6   valor          30953 non-null  float64
 #  
 #  |    | iso3   |   anio | gran_sector   | sector_codigo   | indicador    |   empleo_miles |    valor |
 #  |---:|:-------|-------:|:--------------|:----------------|:-------------|---------------:|---------:|
@@ -91,33 +66,35 @@ query(condition='iso3  == "ARG"'),
 #  
 #  ------------------------------
 #  
-#  drop_col(col=['iso3', 'gran_sector', 'sector_codigo', 'empleo_miles'], axis=1)
-#  Index: 828 entries, 0 to 827
-#  Data columns (total 3 columns):
+#  drop_col(col=['gran_sector', 'sector_codigo', 'empleo_miles'], axis=1)
+#  RangeIndex: 42228 entries, 0 to 42227
+#  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       828 non-null    int64  
-#   1   indicador  828 non-null    object 
-#   2   valor      828 non-null    float64
+#   0   iso3       42228 non-null  object 
+#   1   anio       42228 non-null  int64  
+#   2   indicador  42228 non-null  object 
+#   3   valor      30953 non-null  float64
 #  
-#  |    |   anio | indicador    |   valor |
-#  |---:|-------:|:-------------|--------:|
-#  |  0 |   1950 | Agro y pesca | 25.8262 |
+#  |    | iso3   |   anio | indicador    |   valor |
+#  |---:|:-------|-------:|:-------------|--------:|
+#  |  0 | ARG    |   1950 | Agro y pesca | 25.8262 |
 #  
 #  ------------------------------
 #  
 #  mutiplicar_por_escalar(col='valor', k=100)
-#  Index: 828 entries, 0 to 827
-#  Data columns (total 3 columns):
+#  RangeIndex: 42228 entries, 0 to 42227
+#  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       828 non-null    int64  
-#   1   indicador  828 non-null    object 
-#   2   valor      828 non-null    float64
+#   0   iso3       42228 non-null  object 
+#   1   anio       42228 non-null  int64  
+#   2   indicador  42228 non-null  object 
+#   3   valor      30953 non-null  float64
 #  
-#  |    |   anio | indicador    |   valor |
-#  |---:|-------:|:-------------|--------:|
-#  |  0 |   1950 | Agro y pesca | 25.8262 |
+#  |    | iso3   |   anio | indicador    |   valor |
+#  |---:|:-------|-------:|:-------------|--------:|
+#  |  0 | ARG    |   1950 | Agro y pesca | 25.8262 |
 #  
 #  ------------------------------
 #  
