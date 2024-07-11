7057dabc421a01a4e3001eace7f8c28253df4c32
jgjuara <jg.juara@gmail.com>
2024-07-11 13:33:40 -0300

    Update PRECIO_g17_transformer.py

--- PRECIO/PRECIO_g17_transformer.py
+++ PRECIO/PRECIO_g17_transformer.py
@@ -8,9 +8,13 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def drop_na(df:DataFrame, cols:list):
+    return df.dropna(subset=cols)
 
 @transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
@@ -24,7 +28,8 @@ def sort_values(df: DataFrame, how: str, by: list):
 #  PIPELINE_START
 pipeline = chain(
 drop_col(col='tasa_inflacion', axis=1),
-	rename_columns(tasa_inflacion_valores_positivos='valor'),
+	rename_cols(map={'tasa_inflacion_valores_positivos': 'valor'}),
+	drop_na(cols=['valor']),
 	sort_values(how='ascending', by=['anio'])
 )
 #  PIPELINE_END
@@ -36,8 +41,8 @@ drop_col(col='tasa_inflacion', axis=1),
 #   #   Column                            Non-Null Count  Dtype  
 #  ---  ------                            --------------  -----  
 #   0   anio                              89 non-null     int64  
-#   1   tasa_inflacion                    88 non-null     float64
-#   2   tasa_inflacion_valores_positivos  80 non-null     float64
+#   1   tasa_inflacion                    89 non-null     float64
+#   2   tasa_inflacion_valores_positivos  81 non-null     float64
 #  
 #  |    |   anio |   tasa_inflacion |   tasa_inflacion_valores_positivos |
 #  |---:|-------:|-----------------:|-----------------------------------:|
@@ -51,7 +56,7 @@ drop_col(col='tasa_inflacion', axis=1),
 #   #   Column                            Non-Null Count  Dtype  
 #  ---  ------                            --------------  -----  
 #   0   anio                              89 non-null     int64  
-#   1   tasa_inflacion_valores_positivos  80 non-null     float64
+#   1   tasa_inflacion_valores_positivos  81 non-null     float64
 #  
 #  |    |   anio |   tasa_inflacion_valores_positivos |
 #  |---:|-------:|-----------------------------------:|
@@ -59,13 +64,41 @@ drop_col(col='tasa_inflacion', axis=1),
 #  
 #  ------------------------------
 #  
-#  rename_columns(tasa_inflacion_valores_positivos='valor')
+#  rename_cols(map={'tasa_inflacion_valores_positivos': 'valor'})
 #  RangeIndex: 89 entries, 0 to 88
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
 #   0   anio    89 non-null     int64  
-#   1   valor   80 non-null     float64
+#   1   valor   81 non-null     float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   1935 | 14.0881 |
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
+#  Index: 81 entries, 0 to 88
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    81 non-null     int64  
+#   1   valor   81 non-null     float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   1935 | 14.0881 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 81 entries, 0 to 80
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    81 non-null     int64  
+#   1   valor   81 non-null     float64
 #  
 #  |    |   anio |   valor |
 #  |---:|-------:|--------:|
