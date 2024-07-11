f93369695e56183d8bac0edafd08be0aebe96d45
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-11 16:50:47 -0300

    Update AGROPE_g19_transformer.py

--- AGROPE/AGROPE_g19_transformer.py
+++ AGROPE/AGROPE_g19_transformer.py
@@ -11,13 +11,49 @@ def rename_cols(df: DataFrame, map):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'iso3': 'geocodigo', 'tipo_carne': 'indicador'}),
-	drop_col(col='iso3_desc_fundar', axis=1)
+	drop_col(col='iso3_desc_fundar', axis=1),
+	replace_value(col='indicador', curr_value='pescado_mariscos', new_value='Pescados y mariscos'),
+	replace_value(col='indicador', curr_value='otras_carnes', new_value='Otras carnes'),
+	replace_value(col='indicador', curr_value='aviar', new_value='Aviar'),
+	replace_value(col='indicador', curr_value='caprina_ovina', new_value='Caprina y ovina'),
+	replace_value(col='indicador', curr_value='porcina', new_value='Porcina'),
+	replace_value(col='indicador', curr_value='vacuna', new_value='Vacuna')
 )
 #  PIPELINE_END
 
@@ -66,6 +102,96 @@ rename_cols(map={'iso3': 'geocodigo', 'tipo_carne': 'indicador'}),
 #  |    | geocodigo   | indicador        |    valor |
 #  |---:|:------------|:-----------------|---------:|
 #  |  0 | AFG         | pescado_mariscos | 0.359595 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='pescado_mariscos', new_value='Pescados y mariscos')
+#  RangeIndex: 1110 entries, 0 to 1109
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  1110 non-null   object 
+#   1   indicador  1110 non-null   object 
+#   2   valor      1107 non-null   float64
+#  
+#  |    | geocodigo   | indicador           |    valor |
+#  |---:|:------------|:--------------------|---------:|
+#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='otras_carnes', new_value='Otras carnes')
+#  RangeIndex: 1110 entries, 0 to 1109
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  1110 non-null   object 
+#   1   indicador  1110 non-null   object 
+#   2   valor      1107 non-null   float64
+#  
+#  |    | geocodigo   | indicador           |    valor |
+#  |---:|:------------|:--------------------|---------:|
+#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='aviar', new_value='Aviar')
+#  RangeIndex: 1110 entries, 0 to 1109
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  1110 non-null   object 
+#   1   indicador  1110 non-null   object 
+#   2   valor      1107 non-null   float64
+#  
+#  |    | geocodigo   | indicador           |    valor |
+#  |---:|:------------|:--------------------|---------:|
+#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='caprina_ovina', new_value='Caprina y ovina')
+#  RangeIndex: 1110 entries, 0 to 1109
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  1110 non-null   object 
+#   1   indicador  1110 non-null   object 
+#   2   valor      1107 non-null   float64
+#  
+#  |    | geocodigo   | indicador           |    valor |
+#  |---:|:------------|:--------------------|---------:|
+#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='porcina', new_value='Porcina')
+#  RangeIndex: 1110 entries, 0 to 1109
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  1110 non-null   object 
+#   1   indicador  1110 non-null   object 
+#   2   valor      1107 non-null   float64
+#  
+#  |    | geocodigo   | indicador           |    valor |
+#  |---:|:------------|:--------------------|---------:|
+#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='vacuna', new_value='Vacuna')
+#  RangeIndex: 1110 entries, 0 to 1109
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  1110 non-null   object 
+#   1   indicador  1110 non-null   object 
+#   2   valor      1107 non-null   float64
+#  
+#  |    | geocodigo   | indicador           |    valor |
+#  |---:|:------------|:--------------------|---------:|
+#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
