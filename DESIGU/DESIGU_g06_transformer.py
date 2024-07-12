9fb6d0076c06c208d2f6d5151866bfc515429118
jgjuara <jg.juara@gmail.com>
2024-07-12 17:38:08 -0300

    Update DESIGU_g06_transformer.py

--- DESIGU/DESIGU_g06_transformer.py
+++ DESIGU/DESIGU_g06_transformer.py
@@ -23,8 +23,8 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
-	replace_value(col='geocodigo', curr_value='argentina ', new_value='Argentina'),
-	replace_value(col='geocodigo', curr_value='americalatina', new_value='América Latina')
+	replace_value(col='categoria', curr_value='argentina ', new_value='Argentina'),
+	replace_value(col='categoria', curr_value='americalatina', new_value='América Latina')
 )
 #  PIPELINE_END
 
@@ -59,7 +59,7 @@ rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='argentina ', new_value='Argentina')
+#  replace_value(col='categoria', curr_value='argentina ', new_value='Argentina')
 #  RangeIndex: 16 entries, 0 to 15
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -74,7 +74,7 @@ rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='americalatina', new_value='América Latina')
+#  replace_value(col='categoria', curr_value='americalatina', new_value='América Latina')
 #  RangeIndex: 16 entries, 0 to 15
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -83,9 +83,9 @@ rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
 #   1   anio       16 non-null     int64  
 #   2   valor      16 non-null     float64
 #  
-#  |    | geocodigo      |   anio |   valor |
-#  |---:|:---------------|-------:|--------:|
-#  |  0 | América Latina |   1980 |   48.92 |
+#  |    | geocodigo     |   anio |   valor |
+#  |---:|:--------------|-------:|--------:|
+#  |  0 | americalatina |   1980 |   48.92 |
 #  
 #  ------------------------------
 #  
