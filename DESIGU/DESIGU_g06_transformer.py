93cf5346d64f7e95ce1bf7da4b30b4650f8f211a
jgjuara <jg.juara@gmail.com>
2024-07-12 17:45:16 -0300

    Update DESIGU_g06_transformer.py

--- DESIGU/DESIGU_g06_transformer.py
+++ DESIGU/DESIGU_g06_transformer.py
@@ -17,6 +17,13 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     df = df.replace({col: curr_value}, new_value)
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -24,7 +31,8 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 pipeline = chain(
 rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
 	replace_value(col='categoria', curr_value='argentina ', new_value='Argentina'),
-	replace_value(col='categoria', curr_value='americalatina', new_value='América Latina')
+	replace_value(col='categoria', curr_value='americalatina', new_value='América Latina'),
+	sort_values(how='ascending', by=['anio', 'categoria'])
 )
 #  PIPELINE_END
 
@@ -75,6 +83,21 @@ rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
 #  ------------------------------
 #  
 #  replace_value(col='categoria', curr_value='americalatina', new_value='América Latina')
+#  RangeIndex: 16 entries, 0 to 15
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  16 non-null     object 
+#   1   anio       16 non-null     int64  
+#   2   valor      16 non-null     float64
+#  
+#  |    | categoria      |   anio |   valor |
+#  |---:|:---------------|-------:|--------:|
+#  |  0 | América Latina |   1980 |   48.92 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'categoria'])
 #  RangeIndex: 16 entries, 0 to 15
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
