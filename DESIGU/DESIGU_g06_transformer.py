cd279f75386f09eba9fffb502cc12338a7ec5464
jgjuara <jg.juara@gmail.com>
2024-07-12 17:35:36 -0300

    Update DESIGU_g06_transformer.py

--- DESIGU/DESIGU_g06_transformer.py
+++ DESIGU/DESIGU_g06_transformer.py
@@ -23,8 +23,8 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
-	replace_value(col='geocodigo', curr_value='argentina ', new_value='ARG'),
-	replace_value(col='geocodigo', curr_value='americalatina', new_value='DESIGU_AML')
+	replace_value(col='geocodigo', curr_value='argentina ', new_value='Argentina'),
+	replace_value(col='geocodigo', curr_value='americalatina', new_value='América Latina')
 )
 #  PIPELINE_END
 
@@ -34,13 +34,13 @@ rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
 #  Data columns (total 3 columns):
 #   #   Column    Non-Null Count  Dtype  
 #  ---  ------    --------------  -----  
-#   0   ano       16 non-null     int64  
-#   1   variable  16 non-null     object 
+#   0   variable  16 non-null     object 
+#   1   ano       16 non-null     int64  
 #   2   valor     16 non-null     float64
 #  
-#  |    |   ano | variable      |   valor |
-#  |---:|------:|:--------------|--------:|
-#  |  0 |  1980 | americalatina |   48.92 |
+#  |    | variable      |   ano |   valor |
+#  |---:|:--------------|------:|--------:|
+#  |  0 | americalatina |  1980 |   48.92 |
 #  
 #  ------------------------------
 #  
@@ -49,43 +49,43 @@ rename_cols(map={'ano': 'anio', 'variable': 'geocodigo'}),
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       16 non-null     int64  
-#   1   geocodigo  16 non-null     object 
+#   0   geocodigo  16 non-null     object 
+#   1   anio       16 non-null     int64  
 #   2   valor      16 non-null     float64
 #  
-#  |    |   anio | geocodigo     |   valor |
-#  |---:|-------:|:--------------|--------:|
-#  |  0 |   1980 | americalatina |   48.92 |
+#  |    | geocodigo     |   anio |   valor |
+#  |---:|:--------------|-------:|--------:|
+#  |  0 | americalatina |   1980 |   48.92 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='argentina ', new_value='ARG')
+#  replace_value(col='geocodigo', curr_value='argentina ', new_value='Argentina')
 #  RangeIndex: 16 entries, 0 to 15
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       16 non-null     int64  
-#   1   geocodigo  16 non-null     object 
+#   0   geocodigo  16 non-null     object 
+#   1   anio       16 non-null     int64  
 #   2   valor      16 non-null     float64
 #  
-#  |    |   anio | geocodigo     |   valor |
-#  |---:|-------:|:--------------|--------:|
-#  |  0 |   1980 | americalatina |   48.92 |
+#  |    | geocodigo     |   anio |   valor |
+#  |---:|:--------------|-------:|--------:|
+#  |  0 | americalatina |   1980 |   48.92 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='americalatina', new_value='DESIGU_AML')
+#  replace_value(col='geocodigo', curr_value='americalatina', new_value='América Latina')
 #  RangeIndex: 16 entries, 0 to 15
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       16 non-null     int64  
-#   1   geocodigo  16 non-null     object 
+#   0   geocodigo  16 non-null     object 
+#   1   anio       16 non-null     int64  
 #   2   valor      16 non-null     float64
 #  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1980 | DESIGU_AML  |   48.92 |
+#  |    | geocodigo      |   anio |   valor |
+#  |---:|:---------------|-------:|--------:|
+#  |  0 | América Latina |   1980 |   48.92 |
 #  
 #  ------------------------------
 #  
