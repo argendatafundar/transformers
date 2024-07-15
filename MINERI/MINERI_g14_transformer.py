9ea1345d13c654af8b36688fd133fdd02410af10
jgjuara <jg.juara@gmail.com>
2024-07-15 17:58:12 -0300

    Update MINERI_g14_transformer.py

--- MINERI/MINERI_g14_transformer.py
+++ MINERI/MINERI_g14_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_values(df: DataFrame, col: str, values: dict):
+    df = df.replace({col: values})
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indicador', 'porcentaje_sobre_total_rama': 'valor'})
+rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indicador', 'porcentaje_sobre_total_rama': 'valor'}),
+	replace_values(col='indicador', values={'asalariados_registrados': 'Asalariados registrados', 'asalariados_no_registrados': 'Asalariados no registrados', 'no_asalariados': 'No asalariados'})
 )
 #  PIPELINE_END
 
@@ -44,6 +50,21 @@ rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indica
 #  |    | categoria    | indicador               |   valor |
 #  |---:|:-------------|:------------------------|--------:|
 #  |  0 | Construcción | asalariados_registrados |   15.91 |
+#  
+#  ------------------------------
+#  
+#  replace_values(col='indicador', values={'asalariados_registrados': 'Asalariados registrados', 'asalariados_no_registrados': 'Asalariados no registrados', 'no_asalariados': 'No asalariados'})
+#  RangeIndex: 69 entries, 0 to 68
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  69 non-null     object 
+#   1   indicador  69 non-null     object 
+#   2   valor      69 non-null     float64
+#  
+#  |    | categoria    | indicador               |   valor |
+#  |---:|:-------------|:------------------------|--------:|
+#  |  0 | Construcción | Asalariados registrados |   15.91 |
 #  
 #  ------------------------------
 #  
