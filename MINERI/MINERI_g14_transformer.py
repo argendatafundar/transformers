750e7d6cc50b1420aa288c28beed0203795cdd04
jgjuara <jg.juara@gmail.com>
2024-07-15 17:58:56 -0300

    Update MINERI_g14_transformer.py

--- MINERI/MINERI_g14_transformer.py
+++ MINERI/MINERI_g14_transformer.py
@@ -12,13 +12,21 @@ def rename_cols(df: DataFrame, map):
 def replace_values(df: DataFrame, col: str, values: dict):
     df = df.replace({col: values})
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indicador', 'porcentaje_sobre_total_rama': 'valor'}),
-	replace_values(col='indicador', values={'asalariados_registrados': 'Asalariados registrados', 'asalariados_no_registrados': 'Asalariados no registrados', 'no_asalariados': 'No asalariados'})
+	replace_values(col='indicador', values={'asalariados_registrados': 'Asalariados registrados', 'asalariados_no_registrados': 'Asalariados no registrados', 'no_asalariados': 'No asalariados'}),
+	sort_values(how='ascending', by=['categoria', 'indicador'])
 )
 #  PIPELINE_END
 
@@ -65,6 +73,21 @@ rename_cols(map={'rama_actividad': 'categoria', 'categoria_ocupacional': 'indica
 #  |    | categoria    | indicador               |   valor |
 #  |---:|:-------------|:------------------------|--------:|
 #  |  0 | Construcción | Asalariados registrados |   15.91 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['categoria', 'indicador'])
+#  RangeIndex: 69 entries, 0 to 68
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   categoria  69 non-null     object 
+#   1   indicador  69 non-null     object 
+#   2   valor      69 non-null     float64
+#  
+#  |    | categoria            | indicador                  |   valor |
+#  |---:|:---------------------|:---------------------------|--------:|
+#  |  0 | Act. Administrativas | Asalariados no registrados |   17.88 |
 #  
 #  ------------------------------
 #  
