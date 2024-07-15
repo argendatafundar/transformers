572246acd6f225437e33a4558f52ae2566cfcbbb
jgjuara <jg.juara@gmail.com>
2024-07-15 18:30:20 -0300

    Update MINERI_g04_transformer.py

--- MINERI/MINERI_g04_transformer.py
+++ MINERI/MINERI_g04_transformer.py
@@ -17,6 +17,13 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 def str_to_title(df: DataFrame, col:str):
     df[col] = df[col].str.title()
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -24,7 +31,8 @@ def str_to_title(df: DataFrame, col:str):
 pipeline = chain(
 rename_cols(map={'grupo_nuevo': 'indicador', 'expo_grupo': 'valor'}),
 	multiplicar_por_escalar(col='valor', k=1e-06),
-	str_to_title(col='indicador')
+	str_to_title(col='indicador'),
+	sort_values(how='ascending', by=['anio', 'indicador'])
 )
 #  PIPELINE_END
 
@@ -75,6 +83,21 @@ rename_cols(map={'grupo_nuevo': 'indicador', 'expo_grupo': 'valor'}),
 #  ------------------------------
 #  
 #  str_to_title(col='indicador')
+#  RangeIndex: 139 entries, 0 to 138
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   indicador  139 non-null    object 
+#   1   anio       139 non-null    int64  
+#   2   valor      139 non-null    float64
+#  
+#  |    | indicador   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | Cobre       |   1994 | 3.63523 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio', 'indicador'])
 #  RangeIndex: 139 entries, 0 to 138
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
