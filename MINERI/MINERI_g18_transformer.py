dc05617f2f4dd347f0f4c14c0574015dd1ab854e
jgjuara <jg.juara@gmail.com>
2024-07-16 12:52:03 -0300

    Update MINERI_g18_transformer.py

--- MINERI/MINERI_g18_transformer.py
+++ MINERI/MINERI_g18_transformer.py
@@ -7,12 +7,19 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+# break lines in strings based on nchars
+def break_lines(df: DataFrame, col:str, nchars:int, break_words:bool = False):
+    df[col] = df[col].str.wrap(nchars, break_long_words = break_words)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'rama_vendedora': 'serie', 'porcentaje_compra_sector_minero': 'valor'})
+rename_cols(map={'rama_vendedora': 'serie', 'porcentaje_compra_sector_minero': 'valor'}),
+	break_lines(col='serie', nchars=10, break_words=False)
 )
 #  PIPELINE_END
 
@@ -32,6 +39,20 @@ rename_cols(map={'rama_vendedora': 'serie', 'porcentaje_compra_sector_minero': '
 #  ------------------------------
 #  
 #  rename_cols(map={'rama_vendedora': 'serie', 'porcentaje_compra_sector_minero': 'valor'})
+#  RangeIndex: 15 entries, 0 to 14
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   serie   15 non-null     object 
+#   1   valor   15 non-null     float64
+#  
+#  |    | serie     |   valor |
+#  |---:|:----------|--------:|
+#  |  0 | Industria |      24 |
+#  
+#  ------------------------------
+#  
+#  break_lines(col='serie', nchars=10, break_words=False)
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
