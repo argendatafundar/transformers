bfe441be0afa1fc921b04ffa74946872cb353131
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-15 22:26:39 -0300

    Update AGROPE_g07_transformer.py

--- AGROPE/AGROPE_g07_transformer.py
+++ AGROPE/AGROPE_g07_transformer.py
@@ -19,6 +19,11 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     return df
 
 @transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -38,6 +43,7 @@ pipeline = chain(
 replace_value(col='cultivo', curr_value='Cebada total', new_value='Cebada'),
 	replace_value(col='cultivo', curr_value='Trigo total', new_value='Trigo'),
 	replace_value(col='cultivo', curr_value='Poroto total', new_value='Poroto'),
+	replace_value(col='cultivo', curr_value='Soja total', new_value='Soja'),
 	rename_cols(map={'cultivo': 'categoria', 'share': 'valor'}),
 	drop_col(col='q_total', axis=1),
 	drop_col(col='campania', axis=1)
@@ -94,6 +100,22 @@ replace_value(col='cultivo', curr_value='Cebada total', new_value='Cebada'),
 #  ------------------------------
 #  
 #  replace_value(col='cultivo', curr_value='Poroto total', new_value='Poroto')
+#  RangeIndex: 21 entries, 0 to 20
+#  Data columns (total 4 columns):
+#   #   Column    Non-Null Count  Dtype  
+#  ---  ------    --------------  -----  
+#   0   cultivo   21 non-null     object 
+#   1   campania  21 non-null     object 
+#   2   q_total   21 non-null     int64  
+#   3   share     21 non-null     float64
+#  
+#  |    | cultivo   | campania   |   q_total |   share |
+#  |---:|:----------|:-----------|----------:|--------:|
+#  |  0 | Algodón   | 2021/22    |   1115510 | 0.77728 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='cultivo', curr_value='Soja total', new_value='Soja')
 #  RangeIndex: 21 entries, 0 to 20
 #  Data columns (total 4 columns):
 #   #   Column    Non-Null Count  Dtype  
