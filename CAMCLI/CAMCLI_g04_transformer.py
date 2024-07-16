5dce534a5721711e95aaa89913038d8b141a6277
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-16 16:10:47 -0300

    Update CAMCLI_g04_transformer.py

--- CAMCLI/CAMCLI_g04_transformer.py
+++ CAMCLI/CAMCLI_g04_transformer.py
@@ -97,12 +97,18 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     df = df.replace({col: curr_value}, new_value)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'subsubsector': 'nivel3', 'valor_en_porcent': 'valor'}),
+	replace_value(col='nivel1', curr_value='Industria Química', new_value='PIUP'),
 	replace_value(col='nivel2', curr_value='Metales no ferrosos', new_value='M. no ferrosos'),
 	replace_value(col='nivel2', curr_value='Industria química y petroquímica', new_value='Quím. y petroquím.'),
 	replace_value(col='nivel2', curr_value='Alimentación y tabaco', new_value='Alim. y tabaco'),
@@ -142,6 +148,22 @@ rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'subsubsector': 'niv
 #  ------------------------------
 #  
 #  rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'subsubsector': 'nivel3', 'valor_en_porcent': 'valor'})
+#  RangeIndex: 29 entries, 0 to 28
+#  Data columns (total 4 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   nivel1  29 non-null     object 
+#   1   nivel2  29 non-null     object 
+#   2   nivel3  29 non-null     object 
+#   3   valor   29 non-null     float64
+#  
+#  |    | nivel1   | nivel2     | nivel3    |   valor |
+#  |---:|:---------|:-----------|:----------|--------:|
+#  |  0 | Energía  | Transporte | Carretera |    11.9 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='nivel1', curr_value='Industria Química', new_value='PIUP')
 #  RangeIndex: 29 entries, 0 to 28
 #  Data columns (total 4 columns):
 #   #   Column  Non-Null Count  Dtype  
