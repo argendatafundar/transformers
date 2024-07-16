3f250fbb43c0b0d62d430bc35db30bb3d0f5404b
TuQmano <juanpabloruiznicolini@gmail.com>
2024-07-15 22:13:16 -0300

    Update AGROPE_g07_transformer.py

--- AGROPE/AGROPE_g07_transformer.py
+++ AGROPE/AGROPE_g07_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -20,7 +25,8 @@ def drop_col(df: DataFrame, col, axis=1):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'cultivo   ': 'categoria', 'share': 'valor'}),
+replace_value(col='cultimo', curr_value='Cebada total', new_value='Cebada'),
+	rename_cols(map={'cultivo   ': 'categoria', 'share': 'valor'}),
 	drop_col(col='q_total', axis=1),
 	drop_col(col='campania', axis=1)
 )
@@ -28,6 +34,22 @@ rename_cols(map={'cultivo   ': 'categoria', 'share': 'valor'}),
 
 
 #  start()
+#  RangeIndex: 21 entries, 0 to 20
+#  Data columns (total 4 columns):
+#   #   Column    Non-Null Count  Dtype  
+#  ---  ------    --------------  -----  
+#   0   cultivo   21 non-null     object 
+#   1   campania  21 non-null     object 
+#   2   q_total   21 non-null     int64  
+#   3   share     21 non-null     float64
+#  
+#  |    | cultivo   | campania   |   q_total |   share |
+#  |---:|:----------|:-----------|----------:|--------:|
+#  |  0 | Algodón   | 2021/22    |   1115510 | 0.77728 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='cultimo', curr_value='Cebada total', new_value='Cebada')
 #  RangeIndex: 21 entries, 0 to 20
 #  Data columns (total 4 columns):
 #   #   Column    Non-Null Count  Dtype  
