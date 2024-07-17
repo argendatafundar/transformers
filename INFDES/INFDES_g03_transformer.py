46e6f97d9984e2fd5c3b88c4617406955dae09be
jgjuara <jg.juara@gmail.com>
2024-07-17 10:57:45 -0300

    Update INFDES_g03_transformer.py

--- INFDES/INFDES_g03_transformer.py
+++ INFDES/INFDES_g03_transformer.py
@@ -42,7 +42,7 @@ rename_cols(map={'iso3': 'geocodigo', 'cat_ocup_detalle': 'indicador'}),
 	drop_col(col='cat_ocup_cod', axis=1),
 	drop_col(col='pais', axis=1),
 	query(condition='indicador != "Total formal"'),
-	sort_values_by_comparison(colname='indicador', precedence={'Asalariados en pequeñas y grandes empresas': 0, 'Asalariados públicos': 1, 'Cuentapropistas profesionales': 2, 'Empleadores': 3, 'Asalariados en microempresas': 4, 'Cuentapropistas sin calificación': 5, 'Trabajadores sin ingresos': 6}, prefix=[], suffix=[])
+	sort_values_by_comparison(colname='indicador', precedence={'Asalariados en pequeñas y grandes empresas': 1, 'Asalariados públicos': 2, 'Cuentapropistas profesionales': 3, 'Empleadores': 0, 'Asalariados en microempresas': 4, 'Cuentapropistas sin calificación': 5, 'Trabajadores sin ingresos': 6}, prefix=[], suffix=[])
 )
 #  PIPELINE_END
 
@@ -152,8 +152,8 @@ rename_cols(map={'iso3': 'geocodigo', 'cat_ocup_detalle': 'indicador'}),
 #  
 #  ------------------------------
 #  
-#  sort_values_by_comparison(colname='indicador', precedence={'Asalariados en pequeñas y grandes empresas': 0, 'Asalariados públicos': 1, 'Cuentapropistas profesionales': 2, 'Empleadores': 3, 'Asalariados en microempresas': 4, 'Cuentapropistas sin calificación': 5, 'Trabajadores sin ingresos': 6}, prefix=[], suffix=[])
-#  Index: 105 entries, 1 to 104
+#  sort_values_by_comparison(colname='indicador', precedence={'Asalariados en pequeñas y grandes empresas': 1, 'Asalariados públicos': 2, 'Cuentapropistas profesionales': 3, 'Empleadores': 0, 'Asalariados en microempresas': 4, 'Cuentapropistas sin calificación': 5, 'Trabajadores sin ingresos': 6}, prefix=[], suffix=[])
+#  Index: 105 entries, 0 to 104
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
@@ -162,9 +162,9 @@ rename_cols(map={'iso3': 'geocodigo', 'cat_ocup_detalle': 'indicador'}),
 #   2   indicador  105 non-null    object 
 #   3   valor      105 non-null    float64
 #  
-#  |    | geocodigo   |   anio | indicador                                  |   valor |
-#  |---:|:------------|-------:|:-------------------------------------------|--------:|
-#  |  1 | ARG         |   2022 | Asalariados en pequeñas y grandes empresas |    32.4 |
+#  |    | geocodigo   |   anio | indicador   |   valor |
+#  |---:|:------------|-------:|:------------|--------:|
+#  |  0 | ARG         |   2022 | Empleadores |     3.7 |
 #  
 #  ------------------------------
 #  
