7ca39831c35f0ceb93c0fdf58338b7f91142c3c2
Juan Manuel Dias <juamadias@gmail.com>
2024-07-18 10:29:07 -0300

    Update CRECIM_g04_transformer.py

--- CRECIM/CRECIM_g04_transformer.py
+++ CRECIM/CRECIM_g04_transformer.py
@@ -25,7 +25,7 @@ def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indi
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='anio == anio.max()'),
+query(condition='anio == 2021'),
 	drop_col(col=['pais_nombre', 'anio', 'medida_bienestar'], axis=1),
 	rename_cols(map={'iso3': 'geocodigo', 'continente_fundar': 'grupo', 'medida_bienestar': 'indicador'}),
 	wide_to_long(primary_keys=['geocodigo', 'grupo'], value_name='valor', var_name='indicador')
@@ -53,73 +53,73 @@ query(condition='anio == anio.max()'),
 #  
 #  ------------------------------
 #  
-#  query(condition='anio == anio.max()')
-#  Index: 1 entries, 926 to 926
+#  query(condition='anio == 2021')
+#  Index: 17 entries, 39 to 1948
 #  Data columns (total 8 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   iso3                    1 non-null      object 
-#   1   pais_nombre             1 non-null      object 
-#   2   continente_fundar       1 non-null      object 
-#   3   anio                    1 non-null      int64  
-#   4   pib_percapita_ppp_2017  1 non-null      float64
-#   5   medida_bienestar        1 non-null      object 
-#   6   promedio                1 non-null      float64
-#   7   nivel_agregacion        1 non-null      object 
+#   0   iso3                    17 non-null     object 
+#   1   pais_nombre             17 non-null     object 
+#   2   continente_fundar       17 non-null     object 
+#   3   anio                    17 non-null     int64  
+#   4   pib_percapita_ppp_2017  17 non-null     float64
+#   5   medida_bienestar        17 non-null     object 
+#   6   promedio                17 non-null     float64
+#   7   nivel_agregacion        17 non-null     object 
 #  
-#  |     | iso3   | pais_nombre   | continente_fundar   |   anio |   pib_percapita_ppp_2017 | medida_bienestar   |   promedio | nivel_agregacion   |
-#  |----:|:-------|:--------------|:--------------------|-------:|-------------------------:|:-------------------|-----------:|:-------------------|
-#  | 926 | IDN    | Indonesia     | Asia                |   2022 |                  4061.17 | consumo            |       7.81 | pais               |
+#  |    | iso3   | pais_nombre   | continente_fundar   |   anio |   pib_percapita_ppp_2017 | medida_bienestar   |   promedio | nivel_agregacion   |
+#  |---:|:-------|:--------------|:--------------------|-------:|-------------------------:|:-------------------|-----------:|:-------------------|
+#  | 39 | ARM    | Armenia       | Asia                |   2021 |                  4522.32 | consumo            |       7.78 | pais               |
 #  
 #  ------------------------------
 #  
 #  drop_col(col=['pais_nombre', 'anio', 'medida_bienestar'], axis=1)
-#  Index: 1 entries, 926 to 926
+#  Index: 17 entries, 39 to 1948
 #  Data columns (total 5 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   iso3                    1 non-null      object 
-#   1   continente_fundar       1 non-null      object 
-#   2   pib_percapita_ppp_2017  1 non-null      float64
-#   3   promedio                1 non-null      float64
-#   4   nivel_agregacion        1 non-null      object 
+#   0   iso3                    17 non-null     object 
+#   1   continente_fundar       17 non-null     object 
+#   2   pib_percapita_ppp_2017  17 non-null     float64
+#   3   promedio                17 non-null     float64
+#   4   nivel_agregacion        17 non-null     object 
 #  
-#  |     | iso3   | continente_fundar   |   pib_percapita_ppp_2017 |   promedio | nivel_agregacion   |
-#  |----:|:-------|:--------------------|-------------------------:|-----------:|:-------------------|
-#  | 926 | IDN    | Asia                |                  4061.17 |       7.81 | pais               |
+#  |    | iso3   | continente_fundar   |   pib_percapita_ppp_2017 |   promedio | nivel_agregacion   |
+#  |---:|:-------|:--------------------|-------------------------:|-----------:|:-------------------|
+#  | 39 | ARM    | Asia                |                  4522.32 |       7.78 | pais               |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'iso3': 'geocodigo', 'continente_fundar': 'grupo', 'medida_bienestar': 'indicador'})
-#  Index: 1 entries, 926 to 926
+#  Index: 17 entries, 39 to 1948
 #  Data columns (total 5 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   geocodigo               1 non-null      object 
-#   1   grupo                   1 non-null      object 
-#   2   pib_percapita_ppp_2017  1 non-null      float64
-#   3   promedio                1 non-null      float64
-#   4   nivel_agregacion        1 non-null      object 
+#   0   geocodigo               17 non-null     object 
+#   1   grupo                   17 non-null     object 
+#   2   pib_percapita_ppp_2017  17 non-null     float64
+#   3   promedio                17 non-null     float64
+#   4   nivel_agregacion        17 non-null     object 
 #  
-#  |     | geocodigo   | grupo   |   pib_percapita_ppp_2017 |   promedio | nivel_agregacion   |
-#  |----:|:------------|:--------|-------------------------:|-----------:|:-------------------|
-#  | 926 | IDN         | Asia    |                  4061.17 |       7.81 | pais               |
+#  |    | geocodigo   | grupo   |   pib_percapita_ppp_2017 |   promedio | nivel_agregacion   |
+#  |---:|:------------|:--------|-------------------------:|-----------:|:-------------------|
+#  | 39 | ARM         | Asia    |                  4522.32 |       7.78 | pais               |
 #  
 #  ------------------------------
 #  
 #  wide_to_long(primary_keys=['geocodigo', 'grupo'], value_name='valor', var_name='indicador')
-#  RangeIndex: 3 entries, 0 to 2
+#  RangeIndex: 51 entries, 0 to 50
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype 
 #  ---  ------     --------------  ----- 
-#   0   geocodigo  3 non-null      object
-#   1   grupo      3 non-null      object
-#   2   indicador  3 non-null      object
-#   3   valor      3 non-null      object
+#   0   geocodigo  51 non-null     object
+#   1   grupo      51 non-null     object
+#   2   indicador  51 non-null     object
+#   3   valor      51 non-null     object
 #  
 #  |    | geocodigo   | grupo   | indicador              |   valor |
 #  |---:|:------------|:--------|:-----------------------|--------:|
-#  |  0 | IDN         | Asia    | pib_percapita_ppp_2017 | 4061.17 |
+#  |  0 | ARM         | Asia    | pib_percapita_ppp_2017 | 4522.32 |
 #  
 #  ------------------------------
 #  
