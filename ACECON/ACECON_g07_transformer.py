ab0a6b1ac7b9cd5f94a38028126e9c5c89d5d731
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-18 12:23:36 -0300

    Update ACECON_g07_transformer.py

--- ACECON/ACECON_g07_transformer.py
+++ ACECON/ACECON_g07_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sector': 'indicador'})
+rename_cols(map={'sector': 'indicador'}),
+	replace_value(col='indicador', curr_value='Construccion y Electricidad, Gas y Agua', new_value='Construccion y EGA')
 )
 #  PIPELINE_END
 
@@ -33,6 +39,21 @@ rename_cols(map={'sector': 'indicador'})
 #  ------------------------------
 #  
 #  rename_cols(map={'sector': 'indicador'})
+#  RangeIndex: 440 entries, 0 to 439
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       440 non-null    int64  
+#   1   indicador  440 non-null    object 
+#   2   valor      440 non-null    float64
+#  
+#  |    |   anio | indicador                             |   valor |
+#  |---:|-------:|:--------------------------------------|--------:|
+#  |  0 |   1935 | Agricultura caza silvicultura y pesca |  25.865 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='Construccion y Electricidad, Gas y Agua', new_value='Construccion y EGA')
 #  RangeIndex: 440 entries, 0 to 439
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
