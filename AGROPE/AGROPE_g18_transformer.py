5d78baed206d4dc32b8a6965cbffa19ff81c6d7a
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-22 10:08:26 -0300

    Update AGROPE_g18_transformer.py

--- AGROPE/AGROPE_g18_transformer.py
+++ AGROPE/AGROPE_g18_transformer.py
@@ -7,12 +7,48 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'grupo_carne': 'indicador'})
+rename_cols(map={'grupo_carne': 'indicador'}),
+	replace_value(col='indicador', curr_value='aviar', new_value='Aviar'),
+	replace_value(col='indicador', curr_value='vacuna', new_value='Vacuna'),
+	replace_value(col='indicador', curr_value='caprina_ovina', new_value='Caprina y ovina'),
+	replace_value(col='indicador', curr_value='porcina', new_value='Porcina'),
+	replace_value(col='indicador', curr_value='otras_carnes', new_value='Otras carnes'),
+	replace_value(col='indicador', curr_value='pescado_mariscos', new_value='Pescado y mariscos')
 )
 #  PIPELINE_END
 
@@ -44,6 +80,96 @@ rename_cols(map={'grupo_carne': 'indicador'})
 #  |    |   anio | indicador   |   valor |
 #  |---:|-------:|:------------|--------:|
 #  |  0 |   1961 | aviar       | 2.07759 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='aviar', new_value='Aviar')
+#  RangeIndex: 360 entries, 0 to 359
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       360 non-null    int64  
+#   1   indicador  360 non-null    object 
+#   2   valor      360 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1961 | Aviar       | 2.07759 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='vacuna', new_value='Vacuna')
+#  RangeIndex: 360 entries, 0 to 359
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       360 non-null    int64  
+#   1   indicador  360 non-null    object 
+#   2   valor      360 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1961 | Aviar       | 2.07759 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='caprina_ovina', new_value='Caprina y ovina')
+#  RangeIndex: 360 entries, 0 to 359
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       360 non-null    int64  
+#   1   indicador  360 non-null    object 
+#   2   valor      360 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1961 | Aviar       | 2.07759 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='porcina', new_value='Porcina')
+#  RangeIndex: 360 entries, 0 to 359
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       360 non-null    int64  
+#   1   indicador  360 non-null    object 
+#   2   valor      360 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1961 | Aviar       | 2.07759 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='otras_carnes', new_value='Otras carnes')
+#  RangeIndex: 360 entries, 0 to 359
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       360 non-null    int64  
+#   1   indicador  360 non-null    object 
+#   2   valor      360 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1961 | Aviar       | 2.07759 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='pescado_mariscos', new_value='Pescado y mariscos')
+#  RangeIndex: 360 entries, 0 to 359
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       360 non-null    int64  
+#   1   indicador  360 non-null    object 
+#   2   valor      360 non-null    float64
+#  
+#  |    |   anio | indicador   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1961 | Aviar       | 2.07759 |
 #  
 #  ------------------------------
 #  
