c5048b71513f2257139de1015701c0e12fdd5aa5
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-07-30 18:01:24 -0300

    Update AGROPE_g36_transformer.py

--- AGROPE/AGROPE_g36_transformer.py
+++ AGROPE/AGROPE_g36_transformer.py
@@ -4,35 +4,69 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
-
-@transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
-    return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'tipo_cadena': 'categoria'}),
-	sort_values(how='ascending', by=['categoria']),
-	drop_na(col='valor'),
-	multiplicar_por_escalar(col='valor', k=100)
+mutiplicar_por_escalar(col='valor', k=100),
+	replace_value(col='tipo_cadena', curr_value='Maiz', new_value='Maíz'),
+	replace_value(col='tipo_cadena', curr_value='Bovino', new_value='Bovina'),
+	replace_value(col='tipo_cadena', curr_value='Lacteo', new_value='Lácteo'),
+	replace_value(col='tipo_cadena', curr_value='Avicola', new_value='Avícola'),
+	replace_value(col='tipo_cadena', curr_value='Porcinos', new_value='Porcina'),
+	replace_value(col='tipo_cadena', curr_value='Citrico', new_value='Cítrico'),
+	replace_value(col='tipo_cadena', curr_value='Caprino', new_value='Caprina'),
+	replace_value(col='tipo_cadena', curr_value='Ovinos', new_value='Ovina'),
+	rename_cols(map={'tipo_cadena': 'categoria'})
 )
 #  PIPELINE_END
 
@@ -51,6 +85,132 @@ rename_cols(map={'tipo_cadena': 'categoria'}),
 #  
 #  ------------------------------
 #  
+#  mutiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Maiz', new_value='Maíz')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Bovino', new_value='Bovina')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Lacteo', new_value='Lácteo')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Avicola', new_value='Avícola')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Porcinos', new_value='Porcina')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Citrico', new_value='Cítrico')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Caprino', new_value='Caprina')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='tipo_cadena', curr_value='Ovinos', new_value='Ovina')
+#  RangeIndex: 32 entries, 0 to 31
+#  Data columns (total 2 columns):
+#   #   Column       Non-Null Count  Dtype  
+#  ---  ------       --------------  -----  
+#   0   tipo_cadena  32 non-null     object 
+#   1   valor        32 non-null     float64
+#  
+#  |    | tipo_cadena   |   valor |
+#  |---:|:--------------|--------:|
+#  |  0 | Miel          |      86 |
+#  
+#  ------------------------------
+#  
 #  rename_cols(map={'tipo_cadena': 'categoria'})
 #  RangeIndex: 32 entries, 0 to 31
 #  Data columns (total 2 columns):
@@ -61,49 +221,7 @@ rename_cols(map={'tipo_cadena': 'categoria'}),
 #  
 #  |    | categoria   |   valor |
 #  |---:|:------------|--------:|
-#  |  0 | Miel        |    0.86 |
-#  
-#  ------------------------------
-#  
-#  sort_values(how='ascending', by=['categoria'])
-#  RangeIndex: 32 entries, 0 to 31
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  32 non-null     object 
-#   1   valor      32 non-null     float64
-#  
-#  |    | categoria   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | Ajo         |    0.11 |
-#  
-#  ------------------------------
-#  
-#  drop_na(col='valor')
-#  RangeIndex: 32 entries, 0 to 31
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  32 non-null     object 
-#   1   valor      32 non-null     float64
-#  
-#  |    | categoria   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | Ajo         |      11 |
-#  
-#  ------------------------------
-#  
-#  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 32 entries, 0 to 31
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  32 non-null     object 
-#   1   valor      32 non-null     float64
-#  
-#  |    | categoria   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | Ajo         |      11 |
+#  |  0 | Miel        |      86 |
 #  
 #  ------------------------------
 #  
