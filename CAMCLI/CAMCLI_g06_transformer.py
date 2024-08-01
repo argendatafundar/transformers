f5f68847c4559ae523df2ea54230667af26fff2c
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-01 12:45:55 -0300

    Update CAMCLI_g06_transformer.py

--- CAMCLI/CAMCLI_g06_transformer.py
+++ CAMCLI/CAMCLI_g06_transformer.py
@@ -4,8 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
-    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
+def latest_year(df, by='anio'):
+    latest_year = df[by].max()
+    df = df.query(f'{by} == {latest_year}')
+    df = df.drop(columns = by)
+    return df
 
 @transformer.convert
 def rename_cols(df: DataFrame, map):
@@ -13,17 +16,22 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', var_name='indicador'),
-	rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2'}),
-	query(condition="indicador == 'valor_en_porcent'")
+latest_year(by='anio'),
+	rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'valor_en_porcent': 'valor'}),
+	drop_col(col=['valor_en_mtco2e'], axis=1),
+	replace_value(col='nivel1', curr_value='Procesos industriales y uso de productos', new_value='PIUP')
 )
 #  PIPELINE_END
 
@@ -45,54 +53,65 @@ wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', v
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['sector', 'anio', 'subsector'], value_name='valor', var_name='indicador')
-#  RangeIndex: 30 entries, 0 to 29
-#  Data columns (total 5 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   sector     30 non-null     object 
-#   1   anio       30 non-null     int64  
-#   2   subsector  30 non-null     object 
-#   3   indicador  30 non-null     object 
-#   4   valor      30 non-null     float64
+#  latest_year(by='anio')
+#  Index: 15 entries, 0 to 14
+#  Data columns (total 4 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   sector            15 non-null     object 
+#   1   subsector         15 non-null     object 
+#   2   valor_en_mtco2e   15 non-null     float64
+#   3   valor_en_porcent  15 non-null     float64
 #  
-#  |    | sector   |   anio | subsector                        | indicador       |   valor |
-#  |---:|:---------|-------:|:---------------------------------|:----------------|--------:|
-#  |  0 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa | valor_en_mtco2e | 4.52477 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2'})
-#  RangeIndex: 30 entries, 0 to 29
-#  Data columns (total 5 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   nivel1     30 non-null     object 
-#   1   anio       30 non-null     int64  
-#   2   nivel2     30 non-null     object 
-#   3   indicador  30 non-null     object 
-#   4   valor      30 non-null     float64
-#  
-#  |    | nivel1   |   anio | nivel2                           | indicador       |   valor |
-#  |---:|:---------|-------:|:---------------------------------|:----------------|--------:|
-#  |  0 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa | valor_en_mtco2e | 4.52477 |
+#  |    | sector   | subsector                        |   valor_en_mtco2e |   valor_en_porcent |
+#  |---:|:---------|:---------------------------------|------------------:|-------------------:|
+#  |  0 | AGSyOUT  | Emisiones de la quema de biomasa |           4.52477 |            1.23665 |
 #  
 #  ------------------------------
 #  
-#  query(condition="indicador == 'valor_en_porcent'")
-#  Index: 15 entries, 15 to 29
-#  Data columns (total 5 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   nivel1     15 non-null     object 
-#   1   anio       15 non-null     int64  
-#   2   nivel2     15 non-null     object 
-#   3   indicador  15 non-null     object 
-#   4   valor      15 non-null     float64
+#  rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'valor_en_porcent': 'valor'})
+#  Index: 15 entries, 0 to 14
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   nivel1           15 non-null     object 
+#   1   nivel2           15 non-null     object 
+#   2   valor_en_mtco2e  15 non-null     float64
+#   3   valor            15 non-null     float64
 #  
-#  |    | nivel1   |   anio | nivel2                           | indicador        |   valor |
-#  |---:|:---------|-------:|:---------------------------------|:-----------------|--------:|
-#  | 15 | AGSyOUT  |   2018 | Emisiones de la quema de biomasa | valor_en_porcent | 1.23665 |
+#  |    | nivel1   | nivel2                           |   valor_en_mtco2e |   valor |
+#  |---:|:---------|:---------------------------------|------------------:|--------:|
+#  |  0 | AGSyOUT  | Emisiones de la quema de biomasa |           4.52477 | 1.23665 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['valor_en_mtco2e'], axis=1)
+#  Index: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   nivel1  15 non-null     object 
+#   1   nivel2  15 non-null     object 
+#   2   valor   15 non-null     float64
+#  
+#  |    | nivel1   | nivel2                           |   valor |
+#  |---:|:---------|:---------------------------------|--------:|
+#  |  0 | AGSyOUT  | Emisiones de la quema de biomasa | 1.23665 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='nivel1', curr_value='Procesos industriales y uso de productos', new_value='PIUP')
+#  Index: 15 entries, 0 to 14
+#  Data columns (total 3 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   nivel1  15 non-null     object 
+#   1   nivel2  15 non-null     object 
+#   2   valor   15 non-null     float64
+#  
+#  |    | nivel1   | nivel2                           |   valor |
+#  |---:|:---------|:---------------------------------|--------:|
+#  |  0 | AGSyOUT  | Emisiones de la quema de biomasa | 1.23665 |
 #  
 #  ------------------------------
 #  
