714c43b02328c90ebe469dbe97afa9dd427e7d1b
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-01 13:08:32 -0300

    Update CAMCLI_g10_transformer.py

--- CAMCLI/CAMCLI_g10_transformer.py
+++ CAMCLI/CAMCLI_g10_transformer.py
@@ -11,13 +11,22 @@ def rename_cols(df: DataFrame, map):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def sort_values_by_comparison(df, colname: str, precedence: dict, prefix=[], suffix=[]):
+    mapcol = colname+'_map'
+    df_ = df.copy()
+    df_[mapcol] = df_[colname].map(precedence)
+    df_ = df_.sort_values(by=[*prefix, mapcol, *suffix])
+    return df_.drop(mapcol, axis=1)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'valor_en_mtco2e': 'valor', 'subsector': 'indicador'}),
-	drop_col(col='sector', axis=1)
+	drop_col(col='sector', axis=1),
+	sort_values_by_comparison(colname='indicador', precedence={'Industrias de la energía': 0, 'Transporte': 1, 'Industrias manufactureras y de la construcción': 2, 'Otros sectores': 3, 'Emisiones fugitivas provenientes de la fabricación de combustibles': 4}, prefix=['anio'], suffix=[])
 )
 #  PIPELINE_END
 
@@ -66,6 +75,21 @@ rename_cols(map={'valor_en_mtco2e': 'valor', 'subsector': 'indicador'}),
 #  |    |   anio | indicador                                                          |   valor |
 #  |---:|-------:|:-------------------------------------------------------------------|--------:|
 #  |  0 |   1990 | Emisiones fugitivas provenientes de la fabricación de combustibles |    6.44 |
+#  
+#  ------------------------------
+#  
+#  sort_values_by_comparison(colname='indicador', precedence={'Industrias de la energía': 0, 'Transporte': 1, 'Industrias manufactureras y de la construcción': 2, 'Otros sectores': 3, 'Emisiones fugitivas provenientes de la fabricación de combustibles': 4}, prefix=['anio'], suffix=[])
+#  Index: 145 entries, 1 to 140
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       145 non-null    int64  
+#   1   indicador  145 non-null    object 
+#   2   valor      145 non-null    float64
+#  
+#  |    |   anio | indicador                |   valor |
+#  |---:|-------:|:-------------------------|--------:|
+#  |  1 |   1990 | Industrias de la energía |   22.29 |
 #  
 #  ------------------------------
 #  
