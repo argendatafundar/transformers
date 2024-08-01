b727cb8d52bf61470ce30e25badfe5436b1f10af
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-01 17:29:50 -0300

    Update AGROPE_g01_transformer.py

--- AGROPE/AGROPE_g01_transformer.py
+++ AGROPE/AGROPE_g01_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'participacion_agro': 'valor'})
+rename_cols(map={'participacion_agro': 'valor'}),
+	multiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -39,9 +45,23 @@ rename_cols(map={'participacion_agro': 'valor'})
 #   0   anio    89 non-null     int64  
 #   1   valor   89 non-null     float64
 #  
-#  |    |   anio |    valor |
-#  |---:|-------:|---------:|
-#  |  0 |   1935 | 0.206776 |
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   1935 | 20.6776 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 89 entries, 0 to 88
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    89 non-null     int64  
+#   1   valor   89 non-null     float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   1935 | 20.6776 |
 #  
 #  ------------------------------
 #  
