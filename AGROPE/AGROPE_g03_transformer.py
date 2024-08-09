cc96ac4546cd82f85d50d11a037dd0fa24f1f2fb
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-09 09:11:37 -0300

    Update AGROPE_g03_transformer.py

--- AGROPE/AGROPE_g03_transformer.py
+++ AGROPE/AGROPE_g03_transformer.py
@@ -38,77 +38,77 @@ rename_cols(map={'cuenta': 'indicador', 'participacion_pbi': 'valor'}),
 
 
 #  start()
-#  RangeIndex: 76 entries, 0 to 75
+#  RangeIndex: 80 entries, 0 to 79
 #  Data columns (total 3 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   anio               76 non-null     int64  
-#   1   cuenta             76 non-null     object 
-#   2   participacion_pbi  76 non-null     float64
+#   0   anio               80 non-null     int64  
+#   1   cuenta             80 non-null     object 
+#   2   participacion_pbi  80 non-null     float64
 #  
 #  |    |   anio | cuenta      |   participacion_pbi |
 #  |---:|-------:|:------------|--------------------:|
-#  |  0 |   2004 | Agricultura |                0.05 |
+#  |  0 |   2004 | Agricultura |           0.0496514 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'cuenta': 'indicador', 'participacion_pbi': 'valor'})
-#  RangeIndex: 76 entries, 0 to 75
+#  RangeIndex: 80 entries, 0 to 79
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       76 non-null     int64  
-#   1   indicador  76 non-null     object 
-#   2   valor      76 non-null     float64
+#   0   anio       80 non-null     int64  
+#   1   indicador  80 non-null     object 
+#   2   valor      80 non-null     float64
 #  
 #  |    |   anio | indicador   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2004 | Agricultura |       5 |
+#  |  0 |   2004 | Agricultura | 4.96514 |
 #  
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 76 entries, 0 to 75
+#  RangeIndex: 80 entries, 0 to 79
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       76 non-null     int64  
-#   1   indicador  76 non-null     object 
-#   2   valor      76 non-null     float64
+#   0   anio       80 non-null     int64  
+#   1   indicador  80 non-null     object 
+#   2   valor      80 non-null     float64
 #  
 #  |    |   anio | indicador   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2004 | Agricultura |       5 |
+#  |  0 |   2004 | Agricultura | 4.96514 |
 #  
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio'])
-#  RangeIndex: 76 entries, 0 to 75
+#  RangeIndex: 80 entries, 0 to 79
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       76 non-null     int64  
-#   1   indicador  76 non-null     object 
-#   2   valor      76 non-null     float64
+#   0   anio       80 non-null     int64  
+#   1   indicador  80 non-null     object 
+#   2   valor      80 non-null     float64
 #  
 #  |    |   anio | indicador   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2004 | Agricultura |       5 |
+#  |  0 |   2004 | Agricultura | 4.96514 |
 #  
 #  ------------------------------
 #  
 #  query(condition="indicador != 'Total'")
-#  Index: 57 entries, 0 to 74
+#  Index: 60 entries, 0 to 78
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       57 non-null     int64  
-#   1   indicador  57 non-null     object 
-#   2   valor      57 non-null     float64
+#   0   anio       60 non-null     int64  
+#   1   indicador  60 non-null     object 
+#   2   valor      60 non-null     float64
 #  
 #  |    |   anio | indicador   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   2004 | Agricultura |       5 |
+#  |  0 |   2004 | Agricultura | 4.96514 |
 #  
 #  ------------------------------
 #  
