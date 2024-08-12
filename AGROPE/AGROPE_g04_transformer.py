6bf124dddc4c2156cb7462884007e065938f76fc
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-12 14:54:03 -0300

    Update AGROPE_g04_transformer.py

--- AGROPE/AGROPE_g04_transformer.py
+++ AGROPE/AGROPE_g04_transformer.py
@@ -13,9 +13,8 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
-    return df
+def drop_na(df:DataFrame, cols:list):
+    return df.dropna(subset=cols)
 
 @transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
@@ -30,86 +29,86 @@ def sort_values(df: DataFrame, how: str, by: list):
 pipeline = chain(
 rename_cols(map={'iso3c': 'geocodigo', 'va_agro_sobre_pbi': 'valor'}),
 	drop_col(col='pais', axis=1),
-	drop_na(col=['valor']),
+	drop_na(cols=['valor']),
 	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 756 entries, 0 to 755
+#  RangeIndex: 4214 entries, 0 to 4213
 #  Data columns (total 4 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   iso3c              756 non-null    object 
-#   1   anio               756 non-null    int64  
-#   2   va_agro_sobre_pbi  631 non-null    float64
-#   3   pais               756 non-null    object 
+#   0   iso3c              4214 non-null   object 
+#   1   anio               4214 non-null   int64  
+#   2   va_agro_sobre_pbi  4214 non-null   float64
+#   3   pais               4214 non-null   object 
 #  
-#  |    | iso3c   |   anio |   va_agro_sobre_pbi | pais                   |
-#  |---:|:--------|-------:|--------------------:|:-----------------------|
-#  |  0 | HIC     |   2022 |                 nan | Países de ingreso alto |
+#  |    | iso3c   |   anio |   va_agro_sobre_pbi | pais   |
+#  |---:|:--------|-------:|--------------------:|:-------|
+#  |  0 | ABW     |   2008 |           0.0169974 | Aruba  |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'iso3c': 'geocodigo', 'va_agro_sobre_pbi': 'valor'})
-#  RangeIndex: 756 entries, 0 to 755
+#  RangeIndex: 4214 entries, 0 to 4213
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  756 non-null    object 
-#   1   anio       756 non-null    int64  
-#   2   valor      631 non-null    float64
-#   3   pais       756 non-null    object 
+#   0   geocodigo  4214 non-null   object 
+#   1   anio       4214 non-null   int64  
+#   2   valor      4214 non-null   float64
+#   3   pais       4214 non-null   object 
 #  
-#  |    | geocodigo   |   anio |   valor | pais                   |
-#  |---:|:------------|-------:|--------:|:-----------------------|
-#  |  0 | HIC         |   2022 |     nan | Países de ingreso alto |
+#  |    | geocodigo   |   anio |     valor | pais   |
+#  |---:|:------------|-------:|----------:|:-------|
+#  |  0 | ABW         |   2008 | 0.0169974 | Aruba  |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='pais', axis=1)
-#  RangeIndex: 756 entries, 0 to 755
+#  RangeIndex: 4214 entries, 0 to 4213
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  756 non-null    object 
-#   1   anio       756 non-null    int64  
-#   2   valor      631 non-null    float64
+#   0   geocodigo  4214 non-null   object 
+#   1   anio       4214 non-null   int64  
+#   2   valor      4214 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | HIC         |   2022 |     nan |
+#  |    | geocodigo   |   anio |     valor |
+#  |---:|:------------|-------:|----------:|
+#  |  0 | ABW         |   2008 | 0.0169974 |
 #  
 #  ------------------------------
 #  
-#  drop_na(col=['valor'])
-#  Index: 631 entries, 1 to 732
+#  drop_na(cols=['valor'])
+#  RangeIndex: 4214 entries, 0 to 4213
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  631 non-null    object 
-#   1   anio       631 non-null    int64  
-#   2   valor      631 non-null    float64
+#   0   geocodigo  4214 non-null   object 
+#   1   anio       4214 non-null   int64  
+#   2   valor      4214 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  1 | HIC         |   2021 | 1.27667 |
+#  |    | geocodigo   |   anio |     valor |
+#  |---:|:------------|-------:|----------:|
+#  |  0 | ABW         |   2008 | 0.0169974 |
 #  
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio', 'geocodigo'])
-#  RangeIndex: 631 entries, 0 to 630
+#  RangeIndex: 4214 entries, 0 to 4213
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  631 non-null    object 
-#   1   anio       631 non-null    int64  
-#   2   valor      631 non-null    float64
+#   0   geocodigo  4214 non-null   object 
+#   1   anio       4214 non-null   int64  
+#   2   valor      4214 non-null   float64
 #  
 #  |    | geocodigo   |   anio |   valor |
 #  |---:|:------------|-------:|--------:|
-#  |  0 | BRA         |   1960 | 15.7324 |
+#  |  0 | BEN         |   1960 | 46.1577 |
 #  
 #  ------------------------------
 #  
