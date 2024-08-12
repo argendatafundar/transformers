ddeea2e7119fae546b0d5a64d42205ecf02b2e8a
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-12 13:49:00 -0300

    Update ACECON_g06_transformer.py

--- ACECON/ACECON_g06_transformer.py
+++ ACECON/ACECON_g06_transformer.py
@@ -7,12 +7,30 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'comp_dem_ag': 'indicador'})
+rename_cols(map={'comp_dem_ag': 'indicador'}),
+	replace_value(col='indicador', curr_value='Consumo hogares', new_value='Consumo privado'),
+	replace_value(col='indicador', curr_value='Consumo gobierno', new_value='Consumo público'),
+	replace_value(col='indicador', curr_value='Formacion bruta de capital', new_value='Inversión')
 )
 #  PIPELINE_END
 
@@ -44,6 +62,51 @@ rename_cols(map={'comp_dem_ag': 'indicador'})
 #  |    |   anio | indicador       |   valor |
 #  |---:|-------:|:----------------|--------:|
 #  |  0 |   1935 | Consumo hogares | 75.7468 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='Consumo hogares', new_value='Consumo privado')
+#  RangeIndex: 528 entries, 0 to 527
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       528 non-null    int64  
+#   1   indicador  528 non-null    object 
+#   2   valor      528 non-null    float64
+#  
+#  |    |   anio | indicador       |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1935 | Consumo privado | 75.7468 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='Consumo gobierno', new_value='Consumo público')
+#  RangeIndex: 528 entries, 0 to 527
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       528 non-null    int64  
+#   1   indicador  528 non-null    object 
+#   2   valor      528 non-null    float64
+#  
+#  |    |   anio | indicador       |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1935 | Consumo privado | 75.7468 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='indicador', curr_value='Formacion bruta de capital', new_value='Inversión')
+#  RangeIndex: 528 entries, 0 to 527
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       528 non-null    int64  
+#   1   indicador  528 non-null    object 
+#   2   valor      528 non-null    float64
+#  
+#  |    |   anio | indicador       |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1935 | Consumo privado | 75.7468 |
 #  
 #  ------------------------------
 #  
