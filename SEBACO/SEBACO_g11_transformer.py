cd64f695ef01e21a710fb7c11df45bf6e01c35b6
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2024-08-15 15:28:25 -0300

    Update SEBACO_g11_transformer.py

--- SEBACO/SEBACO_g11_transformer.py
+++ SEBACO/SEBACO_g11_transformer.py
@@ -9,7 +9,7 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 
@@ -49,24 +49,24 @@ def resub(df: DataFrame, col: str, pattern: str, replace: str):
     return df
 
 @transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
+    return df.sort_values(by=[col1,col2])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop_ocupados': 'valor'}),
-	mutiplicar_por_escalar(col='valor', k=100),
+	multiplicar_por_escalar(col='valor', k=100),
 	resub(col='categoria', pattern='a.', replace=''),
 	resub(col='categoria', pattern='b.', replace=''),
 	resub(col='categoria', pattern='c.', replace=''),
 	resub(col='categoria', pattern='d.', replace=''),
 	resub(col='categoria', pattern='e.', replace=''),
-	sort_values(how='ascending', by=['categoria', 'indicador'])
+	ordenar_dos_columnas(col1='indicador', order1=['Asalariado registrado', 'Asalariado no registrado', 'No asalariado'], col2='categoria', order2=[' Actividades profesionales, cientificas y técnicas', ' Sector privado', ' SBC', ' Total economía', ' SSI'])
 )
 #  PIPELINE_END
 
@@ -89,11 +89,11 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop_ocupados': 'valor'})
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -101,14 +101,14 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  
 #  ------------------------------
 #  
-#  mutiplicar_por_escalar(col='valor', k=100)
+#  multiplicar_por_escalar(col='valor', k=100)
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -119,11 +119,11 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  resub(col='categoria', pattern='a.', replace='')
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -134,11 +134,11 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  resub(col='categoria', pattern='b.', replace='')
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -149,11 +149,11 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  resub(col='categoria', pattern='c.', replace='')
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -164,11 +164,11 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  resub(col='categoria', pattern='d.', replace='')
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -179,11 +179,11 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  resub(col='categoria', pattern='e.', replace='')
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
 #  |    | categoria                                         | indicador                |   valor |
 #  |---:|:--------------------------------------------------|:-------------------------|--------:|
@@ -191,18 +191,18 @@ rename_cols(map={'sector': 'categoria', 'estado_ocupacional': 'indicador', 'prop
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['categoria', 'indicador'])
-#  RangeIndex: 15 entries, 0 to 14
+#  ordenar_dos_columnas(col1='indicador', order1=['Asalariado registrado', 'Asalariado no registrado', 'No asalariado'], col2='categoria', order2=[' Actividades profesionales, cientificas y técnicas', ' Sector privado', ' SBC', ' Total economía', ' SSI'])
+#  Index: 15 entries, 1 to 5
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  15 non-null     object 
-#   1   indicador  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  15 non-null     category
+#   1   indicador  15 non-null     category
+#   2   valor      15 non-null     float64 
 #  
-#  |    | categoria                                         | indicador                |   valor |
-#  |---:|:--------------------------------------------------|:-------------------------|--------:|
-#  |  0 | Actividades profesionales, cientificas y técnicas | Asalariado no registrado | 14.6243 |
+#  |    | categoria                                         | indicador             |   valor |
+#  |---:|:--------------------------------------------------|:----------------------|--------:|
+#  |  1 | Actividades profesionales, cientificas y técnicas | Asalariado registrado |  32.478 |
 #  
 #  ------------------------------
 #  
