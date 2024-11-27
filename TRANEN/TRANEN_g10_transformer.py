bd5792b2dff57705eca245ee3d3324309f08239e
jgjuara <jg.juara@gmail.com>
2024-11-27 11:14:58 -0300

    Update TRANEN_g10_transformer.py

--- TRANEN/TRANEN_g10_transformer.py
+++ TRANEN/TRANEN_g10_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     df = df.replace({col: curr_value}, new_value)
     return df
@@ -12,6 +17,10 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 
 @transformer.convert
 def drop_na(df:DataFrame, cols:list):
@@ -27,8 +36,10 @@ def sort_values(df: DataFrame, how: str, by: list):
 
 #  PIPELINE_START
 pipeline = chain(
-replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
-	rename_cols(map={'valor_en_twh': 'valor', 'iso3': 'geocodigo'}),
+query(condition='fuente_energia == "Biocombustibles"'),
+	replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
+	rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'}),
+	drop_col(col=['valor_en_twh', 'tipo_energia', 'indicador'], axis=1),
 	drop_na(cols=['valor']),
 	sort_values(how='ascending', by=['anio', 'geocodigo'])
 )
@@ -36,77 +47,119 @@ replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
 
 
 #  start()
-#  RangeIndex: 850 entries, 0 to 849
-#  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          850 non-null    int64  
-#   1   iso3          850 non-null    object 
-#   2   valor_en_twh  637 non-null    float64
+#  RangeIndex: 52923 entries, 0 to 52922
+#  Data columns (total 6 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   iso3            52923 non-null  object 
+#   1   anio            52923 non-null  int64  
+#   2   fuente_energia  52923 non-null  object 
+#   3   tipo_energia    52923 non-null  object 
+#   4   valor_en_twh    41782 non-null  float64
+#   5   porcentaje      52923 non-null  float64
 #  
-#  |    |   anio | iso3   |   valor_en_twh |
-#  |---:|-------:|:-------|---------------:|
-#  |  0 |   1990 | USA    |        16.7327 |
+#  |    | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
+#  |---:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
+#  |  0 | AGO    |   1965 | Biocombustibles  | Limpias        |            nan |            0 |
+#  
+#  ------------------------------
+#  
+#  query(condition='fuente_energia == "Biocombustibles"')
+#  Index: 6490 entries, 0 to 52804
+#  Data columns (total 6 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   iso3            6490 non-null   object 
+#   1   anio            6490 non-null   int64  
+#   2   fuente_energia  6490 non-null   object 
+#   3   tipo_energia    6490 non-null   object 
+#   4   valor_en_twh    1885 non-null   float64
+#   5   porcentaje      6490 non-null   float64
+#  
+#  |    | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
+#  |---:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
+#  |  0 | AGO    |   1965 | Biocombustibles  | Limpias        |            nan |            0 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD')
-#  RangeIndex: 850 entries, 0 to 849
-#  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          850 non-null    int64  
-#   1   iso3          850 non-null    object 
-#   2   valor_en_twh  637 non-null    float64
+#  Index: 6490 entries, 0 to 52804
+#  Data columns (total 6 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   iso3            6490 non-null   object 
+#   1   anio            6490 non-null   int64  
+#   2   fuente_energia  6490 non-null   object 
+#   3   tipo_energia    6490 non-null   object 
+#   4   valor_en_twh    1885 non-null   float64
+#   5   porcentaje      6490 non-null   float64
 #  
-#  |    |   anio | iso3   |   valor_en_twh |
-#  |---:|-------:|:-------|---------------:|
-#  |  0 |   1990 | USA    |        16.7327 |
+#  |    | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
+#  |---:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
+#  |  0 | AGO    |   1965 | Biocombustibles  | Limpias        |            nan |            0 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'valor_en_twh': 'valor', 'iso3': 'geocodigo'})
-#  RangeIndex: 850 entries, 0 to 849
+#  rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'})
+#  Index: 6490 entries, 0 to 52804
+#  Data columns (total 6 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   geocodigo     6490 non-null   object 
+#   1   anio          6490 non-null   int64  
+#   2   indicador     6490 non-null   object 
+#   3   tipo_energia  6490 non-null   object 
+#   4   valor_en_twh  1885 non-null   float64
+#   5   valor         6490 non-null   float64
+#  
+#  |    | geocodigo   |   anio | indicador       | tipo_energia   |   valor_en_twh |   valor |
+#  |---:|:------------|-------:|:----------------|:---------------|---------------:|--------:|
+#  |  0 | AGO         |   1965 | Biocombustibles | Limpias        |            nan |       0 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['valor_en_twh', 'tipo_energia', 'indicador'], axis=1)
+#  Index: 6490 entries, 0 to 52804
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       850 non-null    int64  
-#   1   geocodigo  850 non-null    object 
-#   2   valor      637 non-null    float64
+#   0   geocodigo  6490 non-null   object 
+#   1   anio       6490 non-null   int64  
+#   2   valor      6490 non-null   float64
 #  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1990 | USA         | 16.7327 |
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AGO         |   1965 |       0 |
 #  
 #  ------------------------------
 #  
 #  drop_na(cols=['valor'])
-#  Index: 637 entries, 0 to 849
+#  Index: 6490 entries, 0 to 52804
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       637 non-null    int64  
-#   1   geocodigo  637 non-null    object 
-#   2   valor      637 non-null    float64
+#   0   geocodigo  6490 non-null   object 
+#   1   anio       6490 non-null   int64  
+#   2   valor      6490 non-null   float64
 #  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1990 | USA         | 16.7327 |
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AGO         |   1965 |       0 |
 #  
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio', 'geocodigo'])
-#  Index: 637 entries, 34 to 169
+#  Index: 6490 entries, 0 to 52804
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       637 non-null    int64  
-#   1   geocodigo  637 non-null    object 
-#   2   valor      637 non-null    float64
+#   0   geocodigo  6490 non-null   object 
+#   1   anio       6490 non-null   int64  
+#   2   valor      6490 non-null   float64
 #  
-#  |    |   anio | geocodigo   |    valor |
-#  |---:|-------:|:------------|---------:|
-#  | 34 |   1990 | AUT         | 0.073269 |
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | AGO         |   1965 |       0 |
 #  
 #  ------------------------------
 #  
