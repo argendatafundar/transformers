b6e9a197a41faa7e8d00cdaa6a810586ea478db7
jgjuara <jg.juara@gmail.com>
2025-02-21 15:39:11 -0300

    Update CRECIM_g14_transformer.py

--- CRECIM/CRECIM_g14_transformer.py
+++ CRECIM/CRECIM_g14_transformer.py
@@ -4,12 +4,9 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def map_values(df: DataFrame, col: str, mapping: dict):
+    # Mapea los valores de la columna especificada usando el diccionario de mapeo
+    df[col] = df[col].map(mapping)
     return df
 
 @transformer.convert
@@ -26,87 +23,70 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='iso3', axis=1),
-	replace_value(col='pais_nombre', curr_value='Resto LATAM', new_value='Resto de América Latina'),
-	rename_cols(map={'pais_nombre': 'categoria', 'participacion': 'valor'}),
+map_values(col='iso3', mapping={'CRECIM_AML-RESTO': 'Resto de América Latina', 'ARG': 'Argentina', 'BRA': 'Brasil', 'MEX': 'México'}),
+	rename_cols(map={'iso3': 'categoria', 'participacion': 'valor'}),
 	multiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 64 entries, 0 to 63
-#  Data columns (total 4 columns):
-#   #   Column         Non-Null Count  Dtype  
-#  ---  ------         --------------  -----  
-#   0   iso3           48 non-null     object 
-#   1   pais_nombre    64 non-null     object 
-#   2   anio           64 non-null     int64  
-#   3   participacion  64 non-null     float64
-#  
-#  |    |   iso3 | pais_nombre   |   anio |   participacion |
-#  |---:|-------:|:--------------|-------:|----------------:|
-#  |  0 |    nan | Resto LATAM   |   1820 |        0.405228 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3', axis=1)
-#  RangeIndex: 64 entries, 0 to 63
+#  RangeIndex: 84 entries, 0 to 83
 #  Data columns (total 3 columns):
 #   #   Column         Non-Null Count  Dtype  
 #  ---  ------         --------------  -----  
-#   0   pais_nombre    64 non-null     object 
-#   1   anio           64 non-null     int64  
-#   2   participacion  64 non-null     float64
+#   0   anio           84 non-null     int64  
+#   1   iso3           84 non-null     object 
+#   2   participacion  84 non-null     float64
 #  
-#  |    | pais_nombre   |   anio |   participacion |
-#  |---:|:--------------|-------:|----------------:|
-#  |  0 | Resto LATAM   |   1820 |        0.405228 |
+#  |    |   anio | iso3   |   participacion |
+#  |---:|-------:|:-------|----------------:|
+#  |  0 |   1820 | ARG    |       0.0428832 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='pais_nombre', curr_value='Resto LATAM', new_value='Resto de América Latina')
-#  RangeIndex: 64 entries, 0 to 63
+#  map_values(col='iso3', mapping={'CRECIM_AML-RESTO': 'Resto de América Latina', 'ARG': 'Argentina', 'BRA': 'Brasil', 'MEX': 'México'})
+#  RangeIndex: 84 entries, 0 to 83
 #  Data columns (total 3 columns):
 #   #   Column         Non-Null Count  Dtype  
 #  ---  ------         --------------  -----  
-#   0   pais_nombre    64 non-null     object 
-#   1   anio           64 non-null     int64  
-#   2   participacion  64 non-null     float64
+#   0   anio           84 non-null     int64  
+#   1   iso3           84 non-null     object 
+#   2   participacion  84 non-null     float64
 #  
-#  |    | pais_nombre             |   anio |   participacion |
-#  |---:|:------------------------|-------:|----------------:|
-#  |  0 | Resto de América Latina |   1820 |        0.405228 |
+#  |    |   anio | iso3      |   participacion |
+#  |---:|-------:|:----------|----------------:|
+#  |  0 |   1820 | Argentina |       0.0428832 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'pais_nombre': 'categoria', 'participacion': 'valor'})
-#  RangeIndex: 64 entries, 0 to 63
+#  rename_cols(map={'iso3': 'categoria', 'participacion': 'valor'})
+#  RangeIndex: 84 entries, 0 to 83
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   categoria  64 non-null     object 
-#   1   anio       64 non-null     int64  
-#   2   valor      64 non-null     float64
+#   0   anio       84 non-null     int64  
+#   1   categoria  84 non-null     object 
+#   2   valor      84 non-null     float64
 #  
-#  |    | categoria               |   anio |   valor |
-#  |---:|:------------------------|-------:|--------:|
-#  |  0 | Resto de América Latina |   1820 | 40.5228 |
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1820 | Argentina   | 4.28832 |
 #  
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 64 entries, 0 to 63
+#  RangeIndex: 84 entries, 0 to 83
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   categoria  64 non-null     object 
-#   1   anio       64 non-null     int64  
-#   2   valor      64 non-null     float64
+#   0   anio       84 non-null     int64  
+#   1   categoria  84 non-null     object 
+#   2   valor      84 non-null     float64
 #  
-#  |    | categoria               |   anio |   valor |
-#  |---:|:------------------------|-------:|--------:|
-#  |  0 | Resto de América Latina |   1820 | 40.5228 |
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1820 | Argentina   | 4.28832 |
 #  
 #  ------------------------------
 #  
