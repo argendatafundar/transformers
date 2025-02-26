7f92cebb16269501f7f779c5a124a9e1372b4123
jgjuara <jg.juara@gmail.com>
2025-02-26 12:48:07 -0300

    Update CRECIM_g17_transformer.py

--- CRECIM/CRECIM_g17_transformer.py
+++ CRECIM/CRECIM_g17_transformer.py
@@ -17,6 +17,13 @@ def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
@@ -24,7 +31,8 @@ def query(df: DataFrame, condition: str):
 pipeline = chain(
 rename_cols(map={'iso3': 'geocodigo', 'cambio_relativo': 'valor'}),
 	mutiplicar_por_escalar(col='valor', k=100),
-	query(condition="geocodigo not in ('LAC', 'TLA', 'DESHUM_ZZH.LAC', 'DESHUM_AHDI.LAC')")
+	query(condition="geocodigo not in ('LAC', 'TLA', 'DESHUM_ZZH.LAC', 'DESHUM_AHDI.LAC')"),
+	sort_values(how='ascending', by='anio')
 )
 #  PIPELINE_END
 
@@ -86,6 +94,21 @@ rename_cols(map={'iso3': 'geocodigo', 'cambio_relativo': 'valor'}),
 #  |    | geocodigo   |   anio |   valor |
 #  |---:|:------------|-------:|--------:|
 #  |  0 | AFE         |   2023 | -2.1853 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by='anio')
+#  RangeIndex: 3229 entries, 0 to 3228
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  3229 non-null   object 
+#   1   anio       3229 non-null   int64  
+#   2   valor      3229 non-null   float64
+#  
+#  |    | geocodigo   |   anio |   valor |
+#  |---:|:------------|-------:|--------:|
+#  |  0 | ZWE         |   2011 |       0 |
 #  
 #  ------------------------------
 #  
