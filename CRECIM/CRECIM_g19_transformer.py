bcf687db837fef1ac94a3224c1b3bbb9323f37ec
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-03-10 12:05:43 -0300

    Update CRECIM_g19_transformer.py

--- CRECIM/CRECIM_g19_transformer.py
+++ CRECIM/CRECIM_g19_transformer.py
@@ -4,152 +4,89 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def latest_year(df, by='anio'):
-    latest_year = df[by].max()
-    df = df.query(f'{by} == {latest_year}')
-    df = df.drop(columns = by)
-    return df
-
-@transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='provincia_nombre', axis=1),
-	rename_cols(map={'provincia_id': 'geocodigo'}),
-	rename_cols(map={'pbg_pc_relativo': 'valor'}),
-	latest_year(by='anio'),
-	sort_values(how='descending', by='valor'),
-	multiplicar_por_escalar(col='valor', k=0.01)
+rename_cols(map={'provincia_id': 'geocodigo', 'pib_pc_relativo': 'valor'}),
+	query(condition='anio == anio.max()'),
+	drop_col(col=['anio', 'region_pbg'], axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 432 entries, 0 to 431
+#  RangeIndex: 672 entries, 0 to 671
 #  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   provincia_id      432 non-null    object 
-#   1   provincia_nombre  432 non-null    object 
-#   2   anio              432 non-null    int64  
-#   3   pbg_pc_relativo   432 non-null    float64
-#  
-#  |    | provincia_id   | provincia_nombre                |   anio |   pbg_pc_relativo |
-#  |---:|:---------------|:--------------------------------|-------:|------------------:|
-#  |  0 | AR-C           | Ciudad Autónoma de Buenos Aires |   2004 |             274.4 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='provincia_nombre', axis=1)
-#  RangeIndex: 432 entries, 0 to 431
-#  Data columns (total 3 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   provincia_id     432 non-null    object 
-#   1   anio             432 non-null    int64  
-#   2   pbg_pc_relativo  432 non-null    float64
+#   0   region_pbg       672 non-null    object 
+#   1   anio             672 non-null    int64  
+#   2   pib_pc_relativo  672 non-null    float64
+#   3   provincia_id     672 non-null    object 
 #  
-#  |    | provincia_id   |   anio |   pbg_pc_relativo |
-#  |---:|:---------------|-------:|------------------:|
-#  |  0 | AR-C           |   2004 |             274.4 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'provincia_id': 'geocodigo'})
-#  RangeIndex: 432 entries, 0 to 431
-#  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geocodigo        432 non-null    object 
-#   1   anio             432 non-null    int64  
-#   2   pbg_pc_relativo  432 non-null    float64
-#  
-#  |    | geocodigo   |   anio |   pbg_pc_relativo |
-#  |---:|:------------|-------:|------------------:|
-#  |  0 | AR-C        |   2004 |             274.4 |
+#  |    | region_pbg      |   anio |   pib_pc_relativo | provincia_id   |
+#  |---:|:----------------|-------:|------------------:|:---------------|
+#  |  0 | Pampeana y AMBA |   1895 |           102.682 | AR-B           |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'pbg_pc_relativo': 'valor'})
-#  RangeIndex: 432 entries, 0 to 431
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  432 non-null    object 
-#   1   anio       432 non-null    int64  
-#   2   valor      432 non-null    float64
+#  rename_cols(map={'provincia_id': 'geocodigo', 'pib_pc_relativo': 'valor'})
+#  RangeIndex: 672 entries, 0 to 671
+#  Data columns (total 4 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   region_pbg  672 non-null    object 
+#   1   anio        672 non-null    int64  
+#   2   valor       672 non-null    float64
+#   3   geocodigo   672 non-null    object 
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AR-C        |   2004 |   274.4 |
+#  |    | region_pbg      |   anio |   valor | geocodigo   |
+#  |---:|:----------------|-------:|--------:|:------------|
+#  |  0 | Pampeana y AMBA |   1895 | 102.682 | AR-B        |
 #  
 #  ------------------------------
 #  
-#  latest_year(by='anio')
-#  Index: 24 entries, 17 to 431
+#  query(condition='anio == anio.max()')
+#  Index: 24 entries, 27 to 671
+#  Data columns (total 4 columns):
+#   #   Column      Non-Null Count  Dtype  
+#  ---  ------      --------------  -----  
+#   0   region_pbg  24 non-null     object 
+#   1   anio        24 non-null     int64  
+#   2   valor       24 non-null     float64
+#   3   geocodigo   24 non-null     object 
+#  
+#  |    | region_pbg      |   anio |   valor | geocodigo   |
+#  |---:|:----------------|-------:|--------:|:------------|
+#  | 27 | Pampeana y AMBA |   2022 | 84.1437 | AR-B        |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['anio', 'region_pbg'], axis=1)
+#  Index: 24 entries, 27 to 671
 #  Data columns (total 2 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  24 non-null     object 
-#   1   valor      24 non-null     float64
+#   0   valor      24 non-null     float64
+#   1   geocodigo  24 non-null     object 
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  | 17 | AR-C        |   293.3 |
-#  
-#  ------------------------------
-#  
-#  sort_values(how='descending', by='valor')
-#  RangeIndex: 24 entries, 0 to 23
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  24 non-null     object 
-#   1   valor      24 non-null     float64
-#  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | AR-C        |   2.933 |
-#  
-#  ------------------------------
-#  
-#  multiplicar_por_escalar(col='valor', k=0.01)
-#  RangeIndex: 24 entries, 0 to 23
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  24 non-null     object 
-#   1   valor      24 non-null     float64
-#  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | AR-C        |   2.933 |
+#  |    |   valor | geocodigo   |
+#  |---:|--------:|:------------|
+#  | 27 | 84.1437 | AR-B        |
 #  
 #  ------------------------------
 #  
