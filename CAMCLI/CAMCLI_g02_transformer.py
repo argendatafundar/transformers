49240c652a4f52647f5dbb90d81dc086be346fc8
jgjuara <jg.juara@gmail.com>
2025-04-15 10:57:31 -0300

    Update CAMCLI_g02_transformer.py

--- CAMCLI/CAMCLI_g02_transformer.py
+++ CAMCLI/CAMCLI_g02_transformer.py
@@ -43,93 +43,93 @@ drop_col(col='iso3', axis=1),
 
 
 #  start()
-#  RangeIndex: 67977 entries, 0 to 67976
+#  RangeIndex: 23645 entries, 0 to 23644
 #  Data columns (total 4 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   iso3               59514 non-null  object 
-#   1   continente_fundar  58968 non-null  object 
-#   2   anio               67977 non-null  int64  
-#   3   valor_en_ton       67977 non-null  float64
+#   0   iso3               23645 non-null  object 
+#   1   continente_fundar  23186 non-null  object 
+#   2   anio               23645 non-null  int64  
+#   3   valor_en_ton       23645 non-null  float64
 #  
 #  |    | iso3   | continente_fundar   |   anio |   valor_en_ton |
 #  |---:|:-------|:--------------------|-------:|---------------:|
-#  |  0 | AFG    | Asia                |   1750 |              0 |
+#  |  0 | AFG    | Asia                |   1949 |          14656 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='iso3', axis=1)
-#  RangeIndex: 67977 entries, 0 to 67976
+#  RangeIndex: 23645 entries, 0 to 23644
 #  Data columns (total 3 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   continente_fundar  58968 non-null  object 
-#   1   anio               67977 non-null  int64  
-#   2   valor_en_ton       67977 non-null  float64
+#   0   continente_fundar  23186 non-null  object 
+#   1   anio               23645 non-null  int64  
+#   2   valor_en_ton       23645 non-null  float64
 #  
 #  |    | continente_fundar   |   anio |   valor_en_ton |
 #  |---:|:--------------------|-------:|---------------:|
-#  |  0 | Asia                |   1750 |              0 |
+#  |  0 | Asia                |   1949 |          14656 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'continente_fundar': 'indicador', 'valor_en_ton': 'valor'})
-#  RangeIndex: 67977 entries, 0 to 67976
+#  RangeIndex: 23645 entries, 0 to 23644
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  58968 non-null  object 
-#   1   anio       67977 non-null  int64  
-#   2   valor      67977 non-null  float64
+#   0   indicador  23186 non-null  object 
+#   1   anio       23645 non-null  int64  
+#   2   valor      23645 non-null  float64
 #  
 #  |    | indicador   |   anio |   valor |
 #  |---:|:------------|-------:|--------:|
-#  |  0 | Asia        |   1750 |       0 |
+#  |  0 | Asia        |   1949 |   14656 |
 #  
 #  ------------------------------
 #  
 #  agrupar_y_sumar(col_indicador='indicador', col_anio='anio')
-#  RangeIndex: 1911 entries, 0 to 1910
+#  RangeIndex: 1381 entries, 0 to 1380
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  1911 non-null   object 
-#   1   anio       1911 non-null   int64  
-#   2   valor      1911 non-null   float64
+#   0   indicador  1381 non-null   object 
+#   1   anio       1381 non-null   int64  
+#   2   valor      1381 non-null   float64
 #  
-#  |    | indicador                              |   anio |   valor |
-#  |---:|:---------------------------------------|-------:|--------:|
-#  |  0 | América del Norte, Central y el Caribe |   1750 |       0 |
+#  |    | indicador                              |   anio |    valor |
+#  |---:|:---------------------------------------|-------:|---------:|
+#  |  0 | América del Norte, Central y el Caribe |   1785 | 0.003664 |
 #  
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=1e-06)
-#  RangeIndex: 1911 entries, 0 to 1910
+#  RangeIndex: 1381 entries, 0 to 1380
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  1911 non-null   object 
-#   1   anio       1911 non-null   int64  
-#   2   valor      1911 non-null   float64
+#   0   indicador  1381 non-null   object 
+#   1   anio       1381 non-null   int64  
+#   2   valor      1381 non-null   float64
 #  
-#  |    | indicador                              |   anio |   valor |
-#  |---:|:---------------------------------------|-------:|--------:|
-#  |  0 | América del Norte, Central y el Caribe |   1750 |       0 |
+#  |    | indicador                              |   anio |    valor |
+#  |---:|:---------------------------------------|-------:|---------:|
+#  |  0 | América del Norte, Central y el Caribe |   1785 | 0.003664 |
 #  
 #  ------------------------------
 #  
 #  sort_values_by_comparison(colname='indicador', precedence={'Asia': 0, 'América del Norte, Central y el Caribe': 1, 'Europa': 2, 'África': 3, 'América del Sur': 4, 'Transporte Internacional': 6, 'Oceanía': 5}, prefix=['anio'], suffix=[])
-#  Index: 1911 entries, 546 to 1637
+#  Index: 1381 entries, 419 to 1240
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   indicador  1911 non-null   object 
-#   1   anio       1911 non-null   int64  
-#   2   valor      1911 non-null   float64
+#   0   indicador  1381 non-null   object 
+#   1   anio       1381 non-null   int64  
+#   2   valor      1381 non-null   float64
 #  
 #  |     | indicador   |   anio |   valor |
 #  |----:|:------------|-------:|--------:|
-#  | 546 | Asia        |   1750 |       0 |
+#  | 419 | Asia        |   1750 |       0 |
 #  
 #  ------------------------------
 #  
