8dfe7f460cfef5bbc058010cb3aa74f75b68e2c3
Ignacio-ibarra <ignacioibarra.data.science@gmail.com>
2025-05-06 12:11:45 -0300

    Update TRANEN_g03_transformer.py

--- TRANEN/TRANEN_g03_transformer.py
+++ TRANEN/TRANEN_g03_transformer.py
@@ -9,26 +9,6 @@ def query(df: DataFrame, condition: str):
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -49,11 +29,7 @@ def drop_col(df: DataFrame, col, axis=1):
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='iso3 == "ARG" & anio == anio.max() & tipo_energia != "Total"'),
-	replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
-	replace_value(col='fuente_energia', curr_value='Eolica', new_value='Eólica'),
-	replace_value(col='fuente_energia', curr_value='Carbon', new_value='Carbón'),
-	replace_value(col='fuente_energia', curr_value='Petroleo', new_value='Petróleo'),
+query(condition='iso3 == "ARG" & anio == anio.max()'),
 	rename_cols(map={'fuente_energia': 'nivel2', 'porcentaje': 'valor', 'tipo_energia': 'nivel1'}),
 	drop_col(col='anio', axis=1),
 	drop_col(col='iso3', axis=1),
@@ -63,176 +39,104 @@ query(condition='iso3 == "ARG" & anio == anio.max() & tipo_energia != "Total"'),
 
 
 #  start()
-#  RangeIndex: 51852 entries, 0 to 51851
+#  RangeIndex: 38938 entries, 0 to 38937
 #  Data columns (total 6 columns):
 #   #   Column          Non-Null Count  Dtype  
 #  ---  ------          --------------  -----  
-#   0   iso3            51852 non-null  object 
-#   1   anio            51852 non-null  int64  
-#   2   fuente_energia  51852 non-null  object 
-#   3   tipo_energia    51852 non-null  object 
-#   4   valor_en_twh    40880 non-null  float64
-#   5   porcentaje      51852 non-null  float64
+#   0   anio            38938 non-null  int64  
+#   1   iso3            38938 non-null  object 
+#   2   fuente_energia  38938 non-null  object 
+#   3   valor_en_twh    38938 non-null  float64
+#   4   porcentaje      38938 non-null  float64
+#   5   tipo_energia    38938 non-null  object 
 #  
-#  |    | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
-#  |---:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
-#  |  0 | AGO    |   1965 | Biocombustibles  | Limpias        |            nan |            0 |
+#  |    |   anio | iso3   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   |
+#  |---:|-------:|:-------|:-----------------|---------------:|-------------:|:---------------|
+#  |  0 |   1971 | DZA    | Otras renovables |              0 |            0 | Limpias        |
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3 == "ARG" & anio == anio.max() & tipo_energia != "Total"')
-#  Index: 9 entries, 985 to 1443
+#  query(condition='iso3 == "ARG" & anio == anio.max()')
+#  Index: 9 entries, 111 to 34590
 #  Data columns (total 6 columns):
 #   #   Column          Non-Null Count  Dtype  
 #  ---  ------          --------------  -----  
-#   0   iso3            9 non-null      object 
-#   1   anio            9 non-null      int64  
+#   0   anio            9 non-null      int64  
+#   1   iso3            9 non-null      object 
 #   2   fuente_energia  9 non-null      object 
-#   3   tipo_energia    9 non-null      object 
-#   4   valor_en_twh    9 non-null      float64
-#   5   porcentaje      9 non-null      float64
+#   3   valor_en_twh    9 non-null      float64
+#   4   porcentaje      9 non-null      float64
+#   5   tipo_energia    9 non-null      object 
 #  
-#  |     | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
-#  |----:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
-#  | 985 | ARG    |   2022 | Biocombustibles  | Limpias        |        13.9313 |      1.39177 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD')
-#  Index: 9 entries, 985 to 1443
-#  Data columns (total 6 columns):
-#   #   Column          Non-Null Count  Dtype  
-#  ---  ------          --------------  -----  
-#   0   iso3            9 non-null      object 
-#   1   anio            9 non-null      int64  
-#   2   fuente_energia  9 non-null      object 
-#   3   tipo_energia    9 non-null      object 
-#   4   valor_en_twh    9 non-null      float64
-#   5   porcentaje      9 non-null      float64
-#  
-#  |     | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
-#  |----:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
-#  | 985 | ARG    |   2022 | Biocombustibles  | Limpias        |        13.9313 |      1.39177 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='fuente_energia', curr_value='Eolica', new_value='Eólica')
-#  Index: 9 entries, 985 to 1443
-#  Data columns (total 6 columns):
-#   #   Column          Non-Null Count  Dtype  
-#  ---  ------          --------------  -----  
-#   0   iso3            9 non-null      object 
-#   1   anio            9 non-null      int64  
-#   2   fuente_energia  9 non-null      object 
-#   3   tipo_energia    9 non-null      object 
-#   4   valor_en_twh    9 non-null      float64
-#   5   porcentaje      9 non-null      float64
-#  
-#  |     | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
-#  |----:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
-#  | 985 | ARG    |   2022 | Biocombustibles  | Limpias        |        13.9313 |      1.39177 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='fuente_energia', curr_value='Carbon', new_value='Carbón')
-#  Index: 9 entries, 985 to 1443
-#  Data columns (total 6 columns):
-#   #   Column          Non-Null Count  Dtype  
-#  ---  ------          --------------  -----  
-#   0   iso3            9 non-null      object 
-#   1   anio            9 non-null      int64  
-#   2   fuente_energia  9 non-null      object 
-#   3   tipo_energia    9 non-null      object 
-#   4   valor_en_twh    9 non-null      float64
-#   5   porcentaje      9 non-null      float64
-#  
-#  |     | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
-#  |----:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
-#  | 985 | ARG    |   2022 | Biocombustibles  | Limpias        |        13.9313 |      1.39177 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='fuente_energia', curr_value='Petroleo', new_value='Petróleo')
-#  Index: 9 entries, 985 to 1443
-#  Data columns (total 6 columns):
-#   #   Column          Non-Null Count  Dtype  
-#  ---  ------          --------------  -----  
-#   0   iso3            9 non-null      object 
-#   1   anio            9 non-null      int64  
-#   2   fuente_energia  9 non-null      object 
-#   3   tipo_energia    9 non-null      object 
-#   4   valor_en_twh    9 non-null      float64
-#   5   porcentaje      9 non-null      float64
-#  
-#  |     | iso3   |   anio | fuente_energia   | tipo_energia   |   valor_en_twh |   porcentaje |
-#  |----:|:-------|-------:|:-----------------|:---------------|---------------:|-------------:|
-#  | 985 | ARG    |   2022 | Biocombustibles  | Limpias        |        13.9313 |      1.39177 |
+#  |     |   anio | iso3   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   |
+#  |----:|-------:|:-------|:-----------------|---------------:|-------------:|:---------------|
+#  | 111 |   2023 | ARG    | Otras renovables |        7.26455 |     0.712392 | Limpias        |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'fuente_energia': 'nivel2', 'porcentaje': 'valor', 'tipo_energia': 'nivel1'})
-#  Index: 9 entries, 985 to 1443
+#  Index: 9 entries, 111 to 34590
 #  Data columns (total 6 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   iso3          9 non-null      object 
-#   1   anio          9 non-null      int64  
+#   0   anio          9 non-null      int64  
+#   1   iso3          9 non-null      object 
 #   2   nivel2        9 non-null      object 
-#   3   nivel1        9 non-null      object 
-#   4   valor_en_twh  9 non-null      float64
-#   5   valor         9 non-null      float64
+#   3   valor_en_twh  9 non-null      float64
+#   4   valor         9 non-null      float64
+#   5   nivel1        9 non-null      object 
 #  
-#  |     | iso3   |   anio | nivel2          | nivel1   |   valor_en_twh |   valor |
-#  |----:|:-------|-------:|:----------------|:---------|---------------:|--------:|
-#  | 985 | ARG    |   2022 | Biocombustibles | Limpias  |        13.9313 | 1.39177 |
+#  |     |   anio | iso3   | nivel2           |   valor_en_twh |    valor | nivel1   |
+#  |----:|-------:|:-------|:-----------------|---------------:|---------:|:---------|
+#  | 111 |   2023 | ARG    | Otras renovables |        7.26455 | 0.712392 | Limpias  |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='anio', axis=1)
-#  Index: 9 entries, 985 to 1443
+#  Index: 9 entries, 111 to 34590
 #  Data columns (total 5 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
 #   0   iso3          9 non-null      object 
 #   1   nivel2        9 non-null      object 
-#   2   nivel1        9 non-null      object 
-#   3   valor_en_twh  9 non-null      float64
-#   4   valor         9 non-null      float64
+#   2   valor_en_twh  9 non-null      float64
+#   3   valor         9 non-null      float64
+#   4   nivel1        9 non-null      object 
 #  
-#  |     | iso3   | nivel2          | nivel1   |   valor_en_twh |   valor |
-#  |----:|:-------|:----------------|:---------|---------------:|--------:|
-#  | 985 | ARG    | Biocombustibles | Limpias  |        13.9313 | 1.39177 |
+#  |     | iso3   | nivel2           |   valor_en_twh |    valor | nivel1   |
+#  |----:|:-------|:-----------------|---------------:|---------:|:---------|
+#  | 111 | ARG    | Otras renovables |        7.26455 | 0.712392 | Limpias  |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='iso3', axis=1)
-#  Index: 9 entries, 985 to 1443
+#  Index: 9 entries, 111 to 34590
 #  Data columns (total 4 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
 #   0   nivel2        9 non-null      object 
-#   1   nivel1        9 non-null      object 
-#   2   valor_en_twh  9 non-null      float64
-#   3   valor         9 non-null      float64
+#   1   valor_en_twh  9 non-null      float64
+#   2   valor         9 non-null      float64
+#   3   nivel1        9 non-null      object 
 #  
-#  |     | nivel2          | nivel1   |   valor_en_twh |   valor |
-#  |----:|:----------------|:---------|---------------:|--------:|
-#  | 985 | Biocombustibles | Limpias  |        13.9313 | 1.39177 |
+#  |     | nivel2           |   valor_en_twh |    valor | nivel1   |
+#  |----:|:-----------------|---------------:|---------:|:---------|
+#  | 111 | Otras renovables |        7.26455 | 0.712392 | Limpias  |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='valor_en_twh', axis=1)
-#  Index: 9 entries, 985 to 1443
+#  Index: 9 entries, 111 to 34590
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
 #   0   nivel2  9 non-null      object 
-#   1   nivel1  9 non-null      object 
-#   2   valor   9 non-null      float64
+#   1   valor   9 non-null      float64
+#   2   nivel1  9 non-null      object 
 #  
-#  |     | nivel2          | nivel1   |   valor |
-#  |----:|:----------------|:---------|--------:|
-#  | 985 | Biocombustibles | Limpias  | 1.39177 |
+#  |     | nivel2           |    valor | nivel1   |
+#  |----:|:-----------------|---------:|:---------|
+#  | 111 | Otras renovables | 0.712392 | Limpias  |
 #  
 #  ------------------------------
 #  
