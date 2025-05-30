42475b590b13409ad36fb59eb618f6ce1b5bfa2d
joangq <jgquiroga@dc.uba.ar>
2025-05-30 13:34:10 -0300

    Created PESCAS/PESCAS_g02_transformer.py

---
+++ PESCAS/PESCAS_g02_transformer.py
@@ -0,0 +1,26 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def identity(df: pl.DataFrame) -> pl.DataFrame:
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	identity()
+)
+#  PIPELINE_END
+
+
+#  start()
+#  
+#  ------------------------------
+#  
+#  identity()
+#  
+#  ------------------------------
+#  
