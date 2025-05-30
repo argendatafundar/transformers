85c466d51cd74c11206bf35462faf4e985ab4c18
Ignacio-ibarra <iibarra@fund.ar>
2025-05-30 17:53:06 -0300

    Created PESCAS/PESCAS_g23_transformer.py

---
+++ PESCAS/PESCAS_g23_transformer.py
@@ -0,0 +1,76 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
+    return df
+
+@transformer.convert
+def imput_na(df: DataFrame, col:str, value:str) -> DataFrame:
+
+    from numpy import nan
+
+    df.loc[df[col] == value,col] = nan
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	drop_na(col='share_acuicola'),
+	imput_na(col='share_acuicola', value=0.0)
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 13486 entries, 0 to 13485
+#  Data columns (total 4 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            13486 non-null  int64  
+#   1   iso3            13486 non-null  object 
+#   2   pais_nombre     13486 non-null  object 
+#   3   share_acuicola  13486 non-null  float64
+#  
+#  |    |   anio | iso3   | pais_nombre            |   share_acuicola |
+#  |---:|-------:|:-------|:-----------------------|-----------------:|
+#  |  0 |   1961 | ARE    | Emiratos Árabes Unidos |                0 |
+#  
+#  ------------------------------
+#  
+#  drop_na(col='share_acuicola')
+#  RangeIndex: 13486 entries, 0 to 13485
+#  Data columns (total 4 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            13486 non-null  int64  
+#   1   iso3            13486 non-null  object 
+#   2   pais_nombre     13486 non-null  object 
+#   3   share_acuicola  8738 non-null   float64
+#  
+#  |    |   anio | iso3   | pais_nombre            |   share_acuicola |
+#  |---:|-------:|:-------|:-----------------------|-----------------:|
+#  |  0 |   1961 | ARE    | Emiratos Árabes Unidos |              nan |
+#  
+#  ------------------------------
+#  
+#  imput_na(col='share_acuicola', value=0.0)
+#  RangeIndex: 13486 entries, 0 to 13485
+#  Data columns (total 4 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            13486 non-null  int64  
+#   1   iso3            13486 non-null  object 
+#   2   pais_nombre     13486 non-null  object 
+#   3   share_acuicola  8738 non-null   float64
+#  
+#  |    |   anio | iso3   | pais_nombre            |   share_acuicola |
+#  |---:|-------:|:-------|:-----------------------|-----------------:|
+#  |  0 |   1961 | ARE    | Emiratos Árabes Unidos |              nan |
+#  
+#  ------------------------------
+#  
