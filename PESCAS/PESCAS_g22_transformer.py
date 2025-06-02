0298b503deb86e8f949683269823cbbe2f4816b1
Ignacio-ibarra <iibarra@fund.ar>
2025-06-02 18:18:54 -0300

    Updated PESCAS/PESCAS_g22_transformer.py

--- PESCAS/PESCAS_g22_transformer.py
+++ PESCAS/PESCAS_g22_transformer.py
@@ -12,7 +12,7 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio.isin([2004,2012,2023])')
+	query(condition='anio.isin([2004,2009,2012,2014,2023])')
 )
 #  PIPELINE_END
 
@@ -34,16 +34,16 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='anio.isin([2004,2012,2023])')
-#  Index: 36 entries, 0 to 239
+#  query(condition='anio.isin([2004,2009,2012,2014,2023])')
+#  Index: 60 entries, 0 to 239
 #  Data columns (total 5 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       36 non-null     int64  
-#   1   vab_pb     36 non-null     float64
-#   2   provincia  36 non-null     object 
-#   3   share      36 non-null     float64
-#   4   geocodigo  36 non-null     object 
+#   0   anio       60 non-null     int64  
+#   1   vab_pb     60 non-null     float64
+#   2   provincia  60 non-null     object 
+#   3   share      60 non-null     float64
+#   4   geocodigo  60 non-null     object 
 #  
 #  |    |   anio |   vab_pb | provincia   |   share | geocodigo   |
 #  |---:|-------:|---------:|:------------|--------:|:------------|
