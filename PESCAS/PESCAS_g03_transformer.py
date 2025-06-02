29953e713e5b4d0e13ca8c6d085fbb3cb1fca0fa
Ignacio-ibarra <iibarra@fund.ar>
2025-06-02 14:11:43 -0300

    Updated PESCAS/PESCAS_g03_transformer.py

--- PESCAS/PESCAS_g03_transformer.py
+++ PESCAS/PESCAS_g03_transformer.py
@@ -4,14 +4,19 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def txtwrapper(df: DataFrame, col:str, width:int)->DataFrame:
-    import textwrap
-    df[col] = df[col].apply(lambda text: "\n".join(textwrap.wrap(text, width=width)))
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 
 @transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def txtwrapper(df: DataFrame, col:str, width:int)->DataFrame:
+    import textwrap
+    df[col] = df[col].apply(lambda text: "\n".join(textwrap.wrap(text, width=width)))
     return df
 #  DEFINITIONS_END
 
@@ -19,7 +24,9 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 #  PIPELINE_START
 pipeline = chain(
 	multiplicar_por_escalar(col='share_fob', k=100),
-	txtwrapper(col='producto', width=15)
+	replace_value(col='producto', curr_value='Pescado sec./sal./en salm. har./pol./pell. aptos p/c humano', new_value='Otras preparaciones aptas para consumo humano'),
+	replace_value(col='producto', curr_value='Harina, polvo y pellets de pescado. No aptos p/c humano', new_value='Harina, polvo y pellets de pescado. No aptos para consumo humano'),
+	txtwrapper(col='producto', width=25)
 )
 #  PIPELINE_END
 
@@ -54,15 +61,13 @@ pipeline = chain(
 #   4   share_toneladas  8 non-null      float64
 #   5   share_fob        8 non-null      float64
 #  
-#  |    | gran_rubro   | producto        |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
-#  |---:|:-------------|:----------------|------------:|----------------:|------------------:|------------:|
-#  |  0 | MOA          | Filetes y demás |     79321.9 |          254036 |          0.152354 |     13.4415 |
-#  |    |              | carnes de       |             |                 |                   |             |
-#  |    |              | pescado         |             |                 |                   |             |
+#  |    | gran_rubro   | producto                          |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
+#  |---:|:-------------|:----------------------------------|------------:|----------------:|------------------:|------------:|
+#  |  0 | MOA          | Filetes y demás carnes de pescado |     79321.9 |          254036 |          0.152354 |     13.4415 |
 #  
 #  ------------------------------
 #  
-#  txtwrapper(col='producto', width=15)
+#  replace_value(col='producto', curr_value='Pescado sec./sal./en salm. har./pol./pell. aptos p/c humano', new_value='Otras preparaciones aptas para consumo humano')
 #  RangeIndex: 8 entries, 0 to 7
 #  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
@@ -74,11 +79,47 @@ pipeline = chain(
 #   4   share_toneladas  8 non-null      float64
 #   5   share_fob        8 non-null      float64
 #  
-#  |    | gran_rubro   | producto        |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
-#  |---:|:-------------|:----------------|------------:|----------------:|------------------:|------------:|
-#  |  0 | MOA          | Filetes y demás |     79321.9 |          254036 |          0.152354 |     13.4415 |
-#  |    |              | carnes de       |             |                 |                   |             |
-#  |    |              | pescado         |             |                 |                   |             |
+#  |    | gran_rubro   | producto                          |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
+#  |---:|:-------------|:----------------------------------|------------:|----------------:|------------------:|------------:|
+#  |  0 | MOA          | Filetes y demás carnes de pescado |     79321.9 |          254036 |          0.152354 |     13.4415 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='producto', curr_value='Harina, polvo y pellets de pescado. No aptos p/c humano', new_value='Harina, polvo y pellets de pescado. No aptos para consumo humano')
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   gran_rubro       8 non-null      object 
+#   1   producto         8 non-null      object 
+#   2   toneladas        8 non-null      float64
+#   3   fob_miles_usd    8 non-null      float64
+#   4   share_toneladas  8 non-null      float64
+#   5   share_fob        8 non-null      float64
+#  
+#  |    | gran_rubro   | producto                  |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
+#  |---:|:-------------|:--------------------------|------------:|----------------:|------------------:|------------:|
+#  |  0 | MOA          | Filetes y demás carnes de |     79321.9 |          254036 |          0.152354 |     13.4415 |
+#  |    |              | pescado                   |             |                 |                   |             |
+#  
+#  ------------------------------
+#  
+#  txtwrapper(col='producto', width=25)
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   gran_rubro       8 non-null      object 
+#   1   producto         8 non-null      object 
+#   2   toneladas        8 non-null      float64
+#   3   fob_miles_usd    8 non-null      float64
+#   4   share_toneladas  8 non-null      float64
+#   5   share_fob        8 non-null      float64
+#  
+#  |    | gran_rubro   | producto                  |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
+#  |---:|:-------------|:--------------------------|------------:|----------------:|------------------:|------------:|
+#  |  0 | MOA          | Filetes y demás carnes de |     79321.9 |          254036 |          0.152354 |     13.4415 |
+#  |    |              | pescado                   |             |                 |                   |             |
 #  
 #  ------------------------------
 #  
