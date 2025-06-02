4ea312049c2c0fc67094eb0cb3e3ef0479863e30
Ignacio-ibarra <iibarra@fund.ar>
2025-06-02 12:44:09 -0300

    Updated PESCAS/PESCAS_g03_transformer.py

--- PESCAS/PESCAS_g03_transformer.py
+++ PESCAS/PESCAS_g03_transformer.py
@@ -9,6 +9,12 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     return df
 
 @transformer.convert
+def txtwrapper(df: DataFrame, col:str, width:int)->DataFrame:
+    import textwrap
+    df[col] = df[col].apply(lambda text: "\n".join(textwrap.wrap(text, width=width)))
+    return df
+
+@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -19,7 +25,8 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 pipeline = chain(
 	multiplicar_por_escalar(col='share_fob', k=100),
 	replace_value(col='gran_rubro', curr_value='PP', new_value='Productos primarios'),
-	replace_value(col='gran_rubro', curr_value='MOA', new_value='Manufacturas de origen agropecuario')
+	replace_value(col='gran_rubro', curr_value='MOA', new_value='Manufacturas de origen agropecuario'),
+	txtwrapper(col='gran_rubro', width=15)
 )
 #  PIPELINE_END
 
@@ -90,9 +97,31 @@ pipeline = chain(
 #   4   share_toneladas  8 non-null      float64
 #   5   share_fob        8 non-null      float64
 #  
-#  |    | gran_rubro                          | producto                          |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
-#  |---:|:------------------------------------|:----------------------------------|------------:|----------------:|------------------:|------------:|
-#  |  0 | Manufacturas de origen agropecuario | Filetes y demás carnes de pescado |     79321.9 |          254036 |          0.152354 |     13.4415 |
+#  |    | gran_rubro      | producto                          |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
+#  |---:|:----------------|:----------------------------------|------------:|----------------:|------------------:|------------:|
+#  |  0 | Manufacturas de | Filetes y demás carnes de pescado |     79321.9 |          254036 |          0.152354 |     13.4415 |
+#  |    | origen          |                                   |             |                 |                   |             |
+#  |    | agropecuario    |                                   |             |                 |                   |             |
+#  
+#  ------------------------------
+#  
+#  txtwrapper(col='gran_rubro', width=15)
+#  RangeIndex: 8 entries, 0 to 7
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   gran_rubro       8 non-null      object 
+#   1   producto         8 non-null      object 
+#   2   toneladas        8 non-null      float64
+#   3   fob_miles_usd    8 non-null      float64
+#   4   share_toneladas  8 non-null      float64
+#   5   share_fob        8 non-null      float64
+#  
+#  |    | gran_rubro      | producto                          |   toneladas |   fob_miles_usd |   share_toneladas |   share_fob |
+#  |---:|:----------------|:----------------------------------|------------:|----------------:|------------------:|------------:|
+#  |  0 | Manufacturas de | Filetes y demás carnes de pescado |     79321.9 |          254036 |          0.152354 |     13.4415 |
+#  |    | origen          |                                   |             |                 |                   |             |
+#  |    | agropecuario    |                                   |             |                 |                   |             |
 #  
 #  ------------------------------
 #  
