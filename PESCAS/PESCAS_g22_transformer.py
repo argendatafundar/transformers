93c8be4cbb15896aaa0c75aee46519348cf023de
Ignacio-ibarra <iibarra@fund.ar>
2025-06-02 18:20:38 -0300

    Updated PESCAS/PESCAS_g22_transformer.py

--- PESCAS/PESCAS_g22_transformer.py
+++ PESCAS/PESCAS_g22_transformer.py
@@ -4,15 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def identity(df: DataFrame, print_str= "identidad") -> DataFrame:
+    print(print_str)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio.isin([2004,2009,2012,2014,2023])')
+	identity(print_str='identidad')
 )
 #  PIPELINE_END
 
@@ -34,16 +34,16 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='anio.isin([2004,2009,2012,2014,2023])')
-#  Index: 60 entries, 0 to 239
+#  identity(print_str='identidad')
+#  RangeIndex: 240 entries, 0 to 239
 #  Data columns (total 5 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       60 non-null     int64  
-#   1   vab_pb     60 non-null     float64
-#   2   provincia  60 non-null     object 
-#   3   share      60 non-null     float64
-#   4   geocodigo  60 non-null     object 
+#   0   anio       240 non-null    int64  
+#   1   vab_pb     240 non-null    float64
+#   2   provincia  240 non-null    object 
+#   3   share      240 non-null    float64
+#   4   geocodigo  240 non-null    object 
 #  
 #  |    |   anio |   vab_pb | provincia   |   share | geocodigo   |
 #  |---:|-------:|---------:|:------------|--------:|:------------|
