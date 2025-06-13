3fdd029466f1940a7b6de84229a64a7ae6c36b27
jgjuara <jg.juara@gmail.com>
2025-06-13 12:33:36 -0300

    Updated TRANEN/TRANEN_g18_transformer.py

--- TRANEN/TRANEN_g18_transformer.py
+++ TRANEN/TRANEN_g18_transformer.py
@@ -4,130 +4,64 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def drop_na(df:pl.DataFrame, cols:list):
+    df = df.drop_nans(subset=cols)
+    df = df.drop_nulls(subset=cols)
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def rename_cols(df: pl.DataFrame, map):
+    df = df.rename(map)
     return df
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
+
+    if not alias:
+        alias = col
+
+    df = df.with_columns(
+        pl.col(col).replace(mapping).alias(alias)
+    )
+
     return df
 
 @transformer.convert
-def drop_na(df:DataFrame, cols:list):
-    return df.dropna(subset=cols)
-
-@transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    return df.sort_values(by=by, ascending=how == 'ascending')
+def sort_values(df: pl.DataFrame, by, descending = None):
+    if not descending:
+        descending = [False] * len(by)
+    df = df.sort(by = by, descending= descending)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD'),
-	replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX'),
+	replace_value(col='iso3', mapping={'OWID_WRL': 'WLD', 'OWID_KOS': 'XKX'}, alias=None),
 	rename_cols(map={'valor_en_gco2_por_kwh': 'valor', 'iso3': 'geocodigo'}),
 	drop_na(cols=['valor']),
-	sort_values(how='ascending', by=['anio', 'geocodigo'])
+	sort_values(by=['geocodigo', 'anio'], descending=None)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 7344 entries, 0 to 7343
-#  Data columns (total 3 columns):
-#   #   Column                 Non-Null Count  Dtype  
-#  ---  ------                 --------------  -----  
-#   0   anio                   7344 non-null   int64  
-#   1   iso3                   7344 non-null   object 
-#   2   valor_en_gco2_por_kwh  5342 non-null   float64
-#  
-#  |    |   anio | iso3   |   valor_en_gco2_por_kwh |
-#  |---:|-------:|:-------|------------------------:|
-#  |  0 |   1990 | GBR    |                  705.02 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='iso3', curr_value='OWID_WRL', new_value='WLD')
-#  RangeIndex: 7344 entries, 0 to 7343
-#  Data columns (total 3 columns):
-#   #   Column                 Non-Null Count  Dtype  
-#  ---  ------                 --------------  -----  
-#   0   anio                   7344 non-null   int64  
-#   1   iso3                   7344 non-null   object 
-#   2   valor_en_gco2_por_kwh  5342 non-null   float64
-#  
-#  |    |   anio | iso3   |   valor_en_gco2_por_kwh |
-#  |---:|-------:|:-------|------------------------:|
-#  |  0 |   1990 | GBR    |                  705.02 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='OWID_KOS', new_value='XKX')
-#  RangeIndex: 7344 entries, 0 to 7343
-#  Data columns (total 3 columns):
-#   #   Column                 Non-Null Count  Dtype  
-#  ---  ------                 --------------  -----  
-#   0   anio                   7344 non-null   int64  
-#   1   iso3                   7344 non-null   object 
-#   2   valor_en_gco2_por_kwh  5342 non-null   float64
-#  
-#  |    |   anio | iso3   |   valor_en_gco2_por_kwh |
-#  |---:|-------:|:-------|------------------------:|
-#  |  0 |   1990 | GBR    |                  705.02 |
+#  replace_value(col='iso3', mapping={'OWID_WRL': 'WLD', 'OWID_KOS': 'XKX'}, alias=None)
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'valor_en_gco2_por_kwh': 'valor', 'iso3': 'geocodigo'})
-#  RangeIndex: 7344 entries, 0 to 7343
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       7344 non-null   int64  
-#   1   geocodigo  7344 non-null   object 
-#   2   valor      5342 non-null   float64
-#  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1990 | GBR         |  705.02 |
 #  
 #  ------------------------------
 #  
 #  drop_na(cols=['valor'])
-#  Index: 5342 entries, 0 to 7342
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       5342 non-null   int64  
-#   1   geocodigo  5342 non-null   object 
-#   2   valor      5342 non-null   float64
-#  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1990 | GBR         |  705.02 |
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'geocodigo'])
-#  Index: 5342 entries, 340 to 2855
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       5342 non-null   int64  
-#   1   geocodigo  5342 non-null   object 
-#   2   valor      5342 non-null   float64
-#  
-#  |     |   anio | geocodigo   |   valor |
-#  |----:|-------:|:------------|--------:|
-#  | 340 |   1990 | AUT         | 249.848 |
+#  sort_values(by=['geocodigo', 'anio'], descending=None)
 #  
 #  ------------------------------
 #  
