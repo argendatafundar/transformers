8ddb4173659601e0cb6883f8f88657431e3e0fbe
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-13 14:57:17 -0300

    Updated PESCAS/PESCAS_g09_transformer.py

--- PESCAS/PESCAS_g09_transformer.py
+++ PESCAS/PESCAS_g09_transformer.py
@@ -9,60 +9,17 @@ def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[s
     df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
     df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
     return df.sort_values(by=[col1,col2])
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='tipo_carne', curr_value='Pescados y mariscos', new_value='Pescados y mariscos asdhndsamdfkm asdnadsna sdmsda masdas'),
-	replace_value(col='tipo_carne', curr_value='Caprina y ovina', new_value='Caprina y ovina asdhndsamdfkm asdnadsna sdmsda masdas'),
-	ordenar_dos_columnas(col1='pais', order1=['Bangladesh', 'Camboya', 'Sierra Leona', 'Indonesia', 'Islas Salomón', 'Maldivas', 'Sri Lanka', 'Myanmar', 'Costa de Marfil', 'Kiribati', 'Uganda', 'Ghana', 'India', 'Santo Tomé y Príncipe', 'Camerún', 'Mozambique', 'Malí', 'Tailandia', 'Nigeria', 'Islandia', 'República Democrática del Congo', 'Gambia', 'Benin', 'Guinea', 'Lao', 'Ruanda', 'Vanuatu', 'Togo', 'Micronesia', 'Vietnam', 'Japón', 'Filipinas', 'Zambia', 'Malasia', 'Tuvalu', 'Omán', 'Noruega', 'Senegal', 'Madagascar', 'Burundi', 'Seychelles', 'Corea del Sur', 'Egipto', 'Macao', 'China', 'Antigua y Barbuda', 'Comoras', 'Fiji', 'Angola', 'Tanzanía', 'Portugal', 'Marruecos', 'Hong Kong', 'Túnez', 'Congo', 'Barbados', 'Perú', 'Mauricio', 'Polinesia Francesa', 'Malta', 'Suecia', 'Finlandia', 'Libia', 'Dinamarca', 'Gabón', 'Burkina Faso', 'Jamaica', 'Moldavia', 'Samoa', 'Italia', 'Bhután', 'Francia', 'Namibia', 'Saint Kitts y Nevis', 'Granada', 'España', 'Bélgica', 'Islas Marshall', 'Santa Lucía', 'Lituania', 'Irán', 'Luxemburgo', 'Mundo', 'Taiwán', 'Ucrania', 'Belice', 'Guyana', 'Emiratos Árabes Unidos', 'Países Bajos', 'Letonia', 'Dominica', 'Malawi', 'Trinidad y Tobago', 'Costa Rica', 'Suriname', 'Nueva Zelanda', 'Rusia', 'Venezuela', 'Nueva Caledonia', 'Qatar', 'Kenia', 'Georgia', 'Djibouti', 'Chipre', 'Suiza', 'Bahamas', 'Haití', 'Grecia', 'Níger', 'Cabo Verde', 'Timor-Leste', 'Liberia', 'Bahrein', 'Mauritania', 'Nauru', 'Israel', 'Canadá', 'Irlanda', 'Australia', 'Croacia', 'Eslovenia', 'Reino Unido', 'Arabia Saudita', 'Nepal', 'Nicaragua', 'Panamá', 'Estonia', 'Tonga', 'Austria', 'Uruguay', 'Alemania', 'Estados Unidos', 'Kuwait', 'México', 'Guinea-Bissau', 'Lesotho', 'Paraguay', 'El Salvador', 'Macedonia del Norte', 'Eslovaquia', 'Albania', 'San Vicente y las Granadinas', 'Argelia', 'Chile', 'Yemen', 'Somalia', 'Eswatini', 'República Dominicana', 'Sudán del Sur', 'Colombia', 'Montenegro', 'Polonia', 'Chequia', 'Jordania', 'Belarús', 'Papua Nueva Guinea', 'Líbano', 'Iraq', 'Ecuador', 'Bulgaria', 'Chad', 'Bosnia y Herzegovina', 'Rumania', 'Turquía', 'Honduras', 'Serbia', 'Siria', 'Cuba', 'Sudáfrica', 'Uzbekistán', 'Armenia', 'Brasil', 'Hungría', 'Etiopía', 'Guatemala', 'Botswana', 'Pakistán', 'Argentina', 'Afganistán', 'Azerbaiyán', 'Kazajstán', 'Sudán', 'Zimbabwe', 'Turkmenistán', 'Kirguistán', 'Bolivia', 'Tayikistán', 'Mongolia', 'República Centroafricana'], col2='tipo_carne', order2=['Pescados y mariscos asdhndsamdfkm asdnadsna sdmsda masdas', 'Aviar', 'Vacuna', 'Porcina', 'Caprina y ovina asdhndsamdfkm asdnadsna sdmsda masdas', 'Otras carnes'])
+	ordenar_dos_columnas(col1='pais', order1=['Bangladesh', 'Camboya', 'Sierra Leona', 'Indonesia', 'Islas Salomón', 'Maldivas', 'Sri Lanka', 'Myanmar', 'Costa de Marfil', 'Kiribati', 'Uganda', 'Ghana', 'India', 'Santo Tomé y Príncipe', 'Camerún', 'Mozambique', 'Malí', 'Tailandia', 'Nigeria', 'Islandia', 'República Democrática del Congo', 'Gambia', 'Benin', 'Guinea', 'Lao', 'Ruanda', 'Vanuatu', 'Togo', 'Micronesia', 'Vietnam', 'Japón', 'Filipinas', 'Zambia', 'Malasia', 'Tuvalu', 'Omán', 'Noruega', 'Senegal', 'Madagascar', 'Burundi', 'Seychelles', 'Corea del Sur', 'Egipto', 'Macao', 'China', 'Antigua y Barbuda', 'Comoras', 'Fiji', 'Angola', 'Tanzanía', 'Portugal', 'Marruecos', 'Hong Kong', 'Túnez', 'Congo', 'Barbados', 'Perú', 'Mauricio', 'Polinesia Francesa', 'Malta', 'Suecia', 'Finlandia', 'Libia', 'Dinamarca', 'Gabón', 'Burkina Faso', 'Jamaica', 'Moldavia', 'Samoa', 'Italia', 'Bhután', 'Francia', 'Namibia', 'Saint Kitts y Nevis', 'Granada', 'España', 'Bélgica', 'Islas Marshall', 'Santa Lucía', 'Lituania', 'Irán', 'Luxemburgo', 'Mundo', 'Taiwán', 'Ucrania', 'Belice', 'Guyana', 'Emiratos Árabes Unidos', 'Países Bajos', 'Letonia', 'Dominica', 'Malawi', 'Trinidad y Tobago', 'Costa Rica', 'Suriname', 'Nueva Zelanda', 'Rusia', 'Venezuela', 'Nueva Caledonia', 'Qatar', 'Kenia', 'Georgia', 'Djibouti', 'Chipre', 'Suiza', 'Bahamas', 'Haití', 'Grecia', 'Níger', 'Cabo Verde', 'Timor-Leste', 'Liberia', 'Bahrein', 'Mauritania', 'Nauru', 'Israel', 'Canadá', 'Irlanda', 'Australia', 'Croacia', 'Eslovenia', 'Reino Unido', 'Arabia Saudita', 'Nepal', 'Nicaragua', 'Panamá', 'Estonia', 'Tonga', 'Austria', 'Uruguay', 'Alemania', 'Estados Unidos', 'Kuwait', 'México', 'Guinea-Bissau', 'Lesotho', 'Paraguay', 'El Salvador', 'Macedonia del Norte', 'Eslovaquia', 'Albania', 'San Vicente y las Granadinas', 'Argelia', 'Chile', 'Yemen', 'Somalia', 'Eswatini', 'República Dominicana', 'Sudán del Sur', 'Colombia', 'Montenegro', 'Polonia', 'Chequia', 'Jordania', 'Belarús', 'Papua Nueva Guinea', 'Líbano', 'Iraq', 'Ecuador', 'Bulgaria', 'Chad', 'Bosnia y Herzegovina', 'Rumania', 'Turquía', 'Honduras', 'Serbia', 'Siria', 'Cuba', 'Sudáfrica', 'Uzbekistán', 'Armenia', 'Brasil', 'Hungría', 'Etiopía', 'Guatemala', 'Botswana', 'Pakistán', 'Argentina', 'Afganistán', 'Azerbaiyán', 'Kazajstán', 'Sudán', 'Zimbabwe', 'Turkmenistán', 'Kirguistán', 'Bolivia', 'Tayikistán', 'Mongolia', 'República Centroafricana'], col2='tipo_carne', order2=['Pescados y mariscos', 'Aviar', 'Vacuna', 'Porcina', 'Caprina y ovina', 'Otras carnes'])
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 1134 entries, 0 to 1133
-#  Data columns (total 6 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   anio        1134 non-null   int64  
-#   1   iso3        1134 non-null   object 
-#   2   tipo_carne  1134 non-null   object 
-#   3   pais        1134 non-null   object 
-#   4   valor       1134 non-null   float64
-#   5   share       1134 non-null   float64
-#  
-#  |    |   anio | iso3   | tipo_carne   | pais       |   valor |   share |
-#  |---:|-------:|:-------|:-------------|:-----------|--------:|--------:|
-#  |  0 |   2022 | AFG    | Aviar        | Afganistán |    0.87 | 12.1678 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='tipo_carne', curr_value='Pescados y mariscos', new_value='Pescados y mariscos asdhndsamdfkm asdnadsna sdmsda masdas')
-#  RangeIndex: 1134 entries, 0 to 1133
-#  Data columns (total 6 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   anio        1134 non-null   int64  
-#   1   iso3        1134 non-null   object 
-#   2   tipo_carne  1134 non-null   object 
-#   3   pais        1134 non-null   object 
-#   4   valor       1134 non-null   float64
-#   5   share       1134 non-null   float64
-#  
-#  |    |   anio | iso3   | tipo_carne   | pais       |   valor |   share |
-#  |---:|-------:|:-------|:-------------|:-----------|--------:|--------:|
-#  |  0 |   2022 | AFG    | Aviar        | Afganistán |    0.87 | 12.1678 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='tipo_carne', curr_value='Caprina y ovina', new_value='Caprina y ovina asdhndsamdfkm asdnadsna sdmsda masdas')
 #  RangeIndex: 1134 entries, 0 to 1133
 #  Data columns (total 6 columns):
 #   #   Column      Non-Null Count  Dtype   
@@ -80,7 +37,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  ordenar_dos_columnas(col1='pais', order1=['Bangladesh', 'Camboya', 'Sierra Leona', 'Indonesia', 'Islas Salomón', 'Maldivas', 'Sri Lanka', 'Myanmar', 'Costa de Marfil', 'Kiribati', 'Uganda', 'Ghana', 'India', 'Santo Tomé y Príncipe', 'Camerún', 'Mozambique', 'Malí', 'Tailandia', 'Nigeria', 'Islandia', 'República Democrática del Congo', 'Gambia', 'Benin', 'Guinea', 'Lao', 'Ruanda', 'Vanuatu', 'Togo', 'Micronesia', 'Vietnam', 'Japón', 'Filipinas', 'Zambia', 'Malasia', 'Tuvalu', 'Omán', 'Noruega', 'Senegal', 'Madagascar', 'Burundi', 'Seychelles', 'Corea del Sur', 'Egipto', 'Macao', 'China', 'Antigua y Barbuda', 'Comoras', 'Fiji', 'Angola', 'Tanzanía', 'Portugal', 'Marruecos', 'Hong Kong', 'Túnez', 'Congo', 'Barbados', 'Perú', 'Mauricio', 'Polinesia Francesa', 'Malta', 'Suecia', 'Finlandia', 'Libia', 'Dinamarca', 'Gabón', 'Burkina Faso', 'Jamaica', 'Moldavia', 'Samoa', 'Italia', 'Bhután', 'Francia', 'Namibia', 'Saint Kitts y Nevis', 'Granada', 'España', 'Bélgica', 'Islas Marshall', 'Santa Lucía', 'Lituania', 'Irán', 'Luxemburgo', 'Mundo', 'Taiwán', 'Ucrania', 'Belice', 'Guyana', 'Emiratos Árabes Unidos', 'Países Bajos', 'Letonia', 'Dominica', 'Malawi', 'Trinidad y Tobago', 'Costa Rica', 'Suriname', 'Nueva Zelanda', 'Rusia', 'Venezuela', 'Nueva Caledonia', 'Qatar', 'Kenia', 'Georgia', 'Djibouti', 'Chipre', 'Suiza', 'Bahamas', 'Haití', 'Grecia', 'Níger', 'Cabo Verde', 'Timor-Leste', 'Liberia', 'Bahrein', 'Mauritania', 'Nauru', 'Israel', 'Canadá', 'Irlanda', 'Australia', 'Croacia', 'Eslovenia', 'Reino Unido', 'Arabia Saudita', 'Nepal', 'Nicaragua', 'Panamá', 'Estonia', 'Tonga', 'Austria', 'Uruguay', 'Alemania', 'Estados Unidos', 'Kuwait', 'México', 'Guinea-Bissau', 'Lesotho', 'Paraguay', 'El Salvador', 'Macedonia del Norte', 'Eslovaquia', 'Albania', 'San Vicente y las Granadinas', 'Argelia', 'Chile', 'Yemen', 'Somalia', 'Eswatini', 'República Dominicana', 'Sudán del Sur', 'Colombia', 'Montenegro', 'Polonia', 'Chequia', 'Jordania', 'Belarús', 'Papua Nueva Guinea', 'Líbano', 'Iraq', 'Ecuador', 'Bulgaria', 'Chad', 'Bosnia y Herzegovina', 'Rumania', 'Turquía', 'Honduras', 'Serbia', 'Siria', 'Cuba', 'Sudáfrica', 'Uzbekistán', 'Armenia', 'Brasil', 'Hungría', 'Etiopía', 'Guatemala', 'Botswana', 'Pakistán', 'Argentina', 'Afganistán', 'Azerbaiyán', 'Kazajstán', 'Sudán', 'Zimbabwe', 'Turkmenistán', 'Kirguistán', 'Bolivia', 'Tayikistán', 'Mongolia', 'República Centroafricana'], col2='tipo_carne', order2=['Pescados y mariscos asdhndsamdfkm asdnadsna sdmsda masdas', 'Aviar', 'Vacuna', 'Porcina', 'Caprina y ovina asdhndsamdfkm asdnadsna sdmsda masdas', 'Otras carnes'])
+#  ordenar_dos_columnas(col1='pais', order1=['Bangladesh', 'Camboya', 'Sierra Leona', 'Indonesia', 'Islas Salomón', 'Maldivas', 'Sri Lanka', 'Myanmar', 'Costa de Marfil', 'Kiribati', 'Uganda', 'Ghana', 'India', 'Santo Tomé y Príncipe', 'Camerún', 'Mozambique', 'Malí', 'Tailandia', 'Nigeria', 'Islandia', 'República Democrática del Congo', 'Gambia', 'Benin', 'Guinea', 'Lao', 'Ruanda', 'Vanuatu', 'Togo', 'Micronesia', 'Vietnam', 'Japón', 'Filipinas', 'Zambia', 'Malasia', 'Tuvalu', 'Omán', 'Noruega', 'Senegal', 'Madagascar', 'Burundi', 'Seychelles', 'Corea del Sur', 'Egipto', 'Macao', 'China', 'Antigua y Barbuda', 'Comoras', 'Fiji', 'Angola', 'Tanzanía', 'Portugal', 'Marruecos', 'Hong Kong', 'Túnez', 'Congo', 'Barbados', 'Perú', 'Mauricio', 'Polinesia Francesa', 'Malta', 'Suecia', 'Finlandia', 'Libia', 'Dinamarca', 'Gabón', 'Burkina Faso', 'Jamaica', 'Moldavia', 'Samoa', 'Italia', 'Bhután', 'Francia', 'Namibia', 'Saint Kitts y Nevis', 'Granada', 'España', 'Bélgica', 'Islas Marshall', 'Santa Lucía', 'Lituania', 'Irán', 'Luxemburgo', 'Mundo', 'Taiwán', 'Ucrania', 'Belice', 'Guyana', 'Emiratos Árabes Unidos', 'Países Bajos', 'Letonia', 'Dominica', 'Malawi', 'Trinidad y Tobago', 'Costa Rica', 'Suriname', 'Nueva Zelanda', 'Rusia', 'Venezuela', 'Nueva Caledonia', 'Qatar', 'Kenia', 'Georgia', 'Djibouti', 'Chipre', 'Suiza', 'Bahamas', 'Haití', 'Grecia', 'Níger', 'Cabo Verde', 'Timor-Leste', 'Liberia', 'Bahrein', 'Mauritania', 'Nauru', 'Israel', 'Canadá', 'Irlanda', 'Australia', 'Croacia', 'Eslovenia', 'Reino Unido', 'Arabia Saudita', 'Nepal', 'Nicaragua', 'Panamá', 'Estonia', 'Tonga', 'Austria', 'Uruguay', 'Alemania', 'Estados Unidos', 'Kuwait', 'México', 'Guinea-Bissau', 'Lesotho', 'Paraguay', 'El Salvador', 'Macedonia del Norte', 'Eslovaquia', 'Albania', 'San Vicente y las Granadinas', 'Argelia', 'Chile', 'Yemen', 'Somalia', 'Eswatini', 'República Dominicana', 'Sudán del Sur', 'Colombia', 'Montenegro', 'Polonia', 'Chequia', 'Jordania', 'Belarús', 'Papua Nueva Guinea', 'Líbano', 'Iraq', 'Ecuador', 'Bulgaria', 'Chad', 'Bosnia y Herzegovina', 'Rumania', 'Turquía', 'Honduras', 'Serbia', 'Siria', 'Cuba', 'Sudáfrica', 'Uzbekistán', 'Armenia', 'Brasil', 'Hungría', 'Etiopía', 'Guatemala', 'Botswana', 'Pakistán', 'Argentina', 'Afganistán', 'Azerbaiyán', 'Kazajstán', 'Sudán', 'Zimbabwe', 'Turkmenistán', 'Kirguistán', 'Bolivia', 'Tayikistán', 'Mongolia', 'República Centroafricana'], col2='tipo_carne', order2=['Pescados y mariscos', 'Aviar', 'Vacuna', 'Porcina', 'Caprina y ovina', 'Otras carnes'])
 #  Index: 1134 entries, 87 to 158
 #  Data columns (total 6 columns):
 #   #   Column      Non-Null Count  Dtype   
@@ -92,9 +49,9 @@ pipeline = chain(
 #   4   valor       1134 non-null   float64 
 #   5   share       1134 non-null   float64 
 #  
-#  |    |   anio | iso3   | tipo_carne                                                | pais       |   valor |   share |
-#  |---:|-------:|:-------|:----------------------------------------------------------|:-----------|--------:|--------:|
-#  | 87 |   2022 | BGD    | Pescados y mariscos asdhndsamdfkm asdnadsna sdmsda masdas | Bangladesh |    25.7 | 85.5241 |
+#  |    |   anio | iso3   | tipo_carne          | pais       |   valor |   share |
+#  |---:|-------:|:-------|:--------------------|:-----------|--------:|--------:|
+#  | 87 |   2022 | BGD    | Pescados y mariscos | Bangladesh |    25.7 | 85.5241 |
 #  
 #  ------------------------------
 #  
