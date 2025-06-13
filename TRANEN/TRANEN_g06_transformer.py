ac56fca4ab973b61108a038a8958acf77b9ca2f3
jgjuara <jg.juara@gmail.com>
2025-06-13 12:23:49 -0300

    Updated TRANEN/TRANEN_g06_transformer.py

--- TRANEN/TRANEN_g06_transformer.py
+++ TRANEN/TRANEN_g06_transformer.py
@@ -4,6 +4,24 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def drop_na(df:pl.DataFrame, cols:list):
+    df = df.drop_nans(subset=cols)
+    df = df.drop_nulls(subset=cols)
+    return df
+
+@transformer.convert
+def sort_values(df: pl.DataFrame, by, descending = None):
+    if not descending:
+        descending = [False] * len(by)
+    df = df.sort(by = by, descending= descending)
+    return df
+
+@transformer.convert
+def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
+    df = df.sql(query)
+    return df
+
+@transformer.convert
 def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
 
     if not alias:
@@ -16,36 +34,20 @@ def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
     return df
 
 @transformer.convert
-def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
-    df = df.sql(query)
-    return df
-
-@transformer.convert
 def rename_cols(df: pl.DataFrame, map):
     df = df.rename(map)
     return df
-
-@transformer.convert
-def drop_na(df:pl.DataFrame, cols:list):
-    return df.drop_nans(subset=cols)
-
-@transformer.convert
-def sort_values(df: pl.DataFrame, by, descending):
-    df = df.sort(by = by, descending= descending)
-    return df
-
-@transformer.convert
-def drop_cols(df, cols):
-    return df.drop(cols)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	replace_value(col='iso3', mapping={'OWID_WRL': 'WLD'}, alias=None),
-	df_sql(query="SELECT * FROM self WHERE tipo_energia = 'Limpias' AND iso3 = 'WLD'"),
+	df_sql(query="SELECT * FROM self WHERE iso3 = 'WLD'"),
+	df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Gas natural'"),
+	df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Petróleo'"),
+	df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Carbón'"),
 	rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'}),
-	drop_cols(cols=['valor_en_twh', 'tipo_energia']),
 	drop_na(cols=['valor']),
 	sort_values(by=['anio'], descending=[False])
 )
@@ -60,15 +62,23 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  df_sql(query="SELECT * FROM self WHERE tipo_energia = 'Limpias' AND iso3 = 'WLD'")
+#  df_sql(query="SELECT * FROM self WHERE iso3 = 'WLD'")
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Gas natural'")
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Petróleo'")
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Carbón'")
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'})
-#  
-#  ------------------------------
-#  
-#  drop_cols(cols=['valor_en_twh', 'tipo_energia'])
 #  
 #  ------------------------------
 #  
