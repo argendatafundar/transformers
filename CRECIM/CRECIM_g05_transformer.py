56b91d0aab286e3672499ab0dd6194854a3c0cd0
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-17 18:05:37 -0300

    Updated CRECIM/CRECIM_g05_transformer.py

--- CRECIM/CRECIM_g05_transformer.py
+++ CRECIM/CRECIM_g05_transformer.py
@@ -4,223 +4,109 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
+    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
-    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
-
-@transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def replace_values(df: DataFrame, col: str, mapper: dict) -> DataFrame:
+    df[col] = df[col].replace(mapper)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition="iso3 == 'ARG'"),
-	drop_col(col='pais_nombre', axis=1),
+	query(condition='iso3 == "ARG"'),
 	drop_col(col='iso3', axis=1),
-	drop_col(col='continente_fundar', axis=1),
-	drop_col(col='nivel_agregacion', axis=1),
-	wide_to_long(primary_keys=['anio'], value_name='valor', var_name='categoria'),
-	mutiplicar_por_escalar(col='valor', k=1e-06),
-	replace_value(col='categoria', curr_value='pib_corriente', new_value='PIB corriente'),
-	replace_value(col='categoria', curr_value='pib_constante', new_value='PIB constante')
+	wide_to_long(primary_keys=['anio'], value_name='valor', var_name='indicador'),
+	replace_values(col='indicador', mapper={'pib_corriente': 'Precios corrientes', 'pib_constante': 'Precios constantes'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 12657 entries, 0 to 12656
-#  Data columns (total 7 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   iso3               12657 non-null  object 
-#   1   pais_nombre        12657 non-null  object 
-#   2   continente_fundar  9941 non-null   object 
-#   3   anio               12657 non-null  int64  
-#   4   pib_corriente      12657 non-null  float64
-#   5   pib_constante      12657 non-null  float64
-#   6   nivel_agregacion   12657 non-null  object 
+#  RangeIndex: 13650 entries, 0 to 13649
+#  Data columns (total 4 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   iso3           13650 non-null  object 
+#   1   anio           13650 non-null  int64  
+#   2   pib_corriente  13650 non-null  float64
+#   3   pib_constante  13650 non-null  float64
 #  
-#  |    | iso3   | pais_nombre   | continente_fundar                      |   anio |   pib_corriente |   pib_constante | nivel_agregacion   |
-#  |---:|:-------|:--------------|:---------------------------------------|-------:|----------------:|----------------:|:-------------------|
-#  |  0 | ABW    | Aruba         | América del Norte, Central y el Caribe |   1986 |       4.056e+08 |       9.826e+08 | pais               |
+#  |    | iso3   |   anio |   pib_corriente |   pib_constante |
+#  |---:|:-------|-------:|----------------:|----------------:|
+#  |  0 | AFE    |   2023 |     1.24547e+12 |     1.06449e+12 |
 #  
 #  ------------------------------
 #  
-#  query(condition="iso3 == 'ARG'")
-#  Index: 61 entries, 413 to 473
-#  Data columns (total 7 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   iso3               61 non-null     object 
-#   1   pais_nombre        61 non-null     object 
-#   2   continente_fundar  61 non-null     object 
-#   3   anio               61 non-null     int64  
-#   4   pib_corriente      61 non-null     float64
-#   5   pib_constante      61 non-null     float64
-#   6   nivel_agregacion   61 non-null     object 
+#  query(condition='iso3 == "ARG"')
+#  Index: 62 entries, 2981 to 3042
+#  Data columns (total 4 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   iso3           62 non-null     object 
+#   1   anio           62 non-null     int64  
+#   2   pib_corriente  62 non-null     float64
+#   3   pib_constante  62 non-null     float64
 #  
-#  |     | iso3   | pais_nombre   | continente_fundar   |   anio |   pib_corriente |   pib_constante | nivel_agregacion   |
-#  |----:|:-------|:--------------|:--------------------|-------:|----------------:|----------------:|:-------------------|
-#  | 413 | ARG    | Argentina     | América del Sur     |   1962 |       2.445e+10 |       1.576e+11 | pais               |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='pais_nombre', axis=1)
-#  Index: 61 entries, 413 to 473
-#  Data columns (total 6 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   iso3               61 non-null     object 
-#   1   continente_fundar  61 non-null     object 
-#   2   anio               61 non-null     int64  
-#   3   pib_corriente      61 non-null     float64
-#   4   pib_constante      61 non-null     float64
-#   5   nivel_agregacion   61 non-null     object 
-#  
-#  |     | iso3   | continente_fundar   |   anio |   pib_corriente |   pib_constante | nivel_agregacion   |
-#  |----:|:-------|:--------------------|-------:|----------------:|----------------:|:-------------------|
-#  | 413 | ARG    | América del Sur     |   1962 |       2.445e+10 |       1.576e+11 | pais               |
+#  |      | iso3   |   anio |   pib_corriente |   pib_constante |
+#  |-----:|:-------|-------:|----------------:|----------------:|
+#  | 2981 | ARG    |   2023 |     6.46075e+11 |      5.8896e+11 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='iso3', axis=1)
-#  Index: 61 entries, 413 to 473
-#  Data columns (total 5 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   continente_fundar  61 non-null     object 
-#   1   anio               61 non-null     int64  
-#   2   pib_corriente      61 non-null     float64
-#   3   pib_constante      61 non-null     float64
-#   4   nivel_agregacion   61 non-null     object 
-#  
-#  |     | continente_fundar   |   anio |   pib_corriente |   pib_constante | nivel_agregacion   |
-#  |----:|:--------------------|-------:|----------------:|----------------:|:-------------------|
-#  | 413 | América del Sur     |   1962 |       2.445e+10 |       1.576e+11 | pais               |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='continente_fundar', axis=1)
-#  Index: 61 entries, 413 to 473
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   anio              61 non-null     int64  
-#   1   pib_corriente     61 non-null     float64
-#   2   pib_constante     61 non-null     float64
-#   3   nivel_agregacion  61 non-null     object 
-#  
-#  |     |   anio |   pib_corriente |   pib_constante | nivel_agregacion   |
-#  |----:|-------:|----------------:|----------------:|:-------------------|
-#  | 413 |   1962 |       2.445e+10 |       1.576e+11 | pais               |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='nivel_agregacion', axis=1)
-#  Index: 61 entries, 413 to 473
+#  Index: 62 entries, 2981 to 3042
 #  Data columns (total 3 columns):
 #   #   Column         Non-Null Count  Dtype  
 #  ---  ------         --------------  -----  
-#   0   anio           61 non-null     int64  
-#   1   pib_corriente  61 non-null     float64
-#   2   pib_constante  61 non-null     float64
+#   0   anio           62 non-null     int64  
+#   1   pib_corriente  62 non-null     float64
+#   2   pib_constante  62 non-null     float64
 #  
-#  |     |   anio |   pib_corriente |   pib_constante |
-#  |----:|-------:|----------------:|----------------:|
-#  | 413 |   1962 |       2.445e+10 |       1.576e+11 |
-#  
-#  ------------------------------
-#  
-#  wide_to_long(primary_keys=['anio'], value_name='valor', var_name='categoria')
-#  RangeIndex: 122 entries, 0 to 121
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       122 non-null    int64  
-#   1   categoria  122 non-null    object 
-#   2   valor      122 non-null    float64
-#  
-#  |    |   anio | categoria     |   valor |
-#  |---:|-------:|:--------------|--------:|
-#  |  0 |   1962 | pib_corriente |   24450 |
+#  |      |   anio |   pib_corriente |   pib_constante |
+#  |-----:|-------:|----------------:|----------------:|
+#  | 2981 |   2023 |     6.46075e+11 |      5.8896e+11 |
 #  
 #  ------------------------------
 #  
-#  mutiplicar_por_escalar(col='valor', k=1e-06)
-#  RangeIndex: 122 entries, 0 to 121
+#  wide_to_long(primary_keys=['anio'], value_name='valor', var_name='indicador')
+#  RangeIndex: 124 entries, 0 to 123
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       122 non-null    int64  
-#   1   categoria  122 non-null    object 
-#   2   valor      122 non-null    float64
+#   0   anio       124 non-null    int64  
+#   1   indicador  124 non-null    object 
+#   2   valor      124 non-null    float64
 #  
-#  |    |   anio | categoria     |   valor |
-#  |---:|-------:|:--------------|--------:|
-#  |  0 |   1962 | pib_corriente |   24450 |
+#  |    |   anio | indicador          |       valor |
+#  |---:|-------:|:-------------------|------------:|
+#  |  0 |   2023 | Precios corrientes | 6.46075e+11 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='categoria', curr_value='pib_corriente', new_value='PIB corriente')
-#  RangeIndex: 122 entries, 0 to 121
+#  replace_values(col='indicador', mapper={'pib_corriente': 'Precios corrientes', 'pib_constante': 'Precios constantes'})
+#  RangeIndex: 124 entries, 0 to 123
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       122 non-null    int64  
-#   1   categoria  122 non-null    object 
-#   2   valor      122 non-null    float64
+#   0   anio       124 non-null    int64  
+#   1   indicador  124 non-null    object 
+#   2   valor      124 non-null    float64
 #  
-#  |    |   anio | categoria     |   valor |
-#  |---:|-------:|:--------------|--------:|
-#  |  0 |   1962 | PIB corriente |   24450 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='categoria', curr_value='pib_constante', new_value='PIB constante')
-#  RangeIndex: 122 entries, 0 to 121
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       122 non-null    int64  
-#   1   categoria  122 non-null    object 
-#   2   valor      122 non-null    float64
-#  
-#  |    |   anio | categoria     |   valor |
-#  |---:|-------:|:--------------|--------:|
-#  |  0 |   1962 | PIB corriente |   24450 |
+#  |    |   anio | indicador          |       valor |
+#  |---:|-------:|:-------------------|------------:|
+#  |  0 |   2023 | Precios corrientes | 6.46075e+11 |
 #  
 #  ------------------------------
 #  
