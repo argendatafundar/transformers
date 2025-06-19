8bbeda110269b53b6b723d35f7909f834e82d202
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-19 17:17:57 -0300

    Updated CRECIM/CRECIM_g20_transformer.py

--- CRECIM/CRECIM_g20_transformer.py
+++ CRECIM/CRECIM_g20_transformer.py
@@ -4,11 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -21,7 +16,6 @@ def agg_sum(df: DataFrame, key_cols:list[str], summarised_col:str) -> DataFrame:
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='(anio == anio.min()) or anio == anio.max()'),
 	agg_sum(key_cols=['anio', 'region_pbg'], summarised_col='participacion_vab'),
 	multiplicar_por_escalar(col='participacion_vab', k=100)
 )
@@ -47,33 +41,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='(anio == anio.min()) or anio == anio.max()')
-#  Index: 48 entries, 0 to 671
-#  Data columns (total 7 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   anio               48 non-null     int64  
-#   1   region_pbg         48 non-null     object 
-#   2   participacion_vab  48 non-null     float64
-#   3   vab                48 non-null     float64
-#   4   provincia_id       48 non-null     object 
-#   5   geocodigoFundar    48 non-null     object 
-#   6   geonombreFundar    48 non-null     object 
-#  
-#  |    |   anio | region_pbg      |   participacion_vab |     vab | provincia_id   | geocodigoFundar   | geonombreFundar   |
-#  |---:|-------:|:----------------|--------------------:|--------:|:---------------|:------------------|:------------------|
-#  |  0 |   1895 | Pampeana y AMBA |             0.23917 | 4158.05 | AR-B           | AR-B              | Buenos Aires      |
-#  
-#  ------------------------------
-#  
 #  agg_sum(key_cols=['anio', 'region_pbg'], summarised_col='participacion_vab')
-#  RangeIndex: 10 entries, 0 to 9
+#  RangeIndex: 140 entries, 0 to 139
 #  Data columns (total 3 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   anio               10 non-null     int64  
-#   1   region_pbg         10 non-null     object 
-#   2   participacion_vab  10 non-null     float64
+#   0   anio               140 non-null    int64  
+#   1   region_pbg         140 non-null    object 
+#   2   participacion_vab  140 non-null    float64
 #  
 #  |    |   anio | region_pbg   |   participacion_vab |
 #  |---:|-------:|:-------------|--------------------:|
@@ -82,13 +57,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='participacion_vab', k=100)
-#  RangeIndex: 10 entries, 0 to 9
+#  RangeIndex: 140 entries, 0 to 139
 #  Data columns (total 3 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   anio               10 non-null     int64  
-#   1   region_pbg         10 non-null     object 
-#   2   participacion_vab  10 non-null     float64
+#   0   anio               140 non-null    int64  
+#   1   region_pbg         140 non-null    object 
+#   2   participacion_vab  140 non-null    float64
 #  
 #  |    |   anio | region_pbg   |   participacion_vab |
 #  |---:|-------:|:-------------|--------------------:|
