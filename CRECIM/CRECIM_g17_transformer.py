43fbd957825e3f995e7dcbf24813725dd739f7a3
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-19 16:17:05 -0300

    Updated CRECIM/CRECIM_g17_transformer.py

--- CRECIM/CRECIM_g17_transformer.py
+++ CRECIM/CRECIM_g17_transformer.py
@@ -4,111 +4,71 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
-
-@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
 
 @transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'cambio_relativo': 'valor'}),
-	mutiplicar_por_escalar(col='valor', k=100),
-	query(condition="geocodigo not in ('LAC', 'TEC', 'SAS', 'EAS', 'MNA', 'ECA', 'MEA', 'SSA', 'TEA', 'TMN', 'TLA', 'DESHUM_ZZH.LAC', 'DESHUM_AHDI.LAC')"),
-	sort_values(how='ascending', by=['geocodigo', 'anio'])
+	query(condition='geocodigoFundar not in ["LAC", "TSA", "TSS", "SSA", "IBD","IDX", "MNA", "TLA", "SAS", "TEA", "TMN", "EAP", "TEA"]'),
+	drop_col(col='iso3', axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 3255 entries, 0 to 3254
-#  Data columns (total 3 columns):
+#  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
 #   0   iso3             3255 non-null   object 
 #   1   anio             3255 non-null   int64  
 #   2   cambio_relativo  3255 non-null   float64
+#   3   geocodigoFundar  3255 non-null   object 
+#   4   geonombreFundar  3255 non-null   object 
 #  
-#  |    | iso3   |   anio |   cambio_relativo |
-#  |---:|:-------|-------:|------------------:|
-#  |  0 | AFE    |   2023 |         -0.021853 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'cambio_relativo': 'valor'})
-#  RangeIndex: 3255 entries, 0 to 3254
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3255 non-null   object 
-#   1   anio       3255 non-null   int64  
-#   2   valor      3255 non-null   float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFE         |   2023 | -2.1853 |
+#  |    | iso3   |   anio |   cambio_relativo | geocodigoFundar   | geonombreFundar              |
+#  |---:|:-------|-------:|------------------:|:------------------|:-----------------------------|
+#  |  0 | AFE    |   2023 |         -0.021853 | AFE               | África Oriental y Meridional |
 #  
 #  ------------------------------
 #  
-#  mutiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 3255 entries, 0 to 3254
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3255 non-null   object 
-#   1   anio       3255 non-null   int64  
-#   2   valor      3255 non-null   float64
+#  query(condition='geocodigoFundar not in ["LAC", "TSA", "TSS", "SSA", "IBD","IDX", "MNA", "TLA", "SAS", "TEA", "TMN", "EAP", "TEA"]')
+#  Index: 3099 entries, 0 to 3254
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   iso3             3099 non-null   object 
+#   1   anio             3099 non-null   int64  
+#   2   cambio_relativo  3099 non-null   float64
+#   3   geocodigoFundar  3099 non-null   object 
+#   4   geonombreFundar  3099 non-null   object 
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFE         |   2023 | -2.1853 |
-#  
-#  ------------------------------
-#  
-#  query(condition="geocodigo not in ('LAC', 'TEC', 'SAS', 'EAS', 'MNA', 'ECA', 'MEA', 'SSA', 'TEA', 'TMN', 'TLA', 'DESHUM_ZZH.LAC', 'DESHUM_AHDI.LAC')")
-#  Index: 3112 entries, 0 to 3254
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3112 non-null   object 
-#   1   anio       3112 non-null   int64  
-#   2   valor      3112 non-null   float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AFE         |   2023 | -2.1853 |
+#  |    | iso3   |   anio |   cambio_relativo | geocodigoFundar   | geonombreFundar              |
+#  |---:|:-------|-------:|------------------:|:------------------|:-----------------------------|
+#  |  0 | AFE    |   2023 |         -0.021853 | AFE               | África Oriental y Meridional |
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['geocodigo', 'anio'])
-#  RangeIndex: 3112 entries, 0 to 3111
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3112 non-null   object 
-#   1   anio       3112 non-null   int64  
-#   2   valor      3112 non-null   float64
+#  drop_col(col='iso3', axis=1)
+#  Index: 3099 entries, 0 to 3254
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             3099 non-null   int64  
+#   1   cambio_relativo  3099 non-null   float64
+#   2   geocodigoFundar  3099 non-null   object 
+#   3   geonombreFundar  3099 non-null   object 
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ABW         |   2011 |       0 |
+#  |    |   anio |   cambio_relativo | geocodigoFundar   | geonombreFundar              |
+#  |---:|-------:|------------------:|:------------------|:-----------------------------|
+#  |  0 |   2023 |         -0.021853 | AFE               | África Oriental y Meridional |
 #  
 #  ------------------------------
 #  
