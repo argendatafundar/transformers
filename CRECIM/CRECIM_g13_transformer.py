9bf2a81b4f229341a18aaba927ed33f97b8f0838
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-19 11:07:12 -0300

    Updated CRECIM/CRECIM_g13_transformer.py

--- CRECIM/CRECIM_g13_transformer.py
+++ CRECIM/CRECIM_g13_transformer.py
@@ -4,87 +4,89 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def latest_year(df, by='anio'):
-    latest_year = df[by].max()
-    df = df.query(f'{by} == {latest_year}')
-    df = df.drop(columns = by)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-latest_year(by='anio'),
-	rename_cols(map={'iso3': 'geocodigo', 'pib_pc': 'valor'}),
-	query(condition="geocodigo not in ['LAC','TLA','SSA','TSS', 'EAP','ECA','MNA','TSA','TSS', 'TEC','TEA']")
+	query(condition='anio == anio.max()'),
+	query(condition='geocodigoFundar not in ["LAC", "TSA", "TSS", "SSA", "IBD","IDX", "MNA", "TLA", "SAS", "TEA", "TMN", "EAP", "TEA"]'),
+	drop_col(col='iso3', axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 8083 entries, 0 to 8082
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    8083 non-null   object 
-#   1   anio    8083 non-null   int64  
-#   2   pib_pc  8083 non-null   float64
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   iso3             8083 non-null   object 
+#   1   anio             8083 non-null   int64  
+#   2   pib_pc           8083 non-null   float64
+#   3   geocodigoFundar  8083 non-null   object 
+#   4   geonombreFundar  8083 non-null   object 
 #  
-#  |    | iso3   |   anio |   pib_pc |
-#  |---:|:-------|-------:|---------:|
-#  |  0 | AFE    |   2023 |  3967.86 |
+#  |    | iso3   |   anio |   pib_pc | geocodigoFundar   | geonombreFundar              |
+#  |---:|:-------|-------:|---------:|:------------------|:-----------------------------|
+#  |  0 | AFE    |   2023 |  3967.86 | AFE               | África Oriental y Meridional |
 #  
 #  ------------------------------
 #  
-#  latest_year(by='anio')
+#  query(condition='anio == anio.max()')
 #  Index: 236 entries, 0 to 8049
-#  Data columns (total 2 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    236 non-null    object 
-#   1   pib_pc  236 non-null    float64
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   iso3             236 non-null    object 
+#   1   anio             236 non-null    int64  
+#   2   pib_pc           236 non-null    float64
+#   3   geocodigoFundar  236 non-null    object 
+#   4   geonombreFundar  236 non-null    object 
 #  
-#  |    | iso3   |   pib_pc |
-#  |---:|:-------|---------:|
-#  |  0 | AFE    |  3967.86 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'pib_pc': 'valor'})
-#  Index: 236 entries, 0 to 8049
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  236 non-null    object 
-#   1   valor      236 non-null    float64
-#  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | AFE         | 3967.86 |
+#  |    | iso3   |   anio |   pib_pc | geocodigoFundar   | geonombreFundar              |
+#  |---:|:-------|-------:|---------:|:------------------|:-----------------------------|
+#  |  0 | AFE    |   2023 |  3967.86 | AFE               | África Oriental y Meridional |
 #  
 #  ------------------------------
 #  
-#  query(condition="geocodigo not in ['LAC','TLA','SSA','TSS', 'EAP','ECA','MNA','TSA','TSS', 'TEC','TEA']")
-#  Index: 226 entries, 0 to 8049
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  226 non-null    object 
-#   1   valor      226 non-null    float64
+#  query(condition='geocodigoFundar not in ["LAC", "TSA", "TSS", "SSA", "IBD","IDX", "MNA", "TLA", "SAS", "TEA", "TMN", "EAP", "TEA"]')
+#  Index: 224 entries, 0 to 8049
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   iso3             224 non-null    object 
+#   1   anio             224 non-null    int64  
+#   2   pib_pc           224 non-null    float64
+#   3   geocodigoFundar  224 non-null    object 
+#   4   geonombreFundar  224 non-null    object 
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | AFE         | 3967.86 |
+#  |    | iso3   |   anio |   pib_pc | geocodigoFundar   | geonombreFundar              |
+#  |---:|:-------|-------:|---------:|:------------------|:-----------------------------|
+#  |  0 | AFE    |   2023 |  3967.86 | AFE               | África Oriental y Meridional |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='iso3', axis=1)
+#  Index: 224 entries, 0 to 8049
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             224 non-null    int64  
+#   1   pib_pc           224 non-null    float64
+#   2   geocodigoFundar  224 non-null    object 
+#   3   geonombreFundar  224 non-null    object 
+#  
+#  |    |   anio |   pib_pc | geocodigoFundar   | geonombreFundar              |
+#  |---:|-------:|---------:|:------------------|:-----------------------------|
+#  |  0 |   2023 |  3967.86 | AFE               | África Oriental y Meridional |
 #  
 #  ------------------------------
 #  
