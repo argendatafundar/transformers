d6aadebedb6bad24c19cba1792bb36584900b343
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-23 12:54:27 -0300

    Updated PESCAS/PESCAS_g13_transformer.py

--- PESCAS/PESCAS_g13_transformer.py
+++ PESCAS/PESCAS_g13_transformer.py
@@ -4,15 +4,16 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	multiplicar_por_escalar(col='desembarque_toneladas', k=0.001)
+	replace_value(col='especie_agregada', curr_value='Merluza Hubbsi', new_value='Merluza hubbsi'),
+	replace_value(col='especie_agregada', curr_value='Calamar Illex', new_value='Calamar illex')
 )
 #  PIPELINE_END
 
@@ -32,7 +33,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  multiplicar_por_escalar(col='desembarque_toneladas', k=0.001)
+#  replace_value(col='especie_agregada', curr_value='Merluza Hubbsi', new_value='Merluza hubbsi')
 #  RangeIndex: 144 entries, 0 to 143
 #  Data columns (total 3 columns):
 #   #   Column                 Non-Null Count  Dtype  
@@ -43,7 +44,22 @@ pipeline = chain(
 #  
 #  |    |   anio | especie_agregada   |   desembarque_toneladas |
 #  |---:|-------:|:-------------------|------------------------:|
-#  |  0 |   1989 | Calamar Illex      |                 23.1017 |
+#  |  0 |   1989 | Calamar Illex      |                 23101.7 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='especie_agregada', curr_value='Calamar Illex', new_value='Calamar illex')
+#  RangeIndex: 144 entries, 0 to 143
+#  Data columns (total 3 columns):
+#   #   Column                 Non-Null Count  Dtype  
+#  ---  ------                 --------------  -----  
+#   0   anio                   144 non-null    int64  
+#   1   especie_agregada       144 non-null    object 
+#   2   desembarque_toneladas  144 non-null    float64
+#  
+#  |    |   anio | especie_agregada   |   desembarque_toneladas |
+#  |---:|-------:|:-------------------|------------------------:|
+#  |  0 |   1989 | Calamar illex      |                 23101.7 |
 #  
 #  ------------------------------
 #  
