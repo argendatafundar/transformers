3a6a083c22d0e0787d71f70263f30463571c8b9f
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-23 18:43:25 -0300

    Updated PESCAS/PESCAS_g13_transformer.py

--- PESCAS/PESCAS_g13_transformer.py
+++ PESCAS/PESCAS_g13_transformer.py
@@ -7,13 +7,19 @@ from data_transformers import chain, transformer
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     df = df.replace({col: curr_value}, new_value)
     return df
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	replace_value(col='especie_agregada', curr_value='Merluza Hubbsi', new_value='Merluza hubbsi'),
-	replace_value(col='especie_agregada', curr_value='Calamar Illex', new_value='Calamar illex')
+	replace_value(col='especie_agregada', curr_value='Calamar Illex', new_value='Calamar illex'),
+	multiplicar_por_escalar(col='desembarque_toneladas', k=0.001)
 )
 #  PIPELINE_END
 
@@ -59,7 +65,22 @@ pipeline = chain(
 #  
 #  |    |   anio | especie_agregada   |   desembarque_toneladas |
 #  |---:|-------:|:-------------------|------------------------:|
-#  |  0 |   1989 | Calamar illex      |                 23101.7 |
+#  |  0 |   1989 | Calamar illex      |                 23.1017 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='desembarque_toneladas', k=0.001)
+#  RangeIndex: 144 entries, 0 to 143
+#  Data columns (total 3 columns):
+#   #   Column                 Non-Null Count  Dtype  
+#  ---  ------                 --------------  -----  
+#   0   anio                   144 non-null    int64  
+#   1   especie_agregada       144 non-null    object 
+#   2   desembarque_toneladas  144 non-null    float64
+#  
+#  |    |   anio | especie_agregada   |   desembarque_toneladas |
+#  |---:|-------:|:-------------------|------------------------:|
+#  |  0 |   1989 | Calamar illex      |                 23.1017 |
 #  
 #  ------------------------------
 #  
