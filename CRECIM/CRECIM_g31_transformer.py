6e0eeeb1917bb58a74079fbdf16c2b3cbb9396e5
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-26 17:49:22 -0300

    Updated CRECIM/CRECIM_g31_transformer.py

--- CRECIM/CRECIM_g31_transformer.py
+++ CRECIM/CRECIM_g31_transformer.py
@@ -4,67 +4,46 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'relativo_arg': 'valor'}),
-	query(condition='anio > 1819')
+	drop_col(col=['geocodigoFundar'], axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 17047 entries, 0 to 17046
-#  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          17047 non-null  int64  
-#   1   iso3          17047 non-null  object 
-#   2   relativo_arg  15936 non-null  float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  17047 non-null  object 
+#   1   geonombreFundar  17047 non-null  object 
+#   2   anio             17047 non-null  int64  
+#   3   relativo_arg     15936 non-null  float64
 #  
-#  |    |   anio | iso3   |   relativo_arg |
-#  |---:|-------:|:-------|---------------:|
-#  |  0 |   1950 | AFG    |         6.8763 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   relativo_arg |
+#  |---:|:------------------|:------------------|-------:|---------------:|
+#  |  0 | AFG               | Afganistán        |   1950 |         6.8763 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'relativo_arg': 'valor'})
+#  drop_col(col=['geocodigoFundar'], axis=1)
 #  RangeIndex: 17047 entries, 0 to 17046
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       17047 non-null  int64  
-#   1   geocodigo  17047 non-null  object 
-#   2   valor      15936 non-null  float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  17047 non-null  object 
+#   1   anio             17047 non-null  int64  
+#   2   relativo_arg     15936 non-null  float64
 #  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1950 | AFG         |  6.8763 |
-#  
-#  ------------------------------
-#  
-#  query(condition='anio > 1819')
-#  Index: 17047 entries, 0 to 17046
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       17047 non-null  int64  
-#   1   geocodigo  17047 non-null  object 
-#   2   valor      15936 non-null  float64
-#  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1950 | AFG         |  6.8763 |
+#  |    | geonombreFundar   |   anio |   relativo_arg |
+#  |---:|:------------------|-------:|---------------:|
+#  |  0 | Afganistán        |   1950 |         6.8763 |
 #  
 #  ------------------------------
 #  
