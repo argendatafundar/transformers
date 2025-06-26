98bafa45bbf2eb1e756c31e077c371c8df6103a2
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-26 17:41:20 -0300

    Updated CRECIM/CRECIM_g30_transformer.py

--- CRECIM/CRECIM_g30_transformer.py
+++ CRECIM/CRECIM_g30_transformer.py
@@ -8,12 +8,12 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 
 @transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
@@ -21,71 +21,74 @@ def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='pib_per_capita', axis=1),
-	rename_cols(map={'iso3': 'geocodigo', 'cambio_relativo': 'valor'}),
-	mutiplicar_por_escalar(col='valor', k=100)
+	query(condition='geonombreFundar in ["Argentina", "Venezuela", "Chile", "Brasil","México", "América Latina", "Uruguay"]'),
+	drop_col(col=['geocodigoFundar', 'pib_per_capita'], axis=1),
+	multiplicar_por_escalar(col='cambio_relativo', k=100)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 7831 entries, 0 to 7830
-#  Data columns (total 4 columns):
+#  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             7831 non-null   int64  
-#   1   iso3             7831 non-null   object 
-#   2   pib_per_capita   7831 non-null   float64
-#   3   cambio_relativo  7831 non-null   float64
+#   0   geocodigoFundar  7831 non-null   object 
+#   1   geonombreFundar  7831 non-null   object 
+#   2   anio             7831 non-null   int64  
+#   3   pib_per_capita   7831 non-null   float64
+#   4   cambio_relativo  7831 non-null   float64
 #  
-#  |    |   anio | iso3   |   pib_per_capita |   cambio_relativo |
-#  |---:|-------:|:-------|-----------------:|------------------:|
-#  |  0 |   1820 | ARG    |             1591 |                 0 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   pib_per_capita |   cambio_relativo |
+#  |---:|:------------------|:------------------|-------:|-----------------:|------------------:|
+#  |  0 | ARG               | Argentina         |   1820 |             1591 |                 0 |
 #  
 #  ------------------------------
 #  
-#  drop_col(col='pib_per_capita', axis=1)
-#  RangeIndex: 7831 entries, 0 to 7830
+#  query(condition='geonombreFundar in ["Argentina", "Venezuela", "Chile", "Brasil","México", "América Latina", "Uruguay"]')
+#  Index: 1040 entries, 0 to 7824
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1040 non-null   object 
+#   1   geonombreFundar  1040 non-null   object 
+#   2   anio             1040 non-null   int64  
+#   3   pib_per_capita   1040 non-null   float64
+#   4   cambio_relativo  1040 non-null   float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   pib_per_capita |   cambio_relativo |
+#  |---:|:------------------|:------------------|-------:|-----------------:|------------------:|
+#  |  0 | ARG               | Argentina         |   1820 |             1591 |                 0 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['geocodigoFundar', 'pib_per_capita'], axis=1)
+#  Index: 1040 entries, 0 to 7824
 #  Data columns (total 3 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             7831 non-null   int64  
-#   1   iso3             7831 non-null   object 
-#   2   cambio_relativo  7831 non-null   float64
+#   0   geonombreFundar  1040 non-null   object 
+#   1   anio             1040 non-null   int64  
+#   2   cambio_relativo  1040 non-null   float64
 #  
-#  |    |   anio | iso3   |   cambio_relativo |
-#  |---:|-------:|:-------|------------------:|
-#  |  0 |   1820 | ARG    |                 0 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'cambio_relativo': 'valor'})
-#  RangeIndex: 7831 entries, 0 to 7830
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       7831 non-null   int64  
-#   1   geocodigo  7831 non-null   object 
-#   2   valor      7831 non-null   float64
-#  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1820 | ARG         |       0 |
+#  |    | geonombreFundar   |   anio |   cambio_relativo |
+#  |---:|:------------------|-------:|------------------:|
+#  |  0 | Argentina         |   1820 |                 0 |
 #  
 #  ------------------------------
 #  
-#  mutiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 7831 entries, 0 to 7830
+#  multiplicar_por_escalar(col='cambio_relativo', k=100)
+#  Index: 1040 entries, 0 to 7824
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       7831 non-null   int64  
-#   1   geocodigo  7831 non-null   object 
-#   2   valor      7831 non-null   float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  1040 non-null   object 
+#   1   anio             1040 non-null   int64  
+#   2   cambio_relativo  1040 non-null   float64
 #  
-#  |    |   anio | geocodigo   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1820 | ARG         |       0 |
+#  |    | geonombreFundar   |   anio |   cambio_relativo |
+#  |---:|:------------------|-------:|------------------:|
+#  |  0 | Argentina         |   1820 |                 0 |
 #  
 #  ------------------------------
 #  
