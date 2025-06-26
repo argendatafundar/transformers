9f9ea9828995156a00ab9ef917a24ff6ecc3153a
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-26 15:13:31 -0300

    Updated CRECIM/CRECIM_g24_transformer.py

--- CRECIM/CRECIM_g24_transformer.py
+++ CRECIM/CRECIM_g24_transformer.py
@@ -4,89 +4,47 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'provincia_id': 'geocodigo', 'pib_pc': 'valor'}),
-	drop_col(col=['region_pbg'], axis=1),
-	query(condition='anio >= 2004')
+	drop_col(col=['geocodigoFundar', 'region_pbg'], axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 672 entries, 0 to 671
-#  Data columns (total 4 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   region_pbg    672 non-null    object 
-#   1   anio          672 non-null    int64  
-#   2   pib_pc        672 non-null    float64
-#   3   provincia_id  672 non-null    object 
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  672 non-null    object 
+#   1   geonombreFundar  672 non-null    object 
+#   2   region_pbg       672 non-null    object 
+#   3   anio             672 non-null    int64  
+#   4   pib_pc           672 non-null    float64
 #  
-#  |    | region_pbg      |   anio |   pib_pc | provincia_id   |
-#  |---:|:----------------|-------:|---------:|:---------------|
-#  |  0 | Pampeana y AMBA |   1895 |  4413.34 | AR-B           |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'provincia_id': 'geocodigo', 'pib_pc': 'valor'})
-#  RangeIndex: 672 entries, 0 to 671
-#  Data columns (total 4 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   region_pbg  672 non-null    object 
-#   1   anio        672 non-null    int64  
-#   2   valor       672 non-null    float64
-#   3   geocodigo   672 non-null    object 
-#  
-#  |    | region_pbg      |   anio |   valor | geocodigo   |
-#  |---:|:----------------|-------:|--------:|:------------|
-#  |  0 | Pampeana y AMBA |   1895 | 4413.34 | AR-B        |
+#  |    | geocodigoFundar   | geonombreFundar   | region_pbg      |   anio |   pib_pc |
+#  |---:|:------------------|:------------------|:----------------|-------:|---------:|
+#  |  0 | AR-B              | Buenos Aires      | Pampeana y AMBA |   1895 |  4413.34 |
 #  
 #  ------------------------------
 #  
-#  drop_col(col=['region_pbg'], axis=1)
+#  drop_col(col=['geocodigoFundar', 'region_pbg'], axis=1)
 #  RangeIndex: 672 entries, 0 to 671
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       672 non-null    int64  
-#   1   valor      672 non-null    float64
-#   2   geocodigo  672 non-null    object 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  672 non-null    object 
+#   1   anio             672 non-null    int64  
+#   2   pib_pc           672 non-null    float64
 #  
-#  |    |   anio |   valor | geocodigo   |
-#  |---:|-------:|--------:|:------------|
-#  |  0 |   1895 | 4413.34 | AR-B        |
-#  
-#  ------------------------------
-#  
-#  query(condition='anio >= 2004')
-#  Index: 456 entries, 9 to 671
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       456 non-null    int64  
-#   1   valor      456 non-null    float64
-#   2   geocodigo  456 non-null    object 
-#  
-#  |    |   anio |   valor | geocodigo   |
-#  |---:|-------:|--------:|:------------|
-#  |  9 |   2004 | 8989.67 | AR-B        |
+#  |    | geonombreFundar   |   anio |   pib_pc |
+#  |---:|:------------------|-------:|---------:|
+#  |  0 | Buenos Aires      |   1895 |  4413.34 |
 #  
 #  ------------------------------
 #  
