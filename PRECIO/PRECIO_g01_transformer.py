6c0640ece1aad5a23ecd4530f5e5091a54fdd1cb
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-06-30 15:03:46 -0300

    Updated PRECIO/PRECIO_g01_transformer.py

--- PRECIO/PRECIO_g01_transformer.py
+++ PRECIO/PRECIO_g01_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict)->DataFrame:
+    df[new_col] = df[curr_col].apply(lambda x: mapper.get(x, None))
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y prendas de vestir', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y prendas de vestir', 'Prendas de vestir y calzado': 'Vivienda, equipamiento y prendas de vestir', 'Comunicaciones': 'Transporte, comunicaciones y otros servicios', 'Transporte': 'Transporte, comunicaciones y otros servicios', 'Bienes y servicios varios': 'Transporte, comunicaciones y otros servicios'})
 )
 #  PIPELINE_END
 
@@ -30,17 +31,18 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y prendas de vestir', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y prendas de vestir', 'Prendas de vestir y calzado': 'Vivienda, equipamiento y prendas de vestir', 'Comunicaciones': 'Transporte, comunicaciones y otros servicios', 'Transporte': 'Transporte, comunicaciones y otros servicios', 'Bienes y servicios varios': 'Transporte, comunicaciones y otros servicios'})
 #  RangeIndex: 12 entries, 0 to 11
-#  Data columns (total 2 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   sector  12 non-null     object 
-#   1   valor   12 non-null     float64
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   sector           12 non-null     object 
+#   1   valor            12 non-null     float64
+#   2   sector_agrupado  12 non-null     object 
 #  
-#  |    | sector                             |   valor |
-#  |---:|:-----------------------------------|--------:|
-#  |  0 | Alimentos y bebidas no alcoholicas |   26.93 |
+#  |    | sector                             |   valor | sector_agrupado             |
+#  |---:|:-----------------------------------|--------:|:----------------------------|
+#  |  0 | Alimentos y bebidas no alcoholicas |   26.93 | Alimentos, bebidas y tabaco |
 #  
 #  ------------------------------
 #  
