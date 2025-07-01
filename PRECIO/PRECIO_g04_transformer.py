30523e24ffb15f426a5e86e42d01b3c237e695f6
LMCappelletti <lcappelletti@fund.ar>
2025-07-01 16:37:48 -0300

    Updated PRECIO/PRECIO_g04_transformer.py

--- PRECIO/PRECIO_g04_transformer.py
+++ PRECIO/PRECIO_g04_transformer.py
@@ -7,60 +7,43 @@ from data_transformers import chain, transformer
 def rename_columns(df: DataFrame, **kwargs):
     df = df.rename(columns=kwargs)
     return df
-
-@transformer.convert
-def inputar(df, row):
-    from pandas import concat, DataFrame
-    return concat([df, DataFrame([row], columns=df.columns)])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(iso3='geocodigo', inflacion_prom_07_22='valor'),
+	rename_columns(geocodigoFundar='geocodigo', geonombreFundar='geonombre', inflacion_prom_07_22='valor')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 132 entries, 0 to 131
-#  Data columns (total 2 columns):
+#  RangeIndex: 137 entries, 0 to 136
+#  Data columns (total 3 columns):
 #   #   Column                Non-Null Count  Dtype  
 #  ---  ------                --------------  -----  
-#   0   iso3                  132 non-null    object 
-#   1   inflacion_prom_07_22  132 non-null    float64
+#   0   geocodigoFundar       137 non-null    object 
+#   1   geonombreFundar       137 non-null    object 
+#   2   inflacion_prom_07_22  137 non-null    float64
 #  
-#  |    | iso3   |   inflacion_prom_07_22 |
-#  |---:|:-------|-----------------------:|
-#  |  0 | AGO    |                16.9548 |
-#  
-#  ------------------------------
-#  
-#  rename_columns(iso3='geocodigo', inflacion_prom_07_22='valor')
-#  RangeIndex: 132 entries, 0 to 131
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  132 non-null    object 
-#   1   valor      132 non-null    float64
-#  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | AGO         | 16.9548 |
+#  |    | geocodigoFundar   | geonombreFundar   |   inflacion_prom_07_22 |
+#  |---:|:------------------|:------------------|-----------------------:|
+#  |  0 | AFG               | Afganistán        |                 5.8615 |
 #  
 #  ------------------------------
 #  
-#  inputar(row=['ZWE', 87.09157143])
-#  Index: 133 entries, 0 to 0
-#  Data columns (total 2 columns):
+#  rename_columns(geocodigoFundar='geocodigo', geonombreFundar='geonombre', inflacion_prom_07_22='valor')
+#  RangeIndex: 137 entries, 0 to 136
+#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  133 non-null    object 
-#   1   valor      133 non-null    float64
+#   0   geocodigo  137 non-null    object 
+#   1   geonombre  137 non-null    object 
+#   2   valor      137 non-null    float64
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | AGO         | 16.9548 |
+#  |    | geocodigo   | geonombre   |   valor |
+#  |---:|:------------|:------------|--------:|
+#  |  0 | AFG         | Afganistán  |  5.8615 |
 #  
 #  ------------------------------
 #  
