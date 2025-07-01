f4cc2bc0dced68999a2a9fb8088b237db351bb23
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-01 18:36:54 -0300

    Updated PRECIO/PRECIO_g05_transformer.py

--- PRECIO/PRECIO_g05_transformer.py
+++ PRECIO/PRECIO_g05_transformer.py
@@ -4,46 +4,46 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
-    return df
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(iso3='geocodigo', inflacion='valor')
+	drop_col(col='geocodigoFundar', axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 3168 entries, 0 to 3167
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       3168 non-null   int64  
-#   1   inflacion  3168 non-null   float64
-#   2   iso3       3168 non-null   object 
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  3168 non-null   object 
+#   1   geonombreFundar  3168 non-null   object 
+#   2   anio             3168 non-null   int64  
+#   3   inflacion        3168 non-null   float64
 #  
-#  |    |   anio |   inflacion | iso3   |
-#  |---:|-------:|------------:|:-------|
-#  |  0 |   2000 |   -0.733699 | ARG    |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   inflacion |
+#  |---:|:------------------|:------------------|-------:|------------:|
+#  |  0 | ARG               | Argentina         |   2000 |   -0.733699 |
 #  
 #  ------------------------------
 #  
-#  rename_columns(iso3='geocodigo', inflacion='valor')
+#  drop_col(col='geocodigoFundar', axis=1)
 #  RangeIndex: 3168 entries, 0 to 3167
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       3168 non-null   int64  
-#   1   valor      3168 non-null   float64
-#   2   geocodigo  3168 non-null   object 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  3168 non-null   object 
+#   1   anio             3168 non-null   int64  
+#   2   inflacion        3168 non-null   float64
 #  
-#  |    |   anio |     valor | geocodigo   |
-#  |---:|-------:|----------:|:------------|
-#  |  0 |   2000 | -0.733699 | ARG         |
+#  |    | geonombreFundar   |   anio |   inflacion |
+#  |---:|:------------------|-------:|------------:|
+#  |  0 | Argentina         |   2000 |   -0.733699 |
 #  
 #  ------------------------------
 #  
