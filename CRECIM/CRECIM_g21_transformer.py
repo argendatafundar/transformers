2879aa5d3cab3883fe6176aa65a6f3db53499cc5
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-02 15:32:21 -0300

    Updated CRECIM/CRECIM_g21_transformer.py

--- CRECIM/CRECIM_g21_transformer.py
+++ CRECIM/CRECIM_g21_transformer.py
@@ -9,13 +9,6 @@ def query(df: DataFrame, condition: str):
     return df
 
 @transformer.convert
-def long_to_wide(df:DataFrame, index:list[str], columns:str, values:str):
-    df = df.pivot(index=index, columns=columns, values=values).reset_index()
-    df.index.name = None
-    df.columns.name = None
-    return df  
-
-@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
@@ -30,8 +23,7 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 pipeline = chain(
 	query(condition='(anio == 1953) | (anio == anio.max())'),
 	drop_col(col=['geocodigoFundar'], axis=1),
-	multiplicar_por_escalar(col='participacion_vab', k=100),
-	long_to_wide(index=['geonombreFundar', 'region_pbg'], columns='anio', values='participacion_vab')
+	multiplicar_por_escalar(col='participacion_vab', k=100)
 )
 #  PIPELINE_END
 
@@ -103,22 +95,6 @@ pipeline = chain(
 #  |    | geonombreFundar   |   anio | region_pbg      |   participacion_vab |     vab |
 #  |---:|:------------------|-------:|:----------------|--------------------:|--------:|
 #  |  4 | Buenos Aires      |   1953 | Pampeana y AMBA |             30.6406 | 36854.1 |
-#  
-#  ------------------------------
-#  
-#  long_to_wide(index=['geonombreFundar', 'region_pbg'], columns='anio', values='participacion_vab')
-#  RangeIndex: 24 entries, 0 to 23
-#  Data columns (total 4 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geonombreFundar  24 non-null     object 
-#   1   region_pbg       24 non-null     object 
-#   2   1953             24 non-null     float64
-#   3   2022             24 non-null     float64
-#  
-#  |    | geonombreFundar   | region_pbg      |    1953 |    2022 |
-#  |---:|:------------------|:----------------|--------:|--------:|
-#  |  0 | Buenos Aires      | Pampeana y AMBA | 30.6406 | 32.5324 |
 #  
 #  ------------------------------
 #  
