6e57645588fed83b9d12ccdc95329bad42b239e8
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-02 15:09:18 -0300

    Updated CRECIM/CRECIM_g21_transformer.py

--- CRECIM/CRECIM_g21_transformer.py
+++ CRECIM/CRECIM_g21_transformer.py
@@ -4,68 +4,52 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_col(col=['provincia_id', 'geocodigoFundar'], axis=1),
-	multiplicar_por_escalar(col='var_participacion', k=100)
+	query(condition='(anio == 1953) | (anio == anio.max())')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 24 entries, 0 to 23
-#  Data columns (total 5 columns):
+#  RangeIndex: 672 entries, 0 to 671
+#  Data columns (total 6 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   region_pbg         24 non-null     object 
-#   1   var_participacion  24 non-null     float64
-#   2   provincia_id       24 non-null     object 
-#   3   geocodigoFundar    24 non-null     object 
-#   4   geonombreFundar    24 non-null     object 
+#   0   geocodigoFundar    672 non-null    object 
+#   1   geonombreFundar    672 non-null    object 
+#   2   anio               672 non-null    int64  
+#   3   region_pbg         672 non-null    object 
+#   4   participacion_vab  672 non-null    float64
+#   5   vab                672 non-null    float64
 #  
-#  |    | region_pbg      |   var_participacion | provincia_id   | geocodigoFundar   | geonombreFundar   |
-#  |---:|:----------------|--------------------:|:---------------|:------------------|:------------------|
-#  |  0 | Pampeana y AMBA |            0.018752 | AR-B           | AR-B              | Buenos Aires      |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | region_pbg      |   participacion_vab |     vab |
+#  |---:|:------------------|:------------------|-------:|:----------------|--------------------:|--------:|
+#  |  0 | AR-B              | Buenos Aires      |   1895 | Pampeana y AMBA |             0.23917 | 4158.05 |
 #  
 #  ------------------------------
 #  
-#  drop_col(col=['provincia_id', 'geocodigoFundar'], axis=1)
-#  RangeIndex: 24 entries, 0 to 23
-#  Data columns (total 3 columns):
+#  query(condition='(anio == 1953) | (anio == anio.max())')
+#  Index: 48 entries, 4 to 671
+#  Data columns (total 6 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   region_pbg         24 non-null     object 
-#   1   var_participacion  24 non-null     float64
-#   2   geonombreFundar    24 non-null     object 
+#   0   geocodigoFundar    48 non-null     object 
+#   1   geonombreFundar    48 non-null     object 
+#   2   anio               48 non-null     int64  
+#   3   region_pbg         48 non-null     object 
+#   4   participacion_vab  48 non-null     float64
+#   5   vab                48 non-null     float64
 #  
-#  |    | region_pbg      |   var_participacion | geonombreFundar   |
-#  |---:|:----------------|--------------------:|:------------------|
-#  |  0 | Pampeana y AMBA |              1.8752 | Buenos Aires      |
-#  
-#  ------------------------------
-#  
-#  multiplicar_por_escalar(col='var_participacion', k=100)
-#  RangeIndex: 24 entries, 0 to 23
-#  Data columns (total 3 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   region_pbg         24 non-null     object 
-#   1   var_participacion  24 non-null     float64
-#   2   geonombreFundar    24 non-null     object 
-#  
-#  |    | region_pbg      |   var_participacion | geonombreFundar   |
-#  |---:|:----------------|--------------------:|:------------------|
-#  |  0 | Pampeana y AMBA |              1.8752 | Buenos Aires      |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | region_pbg      |   participacion_vab |     vab |
+#  |---:|:------------------|:------------------|-------:|:----------------|--------------------:|--------:|
+#  |  4 | AR-B              | Buenos Aires      |   1953 | Pampeana y AMBA |            0.306406 | 36854.1 |
 #  
 #  ------------------------------
 #  
