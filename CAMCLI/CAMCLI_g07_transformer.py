f54c98663c90a48781ee34fc5723ff20f8d30259
jgjuara <jg.juara@gmail.com>
2025-07-02 15:06:16 -0300

    Updated CAMCLI/CAMCLI_g07_transformer.py

--- CAMCLI/CAMCLI_g07_transformer.py
+++ CAMCLI/CAMCLI_g07_transformer.py
@@ -4,46 +4,56 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def recalculate_shares(df: pl.DataFrame, group_col: str, value_col:str):
+    # Group by the specified column and calculate shares within each group
+    df = df.with_columns([
+        pl.col(value_col).sum().over(group_col).alias('group_total')
+    ]).with_columns([
+        (pl.col(value_col) * 100 / pl.col('group_total')).alias('y')
+    ]).drop('group_total')
+    return df
+
+@transformer.convert
+def round(df: pl.DataFrame, col, digits):
+    df = df.with_columns([
+        pl.col(col).round(digits).alias(col)
+    ])
+    return df
+
+@transformer.convert
+def rename_cols(df: pl.DataFrame, map):
+    df = df.rename(mapping=map)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'region': 'categoria', 'sector': 'indicador', 'valor_en_porcent': 'valor'})
+	rename_cols(map={'geonombreFundar': 'x', 'sector': 'categoria', 'valor_en_porcent': 'y'}),
+	round(col='y', digits=1),
+	recalculate_shares(group_col='x', value_col='y'),
+	round(col='y', digits=1)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   region            8 non-null      object 
-#   1   sector            8 non-null      object 
-#   2   valor_en_porcent  8 non-null      float64
-#  
-#  |    | region    | sector   |   valor_en_porcent |
-#  |---:|:----------|:---------|-------------------:|
-#  |  0 | Argentina | Energía  |               53.8 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'region': 'grupo', 'sector': 'indicador', 'valor_en_porcent': 'valor'})
-#  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   grupo      8 non-null      object 
-#   1   indicador  8 non-null      object 
-#   2   valor      8 non-null      float64
+#  rename_cols(map={'geonombreFundar': 'x', 'sector': 'categoria', 'valor_en_porcent': 'y'})
 #  
-#  |    | grupo     | indicador   |   valor |
-#  |---:|:----------|:------------|--------:|
-#  |  0 | Argentina | Energía     |    53.8 |
+#  ------------------------------
+#  
+#  round(col='y', digits=1)
+#  
+#  ------------------------------
+#  
+#  recalculate_shares(group_col='x', value_col='y')
+#  
+#  ------------------------------
+#  
+#  round(col='y', digits=1)
 #  
 #  ------------------------------
 #  
