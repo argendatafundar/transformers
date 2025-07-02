9f115375b2ac71d19b937e8e3979077a9bf8c2ef
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-02 17:13:37 -0300

    Updated PRECIO/PRECIO_g07_transformer.py

--- PRECIO/PRECIO_g07_transformer.py
+++ PRECIO/PRECIO_g07_transformer.py
@@ -4,15 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_columns(df: DataFrame, **kwargs):
-    df = df.rename(columns=kwargs)
+def identity(df: pl.DataFrame) -> pl.DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_columns(pais_o_grupo_de_paises='categoria', mediana_paises_inflacion='valor')
+	identity()
 )
 #  PIPELINE_END
 
@@ -32,18 +31,18 @@ rename_columns(pais_o_grupo_de_paises='categoria', mediana_paises_inflacion='val
 #  
 #  ------------------------------
 #  
-#  rename_columns(pais_o_grupo_de_paises='categoria', mediana_paises_inflacion='valor')
+#  identity()
 #  RangeIndex: 64 entries, 0 to 63
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       64 non-null     int64  
-#   1   valor      64 non-null     float64
-#   2   categoria  64 non-null     object 
+#   #   Column                    Non-Null Count  Dtype  
+#  ---  ------                    --------------  -----  
+#   0   anio                      64 non-null     int64  
+#   1   mediana_paises_inflacion  64 non-null     float64
+#   2   pais_o_grupo_de_paises    64 non-null     object 
 #  
-#  |    |   anio |   valor | categoria   |
-#  |---:|-------:|--------:|:------------|
-#  |  0 |   1992 | 17.5459 | Argentina   |
+#  |    |   anio |   mediana_paises_inflacion | pais_o_grupo_de_paises   |
+#  |---:|-------:|---------------------------:|:-------------------------|
+#  |  0 |   1992 |                    17.5459 | Argentina                |
 #  
 #  ------------------------------
 #  
