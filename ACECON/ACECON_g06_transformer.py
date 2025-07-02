4b42897d961204e38aef64c4fa8d8016ace41bd3
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-02 13:00:34 -0300

    Updated ACECON/ACECON_g06_transformer.py

--- ACECON/ACECON_g06_transformer.py
+++ ACECON/ACECON_g06_transformer.py
@@ -4,109 +4,73 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def abs_col(df:DataFrame, col:str, new_col:str):
+    df[new_col] = df[col].apply(lambda x: abs(x))
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def rescale(df:DataFrame, group_cols:list[str], summarised_col:str, new_col:str) -> DataFrame:
+    df[new_col] = df.groupby(group_cols)[summarised_col].transform(
+    lambda x: 100*(x/x.sum()))
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'comp_dem_ag': 'indicador'}),
-	replace_value(col='indicador', curr_value='Consumo hogares', new_value='Consumo privado'),
-	replace_value(col='indicador', curr_value='Consumo gobierno', new_value='Consumo público'),
-	replace_value(col='indicador', curr_value='Formacion bruta de capital', new_value='Inversión')
+	abs_col(col='valor', new_col='valor_abs'),
+	rescale(group_cols=['anio'], summarised_col='valor_abs', new_col='valor_abs_scaled')
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 528 entries, 0 to 527
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column       Non-Null Count  Dtype  
 #  ---  ------       --------------  -----  
 #   0   anio         528 non-null    int64  
 #   1   comp_dem_ag  528 non-null    object 
 #   2   valor        528 non-null    float64
+#   3   valor_abs    528 non-null    float64
 #  
-#  |    |   anio | comp_dem_ag     |   valor |
-#  |---:|-------:|:----------------|--------:|
-#  |  0 |   1935 | Consumo hogares | 75.7468 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'comp_dem_ag': 'indicador'})
-#  RangeIndex: 528 entries, 0 to 527
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       528 non-null    int64  
-#   1   indicador  528 non-null    object 
-#   2   valor      528 non-null    float64
-#  
-#  |    |   anio | indicador       |   valor |
-#  |---:|-------:|:----------------|--------:|
-#  |  0 |   1935 | Consumo hogares | 75.7468 |
+#  |    |   anio | comp_dem_ag     |   valor |   valor_abs |
+#  |---:|-------:|:----------------|--------:|------------:|
+#  |  0 |   1935 | Consumo hogares | 75.7468 |     75.7468 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='indicador', curr_value='Consumo hogares', new_value='Consumo privado')
+#  abs_col(col='valor', new_col='valor_abs')
 #  RangeIndex: 528 entries, 0 to 527
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       528 non-null    int64  
-#   1   indicador  528 non-null    object 
-#   2   valor      528 non-null    float64
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   anio              528 non-null    int64  
+#   1   comp_dem_ag       528 non-null    object 
+#   2   valor             528 non-null    float64
+#   3   valor_abs         528 non-null    float64
+#   4   valor_abs_scaled  528 non-null    float64
 #  
-#  |    |   anio | indicador       |   valor |
-#  |---:|-------:|:----------------|--------:|
-#  |  0 |   1935 | Consumo privado | 75.7468 |
+#  |    |   anio | comp_dem_ag     |   valor |   valor_abs |   valor_abs_scaled |
+#  |---:|-------:|:----------------|--------:|------------:|-------------------:|
+#  |  0 |   1935 | Consumo hogares | 75.7468 |     75.7468 |            61.5439 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='indicador', curr_value='Consumo gobierno', new_value='Consumo público')
+#  rescale(group_cols=['anio'], summarised_col='valor_abs', new_col='valor_abs_scaled')
 #  RangeIndex: 528 entries, 0 to 527
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       528 non-null    int64  
-#   1   indicador  528 non-null    object 
-#   2   valor      528 non-null    float64
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   anio              528 non-null    int64  
+#   1   comp_dem_ag       528 non-null    object 
+#   2   valor             528 non-null    float64
+#   3   valor_abs         528 non-null    float64
+#   4   valor_abs_scaled  528 non-null    float64
 #  
-#  |    |   anio | indicador       |   valor |
-#  |---:|-------:|:----------------|--------:|
-#  |  0 |   1935 | Consumo privado | 75.7468 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='indicador', curr_value='Formacion bruta de capital', new_value='Inversión')
-#  RangeIndex: 528 entries, 0 to 527
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       528 non-null    int64  
-#   1   indicador  528 non-null    object 
-#   2   valor      528 non-null    float64
-#  
-#  |    |   anio | indicador       |   valor |
-#  |---:|-------:|:----------------|--------:|
-#  |  0 |   1935 | Consumo privado | 75.7468 |
+#  |    |   anio | comp_dem_ag     |   valor |   valor_abs |   valor_abs_scaled |
+#  |---:|-------:|:----------------|--------:|------------:|-------------------:|
+#  |  0 |   1935 | Consumo hogares | 75.7468 |     75.7468 |            61.5439 |
 #  
 #  ------------------------------
 #  
