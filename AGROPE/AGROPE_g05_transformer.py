f0db721c2fe0d7b576eb21f3e2f50c6d3a0d66e1
LMCappelletti <lcappelletti@fund.ar>
2025-07-02 15:36:04 -0300

    Updated AGROPE/AGROPE_g05_transformer.py

--- AGROPE/AGROPE_g05_transformer.py
+++ AGROPE/AGROPE_g05_transformer.py
@@ -4,40 +4,35 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
 
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def drop_na(df, subset:str): 
+    return df.dropna(subset=subset, axis=0)
+
+@transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='anio == 2021'),
-	query(condition="~ iso3c.isin(['SSA', 'TMN','MNA', 'TSS', 'LAC', 'TLA', 'TEC', 'ECA', 'TSA','TEA', 'EAP', 'XT', 'XN', 'XM', 'XD'])"),
-	rename_cols(map={'iso3c': 'geocodigo', 'va_agro_sobre_pbi': 'valor'}),
+	rename_cols(map={'geocodigoFundar': 'geocodigo', 'geonombreFundar': 'geonombre', 'va_agro_sobre_pbi': 'valor'}),
+	drop_na(subset=['valor']),
 	sort_values(how='descending', by='valor'),
-	drop_col(col=['anio', 'pais'], axis=1)
+	query(condition="~ geocodigo.isin(['SSA', 'TMN','MNA', 'TSS', 'LAC', 'TLA', 'TEC', 'ECA', 'TSA','TEA', 'EAP', 'XT', 'XN', 'XM', 'XD'])"),
+	query(condition='anio == 2021')
 )
 #  PIPELINE_END
 
@@ -47,92 +42,94 @@ query(condition='anio == 2021'),
 #  Data columns (total 4 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   iso3c              10772 non-null  object 
-#   1   anio               10772 non-null  int64  
-#   2   va_agro_sobre_pbi  10772 non-null  float64
-#   3   pais               10772 non-null  object 
+#   0   geocodigoFundar    10772 non-null  object 
+#   1   geonombreFundar    10772 non-null  object 
+#   2   anio               10772 non-null  int64  
+#   3   va_agro_sobre_pbi  10772 non-null  float64
 #  
-#  |    | iso3c   |   anio |   va_agro_sobre_pbi | pais   |
-#  |---:|:--------|-------:|--------------------:|:-------|
-#  |  0 | ABW     |   1995 |            0.505922 | Aruba  |
-#  
-#  ------------------------------
-#  
-#  query(condition='anio == 2021')
-#  Index: 240 entries, 65 to 10769
-#  Data columns (total 4 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   iso3c              240 non-null    object 
-#   1   anio               240 non-null    int64  
-#   2   va_agro_sobre_pbi  240 non-null    float64
-#   3   pais               240 non-null    object 
-#  
-#  |    | iso3c   |   anio |   va_agro_sobre_pbi | pais                         |
-#  |---:|:--------|-------:|--------------------:|:-----------------------------|
-#  | 65 | AFE     |   2021 |             13.4028 | África Oriental y Meridional |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   va_agro_sobre_pbi |
+#  |---:|:------------------|:------------------|-------:|--------------------:|
+#  |  0 | ABW               | Aruba             |   1995 |            0.505922 |
 #  
 #  ------------------------------
 #  
-#  query(condition="~ iso3c.isin(['SSA', 'TMN','MNA', 'TSS', 'LAC', 'TLA', 'TEC', 'ECA', 'TSA','TEA', 'EAP', 'XT', 'XN', 'XM', 'XD'])")
-#  Index: 229 entries, 65 to 10769
-#  Data columns (total 4 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   iso3c              229 non-null    object 
-#   1   anio               229 non-null    int64  
-#   2   va_agro_sobre_pbi  229 non-null    float64
-#   3   pais               229 non-null    object 
-#  
-#  |    | iso3c   |   anio |   va_agro_sobre_pbi | pais                         |
-#  |---:|:--------|-------:|--------------------:|:-----------------------------|
-#  | 65 | AFE     |   2021 |             13.4028 | África Oriental y Meridional |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3c': 'geocodigo', 'va_agro_sobre_pbi': 'valor'})
-#  Index: 229 entries, 65 to 10769
+#  rename_cols(map={'geocodigoFundar': 'geocodigo', 'geonombreFundar': 'geonombre', 'va_agro_sobre_pbi': 'valor'})
+#  RangeIndex: 10772 entries, 0 to 10771
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   geocodigo  229 non-null    object 
-#   1   anio       229 non-null    int64  
-#   2   valor      229 non-null    float64
-#   3   pais       229 non-null    object 
+#   0   geocodigo  10772 non-null  object 
+#   1   geonombre  10772 non-null  object 
+#   2   anio       10772 non-null  int64  
+#   3   valor      10772 non-null  float64
 #  
-#  |    | geocodigo   |   anio |   valor | pais                         |
-#  |---:|:------------|-------:|--------:|:-----------------------------|
-#  | 65 | AFE         |   2021 | 13.4028 | África Oriental y Meridional |
+#  |    | geocodigo   | geonombre   |   anio |    valor |
+#  |---:|:------------|:------------|-------:|---------:|
+#  |  0 | ABW         | Aruba       |   1995 | 0.505922 |
+#  
+#  ------------------------------
+#  
+#  drop_na(subset=['valor'])
+#  RangeIndex: 10772 entries, 0 to 10771
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  10772 non-null  object 
+#   1   geonombre  10772 non-null  object 
+#   2   anio       10772 non-null  int64  
+#   3   valor      10772 non-null  float64
+#  
+#  |    | geocodigo   | geonombre   |   anio |    valor |
+#  |---:|:------------|:------------|-------:|---------:|
+#  |  0 | ABW         | Aruba       |   1995 | 0.505922 |
 #  
 #  ------------------------------
 #  
 #  sort_values(how='descending', by='valor')
-#  RangeIndex: 229 entries, 0 to 228
+#  RangeIndex: 10772 entries, 0 to 10771
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  10772 non-null  object 
+#   1   geonombre  10772 non-null  object 
+#   2   anio       10772 non-null  int64  
+#   3   valor      10772 non-null  float64
+#  
+#  |    | geocodigo   | geonombre   |   anio |   valor |
+#  |---:|:------------|:------------|-------:|--------:|
+#  |  0 | LSO         | Lesotho     |   1961 | 89.4145 |
+#  
+#  ------------------------------
+#  
+#  query(condition="~ geocodigo.isin(['SSA', 'TMN','MNA', 'TSS', 'LAC', 'TLA', 'TEC', 'ECA', 'TSA','TEA', 'EAP', 'XT', 'XN', 'XM', 'XD'])")
+#  Index: 10290 entries, 0 to 10771
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  10290 non-null  object 
+#   1   geonombre  10290 non-null  object 
+#   2   anio       10290 non-null  int64  
+#   3   valor      10290 non-null  float64
+#  
+#  |    | geocodigo   | geonombre   |   anio |   valor |
+#  |---:|:------------|:------------|-------:|--------:|
+#  |  0 | LSO         | Lesotho     |   1961 | 89.4145 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio == 2021')
+#  Index: 229 entries, 125 to 10765
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
 #   0   geocodigo  229 non-null    object 
-#   1   anio       229 non-null    int64  
-#   2   valor      229 non-null    float64
-#   3   pais       229 non-null    object 
+#   1   geonombre  229 non-null    object 
+#   2   anio       229 non-null    int64  
+#   3   valor      229 non-null    float64
 #  
-#  |    | geocodigo   |   anio |   valor | pais         |
-#  |---:|:------------|-------:|--------:|:-------------|
-#  |  0 | SLE         |   2021 | 57.4488 | Sierra Leona |
-#  
-#  ------------------------------
-#  
-#  drop_col(col=['anio', 'pais'], axis=1)
-#  RangeIndex: 229 entries, 0 to 228
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  229 non-null    object 
-#   1   valor      229 non-null    float64
-#  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | SLE         | 57.4488 |
+#  |     | geocodigo   | geonombre    |   anio |   valor |
+#  |----:|:------------|:-------------|-------:|--------:|
+#  | 125 | SLE         | Sierra Leona |   2021 | 57.4488 |
 #  
 #  ------------------------------
 #  
