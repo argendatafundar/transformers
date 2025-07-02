cbae7b6b5951ef2b7e9ef13bfad3679f0835f6ca
jgjuara <jg.juara@gmail.com>
2025-07-02 14:47:30 -0300

    Updated CAMCLI/CAMCLI_g06_transformer.py

--- CAMCLI/CAMCLI_g06_transformer.py
+++ CAMCLI/CAMCLI_g06_transformer.py
@@ -4,135 +4,72 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def latest_year(df, by='anio'):
+def latest_year(df: pl.DataFrame, by='anio'):
     latest_year = df[by].max()
-    df = df.query(f'{by} == {latest_year}')
-    df = df.drop(columns = by)
+    df = df.filter(pl.col(by) == latest_year)
+    df = df.drop(by)
     return df
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
+def rename_cols(df: pl.DataFrame, map):
+    return df.rename(map)
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
+def drop_col(df: pl.DataFrame, col, axis=1):
+    # In Polars, axis parameter is not needed
+    if isinstance(col, str):
+        return df.drop(col)
+    elif isinstance(col, list):
+        return df.drop(col)
+    else:
+        return df.drop([col])
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
+def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
 
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+    if not alias:
+        alias = col
+
+    df = df.with_columns(
+        pl.col(col).replace(mapping).alias(alias)
+    )
+
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-latest_year(by='anio'),
+	latest_year(by='anio'),
 	rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'valor_en_porcent': 'valor'}),
 	drop_col(col=['valor_en_mtco2e'], axis=1),
-	replace_value(col='nivel1', curr_value='Procesos industriales y uso de productos', new_value='PIUP'),
-	replace_value(col='nivel1', curr_value='Agricultura, ganadería, silvicultura y otros usos de la tierra', new_value='AGSyOUT')
+	replace_value(col='nivel1', mapping={'Procesos industriales y uso de productos': 'PIUP', 'Agricultura, ganadería, silvicultura y otros usos de la tierra': 'AGSyOUT'}, alias=None),
+	replace_value(col='nivel2', mapping={'Industrias de la energía': 'Energía', 'Industrias manufactureras y de la construcción': 'Industria y construcción', 'Emisiones fugitivas provenientes de la fabricación de combustibles': 'Emisiones fugitivas'}, alias=None)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 435 entries, 0 to 434
-#  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   sector            435 non-null    object 
-#   1   subsector         435 non-null    object 
-#   2   anio              435 non-null    int64  
-#   3   valor_en_mtco2e   435 non-null    float64
-#   4   valor_en_porcent  435 non-null    float64
-#  
-#  |    | sector                                                         | subsector                        |   anio |   valor_en_mtco2e |   valor_en_porcent |
-#  |---:|:---------------------------------------------------------------|:---------------------------------|-------:|------------------:|-------------------:|
-#  |  0 | Agricultura, ganadería, silvicultura y otros usos de la tierra | Emisiones de la quema de biomasa |   1990 |           9.03216 |            3.42726 |
 #  
 #  ------------------------------
 #  
 #  latest_year(by='anio')
-#  Index: 15 entries, 28 to 434
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   sector            15 non-null     object 
-#   1   subsector         15 non-null     object 
-#   2   valor_en_mtco2e   15 non-null     float64
-#   3   valor_en_porcent  15 non-null     float64
-#  
-#  |    | sector                                                         | subsector                        |   valor_en_mtco2e |   valor_en_porcent |
-#  |---:|:---------------------------------------------------------------|:---------------------------------|------------------:|-------------------:|
-#  | 28 | Agricultura, ganadería, silvicultura y otros usos de la tierra | Emisiones de la quema de biomasa |           4.52477 |            1.23665 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'sector': 'nivel1', 'subsector': 'nivel2', 'valor_en_porcent': 'valor'})
-#  Index: 15 entries, 28 to 434
-#  Data columns (total 4 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   nivel1           15 non-null     object 
-#   1   nivel2           15 non-null     object 
-#   2   valor_en_mtco2e  15 non-null     float64
-#   3   valor            15 non-null     float64
-#  
-#  |    | nivel1                                                         | nivel2                           |   valor_en_mtco2e |   valor |
-#  |---:|:---------------------------------------------------------------|:---------------------------------|------------------:|--------:|
-#  | 28 | Agricultura, ganadería, silvicultura y otros usos de la tierra | Emisiones de la quema de biomasa |           4.52477 | 1.23665 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col=['valor_en_mtco2e'], axis=1)
-#  Index: 15 entries, 28 to 434
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   nivel1  15 non-null     object 
-#   1   nivel2  15 non-null     object 
-#   2   valor   15 non-null     float64
-#  
-#  |    | nivel1                                                         | nivel2                           |   valor |
-#  |---:|:---------------------------------------------------------------|:---------------------------------|--------:|
-#  | 28 | Agricultura, ganadería, silvicultura y otros usos de la tierra | Emisiones de la quema de biomasa | 1.23665 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='nivel1', curr_value='Procesos industriales y uso de productos', new_value='PIUP')
-#  Index: 15 entries, 28 to 434
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   nivel1  15 non-null     object 
-#   1   nivel2  15 non-null     object 
-#   2   valor   15 non-null     float64
-#  
-#  |    | nivel1                                                         | nivel2                           |   valor |
-#  |---:|:---------------------------------------------------------------|:---------------------------------|--------:|
-#  | 28 | Agricultura, ganadería, silvicultura y otros usos de la tierra | Emisiones de la quema de biomasa | 1.23665 |
+#  replace_value(col='nivel1', mapping={'Procesos industriales y uso de productos': 'PIUP', 'Agricultura, ganadería, silvicultura y otros usos de la tierra': 'AGSyOUT'}, alias=None)
 #  
 #  ------------------------------
 #  
-#  replace_value(col='nivel1', curr_value='Agricultura, ganadería, silvicultura y otros usos de la tierra', new_value='AGSyOUT')
-#  Index: 15 entries, 28 to 434
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   nivel1  15 non-null     object 
-#   1   nivel2  15 non-null     object 
-#   2   valor   15 non-null     float64
-#  
-#  |    | nivel1   | nivel2                           |   valor |
-#  |---:|:---------|:---------------------------------|--------:|
-#  | 28 | AGSyOUT  | Emisiones de la quema de biomasa | 1.23665 |
+#  replace_value(col='nivel2', mapping={'Industrias de la energía': 'Energía', 'Industrias manufactureras y de la construcción': 'Industria y construcción', 'Emisiones fugitivas provenientes de la fabricación de combustibles': 'Emisiones fugitivas'}, alias=None)
 #  
 #  ------------------------------
 #  
