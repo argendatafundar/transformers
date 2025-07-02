192d277bd2f1fd9b3db837422748189f6e13d31a
jgjuara <jg.juara@gmail.com>
2025-07-02 13:36:02 -0300

    Updated TRANEN/TRANEN_g15_transformer.py

--- TRANEN/TRANEN_g15_transformer.py
+++ TRANEN/TRANEN_g15_transformer.py
@@ -8,6 +8,10 @@ def drop_col(df: pl.DataFrame, col, axis=1):
     return df.drop(col)
 
 @transformer.convert
+def rename_cols(df: pl.DataFrame, map):
+    return df.rename(map)
+
+@transformer.convert
 def replace_value(df: pl.DataFrame, col: str, curr_value: str, new_value: str):
     df = df.with_columns(pl.col(col).replace(curr_value, new_value))
     return df
@@ -83,17 +87,18 @@ def wide_to_long(df: pl.DataFrame, primary_keys, value_name='valor', var_name='i
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_col(col='geonombreFundar', axis=1),
+	sort_values(how='ascending', by=['anio', 'geocodigoFundar']),
 	recalculo_kaya(group='geocodigoFundar', date_col='anio'),
 	query(condition='pl.col("geocodigoFundar") == "WLD"'),
-	wide_to_long(primary_keys=['anio', 'geocodigoFundar'], value_name='valor', var_name='categoria'),
+	rename_cols(map={'geocodigoFundar': 'geocodigo'}),
+	wide_to_long(primary_keys=['anio', 'geocodigo'], value_name='valor', var_name='categoria'),
 	replace_value(col='categoria', curr_value='emision_anual_co2_ton', new_value='Emisiones anuales de CO2'),
 	replace_value(col='categoria', curr_value='energia_por_unidad_pib_kwh', new_value='Intensidad energética'),
 	replace_value(col='categoria', curr_value='pib_per_cap_usd_ppa_2011', new_value='PIB per cápita en dólares'),
 	replace_value(col='categoria', curr_value='poblacion', new_value='Población'),
 	replace_value(col='categoria', curr_value='emision_anual_kgco2_por_kwh', new_value='Intensidad de carbono*'),
 	replace_value(col='categoria', curr_value='emision_anual_kgco2_por_usd_ppa_2011', new_value='Intensidad de carbono**'),
-	drop_col(col='geocodigoFundar', axis=1),
+	drop_col(col='geocodigo', axis=1),
 	drop_na(cols='valor'),
 	sort_values(how='ascending', by=['anio', 'categoria'])
 )
@@ -104,7 +109,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  drop_col(col='geonombreFundar', axis=1)
+#  sort_values(how='ascending', by=['anio', 'geocodigoFundar'])
 #  
 #  ------------------------------
 #  
@@ -116,7 +121,11 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['anio', 'geocodigoFundar'], value_name='valor', var_name='categoria')
+#  rename_cols(map={'geocodigoFundar': 'geocodigo'})
+#  
+#  ------------------------------
+#  
+#  wide_to_long(primary_keys=['anio', 'geocodigo'], value_name='valor', var_name='categoria')
 #  
 #  ------------------------------
 #  
@@ -144,7 +153,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  drop_col(col='geocodigoFundar', axis=1)
+#  drop_col(col='geocodigo', axis=1)
 #  
 #  ------------------------------
 #  
