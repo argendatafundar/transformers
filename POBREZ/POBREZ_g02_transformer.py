3b07b2ac05fac7347d8588c34ddd84d5eeee67e2
jgjuara <jg.juara@gmail.com>
2025-07-03 00:27:41 -0300

    Updated POBREZ/POBREZ_g02_transformer.py

--- POBREZ/POBREZ_g02_transformer.py
+++ POBREZ/POBREZ_g02_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
+    df = df.sql(query)
+    return df
+
+@transformer.convert
 def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
 
     if not alias:
@@ -19,7 +24,8 @@ def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='province', mapping={'san_luis': 'San Luis', 'neuquen': 'Neuquén', 'santa_fe': 'Santa Fe', 'san_juan': 'San Juan', 'santiago_del_estero': 'Santiago del Estero', 'PBA': 'Buenos Aires', 'caba': 'CABA', 'la_pampa': 'La Pampa', 'tierra_del_fuego': 'Tierra del Fuego', 'salta': 'Salta', 'chubut': 'Chubut', 'cordoba': 'Córdoba', 'tucuman': 'Tucumán', 'jujuy': 'Jujuy', 'chaco': 'Chaco', 'la_rioja': 'La Rioja', 'catamarca': 'Catamarca', 'rio_negro': 'Río Negro', 'mendoza': 'Mendoza', 'formosa': 'Formosa', 'partidos_GBA': 'Partidos del GBA', 'santa_cruz': 'Santa Cruz', 'entre rios': 'Entre Ríos', 'corrientes': 'Corrientes', 'misiones': 'Misiones', 'argentina': 'Total Nacional'}, alias=None)
+	replace_value(col='province', mapping={'san_luis': 'San Luis', 'neuquen': 'Neuquén', 'santa_fe': 'Santa Fe', 'san_juan': 'San Juan', 'santiago_del_estero': 'Santiago del Estero', 'PBA': 'Buenos Aires', 'caba': 'CABA', 'la_pampa': 'La Pampa', 'tierra_del_fuego': 'Tierra del Fuego', 'salta': 'Salta', 'chubut': 'Chubut', 'cordoba': 'Córdoba', 'tucuman': 'Tucumán', 'jujuy': 'Jujuy', 'chaco': 'Chaco', 'la_rioja': 'La Rioja', 'catamarca': 'Catamarca', 'rio_negro': 'Río Negro', 'mendoza': 'Mendoza', 'formosa': 'Formosa', 'partidos_GBA': 'Partidos del GBA', 'santa_cruz': 'Santa Cruz', 'entre rios': 'Entre Ríos', 'corrientes': 'Corrientes', 'misiones': 'Misiones', 'argentina': 'Total Nacional'}, alias=None),
+	df_sql(query='select * from self where year  in (1980, 2022)')
 )
 #  PIPELINE_END
 
@@ -29,6 +35,10 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  replace_value(col='province', mapping={'san_luis': 'San Luis', 'neuquen': 'Neuquén', 'santa_fe': 'Santa Fe', 'san_juan': 'San Juan', 'santiago_del_estero': 'Santiago del Estero', 'PBA': 'Buenos Aires', 'caba': 'CABA', 'la_pampa': 'La Pampa', 'tierra_del_fuego': 'Tierra del Fuego', 'salta': 'Salta', 'chubut': 'Chubut', 'cordoba': 'Córdoba', 'tucuman': 'Tucumán', 'jujuy': 'Jujuy', 'chaco': 'Chaco', 'la_rioja': 'La Rioja', 'catamarca': 'Catamarca', 'rio_negro': 'Río Negro', 'mendoza': 'Mendoza', 'formosa': 'Formosa', 'partidos_GBA': 'Partidos del GBA', 'santa_cruz': 'Santa Cruz', 'entre rios': 'Entre Ríos', 'corrientes': 'Corrientes', 'misiones': 'Misiones', 'argentina': 'Total Nacional'}, alias=None)
+#  
+#  ------------------------------
+#  
+#  df_sql(query='select * from self where year  in (1980, 2022)')
 #  
 #  ------------------------------
 #  
