7c0fbadd618d54f058fac8a8e24fd5aa4d6f76c9
LMCappelletti <lcappelletti@fund.ar>
2025-07-03 17:11:07 -0300

    Updated AGROPE/AGROPE_g12_transformer.py

--- AGROPE/AGROPE_g12_transformer.py
+++ AGROPE/AGROPE_g12_transformer.py
@@ -4,23 +4,8 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
     return df
 
 @transformer.convert
@@ -29,28 +14,10 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
-    return df
-
-@transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
-    
+
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
@@ -62,17 +29,11 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-replace_value(col='iso3', curr_value='F15', new_value='BLX'),
-	replace_value(col='iso3', curr_value='F228', new_value='SVU'),
-	replace_value(col='iso3', curr_value='F248', new_value='SER'),
-	replace_value(col='iso3', curr_value='F51', new_value='CSK'),
-	rename_cols(map={'iso3': 'geocodigo', 'rindes_soja_ma5': 'valor'}),
-	drop_col(col='iso3_desc_fundar', axis=1),
-	drop_col(col='rindes', axis=1),
+	rename_cols(map={'rindes_soja_ma5': 'valor'}),
 	query(condition='anio >= 1965'),
 	drop_na(col=['valor']),
-	sort_values(how='ascending', by=['anio', 'geocodigo']),
-	query(condition="geocodigo != 'F351'")
+	sort_values(how='ascending', by=['anio', 'geocodigoFundar']),
+	query(condition="geocodigoFundar != 'F351'")
 )
 #  PIPELINE_END
 
@@ -80,193 +41,102 @@ replace_value(col='iso3', curr_value='F15', new_value='BLX'),
 #  start()
 #  RangeIndex: 4771 entries, 0 to 4770
 #  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              4771 non-null   object 
-#   1   iso3_desc_fundar  4771 non-null   object 
-#   2   anio              4771 non-null   int64  
-#   3   rindes            4771 non-null   float64
-#   4   rindes_soja_ma5   4771 non-null   object 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  4771 non-null   object 
+#   1   geonombreFundar  4771 non-null   object 
+#   2   anio             4771 non-null   int64  
+#   3   rindes           4771 non-null   float64
+#   4   rindes_soja_ma5  4771 non-null   object 
 #  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_soja_ma5   |
-#  |---:|:-------|:-------------------|-------:|---------:|:------------------|
-#  |  0 | AGO    | Angola             |   2000 |   0.2333 | NA                |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   rindes | rindes_soja_ma5   |
+#  |---:|:------------------|:------------------|-------:|---------:|:------------------|
+#  |  0 | AGO               | Angola            |   2000 |   0.2333 | NA                |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='iso3', curr_value='F15', new_value='BLX')
+#  rename_cols(map={'rindes_soja_ma5': 'valor'})
 #  RangeIndex: 4771 entries, 0 to 4770
 #  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              4771 non-null   object 
-#   1   iso3_desc_fundar  4771 non-null   object 
-#   2   anio              4771 non-null   int64  
-#   3   rindes            4771 non-null   float64
-#   4   rindes_soja_ma5   4771 non-null   object 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  4771 non-null   object 
+#   1   geonombreFundar  4771 non-null   object 
+#   2   anio             4771 non-null   int64  
+#   3   rindes           4771 non-null   float64
+#   4   valor            4771 non-null   object 
 #  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_soja_ma5   |
-#  |---:|:-------|:-------------------|-------:|---------:|:------------------|
-#  |  0 | AGO    | Angola             |   2000 |   0.2333 | NA                |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F228', new_value='SVU')
-#  RangeIndex: 4771 entries, 0 to 4770
-#  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              4771 non-null   object 
-#   1   iso3_desc_fundar  4771 non-null   object 
-#   2   anio              4771 non-null   int64  
-#   3   rindes            4771 non-null   float64
-#   4   rindes_soja_ma5   4771 non-null   object 
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_soja_ma5   |
-#  |---:|:-------|:-------------------|-------:|---------:|:------------------|
-#  |  0 | AGO    | Angola             |   2000 |   0.2333 | NA                |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F248', new_value='SER')
-#  RangeIndex: 4771 entries, 0 to 4770
-#  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              4771 non-null   object 
-#   1   iso3_desc_fundar  4771 non-null   object 
-#   2   anio              4771 non-null   int64  
-#   3   rindes            4771 non-null   float64
-#   4   rindes_soja_ma5   4771 non-null   object 
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_soja_ma5   |
-#  |---:|:-------|:-------------------|-------:|---------:|:------------------|
-#  |  0 | AGO    | Angola             |   2000 |   0.2333 | NA                |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F51', new_value='CSK')
-#  RangeIndex: 4771 entries, 0 to 4770
-#  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              4771 non-null   object 
-#   1   iso3_desc_fundar  4771 non-null   object 
-#   2   anio              4771 non-null   int64  
-#   3   rindes            4771 non-null   float64
-#   4   rindes_soja_ma5   4771 non-null   object 
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   rindes | rindes_soja_ma5   |
-#  |---:|:-------|:-------------------|-------:|---------:|:------------------|
-#  |  0 | AGO    | Angola             |   2000 |   0.2333 | NA                |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'rindes_soja_ma5': 'valor'})
-#  RangeIndex: 4771 entries, 0 to 4770
-#  Data columns (total 5 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   geocodigo         4771 non-null   object 
-#   1   iso3_desc_fundar  4771 non-null   object 
-#   2   anio              4771 non-null   int64  
-#   3   rindes            4771 non-null   float64
-#   4   valor             4771 non-null   object 
-#  
-#  |    | geocodigo   | iso3_desc_fundar   |   anio |   rindes | valor   |
-#  |---:|:------------|:-------------------|-------:|---------:|:--------|
-#  |  0 | AGO         | Angola             |   2000 |   0.2333 | NA      |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3_desc_fundar', axis=1)
-#  RangeIndex: 4771 entries, 0 to 4770
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  4771 non-null   object 
-#   1   anio       4771 non-null   int64  
-#   2   rindes     4771 non-null   float64
-#   3   valor      4771 non-null   object 
-#  
-#  |    | geocodigo   |   anio |   rindes | valor   |
-#  |---:|:------------|-------:|---------:|:--------|
-#  |  0 | AGO         |   2000 |   0.2333 | NA      |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='rindes', axis=1)
-#  RangeIndex: 4771 entries, 0 to 4770
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   geocodigo  4771 non-null   object
-#   1   anio       4771 non-null   int64 
-#   2   valor      4771 non-null   object
-#  
-#  |    | geocodigo   |   anio | valor   |
-#  |---:|:------------|-------:|:--------|
-#  |  0 | AGO         |   2000 | NA      |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   rindes | valor   |
+#  |---:|:------------------|:------------------|-------:|---------:|:--------|
+#  |  0 | AGO               | Angola            |   2000 |   0.2333 | NA      |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio >= 1965')
 #  Index: 4576 entries, 0 to 4770
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   geocodigo  4576 non-null   object
-#   1   anio       4576 non-null   int64 
-#   2   valor      4576 non-null   object
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  4576 non-null   object 
+#   1   geonombreFundar  4576 non-null   object 
+#   2   anio             4576 non-null   int64  
+#   3   rindes           4576 non-null   float64
+#   4   valor            4576 non-null   object 
 #  
-#  |    | geocodigo   |   anio | valor   |
-#  |---:|:------------|-------:|:--------|
-#  |  0 | AGO         |   2000 | NA      |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   rindes | valor   |
+#  |---:|:------------------|:------------------|-------:|---------:|:--------|
+#  |  0 | AGO               | Angola            |   2000 |   0.2333 | NA      |
 #  
 #  ------------------------------
 #  
 #  drop_na(col=['valor'])
 #  Index: 4576 entries, 0 to 4770
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   geocodigo  4576 non-null   object
-#   1   anio       4576 non-null   int64 
-#   2   valor      4576 non-null   object
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  4576 non-null   object 
+#   1   geonombreFundar  4576 non-null   object 
+#   2   anio             4576 non-null   int64  
+#   3   rindes           4576 non-null   float64
+#   4   valor            4576 non-null   object 
 #  
-#  |    | geocodigo   |   anio | valor   |
-#  |---:|:------------|-------:|:--------|
-#  |  0 | AGO         |   2000 | NA      |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   rindes | valor   |
+#  |---:|:------------------|:------------------|-------:|---------:|:--------|
+#  |  0 | AGO               | Angola            |   2000 |   0.2333 | NA      |
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  sort_values(how='ascending', by=['anio', 'geocodigoFundar'])
 #  RangeIndex: 4576 entries, 0 to 4575
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   geocodigo  4576 non-null   object
-#   1   anio       4576 non-null   int64 
-#   2   valor      4576 non-null   object
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  4576 non-null   object 
+#   1   geonombreFundar  4576 non-null   object 
+#   2   anio             4576 non-null   int64  
+#   3   rindes           4576 non-null   float64
+#   4   valor            4576 non-null   object 
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ARG         |   1965 | 1.06008 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   rindes |   valor |
+#  |---:|:------------------|:------------------|-------:|---------:|--------:|
+#  |  0 | ARG               | Argentina         |   1965 |   1.0352 | 1.06008 |
 #  
 #  ------------------------------
 #  
-#  query(condition="geocodigo != 'F351'")
+#  query(condition="geocodigoFundar != 'F351'")
 #  Index: 4544 entries, 0 to 4575
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype 
-#  ---  ------     --------------  ----- 
-#   0   geocodigo  4544 non-null   object
-#   1   anio       4544 non-null   int64 
-#   2   valor      4544 non-null   object
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  4544 non-null   object 
+#   1   geonombreFundar  4544 non-null   object 
+#   2   anio             4544 non-null   int64  
+#   3   rindes           4544 non-null   float64
+#   4   valor            4544 non-null   object 
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ARG         |   1965 | 1.06008 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   rindes |   valor |
+#  |---:|:------------------|:------------------|-------:|---------:|--------:|
+#  |  0 | ARG               | Argentina         |   1965 |   1.0352 | 1.06008 |
 #  
 #  ------------------------------
 #  
