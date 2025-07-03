4ae914f2064568e2807c541b4c2991b85fe7930d
LMCappelletti <lcappelletti@fund.ar>
2025-07-03 18:22:36 -0300

    Updated AGROPE/AGROPE_g14_transformer.py

--- AGROPE/AGROPE_g14_transformer.py
+++ AGROPE/AGROPE_g14_transformer.py
@@ -4,24 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -35,12 +17,9 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-replace_value(col='iso3', curr_value='CHA', new_value='CHI'),
 	query(condition='anio == anio.max()'),
-	drop_col(col='iso3_desc_fundar', axis=1),
-	drop_col(col='anio', axis=1),
 	rename_cols(map={'iso3': 'geocodigo'}),
-	query(condition='geocodigo != "F351" ')
+	query(condition='geocodigoFundar != "F351" ')
 )
 #  PIPELINE_END
 
@@ -48,105 +27,64 @@ replace_value(col='iso3', curr_value='CHA', new_value='CHI'),
 #  start()
 #  RangeIndex: 224 entries, 0 to 223
 #  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              224 non-null    object 
-#   1   iso3_desc_fundar  224 non-null    object 
-#   2   anio              224 non-null    int64  
-#   3   valor             224 non-null    float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  224 non-null    object 
+#   1   geonombreFundar  224 non-null    object 
+#   2   anio             224 non-null    int64  
+#   3   valor            224 non-null    float64
 #  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | ABW    | Aruba              |   2021 |       2 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='CHA', new_value='CHI')
-#  RangeIndex: 224 entries, 0 to 223
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              224 non-null    object 
-#   1   iso3_desc_fundar  224 non-null    object 
-#   2   anio              224 non-null    int64  
-#   3   valor             224 non-null    float64
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | ABW    | Aruba              |   2021 |       2 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | ABW               | Aruba             |   2021 |       2 |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio == anio.max()')
 #  Index: 224 entries, 0 to 223
 #  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              224 non-null    object 
-#   1   iso3_desc_fundar  224 non-null    object 
-#   2   anio              224 non-null    int64  
-#   3   valor             224 non-null    float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  224 non-null    object 
+#   1   geonombreFundar  224 non-null    object 
+#   2   anio             224 non-null    int64  
+#   3   valor            224 non-null    float64
 #  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | ABW    | Aruba              |   2021 |       2 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3_desc_fundar', axis=1)
-#  Index: 224 entries, 0 to 223
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    224 non-null    object 
-#   1   anio    224 non-null    int64  
-#   2   valor   224 non-null    float64
-#  
-#  |    | iso3   |   anio |   valor |
-#  |---:|:-------|-------:|--------:|
-#  |  0 | ABW    |   2021 |       2 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='anio', axis=1)
-#  Index: 224 entries, 0 to 223
-#  Data columns (total 2 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    224 non-null    object 
-#   1   valor   224 non-null    float64
-#  
-#  |    | iso3   |   valor |
-#  |---:|:-------|--------:|
-#  |  0 | ABW    |       2 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | ABW               | Aruba             |   2021 |       2 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'iso3': 'geocodigo'})
 #  Index: 224 entries, 0 to 223
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  224 non-null    object 
-#   1   valor      224 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  224 non-null    object 
+#   1   geonombreFundar  224 non-null    object 
+#   2   anio             224 non-null    int64  
+#   3   valor            224 non-null    float64
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | ABW         |       2 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | ABW               | Aruba             |   2021 |       2 |
 #  
 #  ------------------------------
 #  
-#  query(condition='geocodigo != "F351" ')
+#  query(condition='geocodigoFundar != "F351" ')
 #  Index: 223 entries, 0 to 223
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  223 non-null    object 
-#   1   valor      223 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  223 non-null    object 
+#   1   geonombreFundar  223 non-null    object 
+#   2   anio             223 non-null    int64  
+#   3   valor            223 non-null    float64
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | ABW         |       2 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | ABW               | Aruba             |   2021 |       2 |
 #  
 #  ------------------------------
 #  
