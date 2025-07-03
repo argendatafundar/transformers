5be32cb04c240da260c1ff89ff30fc1294a95c34
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-03 17:11:46 -0300

    Updated CIETEC/CIETEC_g03_transformer.py

--- CIETEC/CIETEC_g03_transformer.py
+++ CIETEC/CIETEC_g03_transformer.py
@@ -38,11 +38,11 @@ def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[s
 
 #  PIPELINE_START
 pipeline = chain(
-	ordenar_dos_columnas(col1='geonombreFundar', order1=['Israel', 'Irlanda', 'Taiwán', 'Corea del Sur', 'Japón', 'Estados Unidos', 'China', 'Islandia', 'Suecia', 'Bélgica', 'Países miembros de OCDE', 'Hungría', 'Reino Unido', 'Países Bajos', 'Austria', 'Eslovenia', 'Suiza', 'Alemania', 'Finlandia', 'Unión Europea (27 países)', 'Francia', 'Turquía', 'Chequia', 'Polonia', 'Bulgaria', 'Singapur', 'Portugal', 'Rumania', 'Dinamarca', 'Canadá', 'Nueva Zelanda', 'Italia', 'Estonia', 'Noruega', 'Rusia', 'España', 'Eslovaquia', 'Croacia', 'Australia', 'Grecia', 'Uruguay', 'Luxemburgo', 'Colombia', 'Argentina', 'Chile', 'Lituania', 'Iberoamérica', 'Letonia', 'Sudáfrica', 'América Latina y el Caribe', 'Perú', 'Costa Rica', 'México', 'Panamá', 'Ecuador', 'Guatemala', 'Honduras', 'Paraguay', 'Puerto Rico', 'Trinidad y Tobago'], col2='sector', order2=['Empresas (Públicas y Privadas)', 'Educación Superior', 'Gobierno', 'Org. priv. sin fines de lucro']),
-	query(condition="geocodigoFundar not in ['ECU','HND','PRI']"),
+	query(condition="geonombreFundar not in ['Ecuador','Honduras','Puerto Rico']"),
 	drop_col(col=['ultimo_anio_disponible', 'fuente', 'geocodigoFundar'], axis=1),
 	completar_combinaciones(keys=['geonombreFundar', 'sector']),
-	fill_na(col='valor', fill=0)
+	fill_na(col='valor', fill=0),
+	ordenar_dos_columnas(col1='geonombreFundar', order1=['Israel', 'Irlanda', 'Taiwán', 'Corea del Sur', 'Japón', 'Estados Unidos', 'China', 'Islandia', 'Suecia', 'Bélgica', 'Países miembros de OCDE', 'Hungría', 'Reino Unido', 'Países Bajos', 'Austria', 'Eslovenia', 'Suiza', 'Alemania', 'Finlandia', 'Unión Europea (27 países)', 'Francia', 'Turquía', 'Chequia', 'Polonia', 'Bulgaria', 'Singapur', 'Portugal', 'Rumania', 'Dinamarca', 'Canadá', 'Nueva Zelanda', 'Italia', 'Estonia', 'Noruega', 'Rusia', 'España', 'Eslovaquia', 'Croacia', 'Australia', 'Grecia', 'Uruguay', 'Luxemburgo', 'Colombia', 'Argentina', 'Chile', 'Lituania', 'Iberoamérica', 'Letonia', 'Sudáfrica', 'América Latina y el Caribe', 'Perú', 'Costa Rica', 'México', 'Panamá', 'Guatemala', 'Paraguay', 'Trinidad y Tobago'], col2='sector', order2=['Empresas (Públicas y Privadas)', 'Educación Superior', 'Gobierno', 'Org. priv. sin fines de lucro'])
 )
 #  PIPELINE_END
 
@@ -65,54 +65,36 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  ordenar_dos_columnas(col1='geonombreFundar', order1=['Israel', 'Irlanda', 'Taiwán', 'Corea del Sur', 'Japón', 'Estados Unidos', 'China', 'Islandia', 'Suecia', 'Bélgica', 'Países miembros de OCDE', 'Hungría', 'Reino Unido', 'Países Bajos', 'Austria', 'Eslovenia', 'Suiza', 'Alemania', 'Finlandia', 'Unión Europea (27 países)', 'Francia', 'Turquía', 'Chequia', 'Polonia', 'Bulgaria', 'Singapur', 'Portugal', 'Rumania', 'Dinamarca', 'Canadá', 'Nueva Zelanda', 'Italia', 'Estonia', 'Noruega', 'Rusia', 'España', 'Eslovaquia', 'Croacia', 'Australia', 'Grecia', 'Uruguay', 'Luxemburgo', 'Colombia', 'Argentina', 'Chile', 'Lituania', 'Iberoamérica', 'Letonia', 'Sudáfrica', 'América Latina y el Caribe', 'Perú', 'Costa Rica', 'México', 'Panamá', 'Ecuador', 'Guatemala', 'Honduras', 'Paraguay', 'Puerto Rico', 'Trinidad y Tobago'], col2='sector', order2=['Empresas (Públicas y Privadas)', 'Educación Superior', 'Gobierno', 'Org. priv. sin fines de lucro'])
-#  Index: 230 entries, 159 to 225
+#  query(condition="geonombreFundar not in ['Ecuador','Honduras','Puerto Rico']")
+#  Index: 218 entries, 0 to 229
 #  Data columns (total 6 columns):
-#   #   Column                  Non-Null Count  Dtype   
-#  ---  ------                  --------------  -----   
-#   0   geocodigoFundar         230 non-null    object  
-#   1   geonombreFundar         230 non-null    category
-#   2   ultimo_anio_disponible  230 non-null    int64   
-#   3   sector                  230 non-null    category
-#   4   valor                   218 non-null    float64 
-#   5   fuente                  230 non-null    object  
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   geocodigoFundar         218 non-null    object 
+#   1   geonombreFundar         218 non-null    object 
+#   2   ultimo_anio_disponible  218 non-null    int64  
+#   3   sector                  218 non-null    object 
+#   4   valor                   218 non-null    float64
+#   5   fuente                  218 non-null    object 
 #  
-#  |     | geocodigoFundar   | geonombreFundar   |   ultimo_anio_disponible | sector                         |   valor | fuente   |
-#  |----:|:------------------|:------------------|-------------------------:|:-------------------------------|--------:|:---------|
-#  | 159 | ISR               | Israel            |                     2023 | Empresas (Públicas y Privadas) | 93.0072 | OECD     |
-#  
-#  ------------------------------
-#  
-#  query(condition="geocodigoFundar not in ['ECU','HND','PRI']")
-#  Index: 218 entries, 159 to 225
-#  Data columns (total 6 columns):
-#   #   Column                  Non-Null Count  Dtype   
-#  ---  ------                  --------------  -----   
-#   0   geocodigoFundar         218 non-null    object  
-#   1   geonombreFundar         218 non-null    category
-#   2   ultimo_anio_disponible  218 non-null    int64   
-#   3   sector                  218 non-null    category
-#   4   valor                   218 non-null    float64 
-#   5   fuente                  218 non-null    object  
-#  
-#  |     | geocodigoFundar   | geonombreFundar   |   ultimo_anio_disponible | sector                         |   valor | fuente   |
-#  |----:|:------------------|:------------------|-------------------------:|:-------------------------------|--------:|:---------|
-#  | 159 | ISR               | Israel            |                     2023 | Empresas (Públicas y Privadas) | 93.0072 | OECD     |
+#  |    | geocodigoFundar   | geonombreFundar   |   ultimo_anio_disponible | sector   |   valor | fuente   |
+#  |---:|:------------------|:------------------|-------------------------:|:---------|--------:|:---------|
+#  |  0 | BGR               | Bulgaria          |                     2023 | Gobierno | 28.7441 | OECD     |
 #  
 #  ------------------------------
 #  
 #  drop_col(col=['ultimo_anio_disponible', 'fuente', 'geocodigoFundar'], axis=1)
-#  Index: 218 entries, 159 to 225
+#  Index: 218 entries, 0 to 229
 #  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype   
-#  ---  ------           --------------  -----   
-#   0   geonombreFundar  218 non-null    category
-#   1   sector           218 non-null    category
-#   2   valor            218 non-null    float64 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  218 non-null    object 
+#   1   sector           218 non-null    object 
+#   2   valor            218 non-null    float64
 #  
-#  |     | geonombreFundar   | sector                         |   valor |
-#  |----:|:------------------|:-------------------------------|--------:|
-#  | 159 | Israel            | Empresas (Públicas y Privadas) | 93.0072 |
+#  |    | geonombreFundar   | sector   |   valor |
+#  |---:|:------------------|:---------|--------:|
+#  |  0 | Bulgaria          | Gobierno | 28.7441 |
 #  
 #  ------------------------------
 #  
@@ -125,9 +107,9 @@ pipeline = chain(
 #   1   sector           228 non-null    category
 #   2   valor            228 non-null    float64 
 #  
-#  |    | geonombreFundar   | sector                         |   valor |
-#  |---:|:------------------|:-------------------------------|--------:|
-#  |  0 | Israel            | Empresas (Públicas y Privadas) | 93.0072 |
+#  |    | geonombreFundar   | sector   |   valor |
+#  |---:|:------------------|:---------|--------:|
+#  |  0 | Bulgaria          | Gobierno | 28.7441 |
 #  
 #  ------------------------------
 #  
@@ -140,9 +122,24 @@ pipeline = chain(
 #   1   sector           228 non-null    category
 #   2   valor            228 non-null    float64 
 #  
+#  |    | geonombreFundar   | sector   |   valor |
+#  |---:|:------------------|:---------|--------:|
+#  |  0 | Bulgaria          | Gobierno | 28.7441 |
+#  
+#  ------------------------------
+#  
+#  ordenar_dos_columnas(col1='geonombreFundar', order1=['Israel', 'Irlanda', 'Taiwán', 'Corea del Sur', 'Japón', 'Estados Unidos', 'China', 'Islandia', 'Suecia', 'Bélgica', 'Países miembros de OCDE', 'Hungría', 'Reino Unido', 'Países Bajos', 'Austria', 'Eslovenia', 'Suiza', 'Alemania', 'Finlandia', 'Unión Europea (27 países)', 'Francia', 'Turquía', 'Chequia', 'Polonia', 'Bulgaria', 'Singapur', 'Portugal', 'Rumania', 'Dinamarca', 'Canadá', 'Nueva Zelanda', 'Italia', 'Estonia', 'Noruega', 'Rusia', 'España', 'Eslovaquia', 'Croacia', 'Australia', 'Grecia', 'Uruguay', 'Luxemburgo', 'Colombia', 'Argentina', 'Chile', 'Lituania', 'Iberoamérica', 'Letonia', 'Sudáfrica', 'América Latina y el Caribe', 'Perú', 'Costa Rica', 'México', 'Panamá', 'Guatemala', 'Paraguay', 'Trinidad y Tobago'], col2='sector', order2=['Empresas (Públicas y Privadas)', 'Educación Superior', 'Gobierno', 'Org. priv. sin fines de lucro'])
+#  Index: 228 entries, 63 to 221
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype   
+#  ---  ------           --------------  -----   
+#   0   geonombreFundar  228 non-null    category
+#   1   sector           228 non-null    category
+#   2   valor            228 non-null    float64 
+#  
 #  |    | geonombreFundar   | sector                         |   valor |
 #  |---:|:------------------|:-------------------------------|--------:|
-#  |  0 | Israel            | Empresas (Públicas y Privadas) | 93.0072 |
+#  | 63 | Israel            | Empresas (Públicas y Privadas) | 93.0072 |
 #  
 #  ------------------------------
 #  
