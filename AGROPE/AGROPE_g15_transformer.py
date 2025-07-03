701f7c894675f777913c401e6b1df83c7c63ac1f
LMCappelletti <lcappelletti@fund.ar>
2025-07-03 18:45:28 -0300

    Updated AGROPE/AGROPE_g15_transformer.py

--- AGROPE/AGROPE_g15_transformer.py
+++ AGROPE/AGROPE_g15_transformer.py
@@ -4,33 +4,9 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
 
 @transformer.convert
 def drop_na(df:DataFrame, col:str):
@@ -41,32 +17,21 @@ def drop_na(df:DataFrame, col:str):
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
-    
+
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-replace_value(col='iso3', curr_value='F15', new_value='BLX'),
-	replace_value(col='iso3', curr_value='F228', new_value='SVU'),
-	replace_value(col='iso3', curr_value='F248', new_value='SER'),
-	replace_value(col='iso3', curr_value='F51', new_value='CSK'),
-	rename_cols(map={'iso3': 'geocodigo'}),
-	drop_col(col='iso3_desc_fundar', axis=1),
 	drop_na(col=['valor']),
-	sort_values(how='ascending', by=['anio', 'geocodigo']),
-	query(condition="geocodigo != 'F351'"),
+	sort_values(how='ascending', by=['anio', 'geocodigoFundar']),
+	query(condition="geocodigoFundar != 'F351'"),
 	multiplicar_por_escalar(col='valor', k=1e-06)
 )
 #  PIPELINE_END
@@ -75,171 +40,80 @@ replace_value(col='iso3', curr_value='F15', new_value='BLX'),
 #  start()
 #  RangeIndex: 6138 entries, 0 to 6137
 #  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              6138 non-null   object 
-#   1   iso3_desc_fundar  6138 non-null   object 
-#   2   anio              6138 non-null   int64  
-#   3   valor             6138 non-null   float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  6138 non-null   object 
+#   1   geonombreFundar  6138 non-null   object 
+#   2   anio             6138 non-null   int64  
+#   3   valor            6138 non-null   float64
 #  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | AGO    | Angola             |   1961 |       0 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F15', new_value='BLX')
-#  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              6138 non-null   object 
-#   1   iso3_desc_fundar  6138 non-null   object 
-#   2   anio              6138 non-null   int64  
-#   3   valor             6138 non-null   float64
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | AGO    | Angola             |   1961 |       0 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F228', new_value='SVU')
-#  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              6138 non-null   object 
-#   1   iso3_desc_fundar  6138 non-null   object 
-#   2   anio              6138 non-null   int64  
-#   3   valor             6138 non-null   float64
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | AGO    | Angola             |   1961 |       0 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F248', new_value='SER')
-#  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              6138 non-null   object 
-#   1   iso3_desc_fundar  6138 non-null   object 
-#   2   anio              6138 non-null   int64  
-#   3   valor             6138 non-null   float64
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | AGO    | Angola             |   1961 |       0 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='F51', new_value='CSK')
-#  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              6138 non-null   object 
-#   1   iso3_desc_fundar  6138 non-null   object 
-#   2   anio              6138 non-null   int64  
-#   3   valor             6138 non-null   float64
-#  
-#  |    | iso3   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:-------|:-------------------|-------:|--------:|
-#  |  0 | AGO    | Angola             |   1961 |       0 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo'})
-#  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   geocodigo         6138 non-null   object 
-#   1   iso3_desc_fundar  6138 non-null   object 
-#   2   anio              6138 non-null   int64  
-#   3   valor             6138 non-null   float64
-#  
-#  |    | geocodigo   | iso3_desc_fundar   |   anio |   valor |
-#  |---:|:------------|:-------------------|-------:|--------:|
-#  |  0 | AGO         | Angola             |   1961 |       0 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3_desc_fundar', axis=1)
-#  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  6138 non-null   object 
-#   1   anio       6138 non-null   int64  
-#   2   valor      6138 non-null   float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AGO         |   1961 |       0 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AGO               | Angola            |   1961 |       0 |
 #  
 #  ------------------------------
 #  
 #  drop_na(col=['valor'])
 #  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  6138 non-null   object 
-#   1   anio       6138 non-null   int64  
-#   2   valor      6138 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  6138 non-null   object 
+#   1   geonombreFundar  6138 non-null   object 
+#   2   anio             6138 non-null   int64  
+#   3   valor            6138 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AGO         |   1961 |       0 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AGO               | Angola            |   1961 |       0 |
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'geocodigo'])
+#  sort_values(how='ascending', by=['anio', 'geocodigoFundar'])
 #  RangeIndex: 6138 entries, 0 to 6137
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  6138 non-null   object 
-#   1   anio       6138 non-null   int64  
-#   2   valor      6138 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  6138 non-null   object 
+#   1   geonombreFundar  6138 non-null   object 
+#   2   anio             6138 non-null   int64  
+#   3   valor            6138 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AGO         |   1961 |       0 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AGO               | Angola            |   1961 |       0 |
 #  
 #  ------------------------------
 #  
-#  query(condition="geocodigo != 'F351'")
+#  query(condition="geocodigoFundar != 'F351'")
 #  Index: 6076 entries, 0 to 6137
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  6076 non-null   object 
-#   1   anio       6076 non-null   int64  
-#   2   valor      6076 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  6076 non-null   object 
+#   1   geonombreFundar  6076 non-null   object 
+#   2   anio             6076 non-null   int64  
+#   3   valor            6076 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AGO         |   1961 |       0 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AGO               | Angola            |   1961 |       0 |
 #  
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=1e-06)
 #  Index: 6076 entries, 0 to 6137
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  6076 non-null   object 
-#   1   anio       6076 non-null   int64  
-#   2   valor      6076 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  6076 non-null   object 
+#   1   geonombreFundar  6076 non-null   object 
+#   2   anio             6076 non-null   int64  
+#   3   valor            6076 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | AGO         |   1961 |       0 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AGO               | Angola            |   1961 |       0 |
 #  
 #  ------------------------------
 #  
