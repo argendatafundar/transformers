c52efb57c8339791aef984c8b8e7494f9535f98c
LMCappelletti <lcappelletti@fund.ar>
2025-07-04 12:00:45 -0300

    Updated AGROPE/AGROPE_g19_transformer.py

--- AGROPE/AGROPE_g19_transformer.py
+++ AGROPE/AGROPE_g19_transformer.py
@@ -9,35 +9,6 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     df = df.replace({col: curr_value}, new_value)
     return df
@@ -46,8 +17,7 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'tipo_carne': 'indicador'}),
-	drop_col(col='iso3_desc_fundar', axis=1),
+	rename_cols(map={'tipo_carne': 'indicador'}),
 	replace_value(col='indicador', curr_value='pescado_mariscos', new_value='Pescados y mariscos'),
 	replace_value(col='indicador', curr_value='otras_carnes', new_value='Otras carnes'),
 	replace_value(col='indicador', curr_value='aviar', new_value='Aviar'),
@@ -61,137 +31,128 @@ rename_cols(map={'iso3': 'geocodigo', 'tipo_carne': 'indicador'}),
 #  start()
 #  RangeIndex: 1110 entries, 0 to 1109
 #  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   iso3              1110 non-null   object 
-#   1   iso3_desc_fundar  1110 non-null   object 
-#   2   tipo_carne        1110 non-null   object 
-#   3   valor             1107 non-null   float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   tipo_carne       1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | iso3   | iso3_desc_fundar   | tipo_carne       |    valor |
-#  |---:|:-------|:-------------------|:-----------------|---------:|
-#  |  0 | AFG    | Afganistán         | pescado_mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | tipo_carne       |    valor |
+#  |---:|:------------------|:------------------|:-----------------|---------:|
+#  |  0 | AFG               | Afganistán        | pescado_mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'tipo_carne': 'indicador'})
+#  rename_cols(map={'tipo_carne': 'indicador'})
 #  RangeIndex: 1110 entries, 0 to 1109
 #  Data columns (total 4 columns):
-#   #   Column            Non-Null Count  Dtype  
-#  ---  ------            --------------  -----  
-#   0   geocodigo         1110 non-null   object 
-#   1   iso3_desc_fundar  1110 non-null   object 
-#   2   indicador         1110 non-null   object 
-#   3   valor             1107 non-null   float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | iso3_desc_fundar   | indicador        |    valor |
-#  |---:|:------------|:-------------------|:-----------------|---------:|
-#  |  0 | AFG         | Afganistán         | pescado_mariscos | 0.359595 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3_desc_fundar', axis=1)
-#  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
-#  
-#  |    | geocodigo   | indicador        |    valor |
-#  |---:|:------------|:-----------------|---------:|
-#  |  0 | AFG         | pescado_mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador        |    valor |
+#  |---:|:------------------|:------------------|:-----------------|---------:|
+#  |  0 | AFG               | Afganistán        | pescado_mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='pescado_mariscos', new_value='Pescados y mariscos')
 #  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | indicador           |    valor |
-#  |---:|:------------|:--------------------|---------:|
-#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador           |    valor |
+#  |---:|:------------------|:------------------|:--------------------|---------:|
+#  |  0 | AFG               | Afganistán        | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='otras_carnes', new_value='Otras carnes')
 #  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | indicador           |    valor |
-#  |---:|:------------|:--------------------|---------:|
-#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador           |    valor |
+#  |---:|:------------------|:------------------|:--------------------|---------:|
+#  |  0 | AFG               | Afganistán        | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='aviar', new_value='Aviar')
 #  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | indicador           |    valor |
-#  |---:|:------------|:--------------------|---------:|
-#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador           |    valor |
+#  |---:|:------------------|:------------------|:--------------------|---------:|
+#  |  0 | AFG               | Afganistán        | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='caprina_ovina', new_value='Caprina y ovina')
 #  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | indicador           |    valor |
-#  |---:|:------------|:--------------------|---------:|
-#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador           |    valor |
+#  |---:|:------------------|:------------------|:--------------------|---------:|
+#  |  0 | AFG               | Afganistán        | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='porcina', new_value='Porcina')
 #  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | indicador           |    valor |
-#  |---:|:------------|:--------------------|---------:|
-#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador           |    valor |
+#  |---:|:------------------|:------------------|:--------------------|---------:|
+#  |  0 | AFG               | Afganistán        | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='vacuna', new_value='Vacuna')
 #  RangeIndex: 1110 entries, 0 to 1109
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1110 non-null   object 
-#   1   indicador  1110 non-null   object 
-#   2   valor      1107 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  1110 non-null   object 
+#   1   geonombreFundar  1110 non-null   object 
+#   2   indicador        1110 non-null   object 
+#   3   valor            1107 non-null   float64
 #  
-#  |    | geocodigo   | indicador           |    valor |
-#  |---:|:------------|:--------------------|---------:|
-#  |  0 | AFG         | Pescados y mariscos | 0.359595 |
+#  |    | geocodigoFundar   | geonombreFundar   | indicador           |    valor |
+#  |---:|:------------------|:------------------|:--------------------|---------:|
+#  |  0 | AFG               | Afganistán        | Pescados y mariscos | 0.359595 |
 #  
 #  ------------------------------
 #  
