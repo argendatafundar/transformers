8e582d0e445c6819dd8a8bb73255a004e018df26
LMCappelletti <lcappelletti@fund.ar>
2025-07-07 11:40:51 -0300

    Updated AGROPE/AGROPE_g25_transformer.py

--- AGROPE/AGROPE_g25_transformer.py
+++ AGROPE/AGROPE_g25_transformer.py
@@ -4,15 +4,16 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
+def ordenar_una_columna(df, col1:str, order1:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    return df.sort_values(by=[col1])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	rename_cols(map={'region': 'indicador'})
+	ordenar_una_columna(col1='geonombreFundar', order1=['Cuyo', 'NEA', 'NOA', 'Pampeana', 'Patagonia'])
 )
 #  PIPELINE_END
 
@@ -33,15 +34,15 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'region': 'indicador'})
-#  RangeIndex: 80 entries, 0 to 79
+#  ordenar_una_columna(col1='geonombreFundar', order1=['Cuyo', 'NEA', 'NOA', 'Pampeana', 'Patagonia'])
+#  Index: 80 entries, 0 to 79
 #  Data columns (total 4 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geocodigoFundar  80 non-null     object 
-#   1   geonombreFundar  80 non-null     object 
-#   2   anio             80 non-null     int64  
-#   3   valor            80 non-null     float64
+#   #   Column           Non-Null Count  Dtype   
+#  ---  ------           --------------  -----   
+#   0   geocodigoFundar  80 non-null     object  
+#   1   geonombreFundar  80 non-null     category
+#   2   anio             80 non-null     int64   
+#   3   valor            80 non-null     float64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio |       valor |
 #  |---:|:------------------|:------------------|-------:|------------:|
