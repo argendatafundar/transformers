eccccf1a20a3922d60458722cec2c6229cac1661
LMCappelletti <lcappelletti@fund.ar>
2025-07-07 12:48:10 -0300

    Updated AGROPE/AGROPE_g34_transformer.py

--- AGROPE/AGROPE_g34_transformer.py
+++ AGROPE/AGROPE_g34_transformer.py
@@ -9,27 +9,27 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
-    
+
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
 def drop_na(df:DataFrame, col:str):
     df = df.dropna(subset=col, axis=0)
     return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'cadena': 'categoria'}),
+	rename_cols(map={'cadena': 'categoria'}),
 	sort_values(how='ascending', by=['anio']),
 	drop_na(col='valor'),
 	multiplicar_por_escalar(col='valor', k=1e-06)
