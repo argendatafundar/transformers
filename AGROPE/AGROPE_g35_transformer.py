8590a8c0912198e9f427e0e47a73cc072eee1225
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-08 13:59:05 -0300

    Created AGROPE/AGROPE_g35_transformer.py

---
+++ AGROPE/AGROPE_g35_transformer.py
@@ -0,0 +1,50 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def identity(df: DataFrame) -> DataFrame:
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	identity()
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 720 entries, 0 to 719
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  720 non-null    object 
+#   1   geonombreFundar  720 non-null    object 
+#   2   cadena           720 non-null    object 
+#   3   share            720 non-null    float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   | cadena         |   share |
+#  |---:|:------------------|:------------------|:---------------|--------:|
+#  |  0 | AR-B              | Buenos Aires      | Algodón textil |   36.64 |
+#  
+#  ------------------------------
+#  
+#  identity()
+#  RangeIndex: 720 entries, 0 to 719
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  720 non-null    object 
+#   1   geonombreFundar  720 non-null    object 
+#   2   cadena           720 non-null    object 
+#   3   share            720 non-null    float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   | cadena         |   share |
+#  |---:|:------------------|:------------------|:---------------|--------:|
+#  |  0 | AR-B              | Buenos Aires      | Algodón textil |   36.64 |
+#  
+#  ------------------------------
+#  
