cbcb2d291858f4f4d492cd4652d8e475b74dbdf3
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-08 12:34:38 -0300

    Updated AGROPE/AGROPE_g353_transformer.py

--- AGROPE/AGROPE_g353_transformer.py
+++ AGROPE/AGROPE_g353_transformer.py
@@ -7,18 +7,12 @@ from data_transformers import chain, transformer
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='cadena', curr_value='Sojera', new_value='Soja'),
-	query(condition="cadena == 'Soja'")
+	query(condition="cadena == 'Sojera'")
 )
 #  PIPELINE_END
 
@@ -39,23 +33,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='cadena', curr_value='Sojera', new_value='Soja')
-#  RangeIndex: 720 entries, 0 to 719
-#  Data columns (total 4 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geocodigoFundar  720 non-null    object 
-#   1   geonombreFundar  720 non-null    object 
-#   2   cadena           720 non-null    object 
-#   3   share            720 non-null    float64
-#  
-#  |    | geocodigoFundar   | geonombreFundar   | cadena         |   share |
-#  |---:|:------------------|:------------------|:---------------|--------:|
-#  |  0 | AR-B              | Buenos Aires      | Algodón textil |   36.64 |
-#  
-#  ------------------------------
-#  
-#  query(condition="cadena == 'Soja'")
+#  query(condition="cadena == 'Sojera'")
 #  Index: 24 entries, 15 to 705
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
@@ -67,7 +45,7 @@ pipeline = chain(
 #  
 #  |    | geocodigoFundar   | geonombreFundar   | cadena   |   share |
 #  |---:|:------------------|:------------------|:---------|--------:|
-#  | 15 | AR-B              | Buenos Aires      | Soja     |   16.35 |
+#  | 15 | AR-B              | Buenos Aires      | Sojera   |   16.35 |
 #  
 #  ------------------------------
 #  
