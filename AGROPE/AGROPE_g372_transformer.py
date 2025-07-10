9c2d8ec6d2056a0ced11bed264a91bed7254abad
LMCappelletti <lcappelletti@fund.ar>
2025-07-10 16:14:01 -0300

    Updated AGROPE/AGROPE_g372_transformer.py

--- AGROPE/AGROPE_g372_transformer.py
+++ AGROPE/AGROPE_g372_transformer.py
@@ -6,12 +6,18 @@ from data_transformers import chain, transformer
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_col(col=['productos_primarios', 'moa'], axis=1)
+	drop_col(col=['productos_primarios', 'moa'], axis=1),
+	rename_cols(map={'participacion_expo_totales': 'valor'})
 )
 #  PIPELINE_END
 
@@ -43,6 +49,20 @@ pipeline = chain(
 #  |    |   anio |   participacion_expo_totales |
 #  |---:|-------:|-----------------------------:|
 #  |  0 |   2009 |                     0.526943 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'participacion_expo_totales': 'valor'})
+#  RangeIndex: 14 entries, 0 to 13
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    14 non-null     int64  
+#   1   valor   14 non-null     float64
+#  
+#  |    |   anio |    valor |
+#  |---:|-------:|---------:|
+#  |  0 |   2009 | 0.526943 |
 #  
 #  ------------------------------
 #  
