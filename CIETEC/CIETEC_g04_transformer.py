771b11ef93ff7c5071d6c2979b808c14c1e91fed
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-11 16:13:33 -0300

    Updated CIETEC/CIETEC_g04_transformer.py

--- CIETEC/CIETEC_g04_transformer.py
+++ CIETEC/CIETEC_g04_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def invertir_valores(df: DataFrame, col:str) -> DataFrame:
+    df[col] = df[col].apply(lambda x: 1/x if x != 0 else None)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	invertir_valores(col='ranking')
 )
 #  PIPELINE_END
 
@@ -34,17 +35,17 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  invertir_valores(col='ranking')
 #  RangeIndex: 449 entries, 0 to 448
 #  Data columns (total 6 columns):
-#   #   Column           Non-Null Count  Dtype 
-#  ---  ------           --------------  ----- 
-#   0   geocodigoFundar  449 non-null    object
-#   1   geonombreFundar  449 non-null    object
-#   2   anio             449 non-null    int64 
-#   3   idp              449 non-null    int64 
-#   4   institucion      449 non-null    object
-#   5   ranking          449 non-null    int64 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  449 non-null    object 
+#   1   geonombreFundar  449 non-null    object 
+#   2   anio             449 non-null    int64  
+#   3   idp              449 non-null    int64  
+#   4   institucion      449 non-null    object 
+#   5   ranking          449 non-null    float64
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio |   idp | institucion                                                |   ranking |
 #  |---:|:------------------|:------------------|-------:|------:|:-----------------------------------------------------------|----------:|
