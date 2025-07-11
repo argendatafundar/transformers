a35e835ebf236038ca55e1ef17a7908ea4760637
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-11 18:17:42 -0300

    Updated CIETEC/CIETEC_g25_transformer.py

--- CIETEC/CIETEC_g25_transformer.py
+++ CIETEC/CIETEC_g25_transformer.py
@@ -4,17 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
-    import pandas as pd
-    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
-    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
-    return df.sort_values(by=[col1,col2])
+def identity(df: pl.DataFrame) -> pl.DataFrame:
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	ordenar_dos_columnas(col1='tipo', order1=['Nacional (total)', 'Universidades públicas', 'Universidades privadas', 'Empresas', 'Entidades sin fines de lucro\t'], col2='destino', order2=['Erogaciones en personal', 'Otras erogaciones corrientes', 'Erogaciones en inmuebles y construcciones', 'Erogaciones en equipamiento y rodados', 'Otras erogaciones de capital'])
+	identity()
 )
 #  PIPELINE_END
 
@@ -37,21 +34,21 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  ordenar_dos_columnas(col1='tipo', order1=['Nacional (total)', 'Universidades públicas', 'Universidades privadas', 'Empresas', 'Entidades sin fines de lucro\t'], col2='destino', order2=['Erogaciones en personal', 'Otras erogaciones corrientes', 'Erogaciones en inmuebles y construcciones', 'Erogaciones en equipamiento y rodados', 'Otras erogaciones de capital'])
-#  Index: 30 entries, 15 to 14
+#  identity()
+#  RangeIndex: 30 entries, 0 to 29
 #  Data columns (total 6 columns):
-#   #   Column         Non-Null Count  Dtype   
-#  ---  ------         --------------  -----   
-#   0   tipo           20 non-null     category
-#   1   anio           30 non-null     int64   
-#   2   destino        30 non-null     category
-#   3   inversion_i_d  30 non-null     float64 
-#   4   unidad_medida  30 non-null     object  
-#   5   share          30 non-null     float64 
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   tipo           30 non-null     object 
+#   1   anio           30 non-null     int64  
+#   2   destino        30 non-null     object 
+#   3   inversion_i_d  30 non-null     float64
+#   4   unidad_medida  30 non-null     object 
+#   5   share          30 non-null     float64
 #  
-#  |    | tipo             |   anio | destino                 |   inversion_i_d | unidad_medida                |   share |
-#  |---:|:-----------------|-------:|:------------------------|----------------:|:-----------------------------|--------:|
-#  | 15 | Nacional (total) |   2023 | Erogaciones en personal |          766926 | millones de pesos corrientes | 66.6566 |
+#  |    | tipo     |   anio | destino                 |   inversion_i_d | unidad_medida                |   share |
+#  |---:|:---------|-------:|:------------------------|----------------:|:-----------------------------|--------:|
+#  |  0 | Empresas |   2023 | Erogaciones en personal |          255384 | millones de pesos corrientes | 52.7628 |
 #  
 #  ------------------------------
 #  
