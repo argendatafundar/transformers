73ce57b94f2a35bb09b57555fcafe8cd0d0710ba
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-16 13:00:37 -0300

    Updated CIETEC/CIETEC_g07_transformer.py

--- CIETEC/CIETEC_g07_transformer.py
+++ CIETEC/CIETEC_g07_transformer.py
@@ -4,14 +4,17 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
-    return df
+def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
+    return df.sort_values(by=[col1,col2])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	ordenar_dos_columnas(col1='categoria', order1=['Asistente', 'Adjunto', 'Independiente', 'Principal', 'Superior'], col2='genero', order2=['Mujeres', 'Varones'])
 )
 #  PIPELINE_END
 
@@ -32,19 +35,19 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 10 entries, 0 to 9
+#  ordenar_dos_columnas(col1='categoria', order1=['Asistente', 'Adjunto', 'Independiente', 'Principal', 'Superior'], col2='genero', order2=['Mujeres', 'Varones'])
+#  Index: 10 entries, 2 to 9
 #  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  10 non-null     object 
-#   1   genero     10 non-null     object 
-#   2   cantidad   10 non-null     int64  
-#   3   share      10 non-null     float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  10 non-null     category
+#   1   genero     10 non-null     category
+#   2   cantidad   10 non-null     int64   
+#   3   share      10 non-null     float64 
 #  
 #  |    | categoria   | genero   |   cantidad |   share |
 #  |---:|:------------|:---------|-----------:|--------:|
-#  |  0 | Adjunto     | Mujeres  |       2913 | 58.2367 |
+#  |  2 | Asistente   | Mujeres  |       1805 | 60.3881 |
 #  
 #  ------------------------------
 #  
