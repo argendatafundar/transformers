58a5229c234e26e48589d71486e870e812af8434
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-16 13:37:55 -0300

    Updated CIETEC/CIETEC_g06_transformer.py

--- CIETEC/CIETEC_g06_transformer.py
+++ CIETEC/CIETEC_g06_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
+    return df
+
+@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 #  DEFINITIONS_END
@@ -11,7 +16,8 @@ def drop_col(df: DataFrame, col, axis=1):
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_col(col=['geocodigoFundar', 'ultimo_anio_disponible', 'fuente'], axis=1)
+	drop_col(col=['geocodigoFundar', 'ultimo_anio_disponible', 'fuente'], axis=1),
+	replace_multiple_values(col='geonombreFundar', replacements={'América Latina y el Caribe': 'A. Latina y el Caribe', 'Países miembros de OCDE': 'Miembros de OCDE', 'Unión Europea (27 países)': 'Unión Europea'})
 )
 #  PIPELINE_END
 
@@ -34,6 +40,20 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  drop_col(col=['geocodigoFundar', 'ultimo_anio_disponible', 'fuente'], axis=1)
+#  RangeIndex: 49 entries, 0 to 48
+#  Data columns (total 2 columns):
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   geonombreFundar     49 non-null     object 
+#   1   gasto_investigador  49 non-null     float64
+#  
+#  |    | geonombreFundar   |   gasto_investigador |
+#  |---:|:------------------|---------------------:|
+#  |  0 | Estados Unidos    |              539.315 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='geonombreFundar', replacements={'América Latina y el Caribe': 'A. Latina y el Caribe', 'Países miembros de OCDE': 'Miembros de OCDE', 'Unión Europea (27 países)': 'Unión Europea'})
 #  RangeIndex: 49 entries, 0 to 48
 #  Data columns (total 2 columns):
 #   #   Column              Non-Null Count  Dtype  
