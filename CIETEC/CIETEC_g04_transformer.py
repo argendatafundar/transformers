0a6832d551ffffa25d23a9f289600921f21c6444
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-16 11:42:13 -0300

    Updated CIETEC/CIETEC_g04_transformer.py

--- CIETEC/CIETEC_g04_transformer.py
+++ CIETEC/CIETEC_g04_transformer.py
@@ -4,6 +4,19 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def long_to_wide(df:DataFrame, index:list[str], columns:str, values:str):
+    df = df.pivot(index=index, columns=columns, values=values).reset_index()
+    df.index.name = None
+    df.columns.name = None
+    df.columns = [str(col) for col in df.columns]  # Convertir columnas a str
+    return df  
+
+@transformer.convert
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -12,7 +25,10 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio in [2009, 2016, 2024]')
+	query(condition='anio in [2009, 2024]'),
+	long_to_wide(index=['geocodigoFundar', 'geonombreFundar', 'idp', 'institucion'], columns='anio', values='ranking'),
+	drop_na(col='2009'),
+	drop_na(col='2024')
 )
 #  PIPELINE_END
 
@@ -35,21 +51,75 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='anio in [2009, 2016, 2024]')
-#  Index: 92 entries, 0 to 448
+#  query(condition='anio in [2009, 2024]')
+#  Index: 67 entries, 0 to 448
 #  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype 
 #  ---  ------           --------------  ----- 
-#   0   geocodigoFundar  92 non-null     object
-#   1   geonombreFundar  92 non-null     object
-#   2   anio             92 non-null     int64 
-#   3   idp              92 non-null     int64 
-#   4   institucion      92 non-null     object
-#   5   ranking          92 non-null     int64 
+#   0   geocodigoFundar  67 non-null     object
+#   1   geonombreFundar  67 non-null     object
+#   2   anio             67 non-null     int64 
+#   3   idp              67 non-null     int64 
+#   4   institucion      67 non-null     object
+#   5   ranking          67 non-null     int64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio |   idp | institucion                                                |   ranking |
 #  |---:|:------------------|:------------------|-------:|------:|:-----------------------------------------------------------|----------:|
 #  |  0 | ARG               | Argentina         |   2009 | 25417 | Consejo Nacional de Investigaciones Cientificas y Tecnicas |         1 |
+#  
+#  ------------------------------
+#  
+#  long_to_wide(index=['geocodigoFundar', 'geonombreFundar', 'idp', 'institucion'], columns='anio', values='ranking')
+#  RangeIndex: 48 entries, 0 to 47
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  48 non-null     object 
+#   1   geonombreFundar  48 non-null     object 
+#   2   idp              48 non-null     int64  
+#   3   institucion      48 non-null     object 
+#   4   2009             19 non-null     float64
+#   5   2024             48 non-null     float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   idp | institucion              |   2009 |   2024 |
+#  |---:|:------------------|:------------------|------:|:-------------------------|-------:|-------:|
+#  |  0 | ARG               | Argentina         | 25377 | Centro Atomico Bariloche |    nan |     39 |
+#  
+#  ------------------------------
+#  
+#  drop_na(col='2009')
+#  Index: 19 entries, 1 to 44
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  19 non-null     object 
+#   1   geonombreFundar  19 non-null     object 
+#   2   idp              19 non-null     int64  
+#   3   institucion      19 non-null     object 
+#   4   2009             19 non-null     float64
+#   5   2024             19 non-null     float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   idp | institucion                                |   2009 |   2024 |
+#  |---:|:------------------|:------------------|------:|:-------------------------------------------|-------:|-------:|
+#  |  1 | ARG               | Argentina         | 25408 | Centro Cientifico Tecnologico Bahia Blanca |      8 |     21 |
+#  
+#  ------------------------------
+#  
+#  drop_na(col='2024')
+#  Index: 19 entries, 1 to 44
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  19 non-null     object 
+#   1   geonombreFundar  19 non-null     object 
+#   2   idp              19 non-null     int64  
+#   3   institucion      19 non-null     object 
+#   4   2009             19 non-null     float64
+#   5   2024             19 non-null     float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   idp | institucion                                |   2009 |   2024 |
+#  |---:|:------------------|:------------------|------:|:-------------------------------------------|-------:|-------:|
+#  |  1 | ARG               | Argentina         | 25408 | Centro Cientifico Tecnologico Bahia Blanca |      8 |     21 |
 #  
 #  ------------------------------
 #  
