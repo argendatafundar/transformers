97d588f076d616ba384fb285b15a7272de4deb34
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-17 11:04:28 -0300

    Updated CIETEC/CIETEC_g20_transformer.py

--- CIETEC/CIETEC_g20_transformer.py
+++ CIETEC/CIETEC_g20_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: pl.DataFrame) -> pl.DataFrame:
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	query(condition='anio in [2003, 2013, 2023]')
 )
 #  PIPELINE_END
 
@@ -32,15 +33,15 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 42 entries, 0 to 41
+#  query(condition='anio in [2003, 2013, 2023]')
+#  Index: 6 entries, 0 to 41
 #  Data columns (total 4 columns):
 #   #   Column            Non-Null Count  Dtype  
 #  ---  ------            --------------  -----  
-#   0   anio              42 non-null     int64  
-#   1   funcion           42 non-null     object 
-#   2   personas_fisicas  42 non-null     int64  
-#   3   share_mujeres     42 non-null     float64
+#   0   anio              6 non-null      int64  
+#   1   funcion           6 non-null      object 
+#   2   personas_fisicas  6 non-null      int64  
+#   3   share_mujeres     6 non-null      float64
 #  
 #  |    |   anio | funcion   |   personas_fisicas |   share_mujeres |
 #  |---:|-------:|:----------|-------------------:|----------------:|
