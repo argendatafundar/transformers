7c39256c5e2e2ccac95a8ba05a10a9c1dbef0935
LMCappelletti <lcappelletti@fund.ar>
2025-07-17 10:20:33 -0300

    Updated AGROPE/AGROPE_g03_transformer.py

--- AGROPE/AGROPE_g03_transformer.py
+++ AGROPE/AGROPE_g03_transformer.py
@@ -4,6 +4,24 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def ordenar_categorica(df, col1:str, order1:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    return df.sort_values(by=[col1])
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -12,24 +30,6 @@ def rename_cols(df: DataFrame, map):
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
-
-@transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
-
-@transformer.convert
-def ordenar_categorica(df, col1:str, order1:list[str]):
-    import pandas as pd
-    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
-    return df.sort_values(by=[col1])
 #  DEFINITIONS_END
 
 
@@ -38,8 +38,8 @@ pipeline = chain(
 	rename_cols(map={'cuenta': 'indicador', 'participacion_pbi': 'valor'}),
 	multiplicar_por_escalar(col='valor', k=100),
 	query(condition="indicador != 'Total'"),
-	sort_values(how='ascending', by=['anio']),
-	ordenar_categorica(col1='indicador', order1=['Otros', 'Pecuario', 'Agricultura'])
+	ordenar_categorica(col1='indicador', order1=['Otros', 'Pecuario', 'Agricultura']),
+	sort_values(how='ascending', by=['anio'])
 )
 #  PIPELINE_END
 
@@ -92,21 +92,6 @@ pipeline = chain(
 #  query(condition="indicador != 'Total'")
 #  Index: 60 entries, 0 to 78
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       60 non-null     int64  
-#   1   indicador  60 non-null     object 
-#   2   valor      60 non-null     float64
-#  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   2004 | Agricultura | 4.96514 |
-#  
-#  ------------------------------
-#  
-#  sort_values(how='ascending', by=['anio'])
-#  RangeIndex: 60 entries, 0 to 59
-#  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype   
 #  ---  ------     --------------  -----   
 #   0   anio       60 non-null     int64   
@@ -120,7 +105,7 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  ordenar_categorica(col1='indicador', order1=['Otros', 'Pecuario', 'Agricultura'])
-#  Index: 60 entries, 58 to 0
+#  Index: 60 entries, 77 to 0
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype   
 #  ---  ------     --------------  -----   
@@ -130,7 +115,22 @@ pipeline = chain(
 #  
 #  |    |   anio | indicador   |    valor |
 #  |---:|-------:|:------------|---------:|
-#  | 58 |   2023 | Otros       | 0.476212 |
+#  | 77 |   2023 | Otros       | 0.476212 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 60 entries, 0 to 59
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   anio       60 non-null     int64   
+#   1   indicador  60 non-null     category
+#   2   valor      60 non-null     float64 
+#  
+#  |    |   anio | indicador   |    valor |
+#  |---:|-------:|:------------|---------:|
+#  |  0 |   2004 | Otros       | 0.929939 |
 #  
 #  ------------------------------
 #  
