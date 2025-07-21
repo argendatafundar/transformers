c825ba496afad964e0b13ea4f4ee7590b1c0c822
LMCappelletti <lcappelletti@fund.ar>
2025-07-21 17:13:46 -0300

    Updated MINERI/MINERI_g12_transformer.py

--- MINERI/MINERI_g12_transformer.py
+++ MINERI/MINERI_g12_transformer.py
@@ -4,13 +4,8 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def str_to_title(df: DataFrame, col:str):
-    df[col] = df[col].str.title()
-    return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 
 @transformer.convert
@@ -19,11 +14,22 @@ def sort_values(df: DataFrame, how: str, by: list):
         raise ValueError('how must be either "ascending" or "descending"')
 
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def str_to_title(df: DataFrame, col:str):
+    df[col] = df[col].str.title()
+    return df
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
+	query(condition="geonombreFundar=='Catamarca'"),
 	rename_cols(map={'exportaciones': 'indicador', 'fob': 'valor'}),
 	str_to_title(col='indicador'),
 	sort_values(how='ascending', by=['geonombreFundar', 'anio', 'indicador'])
@@ -48,16 +54,33 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'exportaciones': 'indicador', 'fob': 'valor'})
-#  RangeIndex: 250 entries, 0 to 249
+#  query(condition="geonombreFundar=='Catamarca'")
+#  Index: 50 entries, 0 to 49
 #  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype 
 #  ---  ------           --------------  ----- 
-#   0   geocodigoFundar  250 non-null    object
-#   1   geonombreFundar  250 non-null    object
-#   2   anio             250 non-null    int64 
-#   3   indicador        250 non-null    object
-#   4   valor            250 non-null    int64 
+#   0   geocodigoFundar  50 non-null     object
+#   1   geonombreFundar  50 non-null     object
+#   2   anio             50 non-null     int64 
+#   3   exportaciones    50 non-null     object
+#   4   fob              50 non-null     int64 
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | exportaciones   |       fob |
+#  |---:|:------------------|:------------------|-------:|:----------------|----------:|
+#  |  0 | AR-K              | Catamarca         |   1998 | mineras         | 438881324 |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'exportaciones': 'indicador', 'fob': 'valor'})
+#  Index: 50 entries, 0 to 49
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype 
+#  ---  ------           --------------  ----- 
+#   0   geocodigoFundar  50 non-null     object
+#   1   geonombreFundar  50 non-null     object
+#   2   anio             50 non-null     int64 
+#   3   indicador        50 non-null     object
+#   4   valor            50 non-null     int64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio | indicador   |     valor |
 #  |---:|:------------------|:------------------|-------:|:------------|----------:|
@@ -66,15 +89,15 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  str_to_title(col='indicador')
-#  RangeIndex: 250 entries, 0 to 249
+#  Index: 50 entries, 0 to 49
 #  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype 
 #  ---  ------           --------------  ----- 
-#   0   geocodigoFundar  250 non-null    object
-#   1   geonombreFundar  250 non-null    object
-#   2   anio             250 non-null    int64 
-#   3   indicador        250 non-null    object
-#   4   valor            250 non-null    int64 
+#   0   geocodigoFundar  50 non-null     object
+#   1   geonombreFundar  50 non-null     object
+#   2   anio             50 non-null     int64 
+#   3   indicador        50 non-null     object
+#   4   valor            50 non-null     int64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio | indicador   |     valor |
 #  |---:|:------------------|:------------------|-------:|:------------|----------:|
@@ -83,15 +106,15 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['geonombreFundar', 'anio', 'indicador'])
-#  RangeIndex: 250 entries, 0 to 249
+#  RangeIndex: 50 entries, 0 to 49
 #  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype 
 #  ---  ------           --------------  ----- 
-#   0   geocodigoFundar  250 non-null    object
-#   1   geonombreFundar  250 non-null    object
-#   2   anio             250 non-null    int64 
-#   3   indicador        250 non-null    object
-#   4   valor            250 non-null    int64 
+#   0   geocodigoFundar  50 non-null     object
+#   1   geonombreFundar  50 non-null     object
+#   2   anio             50 non-null     int64 
+#   3   indicador        50 non-null     object
+#   4   valor            50 non-null     int64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio | indicador   |     valor |
 #  |---:|:------------------|:------------------|-------:|:------------|----------:|
