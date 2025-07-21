bcc500609b2a8d061e6c931fa70852aee18c1dc7
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-21 17:21:39 -0300

    Updated CIETEC/CIETEC_g17_transformer.py

--- CIETEC/CIETEC_g17_transformer.py
+++ CIETEC/CIETEC_g17_transformer.py
@@ -4,8 +4,8 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
     return df
 
 @transformer.convert
@@ -13,8 +13,8 @@ def pivot_longer(df: DataFrame, id_cols:list[str], names_to_col:str, values_to_c
     return df.melt(id_vars=id_cols, var_name=names_to_col, value_name=values_to_col)
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
     return df
 #  DEFINITIONS_END
 
@@ -23,7 +23,7 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 pipeline = chain(
 	pivot_longer(id_cols=['anio'], names_to_col='medida', values_to_col='valor'),
 	drop_na(col='valor'),
-	replace_value(col='medida', curr_value='i_d_pib', new_value='I+D'),
+	replace_value(col='medida', curr_value='id_pib', new_value='I+D'),
 	replace_value(col='medida', curr_value='act_pib', new_value='Act. científico tecnológicas')
 )
 #  PIPELINE_END
@@ -35,12 +35,12 @@ pipeline = chain(
 #   #   Column   Non-Null Count  Dtype  
 #  ---  ------   --------------  -----  
 #   0   anio     34 non-null     int64  
-#   1   i_d_pib  20 non-null     float64
+#   1   id_pib   28 non-null     float64
 #   2   act_pib  33 non-null     float64
 #  
-#  |    |   anio |   i_d_pib |   act_pib |
-#  |---:|-------:|----------:|----------:|
-#  |  0 |   1990 |       nan |      0.33 |
+#  |    |   anio |   id_pib |   act_pib |
+#  |---:|-------:|---------:|----------:|
+#  |  0 |   1990 |      nan |      0.33 |
 #  
 #  ------------------------------
 #  
@@ -51,56 +51,56 @@ pipeline = chain(
 #  ---  ------  --------------  -----  
 #   0   anio    68 non-null     int64  
 #   1   medida  68 non-null     object 
-#   2   valor   53 non-null     float64
+#   2   valor   61 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  |  0 |   1990 | i_d_pib  |     nan |
+#  |  0 |   1990 | id_pib   |     nan |
 #  
 #  ------------------------------
 #  
 #  drop_na(col='valor')
-#  Index: 53 entries, 14 to 66
+#  Index: 61 entries, 6 to 66
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    53 non-null     int64  
-#   1   medida  53 non-null     object 
-#   2   valor   53 non-null     float64
+#   0   anio    61 non-null     int64  
+#   1   medida  61 non-null     object 
+#   2   valor   61 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  | 14 |   2004 | i_d_pib  |     0.4 |
+#  |  6 |   1996 | id_pib   |    0.42 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='medida', curr_value='i_d_pib', new_value='I+D')
-#  Index: 53 entries, 14 to 66
+#  replace_value(col='medida', curr_value='id_pib', new_value='I+D')
+#  Index: 61 entries, 6 to 66
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    53 non-null     int64  
-#   1   medida  53 non-null     object 
-#   2   valor   53 non-null     float64
+#   0   anio    61 non-null     int64  
+#   1   medida  61 non-null     object 
+#   2   valor   61 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  | 14 |   2004 | I+D      |     0.4 |
+#  |  6 |   1996 | I+D      |    0.42 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='medida', curr_value='act_pib', new_value='Act. científico tecnológicas')
-#  Index: 53 entries, 14 to 66
+#  Index: 61 entries, 6 to 66
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    53 non-null     int64  
-#   1   medida  53 non-null     object 
-#   2   valor   53 non-null     float64
+#   0   anio    61 non-null     int64  
+#   1   medida  61 non-null     object 
+#   2   valor   61 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  | 14 |   2004 | I+D      |     0.4 |
+#  |  6 |   1996 | I+D      |    0.42 |
 #  
 #  ------------------------------
 #  
