c0dd20b8efcee16c10b3736c0e47fb3cd1905abc
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-21 11:42:12 -0300

    Updated CIETEC/CIETEC_g21_transformer.py

--- CIETEC/CIETEC_g21_transformer.py
+++ CIETEC/CIETEC_g21_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: pl.DataFrame) -> pl.DataFrame:
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	query(condition='anio <= 2022')
 )
 #  PIPELINE_END
 
@@ -33,16 +34,16 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 2527 entries, 0 to 2526
+#  query(condition='anio <= 2022')
+#  Index: 2500 entries, 0 to 2526
 #  Data columns (total 5 columns):
 #   #   Column            Non-Null Count  Dtype  
 #  ---  ------            --------------  -----  
-#   0   geocodigoFundar   2527 non-null   object 
-#   1   geonombreFundar   2527 non-null   object 
-#   2   anio              2527 non-null   int64  
-#   3   gerd_gdp          2527 non-null   float64
-#   4   nivel_agregacion  2527 non-null   object 
+#   0   geocodigoFundar   2500 non-null   object 
+#   1   geonombreFundar   2500 non-null   object 
+#   2   anio              2500 non-null   int64  
+#   3   gerd_gdp          2500 non-null   float64
+#   4   nivel_agregacion  2500 non-null   object 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio |   gerd_gdp | nivel_agregacion   |
 #  |---:|:------------------|:------------------|-------:|-----------:|:-------------------|
