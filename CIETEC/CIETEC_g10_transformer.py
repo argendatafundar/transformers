4f1fb19ce187741d4a6336b8177a1fcc23c9e4cb
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-21 12:39:59 -0300

    Updated CIETEC/CIETEC_g10_transformer.py

--- CIETEC/CIETEC_g10_transformer.py
+++ CIETEC/CIETEC_g10_transformer.py
@@ -4,49 +4,50 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: pl.DataFrame) -> pl.DataFrame:
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	query(condition="medida == 'Publicaciones en SCOPUS en relacion al Gasto en I+D (cada millón de U$S PPC)'")
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 24 entries, 0 to 23
+#  RangeIndex: 48 entries, 0 to 47
 #  Data columns (total 5 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geocodigoFundar  24 non-null     object 
-#   1   geonombreFundar  24 non-null     object 
-#   2   anio             24 non-null     int64  
-#   3   valor            24 non-null     float64
-#   4   medida           24 non-null     object 
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   pais    48 non-null     object 
+#   1   anio    48 non-null     int64  
+#   2   valor   48 non-null     float64
+#   3   medida  48 non-null     object 
+#   4   iso3    48 non-null     object 
 #  
-#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor | medida                                                                       |
-#  |---:|:------------------|:------------------|-------:|--------:|:-----------------------------------------------------------------------------|
-#  |  0 | GTM               | Guatemala         |   2022 | 40.8574 | Publicaciones en SCOPUS en relacion al Gasto en I+D (cada millón de U$S PPC) |
+#  |    | pais      |   anio |   valor | medida                                                                       | iso3   |
+#  |---:|:----------|-------:|--------:|:-----------------------------------------------------------------------------|:-------|
+#  |  0 | Guatemala |   2022 | 40.8574 | Publicaciones en SCOPUS en relacion al Gasto en I+D (cada millón de U$S PPC) | GTM    |
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 24 entries, 0 to 23
+#  query(condition="medida == 'Publicaciones en SCOPUS en relacion al Gasto en I+D (cada millón de U$S PPC)'")
+#  Index: 24 entries, 0 to 23
 #  Data columns (total 5 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geocodigoFundar  24 non-null     object 
-#   1   geonombreFundar  24 non-null     object 
-#   2   anio             24 non-null     int64  
-#   3   valor            24 non-null     float64
-#   4   medida           24 non-null     object 
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   pais    24 non-null     object 
+#   1   anio    24 non-null     int64  
+#   2   valor   24 non-null     float64
+#   3   medida  24 non-null     object 
+#   4   iso3    24 non-null     object 
 #  
-#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor | medida                                                                       |
-#  |---:|:------------------|:------------------|-------:|--------:|:-----------------------------------------------------------------------------|
-#  |  0 | GTM               | Guatemala         |   2022 | 40.8574 | Publicaciones en SCOPUS en relacion al Gasto en I+D (cada millón de U$S PPC) |
+#  |    | pais      |   anio |   valor | medida                                                                       | iso3   |
+#  |---:|:----------|-------:|--------:|:-----------------------------------------------------------------------------|:-------|
+#  |  0 | Guatemala |   2022 | 40.8574 | Publicaciones en SCOPUS en relacion al Gasto en I+D (cada millón de U$S PPC) | GTM    |
 #  
 #  ------------------------------
 #  
