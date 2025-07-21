f845b0b242bfec9982bc5a2576309345031f5025
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-21 11:29:23 -0300

    Updated CIETEC/CIETEC_g18_transformer.py

--- CIETEC/CIETEC_g18_transformer.py
+++ CIETEC/CIETEC_g18_transformer.py
@@ -9,12 +9,18 @@ def sort_values(df: DataFrame, how: str, by: list):
         raise ValueError('how must be either "ascending" or "descending"')
     
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	sort_values(how='ascending', by=['anio'])
+	sort_values(how='ascending', by=['anio']),
+	replace_multiple_values(col='funcion', replacements={'Técnicos y Personal Asimilado': 'P. técnico', 'Otro Personal de Apoyo': 'P. de apoyo'})
 )
 #  PIPELINE_END
 
@@ -37,6 +43,23 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 81 entries, 0 to 80
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype 
+#  ---  ------            --------------  ----- 
+#   0   geocodigoFundar   81 non-null     object
+#   1   geonombreFundar   81 non-null     object
+#   2   anio              81 non-null     int64 
+#   3   funcion           81 non-null     object
+#   4   personas_fisicas  81 non-null     int64 
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | funcion        |   personas_fisicas |
+#  |---:|:------------------|:------------------|-------:|:---------------|-------------------:|
+#  |  0 | ARG               | Argentina         |   1997 | Investigadores |              37198 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='funcion', replacements={'Técnicos y Personal Asimilado': 'P. técnico', 'Otro Personal de Apoyo': 'P. de apoyo'})
 #  RangeIndex: 81 entries, 0 to 80
 #  Data columns (total 5 columns):
 #   #   Column            Non-Null Count  Dtype 
