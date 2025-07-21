1627062ea74e0ca0c49dee984e40cbc919eb6db5
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-21 11:22:16 -0300

    Updated CIETEC/CIETEC_g17_transformer.py

--- CIETEC/CIETEC_g17_transformer.py
+++ CIETEC/CIETEC_g17_transformer.py
@@ -24,18 +24,18 @@ pipeline = chain(
 	pivot_longer(id_cols=['anio'], names_to_col='medida', values_to_col='valor'),
 	drop_na(col='valor'),
 	replace_value(col='medida', curr_value='i_d_pib', new_value='I+D'),
-	replace_value(col='medida', curr_value='act_pib', new_value='Act. científico-tecnológicas')
+	replace_value(col='medida', curr_value='act_pib', new_value='Act. científico tecnológicas')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 33 entries, 0 to 32
+#  RangeIndex: 34 entries, 0 to 33
 #  Data columns (total 3 columns):
 #   #   Column   Non-Null Count  Dtype  
 #  ---  ------   --------------  -----  
-#   0   anio     33 non-null     int64  
-#   1   i_d_pib  27 non-null     float64
+#   0   anio     34 non-null     int64  
+#   1   i_d_pib  20 non-null     float64
 #   2   act_pib  33 non-null     float64
 #  
 #  |    |   anio |   i_d_pib |   act_pib |
@@ -45,13 +45,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  pivot_longer(id_cols=['anio'], names_to_col='medida', values_to_col='valor')
-#  RangeIndex: 66 entries, 0 to 65
+#  RangeIndex: 68 entries, 0 to 67
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    66 non-null     int64  
-#   1   medida  66 non-null     object 
-#   2   valor   60 non-null     float64
+#   0   anio    68 non-null     int64  
+#   1   medida  68 non-null     object 
+#   2   valor   53 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
@@ -60,47 +60,47 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  drop_na(col='valor')
-#  Index: 60 entries, 6 to 65
+#  Index: 53 entries, 14 to 66
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    60 non-null     int64  
-#   1   medida  60 non-null     object 
-#   2   valor   60 non-null     float64
+#   0   anio    53 non-null     int64  
+#   1   medida  53 non-null     object 
+#   2   valor   53 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  |  6 |   1996 | i_d_pib  |    0.42 |
+#  | 14 |   2004 | i_d_pib  |     0.4 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='medida', curr_value='i_d_pib', new_value='I+D')
-#  Index: 60 entries, 6 to 65
+#  Index: 53 entries, 14 to 66
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    60 non-null     int64  
-#   1   medida  60 non-null     object 
-#   2   valor   60 non-null     float64
+#   0   anio    53 non-null     int64  
+#   1   medida  53 non-null     object 
+#   2   valor   53 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  |  6 |   1996 | I+D      |    0.42 |
+#  | 14 |   2004 | I+D      |     0.4 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='medida', curr_value='act_pib', new_value='Act. científico-tecnológicas')
-#  Index: 60 entries, 6 to 65
+#  replace_value(col='medida', curr_value='act_pib', new_value='Act. científico tecnológicas')
+#  Index: 53 entries, 14 to 66
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    60 non-null     int64  
-#   1   medida  60 non-null     object 
-#   2   valor   60 non-null     float64
+#   0   anio    53 non-null     int64  
+#   1   medida  53 non-null     object 
+#   2   valor   53 non-null     float64
 #  
 #  |    |   anio | medida   |   valor |
 #  |---:|-------:|:---------|--------:|
-#  |  6 |   1996 | I+D      |    0.42 |
+#  | 14 |   2004 | I+D      |     0.4 |
 #  
 #  ------------------------------
 #  
