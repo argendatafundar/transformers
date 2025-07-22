5246ca441ea89d74864d161671c97cd3fedf51ba
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-22 11:20:36 -0300

    Updated CIETEC/CIETEC_g07_transformer.py

--- CIETEC/CIETEC_g07_transformer.py
+++ CIETEC/CIETEC_g07_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
+    return df
+
+@transformer.convert
 def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
     import pandas as pd
     df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
@@ -14,7 +19,8 @@ def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[s
 
 #  PIPELINE_START
 pipeline = chain(
-	ordenar_dos_columnas(col1='categoria', order1=['Asistente', 'Adjunto', 'Independiente', 'Principal', 'Superior'], col2='genero', order2=['Mujeres', 'Varones'])
+	replace_multiple_values(col='genero', replacements={'Varones': 'Inv. Varones', 'Mujeres': 'Inv. Mujeres'}),
+	ordenar_dos_columnas(col1='categoria', order1=['Asistente', 'Adjunto', 'Independiente', 'Principal', 'Superior'], col2='genero', order2=['Inv. Mujeres', 'Inv. Varones'])
 )
 #  PIPELINE_END
 
@@ -35,7 +41,23 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  ordenar_dos_columnas(col1='categoria', order1=['Asistente', 'Adjunto', 'Independiente', 'Principal', 'Superior'], col2='genero', order2=['Mujeres', 'Varones'])
+#  replace_multiple_values(col='genero', replacements={'Varones': 'Inv. Varones', 'Mujeres': 'Inv. Mujeres'})
+#  RangeIndex: 10 entries, 0 to 9
+#  Data columns (total 4 columns):
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   categoria  10 non-null     category
+#   1   genero     10 non-null     category
+#   2   cantidad   10 non-null     int64   
+#   3   share      10 non-null     float64 
+#  
+#  |    | categoria   | genero       |   cantidad |   share |
+#  |---:|:------------|:-------------|-----------:|--------:|
+#  |  0 | Adjunto     | Inv. Mujeres |       2913 | 58.2367 |
+#  
+#  ------------------------------
+#  
+#  ordenar_dos_columnas(col1='categoria', order1=['Asistente', 'Adjunto', 'Independiente', 'Principal', 'Superior'], col2='genero', order2=['Inv. Mujeres', 'Inv. Varones'])
 #  Index: 10 entries, 2 to 9
 #  Data columns (total 4 columns):
 #   #   Column     Non-Null Count  Dtype   
@@ -45,9 +67,9 @@ pipeline = chain(
 #   2   cantidad   10 non-null     int64   
 #   3   share      10 non-null     float64 
 #  
-#  |    | categoria   | genero   |   cantidad |   share |
-#  |---:|:------------|:---------|-----------:|--------:|
-#  |  2 | Asistente   | Mujeres  |       1805 | 60.3881 |
+#  |    | categoria   | genero       |   cantidad |   share |
+#  |---:|:------------|:-------------|-----------:|--------:|
+#  |  2 | Asistente   | Inv. Mujeres |       1805 | 60.3881 |
 #  
 #  ------------------------------
 #  
