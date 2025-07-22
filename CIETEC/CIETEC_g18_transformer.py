bb4569781aed0a92721bb936ac42a173d8e19491
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-22 10:04:55 -0300

    Updated CIETEC/CIETEC_g18_transformer.py

--- CIETEC/CIETEC_g18_transformer.py
+++ CIETEC/CIETEC_g18_transformer.py
@@ -11,6 +11,11 @@ def sort_values(df: DataFrame, how: str, by: list):
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
     df[col] = df[col].replace(replacements)
     return df
@@ -19,6 +24,7 @@ def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFr
 
 #  PIPELINE_START
 pipeline = chain(
+	query(condition='anio in [1997, 1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021, 2023]'),
 	sort_values(how='ascending', by=['anio']),
 	replace_multiple_values(col='funcion', replacements={'Técnicos y Personal Asimilado': 'P. técnico', 'Otro Personal de Apoyo': 'P. de apoyo'})
 )
@@ -42,16 +48,33 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio'])
-#  RangeIndex: 81 entries, 0 to 80
+#  query(condition='anio in [1997, 1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015, 2017, 2019, 2021, 2023]')
+#  Index: 42 entries, 0 to 79
 #  Data columns (total 5 columns):
 #   #   Column            Non-Null Count  Dtype 
 #  ---  ------            --------------  ----- 
-#   0   geocodigoFundar   81 non-null     object
-#   1   geonombreFundar   81 non-null     object
-#   2   anio              81 non-null     int64 
-#   3   funcion           81 non-null     object
-#   4   personas_fisicas  81 non-null     int64 
+#   0   geocodigoFundar   42 non-null     object
+#   1   geonombreFundar   42 non-null     object
+#   2   anio              42 non-null     int64 
+#   3   funcion           42 non-null     object
+#   4   personas_fisicas  42 non-null     int64 
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | funcion        |   personas_fisicas |
+#  |---:|:------------------|:------------------|-------:|:---------------|-------------------:|
+#  |  0 | ARG               | Argentina         |   2017 | Investigadores |              84284 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['anio'])
+#  RangeIndex: 42 entries, 0 to 41
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype 
+#  ---  ------            --------------  ----- 
+#   0   geocodigoFundar   42 non-null     object
+#   1   geonombreFundar   42 non-null     object
+#   2   anio              42 non-null     int64 
+#   3   funcion           42 non-null     object
+#   4   personas_fisicas  42 non-null     int64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio | funcion        |   personas_fisicas |
 #  |---:|:------------------|:------------------|-------:|:---------------|-------------------:|
@@ -60,15 +83,15 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  replace_multiple_values(col='funcion', replacements={'Técnicos y Personal Asimilado': 'P. técnico', 'Otro Personal de Apoyo': 'P. de apoyo'})
-#  RangeIndex: 81 entries, 0 to 80
+#  RangeIndex: 42 entries, 0 to 41
 #  Data columns (total 5 columns):
 #   #   Column            Non-Null Count  Dtype 
 #  ---  ------            --------------  ----- 
-#   0   geocodigoFundar   81 non-null     object
-#   1   geonombreFundar   81 non-null     object
-#   2   anio              81 non-null     int64 
-#   3   funcion           81 non-null     object
-#   4   personas_fisicas  81 non-null     int64 
+#   0   geocodigoFundar   42 non-null     object
+#   1   geonombreFundar   42 non-null     object
+#   2   anio              42 non-null     int64 
+#   3   funcion           42 non-null     object
+#   4   personas_fisicas  42 non-null     int64 
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   anio | funcion        |   personas_fisicas |
 #  |---:|:------------------|:------------------|-------:|:---------------|-------------------:|
