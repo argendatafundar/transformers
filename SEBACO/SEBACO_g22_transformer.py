e03bb301ff9e94c3eb2e83f550181fc0d33dc475
LMCappelletti <lcappelletti@fund.ar>
2025-07-23 16:02:39 -0300

    Updated SEBACO/SEBACO_g22_transformer.py

--- SEBACO/SEBACO_g22_transformer.py
+++ SEBACO/SEBACO_g22_transformer.py
@@ -4,18 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
     return df
 
 @transformer.convert
@@ -27,9 +24,8 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'prop_expo': 'valor'}),
-	replace_value(col='geocodigo', curr_value='ROM', new_value='ROU'),
-	replace_value(col='geocodigo', curr_value='CHT', new_value='TWN'),
+	rename_cols(map={'prop_expo': 'valor'}),
+	sort_values(how='ascending', by=['anio', 'geonombreFundar']),
 	multiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
@@ -37,76 +33,65 @@ rename_cols(map={'iso3': 'geocodigo', 'prop_expo': 'valor'}),
 
 #  start()
 #  RangeIndex: 3194 entries, 0 to 3193
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   iso3       3194 non-null   object 
-#   1   anio       3194 non-null   int64  
-#   2   prop_expo  3194 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  3194 non-null   object 
+#   1   geonombreFundar  3194 non-null   object 
+#   2   anio             3194 non-null   int64  
+#   3   prop_expo        3194 non-null   float64
 #  
-#  |    | iso3   |   anio |   prop_expo |
-#  |---:|:-------|-------:|------------:|
-#  |  0 | AFG    |   2008 | 0.000248476 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'prop_expo': 'valor'})
-#  RangeIndex: 3194 entries, 0 to 3193
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3194 non-null   object 
-#   1   anio       3194 non-null   int64  
-#   2   valor      3194 non-null   float64
-#  
-#  |    | geocodigo   |   anio |       valor |
-#  |---:|:------------|-------:|------------:|
-#  |  0 | AFG         |   2008 | 0.000248476 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   prop_expo |
+#  |---:|:------------------|:------------------|-------:|------------:|
+#  |  0 | AFG               | Afganistán        |   2008 | 0.000248476 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='ROM', new_value='ROU')
+#  rename_cols(map={'prop_expo': 'valor'})
 #  RangeIndex: 3194 entries, 0 to 3193
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3194 non-null   object 
-#   1   anio       3194 non-null   int64  
-#   2   valor      3194 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  3194 non-null   object 
+#   1   geonombreFundar  3194 non-null   object 
+#   2   anio             3194 non-null   int64  
+#   3   valor            3194 non-null   float64
 #  
-#  |    | geocodigo   |   anio |       valor |
-#  |---:|:------------|-------:|------------:|
-#  |  0 | AFG         |   2008 | 0.000248476 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |       valor |
+#  |---:|:------------------|:------------------|-------:|------------:|
+#  |  0 | AFG               | Afganistán        |   2008 | 0.000248476 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='geocodigo', curr_value='CHT', new_value='TWN')
+#  sort_values(how='ascending', by=['anio', 'geonombreFundar'])
 #  RangeIndex: 3194 entries, 0 to 3193
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3194 non-null   object 
-#   1   anio       3194 non-null   int64  
-#   2   valor      3194 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  3194 non-null   object 
+#   1   geonombreFundar  3194 non-null   object 
+#   2   anio             3194 non-null   int64  
+#   3   valor            3194 non-null   float64
 #  
-#  |    | geocodigo   |   anio |     valor |
-#  |---:|:------------|-------:|----------:|
-#  |  0 | AFG         |   2008 | 0.0248476 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |     valor |
+#  |---:|:------------------|:------------------|-------:|----------:|
+#  |  0 | ALB               | Albania           |   2005 | 0.0070921 |
 #  
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=100)
 #  RangeIndex: 3194 entries, 0 to 3193
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  3194 non-null   object 
-#   1   anio       3194 non-null   int64  
-#   2   valor      3194 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  3194 non-null   object 
+#   1   geonombreFundar  3194 non-null   object 
+#   2   anio             3194 non-null   int64  
+#   3   valor            3194 non-null   float64
 #  
-#  |    | geocodigo   |   anio |     valor |
-#  |---:|:------------|-------:|----------:|
-#  |  0 | AFG         |   2008 | 0.0248476 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |     valor |
+#  |---:|:------------------|:------------------|-------:|----------:|
+#  |  0 | ALB               | Albania           |   2005 | 0.0070921 |
 #  
 #  ------------------------------
 #  
