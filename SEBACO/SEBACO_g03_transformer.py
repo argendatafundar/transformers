ef330447f91363e2fe3d7e3fb7ba4c1c4d983dda
LMCappelletti <lcappelletti@fund.ar>
2025-07-24 17:14:10 -0300

    Updated SEBACO/SEBACO_g03_transformer.py

--- SEBACO/SEBACO_g03_transformer.py
+++ SEBACO/SEBACO_g03_transformer.py
@@ -4,27 +4,28 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 
 @transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    return df.sort_values(by=by, ascending=how == 'ascending')
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'vab': 'valor', 'anio': 'categoria'}),
-	mutiplicar_por_escalar(col='valor', k=100),
+	rename_cols(map={'vab': 'valor', 'anio': 'categoria'}),
+	multiplicar_por_escalar(col='valor', k=100),
 	sort_values(how='ascending', by=['categoria'])
 )
 #  PIPELINE_END
@@ -58,7 +59,7 @@ rename_cols(map={'vab': 'valor', 'anio': 'categoria'}),
 #  
 #  ------------------------------
 #  
-#  mutiplicar_por_escalar(col='valor', k=100)
+#  multiplicar_por_escalar(col='valor', k=100)
 #  RangeIndex: 4 entries, 0 to 3
 #  Data columns (total 2 columns):
 #   #   Column     Non-Null Count  Dtype  
