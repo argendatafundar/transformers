a9634451898a93d4624f0f15e1ade3eae816d820
jgjuara <jg.juara@gmail.com>
2025-07-25 18:07:45 -0300

    Updated POBREZ/POBREZ_g16_transformer.py

--- POBREZ/POBREZ_g16_transformer.py
+++ POBREZ/POBREZ_g16_transformer.py
@@ -4,6 +4,10 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def drop_cols(df, cols):
+    return df.drop(cols)
+
+@transformer.convert
 def concatenar_columnas(df: pl.DataFrame, cols: list, nueva_col: str, separtor: str = "-") -> pl.DataFrame:
     # Validate that all columns exist
     missing_cols = [col for col in cols if col not in df.columns]
@@ -15,19 +19,9 @@ def concatenar_columnas(df: pl.DataFrame, cols: list, nueva_col: str, separtor: 
     ])
 
 @transformer.convert
-def cast_to(df: pl.DataFrame, col: str, target_type: str = "pl.Float64") -> pl.DataFrame:
-    return df.with_columns([
-        pl.col(col).cast(eval(target_type), strict=False)
-    ])
-
-@transformer.convert
 def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
     df = df.sql(query)
     return df
-
-@transformer.convert
-def drop_cols(df, cols):
-    return df.drop(cols)
 
 @transformer.convert
 def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
@@ -50,11 +44,10 @@ def rename_cols(df: pl.DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='semester', mapping={'I': 0, 'II': 5}, alias=None),
-	concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='.'),
-	cast_to(col='aniosem', target_type='pl.Float64'),
+	replace_value(col='semester', mapping={'I': 1, 'II': 2}, alias=None),
+	concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='-'),
 	df_sql(query="select * from self where poverty_line == 'Pobreza'"),
-	replace_value(col='labor_status', mapping={'CtaPropia_no_Profesional': 'Cuenta propia no profesional', 'CtaPropia_Profesional': 'Cuenta propia profesional', 'Asalariado_Informal': 'Asalariado informal', 'Asalariado_Formal': 'Asalariado formal', 'Patrones': 'Patrón'}, alias=None),
+	replace_value(col='labor_status', mapping={'CtaPropia_no_Profesional': 'Cta. propia no prof.', 'CtaPropia_Profesional': 'Cta. propia prof.', 'Asalariado_Informal': 'Asal. informal', 'Asalariado_Formal': 'Asal. formal', 'Patrones': 'Patrón'}, alias=None),
 	rename_cols(map={'labor_status': 'categoria', 'poverty_rate': 'valor'}),
 	drop_cols(cols='year'),
 	drop_cols(cols='semester'),
@@ -68,15 +61,11 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='semester', mapping={'I': 0, 'II': 5}, alias=None)
+#  replace_value(col='semester', mapping={'I': 1, 'II': 2}, alias=None)
 #  
 #  ------------------------------
 #  
-#  concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='.')
-#  
-#  ------------------------------
-#  
-#  cast_to(col='aniosem', target_type='pl.Float64')
+#  concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='-')
 #  
 #  ------------------------------
 #  
@@ -84,7 +73,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='labor_status', mapping={'CtaPropia_no_Profesional': 'Cuenta propia no profesional', 'CtaPropia_Profesional': 'Cuenta propia profesional', 'Asalariado_Informal': 'Asalariado informal', 'Asalariado_Formal': 'Asalariado formal', 'Patrones': 'Patrón'}, alias=None)
+#  replace_value(col='labor_status', mapping={'CtaPropia_no_Profesional': 'Cta. propia no prof.', 'CtaPropia_Profesional': 'Cta. propia prof.', 'Asalariado_Informal': 'Asal. informal', 'Asalariado_Formal': 'Asal. formal', 'Patrones': 'Patrón'}, alias=None)
 #  
 #  ------------------------------
 #  
