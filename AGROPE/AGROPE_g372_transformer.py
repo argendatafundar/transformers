62a1874c66b14ed457c86711d9e7957af65d67a2
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-07-25 16:31:54 -0300

    Updated AGROPE/AGROPE_g372_transformer.py

--- AGROPE/AGROPE_g372_transformer.py
+++ AGROPE/AGROPE_g372_transformer.py
@@ -4,29 +4,28 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 
 @transformer.convert
 def pivot_longer(df: DataFrame, id_cols:list[str], names_to_col:str, values_to_col:str) -> DataFrame:
     return df.melt(id_vars=id_cols, var_name=names_to_col, value_name=values_to_col)
 
 @transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
     df = df.replace({col: curr_value}, new_value)
     return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    data = df.copy()
-    data[col] = data[col]*k
-    return data
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_col(col=['productos_primarios', 'moa'], axis=1),
+	drop_col(col=['pp_sin_mineria', 'moa', 'total'], axis=1),
 	pivot_longer(id_cols=['anio'], names_to_col='sector', values_to_col='valor'),
 	replace_value(col='sector', curr_value='participacion_expo_totales', new_value='Participación en las exportaciones totales'),
 	multiplicar_por_escalar(col='valor', k=100)
@@ -35,77 +34,78 @@ pipeline = chain(
 
 
 #  start()
-#  RangeIndex: 14 entries, 0 to 13
-#  Data columns (total 4 columns):
+#  RangeIndex: 33 entries, 0 to 32
+#  Data columns (total 5 columns):
 #   #   Column                      Non-Null Count  Dtype  
 #  ---  ------                      --------------  -----  
-#   0   anio                        14 non-null     int64  
-#   1   productos_primarios         14 non-null     float64
-#   2   moa                         14 non-null     float64
-#   3   participacion_expo_totales  14 non-null     float64
+#   0   anio                        33 non-null     int64  
+#   1   moa                         33 non-null     float64
+#   2   pp_sin_mineria              33 non-null     float64
+#   3   total                       33 non-null     float64
+#   4   participacion_expo_totales  33 non-null     float64
 #  
-#  |    |   anio |   productos_primarios |   moa |   participacion_expo_totales |
-#  |---:|-------:|----------------------:|------:|-----------------------------:|
-#  |  0 |   2009 |                  8124 | 21212 |                     0.526943 |
+#  |    |   anio |     moa |   pp_sin_mineria |   total |   participacion_expo_totales |
+#  |---:|-------:|--------:|-----------------:|--------:|-----------------------------:|
+#  |  0 |   1992 | 4837.11 |          3474.69 | 12234.9 |                     0.679349 |
 #  
 #  ------------------------------
 #  
-#  drop_col(col=['productos_primarios', 'moa'], axis=1)
-#  RangeIndex: 14 entries, 0 to 13
+#  drop_col(col=['pp_sin_mineria', 'moa', 'total'], axis=1)
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 2 columns):
 #   #   Column                      Non-Null Count  Dtype  
 #  ---  ------                      --------------  -----  
-#   0   anio                        14 non-null     int64  
-#   1   participacion_expo_totales  14 non-null     float64
+#   0   anio                        33 non-null     int64  
+#   1   participacion_expo_totales  33 non-null     float64
 #  
 #  |    |   anio |   participacion_expo_totales |
 #  |---:|-------:|-----------------------------:|
-#  |  0 |   2009 |                     0.526943 |
+#  |  0 |   1992 |                     0.679349 |
 #  
 #  ------------------------------
 #  
 #  pivot_longer(id_cols=['anio'], names_to_col='sector', values_to_col='valor')
-#  RangeIndex: 14 entries, 0 to 13
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    14 non-null     int64  
-#   1   sector  14 non-null     object 
-#   2   valor   14 non-null     float64
+#   0   anio    33 non-null     int64  
+#   1   sector  33 non-null     object 
+#   2   valor   33 non-null     float64
 #  
 #  |    |   anio | sector                     |    valor |
 #  |---:|-------:|:---------------------------|---------:|
-#  |  0 |   2009 | participacion_expo_totales | 0.526943 |
+#  |  0 |   1992 | participacion_expo_totales | 0.679349 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='sector', curr_value='participacion_expo_totales', new_value='Participación en las exportaciones totales')
-#  RangeIndex: 14 entries, 0 to 13
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    14 non-null     int64  
-#   1   sector  14 non-null     object 
-#   2   valor   14 non-null     float64
+#   0   anio    33 non-null     int64  
+#   1   sector  33 non-null     object 
+#   2   valor   33 non-null     float64
 #  
-#  |    |   anio | sector                                     |    valor |
-#  |---:|-------:|:-------------------------------------------|---------:|
-#  |  0 |   2009 | Participación en las exportaciones totales | 0.526943 |
+#  |    |   anio | sector                                     |   valor |
+#  |---:|-------:|:-------------------------------------------|--------:|
+#  |  0 |   1992 | Participación en las exportaciones totales | 67.9349 |
 #  
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 14 entries, 0 to 13
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 3 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    14 non-null     int64  
-#   1   sector  14 non-null     object 
-#   2   valor   14 non-null     float64
+#   0   anio    33 non-null     int64  
+#   1   sector  33 non-null     object 
+#   2   valor   33 non-null     float64
 #  
 #  |    |   anio | sector                                     |   valor |
 #  |---:|-------:|:-------------------------------------------|--------:|
-#  |  0 |   2009 | Participación en las exportaciones totales | 52.6943 |
+#  |  0 |   1992 | Participación en las exportaciones totales | 67.9349 |
 #  
 #  ------------------------------
 #  
