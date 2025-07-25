983e368610937ffd3a360e8e7310e54f96631190
jgjuara <jg.juara@gmail.com>
2025-07-25 18:09:48 -0300

    Updated POBREZ/POBREZ_g15_transformer.py

--- POBREZ/POBREZ_g15_transformer.py
+++ POBREZ/POBREZ_g15_transformer.py
@@ -4,6 +4,10 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def drop_cols(df, cols):
+    return df.drop(cols)
+
+@transformer.convert
 def concatenar_columnas(df: pl.DataFrame, cols: list, nueva_col: str, separtor: str = "-") -> pl.DataFrame:
     # Validate that all columns exist
     missing_cols = [col for col in cols if col not in df.columns]
@@ -15,23 +19,8 @@ def concatenar_columnas(df: pl.DataFrame, cols: list, nueva_col: str, separtor: 
     ])
 
 @transformer.convert
-def cast_to(df: pl.DataFrame, col: str, target_type: str = "pl.Float64") -> pl.DataFrame:
-    return df.with_columns([
-        pl.col(col).cast(eval(target_type), strict=False)
-    ])
-
-@transformer.convert
 def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
     df = df.sql(query)
-    return df
-
-@transformer.convert
-def drop_cols(df, cols):
-    return df.drop(cols)
-
-@transformer.convert
-def pl_filter(df: pl.DataFrame, query: str):
-    df = df.filter(eval(query))
     return df
 
 @transformer.convert
@@ -50,14 +39,18 @@ def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
 def rename_cols(df: pl.DataFrame, map):
     df = df.rename(map)
     return df
+
+@transformer.convert
+def pl_filter(df: pl.DataFrame, query: str):
+    df = df.filter(eval(query))
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='semester', mapping={'I': 0, 'II': 5}, alias=None),
-	concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='.'),
-	cast_to(col='aniosem', target_type='pl.Float64'),
+	replace_value(col='semester', mapping={'I': 1, 'II': 2}, alias=None),
+	concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='-'),
 	pl_filter(query="pl.col('poverty_line') == 'Pobreza'"),
 	rename_cols(map={'labor_status': 'categoria', 'poverty_rate': 'valor'}),
 	drop_cols(cols='year'),
@@ -72,15 +65,11 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='semester', mapping={'I': 0, 'II': 5}, alias=None)
+#  replace_value(col='semester', mapping={'I': 1, 'II': 2}, alias=None)
 #  
 #  ------------------------------
 #  
-#  concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='.')
-#  
-#  ------------------------------
-#  
-#  cast_to(col='aniosem', target_type='pl.Float64')
+#  concatenar_columnas(cols=['year', 'semester'], nueva_col='aniosem', separtor='-')
 #  
 #  ------------------------------
 #  
