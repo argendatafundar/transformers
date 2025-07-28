37154d3e62eac96adecaa94fdb441cca92653a2d
LMCappelletti <lcappelletti@fund.ar>
2025-07-28 14:05:42 -0300

    Updated SEBACO/SEBACO_g19_transformer.py

--- SEBACO/SEBACO_g19_transformer.py
+++ SEBACO/SEBACO_g19_transformer.py
@@ -9,18 +9,17 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+def ordenar_categorica(df, col1:str, order1:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    return df.sort_values(by=[col1])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	rename_cols(map={'sector': 'indicador', 'balanza': 'valor'}),
-	sort_values(how='ascending', by=['anio', 'valor'])
+	ordenar_categorica(col1='indicador', order1=['Servicios profesionales', 'SSI', 'Investigación y desarrollo', 'Ss. arquitectura, ingeniería y otros', 'Ss. audiovisuales', 'Propiedad intelectual'])
 )
 #  PIPELINE_END
 
@@ -43,11 +42,11 @@ pipeline = chain(
 #  rename_cols(map={'sector': 'indicador', 'balanza': 'valor'})
 #  RangeIndex: 102 entries, 0 to 101
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       102 non-null    int64  
-#   1   indicador  102 non-null    object 
-#   2   valor      102 non-null    float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   anio       102 non-null    int64   
+#   1   indicador  102 non-null    category
+#   2   valor      102 non-null    float64 
 #  
 #  |    |   anio | indicador             |    valor |
 #  |---:|-------:|:----------------------|---------:|
@@ -55,18 +54,18 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'valor'])
-#  RangeIndex: 102 entries, 0 to 101
+#  ordenar_categorica(col1='indicador', order1=['Servicios profesionales', 'SSI', 'Investigación y desarrollo', 'Ss. arquitectura, ingeniería y otros', 'Ss. audiovisuales', 'Propiedad intelectual'])
+#  Index: 102 entries, 56 to 0
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       102 non-null    int64  
-#   1   indicador  102 non-null    object 
-#   2   valor      102 non-null    float64
+#   #   Column     Non-Null Count  Dtype   
+#  ---  ------     --------------  -----   
+#   0   anio       102 non-null    int64   
+#   1   indicador  102 non-null    category
+#   2   valor      102 non-null    float64 
 #  
-#  |    |   anio | indicador             |    valor |
-#  |---:|-------:|:----------------------|---------:|
-#  |  0 |   2006 | Propiedad intelectual | -814.312 |
+#  |    |   anio | indicador               |   valor |
+#  |---:|-------:|:------------------------|--------:|
+#  | 56 |   2011 | Servicios profesionales | 1193.73 |
 #  
 #  ------------------------------
 #  
