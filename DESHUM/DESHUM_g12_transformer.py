80e7b91504e7d0fcd7227abdb64114aeb42464cb
LMCappelletti <lcappelletti@fund.ar>
2025-07-29 18:54:48 -0300

    Updated DESHUM/DESHUM_g12_transformer.py

--- DESHUM/DESHUM_g12_transformer.py
+++ DESHUM/DESHUM_g12_transformer.py
@@ -4,35 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def df_merge_geonomenclador(df: DataFrame, left:str = 'iso3', right:str = 'geocodigo', how:str='left'):
-    df =  df.merge(geonomenclador, left_on = left, right_on = right, how = how)
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -46,166 +17,82 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='anio == anio.max()'),
-	replace_value(col='iso3', curr_value='ZZK.WORLD', new_value='WLD'),
-	replace_value(col='iso3', curr_value='ZZH.LAC', new_value='DESHUM_ZZH.LAC'),
-	replace_value(col='iso3', curr_value='ZZJ.SSA', new_value='DESHUM_ZZJ.SSA'),
-	df_merge_geonomenclador(left='iso3', right='geocodigo', how='left'),
-	drop_col(col=['geocodigo', 'iso3', 'country', 'name_short', 'iso_2', 'anio'], axis=1),
-	rename_cols(map={'name_long': 'categoria', 'sexo': 'indicador', 'idh': 'valor'}),
-	query(condition="categoria in ('Argentina', 'Yemen','Noruega','Brasil','Suiza','Mundo','Burundi')")
+	query(condition='anio == anio.max()'),
+	rename_cols(map={'sexo': 'indicador', 'idh': 'valor'}),
+	query(condition="geonombreFundar in ('Argentina', 'Yemen','Noruega','Brasil','Suiza','Mundo','Burundi')")
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 13596 entries, 0 to 13595
-#  Data columns (total 5 columns):
-#   #   Column   Non-Null Count  Dtype  
-#  ---  ------   --------------  -----  
-#   0   iso3     13596 non-null  object 
-#   1   country  13596 non-null  object 
-#   2   anio     13596 non-null  int64  
-#   3   sexo     13596 non-null  object 
-#   4   idh      10028 non-null  float64
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  13530 non-null  object 
+#   1   geonombreFundar  13530 non-null  object 
+#   2   country          13596 non-null  object 
+#   3   anio             13596 non-null  int64  
+#   4   sexo             13596 non-null  object 
+#   5   idh              10028 non-null  float64
 #  
-#  |    | iso3   | country     |   anio | sexo    |   idh |
-#  |---:|:-------|:------------|-------:|:--------|------:|
-#  |  0 | AFG    | Afghanistan |   1990 | Varones |   nan |
+#  |    | geocodigoFundar   | geonombreFundar   | country     |   anio | sexo    |   idh |
+#  |---:|:------------------|:------------------|:------------|-------:|:--------|------:|
+#  |  0 | AFG               | Afganistán        | Afghanistan |   1990 | Varones |   nan |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio == anio.max()')
 #  Index: 412 entries, 32 to 13595
-#  Data columns (total 5 columns):
-#   #   Column   Non-Null Count  Dtype  
-#  ---  ------   --------------  -----  
-#   0   iso3     412 non-null    object 
-#   1   country  412 non-null    object 
-#   2   anio     412 non-null    int64  
-#   3   sexo     412 non-null    object 
-#   4   idh      386 non-null    float64
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  410 non-null    object 
+#   1   geonombreFundar  410 non-null    object 
+#   2   country          412 non-null    object 
+#   3   anio             412 non-null    int64  
+#   4   sexo             412 non-null    object 
+#   5   idh              386 non-null    float64
 #  
-#  |    | iso3   | country     |   anio | sexo    |      idh |
-#  |---:|:-------|:------------|-------:|:--------|---------:|
-#  | 32 | AFG    | Afghanistan |   2022 | Varones | 0.534145 |
+#  |    | geocodigoFundar   | geonombreFundar   | country     |   anio | sexo    |      idh |
+#  |---:|:------------------|:------------------|:------------|-------:|:--------|---------:|
+#  | 32 | AFG               | Afganistán        | Afghanistan |   2022 | Varones | 0.534145 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='iso3', curr_value='ZZK.WORLD', new_value='WLD')
+#  rename_cols(map={'sexo': 'indicador', 'idh': 'valor'})
 #  Index: 412 entries, 32 to 13595
-#  Data columns (total 5 columns):
-#   #   Column   Non-Null Count  Dtype  
-#  ---  ------   --------------  -----  
-#   0   iso3     412 non-null    object 
-#   1   country  412 non-null    object 
-#   2   anio     412 non-null    int64  
-#   3   sexo     412 non-null    object 
-#   4   idh      386 non-null    float64
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  410 non-null    object 
+#   1   geonombreFundar  410 non-null    object 
+#   2   country          412 non-null    object 
+#   3   anio             412 non-null    int64  
+#   4   indicador        412 non-null    object 
+#   5   valor            386 non-null    float64
 #  
-#  |    | iso3   | country     |   anio | sexo    |      idh |
-#  |---:|:-------|:------------|-------:|:--------|---------:|
-#  | 32 | AFG    | Afghanistan |   2022 | Varones | 0.534145 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='iso3', curr_value='ZZH.LAC', new_value='DESHUM_ZZH.LAC')
-#  Index: 412 entries, 32 to 13595
-#  Data columns (total 5 columns):
-#   #   Column   Non-Null Count  Dtype  
-#  ---  ------   --------------  -----  
-#   0   iso3     412 non-null    object 
-#   1   country  412 non-null    object 
-#   2   anio     412 non-null    int64  
-#   3   sexo     412 non-null    object 
-#   4   idh      386 non-null    float64
-#  
-#  |    | iso3   | country     |   anio | sexo    |      idh |
-#  |---:|:-------|:------------|-------:|:--------|---------:|
-#  | 32 | AFG    | Afghanistan |   2022 | Varones | 0.534145 |
+#  |    | geocodigoFundar   | geonombreFundar   | country     |   anio | indicador   |    valor |
+#  |---:|:------------------|:------------------|:------------|-------:|:------------|---------:|
+#  | 32 | AFG               | Afganistán        | Afghanistan |   2022 | Varones     | 0.534145 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='iso3', curr_value='ZZJ.SSA', new_value='DESHUM_ZZJ.SSA')
-#  Index: 412 entries, 32 to 13595
-#  Data columns (total 5 columns):
-#   #   Column   Non-Null Count  Dtype  
-#  ---  ------   --------------  -----  
-#   0   iso3     412 non-null    object 
-#   1   country  412 non-null    object 
-#   2   anio     412 non-null    int64  
-#   3   sexo     412 non-null    object 
-#   4   idh      386 non-null    float64
+#  query(condition="geonombreFundar in ('Argentina', 'Yemen','Noruega','Brasil','Suiza','Mundo','Burundi')")
+#  Index: 12 entries, 197 to 13133
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  12 non-null     object 
+#   1   geonombreFundar  12 non-null     object 
+#   2   country          12 non-null     object 
+#   3   anio             12 non-null     int64  
+#   4   indicador        12 non-null     object 
+#   5   valor            12 non-null     float64
 #  
-#  |    | iso3   | country     |   anio | sexo    |      idh |
-#  |---:|:-------|:------------|-------:|:--------|---------:|
-#  | 32 | AFG    | Afghanistan |   2022 | Varones | 0.534145 |
-#  
-#  ------------------------------
-#  
-#  df_merge_geonomenclador(left='iso3', right='geocodigo', how='left')
-#  RangeIndex: 412 entries, 0 to 411
-#  Data columns (total 9 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   iso3        412 non-null    object 
-#   1   country     412 non-null    object 
-#   2   anio        412 non-null    int64  
-#   3   sexo        412 non-null    object 
-#   4   idh         386 non-null    float64
-#   5   geocodigo   396 non-null    object 
-#   6   name_long   396 non-null    object 
-#   7   name_short  396 non-null    object 
-#   8   iso_2       388 non-null    object 
-#  
-#  |    | iso3   | country     |   anio | sexo    |      idh | geocodigo   | name_long   | name_short   | iso_2   |
-#  |---:|:-------|:------------|-------:|:--------|---------:|:------------|:------------|:-------------|:--------|
-#  |  0 | AFG    | Afghanistan |   2022 | Varones | 0.534145 | AFG         | Afganistán  | Afganistán   | AF      |
-#  
-#  ------------------------------
-#  
-#  drop_col(col=['geocodigo', 'iso3', 'country', 'name_short', 'iso_2', 'anio'], axis=1)
-#  RangeIndex: 412 entries, 0 to 411
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   sexo       412 non-null    object 
-#   1   idh        386 non-null    float64
-#   2   name_long  396 non-null    object 
-#  
-#  |    | sexo    |      idh | name_long   |
-#  |---:|:--------|---------:|:------------|
-#  |  0 | Varones | 0.534145 | Afganistán  |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'name_long': 'categoria', 'sexo': 'indicador', 'idh': 'valor'})
-#  RangeIndex: 412 entries, 0 to 411
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   indicador  412 non-null    object 
-#   1   valor      386 non-null    float64
-#   2   categoria  396 non-null    object 
-#  
-#  |    | indicador   |    valor | categoria   |
-#  |---:|:------------|---------:|:------------|
-#  |  0 | Varones     | 0.534145 | Afganistán  |
-#  
-#  ------------------------------
-#  
-#  query(condition="categoria in ('Argentina', 'Yemen','Noruega','Brasil','Suiza','Mundo','Burundi')")
-#  Index: 14 entries, 5 to 411
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   indicador  14 non-null     object 
-#   1   valor      14 non-null     float64
-#   2   categoria  14 non-null     object 
-#  
-#  |    | indicador   |    valor | categoria   |
-#  |---:|:------------|---------:|:------------|
-#  |  5 | Varones     | 0.845473 | Argentina   |
+#  |     | geocodigoFundar   | geonombreFundar   | country   |   anio | indicador   |    valor |
+#  |----:|:------------------|:------------------|:----------|-------:|:------------|---------:|
+#  | 197 | ARG               | Argentina         | Argentina |   2022 | Varones     | 0.845473 |
 #  
 #  ------------------------------
 #  
