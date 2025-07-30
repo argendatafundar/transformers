6157a525e59d9c3587d5cf6049129dd6a7879a0a
LMCappelletti <lcappelletti@fund.ar>
2025-07-30 15:13:46 -0300

    Updated DESHUM/DESHUM_g13_transformer.py

--- DESHUM/DESHUM_g13_transformer.py
+++ DESHUM/DESHUM_g13_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -16,6 +21,7 @@ def drop_col(df: DataFrame, col, axis=1):
 
 #  PIPELINE_START
 pipeline = chain(
+	query(condition='anio == anio.max()'),
 	drop_col(col=['geocodigoFundar', 'es_agregacion', 'anio'], axis=1),
 	rename_cols(map={'continente_fundar': 'grupo', 'geonombreFundar': 'geonombre'})
 )
@@ -41,35 +47,54 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
+#  query(condition='anio == anio.max()')
+#  Index: 165 entries, 32 to 5094
+#  Data columns (total 7 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   geocodigoFundar    165 non-null    object 
+#   1   geonombreFundar    165 non-null    object 
+#   2   continente_fundar  154 non-null    object 
+#   3   es_agregacion      154 non-null    float64
+#   4   anio               165 non-null    int64  
+#   5   idh                165 non-null    float64
+#   6   dif_idh_idhp       165 non-null    float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   | continente_fundar   |   es_agregacion |   anio |   idh |   dif_idh_idhp |
+#  |---:|:------------------|:------------------|:--------------------|----------------:|-------:|------:|---------------:|
+#  | 32 | AFG               | Afganistán        | Asia                |               0 |   2022 | 0.462 |       0.649351 |
+#  
+#  ------------------------------
+#  
 #  drop_col(col=['geocodigoFundar', 'es_agregacion', 'anio'], axis=1)
-#  RangeIndex: 5095 entries, 0 to 5094
+#  Index: 165 entries, 32 to 5094
 #  Data columns (total 4 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   geonombreFundar    5095 non-null   object 
-#   1   continente_fundar  4734 non-null   object 
-#   2   idh                5095 non-null   float64
-#   3   dif_idh_idhp       5095 non-null   float64
+#   0   geonombreFundar    165 non-null    object 
+#   1   continente_fundar  154 non-null    object 
+#   2   idh                165 non-null    float64
+#   3   dif_idh_idhp       165 non-null    float64
 #  
 #  |    | geonombreFundar   | continente_fundar   |   idh |   dif_idh_idhp |
 #  |---:|:------------------|:--------------------|------:|---------------:|
-#  |  0 | Afganistán        | Asia                | 0.284 |        1.05634 |
+#  | 32 | Afganistán        | Asia                | 0.462 |       0.649351 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'continente_fundar': 'grupo', 'geonombreFundar': 'geonombre'})
-#  RangeIndex: 5095 entries, 0 to 5094
+#  Index: 165 entries, 32 to 5094
 #  Data columns (total 4 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   geonombre     5095 non-null   object 
-#   1   grupo         4734 non-null   object 
-#   2   idh           5095 non-null   float64
-#   3   dif_idh_idhp  5095 non-null   float64
+#   0   geonombre     165 non-null    object 
+#   1   grupo         154 non-null    object 
+#   2   idh           165 non-null    float64
+#   3   dif_idh_idhp  165 non-null    float64
 #  
 #  |    | geonombre   | grupo   |   idh |   dif_idh_idhp |
 #  |---:|:------------|:--------|------:|---------------:|
-#  |  0 | Afganistán  | Asia    | 0.284 |        1.05634 |
+#  | 32 | Afganistán  | Asia    | 0.462 |       0.649351 |
 #  
 #  ------------------------------
 #  
