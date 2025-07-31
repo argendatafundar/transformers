0d6cd8219c14d448c3c0305c2dcb617d547cd3ce
LMCappelletti <lcappelletti@fund.ar>
2025-07-31 16:16:38 -0300

    Updated DESHUM/DESHUM_g11_transformer.py

--- DESHUM/DESHUM_g11_transformer.py
+++ DESHUM/DESHUM_g11_transformer.py
@@ -9,16 +9,24 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
+def fill_mundo_world(df: DataFrame) -> DataFrame:
+    df.loc[
+        (df['country'] == 'World') & df['geonombreFundar'].isna(),
+        'geonombreFundar'
+    ] = 'Mundo'
     return df
+
+@transformer.convert
+def drop_na(df, subset:str): 
+    return df.dropna(subset=subset, axis=0)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	rename_cols(map={'gdi': 'valor'}),
-	drop_na(col=['valor'])
+	fill_mundo_world(),
+	drop_na(subset=['valor'])
 )
 #  PIPELINE_END
 
@@ -46,7 +54,7 @@ pipeline = chain(
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
 #   0   geocodigoFundar  6765 non-null   object 
-#   1   geonombreFundar  6765 non-null   object 
+#   1   geonombreFundar  6798 non-null   object 
 #   2   anio             6798 non-null   int64  
 #   3   country          6798 non-null   object 
 #   4   valor            5014 non-null   float64
@@ -57,13 +65,30 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  drop_na(col=['valor'])
+#  fill_mundo_world()
+#  RangeIndex: 6798 entries, 0 to 6797
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  6765 non-null   object 
+#   1   geonombreFundar  6798 non-null   object 
+#   2   anio             6798 non-null   int64  
+#   3   country          6798 non-null   object 
+#   4   valor            5014 non-null   float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | country     |   valor |
+#  |---:|:------------------|:------------------|-------:|:------------|--------:|
+#  |  0 | AFG               | Afganistán        |   1990 | Afghanistan |     nan |
+#  
+#  ------------------------------
+#  
+#  drop_na(subset=['valor'])
 #  Index: 5014 entries, 18 to 6797
 #  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
 #   0   geocodigoFundar  4981 non-null   object 
-#   1   geonombreFundar  4981 non-null   object 
+#   1   geonombreFundar  5014 non-null   object 
 #   2   anio             5014 non-null   int64  
 #   3   country          5014 non-null   object 
 #   4   valor            5014 non-null   float64
