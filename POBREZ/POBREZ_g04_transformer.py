a87e051968ee515672deeaa37b34c8e7ff8b92db
jgjuara <jg.juara@gmail.com>
2025-08-07 09:22:39 -0300

    Updated POBREZ/POBREZ_g04_transformer.py

--- POBREZ/POBREZ_g04_transformer.py
+++ POBREZ/POBREZ_g04_transformer.py
@@ -4,13 +4,13 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
 
 @transformer.convert
@@ -18,30 +18,6 @@ def filtrar_filas_ultimo_semestre(df:DataFrame, col_anio:str, col_semestre:str):
     df = df[df[col_anio] ==  df[col_anio].max()]
     df = df[df[col_semestre] == df[col_semestre].max()]
     return df
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
 
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
@@ -53,21 +29,27 @@ def query(df: DataFrame, condition: str):
     return df
 
 @transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-replace_value(col='semester', curr_value='I', new_value=1),
+	to_pandas(dummy=True),
+	replace_value(col='semester', curr_value='I', new_value=1),
 	replace_value(col='date', curr_value='II', new_value=2),
 	filtrar_filas_ultimo_semestre(col_anio='year', col_semestre='semester'),
 	rename_cols(map={'region': 'indicador', 'k_value': 'categoria', 'pov_rate': 'valor'}),
 	replace_value(col='categoria', curr_value=0.25, new_value='k = 0,25'),
 	replace_value(col='categoria', curr_value=0.35, new_value='k = 0,35'),
-	replace_value(col='indicador', curr_value='Partidos', new_value='Partidos GBA'),
+	replace_value(col='indicador', curr_value='Partidos', new_value='Part. del GBA'),
 	drop_col(col='year', axis=1),
 	drop_col(col='semester', axis=1),
 	query(condition="indicador != 'Total'"),
@@ -77,6 +59,10 @@ replace_value(col='semester', curr_value='I', new_value=1),
 
 
 #  start()
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
 #  RangeIndex: 672 entries, 0 to 671
 #  Data columns (total 5 columns):
 #   #   Column    Non-Null Count  Dtype  
@@ -195,7 +181,7 @@ replace_value(col='semester', curr_value='I', new_value=1),
 #  
 #  ------------------------------
 #  
-#  replace_value(col='indicador', curr_value='Partidos', new_value='Partidos GBA')
+#  replace_value(col='indicador', curr_value='Partidos', new_value='Part. del GBA')
 #  Index: 16 entries, 328 to 671
 #  Data columns (total 5 columns):
 #   #   Column     Non-Null Count  Dtype  
@@ -252,9 +238,9 @@ replace_value(col='semester', curr_value='I', new_value=1),
 #   1   categoria  14 non-null     object 
 #   2   valor      14 non-null     float64
 #  
-#  |     | indicador    | categoria   |   valor |
-#  |----:|:-------------|:------------|--------:|
-#  | 329 | Partidos GBA | k = 0,25    | 13.7982 |
+#  |     | indicador     | categoria   |   valor |
+#  |----:|:--------------|:------------|--------:|
+#  | 329 | Part. del GBA | k = 0,25    | 13.7982 |
 #  
 #  ------------------------------
 #  
@@ -267,9 +253,9 @@ replace_value(col='semester', curr_value='I', new_value=1),
 #   1   categoria  14 non-null     object 
 #   2   valor      14 non-null     float64
 #  
-#  |     | indicador    | categoria   |   valor |
-#  |----:|:-------------|:------------|--------:|
-#  | 329 | Partidos GBA | k = 0,25    | 13.7982 |
+#  |     | indicador     | categoria   |   valor |
+#  |----:|:--------------|:------------|--------:|
+#  | 329 | Part. del GBA | k = 0,25    | 13.7982 |
 #  
 #  ------------------------------
 #  
