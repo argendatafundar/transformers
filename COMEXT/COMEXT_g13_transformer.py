4e9833b13021668b5d005049c594ac8c72d3f520
jgjuara <jg.juara@gmail.com>
2025-08-07 16:51:44 -0300

    Updated COMEXT/COMEXT_g13_transformer.py

--- COMEXT/COMEXT_g13_transformer.py
+++ COMEXT/COMEXT_g13_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -16,17 +21,14 @@ def rename_cols(df: DataFrame, map):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='iso3 == "ARG"'),
-	rename_cols(map={'iso3': 'geocodigo', 'sector_bp_name': 'categoria', 'import_value_pc': 'valor'}),
+	to_pandas(dummy=True),
+	query(condition='geocodigoFundar == "ARG"'),
+	rename_cols(map={'geocodigoFundar': 'geocodigo', 'sector_bp_name': 'categoria', 'import_value_pc': 'valor'}),
 	drop_col(col='country_name_abbreviation', axis=1),
 	drop_col(col='sector_bp', axis=1)
 )
@@ -34,89 +36,98 @@ query(condition='iso3 == "ARG"'),
 
 
 #  start()
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
 #  RangeIndex: 2930 entries, 0 to 2929
-#  Data columns (total 6 columns):
+#  Data columns (total 7 columns):
 #   #   Column                     Non-Null Count  Dtype  
 #  ---  ------                     --------------  -----  
-#   0   anio                       2930 non-null   int64  
-#   1   iso3                       2930 non-null   object 
-#   2   country_name_abbreviation  2930 non-null   object 
-#   3   sector_bp                  2930 non-null   int64  
-#   4   sector_bp_name             2930 non-null   object 
-#   5   import_value_pc            2912 non-null   float64
+#   0   geocodigoFundar            2930 non-null   object 
+#   1   geonombreFundar            2930 non-null   object 
+#   2   anio                       2930 non-null   int64  
+#   3   country_name_abbreviation  2930 non-null   object 
+#   4   sector_bp                  2930 non-null   int64  
+#   5   sector_bp_name             2930 non-null   object 
+#   6   import_value_pc            2912 non-null   float64
 #  
-#  |    |   anio | iso3   | country_name_abbreviation   |   sector_bp | sector_bp_name       |   import_value_pc |
-#  |---:|-------:|:-------|:----------------------------|------------:|:---------------------|------------------:|
-#  |  0 |   2020 | BGD    | Bangladesh                  |           2 | Alimentos procesados |           2.64054 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | country_name_abbreviation   |   sector_bp | sector_bp_name       |   import_value_pc |
+#  |---:|:------------------|:------------------|-------:|:----------------------------|------------:|:---------------------|------------------:|
+#  |  0 | BGD               | Bangladesh        |   2020 | Bangladesh                  |           2 | Alimentos procesados |           2.64054 |
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3 == "ARG"')
+#  query(condition='geocodigoFundar == "ARG"')
 #  Index: 13 entries, 477 to 2607
-#  Data columns (total 6 columns):
+#  Data columns (total 7 columns):
 #   #   Column                     Non-Null Count  Dtype  
 #  ---  ------                     --------------  -----  
-#   0   anio                       13 non-null     int64  
-#   1   iso3                       13 non-null     object 
-#   2   country_name_abbreviation  13 non-null     object 
-#   3   sector_bp                  13 non-null     int64  
-#   4   sector_bp_name             13 non-null     object 
-#   5   import_value_pc            13 non-null     float64
+#   0   geocodigoFundar            13 non-null     object 
+#   1   geonombreFundar            13 non-null     object 
+#   2   anio                       13 non-null     int64  
+#   3   country_name_abbreviation  13 non-null     object 
+#   4   sector_bp                  13 non-null     int64  
+#   5   sector_bp_name             13 non-null     object 
+#   6   import_value_pc            13 non-null     float64
 #  
-#  |     |   anio | iso3   | country_name_abbreviation   |   sector_bp | sector_bp_name   |   import_value_pc |
-#  |----:|-------:|:-------|:----------------------------|------------:|:-----------------|------------------:|
-#  | 477 |   2020 | ARG    | Argentina                   |           6 | Cueros y pieles  |          0.242406 |
+#  |     | geocodigoFundar   | geonombreFundar   |   anio | country_name_abbreviation   |   sector_bp | sector_bp_name   |   import_value_pc |
+#  |----:|:------------------|:------------------|-------:|:----------------------------|------------:|:-----------------|------------------:|
+#  | 477 | ARG               | Argentina         |   2020 | Argentina                   |           6 | Cueros y pieles  |          0.242406 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'sector_bp_name': 'categoria', 'import_value_pc': 'valor'})
+#  rename_cols(map={'geocodigoFundar': 'geocodigo', 'sector_bp_name': 'categoria', 'import_value_pc': 'valor'})
 #  Index: 13 entries, 477 to 2607
-#  Data columns (total 6 columns):
+#  Data columns (total 7 columns):
 #   #   Column                     Non-Null Count  Dtype  
 #  ---  ------                     --------------  -----  
-#   0   anio                       13 non-null     int64  
-#   1   geocodigo                  13 non-null     object 
-#   2   country_name_abbreviation  13 non-null     object 
-#   3   sector_bp                  13 non-null     int64  
-#   4   categoria                  13 non-null     object 
-#   5   valor                      13 non-null     float64
+#   0   geocodigo                  13 non-null     object 
+#   1   geonombreFundar            13 non-null     object 
+#   2   anio                       13 non-null     int64  
+#   3   country_name_abbreviation  13 non-null     object 
+#   4   sector_bp                  13 non-null     int64  
+#   5   categoria                  13 non-null     object 
+#   6   valor                      13 non-null     float64
 #  
-#  |     |   anio | geocodigo   | country_name_abbreviation   |   sector_bp | categoria       |    valor |
-#  |----:|-------:|:------------|:----------------------------|------------:|:----------------|---------:|
-#  | 477 |   2020 | ARG         | Argentina                   |           6 | Cueros y pieles | 0.242406 |
+#  |     | geocodigo   | geonombreFundar   |   anio | country_name_abbreviation   |   sector_bp | categoria       |    valor |
+#  |----:|:------------|:------------------|-------:|:----------------------------|------------:|:----------------|---------:|
+#  | 477 | ARG         | Argentina         |   2020 | Argentina                   |           6 | Cueros y pieles | 0.242406 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='country_name_abbreviation', axis=1)
 #  Index: 13 entries, 477 to 2607
-#  Data columns (total 5 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       13 non-null     int64  
-#   1   geocodigo  13 non-null     object 
-#   2   sector_bp  13 non-null     int64  
-#   3   categoria  13 non-null     object 
-#   4   valor      13 non-null     float64
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigo        13 non-null     object 
+#   1   geonombreFundar  13 non-null     object 
+#   2   anio             13 non-null     int64  
+#   3   sector_bp        13 non-null     int64  
+#   4   categoria        13 non-null     object 
+#   5   valor            13 non-null     float64
 #  
-#  |     |   anio | geocodigo   |   sector_bp | categoria       |    valor |
-#  |----:|-------:|:------------|------------:|:----------------|---------:|
-#  | 477 |   2020 | ARG         |           6 | Cueros y pieles | 0.242406 |
+#  |     | geocodigo   | geonombreFundar   |   anio |   sector_bp | categoria       |    valor |
+#  |----:|:------------|:------------------|-------:|------------:|:----------------|---------:|
+#  | 477 | ARG         | Argentina         |   2020 |           6 | Cueros y pieles | 0.242406 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='sector_bp', axis=1)
 #  Index: 13 entries, 477 to 2607
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       13 non-null     int64  
-#   1   geocodigo  13 non-null     object 
-#   2   categoria  13 non-null     object 
-#   3   valor      13 non-null     float64
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigo        13 non-null     object 
+#   1   geonombreFundar  13 non-null     object 
+#   2   anio             13 non-null     int64  
+#   3   categoria        13 non-null     object 
+#   4   valor            13 non-null     float64
 #  
-#  |     |   anio | geocodigo   | categoria       |    valor |
-#  |----:|-------:|:------------|:----------------|---------:|
-#  | 477 |   2020 | ARG         | Cueros y pieles | 0.242406 |
+#  |     | geocodigo   | geonombreFundar   |   anio | categoria       |    valor |
+#  |----:|:------------|:------------------|-------:|:----------------|---------:|
+#  | 477 | ARG         | Argentina         |   2020 | Cueros y pieles | 0.242406 |
 #  
 #  ------------------------------
 #  
