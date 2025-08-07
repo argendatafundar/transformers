098cfd9f36d8e77f106ba71723b920b6c59b1601
jgjuara <jg.juara@gmail.com>
2025-08-07 09:57:54 -0300

    Updated COMEXT/COMEXT_g02_transformer.py

--- COMEXT/COMEXT_g02_transformer.py
+++ COMEXT/COMEXT_g02_transformer.py
@@ -4,35 +4,8 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
-    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
 
 @transformer.convert
@@ -41,17 +14,42 @@ def sort_values_by_comparison(df, colname: str, precedence: dict):
     df.loc[:, mapcol] = df[colname].map(precedence)
     df = df.sort_values(by=['anio', mapcol])
     return df.drop(mapcol, axis=1)
+
+@transformer.convert
+def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
+    return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='countryname', axis=1),
-	wide_to_long(primary_keys=['anio', 'iso3'], value_name='valor', var_name='indicador'),
+	to_pandas(dummy=True),
+	drop_col(col='geocodigoFundar', axis=1),
+	drop_col(col='countryname', axis=1),
+	wide_to_long(primary_keys=['anio', 'geonombreFundar'], value_name='valor', var_name='indicador'),
 	replace_value(col='indicador', curr_value='exportsconstant_goods_v2', new_value='Bienes'),
 	replace_value(col='indicador', curr_value='exportsconstant_servi_v2', new_value='Servicios'),
-	query(condition='iso3 == "ARG"'),
-	drop_col(col='iso3', axis=1),
+	query(condition='geonombreFundar == "Argentina"'),
+	drop_col(col='geonombreFundar', axis=1),
 	drop_na(col='valor'),
 	sort_values_by_comparison(colname='indicador', precedence={'Bienes': 0, 'Servicios': 1})
 )
@@ -59,19 +57,41 @@ drop_col(col='countryname', axis=1),
 
 
 #  start()
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
+#  RangeIndex: 16960 entries, 0 to 16959
+#  Data columns (total 6 columns):
+#   #   Column                    Non-Null Count  Dtype  
+#  ---  ------                    --------------  -----  
+#   0   geocodigoFundar           16960 non-null  object 
+#   1   geonombreFundar           16960 non-null  object 
+#   2   anio                      16960 non-null  int64  
+#   3   countryname               16960 non-null  object 
+#   4   exportsconstant_goods_v2  6251 non-null   float64
+#   5   exportsconstant_servi_v2  6251 non-null   float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | countryname   |   exportsconstant_goods_v2 |   exportsconstant_servi_v2 |
+#  |---:|:------------------|:------------------|-------:|:--------------|---------------------------:|---------------------------:|
+#  |  0 | ABW               | Aruba             |   2023 | Aruba         |                        nan |                        nan |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='geocodigoFundar', axis=1)
 #  RangeIndex: 16960 entries, 0 to 16959
 #  Data columns (total 5 columns):
 #   #   Column                    Non-Null Count  Dtype  
 #  ---  ------                    --------------  -----  
-#   0   anio                      16960 non-null  int64  
-#   1   iso3                      16960 non-null  object 
+#   0   geonombreFundar           16960 non-null  object 
+#   1   anio                      16960 non-null  int64  
 #   2   countryname               16960 non-null  object 
 #   3   exportsconstant_goods_v2  6251 non-null   float64
 #   4   exportsconstant_servi_v2  6251 non-null   float64
 #  
-#  |    |   anio | iso3   | countryname   |   exportsconstant_goods_v2 |   exportsconstant_servi_v2 |
-#  |---:|-------:|:-------|:--------------|---------------------------:|---------------------------:|
-#  |  0 |   2023 | ABW    | Aruba         |                        nan |                        nan |
+#  |    | geonombreFundar   |   anio | countryname   |   exportsconstant_goods_v2 |   exportsconstant_servi_v2 |
+#  |---:|:------------------|-------:|:--------------|---------------------------:|---------------------------:|
+#  |  0 | Aruba             |   2023 | Aruba         |                        nan |                        nan |
 #  
 #  ------------------------------
 #  
@@ -80,82 +100,82 @@ drop_col(col='countryname', axis=1),
 #  Data columns (total 4 columns):
 #   #   Column                    Non-Null Count  Dtype  
 #  ---  ------                    --------------  -----  
-#   0   anio                      16960 non-null  int64  
-#   1   iso3                      16960 non-null  object 
+#   0   geonombreFundar           16960 non-null  object 
+#   1   anio                      16960 non-null  int64  
 #   2   exportsconstant_goods_v2  6251 non-null   float64
 #   3   exportsconstant_servi_v2  6251 non-null   float64
 #  
-#  |    |   anio | iso3   |   exportsconstant_goods_v2 |   exportsconstant_servi_v2 |
-#  |---:|-------:|:-------|---------------------------:|---------------------------:|
-#  |  0 |   2023 | ABW    |                        nan |                        nan |
+#  |    | geonombreFundar   |   anio |   exportsconstant_goods_v2 |   exportsconstant_servi_v2 |
+#  |---:|:------------------|-------:|---------------------------:|---------------------------:|
+#  |  0 | Aruba             |   2023 |                        nan |                        nan |
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['anio', 'iso3'], value_name='valor', var_name='indicador')
+#  wide_to_long(primary_keys=['anio', 'geonombreFundar'], value_name='valor', var_name='indicador')
 #  RangeIndex: 33920 entries, 0 to 33919
 #  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       33920 non-null  int64  
-#   1   iso3       33920 non-null  object 
-#   2   indicador  33920 non-null  object 
-#   3   valor      12502 non-null  float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             33920 non-null  int64  
+#   1   geonombreFundar  33920 non-null  object 
+#   2   indicador        33920 non-null  object 
+#   3   valor            12502 non-null  float64
 #  
-#  |    |   anio | iso3   | indicador                |   valor |
-#  |---:|-------:|:-------|:-------------------------|--------:|
-#  |  0 |   2023 | ABW    | exportsconstant_goods_v2 |     nan |
+#  |    |   anio | geonombreFundar   | indicador                |   valor |
+#  |---:|-------:|:------------------|:-------------------------|--------:|
+#  |  0 |   2023 | Aruba             | exportsconstant_goods_v2 |     nan |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='exportsconstant_goods_v2', new_value='Bienes')
 #  RangeIndex: 33920 entries, 0 to 33919
 #  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       33920 non-null  int64  
-#   1   iso3       33920 non-null  object 
-#   2   indicador  33920 non-null  object 
-#   3   valor      12502 non-null  float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             33920 non-null  int64  
+#   1   geonombreFundar  33920 non-null  object 
+#   2   indicador        33920 non-null  object 
+#   3   valor            12502 non-null  float64
 #  
-#  |    |   anio | iso3   | indicador   |   valor |
-#  |---:|-------:|:-------|:------------|--------:|
-#  |  0 |   2023 | ABW    | Bienes      |     nan |
+#  |    |   anio | geonombreFundar   | indicador   |   valor |
+#  |---:|-------:|:------------------|:------------|--------:|
+#  |  0 |   2023 | Aruba             | Bienes      |     nan |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='exportsconstant_servi_v2', new_value='Servicios')
 #  RangeIndex: 33920 entries, 0 to 33919
 #  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       33920 non-null  int64  
-#   1   iso3       33920 non-null  object 
-#   2   indicador  33920 non-null  object 
-#   3   valor      12502 non-null  float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             33920 non-null  int64  
+#   1   geonombreFundar  33920 non-null  object 
+#   2   indicador        33920 non-null  object 
+#   3   valor            12502 non-null  float64
 #  
-#  |    |   anio | iso3   | indicador   |   valor |
-#  |---:|-------:|:-------|:------------|--------:|
-#  |  0 |   2023 | ABW    | Bienes      |     nan |
+#  |    |   anio | geonombreFundar   | indicador   |   valor |
+#  |---:|-------:|:------------------|:------------|--------:|
+#  |  0 |   2023 | Aruba             | Bienes      |     nan |
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3 == "ARG"')
+#  query(condition='geonombreFundar == "Argentina"')
 #  Index: 128 entries, 576 to 17599
 #  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       128 non-null    int64  
-#   1   iso3       128 non-null    object 
-#   2   indicador  128 non-null    object 
-#   3   valor      94 non-null     float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             128 non-null    int64  
+#   1   geonombreFundar  128 non-null    object 
+#   2   indicador        128 non-null    object 
+#   3   valor            94 non-null     float64
 #  
-#  |     |   anio | iso3   | indicador   |   valor |
-#  |----:|-------:|:-------|:------------|--------:|
-#  | 576 |   2023 | ARG    | Bienes      |     nan |
+#  |     |   anio | geonombreFundar   | indicador   |   valor |
+#  |----:|-------:|:------------------|:------------|--------:|
+#  | 576 |   2023 | Argentina         | Bienes      |     nan |
 #  
 #  ------------------------------
 #  
-#  drop_col(col='iso3', axis=1)
+#  drop_col(col='geonombreFundar', axis=1)
 #  Index: 128 entries, 576 to 17599
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
