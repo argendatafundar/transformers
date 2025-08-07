5562bcbdbdfdf8cbe5dde931085e5490d666f1e1
jgjuara <jg.juara@gmail.com>
2025-08-07 16:32:30 -0300

    Updated COMEXT/COMEXT_g11_transformer.py

--- COMEXT/COMEXT_g11_transformer.py
+++ COMEXT/COMEXT_g11_transformer.py
@@ -4,21 +4,9 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
 
 @transformer.convert
 def rename_cols(df: DataFrame, map):
@@ -26,19 +14,13 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 
 @transformer.convert
 def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
@@ -49,8 +31,9 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='iso3 == "ARG"'),
-	drop_col(col='iso3', axis=1),
+	to_pandas(dummy=True),
+	query(condition='geocodigoFundar == "ARG"'),
+	drop_col(col='geocodigoFundar', axis=1),
 	drop_col(col='location_name_short_en', axis=1),
 	drop_col(col='sitc_2_1_cod', axis=1),
 	rename_cols(map={'year': 'anio', 'sitc_product_name_es': 'indicador', 'import_value_pc': 'valor'}),
@@ -63,161 +46,175 @@ query(condition='iso3 == "ARG"'),
 
 
 #  start()
-#  RangeIndex: 122560 entries, 0 to 122559
-#  Data columns (total 6 columns):
-#   #   Column                  Non-Null Count   Dtype  
-#  ---  ------                  --------------   -----  
-#   0   year                    122560 non-null  int64  
-#   1   iso3                    122560 non-null  object 
-#   2   location_name_short_en  122560 non-null  object 
-#   3   sitc_2_1_cod            122560 non-null  int64  
-#   4   sitc_product_name_es    122560 non-null  object 
-#   5   import_value_pc         122360 non-null  float64
-#  
-#  |    |   year | iso3   | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
-#  |---:|-------:|:-------|:-------------------------|---------------:|:-----------------------|------------------:|
-#  |  0 |   1962 | AFG    | Afghanistan              |              0 | Productos alimenticios |           7.95848 |
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3 == "ARG"')
+#  to_pandas(dummy=True)
+#  RangeIndex: 122560 entries, 0 to 122559
+#  Data columns (total 7 columns):
+#   #   Column                  Non-Null Count   Dtype  
+#  ---  ------                  --------------   -----  
+#   0   geocodigoFundar         122560 non-null  object 
+#   1   geonombreFundar         122560 non-null  object 
+#   2   year                    122560 non-null  int64  
+#   3   location_name_short_en  122560 non-null  object 
+#   4   sitc_2_1_cod            122560 non-null  int64  
+#   5   sitc_product_name_es    122560 non-null  object 
+#   6   import_value_pc         122360 non-null  float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   year | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|:------------------|:------------------|-------:|:-------------------------|---------------:|:-----------------------|------------------:|
+#  |  0 | AFG               | Afganistán        |   1962 | Afghanistan              |              0 | Productos alimenticios |           7.95848 |
+#  
+#  ------------------------------
+#  
+#  query(condition='geocodigoFundar == "ARG"')
+#  Index: 600 entries, 50 to 120309
+#  Data columns (total 7 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   geocodigoFundar         600 non-null    object 
+#   1   geonombreFundar         600 non-null    object 
+#   2   year                    600 non-null    int64  
+#   3   location_name_short_en  600 non-null    object 
+#   4   sitc_2_1_cod            600 non-null    int64  
+#   5   sitc_product_name_es    600 non-null    object 
+#   6   import_value_pc         600 non-null    float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   year | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|:------------------|:------------------|-------:|:-------------------------|---------------:|:-----------------------|------------------:|
+#  | 50 | ARG               | Argentina         |   1962 | Argentina                |              0 | Productos alimenticios |           2.94598 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='geocodigoFundar', axis=1)
 #  Index: 600 entries, 50 to 120309
 #  Data columns (total 6 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   year                    600 non-null    int64  
-#   1   iso3                    600 non-null    object 
+#   0   geonombreFundar         600 non-null    object 
+#   1   year                    600 non-null    int64  
 #   2   location_name_short_en  600 non-null    object 
 #   3   sitc_2_1_cod            600 non-null    int64  
 #   4   sitc_product_name_es    600 non-null    object 
 #   5   import_value_pc         600 non-null    float64
 #  
-#  |    |   year | iso3   | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
-#  |---:|-------:|:-------|:-------------------------|---------------:|:-----------------------|------------------:|
-#  | 50 |   1962 | ARG    | Argentina                |              0 | Productos alimenticios |           2.94598 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3', axis=1)
-#  Index: 600 entries, 50 to 120309
-#  Data columns (total 5 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   year                    600 non-null    int64  
-#   1   location_name_short_en  600 non-null    object 
-#   2   sitc_2_1_cod            600 non-null    int64  
-#   3   sitc_product_name_es    600 non-null    object 
-#   4   import_value_pc         600 non-null    float64
-#  
-#  |    |   year | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
-#  |---:|-------:|:-------------------------|---------------:|:-----------------------|------------------:|
-#  | 50 |   1962 | Argentina                |              0 | Productos alimenticios |           2.94598 |
+#  |    | geonombreFundar   |   year | location_name_short_en   |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|:------------------|-------:|:-------------------------|---------------:|:-----------------------|------------------:|
+#  | 50 | Argentina         |   1962 | Argentina                |              0 | Productos alimenticios |           2.94598 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='location_name_short_en', axis=1)
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 4 columns):
+#  Data columns (total 5 columns):
 #   #   Column                Non-Null Count  Dtype  
 #  ---  ------                --------------  -----  
-#   0   year                  600 non-null    int64  
-#   1   sitc_2_1_cod          600 non-null    int64  
-#   2   sitc_product_name_es  600 non-null    object 
-#   3   import_value_pc       600 non-null    float64
+#   0   geonombreFundar       600 non-null    object 
+#   1   year                  600 non-null    int64  
+#   2   sitc_2_1_cod          600 non-null    int64  
+#   3   sitc_product_name_es  600 non-null    object 
+#   4   import_value_pc       600 non-null    float64
 #  
-#  |    |   year |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
-#  |---:|-------:|---------------:|:-----------------------|------------------:|
-#  | 50 |   1962 |              0 | Productos alimenticios |           2.94598 |
+#  |    | geonombreFundar   |   year |   sitc_2_1_cod | sitc_product_name_es   |   import_value_pc |
+#  |---:|:------------------|-------:|---------------:|:-----------------------|------------------:|
+#  | 50 | Argentina         |   1962 |              0 | Productos alimenticios |           2.94598 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='sitc_2_1_cod', axis=1)
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column                Non-Null Count  Dtype  
 #  ---  ------                --------------  -----  
-#   0   year                  600 non-null    int64  
-#   1   sitc_product_name_es  600 non-null    object 
-#   2   import_value_pc       600 non-null    float64
+#   0   geonombreFundar       600 non-null    object 
+#   1   year                  600 non-null    int64  
+#   2   sitc_product_name_es  600 non-null    object 
+#   3   import_value_pc       600 non-null    float64
 #  
-#  |    |   year | sitc_product_name_es   |   import_value_pc |
-#  |---:|-------:|:-----------------------|------------------:|
-#  | 50 |   1962 | Productos alimenticios |           2.94598 |
+#  |    | geonombreFundar   |   year | sitc_product_name_es   |   import_value_pc |
+#  |---:|:------------------|-------:|:-----------------------|------------------:|
+#  | 50 | Argentina         |   1962 | Productos alimenticios |           2.94598 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'year': 'anio', 'sitc_product_name_es': 'indicador', 'import_value_pc': 'valor'})
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       600 non-null    int64  
-#   1   indicador  600 non-null    object 
-#   2   valor      600 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  600 non-null    object 
+#   1   anio             600 non-null    int64  
+#   2   indicador        600 non-null    object 
+#   3   valor            600 non-null    float64
 #  
-#  |    |   anio | indicador              |   valor |
-#  |---:|-------:|:-----------------------|--------:|
-#  | 50 |   1962 | Productos alimenticios | 2.94598 |
+#  |    | geonombreFundar   |   anio | indicador              |   valor |
+#  |---:|:------------------|-------:|:-----------------------|--------:|
+#  | 50 | Argentina         |   1962 | Productos alimenticios | 2.94598 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='Materiales crudos, no comestibles, excepto combustibles', new_value='Materiales crudos no comestibles')
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       600 non-null    int64  
-#   1   indicador  600 non-null    object 
-#   2   valor      600 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  600 non-null    object 
+#   1   anio             600 non-null    int64  
+#   2   indicador        600 non-null    object 
+#   3   valor            600 non-null    float64
 #  
-#  |    |   anio | indicador              |   valor |
-#  |---:|-------:|:-----------------------|--------:|
-#  | 50 |   1962 | Productos alimenticios | 2.94598 |
+#  |    | geonombreFundar   |   anio | indicador              |   valor |
+#  |---:|:------------------|-------:|:-----------------------|--------:|
+#  | 50 | Argentina         |   1962 | Productos alimenticios | 2.94598 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='Combustibles minerales, lubricantes y productos similares', new_value='Combustibles minerales, lubricantes y similares')
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       600 non-null    int64  
-#   1   indicador  600 non-null    object 
-#   2   valor      600 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  600 non-null    object 
+#   1   anio             600 non-null    int64  
+#   2   indicador        600 non-null    object 
+#   3   valor            600 non-null    float64
 #  
-#  |    |   anio | indicador              |   valor |
-#  |---:|-------:|:-----------------------|--------:|
-#  | 50 |   1962 | Productos alimenticios | 2.94598 |
+#  |    | geonombreFundar   |   anio | indicador              |   valor |
+#  |---:|:------------------|-------:|:-----------------------|--------:|
+#  | 50 | Argentina         |   1962 | Productos alimenticios | 2.94598 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='Artículos manufacturados, clasificados principalmente según el material', new_value='Artículos manufacturados según material')
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       600 non-null    int64  
-#   1   indicador  600 non-null    object 
-#   2   valor      600 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  600 non-null    object 
+#   1   anio             600 non-null    int64  
+#   2   indicador        600 non-null    object 
+#   3   valor            600 non-null    float64
 #  
-#  |    |   anio | indicador              |   valor |
-#  |---:|-------:|:-----------------------|--------:|
-#  | 50 |   1962 | Productos alimenticios | 2.94598 |
+#  |    | geonombreFundar   |   anio | indicador              |   valor |
+#  |---:|:------------------|-------:|:-----------------------|--------:|
+#  | 50 | Argentina         |   1962 | Productos alimenticios | 2.94598 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='Transacciones y mercaderías diversas, N. E. P.', new_value='Transacciones y mercaderías diversas')
 #  Index: 600 entries, 50 to 120309
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       600 non-null    int64  
-#   1   indicador  600 non-null    object 
-#   2   valor      600 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  600 non-null    object 
+#   1   anio             600 non-null    int64  
+#   2   indicador        600 non-null    object 
+#   3   valor            600 non-null    float64
 #  
-#  |    |   anio | indicador              |   valor |
-#  |---:|-------:|:-----------------------|--------:|
-#  | 50 |   1962 | Productos alimenticios | 2.94598 |
+#  |    | geonombreFundar   |   anio | indicador              |   valor |
+#  |---:|:------------------|-------:|:-----------------------|--------:|
+#  | 50 | Argentina         |   1962 | Productos alimenticios | 2.94598 |
 #  
 #  ------------------------------
 #  
