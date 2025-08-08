d504f47ae6df87f033e7f9ad896eebb15ff95f91
jgjuara <jg.juara@gmail.com>
2025-08-08 14:47:38 -0300

    Updated COMEXT/COMEXT_g18_transformer.py

--- COMEXT/COMEXT_g18_transformer.py
+++ COMEXT/COMEXT_g18_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -13,16 +18,8 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
 
 @transformer.convert
@@ -33,10 +30,12 @@ def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indi
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='iso3 == "ARG"'),
-	drop_col(col='iso3', axis=1),
+	to_pandas(dummy=True),
+	query(condition='geocodigoFundar == "ARG"'),
+	drop_col(col='geocodigoFundar', axis=1),
 	drop_col(col='location_name_short_en', axis=1),
 	drop_col(col='partner_name_short_en', axis=1),
+	drop_col(col='geonombreFundar', axis=1),
 	rename_cols(map={'partner_code': 'geocodigo'}),
 	wide_to_long(primary_keys=['geocodigo'], value_name='valor', var_name='indicador')
 )
@@ -44,116 +43,140 @@ query(condition='iso3 == "ARG"'),
 
 
 #  start()
-#  RangeIndex: 45686 entries, 0 to 45685
-#  Data columns (total 6 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   iso3                    45686 non-null  object 
-#   1   location_name_short_en  45686 non-null  object 
-#   2   partner_code            45686 non-null  object 
-#   3   partner_name_short_en   45686 non-null  object 
-#   4   export_value_pca        9952 non-null   float64
-#   5   export_value_pcb        35716 non-null  float64
-#  
-#  |    | iso3   | location_name_short_en   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
-#  |---:|:-------|:-------------------------|:---------------|:------------------------|-------------------:|-------------------:|
-#  |  0 | CHE    | Switzerland              | ANT            | Netherlands Antilles    |           0.061569 |                nan |
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3 == "ARG"')
-#  Index: 296 entries, 630 to 44591
+#  to_pandas(dummy=True)
+#  RangeIndex: 36868 entries, 0 to 36867
+#  Data columns (total 7 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   geocodigoFundar         36868 non-null  object 
+#   1   geonombreFundar         36868 non-null  object 
+#   2   location_name_short_en  36868 non-null  object 
+#   3   partner_code            36868 non-null  object 
+#   4   partner_name_short_en   36868 non-null  object 
+#   5   export_value_pca        9952 non-null   float64
+#   6   export_value_pcb        35716 non-null  float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   | location_name_short_en   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
+#  |---:|:------------------|:------------------|:-------------------------|:---------------|:------------------------|-------------------:|-------------------:|
+#  |  0 | AFG               | Afganistán        | Afghanistan              | ANT            | Netherlands Antilles    |                  0 |                nan |
+#  
+#  ------------------------------
+#  
+#  query(condition='geocodigoFundar == "ARG"')
+#  Index: 200 entries, 207 to 11025
+#  Data columns (total 7 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   geocodigoFundar         200 non-null    object 
+#   1   geonombreFundar         200 non-null    object 
+#   2   location_name_short_en  200 non-null    object 
+#   3   partner_code            200 non-null    object 
+#   4   partner_name_short_en   200 non-null    object 
+#   5   export_value_pca        105 non-null    float64
+#   6   export_value_pcb        191 non-null    float64
+#  
+#  |     | geocodigoFundar   | geonombreFundar   | location_name_short_en   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
+#  |----:|:------------------|:------------------|:-------------------------|:---------------|:------------------------|-------------------:|-------------------:|
+#  | 207 | ARG               | Argentina         | Argentina                | AGO            | Angola                  |           0.122579 |          0.0786303 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='geocodigoFundar', axis=1)
+#  Index: 200 entries, 207 to 11025
 #  Data columns (total 6 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   iso3                    296 non-null    object 
-#   1   location_name_short_en  296 non-null    object 
-#   2   partner_code            296 non-null    object 
-#   3   partner_name_short_en   296 non-null    object 
+#   0   geonombreFundar         200 non-null    object 
+#   1   location_name_short_en  200 non-null    object 
+#   2   partner_code            200 non-null    object 
+#   3   partner_name_short_en   200 non-null    object 
 #   4   export_value_pca        105 non-null    float64
 #   5   export_value_pcb        191 non-null    float64
 #  
-#  |     | iso3   | location_name_short_en   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
-#  |----:|:-------|:-------------------------|:---------------|:------------------------|-------------------:|-------------------:|
-#  | 630 | ARG    | Argentina                | RWA            | Rwanda                  |                nan |         0.00912515 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3', axis=1)
-#  Index: 296 entries, 630 to 44591
-#  Data columns (total 5 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   location_name_short_en  296 non-null    object 
-#   1   partner_code            296 non-null    object 
-#   2   partner_name_short_en   296 non-null    object 
-#   3   export_value_pca        105 non-null    float64
-#   4   export_value_pcb        191 non-null    float64
-#  
-#  |     | location_name_short_en   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
-#  |----:|:-------------------------|:---------------|:------------------------|-------------------:|-------------------:|
-#  | 630 | Argentina                | RWA            | Rwanda                  |                nan |         0.00912515 |
+#  |     | geonombreFundar   | location_name_short_en   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
+#  |----:|:------------------|:-------------------------|:---------------|:------------------------|-------------------:|-------------------:|
+#  | 207 | Argentina         | Argentina                | AGO            | Angola                  |           0.122579 |          0.0786303 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='location_name_short_en', axis=1)
-#  Index: 296 entries, 630 to 44591
-#  Data columns (total 4 columns):
+#  Index: 200 entries, 207 to 11025
+#  Data columns (total 5 columns):
 #   #   Column                 Non-Null Count  Dtype  
 #  ---  ------                 --------------  -----  
-#   0   partner_code           296 non-null    object 
-#   1   partner_name_short_en  296 non-null    object 
-#   2   export_value_pca       105 non-null    float64
-#   3   export_value_pcb       191 non-null    float64
+#   0   geonombreFundar        200 non-null    object 
+#   1   partner_code           200 non-null    object 
+#   2   partner_name_short_en  200 non-null    object 
+#   3   export_value_pca       105 non-null    float64
+#   4   export_value_pcb       191 non-null    float64
 #  
-#  |     | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
-#  |----:|:---------------|:------------------------|-------------------:|-------------------:|
-#  | 630 | RWA            | Rwanda                  |                nan |         0.00912515 |
+#  |     | geonombreFundar   | partner_code   | partner_name_short_en   |   export_value_pca |   export_value_pcb |
+#  |----:|:------------------|:---------------|:------------------------|-------------------:|-------------------:|
+#  | 207 | Argentina         | AGO            | Angola                  |           0.122579 |          0.0786303 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='partner_name_short_en', axis=1)
-#  Index: 296 entries, 630 to 44591
+#  Index: 200 entries, 207 to 11025
+#  Data columns (total 4 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   geonombreFundar   200 non-null    object 
+#   1   partner_code      200 non-null    object 
+#   2   export_value_pca  105 non-null    float64
+#   3   export_value_pcb  191 non-null    float64
+#  
+#  |     | geonombreFundar   | partner_code   |   export_value_pca |   export_value_pcb |
+#  |----:|:------------------|:---------------|-------------------:|-------------------:|
+#  | 207 | Argentina         | AGO            |           0.122579 |          0.0786303 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col='geonombreFundar', axis=1)
+#  Index: 200 entries, 207 to 11025
 #  Data columns (total 3 columns):
 #   #   Column            Non-Null Count  Dtype  
 #  ---  ------            --------------  -----  
-#   0   partner_code      296 non-null    object 
+#   0   partner_code      200 non-null    object 
 #   1   export_value_pca  105 non-null    float64
 #   2   export_value_pcb  191 non-null    float64
 #  
 #  |     | partner_code   |   export_value_pca |   export_value_pcb |
 #  |----:|:---------------|-------------------:|-------------------:|
-#  | 630 | RWA            |                nan |         0.00912515 |
+#  | 207 | AGO            |           0.122579 |          0.0786303 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'partner_code': 'geoselector'})
-#  Index: 296 entries, 630 to 44591
+#  rename_cols(map={'partner_code': 'geocodigo'})
+#  Index: 200 entries, 207 to 11025
 #  Data columns (total 3 columns):
 #   #   Column            Non-Null Count  Dtype  
 #  ---  ------            --------------  -----  
-#   0   geoselector       296 non-null    object 
+#   0   geocodigo         200 non-null    object 
 #   1   export_value_pca  105 non-null    float64
 #   2   export_value_pcb  191 non-null    float64
 #  
-#  |     | geoselector   |   export_value_pca |   export_value_pcb |
-#  |----:|:--------------|-------------------:|-------------------:|
-#  | 630 | RWA           |                nan |         0.00912515 |
+#  |     | geocodigo   |   export_value_pca |   export_value_pcb |
+#  |----:|:------------|-------------------:|-------------------:|
+#  | 207 | AGO         |           0.122579 |          0.0786303 |
 #  
 #  ------------------------------
 #  
-#  wide_to_long(primary_keys=['geoselector'], value_name='valor', var_name='indicador')
-#  RangeIndex: 592 entries, 0 to 591
+#  wide_to_long(primary_keys=['geocodigo'], value_name='valor', var_name='indicador')
+#  RangeIndex: 400 entries, 0 to 399
 #  Data columns (total 3 columns):
-#   #   Column       Non-Null Count  Dtype  
-#  ---  ------       --------------  -----  
-#   0   geoselector  592 non-null    object 
-#   1   indicador    592 non-null    object 
-#   2   valor        296 non-null    float64
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   geocodigo  400 non-null    object 
+#   1   indicador  400 non-null    object 
+#   2   valor      296 non-null    float64
 #  
-#  |    | geoselector   | indicador        |   valor |
-#  |---:|:--------------|:-----------------|--------:|
-#  |  0 | RWA           | export_value_pca |     nan |
+#  |    | geocodigo   | indicador        |    valor |
+#  |---:|:------------|:-----------------|---------:|
+#  |  0 | AGO         | export_value_pca | 0.122579 |
 #  
 #  ------------------------------
 #  
