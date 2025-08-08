9ba8e5284fb21f0bd3af88019828d910b1b0b9b9
jgjuara <jg.juara@gmail.com>
2025-08-08 15:02:39 -0300

    Updated COMEXT/COMEXT_g21_transformer.py

--- COMEXT/COMEXT_g21_transformer.py
+++ COMEXT/COMEXT_g21_transformer.py
@@ -14,6 +14,12 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
+def reemplazar_valor(df: DataFrame, col:str, query:str, nuevo_valor):
+    indice = df.query(query).index
+    df.loc[indice, col] = nuevo_valor
+    return df
+
+@transformer.convert
 def to_pandas(df: pl.DataFrame, dummy = True):
     df = df.to_pandas()
     return df
@@ -23,6 +29,7 @@ def to_pandas(df: pl.DataFrame, dummy = True):
 #  PIPELINE_START
 pipeline = chain(
 	to_pandas(dummy=True),
+	reemplazar_valor(col='descripcionpais', query='descripcionpais == "Resto"', nuevo_valor='Resto del mundo'),
 	rename_cols(map={'year': 'anio', 'geocodigoFundar': 'geocodigo', 'export_value_pc': 'valor'}),
 	query(condition='anio == anio.max()')
 )
@@ -35,49 +42,73 @@ pipeline = chain(
 #  
 #  to_pandas(dummy=True)
 #  RangeIndex: 168 entries, 0 to 167
-#  Data columns (total 4 columns):
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   geocodigoFundar  160 non-null    object 
-#   1   geonombreFundar  160 non-null    object 
-#   2   year             168 non-null    int64  
+#   0   year             168 non-null    int64  
+#   1   iso3             160 non-null    object 
+#   2   descripcionpais  168 non-null    object 
 #   3   export_value_pc  168 non-null    float64
+#   4   geocodigoFundar  160 non-null    object 
+#   5   geonombreFundar  160 non-null    object 
 #  
-#  |    | geocodigoFundar   | geonombreFundar   |   year |   export_value_pc |
-#  |---:|:------------------|:------------------|-------:|------------------:|
-#  |  0 | DEU               | Alemania          |   2015 |           1.84376 |
+#  |    |   year | iso3   | descripcionpais   |   export_value_pc | geocodigoFundar   | geonombreFundar   |
+#  |---:|-------:|:-------|:------------------|------------------:|:------------------|:------------------|
+#  |  0 |   2015 | DEU    | Alemania          |           1.84376 | DEU               | Alemania          |
+#  
+#  ------------------------------
+#  
+#  reemplazar_valor(col='descripcionpais', query='descripcionpais == "Resto"', nuevo_valor='Resto del mundo')
+#  RangeIndex: 168 entries, 0 to 167
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   year             168 non-null    int64  
+#   1   iso3             160 non-null    object 
+#   2   descripcionpais  168 non-null    object 
+#   3   export_value_pc  168 non-null    float64
+#   4   geocodigoFundar  160 non-null    object 
+#   5   geonombreFundar  160 non-null    object 
+#  
+#  |    |   year | iso3   | descripcionpais   |   export_value_pc | geocodigoFundar   | geonombreFundar   |
+#  |---:|-------:|:-------|:------------------|------------------:|:------------------|:------------------|
+#  |  0 |   2015 | DEU    | Alemania          |           1.84376 | DEU               | Alemania          |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'year': 'anio', 'geocodigoFundar': 'geocodigo', 'export_value_pc': 'valor'})
 #  RangeIndex: 168 entries, 0 to 167
-#  Data columns (total 4 columns):
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   geocodigo        160 non-null    object 
-#   1   geonombreFundar  160 non-null    object 
-#   2   anio             168 non-null    int64  
+#   0   anio             168 non-null    int64  
+#   1   iso3             160 non-null    object 
+#   2   descripcionpais  168 non-null    object 
 #   3   valor            168 non-null    float64
+#   4   geocodigo        160 non-null    object 
+#   5   geonombreFundar  160 non-null    object 
 #  
-#  |    | geocodigo   | geonombreFundar   |   anio |   valor |
-#  |---:|:------------|:------------------|-------:|--------:|
-#  |  0 | DEU         | Alemania          |   2015 | 1.84376 |
+#  |    |   anio | iso3   | descripcionpais   |   valor | geocodigo   | geonombreFundar   |
+#  |---:|-------:|:-------|:------------------|--------:|:------------|:------------------|
+#  |  0 |   2015 | DEU    | Alemania          | 1.84376 | DEU         | Alemania          |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio == anio.max()')
 #  Index: 21 entries, 147 to 167
-#  Data columns (total 4 columns):
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   geocodigo        20 non-null     object 
-#   1   geonombreFundar  20 non-null     object 
-#   2   anio             21 non-null     int64  
+#   0   anio             21 non-null     int64  
+#   1   iso3             20 non-null     object 
+#   2   descripcionpais  21 non-null     object 
 #   3   valor            21 non-null     float64
+#   4   geocodigo        20 non-null     object 
+#   5   geonombreFundar  20 non-null     object 
 #  
-#  |     | geocodigo   | geonombreFundar   |   anio |   valor |
-#  |----:|:------------|:------------------|-------:|--------:|
-#  | 147 | DEU         | Alemania          |   2022 | 2.15493 |
+#  |     |   anio | iso3   | descripcionpais   |   valor | geocodigo   | geonombreFundar   |
+#  |----:|-------:|:-------|:------------------|--------:|:------------|:------------------|
+#  | 147 |   2022 | DEU    | Alemania          | 2.15493 | DEU         | Alemania          |
 #  
 #  ------------------------------
 #  
