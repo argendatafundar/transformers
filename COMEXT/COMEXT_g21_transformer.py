e9fb588deca3287a6d5dcf3dc36f85bc88e1179a
jgjuara <jg.juara@gmail.com>
2025-08-08 14:58:44 -0300

    Updated COMEXT/COMEXT_g21_transformer.py

--- COMEXT/COMEXT_g21_transformer.py
+++ COMEXT/COMEXT_g21_transformer.py
@@ -14,12 +14,6 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def reemplazar_valor(df: DataFrame, col:str, query:str, nuevo_valor):
-    indice = df.query(query).index
-    df.loc[indice, col] = nuevo_valor
-    return df
-
-@transformer.convert
 def to_pandas(df: pl.DataFrame, dummy = True):
     df = df.to_pandas()
     return df
@@ -29,7 +23,6 @@ def to_pandas(df: pl.DataFrame, dummy = True):
 #  PIPELINE_START
 pipeline = chain(
 	to_pandas(dummy=True),
-	reemplazar_valor(col='geocodigoFundar', query='geonombreFundar == "Resto"', nuevo_valor='ROW'),
 	rename_cols(map={'year': 'anio', 'geocodigoFundar': 'geocodigo', 'export_value_pc': 'valor'}),
 	query(condition='anio == anio.max()')
 )
@@ -41,22 +34,6 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  to_pandas(dummy=True)
-#  RangeIndex: 168 entries, 0 to 167
-#  Data columns (total 4 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geocodigoFundar  160 non-null    object 
-#   1   geonombreFundar  160 non-null    object 
-#   2   year             168 non-null    int64  
-#   3   export_value_pc  168 non-null    float64
-#  
-#  |    | geocodigoFundar   | geonombreFundar   |   year |   export_value_pc |
-#  |---:|:------------------|:------------------|-------:|------------------:|
-#  |  0 | DEU               | Alemania          |   2015 |           1.84376 |
-#  
-#  ------------------------------
-#  
-#  reemplazar_valor(col='geocodigoFundar', query='geonombreFundar == "Resto"', nuevo_valor='ROW')
 #  RangeIndex: 168 entries, 0 to 167
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
