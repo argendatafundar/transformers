1993efb36c40f04c0ac5d3a9057690ba1148aa48
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-18 18:57:54 -0300

    Updated FISCAL/FISCAL_g06_transformer.py

--- FISCAL/FISCAL_g06_transformer.py
+++ FISCAL/FISCAL_g06_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	multiplicar_por_escalar(col='gasto_publico_porcentaje_consolidado', k=100)
 )
 #  PIPELINE_END
 
@@ -32,7 +33,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  multiplicar_por_escalar(col='gasto_publico_porcentaje_consolidado', k=100)
 #  RangeIndex: 3 entries, 0 to 2
 #  Data columns (total 4 columns):
 #   #   Column                                Non-Null Count  Dtype  
@@ -44,7 +45,7 @@ pipeline = chain(
 #  
 #  |    |   anio | nivel_gobierno   |   gasto_publico_porcentaje_pib |   gasto_publico_porcentaje_consolidado |
 #  |---:|-------:|:-----------------|-------------------------------:|---------------------------------------:|
-#  |  0 |   2023 | Nacional         |                        22.2951 |                               0.532529 |
+#  |  0 |   2023 | Nacional         |                        22.2951 |                                53.2529 |
 #  
 #  ------------------------------
 #  
