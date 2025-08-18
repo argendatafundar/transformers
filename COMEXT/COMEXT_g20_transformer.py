b5f3e847b08aa99cfecb4be2f3b6527da4ce8b65
jgjuara <jg.juara@gmail.com>
2025-08-18 15:42:20 -0300

    Updated COMEXT/COMEXT_g20_transformer.py

--- COMEXT/COMEXT_g20_transformer.py
+++ COMEXT/COMEXT_g20_transformer.py
@@ -4,19 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -28,112 +15,122 @@ def sort_values_by_comparison(df, colname: str, precedence: dict):
     df.sort_values(by=['anio', mapcol], inplace=True)
     df.drop(mapcol, inplace=True, axis=1)
     return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition="iso3 == 'ARG'"),
-	drop_col(col='iso3', axis=1),
+	to_pandas(dummy=True),
+	query(condition="geocodigoFundar == 'ARG'"),
 	drop_col(col='reportingeconomy', axis=1),
 	rename_cols(map={'productsector_agregado': 'indicador', 'export_value_pc': 'valor', 'year': 'anio'}),
-	sort_values_by_comparison(colname='indicador', precedence={'Viajes': 0, 'Otros servicios empresariales': 1, 'Servicios de telecomunicaciones, informática e información': 2, 'Transportes': 3, 'Resto': 4})
+	sort_values_by_comparison(colname='indicador', precedence={'Viajes': 0, 'Otros servicios empresariales': 1, 'Serv. de telecom., informática e información': 2, 'Transportes': 3, 'Resto': 4})
 )
 #  PIPELINE_END
 
 
 #  start()
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
 #  RangeIndex: 16314 entries, 0 to 16313
-#  Data columns (total 5 columns):
+#  Data columns (total 6 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   year                    16314 non-null  int64  
-#   1   iso3                    16314 non-null  object 
-#   2   reportingeconomy        16314 non-null  object 
-#   3   productsector_agregado  16314 non-null  object 
-#   4   export_value_pc         16312 non-null  float64
+#   0   geocodigoFundar         16314 non-null  object 
+#   1   geonombreFundar         16314 non-null  object 
+#   2   year                    16314 non-null  int64  
+#   3   reportingeconomy        16314 non-null  object 
+#   4   productsector_agregado  16314 non-null  object 
+#   5   export_value_pc         16312 non-null  float64
 #  
-#  |    |   year | iso3   | reportingeconomy                       | productsector_agregado        |   export_value_pc |
-#  |---:|-------:|:-------|:---------------------------------------|:------------------------------|------------------:|
-#  |  0 |   2005 | ABW    | Aruba, the Netherlands with respect to | Otros servicios empresariales |           9.33435 |
+#  |    | geocodigoFundar   | geonombreFundar   |   year | reportingeconomy                       | productsector_agregado        |   export_value_pc |
+#  |---:|:------------------|:------------------|-------:|:---------------------------------------|:------------------------------|------------------:|
+#  |  0 | ABW               | Aruba             |   2005 | Aruba, the Netherlands with respect to | Otros servicios empresariales |           9.33435 |
 #  
 #  ------------------------------
 #  
-#  query(condition="iso3 == 'ARG'")
+#  query(condition="geocodigoFundar == 'ARG'")
 #  Index: 90 entries, 23 to 15652
-#  Data columns (total 5 columns):
+#  Data columns (total 6 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   year                    90 non-null     int64  
-#   1   iso3                    90 non-null     object 
-#   2   reportingeconomy        90 non-null     object 
-#   3   productsector_agregado  90 non-null     object 
-#   4   export_value_pc         90 non-null     float64
+#   0   geocodigoFundar         90 non-null     object 
+#   1   geonombreFundar         90 non-null     object 
+#   2   year                    90 non-null     int64  
+#   3   reportingeconomy        90 non-null     object 
+#   4   productsector_agregado  90 non-null     object 
+#   5   export_value_pc         90 non-null     float64
 #  
-#  |    |   year | iso3   | reportingeconomy   | productsector_agregado        |   export_value_pc |
-#  |---:|-------:|:-------|:-------------------|:------------------------------|------------------:|
-#  | 23 |   2005 | ARG    | Argentina          | Otros servicios empresariales |           24.7445 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='iso3', axis=1)
-#  Index: 90 entries, 23 to 15652
-#  Data columns (total 4 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   year                    90 non-null     int64  
-#   1   reportingeconomy        90 non-null     object 
-#   2   productsector_agregado  90 non-null     object 
-#   3   export_value_pc         90 non-null     float64
-#  
-#  |    |   year | reportingeconomy   | productsector_agregado        |   export_value_pc |
-#  |---:|-------:|:-------------------|:------------------------------|------------------:|
-#  | 23 |   2005 | Argentina          | Otros servicios empresariales |           24.7445 |
+#  |    | geocodigoFundar   | geonombreFundar   |   year | reportingeconomy   | productsector_agregado        |   export_value_pc |
+#  |---:|:------------------|:------------------|-------:|:-------------------|:------------------------------|------------------:|
+#  | 23 | ARG               | Argentina         |   2005 | Argentina          | Otros servicios empresariales |           24.7445 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='reportingeconomy', axis=1)
 #  Index: 90 entries, 23 to 15652
-#  Data columns (total 3 columns):
+#  Data columns (total 5 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   year                    90 non-null     int64  
-#   1   productsector_agregado  90 non-null     object 
-#   2   export_value_pc         90 non-null     float64
+#   0   geocodigoFundar         90 non-null     object 
+#   1   geonombreFundar         90 non-null     object 
+#   2   year                    90 non-null     int64  
+#   3   productsector_agregado  90 non-null     object 
+#   4   export_value_pc         90 non-null     float64
 #  
-#  |    |   year | productsector_agregado        |   export_value_pc |
-#  |---:|-------:|:------------------------------|------------------:|
-#  | 23 |   2005 | Otros servicios empresariales |           24.7445 |
+#  |    | geocodigoFundar   | geonombreFundar   |   year | productsector_agregado        |   export_value_pc |
+#  |---:|:------------------|:------------------|-------:|:------------------------------|------------------:|
+#  | 23 | ARG               | Argentina         |   2005 | Otros servicios empresariales |           24.7445 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'productsector_agregado': 'indicador', 'export_value_pc': 'valor', 'year': 'anio'})
-#  Index: 90 entries, 27 to 15649
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       90 non-null     int64  
-#   1   indicador  90 non-null     object 
-#   2   valor      90 non-null     float64
+#  Index: 90 entries, 27 to 15650
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  90 non-null     object 
+#   1   geonombreFundar  90 non-null     object 
+#   2   anio             90 non-null     int64  
+#   3   indicador        90 non-null     object 
+#   4   valor            90 non-null     float64
 #  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  | 27 |   2005 | Viajes      | 42.2577 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | indicador   |   valor |
+#  |---:|:------------------|:------------------|-------:|:------------|--------:|
+#  | 27 | ARG               | Argentina         |   2005 | Viajes      | 42.2577 |
 #  
 #  ------------------------------
 #  
-#  sort_values_by_comparison(colname='indicador', precedence={'Viajes': 0, 'Otros servicios empresariales': 1, 'Servicios de telecomunicaciones, informática e información': 2, 'Transportes': 3, 'Resto': 4})
-#  Index: 90 entries, 27 to 15649
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       90 non-null     int64  
-#   1   indicador  90 non-null     object 
-#   2   valor      90 non-null     float64
+#  sort_values_by_comparison(colname='indicador', precedence={'Viajes': 0, 'Otros servicios empresariales': 1, 'Serv. de telecom., informática e información': 2, 'Transportes': 3, 'Resto': 4})
+#  Index: 90 entries, 27 to 15650
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  90 non-null     object 
+#   1   geonombreFundar  90 non-null     object 
+#   2   anio             90 non-null     int64  
+#   3   indicador        90 non-null     object 
+#   4   valor            90 non-null     float64
 #  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  | 27 |   2005 | Viajes      | 42.2577 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | indicador   |   valor |
+#  |---:|:------------------|:------------------|-------:|:------------|--------:|
+#  | 27 | ARG               | Argentina         |   2005 | Viajes      | 42.2577 |
 #  
 #  ------------------------------
 #  
