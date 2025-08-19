f8a2798736ddb1a39900143ab5007a0f777d061d
LMCappelletti <lcappelletti@fund.ar>
2025-08-19 15:36:30 -0300

    Updated DESHUM/DESHUM_g03_transformer.py

--- DESHUM/DESHUM_g03_transformer.py
+++ DESHUM/DESHUM_g03_transformer.py
@@ -11,13 +11,19 @@ def rename_cols(df: DataFrame, map):
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	rename_cols(map={'idh': 'valor'}),
-	drop_col(col=['geocodigoFundar', 'continente_fundar', 'es_agregacion'], axis=1)
+	drop_col(col=['geocodigoFundar', 'continente_fundar', 'es_agregacion'], axis=1),
+	query(condition='anio in [1990, 2001, 2011, 2022]')
 )
 #  PIPELINE_END
 
@@ -66,6 +72,21 @@ pipeline = chain(
 #   0   geonombreFundar  6171 non-null   object 
 #   1   anio             6171 non-null   int64  
 #   2   valor            6171 non-null   float64
+#  
+#  |    | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|-------:|--------:|
+#  |  0 | Afganistán        |   1990 |   0.284 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio in [1990, 2001, 2011, 2022]')
+#  Index: 747 entries, 0 to 6170
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geonombreFundar  747 non-null    object 
+#   1   anio             747 non-null    int64  
+#   2   valor            747 non-null    float64
 #  
 #  |    | geonombreFundar   |   anio |   valor |
 #  |---:|:------------------|-------:|--------:|
