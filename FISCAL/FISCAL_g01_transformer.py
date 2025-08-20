8541b23f8ec8845ff3ea00cb30c75da78d43cf93
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-20 11:05:53 -0300

    Updated FISCAL/FISCAL_g01_transformer.py

--- FISCAL/FISCAL_g01_transformer.py
+++ FISCAL/FISCAL_g01_transformer.py
@@ -18,31 +18,33 @@ pipeline = chain(
 
 #  start()
 #  RangeIndex: 151 entries, 0 to 150
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   pais                    151 non-null    object 
-#   1   codigo_pais             151 non-null    object 
+#   0   geocodigoFundar         151 non-null    object 
+#   1   geonombreFundar         151 non-null    object 
 #   2   gasto_publico_promedio  151 non-null    float64
+#   3   fuente                  151 non-null    object 
 #  
-#  |    | pais    | codigo_pais   |   gasto_publico_promedio |
-#  |---:|:--------|:--------------|-------------------------:|
-#  |  0 | Albania | ALB           |                  30.3236 |
+#  |    | geocodigoFundar   | geonombreFundar   |   gasto_publico_promedio | fuente   |
+#  |---:|:------------------|:------------------|-------------------------:|:---------|
+#  |  0 | KIR               | Kiribati          |                  97.4885 | FMI      |
 #  
 #  ------------------------------
 #  
 #  identity()
 #  RangeIndex: 151 entries, 0 to 150
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column                  Non-Null Count  Dtype  
 #  ---  ------                  --------------  -----  
-#   0   pais                    151 non-null    object 
-#   1   codigo_pais             151 non-null    object 
+#   0   geocodigoFundar         151 non-null    object 
+#   1   geonombreFundar         151 non-null    object 
 #   2   gasto_publico_promedio  151 non-null    float64
+#   3   fuente                  151 non-null    object 
 #  
-#  |    | pais    | codigo_pais   |   gasto_publico_promedio |
-#  |---:|:--------|:--------------|-------------------------:|
-#  |  0 | Albania | ALB           |                  30.3236 |
+#  |    | geocodigoFundar   | geonombreFundar   |   gasto_publico_promedio | fuente   |
+#  |---:|:------------------|:------------------|-------------------------:|:---------|
+#  |  0 | KIR               | Kiribati          |                  97.4885 | FMI      |
 #  
 #  ------------------------------
 #  
