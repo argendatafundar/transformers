eb5fc8a9e62ad5321bc4eb7284efc10804db5f24
LMCappelletti <lcappelletti@fund.ar>
2025-08-20 12:24:31 -0300

    Updated DESIGU/DESIGU_g04_transformer.py

--- DESIGU/DESIGU_g04_transformer.py
+++ DESIGU/DESIGU_g04_transformer.py
@@ -9,27 +9,27 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_na(df:DataFrame, cols:list):
-    return df.dropna(subset=cols)
-
-@transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
-    
+
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def drop_na(df, subset:str): 
+    return df.dropna(subset=subset, axis=0)
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'gini': 'valor', 'code': 'geocodigo'}),
+	rename_cols(map={'gini': 'valor', 'code': 'geocodigo'}),
 	drop_col(col='pais', axis=1),
-	drop_na(cols='valor'),
+	drop_na(subset=['valor']),
 	sort_values(how='ascending', by=['valor'])
 )
 #  PIPELINE_END
@@ -37,73 +37,78 @@ rename_cols(map={'gini': 'valor', 'code': 'geocodigo'}),
 
 #  start()
 #  RangeIndex: 162 entries, 0 to 161
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   code    162 non-null    object 
-#   1   pais    162 non-null    object 
-#   2   gini    153 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  162 non-null    object 
+#   1   geonombreFundar  162 non-null    object 
+#   2   pais             162 non-null    object 
+#   3   gini             153 non-null    float64
 #  
-#  |    | code   | pais    |   gini |
-#  |---:|:-------|:--------|-------:|
-#  |  0 | ISL    | Iceland |   24.3 |
+#  |    | geocodigoFundar   | geonombreFundar   | pais    |   gini |
+#  |---:|:------------------|:------------------|:--------|-------:|
+#  |  0 | ISL               | Islandia          | Iceland |   24.3 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'gini': 'valor', 'code': 'geocodigo'})
 #  RangeIndex: 162 entries, 0 to 161
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  162 non-null    object 
-#   1   pais       162 non-null    object 
-#   2   valor      153 non-null    float64
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  162 non-null    object 
+#   1   geonombreFundar  162 non-null    object 
+#   2   pais             162 non-null    object 
+#   3   valor            153 non-null    float64
 #  
-#  |    | geocodigo   | pais    |   valor |
-#  |---:|:------------|:--------|--------:|
-#  |  0 | ISL         | Iceland |    24.3 |
+#  |    | geocodigoFundar   | geonombreFundar   | pais    |   valor |
+#  |---:|:------------------|:------------------|:--------|--------:|
+#  |  0 | ISL               | Islandia          | Iceland |    24.3 |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='pais', axis=1)
 #  RangeIndex: 162 entries, 0 to 161
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  162 non-null    object 
-#   1   valor      153 non-null    float64
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  162 non-null    object 
+#   1   geonombreFundar  162 non-null    object 
+#   2   valor            153 non-null    float64
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | ISL         |    24.3 |
+#  |    | geocodigoFundar   | geonombreFundar   |   valor |
+#  |---:|:------------------|:------------------|--------:|
+#  |  0 | ISL               | Islandia          |    24.3 |
 #  
 #  ------------------------------
 #  
-#  drop_na(cols='valor')
+#  drop_na(subset=['valor'])
 #  Index: 153 entries, 0 to 161
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  153 non-null    object 
-#   1   valor      153 non-null    float64
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  153 non-null    object 
+#   1   geonombreFundar  153 non-null    object 
+#   2   valor            153 non-null    float64
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | ISL         |    24.3 |
+#  |    | geocodigoFundar   | geonombreFundar   |   valor |
+#  |---:|:------------------|:------------------|--------:|
+#  |  0 | ISL               | Islandia          |    24.3 |
 #  
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['valor'])
 #  RangeIndex: 153 entries, 0 to 152
-#  Data columns (total 2 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  153 non-null    object 
-#   1   valor      153 non-null    float64
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  153 non-null    object 
+#   1   geonombreFundar  153 non-null    object 
+#   2   valor            153 non-null    float64
 #  
-#  |    | geocodigo   |   valor |
-#  |---:|:------------|--------:|
-#  |  0 | ISL         |    24.3 |
+#  |    | geocodigoFundar   | geonombreFundar   |   valor |
+#  |---:|:------------------|:------------------|--------:|
+#  |  0 | ISL               | Islandia          |    24.3 |
 #  
 #  ------------------------------
 #  
