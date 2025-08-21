93104db421e9de8dba123e1bc2e3081b9e676a52
jgjuara <jg.juara@gmail.com>
2025-08-21 10:55:05 -0300

    Updated SALING/SALING_g01_transformer.py

--- SALING/SALING_g01_transformer.py
+++ SALING/SALING_g01_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
 def concatenar_columnas(df:DataFrame, cols:list, nueva_col:str, separtor:str = "-"):
     df[nueva_col] = df[cols].astype(str).agg(separtor.join, axis=1)
     return df
@@ -13,15 +18,16 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def to_pandas(df: DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'),
+	to_pandas(dummy=True),
+	concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'),
 	drop_col(col=['year', 'semestre'], axis=1),
 	rename_cols(map={'ipcf_index': 'valor'})
 )
@@ -29,17 +35,22 @@ concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'
 
 
 #  start()
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
 #  RangeIndex: 42 entries, 0 to 41
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
 #   0   year        42 non-null     int64  
 #   1   semestre    42 non-null     int64  
 #   2   ipcf_index  40 non-null     float64
+#   3   aniosem     42 non-null     object 
 #  
-#  |    |   year |   semestre |   ipcf_index |
-#  |---:|-------:|-----------:|-------------:|
-#  |  0 |   2003 |          2 |          100 |
+#  |    |   year |   semestre |   ipcf_index | aniosem   |
+#  |---:|-------:|-----------:|-------------:|:----------|
+#  |  0 |   2003 |          2 |          100 | 2003-2    |
 #  
 #  ------------------------------
 #  
