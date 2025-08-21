e781c4b2637ffc393e1df414482ee6d75ad3c98f
LMCappelletti <lcappelletti@fund.ar>
2025-08-21 11:40:42 -0300

    Updated DESIGU/DESIGU_g10_transformer.py

--- DESIGU/DESIGU_g10_transformer.py
+++ DESIGU/DESIGU_g10_transformer.py
@@ -4,22 +4,24 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
+def long_to_wide(df:DataFrame, index:list[str], columns:str, values:str):
+    df = df.pivot(index=index, columns=columns, values=values).reset_index()
+    df.index.name = None
+    df.columns.name = None
+    df.columns = [str(col) for col in df.columns]  # Convertir columnas a str
+    return df  
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
-	replace_value(col='categoria', curr_value='giniila', new_value='Gini ingresos laborales'),
-	replace_value(col='categoria', curr_value='brecha', new_value='Brecha salarial calificados/no calificados')
+	query(condition="variable in ('giniila','brecha')"),
+	long_to_wide(index=['ano'], columns='variable', values='valor')
 )
 #  PIPELINE_END
 
@@ -39,48 +41,33 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'ano': 'anio', 'variable': 'categoria'})
-#  RangeIndex: 66 entries, 0 to 65
+#  query(condition="variable in ('giniila','brecha')")
+#  Index: 66 entries, 0 to 65
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       66 non-null     int64  
-#   1   categoria  66 non-null     object 
-#   2   valor      66 non-null     float64
+#   #   Column    Non-Null Count  Dtype  
+#  ---  ------    --------------  -----  
+#   0   ano       66 non-null     int64  
+#   1   variable  66 non-null     object 
+#   2   valor     66 non-null     float64
 #  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | giniila     |   40.11 |
+#  |    |   ano | variable   |   valor |
+#  |---:|------:|:-----------|--------:|
+#  |  0 |  1992 | giniila    |   40.11 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='categoria', curr_value='giniila', new_value='Gini ingresos laborales')
-#  RangeIndex: 66 entries, 0 to 65
+#  long_to_wide(index=['ano'], columns='variable', values='valor')
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       66 non-null     int64  
-#   1   categoria  66 non-null     object 
-#   2   valor      66 non-null     float64
+#   #   Column   Non-Null Count  Dtype  
+#  ---  ------   --------------  -----  
+#   0   ano      33 non-null     int64  
+#   1   brecha   33 non-null     float64
+#   2   giniila  33 non-null     float64
 #  
-#  |    |   anio | categoria               |   valor |
-#  |---:|-------:|:------------------------|--------:|
-#  |  0 |   1992 | Gini ingresos laborales |   40.11 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='categoria', curr_value='brecha', new_value='Brecha salarial calificados/no calificados')
-#  RangeIndex: 66 entries, 0 to 65
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       66 non-null     int64  
-#   1   categoria  66 non-null     object 
-#   2   valor      66 non-null     float64
-#  
-#  |    |   anio | categoria               |   valor |
-#  |---:|-------:|:------------------------|--------:|
-#  |  0 |   1992 | Gini ingresos laborales |   40.11 |
+#  |    |   ano |   brecha |   giniila |
+#  |---:|------:|---------:|----------:|
+#  |  0 |  1992 |     1.96 |     40.11 |
 #  
 #  ------------------------------
 #  
