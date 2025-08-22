7e834094c0bb0247ababd6e921b424d4acb5728f
LMCappelletti <lcappelletti@fund.ar>
2025-08-22 17:05:02 -0300

    Updated DESIGU/DESIGU_g20_transformer.py

--- DESIGU/DESIGU_g20_transformer.py
+++ DESIGU/DESIGU_g20_transformer.py
@@ -12,36 +12,36 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'ano': 'anio', 'indice': 'valor'})
+	rename_cols(map={'ano': 'anio', 'indice': 'valor'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 43 entries, 0 to 42
+#  RangeIndex: 45 entries, 0 to 44
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   ano     43 non-null     int64  
-#   1   indice  43 non-null     float64
+#   0   ano     45 non-null     int64  
+#   1   indice  45 non-null     float64
 #  
 #  |    |   ano |   indice |
 #  |---:|------:|---------:|
-#  |  0 |  1980 |   0.6285 |
+#  |  0 |  1980 | 0.628549 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'ano': 'anio', 'indice': 'valor'})
-#  RangeIndex: 43 entries, 0 to 42
+#  RangeIndex: 45 entries, 0 to 44
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
 #  ---  ------  --------------  -----  
-#   0   anio    43 non-null     int64  
-#   1   valor   43 non-null     float64
+#   0   anio    45 non-null     int64  
+#   1   valor   45 non-null     float64
 #  
-#  |    |   anio |   valor |
-#  |---:|-------:|--------:|
-#  |  0 |   1980 |  0.6285 |
+#  |    |   anio |    valor |
+#  |---:|-------:|---------:|
+#  |  0 |   1980 | 0.628549 |
 #  
 #  ------------------------------
 #  
