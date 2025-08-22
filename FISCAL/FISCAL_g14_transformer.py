d561f325d09bf9c4bafa83a1a96945936a5051dc
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-22 11:28:35 -0300

    Updated FISCAL/FISCAL_g14_transformer.py

--- FISCAL/FISCAL_g14_transformer.py
+++ FISCAL/FISCAL_g14_transformer.py
@@ -4,14 +4,26 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def imput_value_by_condition(df:DataFrame, bool_mask:list[bool], col:str, value:Any):
+    df.loc[bool_mask,col] = value
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	imput_value_by_condition(bool_mask=0       True
+1       True
+2       True
+3       True
+4       True
+       ...  
+462     True
+463     True
+464    False
+465     True
+466     True
+Name: porcentaje, Length: 467, dtype: bool, col='programa_desc', value='Otros')
 )
 #  PIPELINE_END
 
@@ -31,7 +43,18 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  imput_value_by_condition(bool_mask=0       True
+#  1       True
+#  2       True
+#  3       True
+#  4       True
+#         ...  
+#  462     True
+#  463     True
+#  464    False
+#  465     True
+#  466     True
+#  Name: porcentaje, Length: 467, dtype: bool, col='programa_desc', value='Otros')
 #  RangeIndex: 467 entries, 0 to 466
 #  Data columns (total 3 columns):
 #   #   Column         Non-Null Count  Dtype  
@@ -40,9 +63,9 @@ pipeline = chain(
 #   1   programa_desc  467 non-null    object 
 #   2   porcentaje     467 non-null    float64
 #  
-#  |    | ppg_label   | programa_desc                                                               |   porcentaje |
-#  |---:|:------------|:----------------------------------------------------------------------------|-------------:|
-#  |  0 | No PPG      | Abordaje Integral de Enfermedades No Transmisibles y sus Factores de Riesgo |  0.000362015 |
+#  |    | ppg_label   | programa_desc   |   porcentaje |
+#  |---:|:------------|:----------------|-------------:|
+#  |  0 | No PPG      | Otros           |  0.000362015 |
 #  
 #  ------------------------------
 #  
