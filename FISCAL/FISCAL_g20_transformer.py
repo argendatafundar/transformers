412c5d4f597da6d6b139b8e6345811a1d059b2ed
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-22 12:15:50 -0300

    Updated FISCAL/FISCAL_g20_transformer.py

--- FISCAL/FISCAL_g20_transformer.py
+++ FISCAL/FISCAL_g20_transformer.py
@@ -4,11 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def to_pandas(df: DataFrame, dummy = True):
-    df = df.to_pandas()
-    return df
-
-@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -17,26 +12,21 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-	to_pandas(dummy=True),
 	query(condition='anio >= 2010 and anio <= 2018')
 )
 #  PIPELINE_END
 
 
 #  start()
-#  
-#  ------------------------------
-#  
-#  to_pandas(dummy=True)
-#  RangeIndex: 1615 entries, 0 to 1614
+#  RangeIndex: 1598 entries, 0 to 1597
 #  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   geocodigoFundar  1615 non-null   object 
-#   1   anio             1615 non-null   int64  
-#   2   valor            1615 non-null   float64
-#   3   fuente           1615 non-null   object 
-#   4   geonombreFundar  1615 non-null   object 
+#   0   geocodigoFundar  1598 non-null   object 
+#   1   anio             1598 non-null   int64  
+#   2   valor            1598 non-null   float64
+#   3   fuente           1598 non-null   object 
+#   4   geonombreFundar  1598 non-null   object 
 #  
 #  |    | geocodigoFundar   |   anio |   valor | fuente   | geonombreFundar   |
 #  |---:|:------------------|-------:|--------:|:---------|:------------------|
@@ -45,15 +35,15 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  query(condition='anio >= 2010 and anio <= 2018')
-#  Index: 463 entries, 0 to 1611
+#  Index: 446 entries, 0 to 1594
 #  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   geocodigoFundar  463 non-null    object 
-#   1   anio             463 non-null    int64  
-#   2   valor            463 non-null    float64
-#   3   fuente           463 non-null    object 
-#   4   geonombreFundar  463 non-null    object 
+#   0   geocodigoFundar  446 non-null    object 
+#   1   anio             446 non-null    int64  
+#   2   valor            446 non-null    float64
+#   3   fuente           446 non-null    object 
+#   4   geonombreFundar  446 non-null    object 
 #  
 #  |    | geocodigoFundar   |   anio |   valor | fuente   | geonombreFundar   |
 #  |---:|:------------------|-------:|--------:|:---------|:------------------|
