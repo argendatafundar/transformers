ede2b67b031ea710d5bc249c0ed0b838569789c5
LMCappelletti <lcappelletti@fund.ar>
2025-08-22 16:38:51 -0300

    Updated DESIGU/DESIGU_g18_transformer.py

--- DESIGU/DESIGU_g18_transformer.py
+++ DESIGU/DESIGU_g18_transformer.py
@@ -4,14 +4,18 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
-    return df
+def long_to_wide(df:DataFrame, index:list[str], columns:str, values:str):
+    df = df.pivot(index=index, columns=columns, values=values).reset_index()
+    df.index.name = None
+    df.columns.name = None
+    df.columns = [str(col) for col in df.columns]  # Convertir columnas a str
+    return df  
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	long_to_wide(index=['ano'], columns='variable', values='valor')
 )
 #  PIPELINE_END
 
@@ -31,18 +35,18 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 64 entries, 0 to 63
+#  long_to_wide(index=['ano'], columns='variable', values='valor')
+#  RangeIndex: 32 entries, 0 to 31
 #  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype  
-#  ---  ------    --------------  -----  
-#   0   ano       64 non-null     int64  
-#   1   variable  64 non-null     object 
-#   2   valor     64 non-null     float64
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   ano           32 non-null     int64  
+#   1   brechagbanea  32 non-null     float64
+#   2   cv            32 non-null     float64
 #  
-#  |    |   ano | variable   |    valor |
-#  |---:|------:|:-----------|---------:|
-#  |  0 |  1992 | cv         | 0.264629 |
+#  |    |   ano |   brechagbanea |       cv |
+#  |---:|------:|---------------:|---------:|
+#  |  0 |  1992 |        1.77956 | 0.264629 |
 #  
 #  ------------------------------
 #  
