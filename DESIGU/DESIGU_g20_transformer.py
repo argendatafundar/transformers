714f6455c637b3ca98a3212dc6625731d503dfe5
LMCappelletti <lcappelletti@fund.ar>
2025-08-22 17:15:38 -0300

    Updated DESIGU/DESIGU_g20_transformer.py

--- DESIGU/DESIGU_g20_transformer.py
+++ DESIGU/DESIGU_g20_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -12,7 +17,8 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-	rename_cols(map={'ano': 'anio', 'indice': 'valor'})
+	rename_cols(map={'ano': 'anio', 'indice': 'valor'}),
+	query(condition='anio in [anio.min(), anio.max(), 1984]')
 )
 #  PIPELINE_END
 
@@ -38,6 +44,20 @@ pipeline = chain(
 #  ---  ------  --------------  -----  
 #   0   anio    45 non-null     int64  
 #   1   valor   45 non-null     float64
+#  
+#  |    |   anio |    valor |
+#  |---:|-------:|---------:|
+#  |  0 |   1980 | 0.628549 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio in [anio.min(), anio.max(), 1984]')
+#  Index: 3 entries, 0 to 44
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    3 non-null      int64  
+#   1   valor   3 non-null      float64
 #  
 #  |    |   anio |    valor |
 #  |---:|-------:|---------:|
