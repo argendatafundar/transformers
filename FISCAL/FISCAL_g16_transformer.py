3b0c562e4914ce782e4a2439a35828c426c76140
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-22 11:57:27 -0300

    Updated FISCAL/FISCAL_g16_transformer.py

--- FISCAL/FISCAL_g16_transformer.py
+++ FISCAL/FISCAL_g16_transformer.py
@@ -4,30 +4,27 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def to_pandas(df: DataFrame, dummy = True):
-    df = df.to_pandas()
-    return df
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    data = df.copy()
+    data[col] = data[col]*k
+    return data
 
 @transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	to_pandas(dummy=True),
-	multiplicar_por_escalar(col='participacion_gasto_publico_consolidado', k=100)
+	multiplicar_por_escalar(col='participacion_gasto_publico_consolidado', k=100),
+	replace_multiple_values(col='nivel_de_gobierno', replacements={'Municipal': 'Municip.', 'Provincial': 'Provincia', 'Nacional': 'Nación'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  
-#  ------------------------------
-#  
-#  to_pandas(dummy=True)
 #  RangeIndex: 132 entries, 0 to 131
 #  Data columns (total 3 columns):
 #   #   Column                                   Non-Null Count  Dtype  
@@ -38,7 +35,7 @@ pipeline = chain(
 #  
 #  |    |   anio | nivel_de_gobierno   |   participacion_gasto_publico_consolidado |
 #  |---:|-------:|:--------------------|------------------------------------------:|
-#  |  0 |   1980 | Nacional            |                                   65.8758 |
+#  |  0 |   1980 | Nacional            |                                  0.658758 |
 #  
 #  ------------------------------
 #  
@@ -53,7 +50,22 @@ pipeline = chain(
 #  
 #  |    |   anio | nivel_de_gobierno   |   participacion_gasto_publico_consolidado |
 #  |---:|-------:|:--------------------|------------------------------------------:|
-#  |  0 |   1980 | Nacional            |                                   65.8758 |
+#  |  0 |   1980 | Nación              |                                   65.8758 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='nivel_de_gobierno', replacements={'Municipal': 'Municip.', 'Provincial': 'Provincia', 'Nacional': 'Nación'})
+#  RangeIndex: 132 entries, 0 to 131
+#  Data columns (total 3 columns):
+#   #   Column                                   Non-Null Count  Dtype  
+#  ---  ------                                   --------------  -----  
+#   0   anio                                     132 non-null    int64  
+#   1   nivel_de_gobierno                        132 non-null    object 
+#   2   participacion_gasto_publico_consolidado  132 non-null    float64
+#  
+#  |    |   anio | nivel_de_gobierno   |   participacion_gasto_publico_consolidado |
+#  |---:|-------:|:--------------------|------------------------------------------:|
+#  |  0 |   1980 | Nación              |                                   65.8758 |
 #  
 #  ------------------------------
 #  
