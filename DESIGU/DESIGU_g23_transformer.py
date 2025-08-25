01d9b74dd283372d9f0cc7fa350e760829aad4fb
LMCappelletti <lcappelletti@fund.ar>
2025-08-25 09:21:18 -0300

    Updated DESIGU/DESIGU_g23_transformer.py

--- DESIGU/DESIGU_g23_transformer.py
+++ DESIGU/DESIGU_g23_transformer.py
@@ -4,30 +4,20 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
+	rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
 	replace_value(col='categoria', curr_value='quintil1', new_value='Quintil 1'),
 	replace_value(col='categoria', curr_value='quintil5', new_value='Quintil 5'),
 	replace_value(col='categoria', curr_value='media', new_value='Media')
@@ -36,77 +26,77 @@ rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
 
 
 #  start()
-#  RangeIndex: 90 entries, 0 to 89
+#  RangeIndex: 96 entries, 0 to 95
 #  Data columns (total 3 columns):
 #   #   Column    Non-Null Count  Dtype  
 #  ---  ------    --------------  -----  
-#   0   ano       90 non-null     int64  
-#   1   variable  90 non-null     object 
-#   2   valor     90 non-null     float64
+#   0   ano       96 non-null     int64  
+#   1   variable  96 non-null     object 
+#   2   valor     96 non-null     float64
 #  
 #  |    |   ano | variable   |   valor |
 #  |---:|------:|:-----------|--------:|
-#  |  0 |  1992 | quintil1   |  66.123 |
+#  |  0 |  1992 | quintil1   |    66.1 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'ano': 'anio', 'variable': 'categoria'})
-#  RangeIndex: 90 entries, 0 to 89
+#  RangeIndex: 96 entries, 0 to 95
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       90 non-null     int64  
-#   1   categoria  90 non-null     object 
-#   2   valor      90 non-null     float64
+#   0   anio       96 non-null     int64  
+#   1   categoria  96 non-null     object 
+#   2   valor      96 non-null     float64
 #  
 #  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | quintil1    |  66.123 |
+#  |  0 |   1992 | quintil1    |    66.1 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='categoria', curr_value='quintil1', new_value='Quintil 1')
-#  RangeIndex: 90 entries, 0 to 89
+#  RangeIndex: 96 entries, 0 to 95
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       90 non-null     int64  
-#   1   categoria  90 non-null     object 
-#   2   valor      90 non-null     float64
+#   0   anio       96 non-null     int64  
+#   1   categoria  96 non-null     object 
+#   2   valor      96 non-null     float64
 #  
 #  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | Quintil 1   |  66.123 |
+#  |  0 |   1992 | Quintil 1   |    66.1 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='categoria', curr_value='quintil5', new_value='Quintil 5')
-#  RangeIndex: 90 entries, 0 to 89
+#  RangeIndex: 96 entries, 0 to 95
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       90 non-null     int64  
-#   1   categoria  90 non-null     object 
-#   2   valor      90 non-null     float64
+#   0   anio       96 non-null     int64  
+#   1   categoria  96 non-null     object 
+#   2   valor      96 non-null     float64
 #  
 #  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | Quintil 1   |  66.123 |
+#  |  0 |   1992 | Quintil 1   |    66.1 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='categoria', curr_value='media', new_value='Media')
-#  RangeIndex: 90 entries, 0 to 89
+#  RangeIndex: 96 entries, 0 to 95
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       90 non-null     int64  
-#   1   categoria  90 non-null     object 
-#   2   valor      90 non-null     float64
+#   0   anio       96 non-null     int64  
+#   1   categoria  96 non-null     object 
+#   2   valor      96 non-null     float64
 #  
 #  |    |   anio | categoria   |   valor |
 #  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | Quintil 1   |  66.123 |
+#  |  0 |   1992 | Quintil 1   |    66.1 |
 #  
 #  ------------------------------
 #  
