74b4fcf9ba99de0c8edb46ccbb6d64a6f82d774c
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-26 12:57:15 -0300

    Updated PRECIO/PRECIO_g02_transformer.py

--- PRECIO/PRECIO_g02_transformer.py
+++ PRECIO/PRECIO_g02_transformer.py
@@ -4,18 +4,18 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rescale(df:DataFrame, group_cols:list[str], summarised_col:str) -> DataFrame:
-    df['value_scaled'] = df.groupby(group_cols)[summarised_col].transform(
-    lambda x: 100*(x/x.sum()))
-    return df
-
-@transformer.convert
 def agg_sum(df: DataFrame, key_cols:list[str], summarised_col:str) -> DataFrame:
     return df.groupby(key_cols)[summarised_col].sum().reset_index()
 
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
+
+@transformer.convert
+def rescale(df:DataFrame, group_cols:list[str], summarised_col:str) -> DataFrame:
+    df['value_scaled'] = df.groupby(group_cols)[summarised_col].transform(
+    lambda x: 100*(x/x.sum()))
+    return df
 
 @transformer.convert
 def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict)->DataFrame:
@@ -26,7 +26,7 @@ def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict)->DataFra
 
 #  PIPELINE_START
 pipeline = chain(
-	map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y prendas de vestir', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y prendas de vestir', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Transporte, comunicaciones y otros servicios', 'Transporte': 'Transporte, comunicaciones y otros servicios', 'Bienes y servicios varios': 'Transporte, comunicaciones y otros servicios'}),
+	map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y otros', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Comunicaciones', 'Transporte': 'Transporte', 'Bienes y servicios varios': 'Varios'}),
 	agg_sum(key_cols=['region', 'sector_agrupado'], summarised_col='valor'),
 	rescale(group_cols=['region'], summarised_col='valor'),
 	drop_col(col='valor', axis=1)
@@ -49,7 +49,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y prendas de vestir', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y prendas de vestir', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Transporte, comunicaciones y otros servicios', 'Transporte': 'Transporte, comunicaciones y otros servicios', 'Bienes y servicios varios': 'Transporte, comunicaciones y otros servicios'})
+#  map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y otros', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Comunicaciones', 'Transporte': 'Transporte', 'Bienes y servicios varios': 'Varios'})
 #  RangeIndex: 84 entries, 0 to 83
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
@@ -66,14 +66,14 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  agg_sum(key_cols=['region', 'sector_agrupado'], summarised_col='valor')
-#  RangeIndex: 42 entries, 0 to 41
+#  RangeIndex: 56 entries, 0 to 55
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   region           42 non-null     object 
-#   1   sector_agrupado  42 non-null     object 
-#   2   valor            42 non-null     float64
-#   3   value_scaled     42 non-null     float64
+#   0   region           56 non-null     object 
+#   1   sector_agrupado  56 non-null     object 
+#   2   valor            56 non-null     float64
+#   3   value_scaled     56 non-null     float64
 #  
 #  |    | region   | sector_agrupado             |   valor |   value_scaled |
 #  |---:|:---------|:----------------------------|--------:|---------------:|
@@ -82,14 +82,14 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  rescale(group_cols=['region'], summarised_col='valor')
-#  RangeIndex: 42 entries, 0 to 41
+#  RangeIndex: 56 entries, 0 to 55
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   region           42 non-null     object 
-#   1   sector_agrupado  42 non-null     object 
-#   2   valor            42 non-null     float64
-#   3   value_scaled     42 non-null     float64
+#   0   region           56 non-null     object 
+#   1   sector_agrupado  56 non-null     object 
+#   2   valor            56 non-null     float64
+#   3   value_scaled     56 non-null     float64
 #  
 #  |    | region   | sector_agrupado             |   valor |   value_scaled |
 #  |---:|:---------|:----------------------------|--------:|---------------:|
@@ -98,13 +98,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  drop_col(col='valor', axis=1)
-#  RangeIndex: 42 entries, 0 to 41
+#  RangeIndex: 56 entries, 0 to 55
 #  Data columns (total 3 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   region           42 non-null     object 
-#   1   sector_agrupado  42 non-null     object 
-#   2   value_scaled     42 non-null     float64
+#   0   region           56 non-null     object 
+#   1   sector_agrupado  56 non-null     object 
+#   2   value_scaled     56 non-null     float64
 #  
 #  |    | region   | sector_agrupado             |   value_scaled |
 #  |---:|:---------|:----------------------------|---------------:|
