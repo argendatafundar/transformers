a8cde54353b5b696e63465322a44c1ac6faff126
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-26 13:04:54 -0300

    Updated PRECIO/PRECIO_g02_transformer.py

--- PRECIO/PRECIO_g02_transformer.py
+++ PRECIO/PRECIO_g02_transformer.py
@@ -12,14 +12,21 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def rescale(df:DataFrame, group_cols:list[str], summarised_col:str) -> DataFrame:
-    df['value_scaled'] = df.groupby(group_cols)[summarised_col].transform(
-    lambda x: 100*(x/x.sum()))
+def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict)->DataFrame:
+    df[new_col] = df[curr_col].apply(lambda x: mapper.get(x, None))
     return df
 
 @transformer.convert
-def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict)->DataFrame:
-    df[new_col] = df[curr_col].apply(lambda x: mapper.get(x, None))
+def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
+    return df.sort_values(by=[col1,col2])
+
+@transformer.convert
+def rescale(df:DataFrame, group_cols:list[str], summarised_col:str) -> DataFrame:
+    df['value_scaled'] = df.groupby(group_cols)[summarised_col].transform(
+    lambda x: 100*(x/x.sum()))
     return df
 #  DEFINITIONS_END
 
@@ -29,7 +36,8 @@ pipeline = chain(
 	map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y otros', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Comunicaciones', 'Transporte': 'Transporte', 'Bienes y servicios varios': 'Varios'}),
 	agg_sum(key_cols=['region', 'sector_agrupado'], summarised_col='valor'),
 	rescale(group_cols=['region'], summarised_col='valor'),
-	drop_col(col='valor', axis=1)
+	drop_col(col='valor', axis=1),
+	ordenar_dos_columnas(col1='region', order1=['NEA', 'NOA', 'Pampeana', 'Cuyo', 'Patagonia', 'Nacional', 'GBA'], col2='sector_agrupado', order2=['Alimentos, bebidas y tabaco', 'Cultura y esparcimiento', 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado', 'Transporte', 'Educación y salud', 'Varios', 'Comunicaciones'])
 )
 #  PIPELINE_END
 
@@ -100,15 +108,30 @@ pipeline = chain(
 #  drop_col(col='valor', axis=1)
 #  RangeIndex: 56 entries, 0 to 55
 #  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   region           56 non-null     object 
-#   1   sector_agrupado  56 non-null     object 
-#   2   value_scaled     56 non-null     float64
+#   #   Column           Non-Null Count  Dtype   
+#  ---  ------           --------------  -----   
+#   0   region           56 non-null     category
+#   1   sector_agrupado  56 non-null     category
+#   2   value_scaled     56 non-null     float64 
 #  
 #  |    | region   | sector_agrupado             |   value_scaled |
 #  |---:|:---------|:----------------------------|---------------:|
 #  |  0 | Cuyo     | Alimentos, bebidas y tabaco |         32.032 |
+#  
+#  ------------------------------
+#  
+#  ordenar_dos_columnas(col1='region', order1=['NEA', 'NOA', 'Pampeana', 'Cuyo', 'Patagonia', 'Nacional', 'GBA'], col2='sector_agrupado', order2=['Alimentos, bebidas y tabaco', 'Cultura y esparcimiento', 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado', 'Transporte', 'Educación y salud', 'Varios', 'Comunicaciones'])
+#  Index: 56 entries, 16 to 9
+#  Data columns (total 3 columns):
+#   #   Column           Non-Null Count  Dtype   
+#  ---  ------           --------------  -----   
+#   0   region           56 non-null     category
+#   1   sector_agrupado  56 non-null     category
+#   2   value_scaled     56 non-null     float64 
+#  
+#  |    | region   | sector_agrupado             |   value_scaled |
+#  |---:|:---------|:----------------------------|---------------:|
+#  | 16 | NEA      | Alimentos, bebidas y tabaco |           38.9 |
 #  
 #  ------------------------------
 #  
