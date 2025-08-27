f75c07f020ca8a6c5d854e5da12b5893f07e782f
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-27 16:44:06 -0300

    Updated FISCAL/FISCAL_g01_transformer.py

--- FISCAL/FISCAL_g01_transformer.py
+++ FISCAL/FISCAL_g01_transformer.py
@@ -4,14 +4,20 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	replace_value(col='geonombreFundar', curr_value='Estados Unidos', new_value='EE.UU.', mapping=None)
 )
 #  PIPELINE_END
 
@@ -32,7 +38,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  replace_value(col='geonombreFundar', curr_value='Estados Unidos', new_value='EE.UU.', mapping=None)
 #  RangeIndex: 151 entries, 0 to 150
 #  Data columns (total 4 columns):
 #   #   Column                  Non-Null Count  Dtype  
