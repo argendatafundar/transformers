66b584f73719f8d05c159f7583a6ba63f3189b36
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-27 16:47:49 -0300

    Updated FISCAL/FISCAL_g02_transformer.py

--- FISCAL/FISCAL_g02_transformer.py
+++ FISCAL/FISCAL_g02_transformer.py
@@ -4,14 +4,20 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	replace_value(col='region', curr_value='América central', new_value='América Central', mapping=None)
 )
 #  PIPELINE_END
 
@@ -34,7 +40,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  replace_value(col='region', curr_value='América central', new_value='América Central', mapping=None)
 #  RangeIndex: 292 entries, 0 to 291
 #  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
