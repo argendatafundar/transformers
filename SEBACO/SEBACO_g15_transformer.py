72ffdd350edb64e5d2c2e660e8b4ef0f062c3350
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-08-29 17:28:58 -0300

    Updated SEBACO/SEBACO_g15_transformer.py

--- SEBACO/SEBACO_g15_transformer.py
+++ SEBACO/SEBACO_g15_transformer.py
@@ -4,18 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
-
-@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -26,6 +14,23 @@ def query(df: DataFrame, condition: str):
     return df
 
 @transformer.convert
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
+    return df
+
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
@@ -34,11 +39,11 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio.isin([2007, 2022])'),
+	query(condition='anio.isin([2007, 2014, 2022])'),
 	rename_cols(map={'sector': 'categoria', 'prop_mujeres': 'valor'}),
 	sort_values(how='ascending', by=['anio', 'categoria']),
 	multiplicar_por_escalar(col='valor', k=100),
-	replace_value(col='categoria', curr_value='Promedio economia', new_value='Promedio economía')
+	replace_value(col='categoria', curr_value='Promedio economia', new_value='Promedio economía', mapping=None)
 )
 #  PIPELINE_END
 
@@ -58,14 +63,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='anio.isin([2007, 2022])')
-#  Index: 4 entries, 11 to 53
+#  query(condition='anio.isin([2007, 2014, 2022])')
+#  Index: 6 entries, 11 to 53
 #  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   anio          4 non-null      int64  
-#   1   sector        4 non-null      object 
-#   2   prop_mujeres  4 non-null      float64
+#   0   anio          6 non-null      int64  
+#   1   sector        6 non-null      object 
+#   2   prop_mujeres  6 non-null      float64
 #  
 #  |    |   anio | sector            |   prop_mujeres |
 #  |---:|-------:|:------------------|---------------:|
@@ -74,13 +79,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  rename_cols(map={'sector': 'categoria', 'prop_mujeres': 'valor'})
-#  Index: 4 entries, 11 to 53
+#  Index: 6 entries, 11 to 53
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4 non-null      int64  
-#   1   categoria  4 non-null      object 
-#   2   valor      4 non-null      float64
+#   0   anio       6 non-null      int64  
+#   1   categoria  6 non-null      object 
+#   2   valor      6 non-null      float64
 #  
 #  |    |   anio | categoria         |    valor |
 #  |---:|-------:|:------------------|---------:|
@@ -89,13 +94,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  sort_values(how='ascending', by=['anio', 'categoria'])
-#  RangeIndex: 4 entries, 0 to 3
+#  RangeIndex: 6 entries, 0 to 5
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4 non-null      int64  
-#   1   categoria  4 non-null      object 
-#   2   valor      4 non-null      float64
+#   0   anio       6 non-null      int64  
+#   1   categoria  6 non-null      object 
+#   2   valor      6 non-null      float64
 #  
 #  |    |   anio | categoria         |   valor |
 #  |---:|-------:|:------------------|--------:|
@@ -104,13 +109,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 4 entries, 0 to 3
+#  RangeIndex: 6 entries, 0 to 5
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4 non-null      int64  
-#   1   categoria  4 non-null      object 
-#   2   valor      4 non-null      float64
+#   0   anio       6 non-null      int64  
+#   1   categoria  6 non-null      object 
+#   2   valor      6 non-null      float64
 #  
 #  |    |   anio | categoria         |   valor |
 #  |---:|-------:|:------------------|--------:|
@@ -118,14 +123,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='categoria', curr_value='Promedio economia', new_value='Promedio economía')
-#  RangeIndex: 4 entries, 0 to 3
+#  replace_value(col='categoria', curr_value='Promedio economia', new_value='Promedio economía', mapping=None)
+#  RangeIndex: 6 entries, 0 to 5
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4 non-null      int64  
-#   1   categoria  4 non-null      object 
-#   2   valor      4 non-null      float64
+#   0   anio       6 non-null      int64  
+#   1   categoria  6 non-null      object 
+#   2   valor      6 non-null      float64
 #  
 #  |    |   anio | categoria         |   valor |
 #  |---:|-------:|:------------------|--------:|
