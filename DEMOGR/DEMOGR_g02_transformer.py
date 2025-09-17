927f8b064f2c65aaefa414c49d44b7b6ae6265f6
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-17 15:20:14 -0300

    Updated DEMOGR/DEMOGR_g02_transformer.py

--- DEMOGR/DEMOGR_g02_transformer.py
+++ DEMOGR/DEMOGR_g02_transformer.py
@@ -19,29 +19,31 @@ pipeline = chain(
 
 #  start()
 #  RangeIndex: 156 entries, 0 to 155
-#  Data columns (total 2 columns):
+#  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
 #   0   anio          156 non-null    int64  
 #   1   edad_mediana  155 non-null    float64
+#   2   fuente        156 non-null    object 
 #  
-#  |    |   anio |   edad_mediana |
-#  |---:|-------:|---------------:|
-#  |  0 |   1869 |        16.5049 |
+#  |    |   anio |   edad_mediana | fuente   |
+#  |---:|-------:|---------------:|:---------|
+#  |  0 |   1869 |        16.5049 | INDEC    |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio <= 2025')
 #  Index: 80 entries, 0 to 79
-#  Data columns (total 2 columns):
+#  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
 #   0   anio          80 non-null     int64  
 #   1   edad_mediana  80 non-null     float64
+#   2   fuente        80 non-null     object 
 #  
-#  |    |   anio |   edad_mediana |
-#  |---:|-------:|---------------:|
-#  |  0 |   1869 |        16.5049 |
+#  |    |   anio |   edad_mediana | fuente   |
+#  |---:|-------:|---------------:|:---------|
+#  |  0 |   1869 |        16.5049 | INDEC    |
 #  
 #  ------------------------------
 #  
