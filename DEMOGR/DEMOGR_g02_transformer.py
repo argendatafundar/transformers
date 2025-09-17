7c50e92959c5f3a1d33036da2f01f03f3175b629
Ignacio Ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-17 12:06:37 -0300

    Add files via upload

---
+++ DEMOGR/DEMOGR_g02_transformer.py
@@ -0,0 +1,47 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	query(condition='anio <= 2025')
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 156 entries, 0 to 155
+#  Data columns (total 2 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   anio          156 non-null    int64  
+#   1   edad_mediana  155 non-null    float64
+#  
+#  |    |   anio |   edad_mediana |
+#  |---:|-------:|---------------:|
+#  |  0 |   1869 |        16.5049 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio <= 2025')
+#  Index: 80 entries, 0 to 79
+#  Data columns (total 2 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   anio          80 non-null     int64  
+#   1   edad_mediana  80 non-null     float64
+#  
+#  |    |   anio |   edad_mediana |
+#  |---:|-------:|---------------:|
+#  |  0 |   1869 |        16.5049 |
+#  
+#  ------------------------------
+#  
