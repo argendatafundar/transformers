bd4252c5686b2408ff2a2cd8afe76dc0098b5dfd
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-19 15:50:58 -0300

    Updated DEMOGR/DEMOGR_g14_transformer.py

--- DEMOGR/DEMOGR_g14_transformer.py
+++ DEMOGR/DEMOGR_g14_transformer.py
@@ -13,7 +13,7 @@ def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
 #  PIPELINE_START
 pipeline = chain(
 	replace_value(col='fuente', curr_value='World Population Prospects (UN)', new_value='WPP'),
-	replace_value(col='fuente', curr_value='Lattes et al (1975)', new_value='Lattes et al. (1975)')
+	replace_value(col='fuente', curr_value='Lattes et al (1975)', new_value='Lattes (1975)')
 )
 #  PIPELINE_END
 
@@ -48,7 +48,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='fuente', curr_value='Lattes et al (1975)', new_value='Lattes et al. (1975)')
+#  replace_value(col='fuente', curr_value='Lattes et al (1975)', new_value='Lattes (1975)')
 #  RangeIndex: 92 entries, 0 to 91
 #  Data columns (total 3 columns):
 #   #   Column               Non-Null Count  Dtype  
@@ -57,9 +57,9 @@ pipeline = chain(
 #   1   tasa_migracion_neta  92 non-null     float64
 #   2   fuente               92 non-null     object 
 #  
-#  |    |   anio |   tasa_migracion_neta | fuente               |
-#  |---:|-------:|----------------------:|:---------------------|
-#  |  0 |   1870 |               1.02304 | Lattes et al. (1975) |
+#  |    |   anio |   tasa_migracion_neta | fuente        |
+#  |---:|-------:|----------------------:|:--------------|
+#  |  0 |   1870 |               1.02304 | Lattes (1975) |
 #  
 #  ------------------------------
 #  
