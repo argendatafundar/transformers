1ab5de461bab0cc351c8a8bac05b408488ed2c49
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-19 18:49:39 -0300

    Updated DEMOGR/DEMOGR_g03_transformer.py

--- DEMOGR/DEMOGR_g03_transformer.py
+++ DEMOGR/DEMOGR_g03_transformer.py
@@ -18,34 +18,34 @@ pipeline = chain(
 
 
 #  start()
-#  RangeIndex: 35787 entries, 0 to 35786
+#  RangeIndex: 36089 entries, 0 to 36088
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             35787 non-null  int64  
-#   1   geocodigoFundar  35787 non-null  object 
-#   2   edad_mediana     35787 non-null  float64
-#   3   geonombreFundar  35787 non-null  object 
+#   0   anio             36089 non-null  int64  
+#   1   geocodigoFundar  36089 non-null  object 
+#   2   edad_mediana     36089 non-null  float64
+#   3   geonombreFundar  36089 non-null  object 
 #  
-#  |    |   anio | geocodigoFundar   |   edad_mediana | geonombreFundar   |
-#  |---:|-------:|:------------------|---------------:|:------------------|
-#  |  0 |   1950 | BDI               |        18.3054 | Burundi           |
+#  |    |   anio | geocodigoFundar   |   edad_mediana | geonombreFundar            |
+#  |---:|-------:|:------------------|---------------:|:---------------------------|
+#  |  0 |   1950 | LCN               |        18.3317 | América Latina y el Caribe |
 #  
 #  ------------------------------
 #  
 #  query(condition='anio <= 2025')
-#  Index: 18012 entries, 0 to 35711
+#  Index: 18164 entries, 0 to 36013
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             18012 non-null  int64  
-#   1   geocodigoFundar  18012 non-null  object 
-#   2   edad_mediana     18012 non-null  float64
-#   3   geonombreFundar  18012 non-null  object 
+#   0   anio             18164 non-null  int64  
+#   1   geocodigoFundar  18164 non-null  object 
+#   2   edad_mediana     18164 non-null  float64
+#   3   geonombreFundar  18164 non-null  object 
 #  
-#  |    |   anio | geocodigoFundar   |   edad_mediana | geonombreFundar   |
-#  |---:|-------:|:------------------|---------------:|:------------------|
-#  |  0 |   1950 | BDI               |        18.3054 | Burundi           |
+#  |    |   anio | geocodigoFundar   |   edad_mediana | geonombreFundar            |
+#  |---:|-------:|:------------------|---------------:|:---------------------------|
+#  |  0 |   1950 | LCN               |        18.3317 | América Latina y el Caribe |
 #  
 #  ------------------------------
 #  
