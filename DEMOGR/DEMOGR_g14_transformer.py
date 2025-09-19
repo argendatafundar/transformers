adada53d7b8549af2a2cd19cb4a3f1834b85adcd
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-19 15:41:23 -0300

    Updated DEMOGR/DEMOGR_g14_transformer.py

--- DEMOGR/DEMOGR_g14_transformer.py
+++ DEMOGR/DEMOGR_g14_transformer.py
@@ -4,14 +4,16 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	replace_value(col='fuente', curr_value='World Population Prospects (UN)', new_value='WPP'),
+	replace_value(col='fuente', curr_value='Lattes et al (1975)', new_value='Lattes et al. (1975)')
 )
 #  PIPELINE_END
 
@@ -31,7 +33,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  replace_value(col='fuente', curr_value='World Population Prospects (UN)', new_value='WPP')
 #  RangeIndex: 92 entries, 0 to 91
 #  Data columns (total 3 columns):
 #   #   Column               Non-Null Count  Dtype  
@@ -43,6 +45,21 @@ pipeline = chain(
 #  |    |   anio |   tasa_migracion_neta | fuente              |
 #  |---:|-------:|----------------------:|:--------------------|
 #  |  0 |   1870 |               1.02304 | Lattes et al (1975) |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='fuente', curr_value='Lattes et al (1975)', new_value='Lattes et al. (1975)')
+#  RangeIndex: 92 entries, 0 to 91
+#  Data columns (total 3 columns):
+#   #   Column               Non-Null Count  Dtype  
+#  ---  ------               --------------  -----  
+#   0   anio                 92 non-null     int64  
+#   1   tasa_migracion_neta  92 non-null     float64
+#   2   fuente               92 non-null     object 
+#  
+#  |    |   anio |   tasa_migracion_neta | fuente               |
+#  |---:|-------:|----------------------:|:---------------------|
+#  |  0 |   1870 |               1.02304 | Lattes et al. (1975) |
 #  
 #  ------------------------------
 #  
