e19368b19f46b4e6da626b29cf5e9687cfce3667
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-24 14:26:34 -0300

    Updated DESHUM/DESHUM_g12_transformer.py

--- DESHUM/DESHUM_g12_transformer.py
+++ DESHUM/DESHUM_g12_transformer.py
@@ -19,7 +19,7 @@ def rename_cols(df: DataFrame, map):
 pipeline = chain(
 	query(condition='anio == anio.max()'),
 	rename_cols(map={'sexo': 'indicador', 'idh': 'valor'}),
-	query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Japón', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia')")
+	query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Suecia', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia')")
 )
 #  PIPELINE_END
 
@@ -75,7 +75,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Japón', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia')")
+#  query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Suecia', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia')")
 #  Index: 28 entries, 28 to 10027
 #  Data columns (total 5 columns):
 #   #   Column       Non-Null Count  Dtype  
