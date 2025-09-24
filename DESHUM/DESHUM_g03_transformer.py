32ac8b14765231d2bcee93bcefa3962064bff879
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-24 13:11:46 -0300

    Updated DESHUM/DESHUM_g03_transformer.py

--- DESHUM/DESHUM_g03_transformer.py
+++ DESHUM/DESHUM_g03_transformer.py
@@ -4,41 +4,25 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def to_pandas(df: pl.DataFrame, dummy = True):
-    df = df.to_pandas()
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	to_pandas(dummy=True),
 	rename_cols(map={'idh': 'valor'}),
-	drop_col(col=['geocodigoFundar', 'continente_fundar', 'es_agregacion'], axis=1),
-	query(condition='anio in [1990, 2001, 2011, 2022]')
+	drop_col(col=['geocodigoFundar', 'continente_fundar', 'es_agregacion'], axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  
-#  ------------------------------
-#  
-#  to_pandas(dummy=True)
 #  RangeIndex: 6171 entries, 0 to 6170
 #  Data columns (total 6 columns):
 #   #   Column             Non-Null Count  Dtype  
@@ -82,21 +66,6 @@ pipeline = chain(
 #   0   geonombreFundar  6171 non-null   object 
 #   1   anio             6171 non-null   int64  
 #   2   valor            6171 non-null   float64
-#  
-#  |    | geonombreFundar   |   anio |   valor |
-#  |---:|:------------------|-------:|--------:|
-#  |  0 | Afganistán        |   1990 |   0.284 |
-#  
-#  ------------------------------
-#  
-#  query(condition='anio in [1990, 2001, 2011, 2022]')
-#  Index: 747 entries, 0 to 6170
-#  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   geonombreFundar  747 non-null    object 
-#   1   anio             747 non-null    int64  
-#   2   valor            747 non-null    float64
 #  
 #  |    | geonombreFundar   |   anio |   valor |
 #  |---:|:------------------|-------:|--------:|
