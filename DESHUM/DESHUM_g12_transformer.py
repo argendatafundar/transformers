d98473fddcff753c15072341814f31d28ecc898c
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-24 14:29:11 -0300

    Updated DESHUM/DESHUM_g12_transformer.py

--- DESHUM/DESHUM_g12_transformer.py
+++ DESHUM/DESHUM_g12_transformer.py
@@ -19,7 +19,7 @@ def rename_cols(df: DataFrame, map):
 pipeline = chain(
 	query(condition='anio == anio.max()'),
 	rename_cols(map={'sexo': 'indicador', 'idh': 'valor'}),
-	query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Suecia', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia', 'Chile', 'Uruguay')")
+	query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Suecia', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia', 'Chile', 'Uruguay', 'Liechtenstein')")
 )
 #  PIPELINE_END
 
@@ -75,16 +75,16 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Suecia', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia', 'Chile', 'Uruguay')")
-#  Index: 32 entries, 28 to 10027
+#  query(condition="pais_nombre in ('Argentina', 'Yemen','Afganistán','Iraq','Siria','Mundo', 'India', 'Jordania', 'Suecia', 'América Latina y el Caribe', 'Noruega', 'Suiza', 'España', 'Italia', 'Chile', 'Uruguay', 'Liechtenstein')")
+#  Index: 34 entries, 28 to 10027
 #  Data columns (total 5 columns):
 #   #   Column       Non-Null Count  Dtype  
 #  ---  ------       --------------  -----  
-#   0   iso3         32 non-null     object 
-#   1   anio         32 non-null     int64  
-#   2   pais_nombre  32 non-null     object 
-#   3   indicador    32 non-null     object 
-#   4   valor        32 non-null     float64
+#   0   iso3         34 non-null     object 
+#   1   anio         34 non-null     int64  
+#   2   pais_nombre  34 non-null     object 
+#   3   indicador    34 non-null     object 
+#   4   valor        34 non-null     float64
 #  
 #  |    | iso3   |   anio | pais_nombre   | indicador   |   valor |
 #  |---:|:-------|-------:|:--------------|:------------|--------:|
