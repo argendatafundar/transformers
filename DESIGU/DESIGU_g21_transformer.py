76484a8f828682b72d5683d6189555da601f57ed
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-24 16:20:34 -0300

    Updated DESIGU/DESIGU_g21_transformer.py

--- DESIGU/DESIGU_g21_transformer.py
+++ DESIGU/DESIGU_g21_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	rename_cols(map={'ano': 'anio', 'brecha': 'valor'})
+	rename_cols(map={'ano': 'anio', 'brecha': 'valor'}),
+	multiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -41,7 +47,21 @@ pipeline = chain(
 #  
 #  |    |   anio |   valor |
 #  |---:|-------:|--------:|
-#  |  0 |   2000 |   0.138 |
+#  |  0 |   2000 |    13.8 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 23 entries, 0 to 22
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    23 non-null     int64  
+#   1   valor   23 non-null     float64
+#  
+#  |    |   anio |   valor |
+#  |---:|-------:|--------:|
+#  |  0 |   2000 |    13.8 |
 #  
 #  ------------------------------
 #  
