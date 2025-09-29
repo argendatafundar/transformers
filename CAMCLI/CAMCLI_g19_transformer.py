f56eb1fceca10a729a43021245c06eb7be00ac80
jgjuara <jg.juara@gmail.com>
2025-09-29 09:39:38 -0300

    Updated CAMCLI/CAMCLI_g19_transformer.py

--- CAMCLI/CAMCLI_g19_transformer.py
+++ CAMCLI/CAMCLI_g19_transformer.py
@@ -4,6 +4,13 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def pd_loc(df: DataFrame, expr: str):
+    import pandas as pd
+    import numpy as np
+
+    return df.loc[eval(expr)]
+
+@transformer.convert
 def to_pandas(df, dummy = True):
     import polars as pl
     if isinstance(df, pl.DataFrame):
@@ -20,7 +27,8 @@ def rename_cols(df: DataFrame, map):
 #  PIPELINE_START
 pipeline = chain(
 	to_pandas(dummy=True),
-	rename_cols(map={'year': 'anio', 'near_surface_temperature_anomaly': 'valor'})
+	rename_cols(map={'year': 'anio', 'near_surface_temperature_anomaly': 'valor'}),
+	pd_loc(expr='df["anio"] < df["anio"].max()')
 )
 #  PIPELINE_END
 
@@ -54,6 +62,22 @@ pipeline = chain(
 #   1   anio             176 non-null    int64  
 #   2   valor            176 non-null    float64
 #   3   geonombreFundar  176 non-null    object 
+#  
+#  |    | geocodigoFundar   |   anio |      valor | geonombreFundar   |
+#  |---:|:------------------|-------:|-----------:|:------------------|
+#  |  0 | WLD               |   1850 | -0.0554137 | Mundo             |
+#  
+#  ------------------------------
+#  
+#  pd_loc(expr='df["anio"] < df["anio"].max()')
+#  Index: 175 entries, 0 to 174
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  175 non-null    object 
+#   1   anio             175 non-null    int64  
+#   2   valor            175 non-null    float64
+#   3   geonombreFundar  175 non-null    object 
 #  
 #  |    | geocodigoFundar   |   anio |      valor | geonombreFundar   |
 #  |---:|:------------------|-------:|-----------:|:------------------|
