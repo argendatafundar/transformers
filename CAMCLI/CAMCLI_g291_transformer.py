861683685046dadc9caed705fa26b83ee8795224
jgjuara <jg.juara@gmail.com>
2025-09-29 14:26:26 -0300

    Updated CAMCLI/CAMCLI_g291_transformer.py

--- CAMCLI/CAMCLI_g291_transformer.py
+++ CAMCLI/CAMCLI_g291_transformer.py
@@ -9,6 +9,11 @@ def drop_na(df:DataFrame, col:str):
     return df
 
 @transformer.convert
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
 def to_pandas(df, dummy = True):
     import polars as pl
     if isinstance(df, pl.DataFrame):
@@ -20,7 +25,8 @@ def to_pandas(df, dummy = True):
 #  PIPELINE_START
 pipeline = chain(
 	to_pandas(dummy=True),
-	drop_na(col='geocodigoFundar')
+	drop_na(col='geocodigoFundar'),
+	rename_cols(map={'geonombreFundar': 'name_long'})
 )
 #  PIPELINE_END
 
@@ -60,6 +66,23 @@ pipeline = chain(
 #  |    | geocodigoFundar   |   temperature_anomaly_40_69 |   temperature_anomaly_20_adel |     dif | geonombreFundar   |
 #  |---:|:------------------|----------------------------:|------------------------------:|--------:|:------------------|
 #  |  0 | NOR               |                    -2.41211 |                      0.674379 | 3.08649 | Noruega           |
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'geonombreFundar': 'name_long'})
+#  Index: 195 entries, 0 to 212
+#  Data columns (total 5 columns):
+#   #   Column                       Non-Null Count  Dtype  
+#  ---  ------                       --------------  -----  
+#   0   geocodigoFundar              195 non-null    object 
+#   1   temperature_anomaly_40_69    195 non-null    float64
+#   2   temperature_anomaly_20_adel  195 non-null    float64
+#   3   dif                          195 non-null    float64
+#   4   name_long                    195 non-null    object 
+#  
+#  |    | geocodigoFundar   |   temperature_anomaly_40_69 |   temperature_anomaly_20_adel |     dif | name_long   |
+#  |---:|:------------------|----------------------------:|------------------------------:|--------:|:------------|
+#  |  0 | NOR               |                    -2.41211 |                      0.674379 | 3.08649 | Noruega     |
 #  
 #  ------------------------------
 #  
