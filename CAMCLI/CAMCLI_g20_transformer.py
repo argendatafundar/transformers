296df1823fde29b29b010d570d1f640a3105a988
jgjuara <jg.juara@gmail.com>
2025-09-29 09:49:56 -0300

    Updated CAMCLI/CAMCLI_g20_transformer.py

--- CAMCLI/CAMCLI_g20_transformer.py
+++ CAMCLI/CAMCLI_g20_transformer.py
@@ -4,34 +4,20 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
+    df = df.replace({col: curr_value}, new_value)
+    return df
+
+@transformer.convert
 def wide_to_long(df: DataFrame, primary_keys, value_name='valor', var_name='indicador'):
     return df.melt(id_vars=primary_keys, value_name=value_name, var_name=var_name)
 
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def to_pandas(df, dummy = True):
+    import polars as pl
+    if isinstance(df, pl.DataFrame):
+        df = df.to_pandas()
     return df
-
-@transformer.convert
-def datetime_to_year(df, col: str):
-    import pandas as pd
-    
-    df[col] = pd.to_datetime(df[col]).dt.year
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
-    return df
-
-@transformer.convert
-def promedio_anual(df: DataFrame):
-    return df.groupby(['anio', 'indicador']).agg({'valor': 'mean'}).reset_index()
 
 @transformer.convert
 def rename_cols(df: DataFrame, map):
@@ -42,134 +28,91 @@ def rename_cols(df: DataFrame, map):
 
 #  PIPELINE_START
 pipeline = chain(
-wide_to_long(primary_keys=['fecha'], value_name='valor', var_name='indicador'),
-	rename_cols(map={'fecha': 'anio'}),
-	datetime_to_year(col='anio'),
+	to_pandas(dummy=True),
+	wide_to_long(primary_keys=['anio'], value_name='valor', var_name='indicador'),
 	replace_value(col='indicador', curr_value='anomalia_temperatura_mar_relativ', new_value='Mar'),
 	replace_value(col='indicador', curr_value='anomalia_temperatura_tierra_relativ', new_value='Tierra'),
-	promedio_anual(),
 	rename_cols(map={'indicador': 'categoria'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 2088 entries, 0 to 2087
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
+#  RangeIndex: 175 entries, 0 to 174
 #  Data columns (total 3 columns):
 #   #   Column                               Non-Null Count  Dtype  
 #  ---  ------                               --------------  -----  
-#   0   fecha                                2088 non-null   object 
-#   1   anomalia_temperatura_tierra_relativ  2086 non-null   float64
-#   2   anomalia_temperatura_mar_relativ     2088 non-null   float64
+#   0   anio                                 175 non-null    int64  
+#   1   anomalia_temperatura_mar_relativ     175 non-null    float64
+#   2   anomalia_temperatura_tierra_relativ  175 non-null    float64
 #  
-#  |    | fecha      |   anomalia_temperatura_tierra_relativ |   anomalia_temperatura_mar_relativ |
-#  |---:|:-----------|--------------------------------------:|-----------------------------------:|
-#  |  0 | 1850-01-01 |                             -0.214667 |                         -0.0916589 |
-#  
-#  ------------------------------
-#  
-#  wide_to_long(primary_keys=['fecha'], value_name='valor', var_name='indicador')
-#  RangeIndex: 4176 entries, 0 to 4175
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   fecha      4176 non-null   object 
-#   1   indicador  4176 non-null   object 
-#   2   valor      4174 non-null   float64
-#  
-#  |    | fecha      | indicador                           |     valor |
-#  |---:|:-----------|:------------------------------------|----------:|
-#  |  0 | 1850-01-01 | anomalia_temperatura_tierra_relativ | -0.214667 |
+#  |    |   anio |   anomalia_temperatura_mar_relativ |   anomalia_temperatura_tierra_relativ |
+#  |---:|-------:|-----------------------------------:|--------------------------------------:|
+#  |  0 |   1850 |                         -0.0385351 |                             -0.326669 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'fecha': 'anio'})
-#  RangeIndex: 4176 entries, 0 to 4175
+#  wide_to_long(primary_keys=['anio'], value_name='valor', var_name='indicador')
+#  RangeIndex: 350 entries, 0 to 349
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4176 non-null   int32  
-#   1   indicador  4176 non-null   object 
-#   2   valor      4174 non-null   float64
+#   0   anio       350 non-null    int64  
+#   1   indicador  350 non-null    object 
+#   2   valor      350 non-null    float64
 #  
-#  |    |   anio | indicador                           |     valor |
-#  |---:|-------:|:------------------------------------|----------:|
-#  |  0 |   1850 | anomalia_temperatura_tierra_relativ | -0.214667 |
-#  
-#  ------------------------------
-#  
-#  datetime_to_year(col='anio')
-#  RangeIndex: 4176 entries, 0 to 4175
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       4176 non-null   int32  
-#   1   indicador  4176 non-null   object 
-#   2   valor      4174 non-null   float64
-#  
-#  |    |   anio | indicador                           |     valor |
-#  |---:|-------:|:------------------------------------|----------:|
-#  |  0 |   1850 | anomalia_temperatura_tierra_relativ | -0.214667 |
+#  |    |   anio | indicador                        |      valor |
+#  |---:|-------:|:---------------------------------|-----------:|
+#  |  0 |   1850 | anomalia_temperatura_mar_relativ | -0.0385351 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='anomalia_temperatura_mar_relativ', new_value='Mar')
-#  RangeIndex: 4176 entries, 0 to 4175
+#  RangeIndex: 350 entries, 0 to 349
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4176 non-null   int32  
-#   1   indicador  4176 non-null   object 
-#   2   valor      4174 non-null   float64
+#   0   anio       350 non-null    int64  
+#   1   indicador  350 non-null    object 
+#   2   valor      350 non-null    float64
 #  
-#  |    |   anio | indicador                           |     valor |
-#  |---:|-------:|:------------------------------------|----------:|
-#  |  0 |   1850 | anomalia_temperatura_tierra_relativ | -0.214667 |
+#  |    |   anio | indicador   |      valor |
+#  |---:|-------:|:------------|-----------:|
+#  |  0 |   1850 | Mar         | -0.0385351 |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='indicador', curr_value='anomalia_temperatura_tierra_relativ', new_value='Tierra')
-#  RangeIndex: 4176 entries, 0 to 4175
+#  RangeIndex: 350 entries, 0 to 349
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       4176 non-null   int32  
-#   1   indicador  4176 non-null   object 
-#   2   valor      4174 non-null   float64
-#  
-#  |    |   anio | indicador   |     valor |
-#  |---:|-------:|:------------|----------:|
-#  |  0 |   1850 | Tierra      | -0.214667 |
-#  
-#  ------------------------------
-#  
-#  promedio_anual()
-#  RangeIndex: 348 entries, 0 to 347
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       348 non-null    int32  
-#   1   indicador  348 non-null    object 
-#   2   valor      348 non-null    float64
+#   0   anio       350 non-null    int64  
+#   1   indicador  350 non-null    object 
+#   2   valor      350 non-null    float64
 #  
 #  |    |   anio | indicador   |      valor |
 #  |---:|-------:|:------------|-----------:|
-#  |  0 |   1850 | Mar         | -0.0497822 |
+#  |  0 |   1850 | Mar         | -0.0385351 |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'indicador': 'categoria'})
-#  RangeIndex: 348 entries, 0 to 347
+#  RangeIndex: 350 entries, 0 to 349
 #  Data columns (total 3 columns):
 #   #   Column     Non-Null Count  Dtype  
 #  ---  ------     --------------  -----  
-#   0   anio       348 non-null    int32  
-#   1   categoria  348 non-null    object 
-#   2   valor      348 non-null    float64
+#   0   anio       350 non-null    int64  
+#   1   categoria  350 non-null    object 
+#   2   valor      350 non-null    float64
 #  
 #  |    |   anio | categoria   |      valor |
 #  |---:|-------:|:------------|-----------:|
-#  |  0 |   1850 | Mar         | -0.0497822 |
+#  |  0 |   1850 | Mar         | -0.0385351 |
 #  
 #  ------------------------------
 #  
