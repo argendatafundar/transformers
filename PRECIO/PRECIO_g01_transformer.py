e8212d04118ac453ea162bc0def8286734af1067
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-30 17:25:01 -0300

    Updated PRECIO/PRECIO_g01_transformer.py

--- PRECIO/PRECIO_g01_transformer.py
+++ PRECIO/PRECIO_g01_transformer.py
@@ -4,15 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict)->DataFrame:
-    df[new_col] = df[curr_col].apply(lambda x: mapper.get(x, None))
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y otros', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Comunicaciones', 'Transporte': 'Transporte', 'Bienes y servicios varios': 'Varios'})
+	identity()
 )
 #  PIPELINE_END
 
@@ -31,18 +30,17 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  map_categoria(curr_col='sector', new_col='sector_agrupado', mapper={'Alimentos y bebidas no alcoholicas': 'Alimentos, bebidas y tabaco', 'Bebidas alcoholicas y tabaco': 'Alimentos, bebidas y tabaco', 'Educacion': 'Educación y salud', 'Salud': 'Educación y salud', 'Recreacion y cultura': 'Cultura y esparcimiento', 'Restaurantes y hoteles': 'Cultura y esparcimiento', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, equipamiento y otros', 'Equipamiento y mantenimiento del hogar': 'Vivienda, equipamiento y otros', 'Prendas de vestir y calzado': 'Prendas de vestir y calzado', 'Comunicaciones': 'Comunicaciones', 'Transporte': 'Transporte', 'Bienes y servicios varios': 'Varios'})
+#  identity()
 #  RangeIndex: 12 entries, 0 to 11
-#  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   sector           12 non-null     object 
-#   1   valor            12 non-null     float64
-#   2   sector_agrupado  12 non-null     object 
+#  Data columns (total 2 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   sector  12 non-null     object 
+#   1   valor   12 non-null     float64
 #  
-#  |    | sector                             |   valor | sector_agrupado             |
-#  |---:|:-----------------------------------|--------:|:----------------------------|
-#  |  0 | Alimentos y bebidas no alcoholicas |   26.93 | Alimentos, bebidas y tabaco |
+#  |    | sector                             |   valor |
+#  |---:|:-----------------------------------|--------:|
+#  |  0 | Alimentos y bebidas no alcoholicas |   26.93 |
 #  
 #  ------------------------------
 #  
