8fa80e842533ac21495b69a45d7aa30f66234ae9
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-09-30 15:57:11 -0300

    Updated DESIGU/DESIGU_g19_transformer.py

--- DESIGU/DESIGU_g19_transformer.py
+++ DESIGU/DESIGU_g19_transformer.py
@@ -4,14 +4,27 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def rename_cols(df: DataFrame, map):
+    df = df.rename(columns=map)
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	rename_cols(map={'ano': 'anio', 'variable': 'categoria'}),
+	replace_value(col='categoria', curr_value='quintil1', new_value='Quintil 1', mapping=None),
+	replace_value(col='categoria', curr_value='quintil5', new_value='Quintil 5', mapping=None)
 )
 #  PIPELINE_END
 
@@ -31,18 +44,48 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  rename_cols(map={'ano': 'anio', 'variable': 'categoria'})
 #  RangeIndex: 66 entries, 0 to 65
 #  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype  
-#  ---  ------    --------------  -----  
-#   0   ano       66 non-null     int64  
-#   1   variable  66 non-null     object 
-#   2   valor     66 non-null     float64
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       66 non-null     int64  
+#   1   categoria  66 non-null     object 
+#   2   valor      66 non-null     float64
 #  
-#  |    |   ano | variable   |   valor |
-#  |---:|------:|:-----------|--------:|
-#  |  0 |  1992 | quintil1   |   2.057 |
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | quintil1    |   2.057 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='quintil1', new_value='Quintil 1', mapping=None)
+#  RangeIndex: 66 entries, 0 to 65
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       66 non-null     int64  
+#   1   categoria  66 non-null     object 
+#   2   valor      66 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Quintil 1   |   2.057 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value='quintil5', new_value='Quintil 5', mapping=None)
+#  RangeIndex: 66 entries, 0 to 65
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       66 non-null     int64  
+#   1   categoria  66 non-null     object 
+#   2   valor      66 non-null     float64
+#  
+#  |    |   anio | categoria   |   valor |
+#  |---:|-------:|:------------|--------:|
+#  |  0 |   1992 | Quintil 1   |   2.057 |
 #  
 #  ------------------------------
 #  
