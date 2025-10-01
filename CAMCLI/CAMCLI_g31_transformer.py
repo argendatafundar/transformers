abcdcff8e1ddd652231ba7df48b042df0c139d98
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-01 12:14:33 -0300

    Updated CAMCLI/CAMCLI_g31_transformer.py

--- CAMCLI/CAMCLI_g31_transformer.py
+++ CAMCLI/CAMCLI_g31_transformer.py
@@ -4,13 +4,6 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def to_pandas(df, dummy = True):
-    import polars as pl
-    if isinstance(df, pl.DataFrame):
-        df = df.to_pandas()
-    return df
-
-@transformer.convert
 def pd_loc(df, expr):
     return df.loc[eval(expr)]
 #  DEFINITIONS_END
@@ -18,17 +11,12 @@ def pd_loc(df, expr):
 
 #  PIPELINE_START
 pipeline = chain(
-	to_pandas(dummy=True),
 	pd_loc(expr="df['year'].isin([2024,\n 2019,\n 2014,\n 2009,\n 2004,\n 1999,\n 1994,\n 1989,\n 1984,\n 1979,\n 1974,\n 1969,\n 1964,\n 1959,\n 1954,\n 1949,\n 1944])")
 )
 #  PIPELINE_END
 
 
 #  start()
-#  
-#  ------------------------------
-#  
-#  to_pandas(dummy=True)
 #  RangeIndex: 85 entries, 0 to 84
 #  Data columns (total 4 columns):
 #   #   Column               Non-Null Count  Dtype  
@@ -40,7 +28,7 @@ pipeline = chain(
 #  
 #  |    | geonombreFundar   | geocodigoFundar   |   year |   temperature_anomaly |
 #  |---:|:------------------|:------------------|-------:|----------------------:|
-#  |  0 | Argentina         | ARG               |   1940 |             -0.520686 |
+#  |  0 | Argentina         | ARG               |   1940 |              -0.94151 |
 #  
 #  ------------------------------
 #  
@@ -56,7 +44,7 @@ pipeline = chain(
 #  
 #  |    | geonombreFundar   | geocodigoFundar   |   year |   temperature_anomaly |
 #  |---:|:------------------|:------------------|-------:|----------------------:|
-#  |  4 | Argentina         | ARG               |   1944 |              0.623713 |
+#  |  4 | Argentina         | ARG               |   1944 |              0.202888 |
 #  
 #  ------------------------------
 #  
