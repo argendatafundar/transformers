96bc942356ea0ab2d32f1efd0aaf66c5ca8d5031
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-03 13:56:12 -0300

    Updated PRECIO/PRECIO_g01_transformer.py

--- PRECIO/PRECIO_g01_transformer.py
+++ PRECIO/PRECIO_g01_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	replace_multiple_values(col='sector', replacements={'Prendas de vestir y calzado': 'Vestimenta', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, serv. púb. y comb.', 'Equipamiento y mantenimiento del hogar': 'Equip. y mant. hogar', 'Bienes y servicios varios': 'Varios', 'Bebidas alcoholicas y tabaco': 'Alcohol y tabaco', 'Alimentos y bebidas no alcoholicas': 'Alimentos y bebidas'})
 )
 #  PIPELINE_END
 
@@ -30,7 +31,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  replace_multiple_values(col='sector', replacements={'Prendas de vestir y calzado': 'Vestimenta', 'Vivienda agua electricidad gas y otros combustibles': 'Vivienda, serv. púb. y comb.', 'Equipamiento y mantenimiento del hogar': 'Equip. y mant. hogar', 'Bienes y servicios varios': 'Varios', 'Bebidas alcoholicas y tabaco': 'Alcohol y tabaco', 'Alimentos y bebidas no alcoholicas': 'Alimentos y bebidas'})
 #  RangeIndex: 12 entries, 0 to 11
 #  Data columns (total 2 columns):
 #   #   Column  Non-Null Count  Dtype  
@@ -38,9 +39,9 @@ pipeline = chain(
 #   0   sector  12 non-null     object 
 #   1   valor   12 non-null     float64
 #  
-#  |    | sector                             |   valor |
-#  |---:|:-----------------------------------|--------:|
-#  |  0 | Alimentos y bebidas no alcoholicas |   26.93 |
+#  |    | sector              |   valor |
+#  |---:|:--------------------|--------:|
+#  |  0 | Alimentos y bebidas |   26.93 |
 #  
 #  ------------------------------
 #  
