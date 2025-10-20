6bdacae4ad3b4592a9e0dc833385b09ef97ec817
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-20 14:49:16 -0300

    Updated INDUST/INDUST_g07_transformer.py

--- INDUST/INDUST_g07_transformer.py
+++ INDUST/INDUST_g07_transformer.py
@@ -4,6 +4,13 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -18,7 +25,8 @@ def query(df: DataFrame, condition: str):
 #  PIPELINE_START
 pipeline = chain(
 	query(condition="anio == anio.max() & geocodigoFundar == 'ARG'"),
-	multiplicar_por_escalar(col='prop', k=100)
+	multiplicar_por_escalar(col='prop', k=100),
+	sort_values(how='ascending', by='prop')
 )
 #  PIPELINE_END
 
@@ -80,6 +88,26 @@ pipeline = chain(
 #  |      |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full                  |   exportaciones |    prop | source                 |
 #  |-----:|-------:|:------------------|:------------------|:------------|:--------------------------------|----------------:|--------:|:-----------------------|
 #  | 2366 |   2023 | ARG               | Argentina         | mat         | Manufacturas de alta tecnología |     1.47161e+09 | 2.36206 | proyeccion_indice_baci |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by='prop')
+#  RangeIndex: 5 entries, 0 to 4
+#  Data columns (total 8 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             5 non-null      int64  
+#   1   geocodigoFundar  5 non-null      object 
+#   2   geonombreFundar  5 non-null      object 
+#   3   lall_code        5 non-null      object 
+#   4   lall_desc_full   5 non-null      object 
+#   5   exportaciones    5 non-null      float64
+#   6   prop             5 non-null      float64
+#   7   source           5 non-null      object 
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full                  |   exportaciones |    prop | source                 |
+#  |---:|-------:|:------------------|:------------------|:------------|:--------------------------------|----------------:|--------:|:-----------------------|
+#  |  0 |   2023 | ARG               | Argentina         | mbt         | Manufacturas de baja tecnología |     1.03181e+09 | 1.65614 | proyeccion_indice_baci |
 #  
 #  ------------------------------
 #  
