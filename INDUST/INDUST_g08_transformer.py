92b7ffd1c70ece2e6c1c2ea297fb9443c35e04e8
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-20 14:51:21 -0300

    Created INDUST/INDUST_g08_transformer.py

---
+++ INDUST/INDUST_g08_transformer.py
@@ -0,0 +1,113 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	query(condition="anio == anio.max() & geocodigoFundar == 'ARG'"),
+	multiplicar_por_escalar(col='prop', k=100),
+	sort_values(how='descending', by='prop')
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 62090 entries, 0 to 62089
+#  Data columns (total 8 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             62090 non-null  int64  
+#   1   geocodigoFundar  62090 non-null  object 
+#   2   geonombreFundar  62090 non-null  object 
+#   3   lall_code        62090 non-null  object 
+#   4   lall_desc_full   62090 non-null  object 
+#   5   importaciones    62090 non-null  float64
+#   6   prop             61995 non-null  float64
+#   7   source           62090 non-null  object 
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full                  |   importaciones |       prop | source         |
+#  |---:|-------:|:------------------|:------------------|:------------|:--------------------------------|----------------:|-----------:|:---------------|
+#  |  0 |   1988 | ABW               | Aruba             | mat         | Manufacturas de alta tecnología |          336219 | 0.00949233 | atlas_original |
+#  
+#  ------------------------------
+#  
+#  query(condition="anio == anio.max() & geocodigoFundar == 'ARG'")
+#  Index: 5 entries, 2366 to 2614
+#  Data columns (total 8 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             5 non-null      int64  
+#   1   geocodigoFundar  5 non-null      object 
+#   2   geonombreFundar  5 non-null      object 
+#   3   lall_code        5 non-null      object 
+#   4   lall_desc_full   5 non-null      object 
+#   5   importaciones    5 non-null      float64
+#   6   prop             5 non-null      float64
+#   7   source           5 non-null      object 
+#  
+#  |      |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full                  |   importaciones |    prop | source                 |
+#  |-----:|-------:|:------------------|:------------------|:------------|:--------------------------------|----------------:|--------:|:-----------------------|
+#  | 2366 |   2023 | ARG               | Argentina         | mat         | Manufacturas de alta tecnología |     9.62491e+09 | 15.1895 | proyeccion_indice_baci |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='prop', k=100)
+#  Index: 5 entries, 2366 to 2614
+#  Data columns (total 8 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             5 non-null      int64  
+#   1   geocodigoFundar  5 non-null      object 
+#   2   geonombreFundar  5 non-null      object 
+#   3   lall_code        5 non-null      object 
+#   4   lall_desc_full   5 non-null      object 
+#   5   importaciones    5 non-null      float64
+#   6   prop             5 non-null      float64
+#   7   source           5 non-null      object 
+#  
+#  |      |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full                  |   importaciones |    prop | source                 |
+#  |-----:|-------:|:------------------|:------------------|:------------|:--------------------------------|----------------:|--------:|:-----------------------|
+#  | 2366 |   2023 | ARG               | Argentina         | mat         | Manufacturas de alta tecnología |     9.62491e+09 | 15.1895 | proyeccion_indice_baci |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='descending', by='prop')
+#  RangeIndex: 5 entries, 0 to 4
+#  Data columns (total 8 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             5 non-null      int64  
+#   1   geocodigoFundar  5 non-null      object 
+#   2   geonombreFundar  5 non-null      object 
+#   3   lall_code        5 non-null      object 
+#   4   lall_desc_full   5 non-null      object 
+#   5   importaciones    5 non-null      float64
+#   6   prop             5 non-null      float64
+#   7   source           5 non-null      object 
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full                   |   importaciones |    prop | source                 |
+#  |---:|-------:|:------------------|:------------------|:------------|:---------------------------------|----------------:|--------:|:-----------------------|
+#  |  0 |   2023 | ARG               | Argentina         | mmt         | Manufacturas de media tecnología |     2.60779e+10 | 41.1547 | proyeccion_indice_baci |
+#  
+#  ------------------------------
+#  
