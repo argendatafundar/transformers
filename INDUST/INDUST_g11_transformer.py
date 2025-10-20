96c990c3f842f5f3950c78bf2bf92243228d7fff
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-20 16:17:02 -0300

    Updated INDUST/INDUST_g11_transformer.py

--- INDUST/INDUST_g11_transformer.py
+++ INDUST/INDUST_g11_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -12,7 +17,8 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition="geocodigoFundar == 'ARG'")
+	query(condition="geocodigoFundar == 'ARG'"),
+	replace_multiple_values(col='variable', replacements={'share_industrial_gdp': 'Producto', 'share_industrial_employment': 'Empleo'})
 )
 #  PIPELINE_END
 
@@ -45,9 +51,26 @@ pipeline = chain(
 #   3   variable         163 non-null    object 
 #   4   valor            163 non-null    float64
 #  
-#  |     |   anio | geocodigoFundar   | geonombreFundar   | variable             |    valor |
-#  |----:|-------:|:------------------|:------------------|:---------------------|---------:|
-#  | 421 |   1935 | ARG               | Argentina         | share_industrial_gdp | 0.148221 |
+#  |     |   anio | geocodigoFundar   | geonombreFundar   | variable   |    valor |
+#  |----:|-------:|:------------------|:------------------|:-----------|---------:|
+#  | 421 |   1935 | ARG               | Argentina         | Producto   | 0.148221 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='variable', replacements={'share_industrial_gdp': 'Producto', 'share_industrial_employment': 'Empleo'})
+#  Index: 163 entries, 421 to 583
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             163 non-null    int64  
+#   1   geocodigoFundar  163 non-null    object 
+#   2   geonombreFundar  163 non-null    object 
+#   3   variable         163 non-null    object 
+#   4   valor            163 non-null    float64
+#  
+#  |     |   anio | geocodigoFundar   | geonombreFundar   | variable   |    valor |
+#  |----:|-------:|:------------------|:------------------|:-----------|---------:|
+#  | 421 |   1935 | ARG               | Argentina         | Producto   | 0.148221 |
 #  
 #  ------------------------------
 #  
