b6f79349d6cb142edd8153d35f7b5e87031db8fa
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-21 15:50:52 -0300

    Updated INDUST/INDUST_g12_transformer.py

--- INDUST/INDUST_g12_transformer.py
+++ INDUST/INDUST_g12_transformer.py
@@ -11,6 +11,11 @@ def sort_values(df: DataFrame, how: str, by: list):
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df[col] = df[col].replace(replacements)
+    return df
+
+@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -26,6 +31,7 @@ def query(df: DataFrame, condition: str):
 pipeline = chain(
 	query(condition='anio.isin([1914, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024])'),
 	multiplicar_por_escalar(col='prop', k=100),
+	replace_multiple_values(col='sector', replacements={'Equipos informáticos, electrónicos y eléctricos': 'Electrónica', 'Madera, papel y edición': 'Madera y papel', 'Alimentos, bebidas y tabaco': 'Alimentos, bebidas y tabaco', 'Textiles, cuero y calzado': 'Textiles', 'Equipos de transporte': 'Equipos de transporte', 'Metales básicos y elaborados de metal': 'Siderúrgica y metalúrgica', 'Químicos, minerales no metálicos': 'Química y minerales no metálicos', 'Otras manufacturas': 'Reparaciones y otras manufacturas', 'Maquinarias y equipos': 'Maquinaria y equipos'}),
 	sort_values(how='ascending', by=['intensidad_tecnologica'])
 )
 #  PIPELINE_END
@@ -79,6 +85,22 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
+#  replace_multiple_values(col='sector', replacements={'Equipos informáticos, electrónicos y eléctricos': 'Electrónica', 'Madera, papel y edición': 'Madera y papel', 'Alimentos, bebidas y tabaco': 'Alimentos, bebidas y tabaco', 'Textiles, cuero y calzado': 'Textiles', 'Equipos de transporte': 'Equipos de transporte', 'Metales básicos y elaborados de metal': 'Siderúrgica y metalúrgica', 'Químicos, minerales no metálicos': 'Química y minerales no metálicos', 'Otras manufacturas': 'Reparaciones y otras manufacturas', 'Maquinarias y equipos': 'Maquinaria y equipos'})
+#  Index: 99 entries, 0 to 269
+#  Data columns (total 4 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   anio                    99 non-null     int64  
+#   1   sector                  99 non-null     object 
+#   2   prop                    99 non-null     float64
+#   3   intensidad_tecnologica  99 non-null     object 
+#  
+#  |    |   anio | sector                      |    prop | intensidad_tecnologica   |
+#  |---:|-------:|:----------------------------|--------:|:-------------------------|
+#  |  0 |   1914 | Alimentos, bebidas y tabaco | 50.5876 | Low tech                 |
+#  
+#  ------------------------------
+#  
 #  sort_values(how='ascending', by=['intensidad_tecnologica'])
 #  RangeIndex: 99 entries, 0 to 98
 #  Data columns (total 4 columns):
@@ -89,9 +111,9 @@ pipeline = chain(
 #   2   prop                    99 non-null     float64
 #   3   intensidad_tecnologica  99 non-null     object 
 #  
-#  |    |   anio | sector                |    prop | intensidad_tecnologica   |
-#  |---:|-------:|:----------------------|--------:|:-------------------------|
-#  |  0 |   1973 | Maquinarias y equipos | 5.20309 | High tech                |
+#  |    |   anio | sector               |    prop | intensidad_tecnologica   |
+#  |---:|-------:|:---------------------|--------:|:-------------------------|
+#  |  0 |   1973 | Maquinaria y equipos | 5.20309 | High tech                |
 #  
 #  ------------------------------
 #  
