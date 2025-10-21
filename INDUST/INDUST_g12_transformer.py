e0ae311d9ff070795dd34f1b9165a4eb44b52c4e
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-21 16:04:09 -0300

    Updated INDUST/INDUST_g12_transformer.py

--- INDUST/INDUST_g12_transformer.py
+++ INDUST/INDUST_g12_transformer.py
@@ -4,25 +4,25 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
-    return df
-
-@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 
 @transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
+    return df.sort_values(by=[col1,col2])
 
 @transformer.convert
 def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
     df[col] = df[col].replace(replacements)
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
@@ -31,8 +31,8 @@ def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFr
 pipeline = chain(
 	query(condition='anio.isin([1914, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024])'),
 	multiplicar_por_escalar(col='prop', k=100),
-	replace_multiple_values(col='sector', replacements={'Equipos informáticos, electrónicos y eléctricos': 'Electrónica', 'Madera, papel y edición': 'Madera y papel', 'Alimentos, bebidas y tabaco': 'Alimentos, bebidas y tabaco', 'Textiles, cuero y calzado': 'Textiles', 'Equipos de transporte': 'Equipos de transporte', 'Metales básicos y elaborados de metal': 'Siderúrgica y metalúrgica', 'Químicos, minerales no metálicos': 'Química y minerales no metálicos', 'Otras manufacturas': 'Reparaciones y otras manufacturas', 'Maquinarias y equipos': 'Maquinaria y equipos'}),
-	sort_values(how='ascending', by=['anio', 'intensidad_tecnologica'])
+	replace_multiple_values(col='sector', replacements={'Alimentos, bebidas y tabaco': 'Alimentos, bebidas y tabaco', 'Textiles, cuero y calzado': 'Textiles', 'Otras manufacturas': 'Reparaciones y otras manufacturas', 'Madera, papel y edición': 'Madera y papel', 'Químicos, minerales no metálicos': 'Química y minerales no metálicos', 'Equipos de transporte': 'Equipos de transporte', 'Metales básicos y elaborados de metal': 'Siderúrgica y metalúrgica', 'Equipos informáticos, electrónicos y eléctricos': 'Electrónica', 'Maquinarias y equipos': 'Maquinaria y equipos'}),
+	ordenar_dos_columnas(col1='anio', order1=[1914, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024], col2='sector', order2=['Alimentos, bebidas y tabaco', 'Textiles', 'Reparaciones y otras manufacturas', 'Madera y papel', 'Química y minerales no metálicos', 'Equipos de transporte', 'Siderúrgica y metalúrgica', 'Electrónica', 'Maquinaria y equipos'])
 )
 #  PIPELINE_END
 
@@ -56,12 +56,12 @@ pipeline = chain(
 #  query(condition='anio.isin([1914, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024])')
 #  Index: 99 entries, 0 to 269
 #  Data columns (total 4 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   anio                    99 non-null     int64  
-#   1   sector                  99 non-null     object 
-#   2   prop                    99 non-null     float64
-#   3   intensidad_tecnologica  99 non-null     object 
+#   #   Column                  Non-Null Count  Dtype   
+#  ---  ------                  --------------  -----   
+#   0   anio                    99 non-null     category
+#   1   sector                  99 non-null     category
+#   2   prop                    99 non-null     float64 
+#   3   intensidad_tecnologica  99 non-null     object  
 #  
 #  |    |   anio | sector                      |    prop | intensidad_tecnologica   |
 #  |---:|-------:|:----------------------------|--------:|:-------------------------|
@@ -72,12 +72,12 @@ pipeline = chain(
 #  multiplicar_por_escalar(col='prop', k=100)
 #  Index: 99 entries, 0 to 269
 #  Data columns (total 4 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   anio                    99 non-null     int64  
-#   1   sector                  99 non-null     object 
-#   2   prop                    99 non-null     float64
-#   3   intensidad_tecnologica  99 non-null     object 
+#   #   Column                  Non-Null Count  Dtype   
+#  ---  ------                  --------------  -----   
+#   0   anio                    99 non-null     category
+#   1   sector                  99 non-null     category
+#   2   prop                    99 non-null     float64 
+#   3   intensidad_tecnologica  99 non-null     object  
 #  
 #  |    |   anio | sector                      |    prop | intensidad_tecnologica   |
 #  |---:|-------:|:----------------------------|--------:|:-------------------------|
@@ -85,15 +85,15 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_multiple_values(col='sector', replacements={'Equipos informáticos, electrónicos y eléctricos': 'Electrónica', 'Madera, papel y edición': 'Madera y papel', 'Alimentos, bebidas y tabaco': 'Alimentos, bebidas y tabaco', 'Textiles, cuero y calzado': 'Textiles', 'Equipos de transporte': 'Equipos de transporte', 'Metales básicos y elaborados de metal': 'Siderúrgica y metalúrgica', 'Químicos, minerales no metálicos': 'Química y minerales no metálicos', 'Otras manufacturas': 'Reparaciones y otras manufacturas', 'Maquinarias y equipos': 'Maquinaria y equipos'})
+#  replace_multiple_values(col='sector', replacements={'Alimentos, bebidas y tabaco': 'Alimentos, bebidas y tabaco', 'Textiles, cuero y calzado': 'Textiles', 'Otras manufacturas': 'Reparaciones y otras manufacturas', 'Madera, papel y edición': 'Madera y papel', 'Químicos, minerales no metálicos': 'Química y minerales no metálicos', 'Equipos de transporte': 'Equipos de transporte', 'Metales básicos y elaborados de metal': 'Siderúrgica y metalúrgica', 'Equipos informáticos, electrónicos y eléctricos': 'Electrónica', 'Maquinarias y equipos': 'Maquinaria y equipos'})
 #  Index: 99 entries, 0 to 269
 #  Data columns (total 4 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   anio                    99 non-null     int64  
-#   1   sector                  99 non-null     object 
-#   2   prop                    99 non-null     float64
-#   3   intensidad_tecnologica  99 non-null     object 
+#   #   Column                  Non-Null Count  Dtype   
+#  ---  ------                  --------------  -----   
+#   0   anio                    99 non-null     category
+#   1   sector                  99 non-null     category
+#   2   prop                    99 non-null     float64 
+#   3   intensidad_tecnologica  99 non-null     object  
 #  
 #  |    |   anio | sector                      |    prop | intensidad_tecnologica   |
 #  |---:|-------:|:----------------------------|--------:|:-------------------------|
@@ -101,19 +101,19 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['anio', 'intensidad_tecnologica'])
-#  RangeIndex: 99 entries, 0 to 98
+#  ordenar_dos_columnas(col1='anio', order1=[1914, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024], col2='sector', order2=['Alimentos, bebidas y tabaco', 'Textiles', 'Reparaciones y otras manufacturas', 'Madera y papel', 'Química y minerales no metálicos', 'Equipos de transporte', 'Siderúrgica y metalúrgica', 'Electrónica', 'Maquinaria y equipos'])
+#  Index: 99 entries, 0 to 265
 #  Data columns (total 4 columns):
-#   #   Column                  Non-Null Count  Dtype  
-#  ---  ------                  --------------  -----  
-#   0   anio                    99 non-null     int64  
-#   1   sector                  99 non-null     object 
-#   2   prop                    99 non-null     float64
-#   3   intensidad_tecnologica  99 non-null     object 
+#   #   Column                  Non-Null Count  Dtype   
+#  ---  ------                  --------------  -----   
+#   0   anio                    99 non-null     category
+#   1   sector                  99 non-null     category
+#   2   prop                    99 non-null     float64 
+#   3   intensidad_tecnologica  99 non-null     object  
 #  
-#  |    |   anio | sector                |    prop | intensidad_tecnologica   |
-#  |---:|-------:|:----------------------|--------:|:-------------------------|
-#  |  0 |   1914 | Equipos de transporte | 1.87465 | High tech                |
+#  |    |   anio | sector                      |    prop | intensidad_tecnologica   |
+#  |---:|-------:|:----------------------------|--------:|:-------------------------|
+#  |  0 |   1914 | Alimentos, bebidas y tabaco | 50.5876 | Low tech                 |
 #  
 #  ------------------------------
 #  
