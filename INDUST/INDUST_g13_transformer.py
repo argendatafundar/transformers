5853b0cedbbba888a50afc38fa45d093b817c333
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-21 16:33:13 -0300

    Updated INDUST/INDUST_g13_transformer.py

--- INDUST/INDUST_g13_transformer.py
+++ INDUST/INDUST_g13_transformer.py
@@ -4,17 +4,22 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def agg_sum(df: DataFrame, key_cols:list[str], summarised_col:str) -> DataFrame:
-    return df.groupby(key_cols)[summarised_col].sum().reset_index()
-
-@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
 
 @transformer.convert
+def agg_sum(df: DataFrame, key_cols:list[str], summarised_col:str) -> DataFrame:
+    return df.groupby(key_cols)[summarised_col].sum().reset_index()
+
+@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict, default:str = None)->DataFrame:
+    df[new_col] = df[curr_col].apply(lambda x: mapper.get(x, default))
     return df
 
 @transformer.convert
@@ -23,21 +28,16 @@ def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[s
     df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
     df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
     return df.sort_values(by=[col1,col2])
-
-@transformer.convert
-def map_categoria(df:DataFrame, curr_col:str, new_col:str, mapper:dict, default:str = None )->DataFrame:
-    df[new_col] = df[curr_col].apply(lambda x: mapper.get(x, default))
-    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio.isin([1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024])'),
+	query(condition='anio.isin([1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2023])'),
 	multiplicar_por_escalar(col='prop', k=100),
 	map_categoria(curr_col='provincia', new_col='provincia2', mapper={'Buenos Aires': 'Buenos Aires', 'CABA': 'CABA', 'Santa Fe': 'Santa Fe', 'Córdoba': 'Córdoba', 'Mendoza': 'Mendoza', 'Tucumán': 'Tucumán'}, default='Otros'),
 	agg_sum(key_cols=['anio', 'provincia2'], summarised_col='prop'),
-	ordenar_dos_columnas(col1='anio', order1=[1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024], col2='provincia2', order2=['Buenos Aires', 'CABA', 'Santa Fe', 'Córdoba', 'Mendoza', 'Tucumán', 'Otros'])
+	ordenar_dos_columnas(col1='anio', order1=[1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2023], col2='provincia2', order2=['Buenos Aires', 'CABA', 'Santa Fe', 'Córdoba', 'Mendoza', 'Tucumán', 'Otros'])
 )
 #  PIPELINE_END
 
@@ -58,16 +58,16 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='anio.isin([1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024])')
-#  Index: 240 entries, 0 to 683
+#  query(condition='anio.isin([1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2023])')
+#  Index: 264 entries, 0 to 695
 #  Data columns (total 5 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   anio          240 non-null    int64  
-#   1   provincia_id  240 non-null    int64  
-#   2   provincia     240 non-null    object 
-#   3   prop          240 non-null    float64
-#   4   provincia2    240 non-null    object 
+#   0   anio          264 non-null    int64  
+#   1   provincia_id  264 non-null    int64  
+#   2   provincia     264 non-null    object 
+#   3   prop          264 non-null    float64
+#   4   provincia2    264 non-null    object 
 #  
 #  |    |   anio |   provincia_id | provincia   |   prop | provincia2   |
 #  |---:|-------:|---------------:|:------------|-------:|:-------------|
@@ -76,15 +76,15 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='prop', k=100)
-#  Index: 240 entries, 0 to 683
+#  Index: 264 entries, 0 to 695
 #  Data columns (total 5 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   anio          240 non-null    int64  
-#   1   provincia_id  240 non-null    int64  
-#   2   provincia     240 non-null    object 
-#   3   prop          240 non-null    float64
-#   4   provincia2    240 non-null    object 
+#   0   anio          264 non-null    int64  
+#   1   provincia_id  264 non-null    int64  
+#   2   provincia     264 non-null    object 
+#   3   prop          264 non-null    float64
+#   4   provincia2    264 non-null    object 
 #  
 #  |    |   anio |   provincia_id | provincia   |   prop | provincia2   |
 #  |---:|-------:|---------------:|:------------|-------:|:-------------|
@@ -93,15 +93,15 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  map_categoria(curr_col='provincia', new_col='provincia2', mapper={'Buenos Aires': 'Buenos Aires', 'CABA': 'CABA', 'Santa Fe': 'Santa Fe', 'Córdoba': 'Córdoba', 'Mendoza': 'Mendoza', 'Tucumán': 'Tucumán'}, default='Otros')
-#  Index: 240 entries, 0 to 683
+#  Index: 264 entries, 0 to 695
 #  Data columns (total 5 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   anio          240 non-null    int64  
-#   1   provincia_id  240 non-null    int64  
-#   2   provincia     240 non-null    object 
-#   3   prop          240 non-null    float64
-#   4   provincia2    240 non-null    object 
+#   0   anio          264 non-null    int64  
+#   1   provincia_id  264 non-null    int64  
+#   2   provincia     264 non-null    object 
+#   3   prop          264 non-null    float64
+#   4   provincia2    264 non-null    object 
 #  
 #  |    |   anio |   provincia_id | provincia   |   prop | provincia2   |
 #  |---:|-------:|---------------:|:------------|-------:|:-------------|
@@ -110,13 +110,13 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  agg_sum(key_cols=['anio', 'provincia2'], summarised_col='prop')
-#  RangeIndex: 70 entries, 0 to 69
+#  RangeIndex: 77 entries, 0 to 76
 #  Data columns (total 3 columns):
 #   #   Column      Non-Null Count  Dtype   
 #  ---  ------      --------------  -----   
-#   0   anio        70 non-null     category
-#   1   provincia2  70 non-null     category
-#   2   prop        70 non-null     float64 
+#   0   anio        77 non-null     category
+#   1   provincia2  77 non-null     category
+#   2   prop        77 non-null     float64 
 #  
 #  |    |   anio | provincia2   |   prop |
 #  |---:|-------:|:-------------|-------:|
@@ -124,14 +124,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  ordenar_dos_columnas(col1='anio', order1=[1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024], col2='provincia2', order2=['Buenos Aires', 'CABA', 'Santa Fe', 'Córdoba', 'Mendoza', 'Tucumán', 'Otros'])
-#  Index: 70 entries, 0 to 67
+#  ordenar_dos_columnas(col1='anio', order1=[1913, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2023], col2='provincia2', order2=['Buenos Aires', 'CABA', 'Santa Fe', 'Córdoba', 'Mendoza', 'Tucumán', 'Otros'])
+#  Index: 77 entries, 0 to 74
 #  Data columns (total 3 columns):
 #   #   Column      Non-Null Count  Dtype   
 #  ---  ------      --------------  -----   
-#   0   anio        70 non-null     category
-#   1   provincia2  70 non-null     category
-#   2   prop        70 non-null     float64 
+#   0   anio        77 non-null     category
+#   1   provincia2  77 non-null     category
+#   2   prop        77 non-null     float64 
 #  
 #  |    |   anio | provincia2   |   prop |
 #  |---:|-------:|:-------------|-------:|
