27cf58acd83b7bd4ca59f66bf402c7eec5ed9253
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-21 17:58:40 -0300

    Created INDUST/INDUST_g14_transformer.py

---
+++ INDUST/INDUST_g14_transformer.py
@@ -0,0 +1,76 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	drop_col(col=['exportaciones_industriales'], axis=1),
+	multiplicar_por_escalar(col='prop', k=100)
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 62580 entries, 0 to 62579
+#  Data columns (total 6 columns):
+#   #   Column                      Non-Null Count  Dtype  
+#  ---  ------                      --------------  -----  
+#   0   anio                        62580 non-null  int64  
+#   1   geocodigoFundar             62580 non-null  object 
+#   2   geonombreFundar             62580 non-null  object 
+#   3   lall_desc_full              62580 non-null  object 
+#   4   exportaciones_industriales  62090 non-null  float64
+#   5   prop                        62085 non-null  float64
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_desc_full     |   exportaciones_industriales |    prop |
+#  |---:|-------:|:------------------|:------------------|:-------------------|-----------------------------:|--------:|
+#  |  0 |   1962 | AFG               | Afganistán        | Total manufacturas |                  1.44588e+07 | 0.17697 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['exportaciones_industriales'], axis=1)
+#  RangeIndex: 62580 entries, 0 to 62579
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             62580 non-null  int64  
+#   1   geocodigoFundar  62580 non-null  object 
+#   2   geonombreFundar  62580 non-null  object 
+#   3   lall_desc_full   62580 non-null  object 
+#   4   prop             62085 non-null  float64
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_desc_full     |   prop |
+#  |---:|-------:|:------------------|:------------------|:-------------------|-------:|
+#  |  0 |   1962 | AFG               | Afganistán        | Total manufacturas | 17.697 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='prop', k=100)
+#  RangeIndex: 62580 entries, 0 to 62579
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             62580 non-null  int64  
+#   1   geocodigoFundar  62580 non-null  object 
+#   2   geonombreFundar  62580 non-null  object 
+#   3   lall_desc_full   62580 non-null  object 
+#   4   prop             62085 non-null  float64
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_desc_full     |   prop |
+#  |---:|-------:|:------------------|:------------------|:-------------------|-------:|
+#  |  0 |   1962 | AFG               | Afganistán        | Total manufacturas | 17.697 |
+#  
+#  ------------------------------
+#  
