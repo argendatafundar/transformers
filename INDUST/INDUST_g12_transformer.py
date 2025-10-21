c62a8bc2838f00961dbbd127e0e5bfb276e7b302
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-21 15:39:51 -0300

    Updated INDUST/INDUST_g12_transformer.py

--- INDUST/INDUST_g12_transformer.py
+++ INDUST/INDUST_g12_transformer.py
@@ -4,6 +4,13 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -18,7 +25,8 @@ def query(df: DataFrame, condition: str):
 #  PIPELINE_START
 pipeline = chain(
 	query(condition='anio.isin([1914, 1935, 1946, 1953, 1963, 1973, 1984, 1993, 2003, 2011, 2024])'),
-	multiplicar_por_escalar(col='prop', k=100)
+	multiplicar_por_escalar(col='prop', k=100),
+	sort_values(how='ascending', by=['intensidad_tecnologica'])
 )
 #  PIPELINE_END
 
@@ -68,6 +76,22 @@ pipeline = chain(
 #  |    |   anio | sector                      |    prop | intensidad_tecnologica   |
 #  |---:|-------:|:----------------------------|--------:|:-------------------------|
 #  |  0 |   1914 | Alimentos, bebidas y tabaco | 50.5876 | Low tech                 |
+#  
+#  ------------------------------
+#  
+#  sort_values(how='ascending', by=['intensidad_tecnologica'])
+#  RangeIndex: 99 entries, 0 to 98
+#  Data columns (total 4 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   anio                    99 non-null     int64  
+#   1   sector                  99 non-null     object 
+#   2   prop                    99 non-null     float64
+#   3   intensidad_tecnologica  99 non-null     object 
+#  
+#  |    |   anio | sector                |    prop | intensidad_tecnologica   |
+#  |---:|-------:|:----------------------|--------:|:-------------------------|
+#  |  0 |   1973 | Maquinarias y equipos | 5.20309 | High tech                |
 #  
 #  ------------------------------
 #  
