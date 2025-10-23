e6c468d04b0db7151f371b5facd01f694e29cbab
jgjuara <jg.juara@gmail.com>
2025-10-23 15:23:42 -0300

    Updated TRANEN/TRANEN_g11_transformer.py

--- TRANEN/TRANEN_g11_transformer.py
+++ TRANEN/TRANEN_g11_transformer.py
@@ -62,186 +62,176 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  to_pandas(dummy=True)
-#  RangeIndex: 40851 entries, 0 to 40850
-#  Data columns (total 7 columns):
+#  RangeIndex: 84400 entries, 0 to 84399
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             40851 non-null  int64  
-#   1   geocodigoFundar  40851 non-null  object 
-#   2   fuente_energia   40851 non-null  object 
-#   3   valor_en_twh     40851 non-null  float64
-#   4   porcentaje       40851 non-null  float64
-#   5   tipo_energia     40851 non-null  object 
-#   6   geonombreFundar  40851 non-null  object 
+#   0   anio             84400 non-null  int64  
+#   1   geocodigoFundar  84400 non-null  object 
+#   2   tipo_energia     84400 non-null  object 
+#   3   valor_en_twh     60379 non-null  float64
+#   4   porcentaje       84400 non-null  float64
+#   5   geonombreFundar  84400 non-null  object 
 #  
-#  |    |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
-#  |---:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
-#  |  0 |   1965 | DZA               | Otras renovables |              0 |            0 | Limpias        | Argelia           |
+#  |    |   anio | geocodigoFundar   | tipo_energia     |   valor_en_twh |   porcentaje | geonombreFundar   |
+#  |---:|-------:|:------------------|:-----------------|---------------:|-------------:|:------------------|
+#  |  0 |   2000 | AFG               | Otras renovables |              0 |            0 | Afganistán        |
 #  
 #  ------------------------------
 #  
 #  query(condition='geocodigoFundar == "WLD" & tipo_energia != "Total"')
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 7 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   geocodigoFundar  540 non-null    object 
-#   2   fuente_energia   540 non-null    object 
-#   3   valor_en_twh     540 non-null    float64
-#   4   porcentaje       540 non-null    float64
-#   5   tipo_energia     540 non-null    object 
-#   6   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   geocodigoFundar  360 non-null    object 
+#   2   tipo_energia     360 non-null    object 
+#   3   valor_en_twh     360 non-null    float64
+#   4   porcentaje       360 non-null    float64
+#   5   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
-#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
-#  | 4479 |   1965 | WLD               | Otras renovables |        56.2039 |     0.130121 | Limpias        | Mundo             |
+#  |      |   anio | geocodigoFundar   | tipo_energia     |   valor_en_twh |   porcentaje | geonombreFundar   |
+#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:------------------|
+#  | 8280 |   2000 | WLD               | Otras renovables |          43.65 |     0.285692 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='tipo_energia', curr_value='Bioenergia', new_value='Bioenergía')
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 7 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   geocodigoFundar  540 non-null    object 
-#   2   fuente_energia   540 non-null    object 
-#   3   valor_en_twh     540 non-null    float64
-#   4   porcentaje       540 non-null    float64
-#   5   tipo_energia     540 non-null    object 
-#   6   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   geocodigoFundar  360 non-null    object 
+#   2   tipo_energia     360 non-null    object 
+#   3   valor_en_twh     360 non-null    float64
+#   4   porcentaje       360 non-null    float64
+#   5   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
-#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
-#  | 4479 |   1965 | WLD               | Otras renovables |        56.2039 |     0.130121 | Limpias        | Mundo             |
+#  |      |   anio | geocodigoFundar   | tipo_energia     |   valor_en_twh |   porcentaje | geonombreFundar   |
+#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:------------------|
+#  | 8280 |   2000 | WLD               | Otras renovables |          43.65 |     0.285692 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='tipo_energia', curr_value='Carbon', new_value='Carbón')
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 7 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   geocodigoFundar  540 non-null    object 
-#   2   fuente_energia   540 non-null    object 
-#   3   valor_en_twh     540 non-null    float64
-#   4   porcentaje       540 non-null    float64
-#   5   tipo_energia     540 non-null    object 
-#   6   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   geocodigoFundar  360 non-null    object 
+#   2   tipo_energia     360 non-null    object 
+#   3   valor_en_twh     360 non-null    float64
+#   4   porcentaje       360 non-null    float64
+#   5   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
-#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
-#  | 4479 |   1965 | WLD               | Otras renovables |        56.2039 |     0.130121 | Limpias        | Mundo             |
+#  |      |   anio | geocodigoFundar   | tipo_energia     |   valor_en_twh |   porcentaje | geonombreFundar   |
+#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:------------------|
+#  | 8280 |   2000 | WLD               | Otras renovables |          43.65 |     0.285692 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='tipo_energia', curr_value='Petroleo', new_value='Petróleo')
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 7 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   geocodigoFundar  540 non-null    object 
-#   2   fuente_energia   540 non-null    object 
-#   3   valor_en_twh     540 non-null    float64
-#   4   porcentaje       540 non-null    float64
-#   5   tipo_energia     540 non-null    object 
-#   6   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   geocodigoFundar  360 non-null    object 
+#   2   tipo_energia     360 non-null    object 
+#   3   valor_en_twh     360 non-null    float64
+#   4   porcentaje       360 non-null    float64
+#   5   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
-#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
-#  | 4479 |   1965 | WLD               | Otras renovables |        56.2039 |     0.130121 | Limpias        | Mundo             |
+#  |      |   anio | geocodigoFundar   | tipo_energia     |   valor_en_twh |   porcentaje | geonombreFundar   |
+#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:------------------|
+#  | 8280 |   2000 | WLD               | Otras renovables |          43.65 |     0.285692 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  replace_value(col='tipo_energia', curr_value='Eolica', new_value='Eólica')
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 7 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   geocodigoFundar  540 non-null    object 
-#   2   fuente_energia   540 non-null    object 
-#   3   valor_en_twh     540 non-null    float64
-#   4   porcentaje       540 non-null    float64
-#   5   tipo_energia     540 non-null    object 
-#   6   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   geocodigoFundar  360 non-null    object 
+#   2   tipo_energia     360 non-null    object 
+#   3   valor_en_twh     360 non-null    float64
+#   4   porcentaje       360 non-null    float64
+#   5   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
-#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
-#  | 4479 |   1965 | WLD               | Otras renovables |        56.2039 |     0.130121 | Limpias        | Mundo             |
+#  |      |   anio | geocodigoFundar   | tipo_energia     |   valor_en_twh |   porcentaje | geonombreFundar   |
+#  |-----:|-------:|:------------------|:-----------------|---------------:|-------------:|:------------------|
+#  | 8280 |   2000 | WLD               | Otras renovables |          43.65 |     0.285692 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'tipo_energia': 'indicador', 'valor_en_twh': 'valor'})
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 7 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 6 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   geocodigoFundar  540 non-null    object 
-#   2   fuente_energia   540 non-null    object 
-#   3   valor            540 non-null    float64
-#   4   porcentaje       540 non-null    float64
-#   5   indicador        540 non-null    object 
-#   6   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   geocodigoFundar  360 non-null    object 
+#   2   indicador        360 non-null    object 
+#   3   valor            360 non-null    float64
+#   4   porcentaje       360 non-null    float64
+#   5   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | geocodigoFundar   | fuente_energia   |   valor |   porcentaje | indicador   | geonombreFundar   |
-#  |-----:|-------:|:------------------|:-----------------|--------:|-------------:|:------------|:------------------|
-#  | 4479 |   1965 | WLD               | Otras renovables | 56.2039 |     0.130121 | Limpias     | Mundo             |
+#  |      |   anio | geocodigoFundar   | indicador        |   valor |   porcentaje | geonombreFundar   |
+#  |-----:|-------:|:------------------|:-----------------|--------:|-------------:|:------------------|
+#  | 8280 |   2000 | WLD               | Otras renovables |   43.65 |     0.285692 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  drop_col(col=['geocodigoFundar', 'porcentaje'], axis=1)
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 5 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   fuente_energia   540 non-null    object 
-#   2   valor            540 non-null    float64
-#   3   indicador        540 non-null    object 
-#   4   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   indicador        360 non-null    object 
+#   2   valor            360 non-null    float64
+#   3   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | fuente_energia   |   valor | indicador   | geonombreFundar   |
-#  |-----:|-------:|:-----------------|--------:|:------------|:------------------|
-#  | 4479 |   1965 | Otras renovables | 56.2039 | Limpias     | Mundo             |
+#  |      |   anio | indicador        |   valor | geonombreFundar   |
+#  |-----:|-------:|:-----------------|--------:|:------------------|
+#  | 8280 |   2000 | Otras renovables |   43.65 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  drop_na(cols=['valor'])
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 5 columns):
+#  Index: 360 entries, 8280 to 75839
+#  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   fuente_energia   540 non-null    object 
-#   2   valor            540 non-null    float64
-#   3   indicador        540 non-null    object 
-#   4   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   indicador        360 non-null    object 
+#   2   valor            360 non-null    float64
+#   3   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | fuente_energia   |   valor | indicador   | geonombreFundar   |
-#  |-----:|-------:|:-----------------|--------:|:------------|:------------------|
-#  | 4479 |   1965 | Otras renovables | 56.2039 | Limpias     | Mundo             |
+#  |      |   anio | indicador        |   valor | geonombreFundar   |
+#  |-----:|-------:|:-----------------|--------:|:------------------|
+#  | 8280 |   2000 | Otras renovables |   43.65 | Mundo             |
 #  
 #  ------------------------------
 #  
 #  sort_values_by_comparison(colname='indicador', precedence={'Bioenergía': 10, 'Otras renovables': 1, 'Biocombustibles': 2, 'Solar': 3, 'Eólica': 4, 'Nuclear': 5, 'Hidro': 6, 'Gas natural': 7, 'Petróleo': 8, 'Carbón': 9}, prefix=['anio'], suffix=[])
-#  Index: 540 entries, 4479 to 40850
-#  Data columns (total 5 columns):
+#  Index: 360 entries, 8305 to 16744
+#  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             540 non-null    int64  
-#   1   fuente_energia   540 non-null    object 
-#   2   valor            540 non-null    float64
-#   3   indicador        540 non-null    object 
-#   4   geonombreFundar  540 non-null    object 
+#   0   anio             360 non-null    int64  
+#   1   indicador        360 non-null    object 
+#   2   valor            360 non-null    float64
+#   3   geonombreFundar  360 non-null    object 
 #  
-#  |      |   anio | fuente_energia   |   valor | indicador   | geonombreFundar   |
-#  |-----:|-------:|:-----------------|--------:|:------------|:------------------|
-#  | 4479 |   1965 | Otras renovables | 56.2039 | Limpias     | Mundo             |
+#  |      |   anio | indicador        |   valor | geonombreFundar   |
+#  |-----:|-------:|:-----------------|--------:|:------------------|
+#  | 8305 |   1985 | Otras renovables |       0 | Mundo             |
 #  
 #  ------------------------------
 #  
