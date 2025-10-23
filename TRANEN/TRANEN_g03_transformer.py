bcdda0c259cc278ab73bb32abec01939588a47c3
jgjuara <jg.juara@gmail.com>
2025-10-23 14:36:00 -0300

    Updated TRANEN/TRANEN_g03_transformer.py

--- TRANEN/TRANEN_g03_transformer.py
+++ TRANEN/TRANEN_g03_transformer.py
@@ -4,8 +4,8 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
 
 @transformer.convert
@@ -14,12 +14,9 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
 
 @transformer.convert
 def drop_col(df: DataFrame, col, axis=1):
@@ -29,114 +26,125 @@ def drop_col(df: DataFrame, col, axis=1):
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='iso3 == "ARG" & anio == anio.max()'),
+	to_pandas(dummy=True),
+	query(condition='geocodigoFundar == "ARG" & anio == anio.max()'),
 	rename_cols(map={'fuente_energia': 'nivel2', 'porcentaje': 'valor', 'tipo_energia': 'nivel1'}),
 	drop_col(col='anio', axis=1),
-	drop_col(col='iso3', axis=1),
+	drop_col(col='geocodigoFundar', axis=1),
 	drop_col(col='valor_en_twh', axis=1)
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 38938 entries, 0 to 38937
-#  Data columns (total 6 columns):
-#   #   Column          Non-Null Count  Dtype  
-#  ---  ------          --------------  -----  
-#   0   anio            38938 non-null  int64  
-#   1   iso3            38938 non-null  object 
-#   2   fuente_energia  38938 non-null  object 
-#   3   valor_en_twh    38938 non-null  float64
-#   4   porcentaje      38938 non-null  float64
-#   5   tipo_energia    38938 non-null  object 
-#  
-#  |    |   anio | iso3   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   |
-#  |---:|-------:|:-------|:-----------------|---------------:|-------------:|:---------------|
-#  |  0 |   1971 | DZA    | Otras renovables |              0 |            0 | Limpias        |
 #  
 #  ------------------------------
 #  
-#  query(condition='iso3 == "ARG" & anio == anio.max()')
-#  Index: 9 entries, 111 to 34590
-#  Data columns (total 6 columns):
-#   #   Column          Non-Null Count  Dtype  
-#  ---  ------          --------------  -----  
-#   0   anio            9 non-null      int64  
-#   1   iso3            9 non-null      object 
-#   2   fuente_energia  9 non-null      object 
-#   3   valor_en_twh    9 non-null      float64
-#   4   porcentaje      9 non-null      float64
-#   5   tipo_energia    9 non-null      object 
+#  to_pandas(dummy=True)
+#  RangeIndex: 40851 entries, 0 to 40850
+#  Data columns (total 7 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             40851 non-null  int64  
+#   1   geocodigoFundar  40851 non-null  object 
+#   2   fuente_energia   40851 non-null  object 
+#   3   valor_en_twh     40851 non-null  float64
+#   4   porcentaje       40851 non-null  float64
+#   5   tipo_energia     40851 non-null  object 
+#   6   geonombreFundar  40851 non-null  object 
 #  
-#  |     |   anio | iso3   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   |
-#  |----:|-------:|:-------|:-----------------|---------------:|-------------:|:---------------|
-#  | 111 |   2023 | ARG    | Otras renovables |        7.26455 |     0.712392 | Limpias        |
+#  |    |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
+#  |---:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
+#  |  0 |   1965 | DZA               | Otras renovables |              0 |            0 | Limpias        | Argelia           |
+#  
+#  ------------------------------
+#  
+#  query(condition='geocodigoFundar == "ARG" & anio == anio.max()')
+#  Index: 9 entries, 119 to 36431
+#  Data columns (total 7 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             9 non-null      int64  
+#   1   geocodigoFundar  9 non-null      object 
+#   2   fuente_energia   9 non-null      object 
+#   3   valor_en_twh     9 non-null      float64
+#   4   porcentaje       9 non-null      float64
+#   5   tipo_energia     9 non-null      object 
+#   6   geonombreFundar  9 non-null      object 
+#  
+#  |     |   anio | geocodigoFundar   | fuente_energia   |   valor_en_twh |   porcentaje | tipo_energia   | geonombreFundar   |
+#  |----:|-------:|:------------------|:-----------------|---------------:|-------------:|:---------------|:------------------|
+#  | 119 |   2024 | ARG               | Otras renovables |        8.23241 |     0.844324 | Limpias        | Argentina         |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'fuente_energia': 'nivel2', 'porcentaje': 'valor', 'tipo_energia': 'nivel1'})
-#  Index: 9 entries, 111 to 34590
-#  Data columns (total 6 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   anio          9 non-null      int64  
-#   1   iso3          9 non-null      object 
-#   2   nivel2        9 non-null      object 
-#   3   valor_en_twh  9 non-null      float64
-#   4   valor         9 non-null      float64
-#   5   nivel1        9 non-null      object 
+#  Index: 9 entries, 119 to 36431
+#  Data columns (total 7 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             9 non-null      int64  
+#   1   geocodigoFundar  9 non-null      object 
+#   2   nivel2           9 non-null      object 
+#   3   valor_en_twh     9 non-null      float64
+#   4   valor            9 non-null      float64
+#   5   nivel1           9 non-null      object 
+#   6   geonombreFundar  9 non-null      object 
 #  
-#  |     |   anio | iso3   | nivel2           |   valor_en_twh |    valor | nivel1   |
-#  |----:|-------:|:-------|:-----------------|---------------:|---------:|:---------|
-#  | 111 |   2023 | ARG    | Otras renovables |        7.26455 | 0.712392 | Limpias  |
+#  |     |   anio | geocodigoFundar   | nivel2           |   valor_en_twh |    valor | nivel1   | geonombreFundar   |
+#  |----:|-------:|:------------------|:-----------------|---------------:|---------:|:---------|:------------------|
+#  | 119 |   2024 | ARG               | Otras renovables |        8.23241 | 0.844324 | Limpias  | Argentina         |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='anio', axis=1)
-#  Index: 9 entries, 111 to 34590
-#  Data columns (total 5 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   iso3          9 non-null      object 
-#   1   nivel2        9 non-null      object 
-#   2   valor_en_twh  9 non-null      float64
-#   3   valor         9 non-null      float64
-#   4   nivel1        9 non-null      object 
+#  Index: 9 entries, 119 to 36431
+#  Data columns (total 6 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  9 non-null      object 
+#   1   nivel2           9 non-null      object 
+#   2   valor_en_twh     9 non-null      float64
+#   3   valor            9 non-null      float64
+#   4   nivel1           9 non-null      object 
+#   5   geonombreFundar  9 non-null      object 
 #  
-#  |     | iso3   | nivel2           |   valor_en_twh |    valor | nivel1   |
-#  |----:|:-------|:-----------------|---------------:|---------:|:---------|
-#  | 111 | ARG    | Otras renovables |        7.26455 | 0.712392 | Limpias  |
+#  |     | geocodigoFundar   | nivel2           |   valor_en_twh |    valor | nivel1   | geonombreFundar   |
+#  |----:|:------------------|:-----------------|---------------:|---------:|:---------|:------------------|
+#  | 119 | ARG               | Otras renovables |        8.23241 | 0.844324 | Limpias  | Argentina         |
 #  
 #  ------------------------------
 #  
-#  drop_col(col='iso3', axis=1)
-#  Index: 9 entries, 111 to 34590
-#  Data columns (total 4 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   nivel2        9 non-null      object 
-#   1   valor_en_twh  9 non-null      float64
-#   2   valor         9 non-null      float64
-#   3   nivel1        9 non-null      object 
+#  drop_col(col='geocodigoFundar', axis=1)
+#  Index: 9 entries, 119 to 36431
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   nivel2           9 non-null      object 
+#   1   valor_en_twh     9 non-null      float64
+#   2   valor            9 non-null      float64
+#   3   nivel1           9 non-null      object 
+#   4   geonombreFundar  9 non-null      object 
 #  
-#  |     | nivel2           |   valor_en_twh |    valor | nivel1   |
-#  |----:|:-----------------|---------------:|---------:|:---------|
-#  | 111 | Otras renovables |        7.26455 | 0.712392 | Limpias  |
+#  |     | nivel2           |   valor_en_twh |    valor | nivel1   | geonombreFundar   |
+#  |----:|:-----------------|---------------:|---------:|:---------|:------------------|
+#  | 119 | Otras renovables |        8.23241 | 0.844324 | Limpias  | Argentina         |
 #  
 #  ------------------------------
 #  
 #  drop_col(col='valor_en_twh', axis=1)
-#  Index: 9 entries, 111 to 34590
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   nivel2  9 non-null      object 
-#   1   valor   9 non-null      float64
-#   2   nivel1  9 non-null      object 
+#  Index: 9 entries, 119 to 36431
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   nivel2           9 non-null      object 
+#   1   valor            9 non-null      float64
+#   2   nivel1           9 non-null      object 
+#   3   geonombreFundar  9 non-null      object 
 #  
-#  |     | nivel2           |    valor | nivel1   |
-#  |----:|:-----------------|---------:|:---------|
-#  | 111 | Otras renovables | 0.712392 | Limpias  |
+#  |     | nivel2           |    valor | nivel1   | geonombreFundar   |
+#  |----:|:-----------------|---------:|:---------|:------------------|
+#  | 119 | Otras renovables | 0.844324 | Limpias  | Argentina         |
 #  
 #  ------------------------------
 #  
