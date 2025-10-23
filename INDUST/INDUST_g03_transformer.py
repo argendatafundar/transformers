9b76960888981f4c4f5063590c4d412e3306e8ac
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-23 14:16:26 -0300

    Updated INDUST/INDUST_g03_transformer.py

--- INDUST/INDUST_g03_transformer.py
+++ INDUST/INDUST_g03_transformer.py
@@ -8,6 +8,16 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -17,7 +27,8 @@ def query(df: DataFrame, condition: str):
 #  PIPELINE_START
 pipeline = chain(
 	query(condition='anio == anio.max()'),
-	drop_col(col=['ciiu_rev3_2d', 'rama_de_actividad'], axis=1)
+	drop_col(col=['ciiu_rev3_2d', 'rama_de_actividad'], axis=1),
+	replace_value(col='descripcion_corta', curr_value=None, new_value=None, mapping={'Productos minerales no metálicos': 'Minerales no metálicos', 'Maquinaria de oficina e informática': 'Equipos de oficina', 'Instrumentos médicos y de precisión': 'Instrumental médico'})
 )
 #  PIPELINE_END
 
@@ -57,6 +68,21 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  drop_col(col=['ciiu_rev3_2d', 'rama_de_actividad'], axis=1)
+#  Index: 24 entries, 29 to 719
+#  Data columns (total 3 columns):
+#   #   Column                  Non-Null Count  Dtype  
+#  ---  ------                  --------------  -----  
+#   0   anio                    24 non-null     int64  
+#   1   descripcion_corta       24 non-null     object 
+#   2   salario_respecto_media  24 non-null     float64
+#  
+#  |    |   anio | descripcion_corta   |   salario_respecto_media |
+#  |---:|-------:|:--------------------|-------------------------:|
+#  | 29 |   2024 | Alimentos y bebidas |                  12.7491 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='descripcion_corta', curr_value=None, new_value=None, mapping={'Productos minerales no metálicos': 'Minerales no metálicos', 'Maquinaria de oficina e informática': 'Equipos de oficina', 'Instrumentos médicos y de precisión': 'Instrumental médico'})
 #  Index: 24 entries, 29 to 719
 #  Data columns (total 3 columns):
 #   #   Column                  Non-Null Count  Dtype  
