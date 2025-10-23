69daa280b22c3eba6ed4bbd1e407cbdd72e2a198
jgjuara <jg.juara@gmail.com>
2025-10-23 14:45:20 -0300

    Updated TRANEN/TRANEN_g05_transformer.py

--- TRANEN/TRANEN_g05_transformer.py
+++ TRANEN/TRANEN_g05_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def sort_values(df: pl.DataFrame, by, descending):
+    df = df.sort(by = by, descending= descending)
+    return df
+
+@transformer.convert
 def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
 
     if not alias:
@@ -16,6 +21,14 @@ def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
     return df
 
 @transformer.convert
+def drop_cols(df, cols):
+    return df.drop(cols)
+
+@transformer.convert
+def drop_na(df:pl.DataFrame, cols:list):
+    return df.drop_nans(subset=cols)
+
+@transformer.convert
 def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
     df = df.sql(query)
     return df
@@ -24,30 +37,17 @@ def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame:
 def rename_cols(df: pl.DataFrame, map):
     df = df.rename(map)
     return df
-
-@transformer.convert
-def drop_na(df:pl.DataFrame, cols:list):
-    return df.drop_nans(subset=cols)
-
-@transformer.convert
-def sort_values(df: pl.DataFrame, by, descending):
-    df = df.sort(by = by, descending= descending)
-    return df
-
-@transformer.convert
-def drop_cols(df, cols):
-    return df.drop(cols)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	df_sql(query="SELECT * FROM self WHERE tipo_energia != 'Total' AND iso3 = 'ARG'"),
+	df_sql(query="SELECT * FROM self WHERE tipo_energia != 'Total' AND geocodigoFundar = 'ARG'"),
 	replace_value(col='fuente_energia', mapping={'Eolica': 'Eólica'}, alias=None),
 	replace_value(col='fuente_energia', mapping={'Carbon': 'Carbón'}, alias=None),
 	replace_value(col='fuente_energia', mapping={'Petroleo': 'Petróleo'}, alias=None),
-	replace_value(col='iso3', mapping={'OWID_WRL': 'WLD'}, alias=None),
-	rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'}),
+	replace_value(col='geocodigoFundar', mapping={'OWID_WRL': 'WLD'}, alias=None),
+	rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'geocodigoFundar': 'geocodigo'}),
 	drop_cols(cols=['valor_en_twh', 'tipo_energia']),
 	drop_na(cols=['valor']),
 	replace_value(col='indicador', mapping={'Otras renovables': 0, 'Biocombustibles': 1, 'Solar': 2, 'Eólica': 3, 'Nuclear': 4, 'Hidro': 5, 'Gas natural': 6, 'Petróleo': 7, 'Carbón': 8, 'Biomasa tradicional': 9}, alias='orden'),
@@ -60,7 +60,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  df_sql(query="SELECT * FROM self WHERE tipo_energia != 'Total' AND iso3 = 'ARG'")
+#  df_sql(query="SELECT * FROM self WHERE tipo_energia != 'Total' AND geocodigoFundar = 'ARG'")
 #  
 #  ------------------------------
 #  
@@ -76,11 +76,11 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='iso3', mapping={'OWID_WRL': 'WLD'}, alias=None)
+#  replace_value(col='geocodigoFundar', mapping={'OWID_WRL': 'WLD'}, alias=None)
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'iso3': 'geocodigo'})
+#  rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'geocodigoFundar': 'geocodigo'})
 #  
 #  ------------------------------
 #  
