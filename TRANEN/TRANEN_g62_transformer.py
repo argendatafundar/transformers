2ec7d644bd8d0877731700e45586640082a985dd
jgjuara <jg.juara@gmail.com>
2025-10-27 12:49:42 -0300

    Created TRANEN/TRANEN_g62_transformer.py

---
+++ TRANEN/TRANEN_g62_transformer.py
@@ -0,0 +1,92 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def rename_cols(df: pl.DataFrame, map):
+    df = df.rename(map)
+    return df
+
+@transformer.convert
+def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
+
+    if not alias:
+        alias = col
+
+    df = df.with_columns(
+        pl.col(col).replace(mapping).alias(alias)
+    )
+
+    return df
+
+@transformer.convert
+def df_sql(df: pl.DataFrame, query: str) -> pl.DataFrame: 
+    df = df.sql(query)
+    return df
+
+@transformer.convert
+def sort_values(df: pl.DataFrame, by, descending = None):
+    if not descending:
+        descending = [False] * len(by)
+    df = df.sort(by = by, descending= descending)
+    return df
+
+@transformer.convert
+def drop_na(df:pl.DataFrame, cols:list):
+    df = df.drop_nans(subset=cols)
+    df = df.drop_nulls(subset=cols)
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	replace_value(col='geocodigoFundar', mapping={'OWID_WRL': 'WLD'}, alias=None),
+	df_sql(query="SELECT * FROM self WHERE geocodigoFundar = 'WLD'"),
+	df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Gas natural'"),
+	df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Petróleo'"),
+	df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Carbón'"),
+	rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'geocodigoFundar': 'geocodigo'}),
+	drop_na(cols=['valor']),
+	sort_values(by=['anio'], descending=[False])
+)
+#  PIPELINE_END
+
+
+#  start()
+#  
+#  ------------------------------
+#  
+#  replace_value(col='geocodigoFundar', mapping={'OWID_WRL': 'WLD'}, alias=None)
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE geocodigoFundar = 'WLD'")
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Gas natural'")
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Petróleo'")
+#  
+#  ------------------------------
+#  
+#  df_sql(query="SELECT * FROM self WHERE fuente_energia != 'Carbón'")
+#  
+#  ------------------------------
+#  
+#  rename_cols(map={'fuente_energia': 'indicador', 'porcentaje': 'valor', 'geocodigoFundar': 'geocodigo'})
+#  
+#  ------------------------------
+#  
+#  drop_na(cols=['valor'])
+#  
+#  ------------------------------
+#  
+#  sort_values(by=['anio'], descending=[False])
+#  
+#  ------------------------------
+#  
