ccfba3cc3a2d104a633a58891853ffc147810cb9
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-28 11:24:32 -0300

    Updated INDUST/INDUST_g14_transformer.py

--- INDUST/INDUST_g14_transformer.py
+++ INDUST/INDUST_g14_transformer.py
@@ -9,6 +9,12 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     return df
 
 @transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df_copy = df.copy()
+    df_copy[col] = df_copy[col].replace(replacements)
+    return df_copy
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -23,7 +29,8 @@ def drop_col(df: DataFrame, col, axis=1):
 pipeline = chain(
 	drop_col(col=['exportaciones_industriales'], axis=1),
 	multiplicar_por_escalar(col='prop', k=100),
-	query(condition="geocodigoFundar == 'ARG'")
+	query(condition="geocodigoFundar == 'ARG'"),
+	replace_multiple_values(col='lall_desc_full', replacements={'Total manufacturas': 'Total', 'Manufacturas basadas en recursos naturales': 'Basadas en RRNN', 'Manufacturas de alta tecnología': 'Alta tecnología', 'Manufacturas de media tecnología': 'Media tecnología', 'Manufacturas de baja tecnología': 'Baja tecnología'})
 )
 #  PIPELINE_END
 
@@ -94,6 +101,23 @@ pipeline = chain(
 #  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_desc_full     |    prop |
 #  |---:|-------:|:------------------|:------------------|:-------------------|--------:|
 #  |  5 |   1962 | ARG               | Argentina         | Total manufacturas | 17.9953 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='lall_desc_full', replacements={'Total manufacturas': 'Total', 'Manufacturas basadas en recursos naturales': 'Basadas en RRNN', 'Manufacturas de alta tecnología': 'Alta tecnología', 'Manufacturas de media tecnología': 'Media tecnología', 'Manufacturas de baja tecnología': 'Baja tecnología'})
+#  Index: 310 entries, 5 to 61711
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             310 non-null    int64  
+#   1   geocodigoFundar  310 non-null    object 
+#   2   geonombreFundar  310 non-null    object 
+#   3   lall_desc_full   310 non-null    object 
+#   4   prop             310 non-null    float64
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_desc_full   |    prop |
+#  |---:|-------:|:------------------|:------------------|:-----------------|--------:|
+#  |  5 |   1962 | ARG               | Argentina         | Total            | 17.9953 |
 #  
 #  ------------------------------
 #  
