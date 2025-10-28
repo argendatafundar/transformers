b909f27c39703c25cd57b74b54f72f57f379253b
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-28 11:15:52 -0300

    Updated INDUST/INDUST_g04_transformer.py

--- INDUST/INDUST_g04_transformer.py
+++ INDUST/INDUST_g04_transformer.py
@@ -4,18 +4,24 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 
 @transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df_copy = df.copy()
+    df_copy[col] = df_copy[col].replace(replacements)
+    return df_copy
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
 #  DEFINITIONS_END
 
 
@@ -23,7 +29,8 @@ def query(df: DataFrame, condition: str):
 pipeline = chain(
 	query(condition='anio == anio.max()'),
 	drop_col(col=['provincia_id'], axis=1),
-	multiplicar_por_escalar(col='prop_industria', k=100)
+	multiplicar_por_escalar(col='prop_industria', k=100),
+	replace_multiple_values(col='provincia', replacements={'Tierra del Fuego': 'T. del Fuego', 'Santiago del Estero': 'Sgo. del Estero'})
 )
 #  PIPELINE_END
 
@@ -76,6 +83,21 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  multiplicar_por_escalar(col='prop_industria', k=100)
+#  Index: 24 entries, 0 to 23
+#  Data columns (total 3 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            24 non-null     int64  
+#   1   provincia       24 non-null     object 
+#   2   prop_industria  24 non-null     float64
+#  
+#  |    |   anio | provincia    |   prop_industria |
+#  |---:|-------:|:-------------|-----------------:|
+#  |  0 |   2023 | Buenos Aires |          48.8809 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='provincia', replacements={'Tierra del Fuego': 'T. del Fuego', 'Santiago del Estero': 'Sgo. del Estero'})
 #  Index: 24 entries, 0 to 23
 #  Data columns (total 3 columns):
 #   #   Column          Non-Null Count  Dtype  
