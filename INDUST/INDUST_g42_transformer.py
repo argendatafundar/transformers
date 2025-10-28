b0b096e66b509e9acac3a1536050174d35556b3d
jgjuara <jg.juara@gmail.com>
2025-10-28 19:09:21 -0300

    Created INDUST/INDUST_g42_transformer.py

---
+++ INDUST/INDUST_g42_transformer.py
@@ -0,0 +1,113 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def drop_col(df: DataFrame, col, axis=1):
+    return df.drop(col, axis=axis)
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def replace_value(df: DataFrame, mapping):
+    df = df.replace(mapping)
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	query(condition='anio == anio.max()'),
+	drop_col(col=['provincia_id'], axis=1),
+	multiplicar_por_escalar(col='prop_industria', k=100),
+	replace_value(mapping={'provincia': {'Santiago del Estero': 'S. del Estero', 'Tierra del Fuego': 'T. del Fuego'}})
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 24 entries, 0 to 23
+#  Data columns (total 4 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            24 non-null     int64  
+#   1   provincia_id    24 non-null     int64  
+#   2   provincia       24 non-null     object 
+#   3   prop_industria  24 non-null     float64
+#  
+#  |    |   anio |   provincia_id | provincia    |   prop_industria |
+#  |---:|-------:|---------------:|:-------------|-----------------:|
+#  |  0 |   2023 |              6 | Buenos Aires |         0.488809 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio == anio.max()')
+#  Index: 24 entries, 0 to 23
+#  Data columns (total 4 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            24 non-null     int64  
+#   1   provincia_id    24 non-null     int64  
+#   2   provincia       24 non-null     object 
+#   3   prop_industria  24 non-null     float64
+#  
+#  |    |   anio |   provincia_id | provincia    |   prop_industria |
+#  |---:|-------:|---------------:|:-------------|-----------------:|
+#  |  0 |   2023 |              6 | Buenos Aires |         0.488809 |
+#  
+#  ------------------------------
+#  
+#  drop_col(col=['provincia_id'], axis=1)
+#  Index: 24 entries, 0 to 23
+#  Data columns (total 3 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            24 non-null     int64  
+#   1   provincia       24 non-null     object 
+#   2   prop_industria  24 non-null     float64
+#  
+#  |    |   anio | provincia    |   prop_industria |
+#  |---:|-------:|:-------------|-----------------:|
+#  |  0 |   2023 | Buenos Aires |          48.8809 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='prop_industria', k=100)
+#  Index: 24 entries, 0 to 23
+#  Data columns (total 3 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            24 non-null     int64  
+#   1   provincia       24 non-null     object 
+#   2   prop_industria  24 non-null     float64
+#  
+#  |    |   anio | provincia    |   prop_industria |
+#  |---:|-------:|:-------------|-----------------:|
+#  |  0 |   2023 | Buenos Aires |          48.8809 |
+#  
+#  ------------------------------
+#  
+#  replace_value(mapping={'provincia': {'Santiago del Estero': 'S. del Estero', 'Tierra del Fuego': 'T. del Fuego'}})
+#  Index: 24 entries, 0 to 23
+#  Data columns (total 3 columns):
+#   #   Column          Non-Null Count  Dtype  
+#  ---  ------          --------------  -----  
+#   0   anio            24 non-null     int64  
+#   1   provincia       24 non-null     object 
+#   2   prop_industria  24 non-null     float64
+#  
+#  |    |   anio | provincia    |   prop_industria |
+#  |---:|-------:|:-------------|-----------------:|
+#  |  0 |   2023 | Buenos Aires |          48.8809 |
+#  
+#  ------------------------------
+#  
