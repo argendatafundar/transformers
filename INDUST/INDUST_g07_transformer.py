00cbc266b9718bdd575d7a8aece6eae9e9c0dfee
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-29 10:49:12 -0300

    Updated INDUST/INDUST_g07_transformer.py

--- INDUST/INDUST_g07_transformer.py
+++ INDUST/INDUST_g07_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def sort_values(df: DataFrame, how: str, by: list):
     if how not in ['ascending', 'descending']:
         raise ValueError('how must be either "ascending" or "descending"')
@@ -11,13 +16,15 @@ def sort_values(df: DataFrame, how: str, by: list):
     return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 
 @transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
+def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
+    import pandas as pd
+    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
+    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
+    return df.sort_values(by=[col1,col2])
 
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
@@ -26,7 +33,8 @@ def query(df: DataFrame, condition: str):
 pipeline = chain(
 	query(condition="anio == anio.max() & geocodigoFundar == 'ARG'"),
 	multiplicar_por_escalar(col='prop', k=100),
-	sort_values(how='descending', by='prop')
+	sort_values(how='descending', by='prop'),
+	ordenar_dos_columnas(col1='lall_desc_full', order1=['Productos primarios', 'Manufacturas basadas en recursos naturales', 'Manufacturas de baja tecnología', 'Manufacturas de media tecnología', 'Manufacturas de alta tecnología'], col2='lall_code', order2=['pp', 'mrrnn', 'mbt', 'mmt', 'mat'])
 )
 #  PIPELINE_END
 
@@ -94,16 +102,36 @@ pipeline = chain(
 #  sort_values(how='descending', by='prop')
 #  RangeIndex: 5 entries, 0 to 4
 #  Data columns (total 8 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   anio             5 non-null      int64  
-#   1   geocodigoFundar  5 non-null      object 
-#   2   geonombreFundar  5 non-null      object 
-#   3   lall_code        5 non-null      object 
-#   4   lall_desc_full   5 non-null      object 
-#   5   exportaciones    5 non-null      float64
-#   6   prop             5 non-null      float64
-#   7   source           5 non-null      object 
+#   #   Column           Non-Null Count  Dtype   
+#  ---  ------           --------------  -----   
+#   0   anio             5 non-null      int64   
+#   1   geocodigoFundar  5 non-null      object  
+#   2   geonombreFundar  5 non-null      object  
+#   3   lall_code        5 non-null      category
+#   4   lall_desc_full   5 non-null      category
+#   5   exportaciones    5 non-null      float64 
+#   6   prop             5 non-null      float64 
+#   7   source           5 non-null      object  
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full      |   exportaciones |    prop | source                 |
+#  |---:|-------:|:------------------|:------------------|:------------|:--------------------|----------------:|--------:|:-----------------------|
+#  |  0 |   2023 | ARG               | Argentina         | pp          | Productos primarios |     3.49511e+10 | 56.0994 | proyeccion_indice_baci |
+#  
+#  ------------------------------
+#  
+#  ordenar_dos_columnas(col1='lall_desc_full', order1=['Productos primarios', 'Manufacturas basadas en recursos naturales', 'Manufacturas de baja tecnología', 'Manufacturas de media tecnología', 'Manufacturas de alta tecnología'], col2='lall_code', order2=['pp', 'mrrnn', 'mbt', 'mmt', 'mat'])
+#  Index: 5 entries, 0 to 3
+#  Data columns (total 8 columns):
+#   #   Column           Non-Null Count  Dtype   
+#  ---  ------           --------------  -----   
+#   0   anio             5 non-null      int64   
+#   1   geocodigoFundar  5 non-null      object  
+#   2   geonombreFundar  5 non-null      object  
+#   3   lall_code        5 non-null      category
+#   4   lall_desc_full   5 non-null      category
+#   5   exportaciones    5 non-null      float64 
+#   6   prop             5 non-null      float64 
+#   7   source           5 non-null      object  
 #  
 #  |    |   anio | geocodigoFundar   | geonombreFundar   | lall_code   | lall_desc_full      |   exportaciones |    prop | source                 |
 #  |---:|-------:|:------------------|:------------------|:------------|:--------------------|----------------:|--------:|:-----------------------|
