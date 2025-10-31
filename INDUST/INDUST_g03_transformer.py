c6b376553e851b8239fdbb83a7049c48a21d60e4
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-31 12:44:25 -0300

    Updated INDUST/INDUST_g03_transformer.py

--- INDUST/INDUST_g03_transformer.py
+++ INDUST/INDUST_g03_transformer.py
@@ -8,14 +8,10 @@ def drop_col(df: DataFrame, col, axis=1):
     return df.drop(col, axis=axis)
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
-    if mapping is not None:
-        df = df.replace(mapping).copy()
-    elif col is not None and curr_value is not None and new_value is not None:
-        df = df.replace({col: curr_value}, new_value)
-    else:
-        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
-    return df
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df_copy = df.copy()
+    df_copy[col] = df_copy[col].replace(replacements)
+    return df_copy
 
 @transformer.convert
 def query(df: DataFrame, condition: str):
@@ -28,7 +24,7 @@ def query(df: DataFrame, condition: str):
 pipeline = chain(
 	query(condition='anio == anio.max()'),
 	drop_col(col=['ciiu_rev3_2d', 'rama_de_actividad'], axis=1),
-	replace_value(col='descripcion_corta', curr_value=None, new_value=None, mapping={'Productos minerales no metálicos': 'Minerales no metálicos', 'Maquinaria de oficina e informática': 'Equipos de oficina', 'Instrumentos médicos y de precisión': 'Instrumental médico'})
+	replace_multiple_values(col='descripcion_corta', replacements={'Productos minerales no metálicos': 'Minerales no metálicos', 'Maquinaria de oficina e informática': 'Equipos de oficina', 'Instrumentos médicos y de precisión': 'Instrumental médico', 'Productos del petróleo': 'Refinación de petróleo'})
 )
 #  PIPELINE_END
 
@@ -82,7 +78,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='descripcion_corta', curr_value=None, new_value=None, mapping={'Productos minerales no metálicos': 'Minerales no metálicos', 'Maquinaria de oficina e informática': 'Equipos de oficina', 'Instrumentos médicos y de precisión': 'Instrumental médico'})
+#  replace_multiple_values(col='descripcion_corta', replacements={'Productos minerales no metálicos': 'Minerales no metálicos', 'Maquinaria de oficina e informática': 'Equipos de oficina', 'Instrumentos médicos y de precisión': 'Instrumental médico', 'Productos del petróleo': 'Refinación de petróleo'})
 #  Index: 24 entries, 29 to 719
 #  Data columns (total 3 columns):
 #   #   Column                  Non-Null Count  Dtype  
