0cc55cb8cee2f476e1b97b031505ca88afe7ffd6
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-31 15:07:49 -0300

    Updated SALING/SALING_g15_transformer.py

--- SALING/SALING_g15_transformer.py
+++ SALING/SALING_g15_transformer.py
@@ -4,21 +4,20 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'variable': 'categoria', 'ano': 'anio'}),
-	replace_value(col='categoria', curr_value='AmericaLatina', new_value='América Latina')
+	replace_value(col='variable', curr_value='AmericaLatina', new_value='América Latina', mapping=None)
 )
 #  PIPELINE_END
 
@@ -38,33 +37,18 @@ rename_cols(map={'variable': 'categoria', 'ano': 'anio'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'variable': 'categoria', 'ano': 'anio'})
+#  replace_value(col='variable', curr_value='AmericaLatina', new_value='América Latina', mapping=None)
 #  RangeIndex: 60 entries, 0 to 59
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       60 non-null     int64  
-#   1   categoria  60 non-null     object 
-#   2   valor      60 non-null     float64
+#   #   Column    Non-Null Count  Dtype  
+#  ---  ------    --------------  -----  
+#   0   ano       60 non-null     int64  
+#   1   variable  60 non-null     object 
+#   2   valor     60 non-null     float64
 #  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | Argentina   |     6.2 |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='categoria', curr_value='AmericaLatina', new_value='América Latina')
-#  RangeIndex: 60 entries, 0 to 59
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       60 non-null     int64  
-#   1   categoria  60 non-null     object 
-#   2   valor      60 non-null     float64
-#  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1992 | Argentina   |     6.2 |
+#  |    |   ano | variable   |   valor |
+#  |---:|------:|:-----------|--------:|
+#  |  0 |  1992 | Argentina  |     6.2 |
 #  
 #  ------------------------------
 #  
