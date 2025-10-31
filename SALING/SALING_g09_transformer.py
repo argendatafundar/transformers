fb338fcaf9382cdae158a41886add1be023edd56
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-31 14:43:08 -0300

    Updated SALING/SALING_g09_transformer.py

--- SALING/SALING_g09_transformer.py
+++ SALING/SALING_g09_transformer.py
@@ -4,56 +4,40 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
+
+@transformer.convert
 def concatenar_columnas(df:DataFrame, cols:list, nueva_col:str, separtor:str = "-"):
     df[nueva_col] = df[cols].astype(str).agg(separtor.join, axis=1)
-    return df
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'),
-	drop_col(col=['year', 'semestre'], axis=1),
-	rename_cols(map={'genero': 'categoria', 'proporcion': 'valor'}),
-	multiplicar_por_escalar(col='valor', k=100),
-	replace_value(col='categoria', curr_value='Varon', new_value='Varón')
+	concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'),
+	sort_values(how='ascending', by=['year', 'semestre'])
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 80 entries, 0 to 79
-#  Data columns (total 5 columns):
+#  Data columns (total 4 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
 #   0   year        80 non-null     int64  
 #   1   semestre    80 non-null     int64  
 #   2   genero      80 non-null     object 
 #   3   proporcion  76 non-null     float64
-#   4   aniosem     80 non-null     object 
 #  
-#  |    |   year |   semestre | genero   |   proporcion | aniosem   |
-#  |---:|-------:|-----------:|:---------|-------------:|:----------|
-#  |  0 |   2003 |          2 | Mujer    |     0.727663 | 2003-2    |
+#  |    |   year |   semestre | genero   |   proporcion |
+#  |---:|-------:|-----------:|:---------|-------------:|
+#  |  0 |   2003 |          2 | Mujer    |     0.727663 |
 #  
 #  ------------------------------
 #  
@@ -74,63 +58,20 @@ concatenar_columnas(cols=['year', 'semestre'], nueva_col='aniosem', separtor='-'
 #  
 #  ------------------------------
 #  
-#  drop_col(col=['year', 'semestre'], axis=1)
+#  sort_values(how='ascending', by=['year', 'semestre'])
 #  RangeIndex: 80 entries, 0 to 79
-#  Data columns (total 3 columns):
+#  Data columns (total 5 columns):
 #   #   Column      Non-Null Count  Dtype  
 #  ---  ------      --------------  -----  
-#   0   genero      80 non-null     object 
-#   1   proporcion  76 non-null     float64
-#   2   aniosem     80 non-null     object 
+#   0   year        80 non-null     int64  
+#   1   semestre    80 non-null     int64  
+#   2   genero      80 non-null     object 
+#   3   proporcion  76 non-null     float64
+#   4   aniosem     80 non-null     object 
 #  
-#  |    | genero   |   proporcion | aniosem   |
-#  |---:|:---------|-------------:|:----------|
-#  |  0 | Mujer    |     0.727663 | 2003-2    |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'genero': 'categoria', 'proporcion': 'valor'})
-#  RangeIndex: 80 entries, 0 to 79
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  80 non-null     object 
-#   1   valor      76 non-null     float64
-#   2   aniosem    80 non-null     object 
-#  
-#  |    | categoria   |   valor | aniosem   |
-#  |---:|:------------|--------:|:----------|
-#  |  0 | Mujer       | 72.7663 | 2003-2    |
-#  
-#  ------------------------------
-#  
-#  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 80 entries, 0 to 79
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  80 non-null     object 
-#   1   valor      76 non-null     float64
-#   2   aniosem    80 non-null     object 
-#  
-#  |    | categoria   |   valor | aniosem   |
-#  |---:|:------------|--------:|:----------|
-#  |  0 | Mujer       | 72.7663 | 2003-2    |
-#  
-#  ------------------------------
-#  
-#  replace_value(col='categoria', curr_value='Varon', new_value='Varón')
-#  RangeIndex: 80 entries, 0 to 79
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  80 non-null     object 
-#   1   valor      76 non-null     float64
-#   2   aniosem    80 non-null     object 
-#  
-#  |    | categoria   |   valor | aniosem   |
-#  |---:|:------------|--------:|:----------|
-#  |  0 | Mujer       | 72.7663 | 2003-2    |
+#  |    |   year |   semestre | genero   |   proporcion | aniosem   |
+#  |---:|-------:|-----------:|:---------|-------------:|:----------|
+#  |  0 |   2003 |          2 | Mujer    |     0.727663 | 2003-2    |
 #  
 #  ------------------------------
 #  
