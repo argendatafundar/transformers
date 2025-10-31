863dfff8b933469de154fd7068115fe5338799b0
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-10-31 16:12:02 -0300

    Updated SALING/SALING_g25_transformer.py

--- SALING/SALING_g25_transformer.py
+++ SALING/SALING_g25_transformer.py
@@ -4,46 +4,47 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'salario_real_ppa_consumo_privado_2017': 'valor'})
+	identity()
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 1717 entries, 0 to 1716
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column                                 Non-Null Count  Dtype  
 #  ---  ------                                 --------------  -----  
-#   0   iso3                                   1717 non-null   object 
-#   1   anio                                   1717 non-null   int64  
-#   2   salario_real_ppa_consumo_privado_2017  1717 non-null   float64
+#   0   geocodigoFundar                        1717 non-null   object 
+#   1   geonombreFundar                        1717 non-null   object 
+#   2   anio                                   1717 non-null   int64  
+#   3   salario_real_ppa_consumo_privado_2017  1717 non-null   float64
 #  
-#  |    | iso3   |   anio |   salario_real_ppa_consumo_privado_2017 |
-#  |---:|:-------|-------:|----------------------------------------:|
-#  |  0 | USA    |   1929 |                                 1881.73 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   salario_real_ppa_consumo_privado_2017 |
+#  |---:|:------------------|:------------------|-------:|----------------------------------------:|
+#  |  0 | USA               | Estados Unidos    |   1929 |                                 1881.73 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'salario_real_ppa_consumo_privado_2017': 'valor'})
+#  identity()
 #  RangeIndex: 1717 entries, 0 to 1716
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  1717 non-null   object 
-#   1   anio       1717 non-null   int64  
-#   2   valor      1717 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column                                 Non-Null Count  Dtype  
+#  ---  ------                                 --------------  -----  
+#   0   geocodigoFundar                        1717 non-null   object 
+#   1   geonombreFundar                        1717 non-null   object 
+#   2   anio                                   1717 non-null   int64  
+#   3   salario_real_ppa_consumo_privado_2017  1717 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | USA         |   1929 | 1881.73 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   salario_real_ppa_consumo_privado_2017 |
+#  |---:|:------------------|:------------------|-------:|----------------------------------------:|
+#  |  0 | USA               | Estados Unidos    |   1929 |                                 1881.73 |
 #  
 #  ------------------------------
 #  
