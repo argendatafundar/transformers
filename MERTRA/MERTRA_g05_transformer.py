460872d47ee4bee825ba7ab5ca4538e46d3e739b
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 12:20:16 -0300

    Created MERTRA/MERTRA_g05_transformer.py

---
+++ MERTRA/MERTRA_g05_transformer.py
@@ -0,0 +1,96 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df_copy = df.copy()
+    df_copy[col] = df_copy[col].replace(replacements)
+    return df_copy
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	multiplicar_por_escalar(col='valor', k=100),
+	query(condition="apertura_sexo != 'total'"),
+	replace_multiple_values(col='apertura_sexo', replacements={'varon': 'Varones', 'mujer': 'Mujeres', 'brecha': 'Brecha'})
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 2592 entries, 0 to 2591
+#  Data columns (total 4 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   anio           2592 non-null   int64  
+#   1   edad           2592 non-null   int64  
+#   2   apertura_sexo  2592 non-null   object 
+#   3   valor          2592 non-null   float64
+#  
+#  |    |   anio |   edad | apertura_sexo   |      valor |
+#  |---:|-------:|-------:|:----------------|-----------:|
+#  |  0 |   2016 |     10 | total           | 0.00156134 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='valor', k=100)
+#  RangeIndex: 2592 entries, 0 to 2591
+#  Data columns (total 4 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   anio           2592 non-null   int64  
+#   1   edad           2592 non-null   int64  
+#   2   apertura_sexo  2592 non-null   object 
+#   3   valor          2592 non-null   float64
+#  
+#  |    |   anio |   edad | apertura_sexo   |    valor |
+#  |---:|-------:|-------:|:----------------|---------:|
+#  |  0 |   2016 |     10 | total           | 0.156134 |
+#  
+#  ------------------------------
+#  
+#  query(condition="apertura_sexo != 'total'")
+#  Index: 1944 entries, 1 to 2591
+#  Data columns (total 4 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   anio           1944 non-null   int64  
+#   1   edad           1944 non-null   int64  
+#   2   apertura_sexo  1944 non-null   object 
+#   3   valor          1944 non-null   float64
+#  
+#  |    |   anio |   edad | apertura_sexo   |    valor |
+#  |---:|-------:|-------:|:----------------|---------:|
+#  |  1 |   2016 |     10 | varon           | 0.304733 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='apertura_sexo', replacements={'varon': 'Varones', 'mujer': 'Mujeres', 'brecha': 'Brecha'})
+#  Index: 1944 entries, 1 to 2591
+#  Data columns (total 4 columns):
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   anio           1944 non-null   int64  
+#   1   edad           1944 non-null   int64  
+#   2   apertura_sexo  1944 non-null   object 
+#   3   valor          1944 non-null   float64
+#  
+#  |    |   anio |   edad | apertura_sexo   |    valor |
+#  |---:|-------:|-------:|:----------------|---------:|
+#  |  1 |   2016 |     10 | Varones         | 0.304733 |
+#  
+#  ------------------------------
+#  
