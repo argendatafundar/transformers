6c2e258442f1a00d51f045adcd761a80d03ae06d
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 11:39:55 -0300

    Updated MERTRA/MERTRA_g03_transformer.py

--- MERTRA/MERTRA_g03_transformer.py
+++ MERTRA/MERTRA_g03_transformer.py
@@ -7,12 +7,18 @@ from data_transformers import chain, transformer
 def drop_na(df:DataFrame, col:str):
     df = df.dropna(subset=col, axis=0)
     return df
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_na(col='tasa_actividad')
+	drop_na(col='tasa_actividad'),
+	multiplicar_por_escalar(col='tasa_actividad', k=100)
 )
 #  PIPELINE_END
 
@@ -47,7 +53,24 @@ pipeline = chain(
 #  
 #  |    | geocodigoFundar   | geonombreFundar              |   anio |   tasa_actividad | nivel_agregacion   |
 #  |---:|:------------------|:-----------------------------|-------:|-----------------:|:-------------------|
-#  |  0 | AFE               | África Oriental y Meridional |   2023 |         0.412985 | agregacion         |
+#  |  0 | AFE               | África Oriental y Meridional |   2023 |          41.2985 | agregacion         |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='tasa_actividad', k=100)
+#  Index: 7620 entries, 0 to 16672
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   geocodigoFundar   7620 non-null   object 
+#   1   geonombreFundar   7620 non-null   object 
+#   2   anio              7620 non-null   int64  
+#   3   tasa_actividad    7620 non-null   float64
+#   4   nivel_agregacion  7620 non-null   object 
+#  
+#  |    | geocodigoFundar   | geonombreFundar              |   anio |   tasa_actividad | nivel_agregacion   |
+#  |---:|:------------------|:-----------------------------|-------:|-----------------:|:-------------------|
+#  |  0 | AFE               | África Oriental y Meridional |   2023 |          41.2985 | agregacion         |
 #  
 #  ------------------------------
 #  
