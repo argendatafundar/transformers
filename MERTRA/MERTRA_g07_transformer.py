bd068743ef003ca8d3f4c1b66ccb2e4f5879411b
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 13:21:43 -0300

    Updated MERTRA/MERTRA_g07_transformer.py

--- MERTRA/MERTRA_g07_transformer.py
+++ MERTRA/MERTRA_g07_transformer.py
@@ -4,111 +4,70 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
 def drop_na(df:DataFrame, col:str):
     df = df.dropna(subset=col, axis=0)
     return df
 
 @transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
-
-@transformer.convert
-def multiplicar_por_escalar(df:DataFrame, col:str, k:float):
-    df[col] = df[col] * k
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'iso3': 'geocodigo', 'puestos_per_capita': 'valor'}),
-	drop_na(col='valor'),
-	sort_values(how='ascending', by=['anio', 'geocodigo']),
-	multiplicar_por_escalar(col='valor', k=100)
+	drop_na(col='puestos_per_capita'),
+	multiplicar_por_escalar(col='puestos_per_capita', k=100)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 12810 entries, 0 to 12809
-#  Data columns (total 3 columns):
+#  Data columns (total 4 columns):
 #   #   Column              Non-Null Count  Dtype  
 #  ---  ------              --------------  -----  
-#   0   iso3                12810 non-null  object 
-#   1   anio                12810 non-null  int64  
-#   2   puestos_per_capita  9529 non-null   float64
+#   0   geocodigoFundar     12810 non-null  object 
+#   1   geonombreFundar     12810 non-null  object 
+#   2   anio                12810 non-null  int64  
+#   3   puestos_per_capita  9529 non-null   float64
 #  
-#  |    | iso3   |   anio |   puestos_per_capita |
-#  |---:|:-------|-------:|---------------------:|
-#  |  0 | ABW    |   1950 |                  nan |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo', 'puestos_per_capita': 'valor'})
-#  RangeIndex: 12810 entries, 0 to 12809
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  12810 non-null  object 
-#   1   anio       12810 non-null  int64  
-#   2   valor      9529 non-null   float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ABW         |   1950 |     nan |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   puestos_per_capita |
+#  |---:|:------------------|:------------------|-------:|---------------------:|
+#  |  0 | ABW               | Aruba             |   1950 |                  nan |
 #  
 #  ------------------------------
 #  
-#  drop_na(col='valor')
+#  drop_na(col='puestos_per_capita')
 #  Index: 9529 entries, 41 to 12809
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  9529 non-null   object 
-#   1   anio       9529 non-null   int64  
-#   2   valor      9529 non-null   float64
+#  Data columns (total 4 columns):
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   geocodigoFundar     9529 non-null   object 
+#   1   geonombreFundar     9529 non-null   object 
+#   2   anio                9529 non-null   int64  
+#   3   puestos_per_capita  9529 non-null   float64
 #  
-#  |    | geocodigo   |   anio |    valor |
-#  |---:|:------------|-------:|---------:|
-#  | 41 | ABW         |   1991 | 0.451859 |
-#  
-#  ------------------------------
-#  
-#  sort_values(how='ascending', by=['anio', 'geocodigo'])
-#  RangeIndex: 9529 entries, 0 to 9528
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  9529 non-null   object 
-#   1   anio       9529 non-null   int64  
-#   2   valor      9529 non-null   float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ARG         |   1950 | 38.6666 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   puestos_per_capita |
+#  |---:|:------------------|:------------------|-------:|---------------------:|
+#  | 41 | ABW               | Aruba             |   1991 |              45.1859 |
 #  
 #  ------------------------------
 #  
-#  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 9529 entries, 0 to 9528
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  9529 non-null   object 
-#   1   anio       9529 non-null   int64  
-#   2   valor      9529 non-null   float64
+#  multiplicar_por_escalar(col='puestos_per_capita', k=100)
+#  Index: 9529 entries, 41 to 12809
+#  Data columns (total 4 columns):
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   geocodigoFundar     9529 non-null   object 
+#   1   geonombreFundar     9529 non-null   object 
+#   2   anio                9529 non-null   int64  
+#   3   puestos_per_capita  9529 non-null   float64
 #  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ARG         |   1950 | 38.6666 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   puestos_per_capita |
+#  |---:|:------------------|:------------------|-------:|---------------------:|
+#  | 41 | ABW               | Aruba             |   1991 |              45.1859 |
 #  
 #  ------------------------------
 #  
