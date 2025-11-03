7adab6f5fc0187ac790137c10f30a84f8d5f9509
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 15:03:59 -0300

    Updated MERTRA/MERTRA_g08_transformer.py

--- MERTRA/MERTRA_g08_transformer.py
+++ MERTRA/MERTRA_g08_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def imput_na(df:DataFrame, bool_mask:list[bool], col:str, value:Any):
+    df.loc[bool_mask,col] = value
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
@@ -12,7 +17,8 @@ def query(df: DataFrame, condition: str):
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio == anio.max()')
+	query(condition='anio == anio.max()'),
+	imput_na(bool_mask=[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True], col='geonombreFundar', value='Argentina')
 )
 #  PIPELINE_END
 
@@ -39,7 +45,23 @@ pipeline = chain(
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
 #   0   geocodigoFundar  24 non-null     object 
-#   1   geonombreFundar  24 non-null     object 
+#   1   geonombreFundar  25 non-null     object 
+#   2   anio             25 non-null     int64  
+#   3   tasa_empleo      25 non-null     float64
+#  
+#  |     | geocodigoFundar   | geonombreFundar   |   anio |   tasa_empleo |
+#  |----:|:------------------|:------------------|-------:|--------------:|
+#  | 165 | AR-C              | CABA              |   2023 |      0.528291 |
+#  
+#  ------------------------------
+#  
+#  imput_na(bool_mask=[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True], col='geonombreFundar', value='Argentina')
+#  Index: 25 entries, 165 to 196
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  24 non-null     object 
+#   1   geonombreFundar  25 non-null     object 
 #   2   anio             25 non-null     int64  
 #   3   tasa_empleo      25 non-null     float64
 #  
