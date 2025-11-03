edcc9270d3adb24a175e08c7e207d66c6a6e32df
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 16:30:13 -0300

    Updated MERTRA/MERTRA_g16_transformer.py

--- MERTRA/MERTRA_g16_transformer.py
+++ MERTRA/MERTRA_g16_transformer.py
@@ -4,114 +4,73 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 
 @transformer.convert
-def latest_year(df, by='anio'):
-    latest_year = df[by].max()
-    df = df.query(f'{by} == {latest_year}')
-    df = df.drop(columns = by)
-    return df
-
-@transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'provincia': 'categoria', 'nivel_ed_fundar': 'indicador', 'ocupado': 'valor'}),
-	multiplicar_por_escalar(col='valor', k=100),
-	latest_year(by='anio'),
-	replace_value(col='categoria', curr_value='Ciudad de Buenos Aires', new_value='CABA')
+	query(condition='anio == anio.max()'),
+	multiplicar_por_escalar(col='ocupado', k=100)
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 756 entries, 0 to 755
-#  Data columns (total 4 columns):
+#  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             756 non-null    int64  
-#   1   provincia        756 non-null    object 
-#   2   nivel_ed_fundar  756 non-null    object 
-#   3   ocupado          756 non-null    float64
+#   0   geocodigoFundar  756 non-null    object 
+#   1   geonombreFundar  756 non-null    object 
+#   2   anio             756 non-null    int64  
+#   3   nivel_ed_fundar  756 non-null    object 
+#   4   ocupado          756 non-null    float64
 #  
-#  |    |   anio | provincia    | nivel_ed_fundar             |   ocupado |
-#  |---:|-------:|:-------------|:----------------------------|----------:|
-#  |  0 |   2016 | Buenos Aires | Hasta secundario incompleto |  0.647369 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'provincia': 'categoria', 'nivel_ed_fundar': 'indicador', 'ocupado': 'valor'})
-#  RangeIndex: 756 entries, 0 to 755
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       756 non-null    int64  
-#   1   categoria  756 non-null    object 
-#   2   indicador  756 non-null    object 
-#   3   valor      756 non-null    float64
-#  
-#  |    |   anio | categoria    | indicador                   |   valor |
-#  |---:|-------:|:-------------|:----------------------------|--------:|
-#  |  0 |   2016 | Buenos Aires | Hasta secundario incompleto | 64.7369 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio | nivel_ed_fundar             |   ocupado |
+#  |---:|:------------------|:------------------|-------:|:----------------------------|----------:|
+#  |  0 | AR-B              | Buenos Aires      |   2016 | Hasta secundario incompleto |  0.647369 |
 #  
 #  ------------------------------
 #  
-#  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 756 entries, 0 to 755
-#  Data columns (total 4 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       756 non-null    int64  
-#   1   categoria  756 non-null    object 
-#   2   indicador  756 non-null    object 
-#   3   valor      756 non-null    float64
-#  
-#  |    |   anio | categoria    | indicador                   |   valor |
-#  |---:|-------:|:-------------|:----------------------------|--------:|
-#  |  0 |   2016 | Buenos Aires | Hasta secundario incompleto | 64.7369 |
-#  
-#  ------------------------------
-#  
-#  latest_year(by='anio')
+#  query(condition='anio == anio.max()')
 #  Index: 96 entries, 495 to 755
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  96 non-null     object 
-#   1   indicador  96 non-null     object 
-#   2   valor      96 non-null     float64
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  96 non-null     object 
+#   1   geonombreFundar  96 non-null     object 
+#   2   anio             96 non-null     int64  
+#   3   nivel_ed_fundar  96 non-null     object 
+#   4   ocupado          96 non-null     float64
 #  
-#  |     | categoria    | indicador                   |   valor |
-#  |----:|:-------------|:----------------------------|--------:|
-#  | 495 | Buenos Aires | Hasta secundario incompleto | 69.1307 |
+#  |     | geocodigoFundar   | geonombreFundar   |   anio | nivel_ed_fundar             |   ocupado |
+#  |----:|:------------------|:------------------|-------:|:----------------------------|----------:|
+#  | 495 | AR-B              | Buenos Aires      |   2023 | Hasta secundario incompleto |   69.1307 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='categoria', curr_value='Ciudad de Buenos Aires', new_value='CABA')
+#  multiplicar_por_escalar(col='ocupado', k=100)
 #  Index: 96 entries, 495 to 755
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  96 non-null     object 
-#   1   indicador  96 non-null     object 
-#   2   valor      96 non-null     float64
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  96 non-null     object 
+#   1   geonombreFundar  96 non-null     object 
+#   2   anio             96 non-null     int64  
+#   3   nivel_ed_fundar  96 non-null     object 
+#   4   ocupado          96 non-null     float64
 #  
-#  |     | categoria    | indicador                   |   valor |
-#  |----:|:-------------|:----------------------------|--------:|
-#  | 495 | Buenos Aires | Hasta secundario incompleto | 69.1307 |
+#  |     | geocodigoFundar   | geonombreFundar   |   anio | nivel_ed_fundar             |   ocupado |
+#  |----:|:------------------|:------------------|-------:|:----------------------------|----------:|
+#  | 495 | AR-B              | Buenos Aires      |   2023 | Hasta secundario incompleto |   69.1307 |
 #  
 #  ------------------------------
 #  
