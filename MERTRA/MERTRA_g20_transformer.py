94064a46dfc3601ed3a8b550e6a218b647dc55bc
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 17:07:16 -0300

    Updated MERTRA/MERTRA_g20_transformer.py

--- MERTRA/MERTRA_g20_transformer.py
+++ MERTRA/MERTRA_g20_transformer.py
@@ -4,21 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sexo': 'indicador', 'tipo_trabajo': 'categoria', 'minutos': 'valor'}),
-	query(condition="indicador != 'Total'")
+	identity()
 )
 #  PIPELINE_END
 
@@ -28,43 +21,28 @@ rename_cols(map={'sexo': 'indicador', 'tipo_trabajo': 'categoria', 'minutos': 'v
 #  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
 #  ---  ------        --------------  -----  
-#   0   tipo_trabajo  9 non-null      object 
-#   1   sexo          9 non-null      object 
+#   0   sexo          9 non-null      object 
+#   1   tipo_trabajo  9 non-null      object 
 #   2   minutos       9 non-null      float64
 #  
-#  |    | tipo_trabajo                                          | sexo    |   minutos |
-#  |---:|:------------------------------------------------------|:--------|----------:|
-#  |  0 | Apoyo a otros hogares, para la comunidad y voluntario | Mujeres |    22.134 |
+#  |    | sexo   | tipo_trabajo   |   minutos |
+#  |---:|:-------|:---------------|----------:|
+#  |  0 | Total  | Doméstico      |       168 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sexo': 'indicador', 'tipo_trabajo': 'categoria', 'minutos': 'valor'})
+#  identity()
 #  RangeIndex: 9 entries, 0 to 8
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  9 non-null      object 
-#   1   indicador  9 non-null      object 
-#   2   valor      9 non-null      float64
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   sexo          9 non-null      object 
+#   1   tipo_trabajo  9 non-null      object 
+#   2   minutos       9 non-null      float64
 #  
-#  |    | categoria                                             | indicador   |   valor |
-#  |---:|:------------------------------------------------------|:------------|--------:|
-#  |  0 | Apoyo a otros hogares, para la comunidad y voluntario | Mujeres     |  22.134 |
-#  
-#  ------------------------------
-#  
-#  query(condition="indicador != 'Total'")
-#  Index: 6 entries, 0 to 8
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  6 non-null      object 
-#   1   indicador  6 non-null      object 
-#   2   valor      6 non-null      float64
-#  
-#  |    | categoria                                             | indicador   |   valor |
-#  |---:|:------------------------------------------------------|:------------|--------:|
-#  |  0 | Apoyo a otros hogares, para la comunidad y voluntario | Mujeres     |  22.134 |
+#  |    | sexo   | tipo_trabajo   |   minutos |
+#  |---:|:-------|:---------------|----------:|
+#  |  0 | Total  | Doméstico      |       168 |
 #  
 #  ------------------------------
 #  
