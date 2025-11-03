9dbd6f57557be4d05fbf2e19f245ef136f9cf8c3
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-03 17:03:47 -0300

    Updated MERTRA/MERTRA_g19_transformer.py

--- MERTRA/MERTRA_g19_transformer.py
+++ MERTRA/MERTRA_g19_transformer.py
@@ -4,21 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sexo': 'indicador', 'nivel_educativo': 'categoria', 'minutos': 'valor'}),
-	query(condition="indicador != 'Total'")
+	identity()
 )
 #  PIPELINE_END
 
@@ -32,39 +25,24 @@ rename_cols(map={'sexo': 'indicador', 'nivel_educativo': 'categoria', 'minutos':
 #   1   nivel_educativo  15 non-null     object 
 #   2   minutos          15 non-null     float64
 #  
-#  |    | sexo    | nivel_educativo           |   minutos |
-#  |---:|:--------|:--------------------------|----------:|
-#  |  0 | Mujeres | Hasta primario incompleto |   410.242 |
+#  |    | sexo   | nivel_educativo   |   minutos |
+#  |---:|:-------|:------------------|----------:|
+#  |  0 | Total  | Total             |   266.419 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sexo': 'indicador', 'nivel_educativo': 'categoria', 'minutos': 'valor'})
+#  identity()
 #  RangeIndex: 15 entries, 0 to 14
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   indicador  15 non-null     object 
-#   1   categoria  15 non-null     object 
-#   2   valor      15 non-null     float64
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   sexo             15 non-null     object 
+#   1   nivel_educativo  15 non-null     object 
+#   2   minutos          15 non-null     float64
 #  
-#  |    | indicador   | categoria                 |   valor |
-#  |---:|:------------|:--------------------------|--------:|
-#  |  0 | Mujeres     | Hasta primario incompleto | 410.242 |
-#  
-#  ------------------------------
-#  
-#  query(condition="indicador != 'Total'")
-#  Index: 10 entries, 0 to 14
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   indicador  10 non-null     object 
-#   1   categoria  10 non-null     object 
-#   2   valor      10 non-null     float64
-#  
-#  |    | indicador   | categoria                 |   valor |
-#  |---:|:------------|:--------------------------|--------:|
-#  |  0 | Mujeres     | Hasta primario incompleto | 410.242 |
+#  |    | sexo   | nivel_educativo   |   minutos |
+#  |---:|:-------|:------------------|----------:|
+#  |  0 | Total  | Total             |   266.419 |
 #  
 #  ------------------------------
 #  
