4a203b8f9ade000a670948b83545ffca5a412689
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 16:06:42 -0300

    Updated INFDES/INFDES_g14_transformer.py

--- INFDES/INFDES_g14_transformer.py
+++ INFDES/INFDES_g14_transformer.py
@@ -4,40 +4,21 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
-    return df
-
-@transformer.convert
-def sort_values(df: DataFrame, how: str, by: list):
-    if how not in ['ascending', 'descending']:
-        raise ValueError('how must be either "ascending" or "descending"')
-    
-    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-query(condition='anio == anio.max()'),
-	sort_values(how='ascending', by=['rango_edad', 'sexo']),
-	drop_col(col=['anio', 'rango_edad'], axis=1),
-	rename_cols(map={'rango_edad_desc': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'}),
-	mutiplicar_por_escalar(col='valor', k=100)
+	query(condition='anio == anio.max()'),
+	multiplicar_por_escalar(col='tasa_desocupacion', k=100)
 )
 #  PIPELINE_END
 
@@ -72,12 +53,12 @@ query(condition='anio == anio.max()'),
 #  
 #  |    |   anio |   rango_edad | rango_edad_desc   | sexo    |   tasa_desocupacion |
 #  |---:|-------:|-------------:|:------------------|:--------|--------------------:|
-#  | 56 |   2023 |            1 | Hasta 30          | Mujeres |            0.124757 |
+#  | 56 |   2023 |            1 | Hasta 30          | Mujeres |             12.4757 |
 #  
 #  ------------------------------
 #  
-#  sort_values(how='ascending', by=['rango_edad', 'sexo'])
-#  RangeIndex: 8 entries, 0 to 7
+#  multiplicar_por_escalar(col='tasa_desocupacion', k=100)
+#  Index: 8 entries, 56 to 63
 #  Data columns (total 5 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
@@ -89,52 +70,7 @@ query(condition='anio == anio.max()'),
 #  
 #  |    |   anio |   rango_edad | rango_edad_desc   | sexo    |   tasa_desocupacion |
 #  |---:|-------:|-------------:|:------------------|:--------|--------------------:|
-#  |  0 |   2023 |            1 | Hasta 30          | Mujeres |            0.124757 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col=['anio', 'rango_edad'], axis=1)
-#  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
-#   #   Column             Non-Null Count  Dtype  
-#  ---  ------             --------------  -----  
-#   0   rango_edad_desc    8 non-null      object 
-#   1   sexo               8 non-null      object 
-#   2   tasa_desocupacion  8 non-null      float64
-#  
-#  |    | rango_edad_desc   | sexo    |   tasa_desocupacion |
-#  |---:|:------------------|:--------|--------------------:|
-#  |  0 | Hasta 30          | Mujeres |            0.124757 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'rango_edad_desc': 'categoria', 'sexo': 'indicador', 'tasa_desocupacion': 'valor'})
-#  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  8 non-null      object 
-#   1   indicador  8 non-null      object 
-#   2   valor      8 non-null      float64
-#  
-#  |    | categoria   | indicador   |   valor |
-#  |---:|:------------|:------------|--------:|
-#  |  0 | Hasta 30    | Mujeres     | 12.4757 |
-#  
-#  ------------------------------
-#  
-#  mutiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 8 entries, 0 to 7
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   categoria  8 non-null      object 
-#   1   indicador  8 non-null      object 
-#   2   valor      8 non-null      float64
-#  
-#  |    | categoria   | indicador   |   valor |
-#  |---:|:------------|:------------|--------:|
-#  |  0 | Hasta 30    | Mujeres     | 12.4757 |
+#  | 56 |   2023 |            1 | Hasta 30          | Mujeres |             12.4757 |
 #  
 #  ------------------------------
 #  
