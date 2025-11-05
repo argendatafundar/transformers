1e293c5c648ed27003f630f69193f7ec85b624b7
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 12:45:44 -0300

    Updated INFDES/INFDES_g08_transformer.py

--- INFDES/INFDES_g08_transformer.py
+++ INFDES/INFDES_g08_transformer.py
@@ -4,21 +4,21 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
     return df
 
 @transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'apertura_sexo': 'categoria'}),
-	mutiplicar_por_escalar(col='valor', k=100)
+	multiplicar_por_escalar(col='valor', k=100),
+	query(condition="apertura_sexo != 'Brecha'")
 )
 #  PIPELINE_END
 
@@ -38,33 +38,33 @@ rename_cols(map={'apertura_sexo': 'categoria'}),
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'apertura_sexo': 'categoria'})
+#  multiplicar_por_escalar(col='valor', k=100)
 #  RangeIndex: 105 entries, 0 to 104
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       105 non-null    int64  
-#   1   categoria  105 non-null    object 
-#   2   valor      105 non-null    float64
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   anio           105 non-null    int64  
+#   1   apertura_sexo  105 non-null    object 
+#   2   valor          105 non-null    float64
 #  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1986 | Mujer       | 29.9352 |
+#  |    |   anio | apertura_sexo   |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1986 | Mujer           | 29.9352 |
 #  
 #  ------------------------------
 #  
-#  mutiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 105 entries, 0 to 104
+#  query(condition="apertura_sexo != 'Brecha'")
+#  Index: 70 entries, 0 to 103
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       105 non-null    int64  
-#   1   categoria  105 non-null    object 
-#   2   valor      105 non-null    float64
+#   #   Column         Non-Null Count  Dtype  
+#  ---  ------         --------------  -----  
+#   0   anio           70 non-null     int64  
+#   1   apertura_sexo  70 non-null     object 
+#   2   valor          70 non-null     float64
 #  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1986 | Mujer       | 29.9352 |
+#  |    |   anio | apertura_sexo   |   valor |
+#  |---:|-------:|:----------------|--------:|
+#  |  0 |   1986 | Mujer           | 29.9352 |
 #  
 #  ------------------------------
 #  
