e8297d14e490e79f6a3b6543fd730e85508c552a
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 12:04:17 -0300

    Updated INFDES/INFDES_g04_transformer.py

--- INFDES/INFDES_g04_transformer.py
+++ INFDES/INFDES_g04_transformer.py
@@ -4,15 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def query(df: DataFrame, condition: str):
-    df = df.query(condition)    
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio>=1988')
+	identity()
 )
 #  PIPELINE_END
 
@@ -32,14 +31,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  query(condition='anio>=1988')
-#  Index: 65 entries, 0 to 66
+#  identity()
+#  RangeIndex: 67 entries, 0 to 66
 #  Data columns (total 3 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   anio               65 non-null     int64  
-#   1   tipo_informalidad  65 non-null     object 
-#   2   valor              65 non-null     float64
+#   0   anio               67 non-null     int64  
+#   1   tipo_informalidad  67 non-null     object 
+#   2   valor              67 non-null     float64
 #  
 #  |    |   anio | tipo_informalidad     |   valor |
 #  |---:|-------:|:----------------------|--------:|
