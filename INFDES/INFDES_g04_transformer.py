8c384d5cfeb50e16f2cb1bea3d2070ee8b5e48af
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 12:03:40 -0300

    Updated INFDES/INFDES_g04_transformer.py

--- INFDES/INFDES_g04_transformer.py
+++ INFDES/INFDES_g04_transformer.py
@@ -4,15 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_na(df:DataFrame, col:str):
-    df = df.dropna(subset=col, axis=0)
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	drop_na(col='valor')
+	query(condition='anio>1986')
 )
 #  PIPELINE_END
 
@@ -32,14 +32,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  drop_na(col='valor')
-#  RangeIndex: 67 entries, 0 to 66
+#  query(condition='anio>1986')
+#  Index: 66 entries, 0 to 66
 #  Data columns (total 3 columns):
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
-#   0   anio               67 non-null     int64  
-#   1   tipo_informalidad  67 non-null     object 
-#   2   valor              67 non-null     float64
+#   0   anio               66 non-null     int64  
+#   1   tipo_informalidad  66 non-null     object 
+#   2   valor              66 non-null     float64
 #  
 #  |    |   anio | tipo_informalidad     |   valor |
 #  |---:|-------:|:----------------------|--------:|
