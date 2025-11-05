f92fb2f0d58a92f48a8a84b89419903bb1509943
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 12:11:38 -0300

    Updated INFDES/INFDES_g05_transformer.py

--- INFDES/INFDES_g05_transformer.py
+++ INFDES/INFDES_g05_transformer.py
@@ -4,20 +4,7 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
 #  DEFINITIONS_END
@@ -25,10 +12,7 @@ def mutiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='pais', axis=1),
-	drop_col(col='serie', axis=1),
-	rename_cols(map={'iso3': 'geocodigo'}),
-	mutiplicar_por_escalar(col='valor', k=100)
+	multiplicar_por_escalar(col='valor', k=100)
 )
 #  PIPELINE_END
 
@@ -36,78 +20,34 @@ drop_col(col='pais', axis=1),
 #  start()
 #  RangeIndex: 286 entries, 0 to 285
 #  Data columns (total 5 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    286 non-null    object 
-#   1   pais    286 non-null    object 
-#   2   anio    286 non-null    int64  
-#   3   valor   286 non-null    float64
-#   4   serie   286 non-null    object 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  286 non-null    object 
+#   1   geonombreFundar  286 non-null    object 
+#   2   anio             286 non-null    int64  
+#   3   valor            286 non-null    float64
+#   4   serie            286 non-null    object 
 #  
-#  |    | iso3   | pais      |   anio |    valor | serie           |
-#  |---:|:-------|:----------|-------:|---------:|:----------------|
-#  |  0 | ARG    | Argentina |   1986 | 0.268756 | Serie empalmada |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='pais', axis=1)
-#  RangeIndex: 286 entries, 0 to 285
-#  Data columns (total 4 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    286 non-null    object 
-#   1   anio    286 non-null    int64  
-#   2   valor   286 non-null    float64
-#   3   serie   286 non-null    object 
-#  
-#  |    | iso3   |   anio |    valor | serie           |
-#  |---:|:-------|-------:|---------:|:----------------|
-#  |  0 | ARG    |   1986 | 0.268756 | Serie empalmada |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |    valor | serie           |
+#  |---:|:------------------|:------------------|-------:|---------:|:----------------|
+#  |  0 | ARG               | Argentina         |   1986 | 0.268756 | Serie empalmada |
 #  
 #  ------------------------------
 #  
-#  drop_col(col='serie', axis=1)
+#  multiplicar_por_escalar(col='valor', k=100)
 #  RangeIndex: 286 entries, 0 to 285
-#  Data columns (total 3 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   iso3    286 non-null    object 
-#   1   anio    286 non-null    int64  
-#   2   valor   286 non-null    float64
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  286 non-null    object 
+#   1   geonombreFundar  286 non-null    object 
+#   2   anio             286 non-null    int64  
+#   3   valor            286 non-null    float64
+#   4   serie            286 non-null    object 
 #  
-#  |    | iso3   |   anio |    valor |
-#  |---:|:-------|-------:|---------:|
-#  |  0 | ARG    |   1986 | 0.268756 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'iso3': 'geocodigo'})
-#  RangeIndex: 286 entries, 0 to 285
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  286 non-null    object 
-#   1   anio       286 non-null    int64  
-#   2   valor      286 non-null    float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ARG         |   1986 | 26.8756 |
-#  
-#  ------------------------------
-#  
-#  mutiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 286 entries, 0 to 285
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   geocodigo  286 non-null    object 
-#   1   anio       286 non-null    int64  
-#   2   valor      286 non-null    float64
-#  
-#  |    | geocodigo   |   anio |   valor |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | ARG         |   1986 | 26.8756 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor | serie           |
+#  |---:|:------------------|:------------------|-------:|--------:|:----------------|
+#  |  0 | ARG               | Argentina         |   1986 | 26.8756 | Serie empalmada |
 #  
 #  ------------------------------
 #  
