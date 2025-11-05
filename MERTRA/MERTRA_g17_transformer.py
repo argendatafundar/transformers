6ece45c240ad92fe93c463ae1c8ffa898b23ee45
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 17:55:18 -0300

    Updated MERTRA/MERTRA_g17_transformer.py

--- MERTRA/MERTRA_g17_transformer.py
+++ MERTRA/MERTRA_g17_transformer.py
@@ -4,14 +4,21 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	replace_value(col='sexo', curr_value='Mujeres', new_value='Mujer', mapping=None),
+	replace_value(col='sexo', curr_value='Varones', new_value='Varon', mapping=None)
 )
 #  PIPELINE_END
 
@@ -31,7 +38,22 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  replace_value(col='sexo', curr_value='Mujeres', new_value='Mujer', mapping=None)
+#  RangeIndex: 9 entries, 0 to 8
+#  Data columns (total 3 columns):
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   sexo          9 non-null      object 
+#   1   tipo_trabajo  9 non-null      object 
+#   2   minutos       9 non-null      float64
+#  
+#  |    | sexo   | tipo_trabajo   |   minutos |
+#  |---:|:-------|:---------------|----------:|
+#  |  0 | Total  | Trabajo total  |   501.643 |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='sexo', curr_value='Varones', new_value='Varon', mapping=None)
 #  RangeIndex: 9 entries, 0 to 8
 #  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
