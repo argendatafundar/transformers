b3c6b8c05b5c3c70e29d7c38f5fef5d53f660742
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 14:31:43 -0300

    Updated INFDES/INFDES_g09_transformer.py

--- INFDES/INFDES_g09_transformer.py
+++ INFDES/INFDES_g09_transformer.py
@@ -4,20 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_col(df: DataFrame, col, axis=1):
-    return df.drop(col, axis=axis)
-
-@transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-drop_col(col='pais', axis=1),
-	rename_cols(map={'iso3': 'geocodigo', 'brecha': 'valor', 'circa': 'indicador'})
+	identity()
 )
 #  PIPELINE_END
 
@@ -25,49 +19,34 @@ drop_col(col='pais', axis=1),
 #  start()
 #  RangeIndex: 22 entries, 0 to 21
 #  Data columns (total 5 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   iso3        22 non-null     object 
-#   1   pais        22 non-null     object 
-#   2   anio_circa  22 non-null     int64  
-#   3   brecha      22 non-null     float64
-#   4   circa       22 non-null     int64  
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  22 non-null     object 
+#   1   geonombreFundar  22 non-null     object 
+#   2   anio_circa       22 non-null     int64  
+#   3   brecha           22 non-null     float64
+#   4   circa            22 non-null     int64  
 #  
-#  |    | iso3   | pais      |   anio_circa |   brecha |   circa |
-#  |---:|:-------|:----------|-------------:|---------:|--------:|
-#  |  0 | ARG    | Argentina |         2000 |  13.9852 |    2000 |
-#  
-#  ------------------------------
-#  
-#  drop_col(col='pais', axis=1)
-#  RangeIndex: 22 entries, 0 to 21
-#  Data columns (total 4 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   iso3        22 non-null     object 
-#   1   anio_circa  22 non-null     int64  
-#   2   brecha      22 non-null     float64
-#   3   circa       22 non-null     int64  
-#  
-#  |    | iso3   |   anio_circa |   brecha |   circa |
-#  |---:|:-------|-------------:|---------:|--------:|
-#  |  0 | ARG    |         2000 |  13.9852 |    2000 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio_circa |   brecha |   circa |
+#  |---:|:------------------|:------------------|-------------:|---------:|--------:|
+#  |  0 | ARG               | Argentina         |         2000 |  13.9852 |    2000 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'iso3': 'geocodigo', 'brecha': 'valor', 'circa': 'indicador'})
+#  identity()
 #  RangeIndex: 22 entries, 0 to 21
-#  Data columns (total 4 columns):
-#   #   Column      Non-Null Count  Dtype  
-#  ---  ------      --------------  -----  
-#   0   geocodigo   22 non-null     object 
-#   1   anio_circa  22 non-null     int64  
-#   2   valor       22 non-null     float64
-#   3   indicador   22 non-null     int64  
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  22 non-null     object 
+#   1   geonombreFundar  22 non-null     object 
+#   2   anio_circa       22 non-null     int64  
+#   3   brecha           22 non-null     float64
+#   4   circa            22 non-null     int64  
 #  
-#  |    | geocodigo   |   anio_circa |   valor |   indicador |
-#  |---:|:------------|-------------:|--------:|------------:|
-#  |  0 | ARG         |         2000 | 13.9852 |        2000 |
+#  |    | geocodigoFundar   | geonombreFundar   |   anio_circa |   brecha |   circa |
+#  |---:|:------------------|:------------------|-------------:|---------:|--------:|
+#  |  0 | ARG               | Argentina         |         2000 |  13.9852 |    2000 |
 #  
 #  ------------------------------
 #  
