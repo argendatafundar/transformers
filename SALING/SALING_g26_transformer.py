57844ec9639da0e45b3dde05bab18dcbc0b2459e
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 17:25:38 -0300

    Updated SALING/SALING_g26_transformer.py

--- SALING/SALING_g26_transformer.py
+++ SALING/SALING_g26_transformer.py
@@ -12,13 +12,24 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	rename_cols(map={'tipo_prima': 'categoria', 'prima': 'valor'}),
-	multiplicar_por_escalar(col='valor', k=100)
+	multiplicar_por_escalar(col='valor', k=100),
+	replace_value(col='categoria', curr_value=None, new_value=None, mapping={'Controlando por variables sociodemográficas': 'Con control', 'Sin control por otras variables': 'Sin controles'})
 )
 #  PIPELINE_END
 
@@ -65,6 +76,21 @@ pipeline = chain(
 #  |    |   anio |   valor | categoria                                   |
 #  |---:|-------:|--------:|:--------------------------------------------|
 #  |  0 |   2003 | 43.8929 | Controlando por variables sociodemográficas |
+#  
+#  ------------------------------
+#  
+#  replace_value(col='categoria', curr_value=None, new_value=None, mapping={'Controlando por variables sociodemográficas': 'Con control', 'Sin control por otras variables': 'Sin controles'})
+#  RangeIndex: 42 entries, 0 to 41
+#  Data columns (total 3 columns):
+#   #   Column     Non-Null Count  Dtype  
+#  ---  ------     --------------  -----  
+#   0   anio       42 non-null     int64  
+#   1   valor      42 non-null     float64
+#   2   categoria  42 non-null     object 
+#  
+#  |    |   anio |   valor | categoria   |
+#  |---:|-------:|--------:|:------------|
+#  |  0 |   2003 | 43.8929 | Con control |
 #  
 #  ------------------------------
 #  
