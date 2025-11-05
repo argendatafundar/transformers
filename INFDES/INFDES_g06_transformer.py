a96a65e5ff82ab2a28fe5be6802f49553c02a8db
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 12:26:19 -0300

    Updated INFDES/INFDES_g06_transformer.py

--- INFDES/INFDES_g06_transformer.py
+++ INFDES/INFDES_g06_transformer.py
@@ -7,12 +7,24 @@ from data_transformers import chain, transformer
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
+
+@transformer.convert
+def imput_na(df:DataFrame, bool_mask:list[bool], col:str, value:Any):
+    df.loc[bool_mask,col] = value
+    return df
+
+@transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	multiplicar_por_escalar(col='valor', k=100)
+	multiplicar_por_escalar(col='valor', k=100),
+	imput_na(bool_mask=[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True], col='geonombreFundar', value='Nacional'),
+	query(condition='anio == anio.max()')
 )
 #  PIPELINE_END
 
@@ -41,7 +53,7 @@ pipeline = chain(
 #   #   Column             Non-Null Count  Dtype  
 #  ---  ------             --------------  -----  
 #   0   geocodigoFundar    378 non-null    object 
-#   1   geonombreFundar    378 non-null    object 
+#   1   geonombreFundar    394 non-null    object 
 #   2   anio               394 non-null    int64  
 #   3   prov_cod           394 non-null    int64  
 #   4   tipo_informalidad  394 non-null    object 
@@ -50,6 +62,42 @@ pipeline = chain(
 #  |    | geocodigoFundar   | geonombreFundar   |   anio |   prov_cod | tipo_informalidad                    |   valor |
 #  |---:|:------------------|:------------------|-------:|-----------:|:-------------------------------------|--------:|
 #  |  0 | AR-C              | CABA              |   2016 |          2 | Informalidad (definición productiva) | 26.2605 |
+#  
+#  ------------------------------
+#  
+#  imput_na(bool_mask=[False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True, True], col='geonombreFundar', value='Nacional')
+#  RangeIndex: 394 entries, 0 to 393
+#  Data columns (total 6 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   geocodigoFundar    378 non-null    object 
+#   1   geonombreFundar    394 non-null    object 
+#   2   anio               394 non-null    int64  
+#   3   prov_cod           394 non-null    int64  
+#   4   tipo_informalidad  394 non-null    object 
+#   5   valor              394 non-null    float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   prov_cod | tipo_informalidad                    |   valor |
+#  |---:|:------------------|:------------------|-------:|-----------:|:-------------------------------------|--------:|
+#  |  0 | AR-C              | CABA              |   2016 |          2 | Informalidad (definición productiva) | 26.2605 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio == anio.max()')
+#  Index: 50 entries, 330 to 393
+#  Data columns (total 6 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   geocodigoFundar    48 non-null     object 
+#   1   geonombreFundar    50 non-null     object 
+#   2   anio               50 non-null     int64  
+#   3   prov_cod           50 non-null     int64  
+#   4   tipo_informalidad  50 non-null     object 
+#   5   valor              50 non-null     float64
+#  
+#  |     | geocodigoFundar   | geonombreFundar   |   anio |   prov_cod | tipo_informalidad                    |   valor |
+#  |----:|:------------------|:------------------|-------:|-----------:|:-------------------------------------|--------:|
+#  | 330 | AR-C              | CABA              |   2023 |          2 | Informalidad (definición productiva) | 26.7745 |
 #  
 #  ------------------------------
 #  
