06124704c2f4582d5513065858bfc8e84acd4f4c
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-05 12:01:02 -0300

    Updated INFDES/INFDES_g04_transformer.py

--- INFDES/INFDES_g04_transformer.py
+++ INFDES/INFDES_g04_transformer.py
@@ -4,15 +4,14 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'tipo_informalidad': 'indicador'})
+	identity()
 )
 #  PIPELINE_END
 
@@ -32,16 +31,16 @@ rename_cols(map={'tipo_informalidad': 'indicador'})
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'tipo_informalidad': 'indicador'})
+#  identity()
 #  RangeIndex: 67 entries, 0 to 66
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       67 non-null     int64  
-#   1   indicador  67 non-null     object 
-#   2   valor      67 non-null     float64
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   anio               67 non-null     int64  
+#   1   tipo_informalidad  67 non-null     object 
+#   2   valor              67 non-null     float64
 #  
-#  |    |   anio | indicador             |   valor |
+#  |    |   anio | tipo_informalidad     |   valor |
 #  |---:|-------:|:----------------------|--------:|
 #  |  0 |   1988 | Definición productiva | 48.1013 |
 #  
