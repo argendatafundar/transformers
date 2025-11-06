de9a39a4c0d1da3899edcfa0f22d28f836a6fdf6
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-06 16:05:51 -0300

    Updated MERTRA/MERTRA_g01_transformer.py

--- MERTRA/MERTRA_g01_transformer.py
+++ MERTRA/MERTRA_g01_transformer.py
@@ -4,51 +4,45 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
-    if mapping is not None:
-        df = df.replace(mapping).copy()
-    elif col is not None and curr_value is not None and new_value is not None:
-        df = df.replace({col: curr_value}, new_value)
-    else:
-        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
+def identity(df: DataFrame) -> DataFrame:
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Totales', 'Mujeres': 'Mujer', 'Varones': 'Varon'})
+	identity()
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 9 entries, 0 to 8
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   sexo          9 non-null      object 
-#   1   tipo_trabajo  9 non-null      object 
-#   2   minutos       9 non-null      float64
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   anio                33 non-null     int64  
+#   1   sexo                33 non-null     object 
+#   2   tasa_participacion  33 non-null     float64
 #  
-#  |    | sexo   | tipo_trabajo   |   minutos |
-#  |---:|:-------|:---------------|----------:|
-#  |  0 | Total  | Trabajo total  |   501.643 |
+#  |    |   anio | sexo    |   tasa_participacion |
+#  |---:|-------:|:--------|---------------------:|
+#  |  0 |   1869 | Varones |             0.906793 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Totales', 'Mujeres': 'Mujer', 'Varones': 'Varon'})
-#  RangeIndex: 9 entries, 0 to 8
+#  identity()
+#  RangeIndex: 33 entries, 0 to 32
 #  Data columns (total 3 columns):
-#   #   Column        Non-Null Count  Dtype  
-#  ---  ------        --------------  -----  
-#   0   sexo          9 non-null      object 
-#   1   tipo_trabajo  9 non-null      object 
-#   2   minutos       9 non-null      float64
+#   #   Column              Non-Null Count  Dtype  
+#  ---  ------              --------------  -----  
+#   0   anio                33 non-null     int64  
+#   1   sexo                33 non-null     object 
+#   2   tasa_participacion  33 non-null     float64
 #  
-#  |    | sexo    | tipo_trabajo   |   minutos |
-#  |---:|:--------|:---------------|----------:|
-#  |  0 | Totales | Trabajo total  |   501.643 |
+#  |    |   anio | sexo    |   tasa_participacion |
+#  |---:|-------:|:--------|---------------------:|
+#  |  0 |   1869 | Varones |             0.906793 |
 #  
 #  ------------------------------
 #  
