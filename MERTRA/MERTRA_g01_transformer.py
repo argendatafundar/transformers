e69a1a63e9dce7f8a65151ac6658e391e624af3b
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-06 15:58:27 -0300

    Updated MERTRA/MERTRA_g01_transformer.py

--- MERTRA/MERTRA_g01_transformer.py
+++ MERTRA/MERTRA_g01_transformer.py
@@ -4,67 +4,51 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
-    return df
-
-@transformer.convert
-def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
-    df[col] = df[col]*k
+def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_value: str = None, mapping = None):
+    if mapping is not None:
+        df = df.replace(mapping).copy()
+    elif col is not None and curr_value is not None and new_value is not None:
+        df = df.replace({col: curr_value}, new_value)
+    else:
+        raise ValueError("Either 'mapping' must be provided, or all of 'col', 'curr_value', and 'new_value' must be provided")
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'sexo': 'categoria', 'tasa_participacion': 'valor'}),
-	multiplicar_por_escalar(col='valor', k=100)
+	replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Total\u200b', 'Mujeres': 'Mujeres\u200b', 'Varones': 'Varones\u200b'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 33 entries, 0 to 32
+#  RangeIndex: 9 entries, 0 to 8
 #  Data columns (total 3 columns):
-#   #   Column              Non-Null Count  Dtype  
-#  ---  ------              --------------  -----  
-#   0   anio                33 non-null     int64  
-#   1   sexo                33 non-null     object 
-#   2   tasa_participacion  33 non-null     float64
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   sexo          9 non-null      object 
+#   1   tipo_trabajo  9 non-null      object 
+#   2   minutos       9 non-null      float64
 #  
-#  |    |   anio | sexo    |   tasa_participacion |
-#  |---:|-------:|:--------|---------------------:|
-#  |  0 |   1869 | Varones |             0.906793 |
+#  |    | sexo   | tipo_trabajo   |   minutos |
+#  |---:|:-------|:---------------|----------:|
+#  |  0 | Total  | Trabajo total  |   501.643 |
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'sexo': 'categoria', 'tasa_participacion': 'valor'})
-#  RangeIndex: 33 entries, 0 to 32
+#  replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Total\u200b', 'Mujeres': 'Mujeres\u200b', 'Varones': 'Varones\u200b'})
+#  RangeIndex: 9 entries, 0 to 8
 #  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       33 non-null     int64  
-#   1   categoria  33 non-null     object 
-#   2   valor      33 non-null     float64
+#   #   Column        Non-Null Count  Dtype  
+#  ---  ------        --------------  -----  
+#   0   sexo          9 non-null      object 
+#   1   tipo_trabajo  9 non-null      object 
+#   2   minutos       9 non-null      float64
 #  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1869 | Varones     | 90.6793 |
-#  
-#  ------------------------------
-#  
-#  multiplicar_por_escalar(col='valor', k=100)
-#  RangeIndex: 33 entries, 0 to 32
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       33 non-null     int64  
-#   1   categoria  33 non-null     object 
-#   2   valor      33 non-null     float64
-#  
-#  |    |   anio | categoria   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1869 | Varones     | 90.6793 |
+#  |    | sexo   | tipo_trabajo   |   minutos |
+#  |---:|:-------|:---------------|----------:|
+#  |  0 | Total​  | Trabajo total  |   501.643 |
 #  
 #  ------------------------------
 #  
