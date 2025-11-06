09246a8c8ef7269753bfd6454124029202251c75
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-11-06 16:02:48 -0300

    Updated MERTRA/MERTRA_g01_transformer.py

--- MERTRA/MERTRA_g01_transformer.py
+++ MERTRA/MERTRA_g01_transformer.py
@@ -17,7 +17,7 @@ def replace_value(df: DataFrame, col: str = None, curr_value: str = None, new_va
 
 #  PIPELINE_START
 pipeline = chain(
-	replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Total\u200c', 'Mujeres': 'Mujeres\u200c', 'Varones': 'Varones\u200c\u200c'})
+	replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Total\u200c\u200c', 'Mujeres': 'Mujeres\u200c\u200c', 'Varones': 'Varones\u200c\u200c'})
 )
 #  PIPELINE_END
 
@@ -37,7 +37,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Total\u200c', 'Mujeres': 'Mujeres\u200c', 'Varones': 'Varones\u200c\u200c'})
+#  replace_value(col='sexo', curr_value=None, new_value=None, mapping={'Total': 'Total\u200c\u200c', 'Mujeres': 'Mujeres\u200c\u200c', 'Varones': 'Varones\u200c\u200c'})
 #  RangeIndex: 9 entries, 0 to 8
 #  Data columns (total 3 columns):
 #   #   Column        Non-Null Count  Dtype  
@@ -48,7 +48,7 @@ pipeline = chain(
 #  
 #  |    | sexo   | tipo_trabajo   |   minutos |
 #  |---:|:-------|:---------------|----------:|
-#  |  0 | Total‌  | Trabajo total  |   501.643 |
+#  |  0 | Total‌‌  | Trabajo total  |   501.643 |
 #  
 #  ------------------------------
 #  
