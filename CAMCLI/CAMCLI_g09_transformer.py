e64652cd58bba10b7f7d13e1275d5a17a7355522
jgjuara <jg.juara@gmail.com>
2025-11-25 13:22:18 -0300

    Updated CAMCLI/CAMCLI_g09_transformer.py

--- CAMCLI/CAMCLI_g09_transformer.py
+++ CAMCLI/CAMCLI_g09_transformer.py
@@ -4,21 +4,28 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def rename_cols(df: DataFrame, map):
-    df = df.rename(columns=map)
+def rename_cols(df: pl.DataFrame, map):
+    df = df.rename(map)
     return df
 
 @transformer.convert
-def to_pandas(df: pl.DataFrame, dummy = True):
-    df = df.to_pandas()
+def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
+
+    if not alias:
+        alias = col
+
+    df = df.with_columns(
+        pl.col(col).replace(mapping).alias(alias)
+    )
+
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	to_pandas(dummy=True),
-	rename_cols(map={'valor_en_mtco2e': 'valor'})
+	rename_cols(map={'valor_en_mtco2e': 'valor'}),
+	replace_value(col='sector', mapping={'Procesos industriales y uso de productos': 'PIUP'}, alias=None)
 )
 #  PIPELINE_END
 
@@ -27,37 +34,11 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  to_pandas(dummy=True)
-#  RangeIndex: 132 entries, 0 to 131
-#  Data columns (total 5 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   anio             132 non-null    int64  
-#   1   geonombreFundar  132 non-null    object 
-#   2   geocodigoFundar  132 non-null    object 
-#   3   sector           132 non-null    object 
-#   4   valor_en_mtco2e  132 non-null    float64
-#  
-#  |    |   anio | geonombreFundar   | geocodigoFundar   | sector   |   valor_en_mtco2e |
-#  |---:|-------:|:------------------|:------------------|:---------|------------------:|
-#  |  0 |   1990 | Argentina         | ARG               | AGSyOUT  |           151.182 |
+#  rename_cols(map={'valor_en_mtco2e': 'valor'})
 #  
 #  ------------------------------
 #  
-#  rename_cols(map={'valor_en_mtco2e': 'valor'})
-#  RangeIndex: 132 entries, 0 to 131
-#  Data columns (total 5 columns):
-#   #   Column           Non-Null Count  Dtype  
-#  ---  ------           --------------  -----  
-#   0   anio             132 non-null    int64  
-#   1   geonombreFundar  132 non-null    object 
-#   2   geocodigoFundar  132 non-null    object 
-#   3   sector           132 non-null    object 
-#   4   valor            132 non-null    float64
-#  
-#  |    |   anio | geonombreFundar   | geocodigoFundar   | sector   |   valor |
-#  |---:|-------:|:------------------|:------------------|:---------|--------:|
-#  |  0 |   1990 | Argentina         | ARG               | AGSyOUT  | 151.182 |
+#  replace_value(col='sector', mapping={'Procesos industriales y uso de productos': 'PIUP'}, alias=None)
 #  
 #  ------------------------------
 #  
