da637e6f6f3a64f2c38e2b01ae05313dd9e3a4ac
jgjuara <jg.juara@gmail.com>
2025-11-25 14:08:12 -0300

    Updated CAMCLI/CAMCLI_g14_transformer.py

--- CAMCLI/CAMCLI_g14_transformer.py
+++ CAMCLI/CAMCLI_g14_transformer.py
@@ -4,24 +4,36 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def drop_cols(df, cols):
-    return df.drop(cols)
-
-@transformer.convert
-def pl_filter(df: pl.DataFrame, query: str):
-    df = df.filter(eval(query))
-    return df
-
-@transformer.convert
 def rename_cols(df: pl.DataFrame, map):
     df = df.rename(map)
     return df
+
+@transformer.convert
+def drop_cols(df, cols):
+    return df.drop(cols)
 
 @transformer.convert
 def sort_values(df: pl.DataFrame, by, descending = None):
     if not descending:
         descending = [False] * len(by)
     df = df.sort(by = by, descending= descending)
+    return df
+
+@transformer.convert
+def replace_value(df: pl.DataFrame, col: str, mapping: dict, alias: str = None):
+
+    if not alias:
+        alias = col
+
+    df = df.with_columns(
+        pl.col(col).replace(mapping).alias(alias)
+    )
+
+    return df
+
+@transformer.convert
+def pl_filter(df: pl.DataFrame, query: str):
+    df = df.filter(eval(query))
     return df
 #  DEFINITIONS_END
 
@@ -31,7 +43,8 @@ pipeline = chain(
 	pl_filter(query="pl.col('anio') == max(df['anio'])"),
 	rename_cols(map={'geocodigoFundar': 'geocodigo', 'sector': 'indicador', 'valor_en_mtco2e': 'valor'}),
 	drop_cols(cols=['anio']),
-	sort_values(by=['geonombreFundar', 'indicador'], descending=None)
+	sort_values(by=['geonombreFundar', 'indicador'], descending=None),
+	replace_value(col='geonombreFundar', mapping={'Santiago del Estero': 'S. del Estero', 'Tierra del Fuego': 'T. del Fuego'}, alias=None)
 )
 #  PIPELINE_END
 
@@ -53,6 +66,10 @@ pipeline = chain(
 #  ------------------------------
 #  
 #  sort_values(by=['geonombreFundar', 'indicador'], descending=None)
+#  
+#  ------------------------------
+#  
+#  replace_value(col='geonombreFundar', mapping={'Santiago del Estero': 'S. del Estero', 'Tierra del Fuego': 'T. del Fuego'}, alias=None)
 #  
 #  ------------------------------
 #  
