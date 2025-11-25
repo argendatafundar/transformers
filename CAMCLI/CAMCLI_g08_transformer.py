370a848fe289cb624bf8af5345975d8e98d87ecb
jgjuara <jg.juara@gmail.com>
2025-11-25 13:17:23 -0300

    Updated CAMCLI/CAMCLI_g08_transformer.py

--- CAMCLI/CAMCLI_g08_transformer.py
+++ CAMCLI/CAMCLI_g08_transformer.py
@@ -7,41 +7,55 @@ from data_transformers import chain, transformer
 def rename_cols(df: DataFrame, map):
     df = df.rename(columns=map)
     return df
+
+@transformer.convert
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'valor_en_mtco2e': 'valor'})
+	to_pandas(dummy=True),
+	rename_cols(map={'valor_en_mtco2e': 'valor'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 29 entries, 0 to 28
-#  Data columns (total 2 columns):
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
+#  RangeIndex: 33 entries, 0 to 32
+#  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             29 non-null     int64  
-#   1   valor_en_mtco2e  29 non-null     float64
+#   0   anio             33 non-null     int64  
+#   1   valor_en_mtco2e  33 non-null     float64
+#   2   geonombreFundar  33 non-null     object 
+#   3   geocodigoFundar  33 non-null     object 
 #  
-#  |    |   anio |   valor_en_mtco2e |
-#  |---:|-------:|------------------:|
-#  |  0 |   1990 |           263.539 |
+#  |    |   anio |   valor_en_mtco2e | geonombreFundar   | geocodigoFundar   |
+#  |---:|-------:|------------------:|:------------------|:------------------|
+#  |  0 |   1990 |            273.82 | Argentina         | ARG               |
 #  
 #  ------------------------------
 #  
 #  rename_cols(map={'valor_en_mtco2e': 'valor'})
-#  RangeIndex: 29 entries, 0 to 28
-#  Data columns (total 2 columns):
-#   #   Column  Non-Null Count  Dtype  
-#  ---  ------  --------------  -----  
-#   0   anio    29 non-null     int64  
-#   1   valor   29 non-null     float64
+#  RangeIndex: 33 entries, 0 to 32
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             33 non-null     int64  
+#   1   valor            33 non-null     float64
+#   2   geonombreFundar  33 non-null     object 
+#   3   geocodigoFundar  33 non-null     object 
 #  
-#  |    |   anio |   valor |
-#  |---:|-------:|--------:|
-#  |  0 |   1990 | 263.539 |
+#  |    |   anio |   valor | geonombreFundar   | geocodigoFundar   |
+#  |---:|-------:|--------:|:------------------|:------------------|
+#  |  0 |   1990 |  273.82 | Argentina         | ARG               |
 #  
 #  ------------------------------
 #  
