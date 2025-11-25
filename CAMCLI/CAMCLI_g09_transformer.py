5dac2ddc9acb47953177cb79157434a6ee123764
jgjuara <jg.juara@gmail.com>
2025-11-25 13:18:15 -0300

    Updated CAMCLI/CAMCLI_g09_transformer.py

--- CAMCLI/CAMCLI_g09_transformer.py
+++ CAMCLI/CAMCLI_g09_transformer.py
@@ -9,86 +9,55 @@ def rename_cols(df: DataFrame, map):
     return df
 
 @transformer.convert
-def replace_value(df: DataFrame, col: str, curr_value: str, new_value: str):
-    df = df.replace({col: curr_value}, new_value)
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
     return df
-
-@transformer.convert
-def sort_values_by_comparison(df, colname: str, precedence: dict, prefix=[], suffix=[]):
-    mapcol = colname+'_map'
-    df_ = df.copy()
-    df_[mapcol] = df_[colname].map(precedence)
-    df_ = df_.sort_values(by=[*prefix, mapcol, *suffix])
-    return df_.drop(mapcol, axis=1)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-rename_cols(map={'valor_en_mtco2e': 'valor', 'sector': 'indicador'}),
-	replace_value(col='indicador', curr_value='Procesos industriales y uso de productos', new_value='PIUP'),
-	sort_values_by_comparison(colname='indicador', precedence={'Energía': 0, 'AGSyOUT': 1, 'PIUP': 2, 'Residuos': 3}, prefix=['anio'], suffix=[])
+	to_pandas(dummy=True),
+	rename_cols(map={'valor_en_mtco2e': 'valor'})
 )
 #  PIPELINE_END
 
 
 #  start()
-#  RangeIndex: 116 entries, 0 to 115
-#  Data columns (total 3 columns):
+#  
+#  ------------------------------
+#  
+#  to_pandas(dummy=True)
+#  RangeIndex: 132 entries, 0 to 131
+#  Data columns (total 5 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             116 non-null    int64  
-#   1   sector           116 non-null    object 
-#   2   valor_en_mtco2e  116 non-null    float64
+#   0   anio             132 non-null    int64  
+#   1   geonombreFundar  132 non-null    object 
+#   2   geocodigoFundar  132 non-null    object 
+#   3   sector           132 non-null    object 
+#   4   valor_en_mtco2e  132 non-null    float64
 #  
-#  |    |   anio | sector   |   valor_en_mtco2e |
-#  |---:|-------:|:---------|------------------:|
-#  |  0 |   1990 | AGSyOUT  |            151.97 |
-#  
-#  ------------------------------
-#  
-#  rename_cols(map={'valor_en_mtco2e': 'valor', 'sector': 'indicador'})
-#  RangeIndex: 116 entries, 0 to 115
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       116 non-null    int64  
-#   1   indicador  116 non-null    object 
-#   2   valor      116 non-null    float64
-#  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1990 | AGSyOUT     |  151.97 |
+#  |    |   anio | geonombreFundar   | geocodigoFundar   | sector   |   valor_en_mtco2e |
+#  |---:|-------:|:------------------|:------------------|:---------|------------------:|
+#  |  0 |   1990 | Argentina         | ARG               | AGSyOUT  |           151.182 |
 #  
 #  ------------------------------
 #  
-#  replace_value(col='indicador', curr_value='Procesos industriales y uso de productos', new_value='PIUP')
-#  RangeIndex: 116 entries, 0 to 115
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       116 non-null    int64  
-#   1   indicador  116 non-null    object 
-#   2   valor      116 non-null    float64
+#  rename_cols(map={'valor_en_mtco2e': 'valor'})
+#  RangeIndex: 132 entries, 0 to 131
+#  Data columns (total 5 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             132 non-null    int64  
+#   1   geonombreFundar  132 non-null    object 
+#   2   geocodigoFundar  132 non-null    object 
+#   3   sector           132 non-null    object 
+#   4   valor            132 non-null    float64
 #  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  0 |   1990 | AGSyOUT     |  151.97 |
-#  
-#  ------------------------------
-#  
-#  sort_values_by_comparison(colname='indicador', precedence={'Energía': 0, 'AGSyOUT': 1, 'PIUP': 2, 'Residuos': 3}, prefix=['anio'], suffix=[])
-#  Index: 116 entries, 1 to 115
-#  Data columns (total 3 columns):
-#   #   Column     Non-Null Count  Dtype  
-#  ---  ------     --------------  -----  
-#   0   anio       116 non-null    int64  
-#   1   indicador  116 non-null    object 
-#   2   valor      116 non-null    float64
-#  
-#  |    |   anio | indicador   |   valor |
-#  |---:|-------:|:------------|--------:|
-#  |  1 |   1990 | Energía     |   95.45 |
+#  |    |   anio | geonombreFundar   | geocodigoFundar   | sector   |   valor |
+#  |---:|-------:|:------------------|:------------------|:---------|--------:|
+#  |  0 |   1990 | Argentina         | ARG               | AGSyOUT  | 151.182 |
 #  
 #  ------------------------------
 #  
