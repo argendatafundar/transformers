c920ea722e88ca4b11d8a74acc85f4c7267c434e
jgjuara <jg.juara@gmail.com>
2025-11-25 14:31:23 -0300

    Updated CAMCLI/CAMCLI_g1021_transformer.py

--- CAMCLI/CAMCLI_g1021_transformer.py
+++ CAMCLI/CAMCLI_g1021_transformer.py
@@ -4,15 +4,20 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def multiplicar_por_escalar(df: pl.DataFrame, col: str, k: float) -> pl.DataFrame:
-    return df.with_columns([
-        (pl.col(col) * k).alias(col)
-    ])
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+
+@transformer.convert
+def to_pandas(df: pl.DataFrame, dummy = True):
+    df = df.to_pandas()
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
+	to_pandas(dummy=True),
 	multiplicar_por_escalar(col='valor', k=1e-06)
 )
 #  PIPELINE_END
@@ -22,7 +27,35 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
+#  to_pandas(dummy=True)
+#  RangeIndex: 34278 entries, 0 to 34277
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  34278 non-null  object 
+#   1   geonombreFundar  34278 non-null  object 
+#   2   anio             34278 non-null  int64  
+#   3   valor            34278 non-null  float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AFG               | Afganistán        |   1850 | 7.43574 |
+#  
+#  ------------------------------
+#  
 #  multiplicar_por_escalar(col='valor', k=1e-06)
+#  RangeIndex: 34278 entries, 0 to 34277
+#  Data columns (total 4 columns):
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   geocodigoFundar  34278 non-null  object 
+#   1   geonombreFundar  34278 non-null  object 
+#   2   anio             34278 non-null  int64  
+#   3   valor            34278 non-null  float64
+#  
+#  |    | geocodigoFundar   | geonombreFundar   |   anio |   valor |
+#  |---:|:------------------|:------------------|-------:|--------:|
+#  |  0 | AFG               | Afganistán        |   1850 | 7.43574 |
 #  
 #  ------------------------------
 #  
