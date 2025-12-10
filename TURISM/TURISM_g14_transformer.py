36d97f7ee45b2debc554abfc996a1259c2a4b92c
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 13:24:40 -0300

    Updated TURISM/TURISM_g14_transformer.py

--- TURISM/TURISM_g14_transformer.py
+++ TURISM/TURISM_g14_transformer.py
@@ -7,12 +7,19 @@ from data_transformers import chain, transformer
 def query(df: DataFrame, condition: str):
     df = df.query(condition)    
     return df
+
+@transformer.convert
+def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFrame:
+    df_copy = df.copy()
+    df_copy[col] = df_copy[col].replace(replacements)
+    return df_copy
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	query(condition='anio == anio.max()')
+	query(condition='anio == anio.max()'),
+	replace_multiple_values(col='provincia', replacements={'Tierra Del Fuego': 'Tierra del Fuego', 'Santiago Del Estero': 'Santiago del Estero', 'Entre Rios': 'Entre Ríos', 'Rio Negro': 'Río Negro', 'Neuquen': 'Neuquén', 'Cordoba': 'Córdoba'})
 )
 #  PIPELINE_END
 
@@ -48,6 +55,23 @@ pipeline = chain(
 #  |    |   anio |   provincia_id | provincia   |   empleo_turistico |   prop_turistico |
 #  |---:|-------:|---------------:|:------------|-------------------:|-----------------:|
 #  |  0 |   2022 |             62 | Rio Negro   |              15504 |          13.4383 |
+#  
+#  ------------------------------
+#  
+#  replace_multiple_values(col='provincia', replacements={'Tierra Del Fuego': 'Tierra del Fuego', 'Santiago Del Estero': 'Santiago del Estero', 'Entre Rios': 'Entre Ríos', 'Rio Negro': 'Río Negro', 'Neuquen': 'Neuquén', 'Cordoba': 'Córdoba'})
+#  Index: 25 entries, 0 to 24
+#  Data columns (total 5 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   anio              25 non-null     int64  
+#   1   provincia_id      25 non-null     int64  
+#   2   provincia         25 non-null     object 
+#   3   empleo_turistico  25 non-null     int64  
+#   4   prop_turistico    25 non-null     float64
+#  
+#  |    |   anio |   provincia_id | provincia   |   empleo_turistico |   prop_turistico |
+#  |---:|-------:|---------------:|:------------|-------------------:|-----------------:|
+#  |  0 |   2022 |             62 | Río Negro   |              15504 |          13.4383 |
 #  
 #  ------------------------------
 #  
