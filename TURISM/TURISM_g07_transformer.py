279d7bf8129adfe7d69d6253e3eaa18a33c06d1c
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 11:00:48 -0300

    Updated TURISM/TURISM_g07_transformer.py

--- TURISM/TURISM_g07_transformer.py
+++ TURISM/TURISM_g07_transformer.py
@@ -33,7 +33,8 @@ pipeline = chain(
 	query(condition='anio == anio.max()'),
 	custom_string_funcion(),
 	sort_values(how='descending', by='exportaciones_en_usd_mill'),
-	replace_multiple_values(col='categoria2', replacements={'Soja': 'Soja', 'Petrolero-petroquímico': 'Petrolero-petroq.', 'Servicios basados en conocimiento': 'SBC', 'Automotriz': 'Automotriz', 'Maicero': 'Maíz', 'Resto de exportaciones': 'Resto exp.', 'Viajes': 'Turismo', 'Oro y plata': 'Oro y plata', 'Carne y cuero bovinos': 'Carne/cuero bov.', 'Triguero': 'Trigo', 'Transporte': 'Transporte', 'Pesquero': 'Pesquero', 'Girasol': 'Girasol', 'Lácteo': 'Lácteo', 'Cebada': 'Cebada', 'Maní': 'Maní', 'Farmacéutico': 'Farmacéutico', 'Siderúrgico': 'Siderúrgico', 'Aluminio': 'Aluminio', 'Uva': 'Uva', 'Forestal': 'Forestal', 'Litio': 'Litio', 'Limón': 'Limón', 'Azucarero': 'Azúcar', 'Textil': 'Textil', 'Peras y manzanas': 'Peras/manzanas', 'Papa': 'Papa', 'Tabacalero': 'Tabaco', 'Olivícola': 'Olivo', 'Avícola': 'Avícola', 'Bienes y serv. del gobierno, n.i.o.p.': 'Bienes/serv. del gob.', 'Arrocero': 'Arroz', 'Porotos': 'Porotos', 'Resto del sector frutícola': 'Resto frutícola', 'Miel': 'Miel', 'Serv. financieros': 'Serv. financieros', 'Ajo': 'Ajo', 'Resto del sector hortícola': 'Resto hortícola', 'Cítricos, excluido el limón': 'Cítricos s/limón', 'Yerba mate': 'Yerba mate', 'Serv. de mantenimiento y reparaciones n.i.o.p.': 'Mant./reparaciones', 'Té': 'Té', 'Garbanzos': 'Garbanzos', 'Arándanos y frutos similares': 'Arándanos', 'Equino': 'Equino', 'Serv. de seguros y pensiones': 'Seguros/pensiones', 'Plomo': 'Plomo', 'Otros minerales metalíferos': 'Otros minerales metalif.', 'Construcción': 'Construcción', 'Serv. de manufactura sobre insumos físicos pertenecientes a otros': 'Manuf. c/insumos ajenos'})
+	replace_multiple_values(col='categoria2', replacements={'Soja': 'Soja', 'Petrolero-petroquímico': 'Petrolero-petroq.', 'Servicios basados en conocimiento': 'SBC', 'Automotriz': 'Automotriz', 'Maicero': 'Maíz', 'Resto de exportaciones': 'Resto exp.', 'Viajes': 'Turismo', 'Oro y plata': 'Oro y plata', 'Carne y cuero bovinos': 'Carne/cuero bov.', 'Triguero': 'Trigo', 'Transporte': 'Transporte', 'Pesquero': 'Pesquero', 'Girasol': 'Girasol', 'Lácteo': 'Lácteo', 'Cebada': 'Cebada', 'Maní': 'Maní', 'Farmacéutico': 'Farmacéutico', 'Siderúrgico': 'Siderúrgico', 'Aluminio': 'Aluminio', 'Uva': 'Uva', 'Forestal': 'Forestal', 'Litio': 'Litio', 'Limón': 'Limón', 'Azucarero': 'Azúcar', 'Textil': 'Textil', 'Peras y manzanas': 'Peras/manzanas', 'Papa': 'Papa', 'Tabacalero': 'Tabaco', 'Olivícola': 'Olivo', 'Avícola': 'Avícola', 'Bienes y serv. del gobierno, n.i.o.p.': 'Bienes/serv. del gob.', 'Arrocero': 'Arroz', 'Porotos': 'Porotos', 'Resto del sector frutícola': 'Resto frutícola', 'Miel': 'Miel', 'Serv. financieros': 'Serv. financieros', 'Ajo': 'Ajo', 'Resto del sector hortícola': 'Resto hortícola', 'Cítricos, excluido el limón': 'Cítricos s/limón', 'Yerba mate': 'Yerba mate', 'Serv. de mantenimiento y reparaciones n.i.o.p.': 'Mant./reparaciones', 'Té': 'Té', 'Garbanzos': 'Garbanzos', 'Arándanos y frutos similares': 'Arándanos', 'Equino': 'Equino', 'Serv. de seguros y pensiones': 'Seguros/pensiones', 'Plomo': 'Plomo', 'Otros minerales metalíferos': 'Otros minerales metalif.', 'Construcción': 'Construcción', 'Serv. de manufactura sobre insumos físicos pertenecientes a otros': 'Manuf. c/insumos ajenos'}),
+	query(condition='exportaciones_en_usd_mill > 0')
 )
 #  PIPELINE_END
 
@@ -115,6 +116,23 @@ pipeline = chain(
 #   2   exportaciones_en_usd_mill  50 non-null     float64
 #   3   tipo                       50 non-null     object 
 #   4   categoria2                 50 non-null     object 
+#  
+#  |    | categoria     |   anio |   exportaciones_en_usd_mill | tipo   | categoria2   |
+#  |---:|:--------------|-------:|----------------------------:|:-------|:-------------|
+#  |  0 | Complejo soja |   2024 |                     19628.1 | Bienes | Soja         |
+#  
+#  ------------------------------
+#  
+#  query(condition='exportaciones_en_usd_mill > 0')
+#  Index: 49 entries, 0 to 48
+#  Data columns (total 5 columns):
+#   #   Column                     Non-Null Count  Dtype  
+#  ---  ------                     --------------  -----  
+#   0   categoria                  49 non-null     object 
+#   1   anio                       49 non-null     int64  
+#   2   exportaciones_en_usd_mill  49 non-null     float64
+#   3   tipo                       49 non-null     object 
+#   4   categoria2                 49 non-null     object 
 #  
 #  |    | categoria     |   anio |   exportaciones_en_usd_mill | tipo   | categoria2   |
 #  |---:|:--------------|-------:|----------------------------:|:-------|:-------------|
