8a1e790943fed620d51b327e1924ffa14020f79a
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 16:28:03 -0300

    Created TURISM/TURISM_g18_transformer.py

---
+++ TURISM/TURISM_g18_transformer.py
@@ -0,0 +1,49 @@
+from pandas import DataFrame
+from data_transformers import chain, transformer
+
+
+#  DEFINITIONS_START
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
+#  DEFINITIONS_END
+
+
+#  PIPELINE_START
+pipeline = chain(
+	multiplicar_por_escalar(col='cantidad_turistas', k=0.001)
+)
+#  PIPELINE_END
+
+
+#  start()
+#  RangeIndex: 28 entries, 0 to 27
+#  Data columns (total 3 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   anio               28 non-null     int64  
+#   1   tipo_turismo       28 non-null     object 
+#   2   cantidad_turistas  28 non-null     float64
+#  
+#  |    |   anio | tipo_turismo   |   cantidad_turistas |
+#  |---:|-------:|:---------------|--------------------:|
+#  |  0 |   2010 | Receptivo      |         6.73859e+06 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='cantidad_turistas', k=0.001)
+#  RangeIndex: 28 entries, 0 to 27
+#  Data columns (total 3 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   anio               28 non-null     int64  
+#   1   tipo_turismo       28 non-null     object 
+#   2   cantidad_turistas  28 non-null     float64
+#  
+#  |    |   anio | tipo_turismo   |   cantidad_turistas |
+#  |---:|-------:|:---------------|--------------------:|
+#  |  0 |   2010 | Receptivo      |             6738.59 |
+#  
+#  ------------------------------
+#  
