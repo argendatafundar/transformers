83138b39197496e45d16f9066d5a2906c5ff4954
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 11:08:21 -0300

    Updated TURISM/TURISM_g08_transformer.py

--- TURISM/TURISM_g08_transformer.py
+++ TURISM/TURISM_g08_transformer.py
@@ -4,14 +4,17 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
-    return df
+def sort_values(df: DataFrame, how: str, by: list):
+    if how not in ['ascending', 'descending']:
+        raise ValueError('how must be either "ascending" or "descending"')
+    
+    return df.sort_values(by=by, ascending=how=='ascending').reset_index(drop=True)
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	sort_values(how='descending', by=['ratio_emisivo_ponderado'])
 )
 #  PIPELINE_END
 
@@ -31,7 +34,7 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
+#  sort_values(how='descending', by=['ratio_emisivo_ponderado'])
 #  RangeIndex: 191 entries, 0 to 190
 #  Data columns (total 3 columns):
 #   #   Column                   Non-Null Count  Dtype  
@@ -42,7 +45,7 @@ pipeline = chain(
 #  
 #  |    | geocodigoFundar   | geonombreFundar   |   ratio_emisivo_ponderado |
 #  |---:|:------------------|:------------------|--------------------------:|
-#  |  0 | ABW               | Aruba             |                   16.7893 |
+#  |  0 | ALB               | Albania           |                   22.0768 |
 #  
 #  ------------------------------
 #  
