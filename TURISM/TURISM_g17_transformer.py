297c39647d7cb470708e68c1c807737b1411a98e
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 16:25:43 -0300

    Updated TURISM/TURISM_g17_transformer.py

--- TURISM/TURISM_g17_transformer.py
+++ TURISM/TURISM_g17_transformer.py
@@ -4,6 +4,11 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
+
+@transformer.convert
 def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
     df[col] = df[col]*k
     return df
@@ -12,7 +17,8 @@ def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
 
 #  PIPELINE_START
 pipeline = chain(
-	multiplicar_por_escalar(col='arribos_turistas', k=0.001)
+	multiplicar_por_escalar(col='arribos_turistas', k=0.001),
+	query(condition='anio <= (anio.max()-1)')
 )
 #  PIPELINE_END
 
@@ -42,6 +48,22 @@ pipeline = chain(
 #   1   geocodigoFundar   59 non-null     object 
 #   2   geonombreFundar   59 non-null     object 
 #   3   arribos_turistas  59 non-null     float64
+#  
+#  |    |   anio | geocodigoFundar   | geonombreFundar   |   arribos_turistas |
+#  |---:|-------:|:------------------|:------------------|-------------------:|
+#  |  0 |   1960 | ARG               | Argentina         |            429.319 |
+#  
+#  ------------------------------
+#  
+#  query(condition='anio <= (anio.max()-1)')
+#  Index: 58 entries, 0 to 57
+#  Data columns (total 4 columns):
+#   #   Column            Non-Null Count  Dtype  
+#  ---  ------            --------------  -----  
+#   0   anio              58 non-null     int64  
+#   1   geocodigoFundar   58 non-null     object 
+#   2   geonombreFundar   58 non-null     object 
+#   3   arribos_turistas  58 non-null     float64
 #  
 #  |    |   anio | geocodigoFundar   | geonombreFundar   |   arribos_turistas |
 #  |---:|-------:|:------------------|:------------------|-------------------:|
