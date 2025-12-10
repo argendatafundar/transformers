9f042a2e448c6045452fee86f73fb5007d811075
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 16:21:45 -0300

    Updated TURISM/TURISM_g16_transformer.py

--- TURISM/TURISM_g16_transformer.py
+++ TURISM/TURISM_g16_transformer.py
@@ -4,14 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
+def drop_na(df:DataFrame, col:str):
+    df = df.dropna(subset=col, axis=0)
     return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	drop_na(col='pasajeros_miles')
 )
 #  PIPELINE_END
 
@@ -32,14 +33,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 276 entries, 0 to 275
+#  drop_na(col='pasajeros_miles')
+#  Index: 226 entries, 0 to 275
 #  Data columns (total 4 columns):
 #   #   Column           Non-Null Count  Dtype  
 #  ---  ------           --------------  -----  
-#   0   anio             276 non-null    int64  
-#   1   tipo_vuelo       276 non-null    object 
-#   2   fuente           276 non-null    object 
+#   0   anio             226 non-null    int64  
+#   1   tipo_vuelo       226 non-null    object 
+#   2   fuente           226 non-null    object 
 #   3   pasajeros_miles  226 non-null    float64
 #  
 #  |    |   anio | tipo_vuelo   | fuente                |   pasajeros_miles |
