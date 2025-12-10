4c0acac3187f2c08ed4c4ed01f285ef805a7c557
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-10 16:40:20 -0300

    Updated TURISM/TURISM_g21_transformer.py

--- TURISM/TURISM_g21_transformer.py
+++ TURISM/TURISM_g21_transformer.py
@@ -13,13 +13,19 @@ def replace_multiple_values(df: DataFrame, col:str, replacements:dict) -> DataFr
     df_copy = df.copy()
     df_copy[col] = df_copy[col].replace(replacements)
     return df_copy
+
+@transformer.convert
+def multiplicar_por_escalar(df: DataFrame, col:str, k:float):
+    df[col] = df[col]*k
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
 	query(condition="flujo != 'balanza'"),
-	replace_multiple_values(col='flujo', replacements={'expo_turistica': 'Exportaciones', 'impo_turistica': 'Importaciones'})
+	replace_multiple_values(col='flujo', replacements={'expo_turistica': 'Exportaciones', 'impo_turistica': 'Importaciones'}),
+	multiplicar_por_escalar(col='valor', k=1e-06)
 )
 #  PIPELINE_END
 
@@ -65,7 +71,22 @@ pipeline = chain(
 #  
 #  |    |   anio | flujo         |   valor |
 #  |---:|-------:|:--------------|--------:|
-#  |  0 |   1976 | Exportaciones | 1.8e+08 |
+#  |  0 |   1976 | Exportaciones |     180 |
+#  
+#  ------------------------------
+#  
+#  multiplicar_por_escalar(col='valor', k=1e-06)
+#  Index: 98 entries, 0 to 145
+#  Data columns (total 3 columns):
+#   #   Column  Non-Null Count  Dtype  
+#  ---  ------  --------------  -----  
+#   0   anio    98 non-null     int64  
+#   1   flujo   98 non-null     object 
+#   2   valor   98 non-null     float64
+#  
+#  |    |   anio | flujo         |   valor |
+#  |---:|-------:|:--------------|--------:|
+#  |  0 |   1976 | Exportaciones |     180 |
 #  
 #  ------------------------------
 #  
