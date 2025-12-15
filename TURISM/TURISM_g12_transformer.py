8cd49f87db57f73f8988da778d5443da9fc80767
Ignacio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-15 17:44:37 -0300

    Updated TURISM/TURISM_g12_transformer.py

--- TURISM/TURISM_g12_transformer.py
+++ TURISM/TURISM_g12_transformer.py
@@ -4,14 +4,16 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def identity(df: DataFrame) -> DataFrame:
-    return df
+def ordenar_categorica(df, col_cat:str, order_cat:list[str]):
+    import pandas as pd
+    df[col_cat] = pd.Categorical(df[col_cat], categories=order_cat, ordered=True)
+    return df.sort_values(by=[col_cat])
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	identity()
+	ordenar_categorica(col_cat='rama_etq', order_cat=['Gastronomía', 'Transporte de pasajeros', 'Arte, recreación y cultura', 'Agencias de viaje', 'Alojamiento', 'Resto de actividades'])
 )
 #  PIPELINE_END
 
@@ -31,14 +33,14 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  identity()
-#  RangeIndex: 6 entries, 0 to 5
+#  ordenar_categorica(col_cat='rama_etq', order_cat=['Gastronomía', 'Transporte de pasajeros', 'Arte, recreación y cultura', 'Agencias de viaje', 'Alojamiento', 'Resto de actividades'])
+#  Index: 6 entries, 0 to 4
 #  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype  
-#  ---  ------    --------------  -----  
-#   0   rama_etq  6 non-null      object 
-#   1   emp       6 non-null      int64  
-#   2   prop      6 non-null      float64
+#   #   Column    Non-Null Count  Dtype   
+#  ---  ------    --------------  -----   
+#   0   rama_etq  6 non-null      category
+#   1   emp       6 non-null      int64   
+#   2   prop      6 non-null      float64 
 #  
 #  |    | rama_etq    |    emp |    prop |
 #  |---:|:------------|-------:|--------:|
