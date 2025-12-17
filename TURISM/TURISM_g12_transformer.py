8a719520242ce76e3cef7bf327520eece2b8238e
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-17 16:34:09 -0300

    Updated TURISM/TURISM_g12_transformer.py

--- TURISM/TURISM_g12_transformer.py
+++ TURISM/TURISM_g12_transformer.py
@@ -4,47 +4,51 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def ordenar_categorica(df, col_cat:str, order_cat:list[str]):
-    import pandas as pd
-    df[col_cat] = pd.Categorical(df[col_cat], categories=order_cat, ordered=True)
-    return df.sort_values(by=[col_cat])
+def identity(df: DataFrame) -> DataFrame:
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	ordenar_categorica(col_cat='rama_etq', order_cat=['Gastronomía', 'Transporte de pasajeros', 'Arte, recreación y cultura', 'Agencias de viaje', 'Alojamiento', 'Resto de actividades'])
+	identity()
 )
 #  PIPELINE_END
 
 
 #  start()
 #  RangeIndex: 6 entries, 0 to 5
-#  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype  
-#  ---  ------    --------------  -----  
-#   0   rama_etq  6 non-null      object 
-#   1   emp       6 non-null      int64  
-#   2   prop      6 non-null      float64
+#  Data columns (total 6 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   anio               6 non-null      int64  
+#   1   agregado           6 non-null      object 
+#   2   industria          6 non-null      object 
+#   3   empleo_total       6 non-null      float64
+#   4   prop               6 non-null      float64
+#   5   prop_intraturismo  5 non-null      float64
 #  
-#  |    | rama_etq    |    emp |    prop |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | Gastronomía | 871409 | 44.5475 |
+#  |    |   anio | agregado   | industria                      |   empleo_total |     prop |   prop_intraturismo |
+#  |---:|-------:|:-----------|:-------------------------------|---------------:|---------:|--------------------:|
+#  |  0 |   2022 | Turística  | 1. Alojamiento para visitantes |        92.3066 | 0.422351 |             7.70901 |
 #  
 #  ------------------------------
 #  
-#  ordenar_categorica(col_cat='rama_etq', order_cat=['Gastronomía', 'Transporte de pasajeros', 'Arte, recreación y cultura', 'Agencias de viaje', 'Alojamiento', 'Resto de actividades'])
-#  Index: 6 entries, 0 to 4
-#  Data columns (total 3 columns):
-#   #   Column    Non-Null Count  Dtype   
-#  ---  ------    --------------  -----   
-#   0   rama_etq  6 non-null      category
-#   1   emp       6 non-null      int64   
-#   2   prop      6 non-null      float64 
+#  identity()
+#  RangeIndex: 6 entries, 0 to 5
+#  Data columns (total 6 columns):
+#   #   Column             Non-Null Count  Dtype  
+#  ---  ------             --------------  -----  
+#   0   anio               6 non-null      int64  
+#   1   agregado           6 non-null      object 
+#   2   industria          6 non-null      object 
+#   3   empleo_total       6 non-null      float64
+#   4   prop               6 non-null      float64
+#   5   prop_intraturismo  5 non-null      float64
 #  
-#  |    | rama_etq    |    emp |    prop |
-#  |---:|:------------|-------:|--------:|
-#  |  0 | Gastronomía | 871409 | 44.5475 |
+#  |    |   anio | agregado   | industria                      |   empleo_total |     prop |   prop_intraturismo |
+#  |---:|-------:|:-----------|:-------------------------------|---------------:|---------:|--------------------:|
+#  |  0 |   2022 | Turística  | 1. Alojamiento para visitantes |        92.3066 | 0.422351 |             7.70901 |
 #  
 #  ------------------------------
 #  
