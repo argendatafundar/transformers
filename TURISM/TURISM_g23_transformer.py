a178d2994caaaa27038ed3fbda99ad8b931fad95
Igancio-ibarra <64967345+Ignacio-Ibarra@users.noreply.github.com>
2025-12-17 14:40:47 -0300

    Updated TURISM/TURISM_g23_transformer.py

--- TURISM/TURISM_g23_transformer.py
+++ TURISM/TURISM_g23_transformer.py
@@ -4,17 +4,15 @@ from data_transformers import chain, transformer
 
 #  DEFINITIONS_START
 @transformer.convert
-def ordenar_dos_columnas(df, col1:str, order1:list[str], col2:str, order2:list[str]):
-    import pandas as pd
-    df[col1] = pd.Categorical(df[col1], categories=order1, ordered=True)
-    df[col2] = pd.Categorical(df[col2], categories=order2, ordered=True)
-    return df.sort_values(by=[col1,col2])
+def query(df: DataFrame, condition: str):
+    df = df.query(condition)    
+    return df
 #  DEFINITIONS_END
 
 
 #  PIPELINE_START
 pipeline = chain(
-	ordenar_dos_columnas(col1='anio', order1=[1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024], col2='region', order2=['Europa', 'Asia', 'Américas', 'Oceanía', 'África'])
+	query(condition='anio >= 1995')
 )
 #  PIPELINE_END
 
@@ -34,18 +32,18 @@ pipeline = chain(
 #  
 #  ------------------------------
 #  
-#  ordenar_dos_columnas(col1='anio', order1=[1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024], col2='region', order2=['Europa', 'Asia', 'Américas', 'Oceanía', 'África'])
-#  Index: 245 entries, 2 to 244
+#  query(condition='anio >= 1995')
+#  Index: 150 entries, 95 to 244
 #  Data columns (total 3 columns):
-#   #   Column           Non-Null Count  Dtype   
-#  ---  ------           --------------  -----   
-#   0   anio             245 non-null    category
-#   1   region           245 non-null    category
-#   2   expo_turisticas  245 non-null    float64 
+#   #   Column           Non-Null Count  Dtype  
+#  ---  ------           --------------  -----  
+#   0   anio             150 non-null    int64  
+#   1   region           150 non-null    object 
+#   2   expo_turisticas  150 non-null    float64
 #  
 #  |    |   anio | region   |   expo_turisticas |
 #  |---:|-------:|:---------|------------------:|
-#  |  2 |   1976 | Europa   |           20.3868 |
+#  | 95 |   1995 | Américas |           103.625 |
 #  
 #  ------------------------------
 #  
